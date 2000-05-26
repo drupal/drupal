6e4401daf38b2801ed0326818b6f80df8c86661c
dries <dries>
2000-05-26 07:24:29 +0000

    ------------------------------- IMPORTANT: temporary theme fix! -------------------------------
    * Added a *temporary* (see below) file called 'reset.php'.  Visiting
      this page at http://beta.drop.org/reset.php will reset your drop.org
      cookie.  This will automatically make you logout from your drop.org
      account, eventually (read: hopefully) fixing some cookie problems
      (if any).
      Example: if you can't surf the site anymore because of the theme
               problem, you can visit reset.php to logout.  This will dump
               you on the default theme, which you should always be able
               to use no matter what.
    * Note that this is a temporary file/fix!  Maybe we should start making
      a development page which groups this kind of features.  For instance,
      we could add a 'cookie reset', 'cookie dump', 'cookie examine', etc.
      If non-developers need this kind of features too, then we might want
      to add it to the account settings, make a helpdesk.php page or
      whatever.  Yes or no?  Suggestions?

---
+++ reset.php
@@ -0,0 +1,6 @@
+<? setcookie("user"); ?>
+
+<PRE>
+Your cookie has been reset.  This implies you are logged out from your
+account which means you will at least be able to use the default theme.
+</PRE>
