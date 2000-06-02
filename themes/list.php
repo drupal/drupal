5aa268d6be019396e48f0c23a835d22a57e997c9
natrak <natrak>
2000-06-02 18:36:50 +0000

    Think this is all of it :)

--- themes/list.php
+++ themes/list.php
@@ -1,7 +1,7 @@
 <?php
- $handle=opendir('themes');
+ $handle = opendir('themes');
  while ($file = readdir($handle)) {
-   if(!ereg("^\.",$file) && file_exists("themes/$file/theme.class")) $themelist .= "$file ";
+   if(!ereg("^\.",$file) && file_exists("themes/$file/theme.class")) $themelist[] = $file;
  }
  closedir($handle);
 ?>
