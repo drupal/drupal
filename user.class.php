e8298816587f79e090cb6e78ea17b00fae705deb
natrak <>
2000-06-02 18:43:11 +0000

    CVS drives me nuts *G*

--- user.class.php
+++ user.class.php
@@ -13,7 +13,7 @@ class User {
   }
   function update() {
     dbconnect();
-    $result = mysql_query("SELECT * FROM users WHERE id=$this->id") or die(sprintf("Critical error at line %d in %s: %s", __LINE__, __FILE__, mysql_error()));
+    $result = mysql_query("SELECT * FROM testusers WHERE id=$this->id") or die(sprintf("Critical error at line %d in %s: %s", __LINE__, __FILE__, mysql_error()));
     if (mysql_num_rows($result) == 1) {
       foreach (mysql_fetch_array($result) as $key=>$value) { $this->$key = $value; }
     }
@@ -23,7 +23,7 @@ class User {
       $this->last_access = time();
       $this->last_host = (!empty($GLOBALS[REMOTE_HOST]) ? $GLOBALS[REMOTE_HOST] : $GLOBALS[REMOTE_ADDR] );
       dbconnect();
-      mysql_query("UPDATE users SET last_access='$this->last_access',last_host='$this->last_host' WHERE id=$this->id") or die(sprintf("Critical error at line %d in %s: %s", __LINE__, __FILE__, mysql_error()));
+      mysql_query("UPDATE testusers SET last_access='$this->last_access',last_host='$this->last_host' WHERE id=$this->id") or die(sprintf("Critical error at line %d in %s: %s", __LINE__, __FILE__, mysql_error()));
       if ($this->access & $access || $access == 0) return 1;
     }
     return 0;
