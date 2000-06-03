439cde61f0bb469e4beb7ccf8e04de5c2b000956
dries <dries>
2000-06-03 11:39:07 +0000

    * Improved calendar.class.php speed-wise: did some inline caching to make   rendering a calendar less expensive. * Added a function displayCalendar($theme, $active_date) to functions.inc. * Adjusted index.php so it would support URIs formated like   "?date=$unix_timestamp". * Integrated the calendar in my personal theme: themes/Dries/theme.class.   If you want to include the calendar in your theme, check the code in my   $theme->footer(): global $date and call displayCalendar($theme, $date).
    Check the main page at http://beta.drop.org/ with theme 'Dries' to see it
    yourself!

--- index.php
+++ index.php
@@ -7,18 +7,20 @@ if (($url) && (strstr(getenv("HTTP_REFERER"), $url))) {
   addRefer($url);
 }
 
-
 include "theme.inc";
 $theme->header();
 
 dbconnect();
 
-if (isset($user->storynum)) $number = $user->storynum; else $number = 10;
+### Initialize variables:
+$number = ($user->storynum) ? $user->storynum : 10;
+$date = ($date) ? $date : time();
 
-$result = mysql_query("SELECT * FROM stories ORDER BY sid DESC LIMIT $number");
+### Perform query:
+$result = mysql_query("SELECT * FROM stories WHERE time <= $date ORDER BY sid DESC LIMIT $number");
 
+### Display stories:
 while ($story = mysql_fetch_object($result)) {
-
   ### Compose more-link:
   $morelink = "[ ";
   if ($story->article) {
@@ -28,16 +30,15 @@ while ($story = mysql_fetch_object($result)) {
     $bytes = strlen($story->article);
     $morelink .= "\"><FONT COLOR=\"$theme->hlcolor2\"><B>read more</B></FONT></A> | $bytes bytes in body | "; 
   }
-
   $query = mysql_query("SELECT sid FROM comments WHERE sid = $story->sid");
   if (!$query) { $count = 0; } else { $count = mysql_num_rows($query); }
-
   $morelink .= "<A HREF=\"article.php?sid=$story->sid";
   if (isset($user->umode)) { $morelink .= "&mode=$user->umode"; } else { $morelink .= "&mode=threaded"; }
   if (isset($user->uorder)) { $morelink .= "&order=$user->uorder"; } else { $morelink .= "&order=0"; }
   if (isset($user->thold)) { $morelink .= "&thold=$user->thold"; } else { $morelink .= "&thold=0"; }
   $morelink .= "\"><FONT COLOR=\"$theme->hlcolor2\">$count comments</FONT></A> ]";
 
+  ### Display story:
   $theme->abstract($story->aid, $story->informant, $story->time, stripslashes($story->subject), stripslashes($story->abstract), stripslashes($story->comments), $story->category, $story->department, $morelink);
 }
 
