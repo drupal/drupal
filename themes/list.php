a1c9695e13f8d9ba0254f368ffc4fa4b8221a8d0
dries <dries>
2000-06-05 15:49:35 +0000

    * Enhancement: removed themes/list.php by integrating it directly into   account.php.  list.php was just being clumpsy and did simply not   belong in the themes/-directory.

--- themes/list.php
+++
@@ -1,7 +0,0 @@
-<?php
- $handle = opendir('themes');
- while ($file = readdir($handle)) {
-   if(!ereg("^\.",$file) && file_exists("themes/$file/theme.class")) $themelist[] = $file;
- }
- closedir($handle);
-?>
