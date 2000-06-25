9966696dd63323afc868e54689c66ea5eec5fa92
Dries Buytaert <dries@buytaert.net>
2000-06-25 16:53:56 +0000

    * Small bugfixes and minor updates.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $number = ($user->storynum) ? $user->storynum : 10;
 $date = ($date) ? $date : time();
 
 ### Perform query:
-$result = db_query("SELECT stories.*, users.userid, COUNT(comments.sid) AS comments FROM stories LEFT JOIN comments ON stories.id = comments.sid LEFT JOIN users ON stories.author = users.id WHERE stories.status = 2 AND stories.timestamp <= $date GROUP BY stories.id ORDER BY stories.id DESC LIMIT $number");
+$result = db_query("SELECT stories.*, users.userid, COUNT(comments.sid) AS comments FROM stories LEFT JOIN comments ON stories.id = comments.sid LEFT JOIN users ON stories.author = users.id WHERE stories.status = 2 AND stories.timestamp <= $date GROUP BY stories.id ORDER BY stories.timestamp DESC LIMIT $number");
   // Note on performance: 
   //       we use a LEFT JOIN to retrieve the number of comments associated 
   //       with each story.  By retrieving this data now (outside the while-
