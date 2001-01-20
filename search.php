0c7caa41cbcb37a8c2f3067e7ea2350d292c38b7
dries <dries>
2001-01-20 12:20:30 +0000

    - updated/improved discussion engine: it needs additional fine-tuning   though but I think this is stable enough for public consumption and   real-life testing.    ==> a first big step towards a flexible comment engine.
    IMPORTANT:
      - Required theme updatins:
    
        UnConeD: check your $theme->controls() as I added a very, very
                 dummy implementation
    
      - Required database updates:
    
        alter table users modify mode tinyint(1) DEFAULT '' NOT NULL;
        alter table comments change sid lid int(6) DEFAULT '0' NOT NULL;
        alter table comments add link varchar(16) DEFAULT '' NOT NULL;
        update comments set link = 'story';

--- search.php
+++ search.php
@@ -39,7 +39,7 @@
  $output .= "  <TD>\n";
    
  // Compose and perform query:
- $query = "SELECT s.id, s.subject, u.userid, s.timestamp, COUNT(c.cid) AS comments FROM stories s LEFT JOIN users u ON s.author = u.id LEFT JOIN comments c ON s.id = c.sid WHERE s.status = 2 ";
+ $query = "SELECT s.id, s.subject, u.userid, s.timestamp, COUNT(c.cid) AS comments FROM stories s LEFT JOIN users u ON s.author = u.id LEFT JOIN comments c ON s.id = c.lid WHERE s.status = 2 ";
  $query .= ($author) ? "AND u.userid = '$author' " : "";
  $query .= ($terms) ? "AND (s.subject LIKE '%$terms%' OR s.abstract LIKE '%$terms%' OR s.updates LIKE '%$terms%') " : "";
  $query .= ($category) ? "AND s.category = '$category' GROUP BY s.id " : "GROUP BY s.id ";
@@ -51,7 +51,7 @@
 
  while ($entry = db_fetch_object($result)) {
    $num++;
-   $output .= "<P>$num) <B><A HREF=\"discussion.php?id=$entry->id\">". check_output($entry->subject) ."</A></B> (". format_plural($entry->comments, "comment", comments) .")<BR><SMALL>by ". format_username($entry->userid) ."</B>, posted on ". format_date($entry->timestamp) .".</SMALL></P>\n";
+   $output .= "<P>$num) <B><A HREF=\"story.php?id=$entry->id\">". check_output($entry->subject) ."</A></B> (". format_plural($entry->comments, "comment", comments) .")<BR><SMALL>by ". format_username($entry->userid) ."</B>, posted on ". format_date($entry->timestamp) .".</SMALL></P>\n";
  }
 
  if ($num == 0) $output .= "<P>Your search did <B>not</B> match any articles in our database: <UL><LI>Try using fewer words.</LI><LI>Try using more general keywords.</LI><LI>Try using different keywords.</LI></UL></P>\n";
