fcbb1fae4cf59d9ecbfd692aa425a3c5a053fada
dries <dries>
2001-02-17 12:59:22 +0000

    - More updates on the translation support

--- story.php
+++ story.php
@@ -9,27 +9,27 @@ function story_render($id, $cid) {
   $story = db_fetch_object(db_query("SELECT s.*, u.userid FROM stories s LEFT JOIN users u ON s.author = u.id WHERE s.id = $id"));
 
   if (story_visible($story)) {
-    $theme->article($story, "[ <A HREF=\"story.php?op=reply&id=$id&pid=0\">reply to this story</A> ]");
+    $theme->article($story, "[ <A HREF=\"story.php?op=reply&id=$id&pid=0\">". t("reply to this story") ."</A> ]");
     comment_render($id, $cid);
   }
   else {
-    $theme->box("Warning message", "The story you requested is not available or does not exist.");
+    $theme->box(t("Warning message"), t("The story you requested is not available or does not exist."));
   }
 }
 
 switch($op) {
-  case "Preview comment":
+  case t("Preview comment"):
     $theme->header();
     comment_preview($pid, $id, $subject, $comment);
     $theme->footer();
     break;
-  case "Post comment":
+  case t("Post comment"):
     comment_post($pid, $id, $subject, $comment);
     $theme->header();
     story_render($id, $cid);
     $theme->footer();
     break;
-  case "Add comment":
+  case t("Add comment"):
     $theme->header();
     comment_reply($cid, $id);
     $theme->footer();
@@ -39,13 +39,13 @@ switch($op) {
     comment_reply($pid, $id);
     $theme->footer();
     break;
-  case "Update settings":
+  case t("Update settings"):
     comment_settings($mode, $order, $threshold);
     $theme->header();
     story_render($id, $cid);
     $theme->footer();
     break;
-  case "Moderate comments":
+  case t("Moderate comments"):
     comment_moderate($moderate);
     $theme->header();
     story_render($id, $cid);
