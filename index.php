d52f4bdaca93d37fde982d4555d24e9a0ce81fe4
dries <dries>
2001-03-07 21:29:40 +0000

    A rather large and important update:
    revised most of the SQL queries and tried to make drupal as secure as possible (while trying to avoid redundant/duplicate checks).  For drupal's sake, try to screw something up.  See the mail about PHPNuke being hacked appr. 6 days ago.  The one who finds a problem is rewarded a beer (and I'm willing to ship it to Norway if required).  I beg you to be evil.  Try dumping a table a la "http://localhost/index.php?date=77778;DROP TABLE users" or something. ;)

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once "includes/common.inc";
 
 // Initialize/pre-process variables:
 $number = ($user->stories) ? $user->stories : 10;
-$date = ($date) ? $date : time();
+$date = ($date > 0) ? $date : time();
 
 // Perform query:
 $result = db_query("SELECT stories.*, users.userid, COUNT(comments.lid) AS comments FROM stories LEFT JOIN comments ON stories.id = comments.lid LEFT JOIN users ON stories.author = users.id WHERE stories.status = 2 ". ($section ? "AND section = '$section' " : "") ."AND stories.timestamp <= $date GROUP BY stories.id ORDER BY stories.timestamp DESC LIMIT $number");
