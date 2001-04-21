cccb132c81e82cc01ca7f014d185c1ecce864377
Dries Buytaert <dries@buytaert.net>
2001-04-21 14:19:20 +0000

    - Addition: added a "promote" field to the node table, which   makes "promoting nodes" to the main page possible.  Stories   and reviews could be promoted by default, but - on accasion   a good book entry could be manually promoted too.  Thus all   existing content types can be shown on the main page, not   just stories.
      Requires a SQL update, see 2.00-to-x.xx.sql!
    
    - Addition: implemented "auto-post new submissions" feature
      to disable or by-pass the moderation queue in addition to
      "moderate new submissions".
    
      TODO: admin moderation versus registered user moderation.
    
    - Addition: added category and topic support to page.module.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once "includes/common.inc";
 
 if (variable_get(dev_timing, 0)) timer_start();
 
-$result = db_query("SELECT nid FROM node WHERE type = 'story' AND status = '$status[posted]' AND timestamp <= ". ($date > 0 ? $date : time()) ." ". ($category ? "AND cid = '$category'" : "") ." ". ($topic ? "AND tid = '$topic'" : "") ."  ORDER BY timestamp DESC LIMIT ". ($user->nodes ? $user->nodes : 10));
+$result = db_query("SELECT nid FROM node WHERE promote = '1' AND status = '$status[posted]' AND timestamp <= ". ($date > 0 ? $date : time()) ." ". ($category ? "AND cid = '$category'" : "") ." ". ($topic ? "AND tid = '$topic'" : "") ."  ORDER BY timestamp DESC LIMIT ". ($user->nodes ? $user->nodes : 10));
 
 $theme->header();
 while ($node = db_fetch_object($result)) {
