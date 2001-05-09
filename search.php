aade73751073049d688277479e0ba388bce120d4
dries <dries>
2001-05-09 18:28:07 +0000

    - Code review: improved search API, tidied up the search related code,   and fixed a tiny bug when searching for users.

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ include_once "includes/common.inc";
 
 function find_module($name) {
   global $options, $type;
-  if (module_hook($name, "find")) $options .= "<OPTION VALUE=\"$name\"". ($name == $type ? " SELECTED" : "") .">$name</OPTION>\n";
+  if (module_hook($name, "search")) $options .= "<OPTION VALUE=\"$name\"". ($name == $type ? " SELECTED" : "") .">$name</OPTION>\n";
 }
 
 module_iterate("find_module");
