03c06a86ebb9521f0a81343e1648a87c5ea9247f
Dries Buytaert <dries@buytaert.net>
2001-06-17 18:31:25 +0000

    Oops, a rather large commit:
    - Changed meta.module, node.module and index.php to use comma-seperated
      lists of attributes rather then "foo=a,bar=b" lists.  This makes it a
      a lot easier to use both modules.  In addition, error handling can be
      discarded as it can't be made any simpler, really ...
    
      It fits rather nicely in Drupal's design so I'm getting more and more
      happy with this meta.module (but we are not 100% there yet).
    
    - node.module, node.inc:
    
       + Improved the node-related admin interface so that navigating back
         and forth the administrative menus is made both easier and faster.
    
       + Removed some redundant database fields from the node table.  See
         2.00-to-x.xx.sql!
    
       + Added 2 news hooks called "node_insert" and "node_update".  Just
         like this is the case with the existing hook "node_delete" these
         new hooks will automatically get called when a node has been
         inserted or udpated.  Note that this is an optional call-back that
         only needs to be implemented when required.  With the addition of
         these two hooks, the node mechanism (version 1) is pretty well
         completed.
    
    - watchdog.module:
       + Fixed bug whit the 'regular messages' query in the watchdog.module.
    
    - book.module:
       + Fixed bug in book.module: the 'parent' was not set properly when
         updating a book page.
       + Made it so that older versions of a book page are automatically
         reactived upon deletion of the most recent version, i.e. when
         doing a version roll-back.
    
    - comment.inc:
       + Undid Remco's patch to comment.inc; it does not work in some cases.
    
    
    - conf.module:
       + Fine-tuned some of the options in conf.module a bit.
    
    - marvin.theme:
       + Visual changes to make it look better on Windows browsers.  Mind
         to give some feedback on this?
       + Fixed 3 HTML typos/bugs.
       + XHTML-ified the theme at a best effort basis; I didn't carry the
         XHTML specification with me.
       + Made use of the theme_slogan variable to display the site's slogan.
       + As soon we have at least one valid XHTML theme we can wonder on how
         to integrate other XML namespaces (cfr. MathML story at drop.org).
    
    - database.mysql:
       + Updated database.mysql so that it contains all the latest "database
         patches".

--- index.php
+++ index.php
@@ -4,13 +4,7 @@ include_once "includes/common.inc";
 
 page_header();
 
-foreach(explode("&", $QUERY_STRING) as $attribute) {
-  if ($attribute) $query .= "attributes LIKE '%". check_input($attribute) ."%' AND ";
-}
-
-$query = !$date ? $query : "";
-
-$result = db_query("SELECT nid, type FROM node WHERE $query promote = '1' AND status = '". node_status("posted") ."' AND timestamp <= '". ($date > 0 ? check_input($date) : time()) ."' ORDER BY timestamp DESC LIMIT ". ($user->nodes ? $user->nodes : variable_get(default_nodes_main, 10)));
+$result = db_query("SELECT nid, type FROM node WHERE ". ($meta ? "attributes LIKE '%". check_input($meta) ."%' AND " : "") ." promote = '1' AND status = '". node_status("posted") ."' AND timestamp <= '". ($date > 0 ? check_input($date) : time()) ."' ORDER BY timestamp DESC LIMIT ". ($user->nodes ? $user->nodes : variable_get(default_nodes_main, 10)));
 
 $theme->header();
 while ($node = db_fetch_object($result)) {
