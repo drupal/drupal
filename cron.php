04369e6a68b144ed906ceb6c52fcbf84251eb19c
Dries Buytaert <dries@buytaert.net>
2001-07-16 14:43:56 +0000

    - cron.php:     + potential improvement (thanks KJ)

--- cron.php
+++ cron.php
@@ -2,6 +2,20 @@
 
 include_once "includes/common.inc";
 
-foreach (module_list() as $module) module_invoke($module, "cron");
+/*
+** If not in 'safe mode', increase the maximum execution time:
+*/
+
+if (!get_cfg_var("safe_mode")) { 
+  set_time_limit(180);
+}
+
+/*
+** Iterate through the modules calling their cron handlers (if any):
+*/
+
+foreach (module_list() as $module) {
+  module_invoke($module, "cron");
+}
 
 ?>
