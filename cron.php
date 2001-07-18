b792d171dd6ee4813c7c59a0f1b35491f38ab836
dries <dries>
2001-07-18 08:09:45 +0000

    - node.module:     + added missing t()-functions (reported by Nick).
    - locale.module:
        + renamed '$lang' to '$language' (reported by Nick).

--- cron.php
+++ cron.php
@@ -6,7 +6,7 @@ include_once "includes/common.inc";
 ** If not in 'safe mode', increase the maximum execution time:
 */
 
-if (!get_cfg_var("safe_mode")) { 
+if (!get_cfg_var("safe_mode")) {
   set_time_limit(180);
 }
 
