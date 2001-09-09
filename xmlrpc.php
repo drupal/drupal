1b7510eebf0da114677408674e552201ff0711c9
Dries Buytaert <dries@buytaert.net>
2001-09-09 16:47:10 +0000

    - Added an XML-RPC server.  Modules that want to export remote procedure   calls can implement the new 'xmlrpc' hook.
      Example:
    
       function mymodule_xmlrpc() {
         return array("drupal.myfunction" => array("function" => "mymodule_myfunction"));
       }

---
+++ xmlrpc.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once("includes/xmlrpcs.inc");
+include_once "includes/common.inc";
+
+$functions = array();
+
+foreach (module_list() as $name) {
+  if (module_hook($name, "xmlrpc")) {
+    $functions = array_merge($functions, module_invoke($name, "xmlrpc"));
+  }
+}
+
+$server = new xmlrpc_server($functions);
+
+?>
