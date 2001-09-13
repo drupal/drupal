c9528f4c734fc34dbab6924871fcf00a200de2cf
dries <dries>
2001-09-13 19:22:54 +0000

    - added a 3.00-to-x.xx.sql file : do _NOT_ apply these yet

---
+++ updates/3.00-to-x.xx.sql
@@ -0,0 +1,23 @@
+
+ALTER TABLE boxes DROP link;
+
+ALTER TABLE users RENAME AS user;
+ALTER TABLE user DROP INDEX real_email;
+ALTER TABLE user DROP fake_email;
+ALTER TABLE user DROP nodes;
+ALTER TABLE user DROP bio;
+ALTER TABLE user DROP hash;
+ALTER TABLE user ADD session varchar(32) DEFAULT '' NOT NULL;
+ALTER TABLE user ADD jabber varchar(128) DEFAULT '' NULL;
+ALTER TABLE user ADD drupal varchar(128) DEFAULT '' NULL;
+ALTER TABLE user ADD init varchar(64) DEFAULT '' NULL;
+ALTER TABLE user CHANGE passwd pass varchar(24) DEFAULT '' NOT NULL;
+ALTER TABLE user CHANGE real_email mail varchar(64) DEFAULT '' NULL;
+ALTER TABLE user CHANGE last_access timestamp int(11) DEFAULT '0' NOT NULL;
+ALTER TABLE user CHANGE last_host hostname varchar(128) DEFAULT '' NOT NULL;
+ALTER TABLE user CHANGE id uid int(10) unsigned DEFAULT '0' NOT NULL auto_increment;
+ALTER TABLE user CHANGE url homepage varchar(128) DEFAULT '' NOT NULL;
+UPDATE user SET status = 1 WHERE status = 2;
+UPDATE user SET name = userid;
+ALTER TABLE user DROP userid;
+UPDATE user SET init = mail;
