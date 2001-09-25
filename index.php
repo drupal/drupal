09231ba0a61594bdf8839d4e111f1b6f522d0242
kjartan <kjartan>
2001-09-25 22:13:33 +0000

    - added node_page() to list what index.php used to list. - made the front page of the system configurable. Now any <node>_page()   can be set as the default page. Default is node_page().

--- index.php
+++ index.php
@@ -5,19 +5,12 @@ include_once "includes/common.inc";
 page_header();
 
 
-$theme->header();
+//$theme->header();
 
-if (user_access("access content")) {
-  $result = db_query("SELECT nid, type FROM node WHERE ". ($meta ? "attributes LIKE '%". check_input($meta) ."%' AND " : "") ." promote = '1' AND status = '". node_status("posted") ."' AND timestamp <= '". ($date > 0 ? check_input($date) : time()) ."' ORDER BY timestamp DESC LIMIT ". ($user->nodes ? $user->nodes : variable_get(default_nodes_main, 10)));
-  while ($node = db_fetch_object($result)) {
-    node_view(node_get_object(array("nid" => $node->nid, "type" => $node->type)), 1);
-  }
-}
-else {
-  $theme->box(t("Access denied"), message_access());
-}
+$function = variable_get("site_frontpage", "node") ."_page";
+$function();
 
-$theme->footer();
+//$theme->footer();
 
 page_footer();
 
