4d1f9d44ffd9dd06d17c47002e07d511093761fe
kjartan <kjartan>
2001-10-20 20:58:59 +0000

    - added a new configuration option that lets you set various index.php   variables.
      Example: set site_frontpage to "node" and site_frontpage_extra to
      "$meta = 'news'" and only nodes with that meta tag will be displayed on
       your main page.
    
      This requires some internal knowledge of how the various modules work
      and what settings can be passed to <module>_page() functions.

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-// $Id: index.php,v 1.49 2001/10/20 18:57:07 kjartan Exp $
+// $Id: index.php,v 1.50 2001/10/20 20:58:59 kjartan Exp $
 
 include_once "includes/common.inc";
 
 page_header();
 
+eval(variable_get("site_frontpage_extra", "") .";");
 $function = variable_get("site_frontpage", "node") ."_page";
 $function();
 
