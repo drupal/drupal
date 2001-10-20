3b7425aefb77641468a2183e83481e644b4eb9d6
Kjartan Mannes <kjartan@2.no-reply.drupal.org>
2001-10-20 20:58:59 +0000

    - added a new configuration option that lets you set various index.php   variables.
      Example: set site_frontpage to "node" and site_frontpage_extra to
      "$meta = 'news'" and only nodes with that meta tag will be displayed on
       your main page.
    
      This requires some internal knowledge of how the various modules work
      and what settings can be passed to <module>_page() functions.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include_once "includes/common.inc";
 
 page_header();
 
+eval(variable_get("site_frontpage_extra", "") .";");
 $function = variable_get("site_frontpage", "node") ."_page";
 $function();
 
