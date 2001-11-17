900caec1c9517b2382ef038a8eb1e3953f4876c4
kjartan <kjartan>
2001-11-17 15:18:16 +0000

    - changed the $function code to use module_invoke() as its a lot cleaner and   more in tune with the rest of Drupal.

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-// $Id: index.php,v 1.51 2001/11/01 11:00:46 dries Exp $
+// $Id: index.php,v 1.52 2001/11/17 15:18:16 kjartan Exp $
 
 include_once "includes/common.inc";
 
 page_header();
 
 eval(variable_get("site_frontpage_extra", "") .";");
-$function = variable_get("site_frontpage", "node") ."_page";
-$function();
+module_invoke(variable_get("site_frontpage", "node"), "page");
 
 page_footer();
 
