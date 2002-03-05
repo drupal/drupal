35a9fd1e351d7505f210d5bc1fd908bf5fe3e5dc
Kjartan Mannes <kjartan@2.no-reply.drupal.org>
2002-03-05 22:53:51 +0000

    - specifying an invalid module will redirect you to the index page instead of   showing a blank page.

--- module.php
+++ module.php
@@ -3,10 +3,13 @@
 
 include_once "includes/common.inc";
 
-page_header();
-
-module_invoke($mod, "page");
-
-page_footer();
+if (module_hook($mod, "page")) {
+  page_header();
+  module_invoke($mod, "page");
+  page_footer();
+}
+else {
+  header("Location: index.php");
+}
 
 ?>
