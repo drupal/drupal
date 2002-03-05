ab5f38150cd8bc08ef326fb556f0884dad747bc5
kjartan <kjartan>
2002-03-05 22:53:51 +0000

    - specifying an invalid module will redirect you to the index page instead of   showing a blank page.

--- module.php
+++ module.php
@@ -1,12 +1,15 @@
 <?php
-// $Id: module.php,v 1.12 2001/11/01 11:00:46 dries Exp $
+// $Id: module.php,v 1.13 2002/03/05 22:53:51 kjartan Exp $
 
 include_once "includes/common.inc";
 
-page_header();
-
-module_invoke($mod, "page");
-
-page_footer();
+if (module_hook($mod, "page")) {
+  page_header();
+  module_invoke($mod, "page");
+  page_footer();
+}
+else {
+  header("Location: index.php");
+}
 
 ?>
