ea6682bb2334d4e0d0cf3702d697a0d78e075a14
Dries Buytaert <dries@buytaert.net>
2002-04-25 18:44:22 +0000

    - Wrapped admin_page() in page_header() and page_footer() calls, and made   it display an "access denied" message when access without the proper   access rights.  Patch by Moshe.

--- admin.php
+++ admin.php
@@ -16,7 +16,7 @@ function admin_page($mod) {
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
   <html>
    <head>
-    <title><?php echo variable_get(site_name, "drupal"); ?> administration pages</title>
+    <title><?php echo variable_get("site_name", "drupal"); ?> administration pages</title>
    </head>
    <style>
     body { font-family: helvetica, arial; font-size: 12pt; }
@@ -49,6 +49,12 @@ function admin_page($mod) {
 }
 
 if (user_access("access administration pages")) {
+  page_header();  
   admin_page($mod);
+  page_footer();
 }
+else {
+  print message_access();
+}  
+
 ?>
