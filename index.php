2e43215bbb968f867da05123ba71f303f1aa324b
Dries Buytaert <dries@buytaert.net>
2002-10-18 10:05:46 +0000

    - Removed dead code.

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ include_once "includes/common.inc";
 page_header();
 
 if (module_hook(variable_get("site_frontpage", "node"), "page")) {
-  eval(variable_get("site_frontpage_extra", "") .";");
   module_invoke(variable_get("site_frontpage", "node"), "page");
 }
 else {
