1d901a159a3b08bdc002fd8a7864d52ec8552ef1
dries <dries>
2002-11-01 10:47:19 +0000

    - Fixed glitch in block module: the admin links were not composed   correctly.  Patch by Stefan.
    - Added missing t() functions and improved the %x directives for
      better readability and to ease the process of translating your
      site.  Patches by Stefan.
    
    - Made two small additions to the 'code-clean.sh' script; it will
      now remove patch related junk.
    
    NOTES:
    
    - I removed the &middot; related bits.  Let's tackle these later on
      in a separate patch after we got some sort of consensus.
    
    - I removed the 'module_exist("drupal")' check in the user module;
      I *think* it is incomplete and therefore incorrect.
    
    - Stefan, try using quotes in your translations and check whether
      everything still works.  Example: translate the "Create account"
      button to "Create \"haha\" account \'hihi\'" and see if you can
      still create new accounts.  Maybe automate this using a quick
      hack in the locale module ...

--- scripts/code-clean.sh
+++ scripts/code-clean.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 find . -name "*~" -type f | xargs rm -f
+find . -name "*.rej" -type f | xargs rm -f
+find . -name "*.orig" -type f | xargs rm -f
 find . -name "DEADJOE" -type f | xargs rm -f
 find . -name "*" -type f | grep -v ".psp" | grep -v ".gif" | grep -v ".jpg" | grep -v ".png" | grep -v ".tgz" | xargs perl -wi -pe 's/\s+$/\n/'
 find . -name "*" -type f | grep -v ".psp" | grep -v ".gif" | grep -v ".jpg" | grep -v ".png" | grep -v ".tgz" | xargs perl -wi -pe 's/\t/  /g'
