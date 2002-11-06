7648b3695f5dd1fc786e99835da687cb06a92d2a
kjartan <kjartan>
2002-11-06 13:45:45 +0000

    - testing new loginfo script.

--- scripts/mail-to-sql.php
+++ scripts/mail-to-sql.php
@@ -1,6 +1,6 @@
 #!/usr/local/bin/php -q
 <?php
-// $Id: mail-to-sql.php,v 1.4 2002/11/06 13:44:26 kjartan Exp $
+// $Id: mail-to-sql.php,v 1.5 2002/11/06 13:45:45 kjartan Exp $
 
 ini_set("include_path", ".:/home/dries/personal/cvs/web/pear:/home/dries/personal/cvs/web/drupal.org/x.x.x");
 include_once "includes/common.inc";
