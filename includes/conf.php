47e4f05f84ec43699b708028e5976a231760d00e
dries <dries>
2002-11-26 18:38:01 +0000

    - Added some comments/explanation wrt to the "$db_url" settings; this seemed   to cause problems/confusion.

--- includes/conf.php
+++ includes/conf.php
@@ -1,8 +1,15 @@
 <?php
-// $Id: conf.php,v 1.15 2002/04/14 19:34:02 kjartan Exp $
+// $Id: conf.php,v 1.16 2002/11/26 18:38:01 dries Exp $
 
 #
 # Database settings:
+#   Note that the $db_url variable gets parsed using PHP's built-in 
+#   URL parser (i.e. using the "parse_url()" function) so make sure
+#   not to confuse the parser.  In practice, you should avoid using
+#   special characters that are not used in "normal" URLs either.
+#   That is, the use of ':', '/', '@', '?', '=' and '#', ''', '"', 
+#   and so on is likely to confuse the parser; use alpha-numerical
+#   characters instead. 
 #
 
 # $db_url = "pgsql://user:password@hostname/database";
