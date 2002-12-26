c67e3ed2d28dd76c02acb60896c0c78161f18e2b
dries <dries>
2002-12-26 12:16:06 +0000

    - Made Drupal report an error when magic_quotes are disabled.

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-// $Id: index.php,v 1.54 2002/10/18 10:05:46 dries Exp $
+// $Id: index.php,v 1.55 2002/12/26 12:16:06 dries Exp $
 
 include_once "includes/common.inc";
 
 page_header();
+
+check_php_setting("magic_quotes_gpc", 0);
 
 if (module_hook(variable_get("site_frontpage", "node"), "page")) {
   module_invoke(variable_get("site_frontpage", "node"), "page");
