56e5c29b40c7d926433c07e7bedb8f928a448997
Dries Buytaert <dries@buytaert.net>
2003-01-04 23:43:49 +0000

    - Added a check for register_globals.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include_once "includes/common.inc";
 page_header();
 
 check_php_setting("magic_quotes_gpc", 0);
+check_php_setting("register_globals", 1);
 
 if (module_hook(variable_get("site_frontpage", "node"), "page")) {
   module_invoke(variable_get("site_frontpage", "node"), "page");
