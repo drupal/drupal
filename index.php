eaf68af96797baf46da5a61b42d6d5094e6e38aa
dries <dries>
2003-05-18 09:45:53 +0000

    - The page_footer() function was always called twice due to the introduction   of the "footer" hook (and because of the existence of the page module).   This caused the node view counter to be incremented twice per page view,   the cache being set twice, etc.  Quite a bug.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: index.php,v 1.61 2003/05/13 18:36:31 dries Exp $
+// $Id: index.php,v 1.62 2003/05/18 09:45:53 dries Exp $
 
 include_once "includes/common.inc";
 
@@ -9,15 +9,15 @@ if (isset($_GET["q"])) {
 
 if (isset($mod) && module_hook($mod, "page")) {
   if ($mod != "admin") {
-    page_header();
+    drupal_page_header();
   }
   module_invoke($mod, "page");
   if ($mod != "admin") {
-    page_footer();
+    drupal_page_footer();
   }
 }
 else {
-  page_header();
+  drupal_page_header();
 
   check_php_setting("magic_quotes_gpc", 0);
 
@@ -29,7 +29,7 @@ else {
     theme("footer");
   }
 
-  page_footer();
+  drupal_page_footer();
 }
 
 ?>
