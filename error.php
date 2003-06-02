1839f42c59c88003ac8877c1863d4d0c41808fa7
Dries Buytaert <dries@buytaert.net>
2003-06-02 20:04:40 +0000

    Patch by Kjartan:
     - Fixed the parse errors
     - Send the proper response code to the user

--- error.php
+++ error.php
@@ -3,10 +3,18 @@
 
 include_once "includes/common.inc";
 
-$errors = array(500 => "500 error: internal server error", 404 => "404 error: '".`$_SERVER["REDIRECT_URL"] ."' not found", 403 => "403 error: access denied - forbidden", 401 => "401 error: authorization required", 400 => "400 error: bad request");
+$errors = array(
+  500 => "500 error: internal server error",
+  404 => "404 error: '". $_SERVER["REDIRECT_URL"] ."' not found",
+  403 => "403 error: access denied - forbidden",
+  401 => "401 error: authorization required",
+  400 => "400 error: bad request"
+);
 
-watchdog("httpd", $errors[$_SERVER["REDIRECT_STATUS"]);
+if ($errors[$_SERVER["REDIRECT_STATUS"]]) {
+  watchdog("httpd", $errors[$_SERVER["REDIRECT_STATUS"]]);
+  header("HTTP/1.0 ". $errors[$_SERVER["REDIRECT_STATUS"]]);
+}
 
-drupal_goto($base_url);
-
+include_once("$base_url/index.php");
 ?>
