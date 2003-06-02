68ca58e922395b2379b00a870ec77965c00e971a
Dries Buytaert <dries@buytaert.net>
2003-06-02 22:23:02 +0000

    - Ignore ".eps.gz" files or Druplicon logos.

--- scripts/code-clean.sh
+++ scripts/code-clean.sh
@@ -4,5 +4,5 @@ find . -name ".#*" -type f | xargs rm -f
 find . -name "*.rej" -type f | xargs rm -f
 find . -name "*.orig" -type f | xargs rm -f
 find . -name "DEADJOE" -type f | xargs rm -f
-find . -type f | grep -v ".psp" | grep -v ".gif" | grep -v ".jpg" | grep -v ".png" | grep -v ".tgz" | xargs perl -wi -pe 's/\s+$/\n/'
-find . -type f | grep -v ".psp" | grep -v ".gif" | grep -v ".jpg" | grep -v ".png" | grep -v ".tgz" | xargs perl -wi -pe 's/\t/  /g'
+find . -type f | grep -v ".psp" | grep -v ".gif" | grep -v ".jpg" | grep -v ".png" | grep -v ".tgz" | grep -v "druplicon" | xargs perl -wi -pe 's/\s+$/\n/'
+find . -type f | grep -v ".psp" | grep -v ".gif" | grep -v ".jpg" | grep -v ".png" | grep -v ".tgz" | grep -v "druplicon" | xargs perl -wi -pe 's/\t/  /g'
