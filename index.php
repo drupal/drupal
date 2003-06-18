0c5b324809dcb44de8690f7bba21fec607bd67d1
Dries Buytaert <dries@buytaert.net>
2003-06-18 21:21:30 +0000

    - Improvement: made the default homepage setting more flexible.  Patch by   Eric A. Farris.

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ include_once "includes/common.inc";
 if (isset($_GET["q"])) {
   $mod = arg(0);
 }
+else {
+  $_GET["q"] = variable_get("site_frontpage", "node");
+  $mod = arg(0);
+}
 
 if (isset($mod) && module_hook($mod, "page")) {
   if ($mod != "admin") {
