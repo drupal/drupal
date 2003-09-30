5a667eb580be3f20bde78232f564152f1cb869a9
Dries Buytaert <dries@buytaert.net>
2003-09-30 17:01:34 +0000

    - Url aliasing improvements.  Patch by Matt.  See mailing list for more   information.

--- index.php
+++ index.php
@@ -3,22 +3,13 @@
 
 include_once "includes/common.inc";
 
-if (!empty($_GET["q"])) {
-  if (module_exist("node") && $path = node_get_alias($_GET["q"])) {
-    $_GET["q"] = $path;
-  }
-}
-else {
-  $_GET["q"] = variable_get("site_frontpage", "node");
-}
-
-$mod = arg(0);
-
 drupal_page_header();
 
 check_php_setting("magic_quotes_gpc", 0);
 
 menu_build("system");
+
+$mod = arg(0);
 
 if (isset($mod) && module_hook($mod, "page")) {
   module_invoke($mod, "page");
