b6097ab27992d5699de1cb7efdc78c1ed69d3399
dries <dries>
2004-01-02 22:42:14 +0000

    - Removing some dated scripts.

--- scripts/cvs-pass.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-srand (time());
-my $randletter = "(int (rand (26)) + (int (rand (1) + .5) % 2 ? 65 : 97))";
-my $salt = sprintf ("%c%c", eval $randletter, eval $randletter);
-my $plaintext = shift;
-my $crypttext = crypt ($plaintext, $salt);
-
-print "${crypttext}\n";
