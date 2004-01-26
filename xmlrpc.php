c325e684cae8bd774e928e95b1e56a521fcd231e
Dries Buytaert <dries@buytaert.net>
2004-01-26 18:51:37 +0000

    - Changed the order of include statements.

--- xmlrpc.php
+++ xmlrpc.php
@@ -1,9 +1,9 @@
 <?php
 // $Id$
 
-include_once "includes/xmlrpcs.inc";
 include_once "includes/bootstrap.inc";
 include_once "includes/common.inc";
+include_once "includes/xmlrpcs.inc";
 
 $functions = module_invoke_all("xmlrpc");
 
