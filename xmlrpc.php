ea8fe688a4e7e0d9ca981b3e06ac232416a26231
dries <dries>
2004-01-26 18:51:37 +0000

    - Changed the order of include statements.

--- xmlrpc.php
+++ xmlrpc.php
@@ -1,9 +1,9 @@
 <?php
-// $Id: xmlrpc.php,v 1.5 2003/11/18 19:44:35 dries Exp $
+// $Id: xmlrpc.php,v 1.6 2004/01/26 18:51:37 dries Exp $
 
-include_once "includes/xmlrpcs.inc";
 include_once "includes/bootstrap.inc";
 include_once "includes/common.inc";
+include_once "includes/xmlrpcs.inc";
 
 $functions = module_invoke_all("xmlrpc");
 
