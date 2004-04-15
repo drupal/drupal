d90e3bc4e4bbfc12fbbf717a551903c2845f4e25
dries <dries>
2004-04-15 20:49:39 +0000

    - Patch by JonBob/Jonathan: reworked the menu system so that menus are   configurable!  Menu items can be disabled, repositioned, added and   so on.
      Upgrading to requires you to run update.php.
    
      This functionality depricates some of the 'navigation modules' in the
      contributions repository.  Furthermore, modules can now 'suggest'
      menu items and site adminstrators can choose to enable them.  Modules
      in the contributions repository should try to take advantage of this.

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-// $Id: index.php,v 1.77 2003/12/16 21:06:33 dries Exp $
+// $Id: index.php,v 1.78 2004/04/15 20:49:39 dries Exp $
 
 include_once "includes/bootstrap.inc";
 drupal_page_header();
 include_once "includes/common.inc";
 
 fix_gpc_magic();
-
-menu_build("system");
 
 if (menu_active_handler_exists()) {
   menu_execute_active_handler();
