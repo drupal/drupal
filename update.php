ac3a7fc04dd9fba925a7f11088a2fef38e6de56f
Dries Buytaert <dries@buytaert.net>
2004-09-08 18:53:57 +0000

    - Fixed a warning.

--- update.php
+++ update.php
@@ -92,7 +92,7 @@ function update_page() {
       $dates[$i] = "No updates available";
 
       // make update form and output it.
-      $form .= form_select("Perform updates from", "start", (isset($selected) ? $selected : -1), $dates, "This defaults to the first available update since the last update you performed.");
+      $form = form_select("Perform updates from", "start", (isset($selected) ? $selected : -1), $dates, "This defaults to the first available update since the last update you performed.");
       $form .= form_submit("Update");
       print update_page_header("Drupal database update");
       print form($form);
