b7ee774e52cc8144080b56d5ca411c4d002f8aa9
dries <dries>
2004-11-23 06:43:21 +0000

    - Patch #13334 by Axel: added a simple script to prefix a database scheme.

---
+++ scripts/prefix.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# != 2 ]; then
+	cat >&2 << EOH
+This is Drupal database prefixer.
+
+Usage:
+  $0 prefix original_db.sql >prefixed_db.sql
+
+- all tables will prefixed with 'prefix'
+EOH
+
+exit 1;
+fi
+
+PRFX=$1;
+sed "s/^CREATE TABLE /CREATE TABLE $PRFX/;
+     s/^INSERT INTO /INSERT INTO $PRFX/;
+     s/^REPLACE /REPLACE $PRFX/;
+     s/^ALTER TABLE /ALTER TABLE $PRFX/" $2
+
