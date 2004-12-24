6d8cac8fd1155a2e6244093ade9450f8b7fb88ce
unconed <unconed>
2004-12-24 06:52:02 +0000

    #10457: Warnings during update process

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: update.php,v 1.142 2004/09/08 18:53:57 dries Exp $
+// $Id: update.php,v 1.143 2004/12/24 06:52:02 unconed Exp $
 
 /**
  * @file
@@ -59,9 +59,14 @@ function update_page_footer() {
 function update_page() {
   global $user, $sql_updates;
 
-  $edit = $_POST["edit"];
+  if (isset($_POST['edit'])) {
+    $edit = $_POST['edit'];
+  }
+  if (isset($_POST['op'])) {
+    $op = $_POST['op'];
+  }
 
-  switch ($_POST["op"]) {
+  switch ($op) {
     case "Update":
       // make sure we have updates to run.
       print update_page_header("Drupal database update");
