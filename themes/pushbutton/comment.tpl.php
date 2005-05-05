07cf2c69650502ab729a1edd3b5e414056780347
Dries Buytaert <dries@buytaert.net>
2005-05-05 10:58:27 +0000

    - Added the PHPTemplate port of pushbutton.

---
+++ themes/pushbutton/comment.tpl.php
@@ -0,0 +1,11 @@
+<div class="comment">
+  <?php if ($picture) : ?>
+    <?php print $picture ?>
+  <?php endif; ?>
+  <h3 class="title"><?php print $title ?></h3>
+  <div class="submitted"><?php print $submitted ?><?php if ($comment->new) : ?><span class="new"> *<?php print $new ?></span><?php endif; ?></div>
+  <div class="content"><?php print $content ?></div>
+  <!-- BEGIN: links -->
+  <div class="links">&raquo; <?php print $links ?></div>
+  <!-- END: links -->
+</div>
