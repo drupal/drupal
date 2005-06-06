26dfef95e9f49058fc5aa77a57c2fd0525aad661
dries <dries>
2005-06-06 14:07:04 +0000

    - Removing some whitespace

--- themes/bluemarine/node.tpl.php
+++ themes/bluemarine/node.tpl.php
@@ -2,7 +2,7 @@
     <?php if ($picture) {
       print $picture;
     }?>
-    <?php if ($page == 0) { ?><h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2><?php }; ?> 
+    <?php if ($page == 0) { ?><h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2><?php }; ?>
     <span class="submitted"><?php print $submitted?></span>
     <span class="taxonomy"><?php print $terms?></span>
     <div class="content"><?php print $content?></div>
