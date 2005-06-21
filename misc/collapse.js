cc56d3ab64220c00bad7247341ebb4cb231fc856
dries <dries>
2005-06-21 09:45:42 +0000

    - Patch #16204 by Thox: committed the collapsible form elements patch.
      NOTE: this patch works well, but the improved node edit form still has
            some rough edges.  It is important that we continue to improve
            usability.  Give it a try.

---
+++ misc/collapse.js
@@ -0,0 +1,48 @@
+if (isJsEnabled()) {
+  addLoadEvent(collapseAutoAttach);
+}
+
+function collapseAutoAttach() {
+  var fieldsets = document.getElementsByTagName('fieldset');
+  var legend, fieldset;
+  for (var i = 0; fieldset = fieldsets[i]; i++) {
+    if (!hasClass(fieldset, 'collapsible')) {
+      continue;
+    }
+    legend = fieldset.getElementsByTagName('legend');
+    if (legend.length == 0) {
+      continue;
+    }
+    legend = legend[0];
+    var a = document.createElement('a');
+    a.href = '#';
+    a.onclick = function() {
+      toggleClass(this.parentNode.parentNode, 'collapsed');
+      this.blur();
+      return false;
+    };
+    a.innerHTML = legend.innerHTML;
+    while (legend.hasChildNodes()) {
+      removeNode(legend.childNodes[0]);
+    }
+    legend.appendChild(a);
+    collapseEnsureErrorsVisible(fieldset);
+  }
+}
+
+function collapseEnsureErrorsVisible(fieldset) {
+  if (!hasClass(fieldset, 'collapsed')) {
+    return;
+  }
+  var inputs = [];
+  inputs = inputs.concat(fieldset.getElementsByTagName('input'));
+  inputs = inputs.concat(fieldset.getElementsByTagName('textarea'));
+  inputs = inputs.concat(fieldset.getElementsByTagName('select'));
+  for (var j = 0; j<3; j++) {
+    for (var i = 0; i < inputs[j].length; i++) {
+      if (hasClass(inputs[j][i], 'error')) {
+        return removeClass(fieldset, 'collapsed');
+      }
+    }
+  }
+}
