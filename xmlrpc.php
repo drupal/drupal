ff7047bfc4017ac350b04f06b879cbf6a2165e65
dries <dries>
2005-08-17 15:01:13 +0000

    - Patch #7458 by chx: merged the XML-RPC multicall support into xmlrpc() and use lazy-loading for the XML-RPC libraries.(performance improvement).

--- xmlrpc.php
+++ xmlrpc.php
@@ -1,14 +1,15 @@
 <?php
-// $Id: xmlrpc.php,v 1.12 2005/07/23 05:57:27 dries Exp $
+// $Id: xmlrpc.php,v 1.13 2005/08/17 15:01:13 dries Exp $
 
 /**
  * @file
  * PHP page for handling incoming XML-RPC requests from clients.
  */
 
-include_once 'includes/bootstrap.inc';
+include_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-include_once 'includes/xmlrpcs.inc';
+include_once './includes/xmlrpc.inc';
+include_once './includes/xmlrpcs.inc';
 
 xmlrpc_server(module_invoke_all('xmlrpc'));
 ?>
