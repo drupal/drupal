1d29abfe344fdb25964d666d880ac44342f63122
Steven Wittens <steven@10.no-reply.drupal.org>
2005-08-17 00:49:36 +0000

    - #28944: Remove old bugfix code from update.php.

--- update.php
+++ update.php
@@ -72,15 +72,6 @@ function update_page() {
       break;
 
     default:
-      // NOTE: We need the following five lines in order to fix a bug with
-      //       database.mysql (issue #15337).  We should be able to remove
-      //       this work around in the future.
-      $result = db_query("SELECT * FROM {variable} WHERE name = 'update_start' AND value LIKE '%;\"'");
-      if ($variable = db_fetch_object($result)) {
-        $variable->value = unserialize(substr($variable->value, 0, -2) .'";');
-        variable_set('update_start', $variable->value);
-      }
-
       $start = variable_get("update_start", 0);
       $i = 1;
       foreach ($sql_updates as $date => $sql) {
