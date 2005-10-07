af4888c18ed6e071d4c8994b0ee271dcbf0cb07b
dries <dries>
2005-10-07 06:09:13 +0000

    - Patch #29465: new form API by Adrian et al.
      TODO:
      + The contact.module was broken; a new patch for contact.module is needed.
      + Documentation is needed.
      + The most important modules need to be updated ASAP.

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: update.php,v 1.156 2005/08/31 17:56:07 dries Exp $
+// $Id: update.php,v 1.157 2005/10/07 06:09:13 dries Exp $
 
 /**
  * @file
@@ -54,11 +54,20 @@ function update_selection_page() {
   $dates[$i] = "No updates available";
 
   // make update form and output it.
-  $form = form_select("Perform updates from", "start", (isset($selected) ? $selected : -1), $dates, "This defaults to the first available update since the last update you performed.");
-  $form .= form_submit("Update");
+  $form['start'] = array(
+    type => 'select',
+    title => t('Perform updates from'),
+    default_value => (isset($selected) ? $selected : -1),
+    options => $dates,
+    description => t('This defaults to the first available update since the last update you performed.')
+  );
+  $form['submit'] = array(
+    type => 'submit',
+    value => t('Update')
+  );
 
   drupal_set_title('Drupal database update');
-  return form($form);
+  return drupal_get_form('update_script_selection_form', $form);
 }
 
 function update_do_updates() {
