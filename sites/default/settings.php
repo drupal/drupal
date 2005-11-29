1b9b51f3def992d2601a8f36607fc7de34527a49
Dries Buytaert <dries@buytaert.net>
2005-11-29 15:17:58 +0000

    - Patch #38841 by Morbus/Ber: improved documentation about $base_url and rolled back auto-detection.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -89,17 +89,16 @@ $db_prefix = '';
 /**
  * Base URL:
  *
- * The URL of your website's main page. It is not allowed to have
- * a trailing slash; Drupal will add it for you.
+ * The URL to your Drupal installation. It is not allowed
+ * to have a trailing slash; Drupal will add it for you.
  *
  * Examples:
- *   $base_url = 'http://localhost';
- *   $base_url = 'http://example.com/drupal';
- *
- * By default, Drupal tries to set the $base_url for you using
- * the code below.
+ *   $base_url = 'http://www.example.com';
+ *   $base_url = 'http://www.example.com:8888';
+ *   $base_url = 'http://www.example.com/drupal';
+ *   $base_url = 'https://www.example.com:8888/drupal';
  */
-$base_url = 'http://'. $_SERVER['HTTP_HOST'];
+$base_url = 'http://www.example.com';
 
 /**
  * PHP settings:
