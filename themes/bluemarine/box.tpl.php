84cb44c69833659fad30de073da5079ddfd5f6fb
Gerhard Killesreiter <killes_www_drop_org@227.no-reply.drupal.org>
2006-02-27 22:39:24 +0000

    #51153, Boxes may be themed without a title; don't spit the header., patch by Morbus

--- themes/bluemarine/box.tpl.php
+++ themes/bluemarine/box.tpl.php
@@ -1,5 +1,5 @@
   <div class="box">
-    <h2 class="title"><?php print $title; ?></h2>
+    <?php if ($title) { ?><h2 class="title"><?php print $title; ?></h2><?php } ?>
     <div class="content"><?php print $content; ?></div>
  </div>
 
--- themes/engines/phptemplate/box.tpl.php
+++ themes/engines/phptemplate/box.tpl.php
@@ -1,4 +1,6 @@
 <div class="box">
-  <h2><?php print $title ?></h2>
+  <?php if ($title): ?>
+  <h2 class="title"><?php print $title ?></h2>
+  <?php endif; ?>
   <div class="content"><?php print $content ?></div>
 </div>
