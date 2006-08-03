010eca98ecb89ce2841c17139363c6dce4ffb210
Steven Wittens <steven@10.no-reply.drupal.org>
2006-08-03 01:12:03 +0000

    - Fixing $db_url in settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -84,7 +84,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://bonsai_tree:leafy@localhost/bonsai_install';
+$db_url = 'mysql://username:password@localhost/databasename';
 $db_prefix = '';
 
 /**
