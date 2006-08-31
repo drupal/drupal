eb361f273b11bbcc453318c9523b1d05367557ce
dries <dries>
2006-08-31 07:21:43 +0000

    - Patch #81619 by m3avrck: clean up default phpTemplates.

--- themes/engines/phptemplate/box.tpl.php
+++ themes/engines/phptemplate/box.tpl.php
@@ -1,6 +1,8 @@
 <div class="box">
-  <?php if ($title): ?>
-  <h2 class="title"><?php print $title ?></h2>
-  <?php endif; ?>
+
+<?php if ($title): ?>
+  <h2><?php print $title ?></h2>
+<?php endif; ?>
+
   <div class="content"><?php print $content ?></div>
 </div>
--- themes/engines/phptemplate/comment.tpl.php
+++ themes/engines/phptemplate/comment.tpl.php
@@ -1,12 +1,20 @@
 <div class="comment<?php print ($comment->new) ? ' comment-new' : ''; print ($comment->status == COMMENT_NOT_PUBLISHED) ? ' comment-unpublished' : ''; ?> clear-block">
-  <?php if ($comment->new) : ?>
+  <?php print $picture ?>
+
+<?php if ($comment->new) : ?>
   <a id="new"></a>
   <span class="new"><?php print $new ?></span>
-  <?php endif; ?>
+<?php endif; ?>
 
-  <div class="title"><?php print $title ?></div>
-  <?php print $picture ?>
-  <div class="author"><?php print $submitted ?></div>
-  <div class="content"><?php print $content ?></div>
-  <div class="links"><?php print $links ?></div>
+  <h3><?php print $title ?></h3>
+
+  <div class="submitted">
+    <?php print $submitted ?>
+  </div>
+
+  <div class="content">
+    <?php print $content ?>
+  </div>
+
+  <?php print $links ?>
 </div>
