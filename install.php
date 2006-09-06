971129417e1dbacbe044c986d4d47739f49b65da
Dries Buytaert <dries@buytaert.net>
2006-09-06 07:36:17 +0000

    - Patch #82616 by flk: database password should be optional.

--- install.php
+++ install.php
@@ -227,7 +227,6 @@ function install_settings_form($profile, $install_locale, $settings_file, $db_ur
       '#default_value' => $db_pass,
       '#size' => 45,
       '#maxlength' => 45,
-      '#required' => TRUE,
     );
 
     $form['advanced_options'] = array(
@@ -256,7 +255,6 @@ function install_settings_form($profile, $install_locale, $settings_file, $db_ur
       '#default_value' => $db_prefix,
       '#size' => 45,
       '#maxlength' => 45,
-      '#required' => FALSE,
       '#description' => st('If more than one @drupal web site will be sharing this database, enter a table prefix for your @drupal site here.', array('@drupal' => drupal_install_profile_name())),
     );
 
