dd32f8ddff6c0f865ce9279051d458ba4b36f078
Dries Buytaert <dries@buytaert.net>
2006-10-31 15:03:37 +0000

    - Removing tabs.

--- themes/garland/template.php
+++ themes/garland/template.php
@@ -6,20 +6,20 @@
  */
 function phptemplate_body_class($sidebar_left, $sidebar_right) {
   if ($sidebar_left != '' && $sidebar_right != '') {
-	  $class = 'sidebars';
-	}
-	else {
-		if ($sidebar_left != '') {
-			$class = 'sidebar-left';
-		}
-		if ($sidebar_right != '') {
-			$class = 'sidebar-right';
-		}
-	}
-	
-	if (isset($class)) {
-		print ' class="'. $class .'"';
-	}
+    $class = 'sidebars';
+  }
+  else {
+    if ($sidebar_left != '') {
+      $class = 'sidebar-left';
+    }
+    if ($sidebar_right != '') {
+      $class = 'sidebar-right';
+    }
+  }
+
+  if (isset($class)) {
+    print ' class="'. $class .'"';
+  }
 }
 
 /**
