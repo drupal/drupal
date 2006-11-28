dcd80a6f3f90ce4e6bcb36e6f4bfeec9c8619940
Neil Drumm <drumm@3064.no-reply.drupal.org>
2006-11-28 20:52:51 +0000

    #99970 by Heine. Make page cache keys case-sensitive on MySQL.

--- update.php
+++ update.php
@@ -713,7 +713,7 @@ function update_create_cache_tables() {
         INDEX expire (expire)
       ) /*!40100 DEFAULT CHARACTER SET UTF8 */ ");
       $ret[] = update_sql("CREATE TABLE {cache_page} (
-        cid varchar(255) NOT NULL default '',
+        cid varchar(255) BINARY NOT NULL default '',
         data longblob,
         expire int NOT NULL default '0',
          created int NOT NULL default '0',
