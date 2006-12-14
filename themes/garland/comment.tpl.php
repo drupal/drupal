f35c27cb68b8b1883813f933aac7e88a94ac374c
Steven Wittens <steven@10.no-reply.drupal.org>
2006-12-14 02:55:07 +0000

    - Garland tweak: Swap picture and submitted info on comments. Makes more sense.

--- themes/garland/comment.tpl.php
+++ themes/garland/comment.tpl.php
@@ -1,19 +1,23 @@
 <div class="comment<?php print ($comment->new) ? ' comment-new' : ''; print ($comment->status == COMMENT_NOT_PUBLISHED) ? ' comment-unpublished' : ''; print ' '. $zebra; ?>">
+
+  <div class="clear-block">
+  <?php if ($submitted): ?>
+    <span class="submitted"><?php print t('!date — !username', array('!username' => theme('username', $comment), '!date' => format_date($comment->timestamp))); ?></span>
+  <?php endif; ?>
+
+  <?php if ($comment->new) : ?>
+    <a id="new"></a>
+    <span class="new"><?php print drupal_ucfirst($new) ?></span>
+  <?php endif; ?>
+
   <?php print $picture ?>
 
-<?php if ($submitted): ?>
-  <span class="submitted"><?php print t('!date — !username', array('!username' => theme('username', $comment), '!date' => format_date($comment->timestamp))); ?></span>
-<?php endif; ?>
+    <h3><?php print $title ?></h3>
 
-<?php if ($comment->new) : ?>
-  <a id="new"></a>
-  <span class="new"><?php print drupal_ucfirst($new) ?></span>
-<?php endif; ?>
-
-  <h3><?php print $title ?></h3>
-
-  <div class="content">
-    <?php print $content ?>
+    <div class="content">
+      <?php print $content ?>
+    </div>
+  
   </div>
 
   <?php if ($links): ?>
