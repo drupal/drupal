3c6d61e73c65d89f97474f75abbadc9ac457a43b
Dries Buytaert <dries@buytaert.net>
2007-01-03 11:10:58 +0000

    - Patch #104763 by RobRoy: better defaults for page and story.

--- install.php
+++ install.php
@@ -541,7 +541,12 @@ function install_complete($profile) {
   $function = $profile .'_profile_final';
   if (function_exists($function)) {
     // More steps required
-    $output .= $function();
+    $profile_message = $function();
+  }
+
+  // If the profile returned a welcome message, use that instead of default.
+  if (isset($profile_message)) {
+    $output .= $profile_message;
   }
   else {
     // No more steps
