a1117c0a3d2225e523c04a9744a80ee9f5b2550c
dries <dries>
2007-03-14 19:28:40 +0000

    - Patch #127753 by ChrisKennedy and GreenMother: regex error with session.cookie_domain settings.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: settings.php,v 1.47 2007/03/12 13:53:30 dries Exp $
+// $Id: settings.php,v 1.48 2007/03/14 19:28:40 dries Exp $
 
 /**
  * @file
@@ -140,7 +140,7 @@ ini_set('url_rewriter.tags',        '');
  * We try to set the correct cookie domain.
  */
 if (isset($_SERVER['HTTP_HOST'])) {
-  $domain = '.'. preg_replace('`^www.`', '', $_SERVER['HTTP_HOST']);
+  $domain = '.'. preg_replace('`^www\.`', '', $_SERVER['HTTP_HOST']);
   // Per RFC 2109, cookie domains must contain at least one dot other than the
   // first. For hosts such as 'localhost', we don't set a cookie domain.
   if (count(explode('.', $domain)) > 2) {
