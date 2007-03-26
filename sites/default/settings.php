924c5ec8acb658b4b9dbe7363608ce77dc32d39b
Dries Buytaert <dries@buytaert.net>
2007-03-26 06:01:58 +0000

    - Restoring the proper settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -90,7 +90,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://doug:civicactions@localhost/doug_drupal6_lang';
+$db_url = 'mysql://username:password@localhost/databasename';
 $db_prefix = '';
 
 /**
