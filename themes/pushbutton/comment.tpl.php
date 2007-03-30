7b9eb4baec4564e31e77eead925529a6ba8c3f14
dries <dries>
2007-03-30 07:45:19 +0000

    - Patch #130366 by webchick and steven: improved signature handling.  Step 1 of 2.

--- themes/pushbutton/comment.tpl.php
+++ themes/pushbutton/comment.tpl.php
@@ -4,7 +4,14 @@
   <?php endif; ?>
   <h3 class="title"><?php print $title ?></h3>
   <div class="submitted"><?php print $submitted ?><?php if ($comment->new) : ?><span class="new"> *<?php print $new ?></span><?php endif; ?></div>
-  <div class="content"><?php print $content ?></div>
+  <div class="content">
+    <?php print $content ?>
+    <?php if ($signature): ?>
+      <div class="user-signature clear-block">
+        <?php print $signature ?>
+      </div>
+    <?php endif; ?>
+  </div>
   <!-- BEGIN: links -->
   <div class="links">&raquo; <?php print $links ?></div>
   <!-- END: links -->
