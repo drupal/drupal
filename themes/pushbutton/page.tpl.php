180a2e0a5e4c17f0ff989f1ced5b439f2a0fcd7d
dries <dries>
2007-04-13 08:38:05 +0000

    - Patch #135533 by asimmonds: updated themes to work properly with new language options.

--- themes/pushbutton/page.tpl.php
+++ themes/pushbutton/page.tpl.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language ?>" xml:lang="<?php print $language ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>">
 <head>
   <title><?php print $head_title ?></title>
   <meta http-equiv="Content-Style-Type" content="text/css" />
