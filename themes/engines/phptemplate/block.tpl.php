dad5275365da534d4d753c9c89e08346b963bf34
Dries Buytaert <dries@buytaert.net>
2007-04-29 16:15:39 +0000

    - Patch #137236 by merlin: these template files are now deprecated.

--- themes/engines/phptemplate/block.tpl.php
+++
@@ -1,8 +0,0 @@
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">
-
-<?php if ($block->subject): ?>
-  <h2><?php print $block->subject ?></h2>
-<?php endif;?>
-
-  <div class="content"><?php print $block->content ?></div>
-</div>
--- themes/engines/phptemplate/box.tpl.php
+++
@@ -1,8 +0,0 @@
-<div class="box">
-
-<?php if ($title): ?>
-  <h2><?php print $title ?></h2>
-<?php endif; ?>
-
-  <div class="content"><?php print $content ?></div>
-</div>
