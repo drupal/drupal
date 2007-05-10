258c653f56ce7dd7dddcbe7c3a46c678dcb79b36
Dries Buytaert <dries@buytaert.net>
2007-05-10 19:55:24 +0000

    - Patch #141637 by merlinofchaos, gabor, et al: provide a site config form at the end of install to collect data, plus allow profiles to modify and add more.

--- modules/system/system.js
+++ modules/system/system.js
@@ -24,3 +24,35 @@ Drupal.cleanURLsSettingsCheck = function() {
     }
   }});
 }
+
+/**
+ * Internal function to check using Ajax if clean URLs can be enabled on the
+ * install page.
+ *
+ * This function is not used to verify whether or not clean URLs
+ * are currently enabled.
+ */
+Drupal.cleanURLsInstallCheck = function() {
+  var pathname = location.pathname +"";
+  var url = pathname.replace(/\/[^\/]*?$/, '/') +"node";
+  $("#clean-url .description").append('<span><div id="testing">'+ Drupal.settings.cleanURL.testing +"</div></span>");
+  $("#clean-url.install").css("display", "block");
+  $.ajax({url: location.protocol +"//"+ location.host + url, type: "GET", data: " ", complete: function(response) {
+    $("#testing").toggle();
+    if (response.status == 200) {
+      // Check was successful.
+      $("#clean-url input.form-radio").attr("disabled", "");
+      $("#clean-url .description span").append('<div class="ok">'+ Drupal.settings.cleanURL.success +"</div>");
+      $("#clean-url input.form-radio").attr("checked", 1);
+    }
+    else {
+      // Check failed.
+      $("#clean-url .description span").append('<div class="warning">'+ Drupal.settings.cleanURL.failure +"</div>");
+    }
+  }});
+}
+
+Drupal.installDefaultTimezone = function() {
+  var offset = new Date().getTimezoneOffset() * -60;
+  $("#edit-date-default-timezone").val(offset);
+}
