f321520930aabf1b60f8f146448a21f03c6d20bc
unconed <unconed>
2007-06-21 04:57:19 +0000

    Removing file that didn't belong in last commit

--- themes/garland/user_profile_category.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="profile-category">
-  <?php if ($element['#title']): ?>
-    <h3><?php print $element['#title'] ?></h3>
-  <?php endif; ?>
-
-  <dl<?php (isset($element['#attributes']) ? print drupal_attributes($element['#attributes']) : '') ?>>
-  <?php print $element['#children'] ?>
-  </dl>
-</div>
