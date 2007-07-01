b0ee0e48135b04ec0105483c899e5903a288cf4b
goba <goba>
2007-07-01 23:27:31 +0000

    #141725 by Crell and dvessel: allow themes to define multiple CSS and JS files in their .info files, which can be clearly overriden

--- themes/garland/page.tpl.php
+++ themes/garland/page.tpl.php
@@ -6,7 +6,6 @@
     <?php print $head ?>
     <?php print $styles ?>
     <?php print $scripts ?>
-    <style type="text/css" media="print">@import "<?php print base_path() . path_to_theme() ?>/print.css";</style>
     <!--[if lt IE 7]>
     <style type="text/css" media="all">@import "<?php print base_path() . path_to_theme() ?>/fix-ie.css";</style>
     <![endif]-->
