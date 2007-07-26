bec12a215d52a391f7e2a625092f5d0e33cba28f
Dries Buytaert <dries@buytaert.net>
2007-07-26 06:48:03 +0000

    - Patch #158536 by dvessel: some more gentle massaging of template files.  Trying to make it easier to discover the different template files.

--- modules/forum/forum-submitted.tpl.php
+++ modules/forum/forum-submitted.tpl.php
@@ -1,10 +1,10 @@
-<?php
+<?php 
 // $Id$
 /**
- * @file forum-format-topic.tpl.php
+ * @file forum-submitted.tpl.php
  * Default theme implementation to format a simple string indicated when and
- * by whom a topic was posted.
- *
+ * by whom a topic was submitted.
+ * 
  * Available variables:
  *
  * - $author: The author of the post.
@@ -12,14 +12,14 @@
  * - $topic: An object with the raw data of the post. Unsafe, be sure
  *   to clean this data before printing.
  *
- * @see template_preprocess_forum_format_topic()
- * @see theme_forum_format_topic()
+ * @see template_preprocess_forum_submitted()
+ * @see theme_forum_submitted()
  */
 ?>
-<?php if ($topic->timestamp): ?>
+<?php if ($time): ?>
   <?php print t(
   '@time ago<br />by !author', array(
-    '@time' => $time,
+    '@time' => $time, 
     '!author' => $author,
     )); ?>
 <?php else: ?>
--- modules/forum/forums.tpl.php
+++ modules/forum/forums.tpl.php
@@ -1,6 +1,6 @@
-<?php // $Id$
+<?php // $Id$ 
 /**
- * @file forum-display.tpl.php
+ * @file forums.tpl.php
  * Default theme implementation to display a forum, which may contain forum
  * containers as well as forum topics.
  *
@@ -12,14 +12,12 @@
  * - $forums: The forums to display (as processed by forum-list.tpl.php)
  * - $topics: The topics to display (as processed by forum-topic-list.tpl.php)
  *
- * @see template_preprocess_forum_display()
- * @see theme_forum_display()
+ * @see template_preprocess_forums()
+ * @see theme_forums()
  *
  */
 ?>
-
 <?php if ($forums_defined): ?>
-
 <div id="forum">
   <?php print theme('links', $links); ?>
   <?php print $forums; ?>
