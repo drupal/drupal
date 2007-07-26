7b61e5961a58f58fcfb87471013676ac859d8699
Gábor Hojtsy <gabor@hojtsy.hu>
2007-07-26 07:45:04 +0000

    #85667 by beginner: XHTML validity; lists are not allowed in span tags

--- themes/pushbutton/node.tpl.php
+++ themes/pushbutton/node.tpl.php
@@ -4,7 +4,7 @@
     <h1 class="title"><a href="<?php print $node_url ?>"><?php print $title ?></a></h1>
   <?php endif; ?>
     <span class="submitted"><?php print $submitted ?></span>
-    <span class="taxonomy"><?php print $terms ?></span>
+    <div class="taxonomy"><?php print $terms ?></div>
     <div class="content"><?php print $content ?></div>
     <?php if ($links): ?>
     <div class="links">&raquo; <?php print $links ?></div>
