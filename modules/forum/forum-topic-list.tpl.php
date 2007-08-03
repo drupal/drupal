7e861b27ee2cc6c5b5aac6d4e75fe21f8b1bab95
dries <dries>
2007-08-03 06:07:52 +0000

    - Patch #158536 by dvessel: more forum template massaging.

--- modules/forum/forum-topic-list.tpl.php
+++ modules/forum/forum-topic-list.tpl.php
@@ -1,25 +1,23 @@
 <?php
-// $Id: forum-topic-list.tpl.php,v 1.1 2007/07/22 07:01:07 dries Exp $
+// $Id: forum-topic-list.tpl.php,v 1.2 2007/08/03 06:07:52 dries Exp $
 /**
  * @file forum-topic-list.tpl.php
- *
  * Theme implementation to display a list of forum topics.
  *
  * Available variables:
- *
  * - $header: The table header. This is pre-generated with click-sorting
  *   information. If you need to change this, @see template_preprocess_forum_topic_list().
  * - $pager: The pager to display beneath the table.
  * - $topics: An array of topics to be displayed.
  *
- * Each topic contains:
- *
+ * Each $topic in $topics contains:
  * - $topic->icon: The icon to display.
  * - $topic->moved: A flag to indicate whether the topic has been moved to
  *   another forum.
  * - $topic->title: The title of the topic. Safe to output.
- * - $topic->description: If the topic has been moved, this contains an
+ * - $topic->message: If the topic has been moved, this contains an
  *   explanation and a link.
+ * - $topic->zebra: 'even' or 'odd' string used for row class.
  * - $topic->num_comments: The number of replies on this topic.
  * - $topic->new_replies: A flag to indicate whether there are unread comments.
  * - $topic->new_url: If there are unread replies, this is a link to them.
@@ -38,13 +36,12 @@
     <tr><?php print $header; ?></tr>
   </thead>
   <tbody>
-  <?php $row = 0; ?>
   <?php foreach ($topics as $topic): ?>
-    <tr class="<?php print $row % 2 == 0 ? 'odd' : 'even';?>">
+    <tr class="<?php print $topic->zebra;?>">
       <td class="icon"><?php print $topic->icon; ?></td>
       <td class="title"><?php print $topic->title; ?></td>
     <?php if ($topic->moved): ?>
-      <td colspan="3"><?php print $topic->description; ?></td>
+      <td colspan="3"><?php print $topic->message; ?></td>
     <?php else: ?>
       <td class="replies">
         <?php print $topic->num_comments; ?>
@@ -57,7 +54,6 @@
       <td class="last-reply"><?php print $topic->last_reply; ?>
     <?php endif; ?>
     </tr>
-    <?php $row++; ?>
   <?php endforeach; ?>
   </tbody>
 </table>
--- modules/forum/forums.tpl.php
+++ modules/forum/forums.tpl.php
@@ -1,20 +1,19 @@
-<?php // $Id: forums.tpl.php,v 1.2 2007/07/29 17:28:23 dries Exp $
+<?php // $Id: forums.tpl.php,v 1.3 2007/08/03 06:07:52 dries Exp $
 /**
  * @file forums.tpl.php
- * Default theme implementation to display a forum, which may contain forum
+ * Default theme implementation to display a forum which may contain forum
  * containers as well as forum topics.
  *
  * Variables available:
- *
  * - $links: An array of links that allow a user to post new forum topics.
  *   It may also contain a string telling a user they must log in in order
  *   to post.
  * - $forums: The forums to display (as processed by forum-list.tpl.php)
  * - $topics: The topics to display (as processed by forum-topic-list.tpl.php)
+ * - $forums_defined: A flag to indicate that the forums are configured.
  *
  * @see template_preprocess_forums()
  * @see theme_forums()
- *
  */
 ?>
 <?php if ($forums_defined): ?>
