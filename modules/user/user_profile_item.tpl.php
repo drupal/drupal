d713b11adbd11e6704cabbb52a8c349277fac407
Dries Buytaert <dries@buytaert.net>
2007-08-08 07:50:38 +0000

    - I forgot to remove these earlier when we renamed them.

--- modules/user/user_profile_item.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $Id$
-
-  $attributes = isset($element['#attributes']) ? ' '. drupal_attributes($element['#attributes']) : '';
-?>
-<dt<?php print $attributes ?>><?php print $element['#title'] ?></dt>
-<dd<?php print $attributes ?>><?php print $element['#value'] ?></dd>
