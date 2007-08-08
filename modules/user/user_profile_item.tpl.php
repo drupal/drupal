fba61a02b8c1ff5be9e7d2a6cbd25517f6550387
dries <dries>
2007-08-08 07:50:38 +0000

    - I forgot to remove these earlier when we renamed them.

--- modules/user/user_profile_item.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $Id: user_profile_item.tpl.php,v 1.2 2007/08/07 08:39:36 goba Exp $
-
-  $attributes = isset($element['#attributes']) ? ' '. drupal_attributes($element['#attributes']) : '';
-?>
-<dt<?php print $attributes ?>><?php print $element['#title'] ?></dt>
-<dd<?php print $attributes ?>><?php print $element['#value'] ?></dd>
