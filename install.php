c80a3fe22728a636730acb4e6acccd13b4c2042d
goba <goba>
2007-09-03 14:13:58 +0000

    #172265 by chx: fix user id problems on mysql, when inserting the first two special users

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: install.php,v 1.74 2007/09/02 12:41:01 goba Exp $
+// $Id: install.php,v 1.75 2007/09/03 14:13:58 goba Exp $
 
 require_once './includes/install.inc';
 
@@ -969,8 +969,13 @@ function install_configure_form_submit($form, &$form_state) {
   variable_set('user_email_verification', FALSE);
   $form_state['old_values'] = $form_state['values'];
   $form_state['values'] = $form_state['values']['account'];
-  user_register_submit($form, $form_state);
-  db_query("INSERT INTO {users} (uid, name, mail) VALUES(%d, '%s', '%s')", 0, '', '');
+
+  // We precreated user 1 with placeholder values. Let's save the real values.
+  $account = user_load(1);
+  $merge_data = array('init' => $form_state['values']['mail'], 'roles' => array(), 'status' => 1);
+  user_save($account, array_merge($form_state['values'], $merge_data));
+  // Log in the first user.
+  user_authenticate($form_state['values']['name'], trim($form_state['values']['pass']));
   $form_state['values'] = $form_state['old_values'];
   unset($form_state['old_values']);
   variable_set('user_email_verification', TRUE);
