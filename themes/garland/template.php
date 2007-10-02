c389c90529d45b33c21f55fc7b7f4539fcea762c
Dries Buytaert <dries@buytaert.net>
2007-10-02 16:03:17 +0000

    - Patch #144634 by chx: fixed critical bug that prevented language negotiation to work after/when drupal_goto() is called.

--- themes/garland/template.php
+++ themes/garland/template.php
@@ -102,11 +102,11 @@ function phptemplate_node_submitted($node) {
  */
 function phptemplate_get_ie_styles() {
   global $language;
-  
+
   $iecss = '<link type="text/css" rel="stylesheet" media="all" href="'. base_path() . path_to_theme() .'/fix-ie.css" />';
   if (defined('LANGUAGE_RTL') && $language->direction == LANGUAGE_RTL) {
     $iecss .= '<style type="text/css" media="all">@import "'. base_path() . path_to_theme() .'/fix-ie-rtl.css";</style>';
   }
-  
+
   return $iecss;
 }
