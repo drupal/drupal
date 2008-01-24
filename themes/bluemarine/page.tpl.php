7186c4433e85f4f307466e9706b2a62e30a9b173
Gábor Hojtsy <gabor@hojtsy.hu>
2008-01-24 09:42:53 +0000

    #208768 by dvessel, Arancaytar: language direction should be in the HTML source, so it is more accessible even without CSS

--- themes/bluemarine/page.tpl.php
+++ themes/bluemarine/page.tpl.php
@@ -1,7 +1,7 @@
 <?php
 // $Id$
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language ?>" xml:lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 
 <head>
   <title><?php print $head_title ?></title>
