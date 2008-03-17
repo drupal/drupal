f1ae2ee0515336d209ce6df63ee7218fb5ffdd33
dries <dries>
2008-03-17 16:53:58 +0000

    - Patch #52910 by kbahey, keith.smith, Susurrus, et al: restict access to cron.php.

--- cron.php
+++ cron.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: cron.php,v 1.36 2006/08/09 07:42:55 dries Exp $
+// $Id: cron.php,v 1.37 2008/03/17 16:53:58 dries Exp $
 
 /**
  * @file
@@ -8,4 +8,6 @@
 
 include_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-drupal_cron_run();
+if (isset($_GET['cron_key']) && variable_get('cron_key', 'drupal') == $_GET['cron_key']) {
+  drupal_cron_run();
+}
