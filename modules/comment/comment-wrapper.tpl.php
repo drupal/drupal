b2f0a65024c436abcfc44ae70533ae080ebe3dec
dries <dries>
2008-04-16 11:26:28 +0000

    - Patch #175841 by catch et al: remove comment controls for users.  Yowza.

--- modules/comment/comment-wrapper.tpl.php
+++ modules/comment/comment-wrapper.tpl.php
@@ -1,13 +1,13 @@
 <?php
-// $Id: comment-wrapper.tpl.php,v 1.2 2007/08/07 08:39:35 goba Exp $
+// $Id: comment-wrapper.tpl.php,v 1.3 2008/04/16 11:26:28 dries Exp $
 
 /**
  * @file comment-wrapper.tpl.php
  * Default theme implementation to wrap comments.
  *
  * Available variables:
- * - $content: All comments for a given page. Also contains sorting controls
- *   and comment forms if the site is configured for it.
+ * - $content: All comments for a given page. Also contains comment form
+ *   if enabled.
  *
  * The following variables are provided for contextual information.
  * - $node: Node object the comments are attached to.
@@ -21,11 +21,6 @@
  * - $display_order
  *   - COMMENT_ORDER_NEWEST_FIRST
  *   - COMMENT_ORDER_OLDEST_FIRST
- * - $comment_controls_state
- *   - COMMENT_CONTROLS_ABOVE
- *   - COMMENT_CONTROLS_BELOW
- *   - COMMENT_CONTROLS_ABOVE_BELOW
- *   - COMMENT_CONTROLS_HIDDEN
  *
  * @see template_preprocess_comment_wrapper()
  * @see theme_comment_wrapper()
