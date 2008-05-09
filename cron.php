37a36275d4554ec7e10064d0a72b3a0cf01282c3
Dries Buytaert <dries@buytaert.net>
2008-05-09 19:23:48 +0000

    - Modified patch #256509 by dropcube: report when an invalid cron key is used.

--- cron.php
+++ cron.php
@@ -11,3 +11,6 @@ drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 if (isset($_GET['cron_key']) && variable_get('cron_key', 'drupal') == $_GET['cron_key']) {
   drupal_cron_run();
 }
+else {
+  watchdog('cron', 'Cron did not run because an invalid key used.', array(), WATCHDOG_NOTICE);
+}
