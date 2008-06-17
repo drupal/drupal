0889c01d21ded9b07b117868d12b1032d7c9317c
Dries Buytaert <dries@buytaert.net>
2008-06-17 01:12:50 +0000

    - Patch #270053 by pwolanin: fixed undefined index.

--- modules/simpletest/drupal_web_test_case.php
+++ modules/simpletest/drupal_web_test_case.php
@@ -62,8 +62,8 @@ class DrupalWebTestCase extends UnitTestCase {
     );
     $defaults['teaser'] = $defaults['body'];
     // If we already have a node, we use the original node's created time, and this
-    if (isset($defaults['created'])) {
-      $defaults['date'] = format_date($defaults['created'], 'custom', 'Y-m-d H:i:s O');
+    if (isset($settings['created'])) {
+      $defaults['date'] = format_date($settings['created'], 'custom', 'Y-m-d H:i:s O');
     }
     if (empty($settings['uid'])) {
       global $user;
