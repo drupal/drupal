b1e895f85072f7e2ee22582858c60301e9b8008d
Dries Buytaert <dries@buytaert.net>
2008-06-25 07:45:03 +0000

    - Patch #272636 by dvessel: drupal.js overwrites class of html element, rather than adding to it.

--- misc/drupal.js
+++ misc/drupal.js
@@ -266,7 +266,7 @@ Drupal.ahahError = function(xmlhttp, uri) {
 // Global Killswitch on the <html> element
 if (Drupal.jsEnabled) {
   // Global Killswitch on the <html> element
-  document.documentElement.className = 'js';
+  $(document.documentElement).addClass('js');
   // 'js enabled' cookie
   document.cookie = 'has_js=1; path=/';
   // Attach all behaviors.
