0c6014a0cc2d9aba602ab9ad963e5fcd5958f358
Dries Buytaert <dries@buytaert.net>
2008-07-19 12:50:56 +0000

    - Patch #225880 by webchick: forgot to make a change to default.settings.php.

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -90,7 +90,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://username:password@localhost/databasename';
+$db_url = '';
 $db_prefix = '';
 
 /**
