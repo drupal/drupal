f95f58cc056a5d83df01c59f63dbdbb0bc7767b3
Angie Byron <webchick@24967.no-reply.drupal.org>
2008-10-12 08:30:05 +0000

    #320161 by Dave Reid: Fix problems with drupalGetTestFiles() that causes upload.test to break on some systems.

--- modules/simpletest/drupal_web_test_case.php
+++ modules/simpletest/drupal_web_test_case.php
@@ -486,13 +486,19 @@ class DrupalWebTestCase {
   }
 
   /**
-   * Compare two files based on size.
+   * Compare two files based on size and file name.
    */
   function drupalCompareFiles($file1, $file2) {
-    if (stat($file1->filename) > stat($file2->filename)) {
-      return 1;
+    // Determine which file is larger.
+    $compare_size = (filesize($file1->filename) > filesize($file2->filename));
+    if (!$compare_size) {
+      // Both files were the same size, so return whichever one is alphabetically greater.
+      return strnatcmp($file1->name, $file2->name);
     }
-    return -1;
+    else {
+      // Return TRUE if $file1 is larger than $file2.
+      return $compare_size;
+    }
   }
 
   /**
