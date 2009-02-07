096978579bdf7bcbecc8ec7109aa031de8c3e970
Angie Byron <webchick@24967.no-reply.drupal.org>
2009-02-07 04:46:46 +0000

    #357429 by fusedpro: Update the documentation of hook_menu_alter() to use 'items' rather than 'callbacks' to match hook_menu().

--- modules/menu/menu.api.php
+++ modules/menu/menu.api.php
@@ -88,18 +88,18 @@ function hook_menu() {
  * Alter the data being saved to the {menu_router} table after hook_menu is invoked.
  *
  * This hook is invoked by menu_router_build(). The menu definitions are passed
- * in by reference. Each element of the $callbacks array is one item returned
+ * in by reference. Each element of the $items array is one item returned
  * by a module from hook_menu. Additional items may be added, or existing items
  * altered.
  *
- * @param $callbacks
+ * @param $items
  *   Associative array of menu router definitions returned from hook_menu().
  * @return
  *   None.
  */
-function hook_menu_alter(&$callbacks) {
+function hook_menu_alter(&$items) {
   // Example - disable the page at node/add
-  $callbacks['node/add']['access callback'] = FALSE;
+  $items['node/add']['access callback'] = FALSE;
 }
 
 /**
