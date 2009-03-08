c7b8850a30ac87ed3d1ffe5e8065d240e7ea8799
webchick <webchick>
2009-03-08 01:43:57 +0000

    #304330 by ximo, alpritt, Amitaibu, edmund.kwok, Xano, ff1, BrightLoudNoise, et al: Dramatically improved text format widget. USABILITY AWESOMENESS.

--- misc/form.js
+++ misc/form.js
@@ -1,4 +1,4 @@
-// $Id: form.js,v 1.3 2009/02/18 13:46:52 webchick Exp $
+// $Id: form.js,v 1.4 2009/03/08 01:43:57 webchick Exp $
 (function($) {
 
 Drupal.behaviors.multiselectSelector = {
@@ -9,6 +9,25 @@ Drupal.behaviors.multiselectSelector = {
         $('.multiselect input:radio[value="'+ this.id.substr(5) +'"]')
           .attr('checked', true);
     });
+  }
+};
+
+
+/**
+ * Automatically displays the guidelines of the selected text format.
+ */
+Drupal.behaviors.filterGuidelines = {
+  attach: function(context) {
+  $('.filter-guidelines:not(.filterGuidelines-processed)', context)
+      .addClass('filterGuidelines-processed')
+      .find('label').hide()
+      .parents('.filter-wrapper').find('select.filter-list')
+      .bind('change', function() {
+        $(this).parents('.filter-wrapper')
+        .find('.filter-guidelines-item').hide()
+        .siblings('#filter-guidelines-' + this.value).show();
+      })
+      .change();
   }
 };
 
