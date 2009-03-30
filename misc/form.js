8559c986f0493c27250b99d1e456a88f2c83e113
webchick <webchick>
2009-03-30 03:15:40 +0000

    #304330 follow-up by sun: Clean-up of text format widget patch.

--- misc/form.js
+++ misc/form.js
@@ -1,4 +1,4 @@
-// $Id: form.js,v 1.5 2009/03/13 23:15:08 webchick Exp $
+// $Id: form.js,v 1.6 2009/03/30 03:15:40 webchick Exp $
 (function($) {
 
 Drupal.behaviors.multiselectSelector = {
@@ -14,18 +14,18 @@ Drupal.behaviors.multiselectSelector = {
 
 
 /**
- * Automatically displays the guidelines of the selected text format.
+ * Automatically display the guidelines of the selected text format.
  */
 Drupal.behaviors.filterGuidelines = {
   attach: function(context) {
-  $('.filter-guidelines:not(.filterGuidelines-processed)', context)
-      .addClass('filterGuidelines-processed')
+    $('.filter-guidelines:not(.filter-guidelines-processed)', context)
+      .addClass('filter-guidelines-processed')
       .find('label').hide()
       .parents('.filter-wrapper').find('select.filter-list')
-      .bind('change', function() {
+      .bind('change', function () {
         $(this).parents('.filter-wrapper')
-        .find('.filter-guidelines-item').hide()
-        .siblings('#filter-guidelines-' + this.value).show();
+          .find('.filter-guidelines-item').hide()
+          .siblings('#filter-guidelines-' + this.value).show();
       })
       .change();
   }
