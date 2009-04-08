1de1293df0077fb2e17248e826607ab0004e9851
Dries Buytaert <dries@buytaert.net>
2009-04-08 03:23:46 +0000

    - Patch #369587 by JamesAn: removed code comment that is incorrect.

--- modules/book/book-all-books-block.tpl.php
+++ modules/book/book-all-books-block.tpl.php
@@ -11,8 +11,6 @@
  * - $book_menus: Array of book outlines rendered as an unordered list. It is
  *   keyed to the parent book ID which is also the ID of the parent node
  *   containing an entire outline.
- *
- * @see template_preprocess_book_all_books_block()
  */
 ?>
 <?php foreach ($book_menus as $book_id => $menu) : ?>
