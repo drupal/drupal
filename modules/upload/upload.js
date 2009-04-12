39e0d5bd2eb1d1b3fd6953237690c7e83dba4976
webchick <webchick>
2009-04-12 03:52:22 +0000

    #431304 by pwolanin: Vertical Tabs typo fix.

--- modules/upload/upload.js
+++ modules/upload/upload.js
@@ -1,8 +1,8 @@
-// $Id: upload.js,v 1.1 2009/04/11 22:19:45 webchick Exp $
+// $Id: upload.js,v 1.2 2009/04/12 03:52:22 webchick Exp $
 
 (function($) {
 
-Drupal.behaviors.bookFieldsetSummaries = {
+Drupal.behaviors.uploadFieldsetSummaries = {
   attach: function(context) {
     $('fieldset#edit-attachments', context).setSummary(function(context) {
       var size = $('#upload-attachments tbody tr').size();
