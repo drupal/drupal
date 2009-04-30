1faa09b1afb6c50c4b80d27299ca6f01fc4cc5b1
Dries Buytaert <dries@buytaert.net>
2009-04-30 16:10:10 +0000

    - Patch #394594 by Berdir: additional conversion to the new database abstraction layer plus clean-up.

--- modules/user/user.api.php
+++ modules/user/user.api.php
@@ -129,31 +129,52 @@ function hook_user_cancel($edit, $account, $method) {
     case 'user_cancel_block_unpublish':
       // Unpublish nodes (current revisions).
       module_load_include('inc', 'node', 'node.admin');
-      $nodes = db_select('node', 'n')->fields('n', array('nid'))->condition('uid', $account->uid)->execute()->fetchCol();
+      $nodes = db_select('node', 'n')
+        ->fields('n', array('nid'))
+        ->condition('uid', $account->uid)
+        ->execute()
+        ->fetchCol();
       node_mass_update($nodes, array('status' => 0));
       break;
 
     case 'user_cancel_reassign':
       // Anonymize nodes (current revisions).
       module_load_include('inc', 'node', 'node.admin');
-      $nodes = db_select('node', 'n')->fields('n', array('nid'))->condition('uid', $account->uid)->execute()->fetchCol();
+      $nodes = db_select('node', 'n')
+        ->fields('n', array('nid'))
+        ->condition('uid', $account->uid)
+        ->execute()
+        ->fetchCol();
       node_mass_update($nodes, array('uid' => 0));
       // Anonymize old revisions.
-      db_update('node_revision')->fields(array('uid' => 0))->condition('uid', $account->uid)->execute();
+      db_update('node_revision')
+        ->fields(array('uid' => 0))
+        ->condition('uid', $account->uid)
+        ->execute();
       // Clean history.
-      db_delete('history')->condition('uid', $account->uid)->execute();
+      db_delete('history')
+        ->condition('uid', $account->uid)
+        ->execute();
       break;
 
     case 'user_cancel_delete':
       // Delete nodes (current revisions).
-      $nodes = db_select('node', 'n')->fields('n', array('nid'))->condition('uid', $account->uid)->execute()->fetchCol();
+      $nodes = db_select('node', 'n')
+        ->fields('n', array('nid'))
+        ->condition('uid', $account->uid)
+        ->execute()
+        ->fetchCol();
       foreach ($nodes as $nid) {
         node_delete($nid);
       }
       // Delete old revisions.
-      db_delete('node_revision')->condition('uid', $account->uid)->execute();
+      db_delete('node_revision')
+        ->condition('uid', $account->uid)
+        ->execute();
       // Clean history.
-      db_delete('history')->condition('uid', $account->uid)->execute();
+      db_delete('history')
+        ->condition('uid', $account->uid)
+        ->execute();
       break;
   }
 }
