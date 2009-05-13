8068958d9dba3de7e839d38574d785fe70c405dd
Gábor Hojtsy <gabor@hojtsy.hu>
2009-05-13 19:11:04 +0000

    Drupal 6.12

--- modules/book/book-export-html.tpl.php
+++ modules/book/book-export-html.tpl.php
@@ -20,8 +20,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" xml:lang="<?php print $language->language; ?>">
   <head>
-    <title><?php print $title; ?></title>
     <?php print $head; ?>
+    <title><?php print $title; ?></title>
     <base href="<?php print $base_url; ?>" />
     <link type="text/css" rel="stylesheet" href="misc/print.css" />
     <?php if ($language_rtl): ?>
