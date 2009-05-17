13b38eadd5b66fb20877aa5c99d1b1c76aa4db30
Dries Buytaert <dries@buytaert.net>
2009-05-17 10:49:40 +0000

    - Patch #407294 by Litrik: provide hooks to allow other modules to listen to SimpleTest results.  Java-lamp, here I come!

---
+++ modules/simpletest/simpletest.api.php
@@ -0,0 +1,57 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Hooks provided by the SimpleTest module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * A test group has started.
+ * 
+ * This hook is called just once at the beginning of a test group.
+ *
+ * @return
+ *   None.
+ *
+ */
+function hook_test_group_started() {
+}
+
+/**
+ * A test group has finished.
+ *
+ * This hook is called just once at the end of a test group.
+ * 
+ * @return
+ *   None.
+ *
+ */
+function hook_test_group_finished() {
+}
+
+/**
+ * An individual test has finished.
+ * 
+ * This hook is called when an individual test has finished.
+ *
+ * @param
+ *   $results The results of the test as gathered by DrupalWebTestCase. 
+ *
+ * @return
+ *   None.
+ *
+ * @see DrupalWebTestCase->results
+ */
+function hook_test_finished($results) {
+}
+
+
+/**
+ * @} End of "addtogroup hooks".
+ */
