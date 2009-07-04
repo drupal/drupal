7f1606632dd96aff2aea73c95faf50cd35e454a4
Dries Buytaert <dries@buytaert.net>
2009-07-04 05:37:30 +0000

    - Patch #484820 by Gábor Hojtsy, yhahn, markboulton, leisareichelt, et al: initial version of the new administration toolbar.

---
+++ modules/toolbar/toolbar.js
@@ -0,0 +1,89 @@
+// $Id$
+(function ($) {
+
+/**
+ * Implementation of Drupal.behaviors for admin.
+ */
+Drupal.behaviors.admin = {
+  attach: function() {
+
+    // Set the intial state of the toolbar.
+    $('#toolbar:not(.processed)').each(function() {
+      Drupal.admin.toolbar.init();
+      $(this).addClass('processed');
+    });
+
+    // Toggling of admin shortcuts visibility.
+    $('#toolbar span.toggle:not(.processed)').each(function() {
+      $(this).click(function() {
+        Drupal.admin.toolbar.toggle();
+        return false;
+      });
+      $(this).addClass('processed');
+    });
+  }
+};
+
+/**
+ * Initialize cautiously to avoid collisions with other modules.
+ */
+Drupal.admin = Drupal.admin || {};
+Drupal.admin.toolbar = Drupal.admin.toolbar || {};
+
+/**
+ * Retrieve last saved cookie settings and set up the initial toolbar state.
+ */
+Drupal.admin.toolbar.init = function() {
+  // Retrieve the collapsed status from a stored cookie.
+  var collapsed = $.cookie('Drupal.admin.toolbar.collapsed');
+
+  // Expand or collapse the toolbar based on the cookie value.
+  if (collapsed == 1) {
+    Drupal.admin.toolbar.collapse();
+  }
+  else {
+    Drupal.admin.toolbar.expand();
+  }
+}
+
+/**
+ * Collapse the admin toolbar.
+ */
+Drupal.admin.toolbar.collapse = function() {
+  $('#toolbar div.toolbar-shortcuts').addClass('collapsed');
+  $('#toolbar span.toggle').removeClass('toggle-active');
+  $('body').removeClass('toolbar-shortcuts');
+  $.cookie(
+    'Drupal.admin.toolbar.collapsed', 
+    1, 
+    {path: Drupal.settings.basePath}
+  );
+}
+
+/**
+ * Expand the admin toolbar.
+ */
+Drupal.admin.toolbar.expand = function() {
+  $('#toolbar div.toolbar-shortcuts').removeClass('collapsed');
+  $('#toolbar span.toggle').addClass('toggle-active');
+  $('body').addClass('toolbar-shortcuts');
+  $.cookie(
+    'Drupal.admin.toolbar.collapsed', 
+    0, 
+    {path: Drupal.settings.basePath}
+  );
+}
+
+/**
+ * Toggle the admin toolbar.
+ */
+Drupal.admin.toolbar.toggle = function() {
+  if ($('#toolbar div.toolbar-shortcuts').is('.collapsed')) {
+    Drupal.admin.toolbar.expand();
+  }
+  else {
+    Drupal.admin.toolbar.collapse();
+  }
+}
+
+})(jQuery);
---
+++ modules/toolbar/toolbar.tpl.php
@@ -0,0 +1,29 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Default template for admin toolbar.
+ *
+ * Available variables:
+ * - $toolbar['toolbar_user']: User account / logout links.
+ * - $toolbar['toolbar_menu']: Top level management menu links.
+ * - $toolbar['toolbar_shortcuts']: Convenience shortcuts.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_admin_toolbar()
+ */
+?>
+<div id="toolbar" class="clearfix">
+  <div class="toolbar-menu clearfix">
+    <span class="toggle toggle-active"><?php print t('Show shortcuts'); ?></span>
+    <?php print render($toolbar['toolbar_user']); ?>
+    <?php print render($toolbar['toolbar_menu']); ?>
+  </div>
+
+  <div class="toolbar-shortcuts clearfix">
+    <?php print render($toolbar['toolbar_shortcuts']); ?>
+  </div>
+
+  <div class="shadow"></div>
+</div>
