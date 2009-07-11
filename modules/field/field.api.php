37333b6c1f1f9aadba697134b0ae16fa2d6b939a
webchick <webchick>
2009-07-11 00:56:45 +0000

    #516124 by yched: Clean up some Field API TODOs and remove unused APIs.

--- modules/field/field.api.php
+++ modules/field/field.api.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: field.api.php,v 1.18 2009/07/10 05:58:13 webchick Exp $
+// $Id: field.api.php,v 1.19 2009/07/11 00:56:45 webchick Exp $
 
 /**
  * @ingroup field_fieldable_type
@@ -368,9 +368,6 @@ function hook_field_validate($obj_type, $object, $field, $instance, $items, &$er
 /**
  * Define custom presave behavior for this module's field types.
  *
- * TODO: The behavior of this hook is going to change (see
- * field_attach_presave()).
- *
  * @param $obj_type
  *   The type of $object.
  * @param $object
@@ -507,8 +504,7 @@ function hook_field_prepare_translation($obj_type, $object, $field, $instance, $
  * Field API will call this function as many times as needed.
  *
  * @param $form
- *   The entire form array, $form['#node'] holds node information.
- *   TODO: Not #node any more.
+ *   The entire form array.
  * @param $form_state
  *   The form_state, $form_state['values'][$field['field_name']]
  *   holds the field's form values.
