f981f7b6935a02a859d4b14cafb4408546c615ea
Dries Buytaert <dries@buytaert.net>
2009-07-14 10:29:10 +0000

    - Patch #518284 by Everett Zufelt: remove redundant title from Garland.

--- themes/garland/node.tpl.php
+++ themes/garland/node.tpl.php
@@ -5,7 +5,7 @@
 
 <?php print $picture ?>
 <?php if ($page == 0): ?>
-  <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+  <h2><a href="<?php print $node_url ?>"><?php print $title ?></a></h2>
 <?php endif; ?>
 
   <?php if ($submitted): ?>
