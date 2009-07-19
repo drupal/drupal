48e41d314e7ed278f131e705fbfd0dd6b766a83d
Angie Byron <webchick@24967.no-reply.drupal.org>
2009-07-19 04:48:10 +0000

    #509392 follow-up by adrian: Clean-up for .info files for install profiles patch.

--- install.php
+++ install.php
@@ -972,8 +972,10 @@ function install_task_list($active = NULL) {
   // Add tasks defined by the profile.
   if ($profile) {
     $info = install_profile_info($profile);
-    if (array_key_exists('tasks', $info)) {
-      $tasks += $info['tasks'];
+    if (isset($info['tasks'])) {
+      foreach ($info['tasks'] as $task => $title) {
+        $tasks[$task] = st($title);
+      }
     }
   }
 
