bf501fdae5b1f708dea6ca12f73c77ca84568294
Angie Byron <webchick@24967.no-reply.drupal.org>
2009-07-23 22:14:26 +0000

    #529202 by yched: Fix phpdoc in aggregator-wrapper.tpl.php.

--- modules/aggregator/aggregator-wrapper.tpl.php
+++ modules/aggregator/aggregator-wrapper.tpl.php
@@ -10,7 +10,7 @@
  * - $page: Pager links rendered through theme_pager().
  *
  * @see template_preprocess()
- * @see template_preprocess_comment_wrapper()
+ * @see template_preprocess_aggregator_wrapper()
  */
 ?>
 <div id="aggregator">
