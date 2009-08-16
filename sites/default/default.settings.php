c675fa4b01ff8ddb2faddf8051dfbe068a7a5f07
Angie Byron <webchick@24967.no-reply.drupal.org>
2009-08-16 19:25:39 +0000

    #538032 by killes: Document variables that could help with PCRE memory becoming saturated.

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -221,6 +221,17 @@ ini_set('session.gc_maxlifetime', 200000);
 ini_set('session.cookie_lifetime', 2000000);
 
 /**
+ * If you encounter a situation where users post a large amount of text, and
+ * the result is stripped out upon viewing but can still be edited, Drupal's
+ * output filter may not have sufficient memory to process it. If you
+ * experience this issue, you may wish to uncomment the following two lines
+ * and increase the limits of these variables. For more information, see
+ * http://php.net/manual/en/pcre.configuration.php.
+ */
+# ini_set('pcre.backtrack_limit', 200000);
+# ini_set('pcre.recursion_limit', 200000);
+
+/**
  * Drupal automatically generates a unique session cookie name for each site
  * based on on its full domain name. If you have multiple domains pointing at
  * the same Drupal site, you can either redirect them all to a single domain
