3dfd9a999246af0f80ba4dd51b7415254c2c27ed
webchick <webchick>
2009-08-21 00:21:48 +0000

    #439798 by Rob Loach: Added Vertical Tabs to the Node Type Form.

--- modules/comment/comment-node-form.js
+++ modules/comment/comment-node-form.js
@@ -1,4 +1,4 @@
-// $Id: comment-node-form.js,v 1.2 2009/04/27 20:19:36 webchick Exp $
+// $Id: comment-node-form.js,v 1.3 2009/08/21 00:21:48 webchick Exp $
 
 (function ($) {
 
@@ -6,6 +6,25 @@ Drupal.behaviors.commentFieldsetSummaries = {
   attach: function (context) {
     $('fieldset#edit-comment-settings', context).setSummary(function (context) {
       return Drupal.checkPlain($('input:checked', context).parent().text());
+    });
+    // Provide the summary for the node type form.
+    $('fieldset#edit-comment', context).setSummary(function(context) {
+      var vals = [];
+
+      // Default comment setting.
+      vals.push($("select[name='comment'] option:selected", context).text());
+
+      // Threading.
+      var threading = $("input[name='comment_default_mode']:checked", context).parent().text();
+      if (threading) {
+        vals.push(threading);
+      }
+
+      // Comments per page.
+      var number = $("select[name='comment_default_per_page'] option:selected", context).val();
+      vals.push(Drupal.t('@number comments per page', {'@number': number}));
+
+      return Drupal.checkPlain(vals.join(', '));
     });
   }
 };
