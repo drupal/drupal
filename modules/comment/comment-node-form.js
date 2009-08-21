db61b2603918d582afad82b8036fac7f5240a627
Angie Byron <webchick@24967.no-reply.drupal.org>
2009-08-21 00:21:48 +0000

    #439798 by Rob Loach: Added Vertical Tabs to the Node Type Form.

--- modules/comment/comment-node-form.js
+++ modules/comment/comment-node-form.js
@@ -7,6 +7,25 @@ Drupal.behaviors.commentFieldsetSummaries = {
     $('fieldset#edit-comment-settings', context).setSummary(function (context) {
       return Drupal.checkPlain($('input:checked', context).parent().text());
     });
+    // Provide the summary for the node type form.
+    $('fieldset#edit-comment', context).setSummary(function(context) {
+      var vals = [];
+
+      // Default comment setting.
+      vals.push($("select[name='comment'] option:selected", context).text());
+
+      // Threading.
+      var threading = $("input[name='comment_default_mode']:checked", context).parent().text();
+      if (threading) {
+        vals.push(threading);
+      }
+
+      // Comments per page.
+      var number = $("select[name='comment_default_per_page'] option:selected", context).val();
+      vals.push(Drupal.t('@number comments per page', {'@number': number}));
+
+      return Drupal.checkPlain(vals.join(', '));
+    });
   }
 };
 
--- modules/node/content_types.js
+++ modules/node/content_types.js
@@ -2,15 +2,44 @@
 (function ($) {
 
 Drupal.behaviors.contentTypes = {
-  attach: function () {
+  attach: function (context) {
+    // Provide the vertical tab summaries.
+    $('fieldset#edit-submission', context).setSummary(function(context) {
+      var vals = [];
+      vals.push(Drupal.checkPlain($('#edit-title-label', context).val()) || Drupal.t('Requires a title'));
+      vals.push(Drupal.checkPlain($('#edit-body-label', context).val()) || Drupal.t('No body'));
+      return vals.join(', ');
+    });
+    $('fieldset#edit-workflow', context).setSummary(function(context) {
+      var vals = [];
+      $("input[name^='node_options']:checked", context).parent().each(function() {
+        vals.push(Drupal.checkPlain($(this).text()));
+      });
+      if (!$('#edit-node-options-status', context).is(':checked')) {
+        vals.unshift(Drupal.t('Not published'));
+      }
+      return vals.join(', ');
+    });
+    $('fieldset#edit-display', context).setSummary(function(context) {
+      var vals = [];
+      $('input:checked', context).parent().each(function() {
+        vals.push(Drupal.checkPlain($(this).text()));
+      });
+      if (!$('#edit-node-submitted', context).is(':checked')) {
+        vals.unshift(Drupal.t("Don't display post information"));
+      }
+      return vals.join(', ');
+    });
+
+    // Process the machine name.
     if ($('#edit-type').val() == $('#edit-name').val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_') || $('#edit-type').val() == '') {
-      $('.form-item-type-wrapper').hide();
+      $('.form-item.type-wrapper').hide();
       $('#edit-name').keyup(function () {
         var machine = $(this).val().toLowerCase().replace(/[^a-z0-9]+/g, '_').replace(/_+/g, '_');
         if (machine != '_' && machine != '') {
           $('#edit-type').val(machine);
           $('#node-type-name-suffix').empty().append(' Machine name: ' + machine + ' [').append($('<a href="#">' + Drupal.t('Edit') + '</a>').click(function () {
-            $('.form-item-type-wrapper').show();
+            $('.form-item-textfield.type-wrapper').show();
             $('#node-type-name-suffix').hide();
             $('#edit-name').unbind('keyup');
             return false;
