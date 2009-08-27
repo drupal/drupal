a79556f24150dd4837bce588e44066688b4da183
webchick <webchick>
2009-08-27 00:33:51 +0000

    #559506 by quicksketch and plach: Added  variable to field widgets.

--- modules/field/field.api.php
+++ modules/field/field.api.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: field.api.php,v 1.30 2009/08/25 21:53:47 dries Exp $
+// $Id: field.api.php,v 1.31 2009/08/27 00:33:51 webchick Exp $
 
 /**
  * @ingroup field_fieldable_type
@@ -620,6 +620,8 @@ function hook_field_widget_info_alter(&$info) {
  *   The field structure.
  * @param $instance
  *   The field instance.
+ * @param $langcode
+ *   The language associated to $items.
  * @param $items
  *   Array of default values for this field.
  * @param $delta
@@ -627,7 +629,7 @@ function hook_field_widget_info_alter(&$info) {
  * @return
  *   The form item for a single element for this field.
  */
-function hook_field_widget(&$form, &$form_state, $field, $instance, $items, $delta = 0) {
+function hook_field_widget(&$form, &$form_state, $field, $instance, $langcode, $items, $delta = 0) {
   $element = array(
     '#type' => $instance['widget']['type'],
     '#default_value' => isset($items[$delta]) ? $items[$delta] : '',
