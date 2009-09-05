b376723cb13dc7b8e94aceb328b550d00407ee77
dries <dries>
2009-09-05 08:53:29 +0000

    - Patch #555844 by by NancyDru: made titles consistent.

--- themes/garland/block.tpl.php
+++ themes/garland/block.tpl.php
@@ -1,10 +1,10 @@
 <?php
-// $Id: block.tpl.php,v 1.7 2009/06/12 09:02:55 dries Exp $
+// $Id: block.tpl.php,v 1.8 2009/09/05 08:53:29 dries Exp $
 ?>
 <div id="block-<?php print $block->module . '-' . $block->delta; ?>" class="<?php print $classes; ?> clearfix">
 
 <?php if (!empty($block->subject)): ?>
-  <h2><?php print $block->subject ?></h2>
+  <h2 class="title"><?php print $block->subject ?></h2>
 <?php endif;?>
 
   <div class="content"><?php print $content ?></div>
