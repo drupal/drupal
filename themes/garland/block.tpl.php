01e223d5f8629f35bc57cced13ab2d8a93156c54
Dries Buytaert <dries@buytaert.net>
2009-09-05 08:53:29 +0000

    - Patch #555844 by by NancyDru: made titles consistent.

--- themes/garland/block.tpl.php
+++ themes/garland/block.tpl.php
@@ -4,7 +4,7 @@
 <div id="block-<?php print $block->module . '-' . $block->delta; ?>" class="<?php print $classes; ?> clearfix">
 
 <?php if (!empty($block->subject)): ?>
-  <h2><?php print $block->subject ?></h2>
+  <h2 class="title"><?php print $block->subject ?></h2>
 <?php endif;?>
 
   <div class="content"><?php print $content ?></div>
