28a9529524da705aee4fc761939123e846a32bdb
Dries Buytaert <dries@buytaert.net>
2009-09-11 14:14:16 +0000

    - Patch #526674 by Everett Zufelt, mgifford: made breadcrumbs more accessible for visually impaired people.

--- themes/garland/template.php
+++ themes/garland/template.php
@@ -10,7 +10,12 @@
  */
 function garland_breadcrumb($breadcrumb) {
   if (!empty($breadcrumb)) {
-    return '<div class="breadcrumb">' . implode(' › ', $breadcrumb) . '</div>';
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' › ', $breadcrumb) . '</div>';
+    return $output;
   }
 }
 
