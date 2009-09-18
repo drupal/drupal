2bc3de6a4ffa99e47ddfd05fbe75927c882412d4
Dries Buytaert <dries@buytaert.net>
2009-09-18 10:54:20 +0000

    - Patch #283723 by pwolanin, sun | eddified, moshe weitzman, Dries, aether, Arancaytar: Added Make menu_tree_output() return renderable output.

--- modules/book/book-all-books-block.tpl.php
+++ modules/book/book-all-books-block.tpl.php
@@ -8,13 +8,12 @@
  * all pages" which presents Multiple independent books on all pages.
  *
  * Available variables:
- * - $book_menus: Array of book outlines rendered as an unordered list. It is
- *   keyed to the parent book ID which is also the ID of the parent node
- *   containing an entire outline.
+ * - $book_menus: Array of book outlines keyed to the parent book ID. Call
+ *   render() on each to print it as an unordered list. 
  */
 ?>
 <?php foreach ($book_menus as $book_id => $menu) : ?>
 <div id="book-block-menu-<?php print $book_id; ?>" class="book-block-menu">
-  <?php print $menu; ?>
+  <?php print render($menu); ?>
 </div>
 <?php endforeach; ?>
