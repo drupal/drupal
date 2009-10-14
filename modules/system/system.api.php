a019a9ebb050883437e759cd96b5d32a717de318
Dries Buytaert <dries@buytaert.net>
2009-10-14 10:56:35 +0000

    - Patch #563708 by JohnAlbin, joshmiller: improve theme_get_setting() and make custom theme settings a true form_alter().

--- modules/system/system.api.php
+++ modules/system/system.api.php
@@ -564,6 +564,32 @@ function hook_form_FORM_ID_alter(&$form, &$form_state) {
 }
 
 /**
+ * Allow themes to alter the theme-specific settings form.
+ *
+ * With this hook, themes can alter the theme-specific settings form in any way
+ * allowable by Drupal's Forms API, such as adding form elements, changing
+ * default values and removing form elements. See the Forms API documentation on
+ * api.drupal.org for detailed information.
+ *
+ * Note that the base theme's form alterations will be run before any sub-theme
+ * alterations.
+ *
+ * @param $form
+ *   Nested array of form elements that comprise the form.
+ * @param $form_state
+ *   A keyed array containing the current state of the form.
+ */
+function hook_form_system_theme_settings_alter(&$form, &$form_state) {
+  // Add a checkbox to toggle the breadcrumb trail.
+  $form['toggle_breadcrumb'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Display the breadcrumb'),
+    '#default_value' => theme_get_setting('toggle_breadcrumb'),
+    '#description'   => t('Show a trail of links from the homepage to the current page.'),
+  );
+}
+
+/**
  * Map form_ids to builder functions.
  *
  * This hook allows modules to build multiple forms from a single form "factory"
