5faaa376aa8c0bb5b1a17bca8f506f8431f795b0
Angie Byron <webchick@24967.no-reply.drupal.org>
2009-10-16 00:52:46 +0000

    #602938 by quicksketch: Provide default styles in image.module (add hook_image_default_styles()).

--- modules/image/image.api.php
+++ modules/image/image.api.php
@@ -98,6 +98,81 @@ function hook_image_style_flush($style) {
   // Empty cached data that contains information about the style.
   cache_clear_all('*', 'cache_mymodule', TRUE);
 }
+
+/**
+ * Modify any image styles provided by other modules or the user.
+ *
+ * This hook allows modules to modify, add, or remove image styles. This may
+ * be useful to modify default styles provided by other modules or enforce
+ * that a specific effect is always enabled on a style. Note that modifications
+ * to these styles may negatively affect the user experience, such as if an
+ * effect is added to a style through this hook, the user may attempt to remove
+ * the effect but it will be immediately be re-added.
+ *
+ * The best use of this hook is usually to modify default styles, which are not
+ * editable by the user until they are overridden, so such interface
+ * contradictions will not occur. This hook can target default (or user) styles
+ * by checking the $style['storage'] property.
+ *
+ * If your module needs to provide a new style (rather than modify an existing
+ * one) use hook_image_default_styles() instead.
+ *
+ * @see hook_image_default_styles()
+ */
+function hook_image_styles_alter($styles) {
+  // Check that we only affect a default style.
+  if ($styles['thumbnail']['storage'] == IMAGE_STORAGE_DEFAULT) {
+    // Add an additional effect to the thumbnail style.
+    $styles['thumbnail']['effects'][] = array(
+      'name' => 'image_desaturate',
+      'data' => array(),
+      'weight' => 1,
+    );
+  }
+}
+
+/**
+ * Provide module-based image styles for reuse throughout Drupal.
+ *
+ * This hook allows your module to provide image styles. This may be useful if
+ * you require images to fit within exact dimensions. Note that you should
+ * attempt to re-use the default styles provided by Image module whenever
+ * possible, rather than creating image styles that are specific to your module.
+ * Image provides the styles "thumbnail", "medium", and "large".
+ *
+ * You may use this hook to more easily manage your site's changes by moving
+ * existing image styles from the database to a custom module. Note however that
+ * moving image styles to code instead storing them in the database has a
+ * negligible effect on performance, since custom image styles are loaded
+ * from the database all at once. Even if all styles are pulled from modules,
+ * Image module will still perform the same queries to check the database for
+ * any custom styles.
+ *
+ * @return
+ *   An array of image styles, keyed by the style name.
+ * @see image_image_default_styles()
+ */
+function hook_image_default_styles() {
+  $styles = array();
+
+  $styles['mymodule_preview'] = array(
+    'effects' => array(
+      array(
+        'name' => 'image_scale',
+        'data' => array('width' => 400, 'height' => 400, 'upscale' => 1),
+        'weight' => 0,
+      ),
+      array(
+        'name' => 'image_desaturate',
+        'data' => array(),
+        'weight' => 1,
+      ),
+    ),
+  );
+
+  return $styles;
+}
+
  /**
   * @} End of "addtogroup hooks".
   */
