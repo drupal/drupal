c7d2acbfd0b8216d460a7bde2b04d15534c613b5
dries <dries>
2009-10-17 00:52:36 +0000

    - Forgot to commit this earlier.

---
+++ modules/path/path.api.php
@@ -0,0 +1,44 @@
+<?php
+// $Id: path.api.php,v 1.1 2009/10/17 00:52:36 dries Exp $
+
+/**
+ * @file
+ * Hooks provided by the Path module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+
+/**
+ * The path has been inserted.
+ *
+ * @param $path
+ *   The path array.
+ */
+function hook_path_insert($path) {
+}
+
+/**
+ * The path has been updated.
+ *
+ * @param $path
+ *   The path array.
+ */
+function hook_path_update($path) {
+}
+
+/**
+ * The path has been deleted.
+ *
+ * @param $path
+ *   The path array.
+ */
+function hook_path_delete($path) {
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
