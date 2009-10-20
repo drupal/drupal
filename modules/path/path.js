0fa300a696e42483d6ca3a94ba8ce74b7cbea2be
Dries Buytaert <dries@buytaert.net>
2009-10-20 01:24:34 +0000

    - Patch #332333 by sun: more fixes for the path API.

--- modules/path/path.js
+++ modules/path/path.js
@@ -5,7 +5,7 @@
 Drupal.behaviors.pathFieldsetSummaries = {
   attach: function (context) {
     $('fieldset#edit-path', context).setSummary(function (context) {
-      var path = $('#edit-path-1').val();
+      var path = $('#edit-path-alias').val();
 
       return path ?
         Drupal.t('Alias: @alias', { '@alias': path }) :
