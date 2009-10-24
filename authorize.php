3e41eefcbcc0c0bc8348a123f6a227e414c671b7
webchick <webchick>
2009-10-24 05:13:43 +0000

    #320331 by Dave Reid, dww, John Morahan, cwgordon7, moshe weitzman, c960657, and smoothify: Turn custom_url_rewrite_inbound() and custom_url_rewrite_outbound() into hooks.

--- authorize.php
+++ authorize.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: authorize.php,v 1.2 2009/10/22 00:52:03 dries Exp $
+// $Id: authorize.php,v 1.3 2009/10/24 05:13:43 webchick Exp $
 
 /**
  * @file
@@ -96,8 +96,16 @@ if (authorize_access_allowed()) {
   // Load the code that drives the authorize process.
   require_once DRUPAL_ROOT . '/includes/authorize.inc';
 
-  // Initialize the URL path, but not via raising our bootstrap level.
-  drupal_path_initialize();
+  // For the sake of Batch API and a few other low-level functions, we need to
+  // initialize the URL path into $_GET['q']. However, we do not want to raise
+  // our bootstrap level, nor do we want to call drupal_initialize_path(),
+  // since that is assuming that modules are loaded and invoking hooks.
+  // However, all we really care is if we're in the middle of a batch, in which
+  // case $_GET['q'] will already be set, we just initialize it to an empty
+  // string if it's not already defined.
+  if (!isset($_GET['q'])) {
+    $_GET['q'] = '';
+  }
 
   if (isset($_SESSION['authorize_operation']['page_title'])) {
     drupal_set_title(check_plain($_SESSION['authorize_operation']['page_title']));
