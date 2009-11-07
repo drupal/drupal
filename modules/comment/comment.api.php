a2805cccb30219e74075f206c5f17da215986681
dries <dries>
2009-11-07 13:35:20 +0000

    - Patch #607244 by sun: added permission to decrease performance impact of contextual links.

--- modules/comment/comment.api.php
+++ modules/comment/comment.api.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: comment.api.php,v 1.12 2009/10/10 13:37:09 dries Exp $
+// $Id: comment.api.php,v 1.13 2009/11/07 13:35:20 dries Exp $
 
 /**
  * @file
@@ -70,6 +70,34 @@ function hook_comment_load($comments) {
 function hook_comment_view($comment) {
   // how old is the comment
   $comment->time_ago = time() - $comment->changed;
+}
+
+/**
+ * The comment was built; the module may modify the structured content.
+ *
+ * This hook is called after the content has been assembled in a structured array
+ * and may be used for doing processing which requires that the complete comment
+ * content structure has been built.
+ *
+ * If the module wishes to act on the rendered HTML of the comment rather than the
+ * structured content array, it may use this hook to add a #post_render callback.
+ * Alternatively, it could also implement hook_preprocess_comment(). See
+ * drupal_render() and theme() documentation respectively for details.
+ *
+ * @param $build
+ *   A renderable array representing the comment.
+ *
+ * @see comment_build()
+ */
+function hook_comment_build_alter($build) {
+  // Check for the existence of a field added by another module.
+  if ($build['#build_mode'] == 'full' && isset($build['an_additional_field'])) {
+    // Change its weight.
+    $build['an_additional_field']['#weight'] = -10;
+  }
+
+  // Add a #post_render callback to act on the rendered HTML of the comment.
+  $build['#post_render'][] = 'my_module_comment_post_render';
 }
 
 /**
