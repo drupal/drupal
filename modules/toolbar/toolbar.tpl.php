fa93675178f9469046c547a03db8fdaab2b38f33
webchick <webchick>
2009-11-15 21:13:26 +0000

    #540282 by TheRec, mgifford, and Everett Zufelt: Accessibility improvements for the toolbar.

--- modules/toolbar/toolbar.tpl.php
+++ modules/toolbar/toolbar.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: toolbar.tpl.php,v 1.6 2009/10/27 04:12:39 webchick Exp $
+// $Id: toolbar.tpl.php,v 1.7 2009/11/15 21:13:26 webchick Exp $
 
 /**
  * @file
@@ -24,14 +24,14 @@
 ?>
 <div id="toolbar" class="<?php print $classes; ?> clearfix">
   <div class="toolbar-menu clearfix">
-    <?php if ($toolbar['toolbar_drawer']):?>
-      <span class="toggle toggle-active"><?php print t('Open'); ?></span>
-    <?php endif; ?>
-    <?php print render($toolbar['toolbar_menu']); ?>
     <?php print render($toolbar['toolbar_user']); ?>
+    <?php print render($toolbar['toolbar_menu']); ?>
+    <?php if ($toolbar['toolbar_drawer']):?>
+      <?php print render($toolbar['toolbar_toggle']); ?>
+    <?php endif; ?>
   </div>
 
-  <div class="toolbar-drawer clearfix">
+  <div class="<?php echo $toolbar['toolbar_drawer_classes']; ?>">
     <?php print render($toolbar['toolbar_drawer']); ?>
   </div>
 
