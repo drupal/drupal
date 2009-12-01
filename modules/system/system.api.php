886cc50e75cc045bfce6e655a169f996b0cd5f5c
Dries Buytaert <dries@buytaert.net>
2009-12-01 00:39:35 +0000

    - Patch #491214 by JohnAlbin, Gábor Hojtsy, eigentor: implement a more usable top level Appearance page. Yay.

--- modules/system/system.api.php
+++ modules/system/system.api.php
@@ -2673,6 +2673,23 @@ function hook_page_delivery_callback_alter(&$callback) {
 }
 
 /**
+ * Alters theme operation links.
+ *
+ * @param $theme_groups
+ *   An associative array containing groups of themes.
+ *
+ * @see system_themes_page()
+ */
+function hook_system_themes_page_alter(&$theme_groups) {
+  foreach ($theme_groups as $state => &$group) {
+    foreach($theme_groups[$state] as &$theme) {
+      // Add a foo link to each list of theme operations.
+      $theme->operations[] = l(t('Foo'), 'admin/appearance/foo', array('query' => array('theme' => $theme->name)));
+    }
+  }
+}
+
+/**
  * Alters inbound URL requests.
  *
  * @param $path
