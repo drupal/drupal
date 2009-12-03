ca8416e03637b99cf5846e6e3116ce4d2fcee5a0
Dries Buytaert <dries@buytaert.net>
2009-12-03 20:21:50 +0000

    - Patch #542658 by sun: follow-up on move action 'tabs' out of local tasks.

--- modules/forum/forums.tpl.php
+++ modules/forum/forums.tpl.php
@@ -7,9 +7,6 @@
  * containers as well as forum topics.
  *
  * Variables available:
- * - $links: An array of links that allow a user to post new forum topics.
- *   It may also contain a string telling a user they must log in in order
- *   to post.
  * - $forums: The forums to display (as processed by forum-list.tpl.php)
  * - $topics: The topics to display (as processed by forum-topic-list.tpl.php)
  * - $forums_defined: A flag to indicate that the forums are configured.
@@ -20,7 +17,6 @@
 ?>
 <?php if ($forums_defined): ?>
 <div id="forum">
-  <?php print theme('links', array('links' => $links)); ?>
   <?php print $forums; ?>
   <?php print $topics; ?>
 </div>
