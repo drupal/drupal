0b0519f4ed83b56a19c5df2991bdcdb9c44cdc0a
dries <dries>
2010-01-04 12:04:07 +0000

    - Patch #569024 by joachim: improved variable names.

--- modules/taxonomy/taxonomy.js
+++ modules/taxonomy/taxonomy.js
@@ -1,4 +1,4 @@
-// $Id: taxonomy.js,v 1.6 2009/04/27 20:19:38 webchick Exp $
+// $Id: taxonomy.js,v 1.7 2010/01/04 12:04:07 dries Exp $
 (function ($) {
 
 /**
@@ -19,20 +19,20 @@ Drupal.behaviors.termDrag = {
       $('tr.taxonomy-term-divider-top', table).removeClass('taxonomy-term-divider-top');
       $('tr.taxonomy-term-divider-bottom', table).removeClass('taxonomy-term-divider-bottom');
 
-      if (settings.taxonomy.backPeddle) {
-        for (var n = 0; n < settings.taxonomy.backPeddle; n++) {
+      if (settings.taxonomy.backStep) {
+        for (var n = 0; n < settings.taxonomy.backStep; n++) {
           $(table[0].tBodies[0].rows[n]).addClass('taxonomy-term-preview');
         }
-        $(table[0].tBodies[0].rows[settings.taxonomy.backPeddle - 1]).addClass('taxonomy-term-divider-top');
-        $(table[0].tBodies[0].rows[settings.taxonomy.backPeddle]).addClass('taxonomy-term-divider-bottom');
+        $(table[0].tBodies[0].rows[settings.taxonomy.backStep - 1]).addClass('taxonomy-term-divider-top');
+        $(table[0].tBodies[0].rows[settings.taxonomy.backStep]).addClass('taxonomy-term-divider-bottom');
       }
 
-      if (settings.taxonomy.forwardPeddle) {
-        for (var n = rows - settings.taxonomy.forwardPeddle - 1; n < rows - 1; n++) {
+      if (settings.taxonomy.forwardStep) {
+        for (var n = rows - settings.taxonomy.forwardStep - 1; n < rows - 1; n++) {
           $(table[0].tBodies[0].rows[n]).addClass('taxonomy-term-preview');
         }
-        $(table[0].tBodies[0].rows[rows - settings.taxonomy.forwardPeddle - 2]).addClass('taxonomy-term-divider-top');
-        $(table[0].tBodies[0].rows[rows - settings.taxonomy.forwardPeddle - 1]).addClass('taxonomy-term-divider-bottom');
+        $(table[0].tBodies[0].rows[rows - settings.taxonomy.forwardStep - 2]).addClass('taxonomy-term-divider-top');
+        $(table[0].tBodies[0].rows[rows - settings.taxonomy.forwardStep - 1]).addClass('taxonomy-term-divider-bottom');
       }
     };
   }
