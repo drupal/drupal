f562e86581faa0cc3da859cebb185b2c52b179da
Angie Byron <webchick@24967.no-reply.drupal.org>
2010-01-07 05:23:52 +0000

    #538164 by scor, catch, linclark, effulgentsia, and yched: Convert Comment body as field. Yes, this is WAY past API freeze. :( But is a required follow-up for RDFa support.

--- modules/comment/comment.tpl.php
+++ modules/comment/comment.tpl.php
@@ -18,6 +18,7 @@
  *   Preprocess functions can reformat it by calling format_date() with the
  *   desired parameters on the $comment->changed variable.
  * - $new: New comment marker.
+ * - $permalink: Comment permalink.
  * - $picture: Authors picture.
  * - $signature: Authors signature.
  * - $status: Comment status. Possible values are:
@@ -67,6 +68,7 @@
   <?php print render($title_suffix); ?>
 
   <div class="submitted">
+    <?php print $permalink; ?>
     <?php
       print t('Submitted by !username on !datetime.',
         array('!username' => $author, '!datetime' => $created));
