39f4d7245c0cb5e258b0ca6ec7fd31df6699408d
Angie Byron <webchick@24967.no-reply.drupal.org>
2010-01-07 04:47:38 +0000

    #651722 by casey and ksenzee: Fixed / in URL fragments gets converted to %2F.

--- modules/overlay/overlay-parent.js
+++ modules/overlay/overlay-parent.js
@@ -796,8 +796,12 @@ Drupal.overlay.fragmentizeLink = function (link) {
   var destination = path + link.search + link.hash;
 
   // Assemble the overlay-ready link.
-  var base = window.location.href;
-  return $.param.fragment(base, {'overlay':destination});
+  var newLink = $.param.fragment(window.location.href, { overlay: destination });
+  // $.param.fragment() escaped slashes in the overlay part: unescape them.
+  var regexp = new RegExp("[#&]overlay=" + encodeURIComponent(path));
+  newLink = newLink.replace(regexp, decodeURIComponent);
+
+  return newLink;
 };
 
 /**
