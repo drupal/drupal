6a57391539699d428b537bec2fef660410ee0fe4
Dries Buytaert <dries@buytaert.net>
2010-01-24 15:22:07 +0000

    - Patch #649820 by brianV: added missing documentation for MENU_LOCAL_ACTION in hook_menu().

--- modules/menu/menu.api.php
+++ modules/menu/menu.api.php
@@ -247,7 +247,11 @@
  *       information is generated when the path is accessed.
  *     - MENU_SUGGESTED_ITEM: Modules may "suggest" menu items that the
  *       administrator may enable.
- *     - MENU_LOCAL_TASK: Local tasks are rendered as tabs by default.
+ *     - MENU_LOCAL_ACTION: Local actions are menu items that describe actions
+ *       on the parent item such as adding a new user or block, and are
+ *       rendered in the action-links list in your theme.
+ *     - MENU_LOCAL_TASK: Local tasks are menu items that describe different
+ *       displays of data, and are generally rendered as tabs.
  *     - MENU_DEFAULT_LOCAL_TASK: Every set of local tasks should provide one
  *       "default" task, which should display the same page as the parent item.
  *     If the "type" element is omitted, MENU_NORMAL_ITEM is assumed.
