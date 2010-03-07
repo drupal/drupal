47371be0a5353aef7b34d5bdc70d31c6fd249dfc
Angie Byron <webchick@24967.no-reply.drupal.org>
2010-03-07 23:14:20 +0000

    #414424 by sun, chx, Arancaytar, yched, et al: Introduce Form API #type 'text_format' for additional DX/security around rich text fields.

--- misc/form.js
+++ misc/form.js
@@ -59,23 +59,6 @@ Drupal.behaviors.formUpdated = {
 };
 
 /**
- * Automatically display the guidelines of the selected text format.
- */
-Drupal.behaviors.filterGuidelines = {
-  attach: function (context) {
-    $('.filter-guidelines', context).once('filter-guidelines')
-      .find('label').hide()
-      .parents('.filter-wrapper').find('select.filter-list')
-      .bind('change', function () {
-        $(this).parents('.filter-wrapper')
-          .find('.filter-guidelines-item').hide()
-          .siblings('#filter-guidelines-' + this.value).show();
-      })
-      .change();
-  }
-};
-
-/**
  * Prepopulate form fields with information from the visitor cookie.
  */
 Drupal.behaviors.fillUserInfoFromCookie = {
