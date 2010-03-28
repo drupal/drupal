40fb3a9392de0a0a09200f72dc15f821208450db
Dries Buytaert <dries@buytaert.net>
2010-03-28 11:16:29 +0000

    - Patch #684774 by andypost, sun: critical bug: block visibility settings cannot be properly extended.

--- modules/block/block.api.php
+++ modules/block/block.api.php
@@ -252,7 +252,7 @@ function hook_block_info_alter(&$blocks) {
   foreach ($blocks as $key => $block) {
     // Any module using this alter should inspect the data before changing it,
     // to ensure it is what they expect.
-    if ($block->theme != $theme_key || $block->status != 1) {
+    if (!isset($block->theme) || !isset($block->status) || $block->theme != $theme_key || $block->status != 1) {
       // This block was added by a contrib module, leave it in the list.
       continue;
     }
