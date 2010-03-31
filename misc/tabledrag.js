759c2955c3d1119d1ce597677e67ffd12f4dc2c8
Dries Buytaert <dries@buytaert.net>
2010-03-31 19:22:00 +0000

    - Patch #758514 by fago: second tabledrag table on a page sets wrong parent ids.

--- misc/tabledrag.js
+++ misc/tabledrag.js
@@ -640,7 +640,7 @@ Drupal.tableDrag.prototype.updateField = function (changedRow, group) {
       // Use the first row in the table as source, because it's guaranteed to
       // be at the root level. Find the first item, then compare this row
       // against it as a sibling.
-      sourceRow = $('tr.draggable:first').get(0);
+      sourceRow = $(this.table).find('tr.draggable:first').get(0);
       if (sourceRow == this.rowObject.element) {
         sourceRow = $(this.rowObject.group[this.rowObject.group.length - 1]).next('tr.draggable').get(0);
       }
