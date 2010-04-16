69154c4c675b5a1bb299a1527ec93463cf247ac7
dries <dries>
2010-04-16 13:39:42 +0000

    - Patch #655740 by ksenzee, casey, aspilicious: fixed small JavaScript issues in the overlay module.

--- modules/overlay/overlay-child.js
+++ modules/overlay/overlay-child.js
@@ -1,4 +1,4 @@
-// $Id: overlay-child.js,v 1.6 2010/03/09 20:52:27 webchick Exp $
+// $Id: overlay-child.js,v 1.7 2010/04/16 13:39:42 dries Exp $
 
 (function ($) {
 
@@ -31,8 +31,8 @@ Drupal.behaviors.overlayChild = {
     // may have decided to tell us the parent window to close the popup dialog.
     if (settings.closeOverlay) {
       parent.Drupal.overlay.bindChild(window, true);
-      // Close the child window from a separate thread because the current
-      // one is busy processing Drupal behaviors.
+      // Use setTimeout to close the child window from a separate thread,
+      // because the current one is busy processing Drupal behaviors.
       setTimeout(function () {
         // We need to store the parent variable locally because it will
         // disappear as soon as we close the iframe.
