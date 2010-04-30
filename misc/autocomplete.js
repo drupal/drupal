d9ada384ee9ad1e990fb28fe4728b91f092ad17f
Angie Byron <webchick@24967.no-reply.drupal.org>
2010-04-30 05:23:43 +0000

    #784848 by kbgordon7 : Fixed Coding style for comments in autocomplete.js.

--- misc/autocomplete.js
+++ misc/autocomplete.js
@@ -72,23 +72,23 @@ Drupal.jsAC.prototype.onkeyup = function (input, e) {
     e = window.event;
   }
   switch (e.keyCode) {
-    case 16: // shift.
-    case 17: // ctrl.
-    case 18: // alt.
-    case 20: // caps lock.
-    case 33: // page up.
-    case 34: // page down.
-    case 35: // end.
-    case 36: // home.
-    case 37: // left arrow.
-    case 38: // up arrow.
-    case 39: // right arrow.
-    case 40: // down arrow.
+    case 16: // Shift.
+    case 17: // Ctrl.
+    case 18: // Alt.
+    case 20: // Caps lock.
+    case 33: // Page up.
+    case 34: // Page down.
+    case 35: // End.
+    case 36: // Home.
+    case 37: // Left arrow.
+    case 38: // Up arrow.
+    case 39: // Right arrow.
+    case 40: // Down arrow.
       return true;
 
-    case 9:  // tab.
-    case 13: // enter.
-    case 27: // esc.
+    case 9:  // Tab.
+    case 13: // Enter.
+    case 27: // Esc.
       this.hidePopup(e.keyCode);
       return true;
 
