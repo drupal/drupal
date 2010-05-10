c240ea574fcdf96cc8ffaa9775708dd380dce59b
Dries Buytaert <dries@buytaert.net>
2010-05-10 06:38:23 +0000

    - Patch #654752 by sndev, Berdir, jrbeeman: file_directory() needs to be set before module install.

--- modules/simpletest/drupal_web_test_case.php
+++ modules/simpletest/drupal_web_test_case.php
@@ -1159,6 +1159,11 @@ class DrupalWebTestCase extends DrupalTestCase {
 
     $this->preloadRegistry();
 
+    // Set path variables.
+    variable_set('file_public_path', $public_files_directory);
+    variable_set('file_private_path', $private_files_directory);
+    variable_set('file_temporary_path', $temp_files_directory);
+
     // Include the default profile.
     variable_set('install_profile', 'standard');
     $profile_details = install_profile_info('standard', 'en');
@@ -1212,11 +1217,6 @@ class DrupalWebTestCase extends DrupalTestCase {
     // Set up English language.
     unset($GLOBALS['conf']['language_default']);
     $language = language_default();
-
-    // Set path variables
-    variable_set('file_public_path', $public_files_directory);
-    variable_set('file_private_path', $private_files_directory);
-    variable_set('file_temporary_path', $temp_files_directory);
 
     // Use the test mail class instead of the default mail handler class.
     variable_set('mail_system', array('default-system' => 'TestingMailSystem'));
