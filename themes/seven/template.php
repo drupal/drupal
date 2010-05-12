6cb598ec3c76cb579d20e970c80c5db46261f0b3
Dries Buytaert <dries@buytaert.net>
2010-05-12 09:22:24 +0000

    - Patch #740136 by casey, cosmicdreams: CSS doesn't get included during install.

--- themes/seven/template.php
+++ themes/seven/template.php
@@ -2,6 +2,18 @@
 // $Id$
 
 /**
+ * Override or insert variables into the maintenance page template.
+ */
+function seven_preprocess_maintenance_page(&$vars) {
+  // While markup for normal pages is split into page.tpl.php and html.tpl.php,
+  // the markup for the maintenance page is all in the single
+  // maintenance-page.tpl.php template. So, to have what's done in
+  // seven_preprocess_html() also happen on the maintenance page, it has to be
+  // called here.
+  seven_preprocess_html($vars);
+}
+
+/**
  * Override or insert variables into the html template.
  */
 function seven_preprocess_html(&$vars) {
