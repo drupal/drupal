6037f99a5c9a09cde265f4fc212e1542a3cd7aba
dries <dries>
2010-06-26 22:17:02 +0000

    - Patch #734944 by Dave Reid: documentation for hook_token_info_alter() is incorrect.

--- modules/system/system.api.php
+++ modules/system/system.api.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: system.api.php,v 1.173 2010/06/26 21:38:38 dries Exp $
+// $Id: system.api.php,v 1.174 2010/06/26 22:17:02 dries Exp $
 
 /**
  * @file
@@ -3819,17 +3819,17 @@ function hook_batch_alter(&$batch) {
  */
 function hook_token_info_alter(&$data) {
   // Modify description of node tokens for our site.
-  $node['nid'] = array(
+  $data['tokens']['node']['nid'] = array(
     'name' => t("Node ID"),
     'description' => t("The unique ID of the article."),
   );
-  $node['title'] = array(
+  $data['tokens']['node']['title'] = array(
     'name' => t("Title"),
     'description' => t("The title of the article."),
   );
 
   // Chained tokens for nodes.
-  $node['created'] = array(
+  $data['tokens']['node']['created'] = array(
     'name' => t("Date created"),
     'description' => t("The date the article was posted."),
     'type' => 'date',
