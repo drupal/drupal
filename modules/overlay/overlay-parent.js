35a6a6926b888646fe5a250297926ed62960cc05
dries <dries>
2010-07-25 00:44:29 +0000

    - Patch #762348 by tstoeckler, BarisW, casey: update the document title using jQuery while using the overlay window.

--- modules/overlay/overlay-parent.js
+++ modules/overlay/overlay-parent.js
@@ -1,4 +1,4 @@
-// $Id: overlay-parent.js,v 1.49 2010/07/08 12:20:23 dries Exp $
+// $Id: overlay-parent.js,v 1.50 2010/07/25 00:44:29 dries Exp $
 
 (function ($) {
 
@@ -70,6 +70,8 @@ Drupal.overlay.open = function (url) {
     return this.load(url);
   }
   this.isOpening = true;
+  // Store the original document title.
+  this.originalTitle = document.title;
 
   // Create the dialog and related DOM elements.
   this.create();
@@ -184,6 +186,8 @@ Drupal.overlay.close = function () {
   this.isClosing = true;
   this.isOpen = false;
   $(document.documentElement).removeClass('overlay-open');
+  // Restore the original document title.
+  document.title = this.originalTitle;
 
   // Allow other scripts to respond to this event.
   $(document).trigger('drupalOverlayClose');
@@ -273,6 +277,9 @@ Drupal.overlay.loadChild = function (event) {
   if (this.isOpen && !this.isClosing) {
     // And child document is an actual overlayChild.
     if (iframeWindow.Drupal && iframeWindow.Drupal.overlayChild) {
+      // Replace the document title with title of iframe.
+      document.title = iframeWindow.document.title;
+
       this.activeFrame = $(iframe)
         .addClass('overlay-active')
         // Add a title attribute to the iframe for accessibility.
