af42ed6181db485777f73457efbd70072140d320
dries <dries>
2010-08-22 12:46:21 +0000

    - Patch #812016 by effulgentsia, chx, redndahead, Damien Tournoud: themes cannot always participate in drupal_alter().

--- themes/tests/test_theme/template.php
+++ themes/tests/test_theme/template.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: template.php,v 1.1 2010/03/21 04:05:24 webchick Exp $
+// $Id: template.php,v 1.2 2010/08/22 12:46:21 dries Exp $
 
 /**
  * Tests a theme overriding a suggestion of a base theme hook.
@@ -8,4 +8,15 @@ function test_theme_breadcrumb__suggestion($variables) {
   // Tests that preprocess functions for the base theme hook get called even
   // when the suggestion has an implementation.
   return 'test_theme_breadcrumb__suggestion: ' . $variables['theme_test_preprocess_breadcrumb'];
+}
+
+/**
+ * Tests a theme implementing an alter hook.
+ *
+ * The confusing function name here is due to this being an implementation of
+ * the alter hook invoked when the 'theme_test' module calls
+ * drupal_alter('theme_test_alter').
+ */
+function test_theme_theme_test_alter_alter(&$data) {
+  $data = 'test_theme_theme_test_alter_alter was invoked';
 }
