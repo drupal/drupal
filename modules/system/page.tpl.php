da3ae50dd9838fc819b319ab01b55e837bd26f6a
webchick <webchick>
2010-08-23 23:38:06 +0000

    #410646 by dman, David_Rothstein, and thousands of other people: Fixed 'Secondary menu' exists but is no longer the default source for the secondary links.

--- modules/system/page.tpl.php
+++ modules/system/page.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: page.tpl.php,v 1.45 2010/08/08 19:35:49 dries Exp $
+// $Id: page.tpl.php,v 1.46 2010/08/23 23:38:06 webchick Exp $
 
 /**
  * @file
@@ -102,9 +102,10 @@
 
     </div></div> <!-- /.section, /#header -->
 
-    <?php if ($main_menu): ?>
+    <?php if ($main_menu || $secondary_menu): ?>
       <div id="navigation"><div class="section">
         <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'clearfix')), 'heading' => t('Main menu'))); ?>
+        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
       </div></div> <!-- /.section, /#navigation -->
     <?php endif; ?>
 
@@ -144,7 +145,6 @@
     </div></div> <!-- /#main, /#main-wrapper -->
 
     <div id="footer"><div class="section">
-      <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
       <?php print render($page['footer']); ?>
     </div></div> <!-- /.section, /#footer -->
 
