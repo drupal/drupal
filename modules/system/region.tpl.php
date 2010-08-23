943d9c06b0cfd9ccee5d05d3d86e67bbd48de912
Dries Buytaert <dries@buytaert.net>
2010-08-23 09:09:23 +0000

    - Patch #889882 by dhrosa: page_top() prints an empty DIV when logged out.

--- modules/system/region.tpl.php
+++ modules/system/region.tpl.php
@@ -27,6 +27,8 @@
  * @see template_process()
  */
 ?>
-<div class="<?php print $classes; ?>">
-  <?php print $content; ?>
-</div>
+<?php if($content): ?>
+  <div class="<?php print $classes; ?>">
+    <?php print $content; ?>
+  </div>
+<?php endif; ?>
