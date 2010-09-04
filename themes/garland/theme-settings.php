47326c49cbac1765a804c7c2166244148dc52401
Dries Buytaert <dries@buytaert.net>
2010-09-04 15:21:09 +0000

    - Patch #800042 by Ralf: documentation and variable reference issue in garland.

--- themes/garland/theme-settings.php
+++ themes/garland/theme-settings.php
@@ -2,14 +2,20 @@
 // $Id$
 
 /**
- * Implements hook_form_system_theme_settings_alter().
+ * @file
+ * Theme setting callbacks for the garland theme.
+ */
+
+/**
+ * Implements hook_form_FORM_ID_alter().
  *
  * @param $form
  *   The form.
  * @param $form_state
  *   The form state.
  */
-function garland_form_system_theme_settings_alter(&$form, $form_state) {
+function garland_form_system_theme_settings_alter(&$form, &$form_state) {
+
   $form['garland_width'] = array(
     '#type' => 'radios',
     '#title' => t('Content width'),
