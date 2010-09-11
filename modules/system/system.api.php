84e5d10b66a19211fea8792816bfafc65a446478
Dries Buytaert <dries@buytaert.net>
2010-09-11 14:35:13 +0000

    - Patch #627288 by sun, David_Rothstein: contextual links are not alterable.

--- modules/system/system.api.php
+++ modules/system/system.api.php
@@ -1223,9 +1223,10 @@ function hook_menu_local_tasks_alter(&$data, $router_item, $root_path) {
  *   This is a normalized path, which means that an originally passed path of
  *   'node/123' became 'node/%'.
  *
+ * @see hook_contextual_links_view_alter()
  * @see menu_contextual_links()
  * @see hook_menu()
- * @see system_preprocess()
+ * @see contextual_preprocess()
  */
 function hook_menu_contextual_links_alter(&$links, $router_item, $root_path) {
   // Add a link to all contextual links for nodes.
