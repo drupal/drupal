8ce58d16744e44b1c166e820b88662197eaf6982
Angie Byron <webchick@24967.no-reply.drupal.org>
2010-09-11 05:01:19 +0000

    #787634 by mgifford: Fixed Site title is read three times with VoiceOver in Garland.

--- themes/garland/page.tpl.php
+++ themes/garland/page.tpl.php
@@ -10,16 +10,16 @@
         <div id="logo-floater">
         <?php if ($logo || $site_title): ?>
           <?php if ($title): ?>
-            <div id="branding"><strong><a href="<?php print $front_page ?>" title="<?php print $site_name_and_slogan ?>">
+            <div id="branding"><strong><a href="<?php print $front_page ?>">
             <?php if ($logo): ?>
-              <img src="<?php print $logo ?>" alt="<?php print $site_name_and_slogan ?>" id="logo" />
+              <img src="<?php print $logo ?>" alt="<?php print $site_name_and_slogan ?>" title="<?php print $site_name_and_slogan ?>" id="logo" />
             <?php endif; ?>
             <?php print $site_html ?>
             </a></strong></div>
           <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="branding"><a href="<?php print $front_page ?>" title="<?php print $site_name_and_slogan ?>">
+            <h1 id="branding"><a href="<?php print $front_page ?>">
             <?php if ($logo): ?>
-              <img src="<?php print $logo ?>" alt="<?php print $site_name_and_slogan ?>" id="logo" />
+              <img src="<?php print $logo ?>" alt="<?php print $site_name_and_slogan ?>" title="<?php print $site_name_and_slogan ?>" id="logo" />
             <?php endif; ?>
             <?php print $site_html ?>
             </a></h1>
