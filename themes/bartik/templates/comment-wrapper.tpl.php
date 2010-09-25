7531187c873e234baf419d604b36923eecdf5085
Dries Buytaert <dries@buytaert.net>
2010-09-25 02:05:51 +0000

    - Patch #921792 by Jeff Burnz: remove Bartiks comments-wrapper div, it does nothing.

--- themes/bartik/templates/comment-wrapper.tpl.php
+++ themes/bartik/templates/comment-wrapper.tpl.php
@@ -36,19 +36,17 @@
  * @see theme_comment_wrapper()
  */
 ?>
-<div id="comments-wrapper">
-  <div id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-    <?php if ($content['comments'] && $node->type != 'forum'): ?>
-      <?php print render($title_prefix); ?>
-      <h2 class="title"><?php print t('Comments'); ?></h2>
-      <?php print render($title_suffix); ?>
-    <?php endif; ?>
+<div id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php if ($content['comments'] && $node->type != 'forum'): ?>
+    <?php print render($title_prefix); ?>
+    <h2 class="title"><?php print t('Comments'); ?></h2>
+    <?php print render($title_suffix); ?>
+  <?php endif; ?>
 
-    <?php print render($content['comments']); ?>
+  <?php print render($content['comments']); ?>
 
-    <?php if ($content['comment_form']): ?>
-      <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
-      <?php print render($content['comment_form']); ?>
-    <?php endif; ?>
-  </div>
+  <?php if ($content['comment_form']): ?>
+    <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
+    <?php print render($content['comment_form']); ?>
+  <?php endif; ?>
 </div>
