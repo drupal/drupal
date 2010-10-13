21ca575b476575d27c7c3fbd94d3a29536712ae5
Angie Byron <webchick@24967.no-reply.drupal.org>
2010-10-13 01:30:20 +0000

    #937564 by duellj: Fix link to batch operations page in API docs.

--- modules/system/system.api.php
+++ modules/system/system.api.php
@@ -2871,7 +2871,7 @@ function hook_install() {
  * to provide feedback regarding completion level.
  *
  * See the batch operations page for more information on how to use the batch API:
- * @link http://drupal.org/node/146843 http://drupal.org/node/146843 @endlink
+ * @link http://drupal.org/node/180528 http://drupal.org/node/180528 @endlink
  *
  * @throws DrupalUpdateException, PDOException
  *   In case of error, update hooks should throw an instance of DrupalUpdateException
