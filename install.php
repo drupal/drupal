4acbcf6d8fc2b24e2c31a573913aa69283efb6bd
Angie Byron <webchick@24967.no-reply.drupal.org>
2010-10-22 02:53:19 +0000

    #722974 by chx, moshe weitzman, David_Rothstein: Fixed install script returns a blank page for PHP 5.1.

--- install.php
+++ install.php
@@ -16,6 +16,12 @@ define('DRUPAL_ROOT', getcwd());
  */
 define('MAINTENANCE_MODE', 'install');
 
+// Exit early if running an incompatible PHP version to avoid fatal errors.
+if (version_compare(PHP_VERSION, '5.2.4') < 0) {
+  print 'Your PHP installation is too old. Drupal requires at least PHP 5.2.4. See the <a href="http://drupal.org/requirements">system requirements</a> page for more information.';
+  exit;
+}
+
 // Start the installer.
 require_once DRUPAL_ROOT . '/includes/install.core.inc';
 install_drupal();
