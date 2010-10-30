90b8e8aa095d9a07b8b487bfb6ba59369953c8fd
Dries Buytaert <dries@buytaert.net>
2010-10-30 03:52:14 +0000

    - Patch #639460 by Jeff Burnz, effulgentsia, Jacine, mgifford, james.elliott: evaluate CSS of #skip-link, .element-focusable, and upcoming 'disable overlay' links for their impact on contrib/custom themes.

--- modules/system/html.tpl.php
+++ modules/system/html.tpl.php
@@ -45,7 +45,7 @@
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
-    <a href="#main-content"><?php print t('Skip to main content'); ?></a>
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
