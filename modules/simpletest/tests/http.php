e920fe34ef16d30af0f4fb8e33b565e572ab30c8
Dries Buytaert <dries@buytaert.net>
2010-11-05 19:05:02 +0000

    - Patch #575280 by mfb, carlos8f, chx, bleen18: impersonation when an https session exists.

--- modules/simpletest/tests/http.php
+++ modules/simpletest/tests/http.php
@@ -3,17 +3,18 @@
 
 /**
  * @file
- * Fake an https request, for use during testing.
+ * Fake an HTTP request, for use during testing.
  */
 
-// Set a global variable to indicate a mock HTTPS request.
-$is_https_mock = empty($_SERVER['HTTPS']);
+// Set a global variable to indicate a mock HTTP request.
+$is_http_mock = !empty($_SERVER['HTTPS']);
 
-// Change to https.
-$_SERVER['HTTPS'] = 'on';
+// Change to HTTP.
+$_SERVER['HTTPS'] = NULL;
+ini_set('session.cookie_secure', FALSE);
 foreach ($_SERVER as $key => $value) {
-  $_SERVER[$key] = str_replace('modules/simpletest/tests/https.php', 'index.php', $value);
-  $_SERVER[$key] = str_replace('http://', 'https://', $_SERVER[$key]);
+  $_SERVER[$key] = str_replace('modules/simpletest/tests/http.php', 'index.php', $value);
+  $_SERVER[$key] = str_replace('https://', 'http://', $_SERVER[$key]);
 }
 
 // Change current directory to the Drupal root.
