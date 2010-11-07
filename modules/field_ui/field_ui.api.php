1e03b87733bbb0b69052742b7a47255245224a5b
Dries Buytaert <dries@buytaert.net>
2010-11-07 21:53:11 +0000

    - Patch #961412 by yched, andypost: improved documentation of formatter settings.

--- modules/field_ui/field_ui.api.php
+++ modules/field_ui/field_ui.api.php
@@ -173,6 +173,10 @@ function hook_field_formatter_settings_form($field, $instance, $view_mode, $form
 /**
  * Returns a short summary for the current formatter settings of an instance.
  *
+ * If an empty result is returned, the formatter is asuumed to have no
+ * configurable settings, and no UI will be provided to display a settings
+ * form.
+ *
  * @param $field
  *   The field structure.
  * @param $instance
