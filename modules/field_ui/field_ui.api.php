0e8ab9970e46ad781d06c34fe6345effb5c91985
Dries Buytaert <dries@buytaert.net>
2010-11-12 03:10:38 +0000

    - Patch #961412 by Eric_A: fixed typo in documentation.

--- modules/field_ui/field_ui.api.php
+++ modules/field_ui/field_ui.api.php
@@ -173,7 +173,7 @@ function hook_field_formatter_settings_form($field, $instance, $view_mode, $form
 /**
  * Returns a short summary for the current formatter settings of an instance.
  *
- * If an empty result is returned, the formatter is asuumed to have no
+ * If an empty result is returned, the formatter is assumed to have no
  * configurable settings, and no UI will be provided to display a settings
  * form.
  *
