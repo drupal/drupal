f281f86bf4633dcc0c58b5f422ec2b2f2c9d8d8f
webchick <webchick>
2010-11-22 08:07:57 +0000

    #405238 by brianV: Fixed Duplicate ID 'new' in forums breaks XHTML validation

--- modules/forum/forum-icon.tpl.php
+++ modules/forum/forum-icon.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: forum-icon.tpl.php,v 1.7 2010/10/03 00:41:14 dries Exp $
+// $Id: forum-icon.tpl.php,v 1.8 2010/11/22 08:07:57 webchick Exp $
 
 /**
  * @file
@@ -9,19 +9,17 @@
  * - $new_posts: Indicates whether or not the topic contains new posts.
  * - $icon: The icon to display. May be one of 'hot', 'hot-new', 'new',
  *   'default', 'closed', or 'sticky'.
+ * - $first_new: Indicates whether this is the first topic with new posts.
  *
  * @see template_preprocess_forum_icon()
  * @see theme_forum_icon()
  */
 ?>
 <div class="topic-status-<?php print $icon_class ?>" title="<?php print $icon_title ?>">
-<?php if ($new_posts): ?>
-  <a id="new">
+<?php if ($first_new): ?>
+  <a id="new"></a>
 <?php endif; ?>
 
   <span class="element-invisible"><?php print $icon_title ?></span>
 
-<?php if ($new_posts): ?>
-  </a>
-<?php endif; ?>
 </div>
