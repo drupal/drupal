5eaf3e033fda5864b93b864ba2e97038ae2cec51
Angie Byron <webchick@24967.no-reply.drupal.org>
2010-11-24 03:30:59 +0000

    #655742 by sun, jide: Add template variable for outputting HTML HEAD title safely.

--- modules/system/maintenance-page.tpl.php
+++ modules/system/maintenance-page.tpl.php
@@ -5,8 +5,8 @@
  * @file
  * Default theme implementation to display a single Drupal page while offline.
  *
- * All the available variables are mirrored in page.tpl.php. Some may be left
- * blank but they are provided for consistency.
+ * All the available variables are mirrored in html.tpl.php and page.tpl.php.
+ * Some may be blank but they are provided for consistency.
  *
  * @see template_preprocess()
  * @see template_preprocess_maintenance_page()
