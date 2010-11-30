ffc971142cce5371bf353f92757da1b6d4d3e66e
dries <dries>
2010-11-30 17:55:13 +0000

    - Patch #903046 by tim.plunkett, Jeff Burnz: cleanup bartiks maintenance-page.tpl.php.

--- themes/bartik/templates/maintenance-page.tpl.php
+++ themes/bartik/templates/maintenance-page.tpl.php
@@ -1,12 +1,11 @@
 <?php
-// $Id: maintenance-page.tpl.php,v 1.3 2010/11/26 11:00:37 webchick Exp $
+// $Id: maintenance-page.tpl.php,v 1.4 2010/11/30 17:55:13 dries Exp $
 
 /**
  * @file
- * Bartik's theme implementation to display a single Drupal page while offline.
+ * Implementation to display a single Drupal page while offline.
  *
- * All the available variables are mirrored in page.tpl.php. Some may be left
- * blank but they are provided for consistency.
+ * All the available variables are mirrored in page.tpl.php.
  *
  * @see template_preprocess()
  * @see template_preprocess_maintenance_page()
@@ -16,7 +15,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
-
 <head>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
@@ -24,69 +22,46 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
-  <?php print $page_top; ?>
 
-<div id="page-wrapper"><div id="page">
+  <div id="page-wrapper"><div id="page">
 
-  <div id="header"><div class="section clearfix">
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
+    <div id="header"><div class="section clearfix">
+      <?php if ($site_name || $site_slogan): ?>
+        <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
+          <?php if ($site_name): ?>
             <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
               <strong>
                 <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
               </strong>
             </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
           <?php endif; ?>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-            <?php print $site_slogan; ?>
-          </div>
-        <?php endif; ?>
-
-      </div> <!-- /#name-and-slogan -->
-    <?php endif; ?>
-
-    <?php print $header; ?>
-
-  </div></div> <!-- /.section, /#header -->
-
-  <div id="main-wrapper"><div id="main" class="clearfix">
-
-    <div id="content" class="column"><div class="section">
-      <a id="main-content"></a>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
+          <?php if ($site_slogan): ?>
+            <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
+              <?php print $site_slogan; ?>
+            </div>
+          <?php endif; ?>
+        </div> <!-- /#name-and-slogan -->
       <?php endif; ?>
-      <?php print $content; ?>
-      
-      <?php if ($messages): ?>
-        <div id="messages"><div class="section clearfix">
-          <?php print $messages; ?>
-        </div></div> <!-- /.section, /#messages -->
-      <?php endif; ?>
+    </div></div> <!-- /.section, /#header -->
 
-    </div></div> <!-- /.section, /#content -->
+    <div id="main-wrapper"><div id="main" class="clearfix">
+      <div id="content" class="column"><div class="section">
+        <a id="main-content"></a>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <?php print $content; ?>
+        <?php if ($messages): ?>
+          <div id="messages"><div class="section clearfix">
+            <?php print $messages; ?>
+          </div></div> <!-- /.section, /#messages -->
+        <?php endif; ?>
+      </div></div> <!-- /.section, /#content -->
+    </div></div> <!-- /#main, /#main-wrapper -->
 
+  </div></div> <!-- /#page, /#page-wrapper -->
 
-  </div></div> <!-- /#main, /#main-wrapper -->
-
-</div></div> <!-- /#page, /#page-wrapper -->
-
-  <?php print $page_bottom; ?>
 </body>
 </html>
