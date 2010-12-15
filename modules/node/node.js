f735aa35a7398976aeeb9ba40e2f1b76ffee6472
Angie Byron <webchick@24967.no-reply.drupal.org>
2010-12-15 03:42:25 +0000

    #728090 by alexjarvis: Fixed 'Revision information' can have incorrect summary

--- modules/node/node.js
+++ modules/node/node.js
@@ -5,9 +5,17 @@
 Drupal.behaviors.nodeFieldsetSummaries = {
   attach: function (context) {
     $('fieldset.node-form-revision-information', context).drupalSetSummary(function (context) {
-      return $('.form-item-revision input', context).is(':checked') ?
-        Drupal.t('New revision') :
-        Drupal.t('No revision');
+      var revisionCheckbox = $('.form-item-revision input', context);
+
+      // Return 'New revision' if the 'Create new revision' checkbox is checked,
+      // or if the checkbox doesn't exist, but the revision log does. For users
+      // without the "Administer content" permission the checkbox won't appear,
+      // but the revision log will if the content type is set to auto-revision.
+      if (revisionCheckbox.is(':checked') || (!revisionCheckbox.length && $('.form-item-log textarea', context).length)) {
+        return Drupal.t('New revision');
+      }
+
+      return Drupal.t('No revision');
     });
 
     $('fieldset.node-form-author', context).drupalSetSummary(function (context) {
