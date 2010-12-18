7c8c347058fe8a42b82ae01c2677c1caead8b3f5
Dries Buytaert <dries@buytaert.net>
2010-12-18 01:14:36 +0000

    - Patch #821646 by David_Rothstein: dashboard prints the search form title twice on the page (can confuse screen reader users).

--- modules/search/search-block-form.tpl.php
+++ modules/search/search-block-form.tpl.php
@@ -34,7 +34,7 @@
  */
 ?>
 <div class="container-inline">
-  <?php if (empty($variables['form']['#block']->title)) : ?>
+  <?php if (empty($variables['form']['#block']->subject)) : ?>
     <h2 class="element-invisible"><?php print t('Search form'); ?></h2>
   <?php endif; ?>
   <?php print $search_form; ?>
