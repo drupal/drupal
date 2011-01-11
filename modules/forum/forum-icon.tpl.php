13f5311800b62820e78e63ca706ba05bbc513de4
Dries Buytaert <dries@buytaert.net>
2011-01-11 13:34:17 +0000

    - Patch #1021724 by droplet: documentation problem with forum-icon.tpl.php.

--- modules/forum/forum-icon.tpl.php
+++ modules/forum/forum-icon.tpl.php
@@ -7,7 +7,7 @@
  *
  * Available variables:
  * - $new_posts: Indicates whether or not the topic contains new posts.
- * - $icon: The icon to display. May be one of 'hot', 'hot-new', 'new',
+ * - $icon_class: The icon to display. May be one of 'hot', 'hot-new', 'new',
  *   'default', 'closed', or 'sticky'.
  * - $first_new: Indicates whether this is the first topic with new posts.
  *
