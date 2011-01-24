bc1b67fd689edd35e8ec6a9cd96e79a508b1e0b4
Dries Buytaert <dries@buytaert.net>
2011-01-24 12:49:34 +0000

    - Patch #1037416 by Jody Lynn: typos in user-picture.tpl comment.

--- modules/user/user-picture.tpl.php
+++ modules/user/user-picture.tpl.php
@@ -3,12 +3,12 @@
 
 /**
  * @file
- * Default theme implementation to present an picture configured for the
+ * Default theme implementation to present a picture configured for the
  * user's account.
  *
  * Available variables:
  * - $user_picture: Image set by the user or the site's default. Will be linked
- *   depending on the viewer's permission to view the users profile page.
+ *   depending on the viewer's permission to view the user's profile page.
  * - $account: Array of account information. Potentially unsafe. Be sure to
  *   check_plain() before use.
  *
