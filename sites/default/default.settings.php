c09ea4dac1d6a58d4f3fded91f53646ee3489a6c
webchick <webchick>
2011-01-28 07:03:57 +0000

    #1040190 by 1V: Fix typo in comment for cookie_domain in settings.php

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: default.settings.php,v 1.51 2010/10/11 23:49:48 dries Exp $
+// $Id: default.settings.php,v 1.52 2011/01/28 07:03:57 webchick Exp $
 
 /**
  * @file
@@ -282,11 +282,11 @@ ini_set('session.cookie_lifetime', 2000000);
 
 /**
  * Drupal automatically generates a unique session cookie name for each site
- * based on on its full domain name. If you have multiple domains pointing at
- * the same Drupal site, you can either redirect them all to a single domain
- * (see comment in .htaccess), or uncomment the line below and specify their
- * shared base domain. Doing so assures that users remain logged in as they
- * cross between your various domains.
+ * based on its full domain name. If you have multiple domains pointing at the
+ * same Drupal site, you can either redirect them all to a single domain (see
+ * comment in .htaccess), or uncomment the line below and specify their shared
+ * base domain. Doing so assures that users remain logged in as they cross
+ * between your various domains.
  */
 # $cookie_domain = 'example.com';
 
