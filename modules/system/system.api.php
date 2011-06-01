14d2ba86befdbdbaaf34c9be642e60ff78bfccc7
webchick <webchick@24967.no-reply.drupal.org>
2011-06-01 01:34:05 -0700

    Issue #1161964 by arcaneadam: Fixed  hook_system_themes_page_alter() documentation is incorrect.

--- modules/system/system.api.php
+++ modules/system/system.api.php
@@ -3924,7 +3924,11 @@ function hook_system_themes_page_alter(&$theme_groups) {
   foreach ($theme_groups as $state => &$group) {
     foreach ($theme_groups[$state] as &$theme) {
       // Add a foo link to each list of theme operations.
-      $theme->operations[] = l(t('Foo'), 'admin/appearance/foo', array('query' => array('theme' => $theme->name)));
+      $theme->operations[] = array(
+        'title' => t('Foo'),
+        'href' => 'admin/appearance/foo',
+        'query' => array('theme' => $theme->name)
+      );
     }
   }
 }
