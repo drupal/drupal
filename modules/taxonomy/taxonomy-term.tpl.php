9984561f8045abfca6d87a08564b244c8e0591c0
Dries Buytaert <dries@buytaert.net>
2011-07-31 23:25:19 -0400

    - Patch #1198088 by Jacine: obsolete .clearfix in default taxonomy-term.tpl.php template.

--- modules/taxonomy/taxonomy-term.tpl.php
+++ modules/taxonomy/taxonomy-term.tpl.php
@@ -37,7 +37,7 @@
  * @see template_process()
  */
 ?>
-<div id="taxonomy-term-<?php print $term->tid; ?>" class="<?php print $classes; ?> clearfix">
+<div id="taxonomy-term-<?php print $term->tid; ?>" class="<?php print $classes; ?>">
 
   <?php if (!$page): ?>
     <h2><a href="<?php print $term_url; ?>"><?php print $term_name; ?></a></h2>
