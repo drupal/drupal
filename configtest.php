dc628f0e943bfefb2c330d57e58dd9608e60679e
Greg Dunlap <gdd@heyrocker.com>
2011-09-05 16:32:12 +0200

    Adding back configtest.php which was actually useful for now

---
+++ configtest.php
@@ -0,0 +1,36 @@
+<?php
+
+define('DRUPAL_ROOT', getcwd());
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+
+include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+config_write_signed_file_storage_key();
+//echo config_sign_data('onetwothree');
+$sfs = new SignedFileStorage('one.two');
+
+// Write and read
+$sfs->write('nothing');
+echo $sfs->read() . PHP_EOL;
+$existing_content = file_get_contents($sfs->getFilePath());
+echo $sfs->getFilePath() . PHP_EOL;
+
+// Modify and resign
+file_put_contents($sfs->getFilePath(), $existing_content . 'extra');
+$sfs->resign();
+echo $sfs->read() . PHP_EOL;
+
+// Fail
+////file_put_contents($sfs->getFilePath(), $existing_content . 'extra');
+//echo $sfs->read() . PHP_EOL;
+
+print_r(get_signed_file_storage_names_with_prefix());
+
+print '<hr>';
+
+$config = config('foo.bar');
+$config->foo = 'bar';
+$config->save();
+echo config('foo.bar')->foo;
+echo '<br>That should be bar';
