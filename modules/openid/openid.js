5b98347d00bdd0895e630a4e5ea347b197aacb78
Dries Buytaert <dries@buytaert.net>
2011-09-11 15:40:17 -0400

    - Patch #160163 by c960657, Pancho, danillonunes: login with OpenID link has mis-encoding of #.

--- modules/openid/openid.js
+++ modules/openid/openid.js
@@ -11,10 +11,10 @@ Drupal.behaviors.openid = {
       if (cookie) {
         $('#edit-openid-identifier').val(cookie);
       }
-      if ($('#edit-openid-identifier').val()) {
+      if ($('#edit-openid-identifier').val() || location.hash == '#openid-login') {
         $('#edit-openid-identifier').addClass('openid-processed');
         loginElements.hide();
-        // Use .css('display', 'block') instead of .show() to  Konqueror friendly.
+        // Use .css('display', 'block') instead of .show() to be Konqueror friendly.
         openidElements.css('display', 'block');
       }
     }
