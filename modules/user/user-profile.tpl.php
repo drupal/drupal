cf6f83746093c3c2533cc447a384b10869e47d2c
webchick <webchick@24967.no-reply.drupal.org>
2011-09-30 14:45:00 -0700

    Issue #1123092 by michaellenahan: Fixed user-profile.tpl.php doc has confusing/misleading first paragraph.

--- modules/user/user-profile.tpl.php
+++ modules/user/user-profile.tpl.php
@@ -8,12 +8,12 @@
  * e.g., example.com/user/123. 123 being the users ID.
  *
  * Use render($user_profile) to print all profile items, or print a subset
- * such as render($content['field_example']). Always call render($user_profile)
- * at the end in order to print all remaining items. If the item is a category,
- * it will contain all its profile items. By default, $user_profile['summary']
- * is provided which contains data on the user's history. Other data can be
- * included by modules. $user_profile['user_picture'] is available
- * for showing the account picture.
+ * such as render($user_profile['user_picture']). Always call
+ * render($user_profile) at the end in order to print all remaining items. If
+ * the item is a category, it will contain all its profile items. By default,
+ * $user_profile['summary'] is provided, which contains data on the user's
+ * history. Other data can be included by modules. $user_profile['user_picture']
+ * is available for showing the account picture.
  *
  * Available variables:
  *   - $user_profile: An array of profile items. Use render() to print them.
