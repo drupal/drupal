3312541e08d25f72843a9117c77ce364a49da06d
catch <catch@35733.no-reply.drupal.org>
2011-10-29 20:32:13 +0900

    Issue #1077602 by Jacine, skottler, dcmouyard, jasonrsavino, matglas86, cosmicdreams, scor: Convert node.tpl.php to HTML5.

--- modules/node/node.tpl.php
+++ modules/node/node.tpl.php
@@ -77,34 +77,36 @@
  * @see template_preprocess_node()
  * @see template_process()
  */
-?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php print $user_picture; ?>
+// Hide the comments and links so they can be rendered apart from $content
+// afterwards.
+hide($content['comments']);
+hide($content['links']);
+?>
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php if ($display_submitted || $user_picture || $title): ?>
+    <header>
+      <?php print $user_picture; ?>
+      <?php if ($page): ?>
+        <h1<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
+      <?php else: ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php endif; ?>
+      <p class="submitted"><?php print $submitted; ?></p>
+    </header>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; ?>
-
   <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
+    <?php print render($content); ?>
   </div>
 
-  <?php print render($content['links']); ?>
+  <?php if ($links = render($content['links'])): ?>
+    <nav><?php print $links; ?></nav>
+  <?php endif; ?>
 
   <?php print render($content['comments']); ?>
 
-</div>
+</article>
