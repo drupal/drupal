3629b1fd8bd14cae233ac89b6349d251fe4fb27e
catch <catch@35733.no-reply.drupal.org>
2011-10-29 23:23:17 +0900

    Issue #1077602: roll back.

--- modules/node/node.tpl.php
+++ modules/node/node.tpl.php
@@ -77,36 +77,34 @@
  * @see template_preprocess_node()
  * @see template_process()
  */
-
-// Hide the comments and links so they can be rendered apart from $content
-// afterwards.
-hide($content['comments']);
-hide($content['links']);
 ?>
-<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print $user_picture; ?>
 
   <?php print render($title_prefix); ?>
-  <?php if ($display_submitted || $user_picture || $title): ?>
-    <header>
-      <?php print $user_picture; ?>
-      <?php if ($page): ?>
-        <h1<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
-      <?php else: ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-      <p class="submitted"><?php print $submitted; ?></p>
-    </header>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
+  <?php if ($display_submitted): ?>
+    <div class="submitted">
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
+
   <div class="content"<?php print $content_attributes; ?>>
-    <?php print render($content); ?>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
   </div>
 
-  <?php if ($links = render($content['links'])): ?>
-    <nav><?php print $links; ?></nav>
-  <?php endif; ?>
+  <?php print render($content['links']); ?>
 
   <?php print render($content['comments']); ?>
 
-</article>
+</div>
