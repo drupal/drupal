56d12d5efb840037b50b738f22cb6263c99e3488
catch <catch@35733.no-reply.drupal.org>
2011-11-05 11:46:09 +0900

    Issue #1189832 by karschsp: Convert book-all-books-block.tpl.php to HTML5.

--- core/modules/book/book-all-books-block.tpl.php
+++ core/modules/book/book-all-books-block.tpl.php
@@ -12,7 +12,7 @@
  */
 ?>
 <?php foreach ($book_menus as $book_id => $menu): ?>
-  <div id="book-block-menu-<?php print $book_id; ?>" class="book-block-menu">
+  <nav id="book-block-menu-<?php print $book_id; ?>" class="book-block-menu">
     <?php print render($menu); ?>
-  </div>
+  </nav>
 <?php endforeach; ?>
