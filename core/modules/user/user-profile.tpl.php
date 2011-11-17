cc453cc5b370b9b1b4502c0b861a4ae6cd628f00
Nathaniel <catch@35733.no-reply.drupal.org>
2011-11-17 15:24:57 +0900

    Issue #1190214 by stijnbe, aspilicious: Convert user-profile.tpl.php to HTML5.

--- core/modules/user/user-profile.tpl.php
+++ core/modules/user/user-profile.tpl.php
@@ -28,6 +28,6 @@
  * @see template_preprocess_user_profile()
  */
 ?>
-<div class="profile"<?php print $attributes; ?>>
+<article class="profile"<?php print $attributes; ?>>
   <?php print render($user_profile); ?>
-</div>
+</article>
