60dcf9501d74d916e8e06c2e8682322186bbad05
webchick <webchick@24967.no-reply.drupal.org>
2012-01-11 23:23:55 -0500

    Issue #1376502 by TR, vitalsouto: Fixed Code cleanup: Removed empty PHP blocks from templates left over from git migration.

--- themes/seven/maintenance-page.tpl.php
+++ themes/seven/maintenance-page.tpl.php
@@ -1,5 +1,4 @@
-<?php
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
   <head>
