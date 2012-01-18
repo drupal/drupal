0a324d30179a50080152a90a32e02422b5f4beda
ayelet_Cr <ayelet_Cr@yahoo.com>
2012-01-17 20:34:19 -0600

    Fix issue with Select builder.

--- core/includes/Drupal/Database/Driver/sqlite/Select.php
+++ core/includes/Drupal/Database/Driver/sqlite/Select.php
@@ -4,4 +4,9 @@ namespace Drupal\Database\Driver\sqlite;
 
 use Drupal\Database\Query\Select as QuerySelect;
 
-class Select extends QuerySelect { }
+class Select extends QuerySelect {
+  public function forUpdate($set = TRUE) {
+    // SQLite does not support FOR UPDATE so nothing to do.
+    return $this;
+  }
+}
