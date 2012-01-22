eeed9ed8a640470003db4209a2a40b82ee49691a
Dries <dries@buytaert.net>
2012-01-22 13:53:27 -0500

    - Patch #764558 by Rob Loach, sun, xjm, chx, catch: Remove trigger module from core.

--- core/modules/system/system.api.php
+++ core/modules/system/system.api.php
@@ -3384,9 +3384,7 @@ function hook_file_mimetype_mapping_alter(&$mapping) {
  * Declares information about actions.
  *
  * Any module can define actions, and then call actions_do() to make those
- * actions happen in response to events. The trigger module provides a user
- * interface for associating actions with module-defined triggers, and it makes
- * sure the core triggers fire off actions when their events happen.
+ * actions happen in response to events.
  *
  * An action consists of two or three parts:
  * - an action definition (returned by this hook)
@@ -3417,14 +3415,9 @@ function hook_file_mimetype_mapping_alter(&$mapping) {
  *     declare support for any trigger by returning array('any') for this value.
  *   - 'behavior': (optional) A machine-readable array of behaviors of this
  *     action, used to signal additionally required actions that may need to be
- *     triggered. Currently recognized behaviors by Trigger module:
- *     - 'changes_property': If an action with this behavior is assigned to a
- *       trigger other than a "presave" hook, any save actions also assigned to
- *       this trigger are moved later in the list. If no save action is present,
- *       one will be added.
- *       Modules that are processing actions (like Trigger module) should take
- *       special care for the "presave" hook, in which case a dependent "save"
- *       action should NOT be invoked.
+ *     triggered. Modules that are processing actions should take special care
+ *     for the "presave" hook, in which case a dependent "save" action should
+ *     NOT be invoked.
  *
  * @ingroup actions
  */
@@ -3470,8 +3463,6 @@ function hook_actions_delete($aid) {
  *
  * Called by actions_list() to allow modules to alter the return values from
  * implementations of hook_action_info().
- *
- * @see trigger_example_action_info_alter()
  */
 function hook_action_info_alter(&$actions) {
   $actions['node_unpublish_action']['label'] = t('Unpublish and remove from public view.');
