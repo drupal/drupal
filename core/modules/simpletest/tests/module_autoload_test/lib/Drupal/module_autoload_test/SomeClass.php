9ef50537bcd7b6585f33cf7a8a646403d0cd72d2
catch <catch@35733.no-reply.drupal.org>
2012-03-15 00:14:55 +0900

    Issue #1467126: committing newly added files.

---
+++ core/modules/simpletest/tests/module_autoload_test/lib/Drupal/module_autoload_test/SomeClass.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\module_autoload_test\SomeClass.
+ */
+
+namespace Drupal\module_autoload_test;
+
+class SomeClass {
+  function testMethod() {
+    return 'Drupal\\module_autoload_test\\SomeClass::testMethod() was invoked.';
+  }
+}
