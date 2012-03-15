e2b2a3e0ddc33a6b8338ab63c457d506343277ae
Dries <dries@buytaert.net>
2012-03-15 12:38:32 -0400

    - Patch #1473600 by amateescu, Berdir: Convert archiver.inc to PSR-0.

---
+++ core/lib/Drupal/Component/Archiver/ArchiverInterface.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\Component\Archiver\ArchiverInterface.
+ */
+
+namespace Drupal\Component\Archiver;
+
+/**
+ * Defines the common interface for all Archiver classes.
+ */
+interface ArchiverInterface {
+
+  /**
+   * Constructs a new archiver instance.
+   *
+   * @param string $file_path
+   *   The full system path of the archive to manipulate. Only local files
+   *   are supported. If the file does not yet exist, it will be created if
+   *   appropriate.
+   */
+  public function __construct($file_path);
+
+  /**
+   * Adds the specified file or directory to the archive.
+   *
+   * @param string $file_path
+   *   The full system path of the file or directory to add. Only local files
+   *   and directories are supported.
+   *
+   * @return Drupal\Component\Archiver\ArchiverInterface
+   *   The called object.
+   */
+  public function add($file_path);
+
+  /**
+   * Removes the specified file from the archive.
+   *
+   * @param string $path
+   *   The file name relative to the root of the archive to remove.
+   *
+   * @return Drupal\Component\Archiver\ArchiverInterface
+   *   The called object.
+   */
+  public function remove($path);
+
+  /**
+   * Extracts multiple files in the archive to the specified path.
+   *
+   * @param string $path
+   *   A full system path of the directory to which to extract files.
+   * @param array $files
+   *   Optionally specify a list of files to be extracted. Files are
+   *   relative to the root of the archive. If not specified, all files
+   *   in the archive will be extracted.
+   *
+   * @return Drupal\Component\Archiver\ArchiverInterface
+   *   The called object.
+   */
+  public function extract($path, array $files = array());
+
+  /**
+   * Lists all files in the archive.
+   *
+   * @return array
+   *   An array of file names relative to the root of the archive.
+   */
+  public function listContents();
+}
---
+++ core/lib/Drupal/Component/Archiver/Zip.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\Component\Archiver\Zip.
+ */
+
+namespace Drupal\Component\Archiver;
+
+use ZipArchive;
+
+/**
+ * Defines a archiver implementation for .zip files.
+ *
+ * @link http://php.net/zip
+ */
+class Zip implements ArchiverInterface {
+
+  /**
+   * The underlying ZipArchive instance that does the heavy lifting.
+   *
+   * @var ZipArchive
+   */
+  protected $zip;
+
+  /**
+   * Constructs a Tar object.
+   *
+   * @param string $file_path
+   *   The full system path of the archive to manipulate.
+   */
+  public function __construct($file_path) {
+    $this->zip = new ZipArchive();
+    if ($this->zip->open($file_path) !== TRUE) {
+      // @todo: This should be an interface-specific exception some day.
+      throw new Exception(t('Cannot open %file_path', array('%file_path' => $file_path)));
+    }
+  }
+
+  /**
+   * Implements Drupal\Component\Archiver\ArchiveInterface::add().
+   */
+  public function add($file_path) {
+    $this->zip->addFile($file_path);
+
+    return $this;
+  }
+
+  /**
+   * Implements Drupal\Component\Archiver\ArchiveInterface::remove().
+   */
+  public function remove($file_path) {
+    $this->zip->deleteName($file_path);
+
+    return $this;
+  }
+
+  /**
+   * Implements Drupal\Component\Archiver\ArchiveInterface::extract().
+   */
+  public function extract($path, Array $files = array()) {
+    if ($files) {
+      $this->zip->extractTo($path, $files);
+    }
+    else {
+      $this->zip->extractTo($path);
+    }
+
+    return $this;
+  }
+
+  /**
+   * Implements Drupal\Component\Archiver\ArchiveInterface::listContents().
+   */
+  public function listContents() {
+    $files = array();
+    for ($i=0; $i < $this->zip->numFiles; $i++) {
+      $files[] = $this->zip->getNameIndex($i);
+    }
+    return $files;
+  }
+
+  /**
+   * Retrieves the zip engine itself.
+   *
+   * In some cases it may be necessary to directly access the underlying
+   * ZipArchive object for implementation-specific logic. This is for advanced
+   * use only as it is not shared by other implementations of ArchiveInterface.
+   *
+   * @return ZipArchive
+   *   The ZipArchive object used by this object.
+   */
+  public function getArchive() {
+    return $this->zip;
+  }
+}
