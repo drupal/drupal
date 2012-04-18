0ca408e1d32fea988d1b64e40da4aef1b36eeafc
Dries <dries@buytaert.net>
2012-04-18 14:31:33 -0400

    - Patch #1497230 by Rob Loach, pdrake, effulgentsia: Use Dependency Injection to handle object definitions.

---
+++ core/lib/Drupal/Core/Language/Language.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\Core\Language\Language.
+ */
+
+namespace Drupal\Core\Language;
+
+/**
+ * An object containing the information for an interface language.
+ *
+ * @todo To keep backwards compatibility with stdClass, we currently use
+ * public scopes for the Language class's variables. We will change these to
+ * full get/set functions in a follow-up issue: http://drupal.org/node/1512424
+ *
+ * @see language_default()
+ */
+class Language {
+  // Properties within the Language are set up as the default language.
+  public $name = 'English';
+  public $langcode = 'en';
+  public $direction = 0;
+  public $enabled = 1;
+  public $weight = 0;
+  public $default = FALSE;
+  public $method_id = NULL;
+
+  /**
+   * Language constructor builds the default language object.
+   *
+   * @param array $options
+   *   The properties used to construct the language.
+   */
+  public function __construct(array $options = array()) {
+    // Set all the properties for the language.
+    foreach ($options as $name => $value) {
+      $this->$name = $value;
+    }
+  }
+
+  /**
+   * Extend $this with properties from the given object.
+   *
+   * @todo Remove this function once $GLOBALS['language'] is gone.
+   */
+  public function extend($obj) {
+    $vars = get_object_vars($obj);
+    foreach ($vars as $var => $value) {
+      $this->$var = $value;
+    }
+  }
+}
