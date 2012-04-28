34ef955ae5ef4dd8b63dc686c32f4b65676bccd1
Dries <dries@buytaert.net>
2012-04-28 18:07:40 -0400

    - Patch #1542710 by BTMash, Crell: update to latest Symfony 2.1 code.

--- core/vendor/Symfony/Component/HttpFoundation/Response.php
+++ core/vendor/Symfony/Component/HttpFoundation/Response.php
@@ -83,6 +83,7 @@ class Response
         305 => 'Use Proxy',
         306 => 'Reserved',
         307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',    // RFC-reschke-http-status-308-07
         400 => 'Bad Request',
         401 => 'Unauthorized',
         402 => 'Payment Required',
@@ -165,7 +166,7 @@ class Response
     /**
      * Returns the Response as an HTTP string.
      *
-     * The string representation of the Resonse is the same as the
+     * The string representation of the Response is the same as the
      * one that will be sent to the client only if the prepare() method
      * has been called before.
      *
@@ -197,6 +198,8 @@ class Response
      * the Request that is "associated" with this Response.
      *
      * @param Request $request A Request instance
+     *
+     * @return Response The current response.
      */
     public function prepare(Request $request)
     {
@@ -236,6 +239,8 @@ class Response
                 $headers->set('Content-Length', $length);
             }
         }
+
+        return $this;
     }
 
     /**
