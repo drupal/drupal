18f12d9043d41127db756c0540e09bbf130511aa
catch <catch@35733.no-reply.drupal.org>
2012-05-28 20:19:16 +0900

    Issue #1591438 by aspilicious: Convert forum tests to PSR-0.

--- core/modules/forum/lib/Drupal/forum/Tests/ForumTest.php
+++ core/modules/forum/lib/Drupal/forum/Tests/ForumTest.php
@@ -2,13 +2,15 @@
 
 /**
  * @file
- * Tests for forum.module.
+ * Definition of Drupal\forum\Tests\ForumTest.
  */
+
+namespace Drupal\forum\Tests;
 
 use Drupal\node\Node;
 use Drupal\simpletest\WebTestBase;
 
-class ForumTestCase extends WebTestBase {
+class ForumTest extends WebTestBase {
   protected $admin_user;
   protected $edit_own_topics_user;
   protected $edit_any_topics_user;
@@ -589,67 +591,5 @@ class ForumTestCase extends WebTestBase {
         $this->drupalGet('node/' . $nid);
       }
     }
-  }
-}
-
-/**
- * Tests the forum index listing.
- */
-class ForumIndexTestCase extends WebTestBase {
-
-  public static function getInfo() {
-    return array(
-      'name' => 'Forum index',
-      'description' => 'Tests the forum index listing.',
-      'group' => 'Forum',
-    );
-  }
-
-  function setUp() {
-    parent::setUp('taxonomy', 'comment', 'forum');
-
-    // Create a test user.
-    $web_user = $this->drupalCreateUser(array('create forum content', 'edit own forum content', 'edit any forum content', 'administer nodes'));
-    $this->drupalLogin($web_user);
-  }
-
-  /**
-   * Tests the forum index for published and unpublished nodes.
-   */
-  function testForumIndexStatus() {
-
-    $langcode = LANGUAGE_NOT_SPECIFIED;
-
-    // The forum ID to use.
-    $tid = 1;
-
-    // Create a test node.
-    $title = $this->randomName(20);
-    $edit = array(
-      "title" => $title,
-      "body[$langcode][0][value]" => $this->randomName(200),
-    );
-
-    // Create the forum topic, preselecting the forum ID via a URL parameter.
-    $this->drupalPost('node/add/forum/' . $tid, $edit, t('Save'));
-
-    // Check that the node exists in the database.
-    $node = $this->drupalGetNodeByTitle($title);
-    $this->assertTrue(!empty($node), 'New forum node found in database.');
-
-    // Verify that the node appears on the index.
-    $this->drupalGet('forum/' . $tid);
-    $this->assertText($title, 'Published forum topic appears on index.');
-
-    // Unpublish the node.
-    $edit = array(
-      'status' => FALSE,
-    );
-    $this->drupalPost("node/{$node->nid}/edit", $edit, t('Save'));
-    $this->assertText(t('Access denied'), 'Unpublished node is no longer accessible.');
-
-    // Verify that the node no longer appears on the index.
-    $this->drupalGet('forum/' . $tid);
-    $this->assertNoText($title, 'Unpublished forum topic no longer appears on index.');
   }
 }
