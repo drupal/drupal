43e4ad70fc98a534a9df57c90639932bcabec326
catch <catch@35733.no-reply.drupal.org>
2012-06-12 21:04:44 +0900

    Issue #507502 by fietserwin, cosmicdreams, tim.plunkett, sun, nod_: Fixed locale support for jQuery UI.

---
+++ core/modules/locale/lib/Drupal/locale/Tests/LocaleLibraryInfoAlterTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @file
+ * Definition of Drupal\locale\Tests\LocaleLibraryInfoAlterTest.
+ */
+
+namespace Drupal\locale\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests localization of the JavaScript libraries.
+ *
+ * Currently, only the jQuery datepicker is localized using Drupal translations.
+ */
+class LocaleLibraryInfoAlterTest extends WebTestBase {
+  public static function getInfo() {
+    return array(
+      'name' => 'Javascript library localisation',
+      'description' => 'Tests localization of the JavaScript libraries.',
+      'group' => 'Locale',
+    );
+  }
+
+  function setUp() {
+    parent::setUp(array('locale'));
+  }
+
+  /**
+     * Verifies that the datepicker can be localized.
+     *
+     * @see locale_library_info_alter()
+     */
+  public function testLibraryInfoAlter() {
+    drupal_add_library('system', 'ui.datepicker');
+    $scripts = drupal_get_js();
+    $this->assertTrue(strpos($scripts, 'locale.datepicker.js'), t('locale.datepicker.js added to scripts.'));
+  }
+}
