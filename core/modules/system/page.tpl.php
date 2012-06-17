47aca952c5926100bcdf17a8f7dda91aef66688e
Dries <dries@buytaert.net>
2012-06-17 13:15:47 -0400

    - Patch #998302 by mgifford: 'Skip to main content' link links to region, not content.

--- core/modules/system/page.tpl.php
+++ core/modules/system/page.tpl.php
@@ -118,10 +118,10 @@
     <?php endif; ?>
 
     <div id="main" role="main" class="clearfix">
+    <a id="main-content"></a>
 
       <div id="content" class="column">
         <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-        <a id="main-content"></a>
         <?php print render($title_prefix); ?>
         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
         <?php print render($title_suffix); ?>
