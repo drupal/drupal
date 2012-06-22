81c90b3cbf6449cdf2f1fde63b636e39328baa65
Dries <dries@buytaert.net>
2012-06-22 10:46:33 -0700

    - Patch #1651270 by effulgentsia: remove IE8-specific workaround for lack of CSS3 box-shadow support.

--- core/modules/overlay/overlay-parent.js
+++ core/modules/overlay/overlay-parent.js
@@ -861,17 +861,6 @@ Drupal.overlay.getDisplacement = function (region) {
   var lastDisplaced = $('.overlay-displace-' + region + ':last');
   if (lastDisplaced.length) {
     displacement = lastDisplaced.offset().top + lastDisplaced.outerHeight();
-
-    // In modern browsers (including IE9), when box-shadow is defined, use the
-    // normal height.
-    var cssBoxShadowValue = lastDisplaced.css('box-shadow');
-    var boxShadow = (typeof cssBoxShadowValue !== 'undefined' && cssBoxShadowValue !== 'none');
-    // In IE8 and below, we use the shadow filter to apply box-shadow styles to
-    // the toolbar. It adds some extra height that we need to remove.
-    if (!boxShadow && /DXImageTransform\.Microsoft\.Shadow/.test(lastDisplaced.css('filter'))) {
-      displacement -= lastDisplaced[0].filters.item('DXImageTransform.Microsoft.Shadow').strength;
-      displacement = Math.max(0, displacement);
-    }
   }
   return displacement;
 };
--- core/modules/toolbar/toolbar.js
+++ core/modules/toolbar/toolbar.js
@@ -98,15 +98,6 @@ Drupal.toolbar.toggle = function() {
 Drupal.toolbar.height = function() {
   var $toolbar = $('#toolbar');
   var height = $toolbar.outerHeight();
-  // In modern browsers (including IE9), when box-shadow is defined, use the
-  // normal height.
-  var cssBoxShadowValue = $toolbar.css('box-shadow');
-  var boxShadow = (typeof cssBoxShadowValue !== 'undefined' && cssBoxShadowValue !== 'none');
-  // In IE8 and below, we use the shadow filter to apply box-shadow styles to
-  // the toolbar. It adds some extra height that we need to remove.
-  if (!boxShadow && /DXImageTransform\.Microsoft\.Shadow/.test($toolbar.css('filter'))) {
-    height -= $toolbar[0].filters.item("DXImageTransform.Microsoft.Shadow").strength;
-  }
   return height;
 };
 
