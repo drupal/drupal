049b4fad8db27784f6c58889151b28d87ab8abed
pl2 <pl2@123766.no-reply.drupal.org>
2012-06-28 09:22:47 +0200

    Issue #1641662 by Pedro Lozano, aspilicious: Convert groupby tests to PSR-0.

--- lib/Drupal/views/Tests/QueryGroupByTest.php
+++ lib/Drupal/views/Tests/QueryGroupByTest.php
@@ -2,16 +2,18 @@
 
 /**
  * @file
- * Tests aggregate functionality of Views.
+ * Definition of Drupal\views\Tests\QueryGroupByTest.
  */
 
-use Drupal\simpletest\WebTestBase;
+namespace Drupal\views\Tests;
+
 use Drupal\views\View;
+use ViewsSqlTest;
 
 /**
  * Tests aggregate functionality of views, for example count.
  */
-class ViewsQueryGroupByTest extends ViewsSqlTest {
+class QueryGroupByTest extends ViewsSqlTest {
   public static function getInfo() {
     return array(
       'name' => 'Groupby',
@@ -284,48 +286,5 @@ class ViewsQueryGroupByTest extends ViewsSqlTest {
     $handler->display->display_options['filters']['nid']['value']['value'] = '3';
 
     return $view;
-  }
-}
-
-/**
- * Tests UI of aggregate functionality..
- */
-class viewsUiGroupbyTestCase extends WebTestBase {
-  protected $profile = 'standard';
-
-  function setUp() {
-    // Enable views_ui.
-    parent::setUp('views_ui', 'views_test');
-
-    // Create and log in a user with administer views permission.
-    $views_admin = $this->drupalCreateUser(array('administer views', 'administer blocks', 'bypass node access', 'access user profiles', 'view revisions'));
-    $this->drupalLogin($views_admin);
-  }
-
-  public static function getInfo() {
-    return array(
-      'name' => 'Groupby UI',
-      'description' => 'Tests UI of aggregate functionality.',
-      'group' => 'Views UI',
-    );
-  }
-
-  /**
-   * Tests whether basic saving works.
-   *
-   * @todo: this should check the change of the settings as well.
-   */
-  function testGroupBySave() {
-    $this->drupalGet('admin/structure/views/view/test_views_groupby_save/edit');
-
-    $edit = array(
-      'group_by' => TRUE,
-    );
-    $this->drupalPost('admin/structure/views/nojs/display/test_views_groupby_save/default/group_by', $edit, t('Apply'));
-
-    $this->drupalGet('admin/structure/views/view/test_views_groupby_save/edit');
-    $this->drupalPost('admin/structure/views/view/test_views_groupby_save/edit', array(), t('Save'));
-
-    $this->drupalGet('admin/structure/views/nojs/display/test_views_groupby_save/default/group_by');
   }
 }
---
+++ lib/Drupal/views/Tests/UiGroupByTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\views\Tests\UiGroupByTest.
+ */
+
+namespace Drupal\views\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests UI of aggregate functionality..
+ */
+class UiGroupByTest extends WebTestBase {
+  protected $profile = 'standard';
+
+  function setUp() {
+    // Enable views_ui.
+    parent::setUp('views_ui', 'views_test');
+
+    // Create and log in a user with administer views permission.
+    $views_admin = $this->drupalCreateUser(array('administer views', 'administer blocks', 'bypass node access', 'access user profiles', 'view revisions'));
+    $this->drupalLogin($views_admin);
+  }
+
+  public static function getInfo() {
+    return array(
+      'name' => 'Groupby UI',
+      'description' => 'Tests UI of aggregate functionality.',
+      'group' => 'Views UI',
+    );
+  }
+
+  /**
+   * Tests whether basic saving works.
+   *
+   * @todo: this should check the change of the settings as well.
+   */
+  function testGroupBySave() {
+    $this->drupalGet('admin/structure/views/view/test_views_groupby_save/edit');
+
+    $edit = array(
+      'group_by' => TRUE,
+    );
+    $this->drupalPost('admin/structure/views/nojs/display/test_views_groupby_save/default/group_by', $edit, t('Apply'));
+
+    $this->drupalGet('admin/structure/views/view/test_views_groupby_save/edit');
+    $this->drupalPost('admin/structure/views/view/test_views_groupby_save/edit', array(), t('Save'));
+
+    $this->drupalGet('admin/structure/views/nojs/display/test_views_groupby_save/default/group_by');
+  }
+}
