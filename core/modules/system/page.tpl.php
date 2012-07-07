218cf051f068122cbf0bc4d2ab21872b2183b9d5
webchick <webchick@24967.no-reply.drupal.org>
2012-07-07 13:11:03 -0700

    Issue #1621740 by sun: Fixed Headings for primary and secondary links/menu are not supposed to be visible.

--- core/modules/system/page.tpl.php
+++ core/modules/system/page.tpl.php
@@ -106,8 +106,8 @@
 
     <?php if ($main_menu || $secondary_menu): ?>
       <nav role="navigation">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => array('text' => t('Main menu'), 'class' => array('element-invisible')))); ?>
+        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => array('text' => t('Secondary menu'), 'class' => array('element-invisible')))); ?>
       </nav>
     <?php endif; ?>
 
