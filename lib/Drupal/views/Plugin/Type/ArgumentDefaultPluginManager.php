427ec64dfcc763042434b276338e0295442f2d97
Tim Plunkett <git@plnktt.com>
2012-07-29 12:41:48 -0500

    Switch from AnnotatedClassDiscovery to ViewsDiscovery.

--- lib/Drupal/views/Plugin/Type/ArgumentDefaultPluginManager.php
+++ lib/Drupal/views/Plugin/Type/ArgumentDefaultPluginManager.php
@@ -9,12 +9,11 @@ namespace Drupal\views\Plugin\Type;
 
 use Drupal\Component\Plugin\PluginManagerBase;
 use Drupal\Component\Plugin\Factory\DefaultFactory;
-use Drupal\Core\Plugin\Discovery\AnnotatedClassDiscovery;
-
+use Drupal\views\Plugin\Discovery\ViewsDiscovery;
 
 class ArgumentDefaultPluginManager extends PluginManagerBase {
   public function __construct() {
-    $this->discovery = new AnnotatedClassDiscovery('views', 'argument_default');
+    $this->discovery = new ViewsDiscovery('views', 'argument_default');
     $this->factory = new DefaultFactory($this->discovery);
   }
 }
--- lib/Drupal/views/Plugin/Type/CachePluginManager.php
+++ lib/Drupal/views/Plugin/Type/CachePluginManager.php
@@ -10,11 +10,10 @@ namespace Drupal\views\Plugin\Type;
 use Drupal\Component\Plugin\PluginManagerBase;
 use Drupal\Component\Plugin\Factory\DefaultFactory;
 use Drupal\views\Plugin\Discovery\ViewsDiscovery;
-use Drupal\Core\Plugin\Discovery\AnnotatedClassDiscovery;
 
 class CachePluginManager extends PluginManagerBase {
   public function __construct() {
-    $this->discovery = new AnnotatedClassDiscovery('views', 'cache');
+    $this->discovery = new ViewsDiscovery('views', 'cache');
     $this->factory = new DefaultFactory($this->discovery);
   }
 }
