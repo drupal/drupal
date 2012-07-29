d6092dd574c5e727e567c8bb1d0ff0b8c358664b
Tim Plunkett <git@plnktt.com>
2012-07-29 16:54:21 -0500

    Add missing namespace and use statements for row view.

--- lib/Views/user/Plugin/views/row/View.php
+++ lib/Views/user/Plugin/views/row/View.php
@@ -5,7 +5,11 @@
  * Contains the user view row plugin.
  */
 
+namespace Views\user\Plugin\views\row;
+
 use Drupal\views\Plugin\views\row\RowPluginBase;
+use Drupal\Core\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
 
 /**
  * A row plugin which renders a user via user_view.
