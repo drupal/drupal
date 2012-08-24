6d6e73a490ca054a311d17bcde135ce3afbf6d81
Jennifer Hodgdon <yahgrp@poplarware.com>
2012-08-24 11:06:41 -0700

    Issue #1728472 by plopesc: Add link from form alter hooks to form API reference

--- core/modules/system/system.api.php
+++ core/modules/system/system.api.php
@@ -1216,6 +1216,7 @@ function hook_page_alter(&$page) {
  *
  * @see hook_form_BASE_FORM_ID_alter()
  * @see hook_form_FORM_ID_alter()
+ * @see forms_api_reference.html
  */
 function hook_form_alter(&$form, &$form_state, $form_id) {
   if (isset($form['type']) && $form['type']['#value'] . '_node_settings' == $form_id) {
@@ -1252,6 +1253,7 @@ function hook_form_alter(&$form, &$form_state, $form_id) {
  * @see hook_form_alter()
  * @see hook_form_BASE_FORM_ID_alter()
  * @see drupal_prepare_form()
+ * @see forms_api_reference.html
  */
 function hook_form_FORM_ID_alter(&$form, &$form_state, $form_id) {
   // Modification for the form with the given form ID goes here. For example, if
