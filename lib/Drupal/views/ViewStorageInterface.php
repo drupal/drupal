151d7b25fb5ec0a52b9957aede8f7642d0ad3a8a
damiankloip <damian@damoweb.co.uk>
2012-08-27 14:59:53 +0200

    Added a ViewStorageInterface

---
+++ lib/Drupal/views/ViewStorageInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Definition of Drupal\views\ViewStorageInterface.
+ */
+
+namespace Drupal\views;
+
+use Drupal\config\ConfigurableInterface;
+
+/**
+ * Defines an interface for View storage classes.
+ */
+interface ViewStorageInterface extends ConfigurableInterface {
+
+  /**
+   * Sets the configurable entity status to enabled.
+   */
+  public function enable();
+
+  /**
+   * Sets the configurable entity status to disabled.
+   */
+  public function disable();
+
+  /**
+   * Returns whether the configurable entity is enabled.
+   *
+   * @return bool
+   */
+  public function isEnabled();
+
+}
