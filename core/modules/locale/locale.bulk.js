6f6cc1f984888c8e913217f3f7e96e4281ea7544
Dries <dries@buytaert.net>
2012-08-31 21:59:54 -0400

    Issue #1744228 by Dave Reid, nod_, penyaskito: auto-detect the language code of an imported file by file name.

---
+++ core/modules/locale/locale.bulk.js
@@ -0,0 +1,26 @@
+(function ($) {
+
+"use strict";
+
+/**
+ * Select the language code of an imported file based on its filename.
+ *
+ * This only works if the file name ends with "LANGCODE.po".
+ */
+Drupal.behaviors.importLanguageCodeSelector = {
+  attach: function (context, settings) {
+    var $form = $('#locale-translate-import-form').once('autodetect-lang');
+    if ($form.length) {
+      var $langcode = $form.find('.langcode-input');
+      var $file = $form.find('.file-import-input')
+        .on('change', function () {
+          var matches = $(this).val().match(/\.([\-\w]+)\.po/);
+          if (matches && $langcode.find('option[value="' + matches[1] + '"]').length) {
+            $langcode.val(matches[1]);
+          }
+        });
+    }
+  }
+};
+
+})(jQuery);
