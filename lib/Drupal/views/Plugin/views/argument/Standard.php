3a12bbe055bdc117172d69cc1c2a75a4d32242cf
dereine <dereine@99340.no-reply.drupal.org>
2012-09-13 08:34:47 +0200

    Issue #1783028 by dawehner: Fixed Add a standard implementation of arguments.

---
+++ lib/Drupal/views/Plugin/views/argument/Standard.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\views\Plugin\views\argument\Standard.
+ */
+
+namespace Drupal\views\Plugin\views\argument;
+
+use Drupal\Core\Annotation\Plugin;
+
+/**
+ * Default implementation of the base argument plugin.
+ *
+ * @ingroup views_argument_handlers
+ *
+ * @Plugin(
+ *   id = "standard"
+ * )
+ */
+class Standard extends ArgumentPluginBase {
+
+}
