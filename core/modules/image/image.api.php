f2f1d888f693c095027c5a1d67ee61ca72e1dc1c
Jennifer Hodgdon <yahgrp@poplarware.com>
2012-09-19 06:27:40 -0700

    Issue #1787156 by jaffaralia: Remove documentation for obsolete hook_image_default_styles

--- core/modules/image/image.api.php
+++ core/modules/image/image.api.php
@@ -133,11 +133,6 @@ function hook_image_style_flush($style) {
  * editable by the user until they are overridden, so such interface
  * contradictions will not occur. This hook can target default (or user) styles
  * by checking the $style['storage'] property.
- *
- * If your module needs to provide a new style (rather than modify an existing
- * one) use hook_image_default_styles() instead.
- *
- * @see hook_image_default_styles()
  */
 function hook_image_styles_alter(&$styles) {
   // Check that we only affect a default style.
@@ -150,48 +145,6 @@ function hook_image_styles_alter(&$styles) {
       'effect callback' => 'image_desaturate_effect',
     );
   }
-}
-
-/**
- * Provide module-based image styles for reuse throughout Drupal.
- *
- * This hook allows your module to provide image styles. This may be useful if
- * you require images to fit within exact dimensions. Note that you should
- * attempt to re-use the default styles provided by Image module whenever
- * possible, rather than creating image styles that are specific to your module.
- * Image provides the styles "thumbnail", "medium", and "large".
- *
- * You may use this hook to more easily manage your site's changes by moving
- * existing image styles from the database to a custom module. Note however that
- * moving image styles to code instead storing them in the database has a
- * negligible effect on performance, since custom image styles are loaded
- * from the database all at once. Even if all styles are pulled from modules,
- * Image module will still perform the same queries to check the database for
- * any custom styles.
- *
- * @return
- *   An array of image styles, keyed by the style name.
- * @see image_image_default_styles()
- */
-function hook_image_default_styles() {
-  $styles = array();
-
-  $styles['mymodule_preview'] = array(
-    'effects' => array(
-      array(
-        'name' => 'image_scale',
-        'data' => array('width' => 400, 'height' => 400, 'upscale' => 1),
-        'weight' => 0,
-      ),
-      array(
-        'name' => 'image_desaturate',
-        'data' => array(),
-        'weight' => 1,
-      ),
-    ),
-  );
-
-  return $styles;
 }
 
  /**
