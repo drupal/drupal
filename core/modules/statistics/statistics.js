6a064f416a22e5200e93696c1876f57a9ca49ab5
webchick <webchick@24967.no-reply.drupal.org>
2012-09-19 23:36:58 -0700

    Issue #1209532 by timmillwood, lucascaro, wiifm, mikeytown2, slashrsm: Count node views via AJAX in the statistics module.

--- core/modules/statistics/statistics.js
+++ core/modules/statistics/statistics.js
@@ -3,13 +3,11 @@
   "use strict";
 
   $(document).ready(function() {
-    var nid = Drupal.settings.statistics.nid;
-    var basePath = Drupal.settings.basePath;
     $.ajax({
       type: "POST",
       cache: false,
-      url: basePath+"core/modules/statistics/statistics.php",
-      data: "nid="+nid
+      url: Drupal.settings.statistics.url,
+      data: Drupal.settings.statistics.data
     });
   });
 })(jQuery);
