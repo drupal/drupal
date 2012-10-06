7bd2b2c3ae97ec55a06db25df3f91ca5544a5325
webchick <webchick@24967.no-reply.drupal.org>
2012-10-06 10:42:38 -0700

    Issue #1742438 by larowlan, Christian Biggins, kim.pepper, Everett Zufelt: Fixed Vertical tabs header and container rendered even when no vertical tabs.

--- core/modules/system/lib/Drupal/system/Tests/Form/ElementsVerticalTabsTest.php
+++ core/modules/system/lib/Drupal/system/Tests/Form/ElementsVerticalTabsTest.php
@@ -29,6 +29,14 @@ class ElementsVerticalTabsTest extends WebTestBase {
     );
   }
 
+  function setUp() {
+    parent::setUp();
+
+    $this->admin_user = $this->drupalCreateUser(array('access vertical_tab_test tabs'));
+    $this->web_user = $this->drupalCreateUser();
+    $this->drupalLogin($this->admin_user);
+  }
+
   /**
    * Ensures that vertical-tabs.js is included before collapse.js.
    *
@@ -39,5 +47,21 @@ class ElementsVerticalTabsTest extends WebTestBase {
     $position1 = strpos($this->content, 'core/misc/vertical-tabs.js');
     $position2 = strpos($this->content, 'core/misc/collapse.js');
     $this->assertTrue($position1 !== FALSE && $position2 !== FALSE && $position1 < $position2, t('vertical-tabs.js is included before collapse.js'));
+  }
+
+  /**
+   * Ensures that vertical tab markup is not shown if user has no tab access.
+   */
+  function testWrapperNotShownWhenEmpty() {
+    // Test admin user can see vertical tabs and wrapper.
+    $this->drupalGet('form_test/vertical-tabs');
+    $wrapper = $this->xpath("//div[@class='vertical-tabs-panes']");
+    $this->assertTrue(isset($wrapper[0]), 'Vertical tab panes found.');
+
+    // Test wrapper markup not present for non-privileged web user.
+    $this->drupalLogin($this->web_user);
+    $this->drupalGet('form_test/vertical-tabs');
+    $wrapper = $this->xpath("//div[@class='vertical-tabs-panes']");
+    $this->assertFalse(isset($wrapper[0]), 'Vertical tab wrappers are not displayed to unprivileged users.');
   }
 }
