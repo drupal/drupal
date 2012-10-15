161a3532d3c740e02ed1d0dd4f65d4d96515073f
webchick <webchick@24967.no-reply.drupal.org>
2012-10-15 16:24:33 -0700

    Issue #1734642 by attiks, Jelle_S, Gábor Hojtsy: Added Move breakpoint module into core.

---
+++ core/modules/breakpoint/lib/Drupal/breakpoint/InvalidBreakpointException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\breakpoint\InvalidBreakpointException.
+ */
+
+namespace Drupal\breakpoint;
+
+/**
+ * Base exception for breakpoint exception.
+ */
+class InvalidBreakpointException extends \RuntimeException {}
---
+++ core/modules/breakpoint/lib/Drupal/breakpoint/InvalidBreakpointSourceException.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\breakpoint\InvalidBreakpointSourceException.
+ */
+
+namespace Drupal\breakpoint;
+
+use Drupal\breakpoint\InvalidBreakpointException;
+
+/**
+ * Exception thrown if an invalid source is detected.
+ */
+class InvalidBreakpointSourceException extends InvalidBreakpointException {}
