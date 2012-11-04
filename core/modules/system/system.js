ab459f876228bf051715fe8716df9b45ebed4c45
catch <catch@35733.no-reply.drupal.org>
2012-11-04 21:32:20 +0000

    Issue #492720 by pivica, seutje, nod_, JamesAn, sun: Refactor system.js file and use Drupal form states and form ajax.

--- core/modules/system/system.js
+++ core/modules/system/system.js
@@ -1,25 +1,9 @@
-(function ($) {
+(function ($, Drupal, drupalSettings) {
 
 "use strict";
 
-/**
- * Show/hide the 'Email site administrator when updates are available' checkbox
- * on the install page.
- */
-Drupal.hideEmailAdministratorCheckbox = function () {
-  // Make sure the secondary box is shown / hidden as necessary on page load.
-  if ($('#edit-update-status-module-1').is(':checked')) {
-    $('.form-item-update-status-module-2').show();
-  }
-  else {
-    $('.form-item-update-status-module-2').hide();
-  }
-
-  // Toggle the display as necessary when the checkbox is clicked.
-  $('#edit-update-status-module-1').change( function () {
-    $('.form-item-update-status-module-2').toggle();
-  });
-};
+// Cache IDs in an array for ease of use.
+var ids = [];
 
 /**
  * When a field is filled out, apply its value to other fields that will likely
@@ -27,68 +11,53 @@ Drupal.hideEmailAdministratorCheckbox = function () {
  * administrator e-mail address with the same value as the site e-mail address.
  */
 Drupal.behaviors.copyFieldValue = {
-  attach: function (context, settings) {
-    for (var sourceId in settings.copyFieldValue) {
-      $('#' + sourceId, context).once('copy-field-values').bind('blur', function () {
-        // Get the list of target fields.
-        var targetIds = settings.copyFieldValue[sourceId];
-        // Add the behavior to update target fields on blur of the primary field.
-        for (var delta in targetIds) {
-          var targetField = $('#' + targetIds[delta]);
-          if (targetField.val() === '') {
-            targetField.val(this.value);
-          }
-        }
-      });
-    }
-  }
-};
-
-/**
- * Show/hide custom format sections on the regional settings page.
- */
-Drupal.behaviors.dateTime = {
-  attach: function (context, settings) {
-    for (var fieldName in settings.dateTime) {
-      if (settings.dateTime.hasOwnProperty(fieldName)) {
-        (function (fieldSettings, fieldName) {
-          var source = '#edit-' + fieldName;
-          var suffix = source + '-suffix';
-
-          // Attach keyup handler to custom format inputs.
-          $(context).find('input' + source).once('date-time').keyup(function () {
-            var input = $(this);
-            var url = fieldSettings.lookup + (/\?q=/.test(fieldSettings.lookup) ? '&format=' : '?format=') + encodeURIComponent(input.val());
-            $.getJSON(url, function (data) {
-              $(suffix).empty().append(' ' + fieldSettings.text + ': <em>' + data + '</em>');
-            });
-          });
-        })(settings.dateTime[fieldName], fieldName);
+  attach: function (context) {
+    // List of fields IDs on which to bind the event listener.
+    // Create an array of IDs to use with jQuery.
+    for (var sourceId in drupalSettings.copyFieldValue) {
+      if (drupalSettings.copyFieldValue.hasOwnProperty(sourceId)) {
+        ids.push(sourceId);
       }
     }
+    if (ids.length) {
+      // Listen to value:copy events on all dependant fields.
+      // We have to use body and not document because of the way jQuery events
+      // bubble up the DOM tree.
+      $('body').once('copy-field-values').on('value:copy', this.valueTargetCopyHandler);
+      // Listen on all source elements.
+      $('#' + ids.join(', #')).once('copy-field-values').on('blur', this.valueSourceBlurHandler);
+    }
+  },
+  detach: function (context, settings, trigger) {
+    if (trigger === 'unload' && ids.length) {
+      $('body').removeOnce('copy-field-values').off('value:copy');
+      $('#' + ids.join(', #')).removeOnce('copy-field-values').off('blur');
+    }
+  },
+  /**
+   * Event handler that fill the target element with the specified value.
+   *
+   * @param e
+   *   Event object.
+   * @param value
+   *   Custom value from jQuery trigger.
+   */
+  valueTargetCopyHandler: function (e, value) {
+    var $target = $(e.target);
+    if ($target.val() === '') {
+      $target.val(value);
+    }
+  },
+  /**
+   * Handler for a Blur event on a source field.
+   *
+   * This event handler will trigger a 'value:copy' event on all dependant fields.
+   */
+  valueSourceBlurHandler: function (e) {
+    var value = $(e.target).val();
+    var targetIds = drupalSettings.copyFieldValue[e.target.id];
+    $('#' + targetIds.join(', #')).trigger('value:copy', value);
   }
 };
 
- /**
- * Show/hide settings for page caching depending on whether page caching is
- * enabled or not.
- */
-Drupal.behaviors.pageCache = {
-  attach: function (context, settings) {
-    var $context = $(context);
-    $context.find('#edit-cache-0').change(function () {
-      $('#page-compression-wrapper').hide();
-      $('#cache-error').hide();
-    });
-    $context.find('#edit-cache-1').change(function () {
-      $('#page-compression-wrapper').show();
-      $('#cache-error').hide();
-    });
-    $context.find('#edit-cache-2').change(function () {
-      $('#page-compression-wrapper').show();
-      $('#cache-error').show();
-    });
-  }
-};
-
-})(jQuery);
+})(jQuery, Drupal, drupalSettings);
