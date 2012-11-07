72654e025610cee63dedc1d45f50169fd35057eb
webchick <webchick@24967.no-reply.drupal.org>
2012-11-07 04:46:43 -0800

    Issue #1826368 by larowlan, falcon03: Place dropbutton operations after their heading.

--- core/modules/views/views_ui/theme/views-ui-display-tab-bucket.tpl.php
+++ core/modules/views/views_ui/theme/views-ui-display-tab-bucket.tpl.php
@@ -6,11 +6,11 @@
  */
 ?>
 <div <?php print $attributes; ?>>
-  <?php if (!empty($actions)) : ?>
-    <?php print $actions; ?>
-  <?php endif; ?>
   <?php if (!empty($title)) : ?>
     <h3><?php print $title; ?></h3>
+  <?php endif; ?>
+  <?php if (!empty($actions)) : ?>
+    <?php print $actions; ?>
   <?php endif; ?>
   <?php print $content; ?>
 </div>
