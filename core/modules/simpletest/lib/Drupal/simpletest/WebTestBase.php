fcf8c1b207bc30f018ed74e24c0cbf629f87ad8c
webchick <webchick@24967.no-reply.drupal.org>
2013-01-08 22:38:39 -0800

    Issue #1882584 by Berdir: Improve concurrent testing by avoiding unecessary persistent rebuilds.

--- core/modules/simpletest/lib/Drupal/simpletest/WebTestBase.php
+++ core/modules/simpletest/lib/Drupal/simpletest/WebTestBase.php
@@ -900,18 +900,6 @@ abstract class WebTestBase extends TestBase {
     $this->loggedInUser = FALSE;
     $this->additionalCurlOptions = array();
 
-    // Reload module list and implementations to ensure that test module hooks
-    // aren't called after tests.
-    system_list_reset();
-    module_list_reset();
-    module_implements_reset();
-
-    // Reset the Field API.
-    field_cache_clear();
-
-    // Rebuild caches.
-    $this->refreshVariables();
-
     // Close the CURL handler.
     $this->curlClose();
   }
