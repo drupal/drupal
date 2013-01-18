c27d77eb6cd2ced7e25b1c284f9339d9f895e698
webchick <webchick@24967.no-reply.drupal.org>
2013-01-17 16:05:10 -0800

    Revert "Issue #1854874 by damiankloip: Add serializer support for XML."
    Testbot done did brokeded.
    
    This reverts commit 4aaa725268a03eced121a3131ccaef47b1c4d649.

--- core/modules/system/lib/Drupal/system/Tests/Serialization/EntitySerializationTest.php
+++ core/modules/system/lib/Drupal/system/Tests/Serialization/EntitySerializationTest.php
@@ -10,7 +10,6 @@ namespace Drupal\system\Tests\Serialization;
 use Drupal\simpletest\WebTestBase;
 use Symfony\Component\Serializer\Serializer;
 use Drupal\Core\Serialization\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder as BaseXmlEncoder;
 use Drupal\Core\Serialization\ComplexDataNormalizer;
 use Drupal\Core\Serialization\TypedDataNormalizer;
 
@@ -121,21 +120,9 @@ class EntitySerializationTest extends WebTestBase {
     $expected = json_encode($normalized);
     // Test 'json'.
     $actual = $serializer->serialize($this->entity, 'json');
-    $this->assertIdentical($actual, $expected, 'Entity serializes to JSON when "json" is requested.');
-    $actual = $serializer->serialize($normalized, 'json');
-    $this->assertIdentical($actual, $expected, 'A normalized array serializes to JSON when "json" is requested');
+    $this->assertIdentical($actual, $expected, 'Entity serializes to JSON when json is requested.');
     // Test 'ajax'.
     $actual = $serializer->serialize($this->entity, 'ajax');
-    $this->assertIdentical($actual, $expected, 'Entity serializes to JSON when "ajax" is requested.');
-    $actual = $serializer->serialize($normalized, 'ajax');
-    $this->assertIdentical($actual, $expected, 'A normalized array serializes to JSON when "ajax" is requested');
-    // Test 'xml'. The expected output should match that of Symfony's XmlEncoder.
-    $expected ='<?xml version="1.0"?>
-<response><id><value>1</value></id><revision_id><value>1</value></revision_id><uuid><value>' . $this->entity->uuid() . '</value></uuid><langcode><value>und</value></langcode><default_langcode><value/></default_langcode><name><value>' . $this->values['name'] . '</value></name><user_id><value>1</value></user_id><field_test_text><value>' . $this->values['field_test_text']['value'] . '</value><format>full_html</format></field_test_text></response>
-';
-    $actual = $serializer->serialize($this->entity, 'xml');
-    $this->assertIdentical($actual, $expected, 'Entity serializes to XML when "xml" is requested.');
-    $actual = $serializer->serialize($normalized, 'xml');
-    $this->assertIdentical($actual, $expected, 'A normalized array serializes to XML when "xml" is requested');
+    $this->assertIdentical($actual, $expected, 'Entity serializes to JSON when ajax is requested.');
   }
 }
