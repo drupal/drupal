f0d3dcfaeddba6ecbc61fa07c6f57496e5d26828
catch <catch@35733.no-reply.drupal.org>
2013-02-06 12:07:06 +0000

    Issue #1643410 by mgifford, mparker17: Add ARIA landmarks for Seven theme.

--- core/themes/seven/templates/page.tpl.php
+++ core/themes/seven/templates/page.tpl.php
@@ -16,22 +16,24 @@
  * @ingroup themeable
  */
 ?>
-  <div id="branding" class="clearfix">
+  <div id="branding" class="clearfix" role="navigation">
     <?php print $breadcrumb; ?>
     <?php print render($title_prefix); ?>
     <?php if ($title): ?>
       <h1 class="page-title"><?php print $title; ?></h1>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
-    <?php print render($primary_local_tasks); ?>
+    <?php if ($primary_local_tasks): ?>
+      <?php print '<div role="tab">' . render($primary_local_tasks) . '</div>'; ?>
+    <?php endif; ?>
   </div>
 
   <div id="page">
     <?php if ($secondary_local_tasks): ?>
-      <div class="tabs-secondary clearfix"><?php print render($secondary_local_tasks); ?></div>
+      <div class="tabs-secondary clearfix" role="navigation"><?php print render($secondary_local_tasks); ?></div>
     <?php endif; ?>
 
-    <div id="content" class="clearfix">
+    <div id="content" class="clearfix" role="main">
       <div class="element-invisible"><a id="main-content"></a></div>
       <?php if ($messages): ?>
         <div id="console" class="clearfix"><?php print $messages; ?></div>
@@ -45,7 +47,7 @@
       <?php print render($page['content']); ?>
     </div>
 
-    <div id="footer">
+    <div id="footer" role="contentinfo">
       <?php print $feed_icons; ?>
     </div>
 
