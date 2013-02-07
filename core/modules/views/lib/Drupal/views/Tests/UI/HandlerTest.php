a039a98aef8b34b0be0e7e69b99fc881f37d4312
catch <catch@35733.no-reply.drupal.org>
2013-02-07 14:12:47 +0000

    Issue #1906794 by dawehner: Fixed Using a relationship in the UI fails completly.

--- core/modules/views/lib/Drupal/views/Tests/UI/HandlerTest.php
+++ core/modules/views/lib/Drupal/views/Tests/UI/HandlerTest.php
@@ -21,7 +21,7 @@ class HandlerTest extends UITestBase {
    *
    * @var array
    */
-  public static $testViews = array('test_view');
+  public static $testViews = array('test_view_empty');
 
   public static function getInfo() {
     return array(
@@ -32,38 +32,115 @@ class HandlerTest extends UITestBase {
   }
 
   /**
+   * Overrides \Drupal\views\Tests\ViewTestBase::schemaDefinition().
+   *
+   * Adds a uid column to test the relationships.
+   */
+  protected function schemaDefinition() {
+    $schema = parent::schemaDefinition();
+
+    $schema['views_test_data']['fields']['uid'] = array(
+      'description' => "The {users}.uid of the author of the beatle entry.",
+      'type' => 'int',
+      'unsigned' => TRUE,
+      'not null' => TRUE,
+      'default' => 0
+    );
+
+    return $schema;
+  }
+
+  /**
+   * Overrides \Drupal\views\Tests\ViewTestBase::viewsData().
+   *
+   * Adds a relationship for the uid column.
+   */
+  protected function viewsData() {
+    $data = parent::viewsData();
+    $data['views_test_data']['uid'] = array(
+      'title' => t('UID'),
+      'help' => t('The test data UID'),
+      'relationship' => array(
+        'id' => 'standard',
+        'base' => 'users',
+        'base field' => 'uid'
+      )
+    );
+
+    return $data;
+  }
+
+  /**
    * Tests UI CRUD.
    */
   public function testUICRUD() {
     $handler_types = ViewExecutable::viewsHandlerTypes();
     foreach ($handler_types as $type => $type_info) {
       // Test adding handlers.
-      $add_handler_url = "admin/structure/views/nojs/add-item/test_view/default/$type";
+      $add_handler_url = "admin/structure/views/nojs/add-item/test_view_empty/default/$type";
 
       // Area handler types need to use a different handler.
       if (in_array($type, array('header', 'footer', 'empty'))) {
         $this->drupalPost($add_handler_url, array('name[views.area]' => TRUE), t('Add and configure @handler', array('@handler' => $type_info['ltitle'])));
-        $edit_handler_url = "admin/structure/views/nojs/config-item/test_view/default/$type/area";
+        $id = 'area';
+        $edit_handler_url = "admin/structure/views/nojs/config-item/test_view_empty/default/$type/$id";
       }
       elseif ($type == 'relationship') {
-        // @todo Add a views_test_data relationship handler to test.
-        continue;
+        $this->drupalPost($add_handler_url, array('name[views_test_data.uid]' => TRUE), t('Add and configure @handler', array('@handler' => $type_info['ltitle'])));
+        $id = 'uid';
+        $edit_handler_url = "admin/structure/views/nojs/config-item/test_view_empty/default/$type/$id";
       }
       else {
         $this->drupalPost($add_handler_url, array('name[views_test_data.job]' => TRUE), t('Add and configure @handler', array('@handler' => $type_info['ltitle'])));
-        $edit_handler_url = "admin/structure/views/nojs/config-item/test_view/default/$type/job";
+        $id = 'job';
+        $edit_handler_url = "admin/structure/views/nojs/config-item/test_view_empty/default/$type/$id";
       }
 
       $this->assertUrl($edit_handler_url, array(), 'The user got redirected to the handler edit form.');
       $this->drupalPost(NULL, array(), t('Apply'));
 
-      $this->assertUrl('admin/structure/views/view/test_view/edit', array(), 'The user got redirected to the views edit form.');
+      $this->assertUrl('admin/structure/views/view/test_view_empty/edit', array(), 'The user got redirected to the views edit form.');
 
       $this->assertLinkByHref($edit_handler_url, 0, 'The handler edit link appears in the UI.');
 
+      // Save the view and have a look whether the handler was added as expected.
+      $this->drupalPost(NULL, array(), t('Save'));
+      $views = $this->container->get('plugin.manager.entity')->getStorageController('view')->load(array('test_view_empty'));
+      $view = reset($views);
+      $display = $view->getDisplay('default');
+      $this->assertTrue(isset($display['display_options'][$type_info['plural']][$id]), 'Ensure the field was added to the view itself.');
+
+      // Remove the item and check that it's removed
       $this->drupalPost($edit_handler_url, array(), t('Remove'));
       $this->assertNoLinkByHref($edit_handler_url, 0, 'The handler edit link does not appears in the UI after removing.');
+
+      $this->drupalPost(NULL, array(), t('Save'));
+      $views = $this->container->get('plugin.manager.entity')->getStorageController('view')->load(array('test_view_empty'));
+      $view = reset($views);
+      $display = $view->getDisplay('default');
+      $this->assertFalse(isset($display['display_options'][$type_info['plural']][$id]), 'Ensure the field was removed from the view itself.');
     }
+
+    // Test adding a field of the user table using the uid relationship.
+    $type_info = $handler_types['relationship'];
+    $add_handler_url = "admin/structure/views/nojs/add-item/test_view_empty/default/relationship";
+    $this->drupalPost($add_handler_url, array('name[views_test_data.uid]' => TRUE), t('Add and configure @handler', array('@handler' => $type_info['ltitle'])));
+
+    $add_handler_url = "admin/structure/views/nojs/add-item/test_view_empty/default/field";
+    $type_info = $handler_types['field'];
+    $this->drupalPost($add_handler_url, array('name[users.signature]' => TRUE), t('Add and configure @handler', array('@handler' => $type_info['ltitle'])));
+    $id = 'signature';
+    $edit_handler_url = "admin/structure/views/nojs/config-item/test_view_empty/default/field/$id";
+
+    $this->assertUrl($edit_handler_url, array(), 'The user got redirected to the handler edit form.');
+    $this->assertFieldByName('options[relationship]', 'uid', 'Ensure the relationship select is filled with the UID relationship.');
+    $this->drupalPost(NULL, array(), t('Apply'));
+
+    $this->drupalPost(NULL, array(), t('Save'));
+    $views = $this->container->get('plugin.manager.entity')->getStorageController('view')->load(array('test_view_empty'));
+    $view = reset($views);
+    $display = $view->getDisplay('default');
+    $this->assertTrue(isset($display['display_options'][$type_info['plural']][$id]), 'Ensure the field was added to the view itself.');
   }
 
 }
