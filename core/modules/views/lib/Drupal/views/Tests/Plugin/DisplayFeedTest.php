c432629f25c4b01766951b318bc7991a3ea32dea
Dries <dries@buytaert.net>
2013-02-09 11:58:36 +1100

    Issue #1903410 by dawehner: Fixed Wrong config key is used in template_preprocess_views_view_rss().

--- core/modules/views/lib/Drupal/views/Tests/Plugin/DisplayFeedTest.php
+++ core/modules/views/lib/Drupal/views/Tests/Plugin/DisplayFeedTest.php
@@ -52,7 +52,7 @@ class DisplayFeedTest extends PluginTestBase {
     $this->drupalGet('admin/structure/views');
 
     // Check the attach TO interface.
-    $this->drupalGet('admin/structure/views/nojs/display/test_feed_display/feed/displays');
+    $this->drupalGet('admin/structure/views/nojs/display/test_feed_display/feed_1/displays');
 
     // Load all the options of the checkbox.
     $result = $this->xpath('//div[@id="edit-displays"]/div');
@@ -68,14 +68,39 @@ class DisplayFeedTest extends PluginTestBase {
     $this->assertEqual($options, array('default', 'page'), 'Make sure all displays appears as expected.');
 
     // Post and save this and check the output.
-    $this->drupalPost('admin/structure/views/nojs/display/test_feed_display/feed/displays', array('displays[page]' => 'page'), t('Apply'));
-    $this->drupalGet('admin/structure/views/view/test_feed_display/edit/feed');
-    $this->assertFieldByXpath('//*[@id="views-feed-displays"]', 'Page');
+    $this->drupalPost('admin/structure/views/nojs/display/test_feed_display/feed_1/displays', array('displays[page]' => 'page'), t('Apply'));
+    $this->drupalGet('admin/structure/views/view/test_feed_display/edit/feed_1');
+    $this->assertFieldByXpath('//*[@id="views-feed-1-displays"]', 'Page');
 
     // Add the default display, so there should now be multiple displays.
-    $this->drupalPost('admin/structure/views/nojs/display/test_feed_display/feed/displays', array('displays[default]' => 'default'), t('Apply'));
-    $this->drupalGet('admin/structure/views/view/test_feed_display/edit/feed');
-    $this->assertFieldByXpath('//*[@id="views-feed-displays"]', 'Multiple displays');
+    $this->drupalPost('admin/structure/views/nojs/display/test_feed_display/feed_1/displays', array('displays[default]' => 'default'), t('Apply'));
+    $this->drupalGet('admin/structure/views/view/test_feed_display/edit/feed_1');
+    $this->assertFieldByXpath('//*[@id="views-feed-1-displays"]', 'Multiple displays');
+  }
+
+  /**
+   * Tests the rendered output.
+   */
+  public function testFeedOutput() {
+    $this->drupalCreateNode();
+
+    // Test the site name setting.
+    $site_name = $this->randomName();
+    $this->container->get('config.factory')->get('system.site')->set('name', $site_name)->save();
+
+    $this->drupalGet('test-feed-display.xml');
+    $result = $this->xpath('//title');
+    $this->assertEqual($result[0], $site_name, 'The site title is used for the feed title.');
+
+    $views = $this->container->get('plugin.manager.entity')->getStorageController('view')->load(array('test_feed_display'));
+    $view = array_pop($views);
+    $display = &$view->getDisplay('feed_1');
+    $display['display_options']['sitename_title'] = 0;
+    $view->save();
+
+    $this->drupalGet('test-feed-display.xml');
+    $result = $this->xpath('//title');
+    $this->assertEqual($result[0], 'test_feed_display', 'The display title is used for the feed title.');
   }
 
 }
