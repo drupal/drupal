e2941705fbddfb6948d94fb96e23b468bfd47fc4
webchick <webchick@24967.no-reply.drupal.org>
2013-03-26 20:40:17 -0700

    Issue #1949266 by chx: Fixed phpunit errors are silently eaten.

---
+++ core/modules/simpletest/tests/Drupal/simpletest/Tests/PhpUnitErrorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\simpletest\Tests;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Test PHPUnit errors are getting converted to Simpletest errors.
+ */
+class PhpUnitErrorTest extends UnitTestCase {
+
+  public static function getInfo() {
+    return array(
+      'name' => 'PHPUnit errors',
+      'description' => 'Test PHPUnit errors getting converted to Simpletest errors.',
+      'group' => 'Simpletest',
+    );
+  }
+
+  /**
+   * Test errors reported.
+   */
+  public function testPhpUnitXmlParsing() {
+    require_once __DIR__ . '/../../../../simpletest.module';
+    $phpunit_error_xml = __DIR__ . '/phpunit_error.xml';
+    $res = simpletest_phpunit_xml_to_rows(1, $phpunit_error_xml);
+    $this->assertNotEquals($res[0]['status'], 'pass');
+    $this->assertEquals($res[0]['status'], 'fail');
+    // Make sure simpletest_phpunit_xml_to_rows() does not balk if the test
+    // didn't run.
+    simpletest_phpunit_xml_to_rows(1, 'foobar');
+  }
+}
