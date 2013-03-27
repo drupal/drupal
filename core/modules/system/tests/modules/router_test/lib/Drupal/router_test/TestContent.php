221099b5717292a8d7e4d9f76fc3b93c12a42ef9
Dries <dries@buytaert.net>
2013-03-27 15:27:28 -0400

    Issue #1938980 by Crell, rootatwc, tim.plunkett: Move controller derivation to route enhancers.

---
+++ core/modules/system/tests/modules/router_test/lib/Drupal/router_test/TestContent.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\router_test\TestContent.
+ */
+
+namespace Drupal\router_test;
+
+/**
+ * Test controllers that are intended to be wrapped in a main controller.
+ */
+class TestContent {
+
+  public function test1() {
+    return 'abcde';
+  }
+
+}
