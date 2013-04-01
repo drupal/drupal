8104ea72c28c1a78fe039f2a9f918d724b9ab545
David Rothstein <drothstein@gmail.com>
2013-04-01 17:24:49 -0400

    Issue #1728122 by rymo | HonoredMule: Fixed Sticky Table header tables don't inherit parent table's outer width.

--- misc/tableheader.js
+++ misc/tableheader.js
@@ -126,7 +126,7 @@ Drupal.tableHeader.prototype.eventhandlerRecalculateStickyHeader = function (eve
         $stickyCell.css('display', 'none');
       }
     }
-    this.stickyTable.css('width', this.originalTable.css('width'));
+    this.stickyTable.css('width', this.originalTable.outerWidth());
   }
 };
 
