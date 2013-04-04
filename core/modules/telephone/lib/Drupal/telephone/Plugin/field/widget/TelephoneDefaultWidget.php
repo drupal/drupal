e28f1738f82d12b5c6cff2b623343f211b55924e
catch <catch@35733.no-reply.drupal.org>
2013-04-04 21:31:25 +0100

    Issue #1942902 by Dave Reid: Fixed Placeholder not supported in telephone default widget.

--- core/modules/telephone/lib/Drupal/telephone/Plugin/field/widget/TelephoneDefaultWidget.php
+++ core/modules/telephone/lib/Drupal/telephone/Plugin/field/widget/TelephoneDefaultWidget.php
@@ -20,10 +20,26 @@ use Drupal\field\Plugin\Type\Widget\WidgetBase;
  *   label = @Translation("Telephone number"),
  *   field_types = {
  *     "telephone"
+ *   },
+ *   settings = {
+ *     "placeholder" = ""
  *   }
  * )
  */
 class TelephoneDefaultWidget extends WidgetBase {
+
+  /**
+   * Implements Drupal\field\Plugin\Type\Widget\WidgetInterface::settingsForm().
+   */
+  public function settingsForm(array $form, array &$form_state) {
+    $element['placeholder'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Placeholder'),
+      '#default_value' => $this->getSetting('placeholder'),
+      '#description' => t('Text that will be shown inside the field until a value is entered. This hint is usually a sample value or a brief description of the expected format.'),
+    );
+    return $element;
+  }
 
   /**
    * Implements \Drupal\field\Plugin\Type\Widget\WidgetInterface::formElement().
@@ -32,6 +48,7 @@ class TelephoneDefaultWidget extends WidgetBase {
     $element['value'] = $element + array(
       '#type' => 'tel',
       '#default_value' => isset($items[$delta]['value']) ? $items[$delta]['value'] : NULL,
+      '#placeholder' => $this->getSetting('placeholder'),
     );
     return $element;
   }
