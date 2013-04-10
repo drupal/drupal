38226cc8185246d723cbb7352a636f34139d97f3
webchick <webchick@24967.no-reply.drupal.org>
2013-04-10 13:12:01 -0700

    Issue #1914256 by damiankloip, dawehner, tim.plunkett: Add all views hooks to hook_hook_info().

--- core/modules/views/lib/Drupal/views/Tests/ViewsHooksTest.php
+++ core/modules/views/lib/Drupal/views/Tests/ViewsHooksTest.php
@@ -16,42 +16,83 @@ namespace Drupal\views\Tests;
 class ViewsHooksTest extends ViewUnitTestBase {
 
   /**
-   * Modules to enable.
+   * Views used by this test.
    *
    * @var array
    */
-  public static $modules = array('views_test_data');
+  public static $testViews = array('test_view');
+
+  /**
+   * An array of available views hooks to test.
+   *
+   * @var array
+   */
+  protected static $hooks = array (
+    'views_data' => 'all',
+    'views_data_alter' => 'alter',
+    'views_query_substitutions' => 'view',
+    'views_form_substitutions' => 'view',
+    'views_analyze' => 'view',
+    'views_pre_view' => 'view',
+    'views_pre_build' => 'view',
+    'views_post_build' => 'view',
+    'views_pre_execute' => 'view',
+    'views_post_execute' => 'view',
+    'views_pre_render' => 'view',
+    'views_post_render' => 'view',
+    'views_query_alter'  => 'view',
+    'views_invalidate_cache' => 'all',
+  );
+
+  /**
+   * The module handler to use for invoking hooks.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
 
   public static function getInfo() {
     return array(
-      'name' => 'Views Hooks',
+      'name' => 'Views hooks',
       'description' => 'Tests that views hooks are registered when defined in $module.views.inc.',
       'group' => 'Views',
     );
+  }
+
+  protected function setUp() {
+    parent::setUp();
+
+    $this->moduleHandler = $this->container->get('module_handler');
   }
 
   /**
    * Tests the hooks.
    */
   public function testHooks() {
-    $hooks = &drupal_static(__FUNCTION__);
+    $view = views_get_view('test_view');
 
-    $views_hooks = array(
-      'views_data',
-      'views_data_alter',
-      'views_query_substitutions',
-      'views_form_substitutions',
-      'field_views_data',
-      'field_views_data_alter',
-    );
+    // Test each hook is found in the implementations array and is invoked.
+    foreach (static::$hooks as $hook => $type) {
+      $this->assertTrue($this->moduleHandler->implementsHook('views_test_data', $hook), format_string('The hook @hook was registered.', array('@hook' => $hook)));
 
-    foreach ($views_hooks as $hook) {
-      $implementations = module_implements($hook);
-      $this->assertTrue(in_array('views_test_data', $implementations), format_string('The hook @hook was registered.', array('@hook' => $hook)));
+      switch ($type) {
+        case 'view':
+          $this->moduleHandler->invoke('views_test_data', $hook, array($view));
+          break;
 
-      // Reset the module implements cache, so we ensure that the .views.inc
-      // file is loaded actively.
-      unset($hooks['implementations']);
+        case 'alter':
+          $data = array();
+          $this->moduleHandler->invoke('views_test_data', $hook, array($data));
+          break;
+
+        default:
+          $this->moduleHandler->invoke('views_test_data', $hook);
+      }
+
+      $this->assertTrue($this->container->get('state')->get('views_hook_test_' . $hook), format_string('The %hook hook was invoked.', array('%hook' => $hook)));
+      // Reset the module implementations cache, so we ensure that the
+      // .views.inc file is loaded actively.
+      $this->moduleHandler->resetImplementations();
     }
   }
 
