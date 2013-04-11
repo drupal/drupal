31618908e4ebae62ab7e4f5311d575bdfd9862b6
Alex Pott <alex.a.pott@googlemail.com>
2013-04-11 18:09:43 +0100

    Issue #1964880 by falcon03 | jessebeach: Convert the Overlay close button from an anchor element to a button element.

--- core/modules/overlay/templates/overlay.tpl.php
+++ core/modules/overlay/templates/overlay.tpl.php
@@ -28,7 +28,7 @@
       <h1 id="overlay-title"<?php print $title_attributes; ?>><?php print $title; ?></h1>
     </div>
     <div id="overlay-close-wrapper">
-      <a id="overlay-close" href="#" class="overlay-close"><span class="element-invisible"><?php print t('Close overlay'); ?></span></a>
+      <a id="overlay-close" href="#" class="overlay-close" role="button" aria-controls="overlay-content"><span class="element-invisible"><?php print t('Close overlay'); ?></span></a>
     </div>
     <?php if ($tabs): ?><h2 class="element-invisible"><?php print t('Primary tabs'); ?></h2><ul id="overlay-tabs"><?php print render($tabs); ?></ul><?php endif; ?>
   </div>
