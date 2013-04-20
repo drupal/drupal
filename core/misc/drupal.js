7981d5d07b2cade4655e6e4eeca0650e81df80a0
webchick <webchick@24967.no-reply.drupal.org>
2013-04-19 20:43:48 -0700

    Issue #1959306 by jessebeach: Drupal.announce does not handle multiple messages at the same time; Improve the utility so that simultaneously calls are read.

--- core/misc/drupal.js
+++ core/misc/drupal.js
@@ -259,66 +259,6 @@ Drupal.t = function (str, args, options) {
 };
 
 /**
- * Adds an HTML element and method to trigger audio UAs to read system messages.
- */
-var liveElement;
-
-/**
- * Builds a div element with the aria-live attribute and attaches it
- * to the DOM.
- */
-Drupal.behaviors.drupalAnnounce = {
-  attach: function (settings, context) {
-    liveElement = document.createElement('div');
-    liveElement.id = 'drupal-live-announce';
-    liveElement.className = 'element-invisible';
-    liveElement.setAttribute('aria-live', 'polite');
-    liveElement.setAttribute('aria-busy', 'false');
-    document.body.appendChild(liveElement);
-  }
-};
-
-/**
- * Triggers audio UAs to read the supplied text.
- *
- * @param {String} text
- *   - A string to be read by the UA.
- *
- * @param {String} priority
- *   - A string to indicate the priority of the message. Can be either
- *   'polite' or 'assertive'. Polite is the default.
- *
- * Use Drupal.announce to indicate to screen reader users that an element on
- * the page has changed state. For instance, if clicking a link loads 10 more
- * items into a list, one might announce the change like this.
- * $('#search-list')
- *   .on('itemInsert', function (event, data) {
- *     // Insert the new items.
- *     $(data.container.el).append(data.items.el);
- *     // Announce the change to the page contents.
- *     Drupal.announce(Drupal.t('@count items added to @container',
- *       {'@count': data.items.length, '@container': data.container.title}
- *     ));
- *   });
- *
- * @see http://www.w3.org/WAI/PF/aria-practices/#liveprops
- */
-Drupal.announce = function (text, priority) {
-  if (typeof text === 'string') {
-    // Clear the liveElement so that repeated strings will be read.
-    liveElement.innerHTML = '';
-    // Set the busy state to true until the node changes are complete.
-    liveElement.setAttribute('aria-busy', 'true');
-    // Set the priority to assertive, or default to polite.
-    liveElement.setAttribute('aria-live', (priority === 'assertive') ? 'assertive' : 'polite');
-    // Print the text to the live region.
-    liveElement.innerHTML = Drupal.checkPlain(text);
-    // The live text area is updated. Allow the AT to announce the text.
-    liveElement.setAttribute('aria-busy', 'false');
-  }
-};
-
-/**
  * Returns the URL to a Drupal page.
  */
 Drupal.url = function (path) {
