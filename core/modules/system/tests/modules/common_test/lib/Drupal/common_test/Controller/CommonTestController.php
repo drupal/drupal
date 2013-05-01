478490f23c74b21454da10965620ee30580fa910
catch <catch@35733.no-reply.drupal.org>
2013-05-01 09:53:04 +0100

    Issue #1922454 by thedavidmeister, steveoliver, Cottser: Gut l(), fix theme('link'), have a working framework for consistently generated links inside and outside of Twig.

---
+++ core/modules/system/tests/modules/common_test/lib/Drupal/common_test/Controller/CommonTestController.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\common_test\Controller\CommonTestController.
+ */
+
+namespace Drupal\common_test\Controller;
+
+use Drupal\Core\ControllerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Controller routines for common_test routes.
+ */
+class CommonTestController implements ControllerInterface {
+
+  /**
+   * Implements \Drupal\Core\ControllerInterface::create().
+   */
+  public static function create(ContainerInterface $container) {
+    return new static();
+  }
+
+  /**
+   * Returns links to the current page, with and without query strings.
+   *
+   * Using #type causes these links to be rendered with l().
+   */
+  public function lActiveClass() {
+    return array(
+      'no_query' => array(
+        '#type' => 'link',
+        '#title' => t('Link with no query string'),
+        '#href' => current_path(),
+      ),
+      'with_query' => array(
+        '#type' => 'link',
+        '#title' => t('Link with a query string'),
+        '#href' => current_path(),
+        '#options' => array(
+          'query' => array(
+            'foo' => 'bar',
+            'one' => 'two',
+          ),
+        ),
+      ),
+      'with_query_reversed' => array(
+        '#type' => 'link',
+        '#title' => t('Link with the same query string in reverse order'),
+        '#href' => current_path(),
+        '#options' => array(
+          'query' => array(
+            'one' => 'two',
+            'foo' => 'bar',
+          ),
+        ),
+      ),
+    );
+  }
+
+  /**
+   * Returns links to the current page, with and without query strings.
+   *
+   * Using #theme causes these links to be rendered with theme_link().
+   */
+  public function themeLinkActiveClass() {
+    return array(
+      'no_query' => array(
+        '#theme' => 'link',
+        '#text' => t('Link with no query string'),
+        '#path' => current_path(),
+      ),
+      'with_query' => array(
+        '#theme' => 'link',
+        '#text' => t('Link with a query string'),
+        '#path' => current_path(),
+        '#options' => array(
+          'query' => array(
+            'foo' => 'bar',
+            'one' => 'two',
+          ),
+        ),
+      ),
+      'with_query_reversed' => array(
+        '#theme' => 'link',
+        '#text' => t('Link with the same query string in reverse order'),
+        '#path' => current_path(),
+        '#options' => array(
+          'query' => array(
+            'one' => 'two',
+            'foo' => 'bar',
+          ),
+        ),
+      ),
+    );
+  }
+
+}
