6df1d3adf8464abbb87a0ce03ba134c9ba3ccbe9
Alex Pott <alex.a.pott@googlemail.com>
2013-05-01 17:51:13 +0100

    Issue #1979004 by plopesc: Convert tour_test_1() to a Controller.

---
+++ core/modules/tour/tests/tour_test/lib/Drupal/tour_test/Controller/TourTestController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\tour_test\Controller\TourTestController.
+ */
+
+namespace Drupal\tour_test\Controller;
+
+use Drupal\Core\ControllerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Controller routines for tour_test routes.
+ */
+class TourTestController implements ControllerInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static();
+  }
+
+  /**
+   * Outputs some content for testing tours.
+   */
+  public function tourTest1() {
+    return array(
+      'tip-1' => array(
+        '#type' => 'container',
+        '#attributes' => array(
+          'id' => 'tour-test-1',
+        ),
+        '#children' => t('Where does the rain in Spain fail?'),
+      ),
+      'tip-4' => array(
+        '#type' => 'container',
+        '#attributes' => array(
+          'id' => 'tour-test-4',
+        ),
+        '#children' => t('Tip created later?'),
+      ),
+    );
+  }
+
+  /**
+   * Outputs some content for testing tours.
+   */
+  public function tourTest2() {
+    return array(
+      '#type' => 'container',
+      '#attributes' => array(
+        'id' => 'tour-test-2',
+      ),
+      '#children' => t('Pangram example'),
+    );
+
+  }
+
+}
