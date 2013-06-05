fd7e482b11a0ce2aa130dc75b00cb363c3f5ece8
Dries <dries@buytaert.net>
2013-06-05 17:27:30 -0400

    Issue #1960888 by tim.plunkett, dawehner, jibran, damiankloip: Fixed Default archive view uses wrong argument plugin.

--- core/modules/views/lib/Drupal/views/Tests/DefaultViewsTest.php
+++ core/modules/views/lib/Drupal/views/Tests/DefaultViewsTest.php
@@ -14,7 +14,7 @@ use Drupal\views\ViewExecutable;
 /**
  * Tests for views default views.
  */
-class DefaultViewsTest extends WebTestBase {
+class DefaultViewsTest extends ViewTestBase {
 
   /**
    * Modules to enable.
@@ -158,6 +158,55 @@ class DefaultViewsTest extends WebTestBase {
     ));
     $term->save();
     return $term;
+  }
+
+  /**
+   * Tests the archive view.
+   */
+  public function testArchiveView() {
+    // Create additional nodes compared to the one in the setup method.
+    // Create two nodes in the same month, and one in each following month.
+    $node = array(
+      'created' => 280299600, // Sun, 19 Nov 1978 05:00:00 GMT
+    );
+    $this->drupalCreateNode($node);
+    $this->drupalCreateNode($node);
+    $node = array(
+      'created' => 282891600, // Tue, 19 Dec 1978 05:00:00 GMT
+    );
+    $this->drupalCreateNode($node);
+    $node = array(
+      'created' => 285570000, // Fri, 19 Jan 1979 05:00:00 GMT
+    );
+    $this->drupalCreateNode($node);
+
+    $view = views_get_view('archive');
+    $view->setDisplay('page_1');
+    $this->executeView($view);
+    $column_map = drupal_map_assoc(array('nid', 'created_year_month', 'num_records'));
+    $expected_result = array(
+      array(
+        'nid' => 1,
+        'created_year_month' => 201305,
+        'num_records' => 11,
+      ),
+      array(
+        'nid' => 15,
+        'created_year_month' => 197901,
+        'num_records' => 1,
+      ),
+      array(
+        'nid' => 14,
+        'created_year_month' => 197812,
+        'num_records' => 1,
+      ),
+      array(
+        'nid' => 12,
+        'created_year_month' => 197811,
+        'num_records' => 2,
+      ),
+    );
+    $this->assertIdenticalResultset($view, $expected_result, $column_map);
   }
 
 }
