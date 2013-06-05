99861e89d4f5b455ea609af3a489bd4219eb0e39
Alex Pott <alex.a.pott@googlemail.com>
2013-06-05 10:18:02 +0100

    Issue #1856556 by Berdir, andypost, das-peter: Convert user contact form into a contact category/bundle.

--- core/modules/contact/lib/Drupal/contact/CategoryListController.php
+++ core/modules/contact/lib/Drupal/contact/CategoryListController.php
@@ -34,6 +34,13 @@ class CategoryListController extends ConfigEntityListController {
         'weight' => 12,
       );
     }
+
+    if (!$entity->access('delete')) {
+      unset($operations['delete']);
+    }
+    if (!$entity->access('update')) {
+      unset($operations['edit']);
+    }
     return $operations;
   }
 
@@ -53,9 +60,16 @@ class CategoryListController extends ConfigEntityListController {
    */
   public function buildRow(EntityInterface $entity) {
     $row['category'] = check_plain($entity->label());
-    $row['recipients'] = check_plain(implode(', ', $entity->recipients));
-    $default_category = config('contact.settings')->get('default_category');
-    $row['selected'] = ($default_category == $entity->id() ? t('Yes') : t('No'));
+    // Special case the personal category.
+    if ($entity->id() == 'personal') {
+      $row['recipients'] = t('Selected user');
+      $row['selected'] = t('No');
+    }
+    else {
+      $row['recipients'] = check_plain(implode(', ', $entity->recipients));
+      $default_category = config('contact.settings')->get('default_category');
+      $row['selected'] = ($default_category == $entity->id() ? t('Yes') : t('No'));
+    }
     $row['operations']['data'] = $this->buildOperations($entity);
     return $row;
   }
