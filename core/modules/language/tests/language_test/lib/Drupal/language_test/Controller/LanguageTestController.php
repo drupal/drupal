dec3e5c014f4ba61d443b4590044c33de7217cb8
Alex Pott <alex.a.pott@googlemail.com>
2013-06-18 00:37:09 +0200

    Issue #1987728 by Niklas Fiekas, vijaycs85: Convert language_test_subrequest() to a new style controller.

--- core/modules/language/tests/language_test/lib/Drupal/language_test/Controller/LanguageTestController.php
+++ core/modules/language/tests/language_test/lib/Drupal/language_test/Controller/LanguageTestController.php
@@ -9,6 +9,8 @@ namespace Drupal\language_test\Controller;
 
 use Drupal\Core\Controller\ControllerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
  * Controller routines for language_test routes.
@@ -16,10 +18,27 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class LanguageTestController implements ControllerInterface {
 
   /**
+   * The HTTP kernel service.
+   *
+   * @var \Symfony\Component\HttpKernel\HttpKernelInterface
+   */
+  protected $httpKernel;
+
+  /**
+   * Constructs a new LanguageTestController object.
+   *
+   * @param \Symfony\Component\HttpKernel\HttpKernelInterface $httpKernel
+   *   An HTTP kernel.
+   */
+  public function __construct(HttpKernelInterface $httpKernel) {
+    $this->httpKernel = $httpKernel;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static();
+    return new static($container->get('http_kernel'));
   }
 
   /**
@@ -108,6 +127,29 @@ class LanguageTestController implements ControllerInterface {
         ),
       ),
     );
+  }
+
+  /**
+   * Uses a sub request to retrieve the 'user' page.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   The kernels response to the sub request.
+   */
+  public function testSubRequest() {
+    $request = Request::createFromGlobals();
+    $server = $request->server->all();
+    if (basename($server['SCRIPT_FILENAME']) != basename($server['SCRIPT_NAME'])) {
+      // We need this for when the test is executed by run-tests.sh.
+      // @todo Remove this once run-tests.sh has been converted to use a Request
+      //   object.
+      $server['SCRIPT_FILENAME'] = $server['SCRIPT_NAME'];
+      $base_path = ltrim($server['REQUEST_URI'], '/');
+    }
+    else {
+      $base_path = $request->getBasePath();
+    }
+    $sub_request = Request::create($base_path . '/user', 'GET', $request->query->all(), $request->cookies->all(), array(), $server);
+    return $this->httpKernel->handle($sub_request, HttpKernelInterface::SUB_REQUEST);
   }
 
 }
