270db4a5046ea1c2421ca62f3c0893a8bf558a97
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2013-06-19 10:14:55 +0100

    Issue #1852454 by patrickd, xjm, YesCT, Berdir, amateescu, Pancho, djroshi, dealancer, Wim Leers, jessebeach: Restrict module and theme name lengths to 50 characters.

---
+++ core/lib/Drupal/Core/Extension/ExtensionNameLengthException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Core\Extension\ExtensionNameLengthException.
+ */
+
+namespace Drupal\Core\Extension;
+
+/**
+ * Exception thrown when the extension's name length exceeds the allowed maximum.
+ */
+class ExtensionNameLengthException extends \Exception { }
