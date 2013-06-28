389338d5b2d93ed67160b76b0cf451822dceab67
Alex Pott <alex.a.pott@googlemail.com>
2013-06-28 10:13:11 +0100

    Issue #2015683 by plopesc: Convert field type to typed data plugin for telephone module .

---
+++ core/modules/telephone/lib/Drupal/telephone/Plugin/field/field_type/TelephoneItem.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\telephone\Plugin\field\field_type\TelephoneItem.
+ */
+
+namespace Drupal\telephone\Plugin\field\field_type;
+
+use Drupal\Core\Entity\Annotation\FieldType;
+use Drupal\Core\Annotation\Translation;
+use Drupal\field\Plugin\Type\FieldType\ConfigFieldItemBase;
+use Drupal\field\Plugin\Core\Entity\Field;
+
+/**
+ * Plugin implementation of the 'telephone' field type.
+ *
+ * @FieldType(
+ *   id = "telephone",
+ *   module = "telephone",
+ *   label = @Translation("Telephone number"),
+ *   description = @Translation("This field stores a telephone number in the database."),
+ *   default_widget = "telephone_default",
+ *   default_formatter = "telephone_link"
+ * )
+ */
+class TelephoneItem extends ConfigFieldItemBase {
+
+  /**
+   * Definitions of the contained properties.
+   *
+   * @var array
+   */
+  static $propertyDefinitions;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(Field $field) {
+    return array(
+      'columns' => array(
+        'value' => array(
+          'type' => 'varchar',
+          'length' => 256,
+          'not null' => FALSE,
+        ),
+      ),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions() {
+    if (!isset(static::$propertyDefinitions)) {
+      static::$propertyDefinitions['value'] = array(
+        'type' => 'string',
+        'label' => t('Telephone number'),
+      );
+    }
+    return static::$propertyDefinitions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $value = $this->get('value')->getValue();
+    return $value === NULL || $value === '';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConstraints() {
+    $constraint_manager = \Drupal::typedData()->getValidationConstraintManager();
+    $constraints = parent::getConstraints();
+
+    $max_length = 256;
+    $constraints[] = $constraint_manager->create('ComplexData', array(
+      'value' => array(
+        'Length' => array(
+          'max' => $max_length,
+          'maxMessage' => t('%name: the telephone number may not be longer than @max characters.', array('%name' => $this->getInstance()->label, '@max' => $max_length)),
+        )
+      ),
+    ));
+
+    return $constraints;
+  }
+
+}
