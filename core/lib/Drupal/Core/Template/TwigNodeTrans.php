2460eb7c77a59293f6a62cfc599cd638295314bb
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2013-07-21 17:50:08 +0100

    Issue #1927584 by Mark Carver, ezeedub, drupalninja99, Cottser, geoffreyr, John Bickar, jenlampton, penyaskito: Add support for the Twig {% trans %} tag extension.

---
+++ core/lib/Drupal/Core/Template/TwigNodeTrans.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Core\Template\TwigNodeTrans.
+ *
+ * This Twig extension was originally based on Twig i18n extension. It has been
+ * severely modified to work properly with the complexities of the Drupal
+ * translation system.
+ *
+ * @see http://twig.sensiolabs.org/doc/extensions/i18n.html
+ * @see https://github.com/fabpot/Twig-extensions
+ */
+
+namespace Drupal\Core\Template;
+
+/**
+ * A class that defines the Twig 'trans' tag for Drupal.
+ */
+class TwigNodeTrans extends \Twig_Node {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(\Twig_NodeInterface $body, \Twig_NodeInterface $plural = NULL, \Twig_Node_Expression $count = NULL, $lineno, $tag = NULL) {
+    parent::__construct(array(
+      'count' => $count,
+      'body' => $body,
+      'plural' => $plural
+    ), array(), $lineno, $tag);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function compile(\Twig_Compiler $compiler) {
+    $compiler->addDebugInfo($this);
+
+    list($singular, $tokens) = $this->compileString($this->getNode('body'));
+    $plural = NULL;
+
+    if (NULL !== $this->getNode('plural')) {
+      list($plural, $pluralTokens) = $this->compileString($this->getNode('plural'));
+      $tokens = array_merge($tokens, $pluralTokens);
+    }
+
+    // Start writing with the function to be called.
+    $compiler->write('echo ' . (empty($plural) ? 't' : 'format_plural') . '(');
+
+    // Move the count to the beginning of the parameters list.
+    if (!empty($plural)) {
+      $compiler->raw('abs(')->subcompile($this->getNode('count'))->raw('), ');
+    }
+
+    // Write the singular text parameter.
+    $compiler->subcompile($singular);
+
+    // Write the plural text parameter, if necessary.
+    if (!empty($plural)) {
+      $compiler->raw(', ')->subcompile($plural);
+    }
+
+    // Write any tokens found as an associative array parameter.
+    if (!empty($tokens)) {
+      $compiler->raw(', array(');
+      foreach ($tokens as $token) {
+        $compiler->string($token->getAttribute('placeholder'))->raw(' => ')->subcompile($token)->raw(', ');
+      }
+      $compiler->raw(')');
+    }
+
+    // Write function closure.
+    $compiler->raw(')');
+
+    // Append translation debug markup, if necessary.
+    if (settings()->get('twig_debug', FALSE)) {
+      $compiler->raw(" . '\n<!-- TRANSLATION: ");
+      $compiler->subcompile($singular);
+      if (!empty($plural)) {
+        $compiler->raw(', PLURAL: ')->subcompile($plural);
+      }
+      $compiler->raw(" -->\n'");
+    }
+
+    // End writing.
+    $compiler->raw(";\n");
+  }
+
+  /**
+   * Extracts the text and tokens for the "trans" tag.
+   *
+   * @param \Twig_NodeInterface $body
+   *   The node to compile.
+   *
+   * @return array
+   *   Returns an array containing the two following parameters:
+   *   - string $text
+   *       The extracted text.
+   *   - array $tokens
+   *       The extracted tokens as new \Twig_Node_Expression_Name instances.
+   */
+  protected function compileString(\Twig_NodeInterface $body) {
+    if ($body instanceof \Twig_Node_Expression_Name || $body instanceof \Twig_Node_Expression_Constant || $body instanceof \Twig_Node_Expression_TempName) {
+      return array($body, array());
+    }
+
+    $tokens = array();
+    if (count($body)) {
+      $text = '';
+
+      foreach ($body as $node) {
+        if (get_class($node) === 'Twig_Node' && $node->getNode(0) instanceof \Twig_Node_SetTemp) {
+          $node = $node->getNode(1);
+        }
+
+        if ($node instanceof \Twig_Node_Print) {
+          $n = $node->getNode('expr');
+          while ($n instanceof \Twig_Node_Expression_Filter) {
+            $n = $n->getNode('node');
+          }
+          $args = $n->getNode('arguments')->getNode(0);
+
+          // Detect if a token implements one of the filters reserved for
+          // modifying the prefix of a token. The default prefix used for
+          // translations is "@". This escapes the printed token and makes them
+          // safe for templates.
+          // @see TwigExtension::getFilters()
+          $argPrefix = '@';
+          while ($args instanceof \Twig_Node_Expression_Filter) {
+            switch ($args->getNode('filter')->getAttribute('value')) {
+              case 'passthrough':
+                $argPrefix = '!';
+                break;
+              case 'placeholder':
+                $argPrefix = '%';
+                break;
+            }
+            $args = $args->getNode('node');
+          }
+          if ($args instanceof \Twig_Node_Expression_GetAttr) {
+            $argName = $args->getNode('attribute')->getAttribute('value');
+            $expr = $n;
+          }
+          else {
+            $argName = $n->getAttribute('name');
+            if (!is_null($args)) {
+              $argName = $args->getAttribute('name');
+            }
+            $expr = new \Twig_Node_Expression_Name($argName, $n->getLine());
+          }
+          $placeholder = sprintf('%s%s', $argPrefix, $argName);
+          $text .= $placeholder;
+          $expr->setAttribute('placeholder', $placeholder);
+          $tokens[] = $expr;
+        }
+        else {
+          $text .= $node->getAttribute('data');
+        }
+      }
+    }
+    else {
+      $text = $body->getAttribute('data');
+    }
+
+    return array(new \Twig_Node(array(new \Twig_Node_Expression_Constant(trim($text), $body->getLine()))), $tokens);
+  }
+
+}
---
+++ core/lib/Drupal/Core/Template/TwigTransTokenParser.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Core\Template\TwigTransTokenParser.
+ *
+ * @see http://twig.sensiolabs.org/doc/extensions/i18n.html
+ * @see https://github.com/fabpot/Twig-extensions
+ */
+
+namespace Drupal\Core\Template;
+
+/**
+ * A class that defines the Twig 'trans' token parser for Drupal.
+ *
+ * The token parser converts a token stream created from template source
+ * code into an Abstract Syntax Tree (AST).  The AST will later be compiled
+ * into PHP code usable for runtime execution of the template.
+ *
+ * @see \Twig_TokenParser
+ */
+class TwigTransTokenParser extends \Twig_TokenParser {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function parse(\Twig_Token $token) {
+    $lineno = $token->getLine();
+    $stream = $this->parser->getStream();
+    $count = NULL;
+    $plural = NULL;
+
+    if (!$stream->test(\Twig_Token::BLOCK_END_TYPE)) {
+      $body = $this->parser->getExpressionParser()->parseExpression();
+    }
+    else {
+      $stream->expect(\Twig_Token::BLOCK_END_TYPE);
+      $body = $this->parser->subparse(array($this, 'decideForFork'));
+      if ('plural' === $stream->next()->getValue()) {
+        $count = $this->parser->getExpressionParser()->parseExpression();
+        $stream->expect(\Twig_Token::BLOCK_END_TYPE);
+        $plural = $this->parser->subparse(array($this, 'decideForEnd'), TRUE);
+      }
+    }
+
+    $stream->expect(\Twig_Token::BLOCK_END_TYPE);
+
+    $this->checkTransString($body, $lineno);
+
+    $node = new TwigNodeTrans($body, $plural, $count, $lineno, $this->getTag());
+
+    return $node;
+  }
+
+  /**
+   * Detect a 'plural' switch or the end of a 'trans' tag.
+   */
+  public function decideForFork($token) {
+    return $token->test(array('plural', 'endtrans'));
+  }
+
+  /**
+   * Detect the end of a 'trans' tag.
+   */
+  public function decideForEnd($token) {
+    return $token->test('endtrans');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTag() {
+    return 'trans';
+  }
+
+  /**
+   * Ensure that any nodes that are parsed are only of allowed types.
+   *
+   * @param \Twig_NodeInterface $body
+   *   The expression to check.
+   * @param integer $lineno
+   *   The source line.
+   *
+   * @throws \Twig_Error_Syntax
+   */
+  protected function checkTransString(\Twig_NodeInterface $body, $lineno) {
+    foreach ($body as $node) {
+      if (
+        $node instanceof \Twig_Node_Text
+        ||
+        ($node instanceof \Twig_Node_Print && $node->getNode('expr') instanceof \Twig_Node_Expression_Name)
+        ||
+        ($node instanceof \Twig_Node_Print && $node->getNode('expr') instanceof \Twig_Node_Expression_GetAttr)
+        ||
+        ($node instanceof \Twig_Node_Print && $node->getNode('expr') instanceof \Twig_Node_Expression_Filter)
+      ) {
+        continue;
+      }
+      throw new \Twig_Error_Syntax(sprintf('The text to be translated with "trans" can only contain references to simple variables'), $lineno);
+    }
+  }
+
+}
