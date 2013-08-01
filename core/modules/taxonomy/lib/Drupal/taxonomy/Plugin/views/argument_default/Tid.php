63d299d9d2a252874eef8e8b654085a757187ba6
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2013-08-01 13:46:59 +0100

    Issue #2051721 by yched: Get rid of field_get_items().

--- core/modules/taxonomy/lib/Drupal/taxonomy/Plugin/views/argument_default/Tid.php
+++ core/modules/taxonomy/lib/Drupal/taxonomy/Plugin/views/argument_default/Tid.php
@@ -139,8 +139,8 @@ class Tid extends ArgumentDefaultPluginBase {
         foreach ($fields as $name => $info) {
           $field_info = field_info_field($name);
           if ($field_info['type'] == 'taxonomy_term_reference') {
-            foreach (field_get_items($node, $name) as $item) {
-              $taxonomy[$item['tid']] = $field_info['settings']['allowed_values'][0]['vocabulary'];
+            foreach ($node->get($name) as $item) {
+              $taxonomy[$item->target_id] = $field_info['settings']['allowed_values'][0]['vocabulary'];
             }
           }
         }
