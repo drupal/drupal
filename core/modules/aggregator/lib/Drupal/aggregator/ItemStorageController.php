91bd01a85281a4fd2eb0b2defe8f9e961882c23e
Alex Pott <alex.a.pott@googlemail.com>
2013-08-03 13:15:55 +0200

    Issue #2043581 by larowlan, kim.pepper: Move aggregator_load_feed_items() to the ItemStorageController, retain procedural wrapper.

--- core/modules/aggregator/lib/Drupal/aggregator/ItemStorageController.php
+++ core/modules/aggregator/lib/Drupal/aggregator/ItemStorageController.php
@@ -7,9 +7,10 @@
 
 namespace Drupal\aggregator;
 
-use Drupal\Core\Entity\DatabaseStorageControllerNG;
 use Drupal\aggregator\Plugin\Core\Entity\Item;
-use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Database\Query\PagerSelectExtender;
+use Drupal\Core\Database\Query\SelectInterface;
+use Drupal\Core\Entity\DatabaseStorageControllerNG;
 
 /**
  * Controller class for aggregators items.
@@ -112,4 +113,61 @@ class ItemStorageController extends DatabaseStorageControllerNG implements ItemS
         ->execute();
     }
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadAll($limit = 20) {
+    $query = $this->database->select('aggregator_item', 'i');
+    $query->join('aggregator_feed', 'f', 'i.fid = f.fid');
+    $query->fields('i', array('iid'));
+    return $this->executeFeedItemQuery($query, $limit);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadByFeed($fid, $limit = 20) {
+    $query = $this->database->select('aggregator_item', 'i');
+    $query
+      ->fields('i', array('iid'))
+      ->condition('i.fid', $fid);
+    return $this->executeFeedItemQuery($query, $limit);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadByCategory($cid, $limit = 20) {
+    $query = $this->database->select('aggregator_category_item', 'c');
+    $query->leftJoin('aggregator_item', 'i', 'c.iid = i.iid');
+    $query->leftJoin('aggregator_feed', 'f', 'i.fid = f.fid');
+    $query
+      ->fields('i', array('iid'))
+      ->condition('cid', $cid);
+    return $this->executeFeedItemQuery($query, $limit);
+  }
+
+  /**
+   * Helper method to execute an item query.
+   *
+   * @param SelectInterface $query
+   *   The query to execute.
+   * @param int $limit
+   *   (optional) The number of items to return. Defaults to 20.
+   *
+   * @return \Drupal\aggregator\ItemInterface[]
+   *   An array of the feed items.
+   */
+  protected function executeFeedItemQuery(SelectInterface $query, $limit) {
+    $result = $query
+      ->extend('Drupal\Core\Database\Query\PagerSelectExtender')
+      ->limit($limit)
+      ->orderBy('i.timestamp', 'DESC')
+      ->orderBy('i.iid', 'DESC')
+      ->execute();
+
+    return $this->loadMultiple($result->fetchCol());
+  }
+
 }
