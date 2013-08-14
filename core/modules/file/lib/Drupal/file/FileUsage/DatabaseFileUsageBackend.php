858674f0d1c4205a181686f82202eb03aa01835d
Alex Pott <alex.a.pott@googlemail.com>
2013-08-14 07:54:05 -0500

    Issue #2060405 by Wim Leers: Fixed Impossible to uninstall any module using the file usage service, because it is impossible to delete all file usages by a module.

--- core/modules/file/lib/Drupal/file/FileUsage/DatabaseFileUsageBackend.php
+++ core/modules/file/lib/Drupal/file/FileUsage/DatabaseFileUsageBackend.php
@@ -46,7 +46,7 @@ class DatabaseFileUsageBackend extends FileUsageBase {
   }
 
   /**
-   * Implements Drupal\file\FileUsage\FileUsageInterface::add().
+   * {@inheritdoc}
    */
   public function add(File $file, $module, $type, $id, $count = 1) {
     $this->connection->merge($this->tableName)
@@ -64,7 +64,7 @@ class DatabaseFileUsageBackend extends FileUsageBase {
   }
 
   /**
-   * Implements Drupal\file\FileUsage\FileUsageInterface::delete().
+   * {@inheritdoc}
    */
   public function delete(File $file, $module, $type = NULL, $id = NULL, $count = 1) {
     // Delete rows that have a exact or less value to prevent empty rows.
@@ -99,7 +99,7 @@ class DatabaseFileUsageBackend extends FileUsageBase {
   }
 
   /**
-   * Implements Drupal\file\FileUsage\FileUsageInterface::listUsage().
+   * {@inheritdoc}
    */
   public function listUsage(File $file) {
     $result = $this->connection->select($this->tableName, 'f')
@@ -113,4 +113,14 @@ class DatabaseFileUsageBackend extends FileUsageBase {
     }
     return $references;
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function deleteByModule($module) {
+    $this->connection->delete($this->tableName)
+      ->condition('module', $module)
+      ->execute();
+  }
+
 }
