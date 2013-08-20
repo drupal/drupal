3e8393446c4f208802cebdf442df4471cb6e314e
Alex Pott <alex.a.pott@googlemail.com>
2013-08-21 00:05:40 +0100

    Issue #2049709 by plopesc: TranslationManager::translate() should be on an interface.

--- core/lib/Drupal/Core/StringTranslation/TranslationManager.php
+++ core/lib/Drupal/Core/StringTranslation/TranslationManager.php
@@ -13,7 +13,7 @@ use Drupal\Component\Utility\String;
 /**
  * Defines a chained translation implementation combining multiple translators.
  */
-class TranslationManager implements TranslatorInterface {
+class TranslationManager implements TranslationInterface, TranslatorInterface {
 
   /**
    * An array of active translators keyed by priority.
@@ -105,25 +105,7 @@ class TranslationManager implements TranslatorInterface {
   }
 
   /**
-   * Translates a string to the current language or to a given language.
-   *
-   * @param string $string
-   *   A string containing the English string to translate.
-   * @param array $args
-   *   An associative array of replacements to make after translation. Based
-   *   on the first character of the key, the value is escaped and/or themed.
-   *   See \Drupal\Core\Utility\String::format() for details.
-   * @param array $options
-   *   An associative array of additional options, with the following elements:
-   *   - 'langcode': The language code to translate to a language other than
-   *      what is used to display the page.
-   *   - 'context': The context the source string belongs to.
-   *
-   * @return string
-   *   The translated string.
-   *
-   * @see t()
-   * @see \Drupal\Core\Utility\String::format()
+   * {@inheritdoc}
    */
   public function translate($string, array $args = array(), array $options = array()) {
     // Merge in defaults.
