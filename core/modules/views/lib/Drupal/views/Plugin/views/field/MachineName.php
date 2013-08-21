de9f13c1b81f3e8c0143a7422a5a4c4bea2896c3
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2013-08-21 06:50:33 +0100

    Issue #2062131 by danylevskyi, NonProfit: In MachineName Class Rename Views properties to core standards.

--- core/modules/views/lib/Drupal/views/Plugin/views/field/MachineName.php
+++ core/modules/views/lib/Drupal/views/Plugin/views/field/MachineName.php
@@ -25,23 +25,23 @@ class MachineName extends FieldPluginBase {
   /**
    * @var array Stores the available options.
    */
-  var $value_options;
+  protected $valueOptions;
 
   public function getValueOptions() {
-    if (isset($this->value_options)) {
+    if (isset($this->valueOptions)) {
       return;
     }
 
     if (isset($this->definition['options callback']) && is_callable($this->definition['options callback'])) {
       if (isset($this->definition['options arguments']) && is_array($this->definition['options arguments'])) {
-        $this->value_options = call_user_func_array($this->definition['options callback'], $this->definition['options arguments']);
+        $this->valueOptions = call_user_func_array($this->definition['options callback'], $this->definition['options arguments']);
       }
       else {
-        $this->value_options = call_user_func($this->definition['options callback']);
+        $this->valueOptions = call_user_func($this->definition['options callback']);
       }
     }
     else {
-      $this->value_options = array();
+      $this->valueOptions = array();
     }
   }
 
@@ -72,11 +72,11 @@ class MachineName extends FieldPluginBase {
    */
   public function render(ResultRow $values) {
     $value = $values->{$this->field_alias};
-    if (!empty($this->options['machine_name']) || !isset($this->value_options[$value])) {
+    if (!empty($this->options['machine_name']) || !isset($this->valueOptions[$value])) {
       $result = check_plain($value);
     }
     else {
-      $result = $this->value_options[$value];
+      $result = $this->valueOptions[$value];
     }
 
     return $result;
