350c73f1eb9b5c9ea7d9d4694016c1ec822ed5fa
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2013-08-21 08:07:55 +0100

    Issue #1987884 by dawehner: Convert update_callback_service_unavailable() to a new style controller.

---
+++ core/modules/update/tests/modules/update_test/lib/Drupal/update_test/Controller/UpdateTestController.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\update_test\Controller\UpdateTestController.
+ */
+
+namespace Drupal\update_test\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Provides different routes of the update_test module.
+ */
+class UpdateTestController {
+
+
+  /**
+   * Displays an Error 503 (Service unavailable) page.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   Returns the response with a special header.
+   */
+  public function updateError() {
+    $response = new Response();
+    $response->setStatusCode(503);
+    $response->headers->set('Status', '503 Service unavailable');
+
+    return $response;
+  }
+
+}
