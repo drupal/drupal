9c8dc9395585b61596ffb04e5416fcd496a3db6e
Alex Pott <alex.a.pott@googlemail.com>
2013-08-27 01:42:23 +0100

    Issue #2071485 by tim.plunkett: Modernize action.module forms.

--- core/modules/action/lib/Drupal/action/ActionFormControllerBase.php
+++ core/modules/action/lib/Drupal/action/ActionFormControllerBase.php
@@ -64,19 +64,19 @@ abstract class ActionFormControllerBase extends EntityFormController {
   public function form(array $form, array &$form_state) {
     $form['label'] = array(
       '#type' => 'textfield',
-      '#title' => t('Label'),
+      '#title' => $this->t('Label'),
       '#default_value' => $this->entity->label(),
       '#maxlength' => '255',
-      '#description' => t('A unique label for this advanced action. This label will be displayed in the interface of modules that integrate with actions.'),
+      '#description' => $this->t('A unique label for this advanced action. This label will be displayed in the interface of modules that integrate with actions.'),
     );
 
     $form['id'] = array(
       '#type' => 'machine_name',
-      '#title' => t('Machine name'),
+      '#title' => $this->t('Machine name'),
       '#default_value' => $this->entity->id(),
       '#disabled' => !$this->entity->isNew(),
       '#maxlength' => 64,
-      '#description' => t('A unique name for this action. It must only contain lowercase letters, numbers and underscores.'),
+      '#description' => $this->t('A unique name for this action. It must only contain lowercase letters, numbers and underscores.'),
       '#machine_name' => array(
         'exists' => array($this, 'exists'),
       ),
@@ -148,7 +148,7 @@ abstract class ActionFormControllerBase extends EntityFormController {
    */
   public function save(array $form, array &$form_state) {
     $this->entity->save();
-    drupal_set_message(t('The action has been successfully saved.'));
+    drupal_set_message($this->t('The action has been successfully saved.'));
 
     $form_state['redirect'] = 'admin/config/system/actions';
   }
