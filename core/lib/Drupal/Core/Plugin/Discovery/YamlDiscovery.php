182ba50b8a9374ba9fb60395824e749260f41557
Alex Pott <alex.a.pott@googlemail.com>
2013-08-29 02:48:27 +0100

    Issue #2065571 by pwolanin, damiankloip, neclimdul: Add YAML Plugin discovery.

---
+++ core/lib/Drupal/Core/Plugin/Discovery/YamlDiscovery.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Core\Plugin\Discovery\YamlDiscovery.
+ */
+
+namespace Drupal\Core\Plugin\Discovery;
+
+use Drupal\Component\Plugin\Discovery\DiscoveryInterface;
+use Drupal\Component\Discovery\YamlDiscovery as ComponentYamlDiscovery;
+
+/**
+ * Allows YAML files to define plugin definitions.
+ */
+class YamlDiscovery implements DiscoveryInterface {
+
+  /**
+   * YAML file discovery and parsing handler.
+   *
+   * @var \Drupal\Component\Discovery\YamlDiscovery
+   */
+  protected $discovery;
+
+  /**
+   * Construct a YamlDiscovery object.
+   *
+   * @param string $name
+   *   The file name suffix to use for discovery. E.g. 'test' will become
+   *   'MODULE.test.yml'.
+   * @param array $directories
+   *   An array of directories to scan.
+   */
+  function __construct($name, array $directories) {
+    $this->discovery = new ComponentYamlDiscovery($name, $directories);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefinition($plugin_id) {
+    $definitions = $this->getDefinitions();
+    return isset($definitions[$plugin_id]) ? $definitions[$plugin_id] : NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefinitions() {
+    $plugins = $this->discovery->findAll();
+
+    // Flatten definitions into what's expected from plugins.
+    $definitions = array();
+    foreach ($plugins as $provider => $list) {
+      foreach ($list as $id => $definition) {
+        $definitions[$id] = $definition + array(
+          'provider' => $provider,
+          'id' => $id,
+        );
+      }
+    }
+
+    return $definitions;
+  }
+}
