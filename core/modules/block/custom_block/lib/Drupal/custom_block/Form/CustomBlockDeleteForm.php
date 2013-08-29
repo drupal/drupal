23fba8070eb9e024005900352e728e0e20e423e6
Alex Pott <alex.a.pott@googlemail.com>
2013-08-29 02:34:22 +0100

    Issue #1997262 by andypost, nick_schuch, larowlan, tim.plunkett, jibran, chertzog, kim.pepper: Convert custom_block_delete_form() to new form interface.

---
+++ core/modules/block/custom_block/lib/Drupal/custom_block/Form/CustomBlockDeleteForm.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\block\Form\CustomBlockDeleteForm.
+ */
+
+namespace Drupal\custom_block\Form;
+
+use Drupal\Core\Entity\EntityNGConfirmFormBase;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Provides a confirmation form for deleting a custom block entity.
+ */
+class CustomBlockDeleteForm extends EntityNGConfirmFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete %name?', array('%name' => $this->entity->label()));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCancelPath() {
+    return 'admin/structure/block';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state, Request $request = NULL) {
+    $form = parent::buildForm($form, $form_state, $request);
+    $instances = $this->entity->getInstances();
+
+    $form['message'] = array(
+      '#type' => 'markup',
+      '#markup' => format_plural(count($instances), 'This will also remove 1 placed block instance.', 'This will also remove @count placed block instances.'),
+      '#access' => !empty($instances),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submit(array $form, array &$form_state) {
+    $this->entity->delete();
+    drupal_set_message($this->t('Custom block %label has been deleted.', array('%label' => $this->entity->label())));
+    watchdog('custom_block', 'Custom block %label has been deleted.', array('%label' => $this->entity->label()), WATCHDOG_NOTICE);
+    $form_state['redirect'] = 'admin/structure/custom-blocks';
+  }
+
+}
