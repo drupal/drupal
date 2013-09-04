970fdb9e479f15291a9b54ea429c68e1b621607a
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2013-09-04 12:09:19 +0100

    Issue #2032453 by alexpott, tsphethean: Fixed WebTestBase::randomString returning a string containing a $ followed by a number causes assertLink() to fail it's xpath match.

--- core/tests/Drupal/Tests/UnitTestCase.php
+++ core/tests/Drupal/Tests/UnitTestCase.php
@@ -15,6 +15,13 @@ use Drupal\Component\Utility\Random;
 abstract class UnitTestCase extends \PHPUnit_Framework_TestCase {
 
   /**
+   * The random generator.
+   *
+   * @var \Drupal\Component\Utility\Random
+   */
+  protected $randomGenerator;
+
+  /**
    * This method exists to support the simpletest UI runner.
    *
    * It should eventually be replaced with something native to phpunit.
@@ -43,11 +50,25 @@ abstract class UnitTestCase extends \PHPUnit_Framework_TestCase {
    * @return string
    *   Randomly generated unique string.
    *
-   * @see \Drupal\Component\Utility::string()
+   * @see \Drupal\Component\Utility\Random::name()
    */
   public function randomName($length = 8) {
-    return Random::name($length, TRUE);
+    return $this->getRandomGenerator()->name($length, TRUE);
   }
+
+  /**
+   * Gets the random generator for the utility methods.
+   *
+   * @return \Drupal\Component\Utility\Random
+   *   The random generator
+   */
+  protected function getRandomGenerator() {
+    if (!is_object($this->randomGenerator)) {
+      $this->randomGenerator = new Random();
+    }
+    return $this->randomGenerator;
+  }
+
 
   /**
    * Returns a stub config factory that behaves according to the passed in array.
