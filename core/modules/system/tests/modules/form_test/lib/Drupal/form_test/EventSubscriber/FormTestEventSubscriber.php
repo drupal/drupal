1a94d949b786f5dbd7b0b13699d65f44321d46d2
webchick <webchick@24967.no-reply.drupal.org>
2013-09-12 23:33:45 -0700

    Issue #1987718 by brianV, larowlan: Convert form_test_two_instances() to a new style controller.

--- core/modules/system/tests/modules/form_test/lib/Drupal/form_test/EventSubscriber/FormTestEventSubscriber.php
+++ core/modules/system/tests/modules/form_test/lib/Drupal/form_test/EventSubscriber/FormTestEventSubscriber.php
@@ -7,6 +7,10 @@
 
 namespace Drupal\form_test\EventSubscriber;
 
+use Drupal\Core\Routing\RouteBuildEvent;
+use Drupal\Core\Routing\RoutingEvents;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Symfony\Component\Routing\Route;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -15,6 +19,20 @@ use Symfony\Component\HttpKernel\KernelEvents;
  * Test event subscriber to add new attributes to the request.
  */
 class FormTestEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Constructs a FormTestController object.
+   */
+  public function __construct(ModuleHandlerInterface $moduleHandler) {
+    $this->moduleHandler = $moduleHandler;
+  }
 
   /**
    * Adds custom attributes to the request object.
@@ -33,8 +51,26 @@ class FormTestEventSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents() {
     $events[KernelEvents::REQUEST][] = array('onKernelRequest');
+    $events[RoutingEvents::DYNAMIC] = 'routes';
 
     return $events;
+  }
+
+  /**
+   * Adds routes for the Form Tests.
+   */
+  public function routes(RouteBuildEvent $event) {
+    $collection = $event->getRouteCollection();
+    $defaults = array();
+
+    if ($this->moduleHandler->moduleExists('node')) {
+      $route = new Route(
+        "form-test/two-instances-of-same-form",
+        array('_content' => '\Drupal\form_test\Controller\FormTestController::twoFormInstances'),
+        array('_permission' => 'create page content')
+      );
+      $collection->add("form_test.two_instances", $route);
+    }
   }
 
 }
