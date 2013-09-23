f384cbdcef625ce3c8bb1f4eb27ea9d15806ac9f
webchick <webchick@24967.no-reply.drupal.org>
2013-09-23 17:21:40 +0200

    Issue #2087327 by damiankloip: Add text search functionality to views listing page.

---
+++ core/modules/views_ui/js/views_ui.listing.js
@@ -0,0 +1,44 @@
+(function ($, Drupal) {
+
+  "use strict";
+
+  /**
+   * Filters the view listing tables by a text input search string.
+   *
+   * Text search input: input.views-filter-text
+   * Target table:      input.views-filter-text[data-table]
+   * Source text:       .views-table-filter-text-source
+   */
+  Drupal.behaviors.viewTableFilterByText = {
+    attach: function (context, settings) {
+      var $input = $('input.views-filter-text').once('views-filter-text');
+      var $table = $($input.attr('data-table'));
+      var $rows;
+
+      function filterViewList (e) {
+        var query = $(e.target).val().toLowerCase();
+
+        function showViewRow (index, row) {
+          var $row = $(row);
+          var $sources = $row.find('.views-table-filter-text-source');
+          var textMatch = $sources.text().toLowerCase().indexOf(query) !== -1;
+          $row.closest('tr').toggle(textMatch);
+        }
+
+        // Filter if the length of the query is at least 2 characters.
+        if (query.length >= 2) {
+          $rows.each(showViewRow);
+        }
+        else {
+          $rows.show();
+        }
+      }
+
+      if ($table.length) {
+        $rows = $table.find('tbody tr');
+        $input.on('keyup', filterViewList);
+      }
+    }
+  };
+
+}(jQuery, Drupal));
