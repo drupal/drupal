bd0b82f08369856c083961f20f4ca051e479233b
Lee Rowlands <lee.rowlands@previousnext.com.au>
2013-09-24 15:47:59 +1000

    Remove field_info_instance from CommentUpgradePathTest

--- core/modules/system/lib/Drupal/system/Tests/Upgrade/CommentUpgradePathTest.php
+++ core/modules/system/lib/Drupal/system/Tests/Upgrade/CommentUpgradePathTest.php
@@ -7,6 +7,8 @@
 
 namespace Drupal\system\Tests\Upgrade;
 
+use Drupal\field\Field;
+
 /**
  * Tests upgrading a filled database with comment data.
  *
@@ -102,7 +104,7 @@ class CommentUpgradePathTest extends UpgradePathTestBase {
     sort($types);
     $this->assertIdentical(array_keys($expected_settings), $types, 'All node types are upgraded');
     foreach ($types as $type) {
-      $instance = field_info_instance('node', 'comment_' . $type, $type);
+      $instance = Field::fieldInfo()->getInstance($node, $type, 'comment_'. $type);
       $this->assertTrue($instance, format_string('Comment field found for the %type node type', array(
         '%type' => $type
       )));
