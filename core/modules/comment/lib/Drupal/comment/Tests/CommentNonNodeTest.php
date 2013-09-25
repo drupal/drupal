a4a6317aed6685ac9c6e22a898cc7f7995baf717
Lee Rowlands <lee.rowlands@previousnext.com.au>
2013-09-26 07:24:29 +1000

    Moves tests to use entity_test_render

--- core/modules/comment/lib/Drupal/comment/Tests/CommentNonNodeTest.php
+++ core/modules/comment/lib/Drupal/comment/Tests/CommentNonNodeTest.php
@@ -2,27 +2,21 @@
 
 /**
  * @file
- * Contains \Drupal\comment\Tests\CommentUserTest.
+ * Contains \Drupal\comment\Tests\CommentNonNodeTest.
  */
 
 namespace Drupal\comment\Tests;
 
-use Drupal\Core\Language\Language;
 use Drupal\comment\CommentInterface;
 use Drupal\simpletest\WebTestBase;
-use Drupal\user\UserInterface;
+use Drupal\Core\Entity\EntityInterface;
 
 /**
- * Tests basic comment functionality against a user entity.
+ * Tests basic comment functionality against the entity_test_render entity type.
  */
-class CommentUserTest extends WebTestBase {
+class CommentNonNodeTest extends WebTestBase {
 
-  /**
-   * Modules to enable.
-   *
-   * @var array
-   */
-  public static $modules = array('comment', 'user', 'field_ui');
+  public static $modules = array('comment', 'user', 'field_ui', 'entity_test');
 
   /**
    * An administrative user with permission to configure comment settings.
@@ -31,20 +25,10 @@ class CommentUserTest extends WebTestBase {
    */
   protected $admin_user;
 
-  /**
-   * A normal user with permission to post comments.
-   *
-   * @var \Drupal\user\UserInterface
-   */
-  protected $web_user;
-
-  /**
-   * Provides test information.
-   */
   public static function getInfo() {
     return array(
-      'name' => 'Comment user tests',
-      'description' => 'Test commenting on users.',
+      'name' => 'Comment non-node tests',
+      'description' => 'Test commenting on a test entity.',
       'group' => 'Comment',
     );
   }
@@ -52,27 +36,19 @@ class CommentUserTest extends WebTestBase {
   function setUp() {
     parent::setUp();
 
-    // Create comment field on user bundle.
-    $this->container->get('comment.manager')->addDefaultField('user', 'user');
+    // Create a bundle for entity_test_render.
+    entity_test_create_bundle('entity_test_render', 'Entity Test Render', 'entity_test_render');
+    // Create comment field on entity_test_render bundle.
+    $this->container->get('comment.manager')->addDefaultField('entity_test_render', 'entity_test_render');
 
-    // Create two test users.
+    // Create test user.
     $this->admin_user = $this->drupalCreateUser(array(
       'administer comments',
       'skip comment approval',
       'post comments',
       'access comments',
-      'access content',
-      'administer users',
-      'access user profiles',
-    ));
-    $this->web_user = $this->drupalCreateUser(array(
-      'access comments',
-      'post comments',
-      'edit own comments',
-      'post comments',
-      'skip comment approval',
-      'access content',
-      'access user profiles',
+      'view test entity',
+      'administer entity_test content',
     ));
 
     // Enable anonymous and authenticated user comments.
@@ -86,13 +62,19 @@ class CommentUserTest extends WebTestBase {
       'post comments',
       'skip comment approval',
     ));
+
+    // Create a test entity.
+    $random_label = $this->randomName();
+    $data = array('type' => 'entity_test_render', 'name' => $random_label);
+    $this->entity = entity_create('entity_test_render', $data);
+    $this->entity->save();
   }
 
   /**
    * Posts a comment.
    *
-   * @param \Drupal\user\UserInterface|null $account
-   *   User to post comment on or NULL to post to the previously loaded page.
+   * @param \Drupal\Core\Entity\EntityInterface|null $entity
+   *   Entity to post comment on or NULL to post to the previously loaded page.
    * @param $comment
    *   Comment body.
    * @param $subject
@@ -101,17 +83,17 @@ class CommentUserTest extends WebTestBase {
    *   Set to NULL for no contact info, TRUE to ignore success checking, and
    *   array of values to set contact info.
    */
-  function postComment(UserInterface $account, $comment, $subject = '', $contact = NULL) {
+  function postComment(EntityInterface $entity, $comment, $subject = '', $contact = NULL) {
     $edit = array();
     $edit['comment_body[0][value]'] = $comment;
 
-    $instance = $this->container->get('field.info')->getInstance('user', 'user', 'comment');
+    $instance = $this->container->get('field.info')->getInstance('entity_test_render', 'entity_test_render', 'comment');
     $preview_mode = $instance['settings']['preview'];
     $subject_mode = $instance['settings']['subject'];
 
     // Must get the page before we test for fields.
-    if ($account !== NULL) {
-      $this->drupalGet('comment/reply/user/' . $account->id() . '/comment');
+    if ($entity !== NULL) {
+      $this->drupalGet('comment/reply/entity_test_render/' . $entity->id() . '/comment');
     }
 
     if ($subject_mode == TRUE) {
@@ -153,7 +135,7 @@ class CommentUserTest extends WebTestBase {
         $this->assertText($subject, 'Comment subject posted.');
       }
       $this->assertText($comment, 'Comment body posted.');
-      $this->assertTrue((!empty($match) && !empty($match[1])), 'Comment id found.');
+      $this->assertTrue((!empty($match) && !empty($match[1])), 'Comment ID found.');
     }
 
     if (isset($match[1])) {
@@ -185,17 +167,6 @@ class CommentUserTest extends WebTestBase {
     else {
       return FALSE;
     }
-  }
-
-  /**
-   * Deletes a comment.
-   *
-   * @param \Drupal\comment\CommentInterface $comment
-   *   Comment to delete.
-   */
-  function deleteComment(CommentInterface $comment) {
-    $this->drupalPostForm('comment/' . $comment->id() . '/delete', array(), t('Delete'));
-    $this->assertText(t('The comment and all its replies have been deleted.'), 'Comment deleted.');
   }
 
   /**
@@ -240,7 +211,7 @@ class CommentUserTest extends WebTestBase {
    *   Comment subject to find.
    *
    * @return integer
-   *   Comment id.
+   *   Comment ID.
    */
   function getUnapprovedComment($subject) {
     $this->drupalGet('admin/content/comment/approval');
@@ -252,83 +223,78 @@ class CommentUserTest extends WebTestBase {
   /**
    * Tests anonymous comment functionality.
    */
-  function testCommentUser() {
+  function testCommentFunctionality() {
     $limited_user = $this->drupalCreateUser(array(
-      'administer user fields'
+      'administer entity_test_render fields'
     ));
     $this->drupalLogin($limited_user);
     // Test that default field exists.
-    $this->drupalGet('admin/config/people/accounts/fields');
+    $this->drupalGet('admin/structure/entity-test-render/manage/entity_test_render/fields');
     $this->assertText(t('Comment settings'));
-    $this->assertLinkByHref('admin/config/people/accounts/fields/user.user.comment');
+    $this->assertLinkByHref('admin/structure/entity-test-render/manage/entity_test_render/fields/entity_test_render.entity_test_render.comment');
     // Test widget hidden option is not visible when there's no comments.
-    $this->drupalGet('admin/config/people/accounts/fields/user.user.comment');
+    $this->drupalGet('admin/structure/entity-test-render/manage/entity_test_render/entity-test-render/fields/entity_test_render.entity_test_render.comment');
     $this->assertNoField('edit-default-value-input-comment-und-0-status-0');
 
     $this->drupalLogin($this->admin_user);
 
     // Post a comment.
-    $comment1 = $this->postComment($this->web_user, $this->randomName(), $this->randomName());
-    $this->assertTrue($this->commentExists($comment1), 'Comment on web user exists.');
+    $comment1 = $this->postComment($this->entity, $this->randomName(), $this->randomName());
+    $this->assertTrue($this->commentExists($comment1), 'Comment on test entity exists.');
 
     // Assert the breadcrumb is valid.
-    $this->drupalGet('comment/reply/user/' . $this->web_user->id() . '/comment');
-    $this->assertLink($this->web_user->label());
+    $this->drupalGet('comment/reply/entity_test_render/' . $this->entity->id() . '/comment');
+    $this->assertLink($this->entity->label());
 
-    // Unpublish comment.
+    // Unpublish the comment.
     $this->performCommentOperation($comment1, 'unpublish');
-
     $this->drupalGet('admin/content/comment/approval');
     $this->assertRaw('comments[' . $comment1->id() . ']', 'Comment was unpublished.');
 
-    // Publish comment.
+    // Publish the comment.
     $this->performCommentOperation($comment1, 'publish', TRUE);
-
     $this->drupalGet('admin/content/comment');
     $this->assertRaw('comments[' . $comment1->id() . ']', 'Comment was published.');
 
-    // Delete comment.
+    // Delete the comment.
     $this->performCommentOperation($comment1, 'delete');
-
     $this->drupalGet('admin/content/comment');
     $this->assertNoRaw('comments[' . $comment1->id() . ']', 'Comment was deleted.');
 
     // Post another comment.
-    $comment1 = $this->postComment($this->web_user, $this->randomName(), $this->randomName());
-    $this->assertTrue($this->commentExists($comment1), 'Comment on web user exists.');
+    $comment1 = $this->postComment($this->entity, $this->randomName(), $this->randomName());
+    $this->assertTrue($this->commentExists($comment1), 'Comment on test entity exists.');
 
-    // Check comment was found.
+    // Check that the comment was found.
     $this->drupalGet('admin/content/comment');
     $this->assertRaw('comments[' . $comment1->id() . ']', 'Comment was published.');
 
     // Check that entity access applies to administrative page.
-    $this->assertText($this->web_user->label(), 'Name of commented account found.');
+    $this->assertText($this->entity->label(), 'Name of commented account found.');
     $limited_user = $this->drupalCreateUser(array(
       'administer comments',
     ));
     $this->drupalLogin($limited_user);
     $this->drupalGet('admin/content/comment');
-    $this->assertNoText($this->web_user->label(), 'No commented account name found.');
+    $this->assertNoText($this->entity->label(), 'No commented account name found.');
 
     $this->drupalLogout();
 
-    // Reset.
+    // Deny anonymous users access to comments.
     user_role_change_permissions(DRUPAL_ANONYMOUS_RID, array(
       'access comments' => FALSE,
       'post comments' => FALSE,
       'skip comment approval' => FALSE,
-      'access user profiles' => TRUE,
+      'view test entity' => TRUE,
     ));
 
     // Attempt to view comments while disallowed.
-    // NOTE: if authenticated user has permission to post comments, then a
-    // "Login or register to post comments" type link may be shown.
-    $this->drupalGet('user/' . $this->web_user->id());
+    $this->drupalGet('entity-test-render/' . $this->entity->id());
     $this->assertNoPattern('@<h2[^>]*>Comments</h2>@', 'Comments were not displayed.');
     $this->assertNoLink('Add new comment', 'Link to add comment was found.');
 
-    // Attempt to view user-comment form while disallowed.
-    $this->drupalGet('comment/reply/user/' . $this->web_user->id() . '/comment');
+    // Attempt to view test entity comment form while disallowed.
+    $this->drupalGet('comment/reply/entity_test_render/' . $this->entity->id() . '/comment');
     $this->assertText('You are not authorized to post comments', 'Error attempting to post comment.');
     $this->assertNoFieldByName('subject', '', 'Subject field not found.');
     $this->assertNoFieldByName('comment_body[0][value]', '', 'Comment field not found.');
@@ -336,16 +302,19 @@ class CommentUserTest extends WebTestBase {
     user_role_change_permissions(DRUPAL_ANONYMOUS_RID, array(
       'access comments' => TRUE,
       'post comments' => FALSE,
-      'access user profiles' => TRUE,
+      'view test entity' => TRUE,
       'skip comment approval' => FALSE,
     ));
-    // Ensure the render cache is flushed.
-    // @todo Fix render cache per user.
-    \Drupal::entityManager()->getRenderController('user')->resetCache();
-    $this->drupalGet('user/' . $this->web_user->id());
+    // Ensure the page cache is flushed.
+    \Drupal::cache('page')->deleteAll();
+    // We've changed role permissions, so need to reset render cache.
+    \Drupal::entityManager()->getRenderController('entity_test_render')->resetCache(array($this->entity->id()));
+    $this->drupalGet('entity-test-render/' . $this->entity->id());
     $this->assertPattern('@<h2[^>]*>Comments</h2>@', 'Comments were displayed.');
     $this->assertLink('Log in', 0, 'Link to log in was found.');
     $this->assertLink('register', 0, 'Link to register was found.');
+    $this->assertNoFieldByName('subject', '', 'Subject field not found.');
+    $this->assertNoFieldByName('comment_body[0][value]', '', 'Comment field not found.');
 
     // Test the combination of anonymous users being able to post, but not view
     // comments, to ensure that access to post comments doesn't grant access to
@@ -354,42 +323,42 @@ class CommentUserTest extends WebTestBase {
       'access comments' => FALSE,
       'post comments' => TRUE,
       'skip comment approval' => TRUE,
-      'access user profiles' => TRUE,
+      'view test entity' => TRUE,
     ));
-    // Ensure the render cache is flushed.
-    // @todo Fix render cache per user.
-    \Drupal::entityManager()->getRenderController('user')->resetCache();
-    $this->drupalGet('user/' . $this->web_user->id());
+    // We've changed role permissions, so need to reset render cache.
+    \Drupal::entityManager()->getRenderController('entity_test_render')->resetCache(array($this->entity->id()));
+    $this->drupalGet('entity-test-render/' . $this->entity->id());
     $this->assertNoPattern('@<h2[^>]*>Comments</h2>@', 'Comments were not displayed.');
     $this->assertFieldByName('subject', '', 'Subject field found.');
     $this->assertFieldByName('comment_body[0][value]', '', 'Comment field found.');
 
-    $this->drupalGet('comment/reply/user/' . $this->web_user->id() . '/comment/' . $comment1->id());
+    $this->drupalGet('comment/reply/entity_test_render/' . $this->entity->id() . '/comment/' . $comment1->id());
     $this->assertText('You are not authorized to view comments');
     $this->assertNoText($comment1->subject->value, 'Comment not displayed.');
 
     // Test comment field widget changes.
     $limited_user = $this->drupalCreateUser(array(
-      'administer user fields'
+      'administer entity_test_render fields',
+      'view test entity',
+      'administer entity_test content',
     ));
     $this->drupalLogin($limited_user);
-    $this->drupalGet('admin/config/people/accounts/fields/user.user.comment');
+    // @todo Remove when entity render cache supports roles.
+    \Drupal::entityManager()->getRenderController('entity_test_render')->resetCache(array($this->entity->id()));
+    $this->drupalGet('admin/structure/entity-test-render/manage/entity_test_render/fields/entity_test_render.entity_test_render.comment');
     $this->assertNoFieldChecked('edit-default-value-input-comment-0-status-0');
     $this->assertNoFieldChecked('edit-default-value-input-comment-0-status-1');
     $this->assertFieldChecked('edit-default-value-input-comment-0-status-2');
     // Test comment option change in field settings.
     $edit = array('default_value_input[comment][0][status]' => COMMENT_CLOSED);
     $this->drupalPostForm(NULL, $edit, t('Save settings'));
-    $this->drupalGet('admin/config/people/accounts/fields/user.user.comment');
+    $this->drupalGet('admin/structure/entity-test-render/manage/entity_test_render/fields/entity_test_render.entity_test_render.comment');
     $this->assertNoFieldChecked('edit-default-value-input-comment-0-status-0');
     $this->assertFieldChecked('edit-default-value-input-comment-0-status-1');
     $this->assertNoFieldChecked('edit-default-value-input-comment-0-status-2');
-    $this->drupalGet('user/' . $this->web_user->id());
-    $this->assertNoPattern('@<h2[^>]*>Comments</h2>@', 'Comments were not displayed.');
-    $this->assertNoLink('Add new comment', 'Link to add comment was found.');
-
-    // Add a new comment-field.
-    $this->drupalGet('admin/config/people/accounts/fields');
+    
+    // Add a new comment field.
+    $this->drupalGet('admin/structure/entity-test-render/manage/entity_test_render/fields');
     $edit = array(
       'fields[_add_new_field][label]' => 'Foobar',
       'fields[_add_new_field][field_name]' => 'foobar',
@@ -400,17 +369,17 @@ class CommentUserTest extends WebTestBase {
     $this->drupalPostForm(NULL, array(), t('Save settings'));
     $this->assertRaw(t('Saved %name configuration', array('%name' => 'Foobar')));
 
-    // Test the new user commenting inherits default.
-    $limited_user = $this->drupalCreateUser(array(
-      'access user profiles',
-    ));
-    $this->drupalLogin($limited_user);
-    $this->drupalGet('user/' . $limited_user->id() . '/edit');
-    $this->assertFieldChecked('edit-field-foobar-0-status-2');
-    $this->assertNoFieldChecked('edit-field-foobar-0-status-1');
+    // Test the new entity commenting inherits default.
+    $random_label = $this->randomName();
+    $data = array('bundle' => 'entity_test_render', 'name' => $random_label);
+    $new_entity = entity_create('entity_test_render', $data);
+    $new_entity->save();
+    $this->drupalGet('entity_test_render/manage/' . $new_entity->id() . '/edit');
+    $this->assertFieldChecked('edit-field-foobar-0-status-1');
+    $this->assertNoFieldChecked('edit-field-foobar-0-status-2');
     $this->assertNoField('edit-field-foobar-0-status-0');
 
-    $this->drupalGet('comment/reply/user/comment/' . $limited_user->id());
+    $this->drupalGet('comment/reply/entity_test_render/comment/' . $new_entity->id());
     $this->assertNoFieldByName('subject', '', 'Subject field found.');
     $this->assertNoFieldByName('comment_body[0][value]', '', 'Comment field found.');
   }
