227f21b3225f1e4f933a4e85b4e298d13d32c3cf
Alex Pott <alex.a.pott@googlemail.com>
2013-09-29 16:41:09 +0200

    Change KeyValueFactory to use Settings instead of $conf

--- core/lib/Drupal/Core/KeyValueStore/KeyValueExpirableFactory.php
+++ core/lib/Drupal/Core/KeyValueStore/KeyValueExpirableFactory.php
@@ -14,30 +14,11 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class KeyValueExpirableFactory extends KeyValueFactory {
 
-  /**
-   * Constructs a new expirable key/value store for a given collection name.
-   *
-   * @param string $collection
-   *   The name of the collection holding key and value pairs.
-   *
-   * @return \Drupal\Core\KeyValueStore\KeyValueStoreExpirableInterface
-   *   An expirable key/value store implementation for the given $collection.
-   */
-  public function get($collection) {
-    global $conf;
-    if (!isset($this->stores[$collection])) {
-      if (isset($conf['keyvalue_expirable_service_' . $collection])) {
-        $service_name = $conf['keyvalue_expirable_service_' . $collection];
-      }
-      elseif (isset($conf['keyvalue_expirable_default'])) {
-        $service_name = $conf['keyvalue_expirable_default'];
-      }
-      else {
-        $service_name = 'keyvalue.expirable.database';
-      }
-      $this->stores[$collection] = $this->container->get($service_name)->get($collection);
-    }
-    return $this->stores[$collection];
-  }
+  const DEFAULT_SERVICE = 'keyvalue.expirable.database';
+
+  const SPECIFIC_PREFIX = 'keyvalue_expirable_service_';
+
+  const DEFAULT_SETTING = 'keyvalue_expirable_default';
+
 }
 
