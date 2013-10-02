0dd4b14fbbbb6c50ba66b52e5d752c55d9f282e3
webchick <webchick@24967.no-reply.drupal.org>
2013-10-02 09:25:40 -0700

    Issue #2099261 by JulienD, mr.baileys, amateescu: Fixed field_help() no longer lists field_type()/widget modules and can throw undefined index notice.

---
+++ core/modules/field/lib/Drupal/field/Tests/FieldHelpTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\field\Tests\FieldHelpTest.
+ */
+
+namespace Drupal\field\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests help display for the Field module.
+ */
+class FieldHelpTest extends WebTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array.
+   */
+  public static $modules = array('field', 'help');
+
+  // Tests field help implementation without optional core modules enabled.
+  protected $profile = 'minimal';
+
+  /**
+   * The admin user that will be created.
+   */
+  protected $adminUser;
+
+  public static function getInfo() {
+    return array(
+      'name' => 'Field help functionality',
+      'description' => 'Verify help display for the Field module.',
+      'group' => 'Field',
+    );
+  }
+
+  public function setUp() {
+    parent::setUp();
+
+    // Create the admin user.
+    $this->adminUser = $this->drupalCreateUser(array('access administration pages', 'view the administration theme'));
+  }
+
+  /**
+   * Test the Field module's help page.
+   */
+  public function testFieldHelp() {
+    // Login the admin user.
+    $this->drupalLogin($this->adminUser);
+
+    // Visit the Help page and make sure no warnings or notices are thrown.
+    $this->drupalGet('admin/help/field');
+
+    // Enable the Options, Telephone and E-mail modules.
+    \Drupal::moduleHandler()->install(array('options', 'telephone', 'email'));
+    \Drupal::service('plugin.manager.field.widget')->clearCachedDefinitions();
+    \Drupal::service('plugin.manager.entity.field.field_type')->clearCachedDefinitions();
+
+    $this->drupalGet('admin/help/field');
+    $this->assertLink('Options', 0, 'Options module is listed on the Field help page.');
+    $this->assertLink('E-mail', 0, 'E-mail module is listed on the Field help page.');
+    $this->assertText('Telephone', 'Modules with field types that do not implement hook_help are listed.');
+    $this->assertNoLink('Telephone', 'Modules with field types that do not implement hook_help are not linked.');
+    $this->assertNoLink('Link', 'Modules that have not been installed, are not listed.');
+  }
+}
