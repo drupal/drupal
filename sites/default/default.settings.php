3970a0716018a908a8092c22a98f2c5d80910404
Jennifer Hodgdon <yahgrp@poplarware.com>
2013-10-09 08:29:56 -0700

    Issue #2049207 by LinL, Cottser: Fix reference to maintenance page tpl.php in default.settings.php

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -478,7 +478,8 @@ $settings['update_free_access'] = FALSE;
  * This applies when the site is explicitly set to maintenance mode through the
  * administration page or when the database is inactive due to an error.
  * The template file should also be copied into the theme. It is located inside
- * 'core/modules/system/maintenance-page.tpl.php'.
+ * 'core/modules/system/templates/maintenance-page.html.twig'.
+ *
  * Note: This setting does not apply to installation and update pages.
  */
 # $settings['maintenance_theme'] = 'bartik';
