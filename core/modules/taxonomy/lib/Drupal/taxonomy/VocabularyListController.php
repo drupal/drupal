c6318824f8e3d125b9d062b711e7896b79291a1c
Dries <dries@buytaert.net>
2013-11-05 21:01:12 -0500

    Issue #2104517 by Sree, swentel: Vocabulary list controller should not add the 'weight' key in buildHeader().

--- core/modules/taxonomy/lib/Drupal/taxonomy/VocabularyListController.php
+++ core/modules/taxonomy/lib/Drupal/taxonomy/VocabularyListController.php
@@ -61,7 +61,6 @@ class VocabularyListController extends DraggableListController {
    */
   public function buildHeader() {
     $header['label'] = t('Vocabulary name');
-    $header['weight'] = t('Weight');
     return $header + parent::buildHeader();
   }
 
