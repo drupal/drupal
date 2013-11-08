fcb206d7d34b378b239b714044afc94ca60e63f6
Lee Rowlands <lee.rowlands@previousnext.com.au>
2013-11-08 13:46:47 +1000

    Patch 46

--- core/modules/tour/lib/Drupal/tour/TourInterface.php
+++ core/modules/tour/lib/Drupal/tour/TourInterface.php
@@ -15,12 +15,25 @@ use Drupal\Core\Config\Entity\ConfigEntityInterface;
 interface TourInterface extends ConfigEntityInterface {
 
   /**
-   * The paths that this tour will appear on.
+   * The routes that this tour will appear on.
    *
    * @return array
-   *   Returns array of paths for the tour.
+   *   Returns array of routes for the tour.
    */
-  public function getPaths();
+  public function getRoutes();
+
+  /**
+   * Whether the tour matches a given set of route parameters.
+   *
+   * @param string $route_name
+   *   The route name the parameters are for.
+   * @param array $route_params
+   *   Associative array of raw route params.
+   *
+   * @return bool
+   *   TRUE if the tour matches the route parameters.
+   */
+  public function hasMatchingRoute($route_name, $route_params);
 
   /**
    * Returns tip plugin.
@@ -40,5 +53,10 @@ interface TourInterface extends ConfigEntityInterface {
    *   An array of tip plugins.
    */
   public function getTips();
+
+  /**
+   * Resets the statically cached keyed routes.
+   */
+  public function resetKeyedRoutes();
 
 }
