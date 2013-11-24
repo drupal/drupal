1ee5f8013e902f017a7f0a347136a69f8b7d625c
webchick <webchick@24967.no-reply.drupal.org>
2013-11-23 20:34:54 -0800

    Issue #2105609 by RoSk0, jhodgdon: Convert search_embedded_form_form to a Controller.

--- core/modules/search/tests/modules/search_embedded_form/lib/Drupal/search_embedded_form/Form/SearchEmbeddedForm.php
+++ core/modules/search/tests/modules/search_embedded_form/lib/Drupal/search_embedded_form/Form/SearchEmbeddedForm.php
@@ -7,16 +7,52 @@
 
 namespace Drupal\search_embedded_form\Form;
 
+use Drupal\Core\Form\FormBase;
+
 /**
- * Temporary form controller for search_embedded_form module.
+ * Form controller for search_embedded_form form.
  */
-class SearchEmbeddedForm {
+class SearchEmbeddedForm extends FormBase {
 
   /**
-   * @todo Remove search_embedded_form_form().
+   * {@inheritdoc}
    */
-  public function testEmbeddedForm() {
-    return drupal_get_form('search_embedded_form_form');
+  public function getFormID() {
+    return 'search_embedded_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state) {
+    $count = $this->config('search_embedded_form.settings')->get('submitted');
+
+    $form['name'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Your name'),
+      '#maxlength' => 255,
+      '#default_value' => '',
+      '#required' => TRUE,
+      '#description' => $this->t('Times form has been submitted: %count', array('%count' => $count)),
+    );
+
+    $form['actions'] = array('#type' => 'actions');
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Send away'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    $config = $this->config('search_embedded_form.settings');
+    $submit_count = (int) $config->get('submitted');
+    $config->set('submitted', $submit_count + 1)->save();
+    drupal_set_message($this->t('Test form was submitted'));
   }
 
 }
