d50b1695988ea7e37d7a985f0bd458fc383d219a
webchick <webchick@24967.no-reply.drupal.org>
2013-12-10 22:23:11 -0800

    Issue #2115025 by Gábor Hojtsy, pfrenssen, dawehner: Content admin views title saved localized to the menu table.

---
+++ core/modules/system/lib/Drupal/system/Tests/Menu/MenuRouterRebuildTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\system\Tests\Menu\MenuRouterRebuildTest.
+ */
+
+namespace Drupal\system\Tests\Menu;
+
+use Drupal\simpletest\WebTestBase;
+use Drupal\Core\Language\Language;
+
+/**
+ * Tests menu_router_rebuild().
+ */
+class MenuRouterRebuildTest extends WebTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('locale', 'menu_test');
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'Menu router rebuild',
+      'description' => 'Tests menu_router_rebuild().',
+      'group' => 'Menu',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  function setUp() {
+    parent::setUp();
+
+    $language = new Language(array('id' => 'nl'));
+    language_save($language);
+  }
+
+  /**
+   * Tests configuration context when rebuilding the menu router table.
+   */
+  public function testMenuRouterRebuildContext() {
+    // Enter a language context before rebuilding the menu router tables.
+    $language_context = config_context_enter('Drupal\Core\Config\Context\LanguageConfigContext');
+    $language_context->setLanguage(language_load('nl'));
+    menu_router_rebuild();
+
+    // Check that the language context was not used for building the menu item.
+    $menu_item = menu_get_item('menu-test/context');
+    $this->assertTrue($menu_item['title'] == 'English', 'Config context overrides are ignored when rebuilding menu router items.');
+  }
+
+}
