8c59a99310eb5e18a134e0ad08103a3a8686f9ca
webchick <webchick@24967.no-reply.drupal.org>
2013-12-10 22:55:16 -0800

    Issue #2062247 by m1r1k, Garbar: Remove calls to deprecated global  in core/modules/system/lib/Drupal/system/Tests/Entity/EntityTranslationTest.php.

--- core/modules/system/lib/Drupal/system/Tests/Entity/EntityTranslationTest.php
+++ core/modules/system/lib/Drupal/system/Tests/Entity/EntityTranslationTest.php
@@ -43,7 +43,7 @@ class EntityTranslationTest extends EntityLanguageTestBase {
   protected function _testEntityLanguageMethods($entity_type) {
     $entity = entity_create($entity_type, array(
       'name' => 'test',
-      'user_id' => $GLOBALS['user']->id(),
+      'user_id' => $this->container->get('current_user')->id(),
     ));
     $this->assertEqual($entity->language()->id, Language::LANGCODE_NOT_SPECIFIED, format_string('%entity_type: Entity language not specified.', array('%entity_type' => $entity_type)));
     $this->assertFalse($entity->getTranslationLanguages(FALSE), format_string('%entity_type: No translations are available', array('%entity_type' => $entity_type)));
