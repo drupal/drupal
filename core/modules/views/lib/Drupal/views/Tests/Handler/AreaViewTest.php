cfd1c3ae1def7d59d603aa150f353e2e1a37a507
Alex Pott <alex.a.pott@googlemail.com>
2013-12-26 14:36:17 +0000

    Issue #2149107 by longwave: "View" area handler in Views gives fatal error.

---
+++ core/modules/views/lib/Drupal/views/Tests/Handler/AreaViewTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\views\Tests\Handler\AreaViewTest.
+ */
+
+namespace Drupal\views\Tests\Handler;
+
+use Drupal\views\Tests\ViewUnitTestBase;
+use Drupal\views\Views;
+
+/**
+ * Tests the view area handler.
+ *
+ * @see \Drupal\views\Plugin\views\area\View
+ */
+class AreaViewTest extends ViewUnitTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('user');
+
+  /**
+   * Views used by this test.
+   *
+   * @var array
+   */
+  public static $testViews = array('test_area_view', 'test_simple_argument');
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'Area: View',
+      'description' => 'Tests the view area handler.',
+      'group' => 'Views Handlers',
+    );
+  }
+
+  /**
+   * Tests the view area handler.
+   */
+  public function testViewArea() {
+    $view = Views::getView('test_area_view');
+
+    $this->executeView($view);
+    $output = $view->render();
+    $output = drupal_render($output);
+    $this->assertTrue(strpos($output, 'view-test-simple-argument') !== FALSE, 'The test view is correctly embedded.');
+    $view->destroy();
+
+    $view->setArguments(array(27));
+    $this->executeView($view);
+    $output = $view->render();
+    $output = drupal_render($output);
+    $this->assertTrue(strpos($output, 'John') === FALSE, 'The test view is correctly embedded with inherited arguments.');
+    $this->assertTrue(strpos($output, 'George') !== FALSE, 'The test view is correctly embedded with inherited arguments.');
+    $view->destroy();
+  }
+
+}
