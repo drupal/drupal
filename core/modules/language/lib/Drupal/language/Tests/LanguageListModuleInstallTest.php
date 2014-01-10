634e3a3f65babed765aa1689c233ab54e259bc49
Dries <dries@buytaert.net>
2014-01-10 12:31:59 -0500

    Issue #2171015 by alexpott: Drupal 8 HEAD broken: installing Language module fails, after that cannot install any other module.

---
+++ core/modules/language/lib/Drupal/language/Tests/LanguageListModuleInstallTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\language\Tests\LanguageListModuleInstallTest.
+ */
+
+namespace Drupal\language\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Functional tests for the language list configuration forms.
+ */
+class LanguageListModuleInstallTest extends WebTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('language_test');
+
+  public static function getInfo() {
+    return array(
+      'name' => 'Language list during module install',
+      'description' => 'Tests enabling Language if a module exists that calls language_list during installation.',
+      'group' => 'Language',
+    );
+  }
+
+  /**
+   * Tests enabling Language.
+   */
+  function testModuleInstallLanguageList() {
+    // Since language_list uses static caches we need to do this be enabling
+    // the module using a the UI.
+    $admin_user = $this->drupalCreateUser(array('access administration pages', 'administer modules'));
+    $this->drupalLogin($admin_user);
+    $edit = array();
+    $edit['modules[Multilingual][language][enable]'] = 'language';
+    $this->drupalPostForm('admin/modules', $edit, t('Save configuration'));
+
+    $this->assertEqual(\Drupal::state()->get('language_test.language_count_preinstall', 0), 1, 'Using language_list() returns 1 language during Language installation.');
+
+    // Get updated module list by rebuilding container.
+    $this->rebuildContainer();
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('language'), 'Language module is enabled');
+  }
+}
