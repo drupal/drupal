2d0932354ef7994e2ae78d6080343ef2c144fc73
Jennifer Hodgdon <yahgrp@poplarware.com>
2014-01-10 08:29:39 -0800

    Issue #2169101 by penyaskito, chx: Improve docs in MigrateSqlSourceTestCase

--- core/modules/migrate/tests/Drupal/migrate/Tests/MigrateSqlSourceTestCase.php
+++ core/modules/migrate/tests/Drupal/migrate/Tests/MigrateSqlSourceTestCase.php
@@ -15,19 +15,52 @@ abstract class MigrateSqlSourceTestCase extends MigrateTestCase {
   /**
    * The tested source plugin.
    *
-   * @var \Drupal\migrate\Plugin\migrate\source\d6\Comment.
+   * @var \Drupal\migrate\Plugin\migrate\source\d6\Drupal6SqlBase.
    */
   protected $source;
 
+  /**
+   * The database contents.
+   *
+   * Database contents represents a mocked database. It should contain an
+   * associative array with the table name as key, and as many nested arrays as
+   * the number of mocked rows. Each of those faked rows must be another array
+   * with the column name as the key and the value as the cell.
+   *
+   * @var array
+   */
   protected $databaseContents = array();
 
+  /**
+   * The plugin class under test.
+   *
+   * The plugin system is not working during unit testing so the source plugin
+   * class needs to be manually specified.
+   *
+   * @var string
+   */
   const PLUGIN_CLASS = '';
 
+  /**
+   * The highwater mark at the beginning of the import operation.
+   *
+   * Once the migration is run, we save a mark of the migrated sources, so the
+   * migration can run again and update only new sources or changed sources.
+   *
+   * @var string
+   */
   const ORIGINAL_HIGHWATER = '';
 
+  /**
+   * Expected results after the source parsing.
+   *
+   * @var array
+   */
   protected $expectedResults = array();
 
   /**
+   * The source plugin instance under test.
+   *
    * @var \Drupal\migrate\Plugin\MigrateSourceInterface
    */
   protected $plugin;
