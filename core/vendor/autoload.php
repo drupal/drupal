56d3a638f449212c5dab792a8d20e43d3d391df1
webchick <webchick@24967.no-reply.drupal.org>
2014-01-16 20:26:13 -0800

    Issue #2175447 by tim.plunkett, dawehner: Update composer to latest version.

--- core/vendor/autoload.php
+++ core/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit50432d86c534be2ddddf85d27e021721::getLoader();
+return ComposerAutoloaderInitf089bd3ae5f9f883a3ebe89eb643eced::getLoader();
