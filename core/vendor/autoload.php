2a228e60a2e2786b6d5547850f6633e1314ea073
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2014-01-20 10:41:57 +0000

    Issue #2173719 by dawehner, tim.plunkett: Remove the fork of doctrine/common.

--- core/vendor/autoload.php
+++ core/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitf089bd3ae5f9f883a3ebe89eb643eced::getLoader();
+return ComposerAutoloaderInitc8a0ebe8345566b52e82e4437041f111::getLoader();
