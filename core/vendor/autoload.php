fd06a6c0b6ed965402e78df6cc8852f465f0cc5e
webchick <webchick@24967.no-reply.drupal.org>
2014-02-06 23:33:05 -0800

    Issue #2176265 by tim.plunkett, donquixote: Set an explicit ["config"]["autoloader-suffix"] in composer.json to avoid noise on composer update.

--- core/vendor/autoload.php
+++ core/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit92462a247bf83504e990c5f706f228cc::getLoader();
+return ComposerAutoloaderInitDrupal8::getLoader();
