784f08f519c76d1a2e75f07fcd06ad3fd4638b09
webchick <webchick@24967.no-reply.drupal.org>
2014-02-08 12:57:42 -0800

    Issue #1490402 by LewisNyman, nod_, mcjim, mdrummond, rteijeiro, jessebeach, Bojhan, ry5n: Redesign tabs and the content header.

---
+++ core/themes/seven/js/nav-tabs.js
@@ -0,0 +1,56 @@
+/**
+ * @file
+ * Responsive navigation tabs.
+ * Utilizes jquery.intrinsic.js to calculate if the tabs are larger than their
+ * container and toggles the 'is-horizontal' class.
+ *
+ * This also supports collapsible navigable is the 'is-collasible' class is
+ * added to the main element, and a target element is included.
+ */
+(function ($, Drupal) {
+
+  "use strict";
+
+  function init (i, tab) {
+    var $tab = $(tab);
+    var $target = $tab.find('[data-drupal-nav-tabs-target]');
+    var isCollapsible = $tab.hasClass('is-collapsible');
+
+    function openMenu (e) {
+      $target.toggleClass('is-open');
+    }
+
+    function handleResize (e) {
+      $tab.addClass('is-horizontal');
+      var isHorizontal = $tab.parent().width() > $tab.intrinsic('width');
+      $tab.toggleClass('is-horizontal', isHorizontal);
+      if(isCollapsible) {
+        $tab.toggleClass('is-collapse-enabled', !isHorizontal);
+      }
+      if (isHorizontal) {
+        $target.removeClass('is-open');
+      }
+    }
+
+    $tab.addClass('position-container is-horizontal-enabled');
+
+    $tab.on('click.tabs', '[data-drupal-nav-tabs-trigger]', openMenu);
+    $(window).on('resize.tabs', Drupal.debounce(handleResize)).trigger('resize.tabs');
+  }
+
+  /**
+   * Initialise the tabs JS.
+   */
+  Drupal.behaviors.navTabs = {
+    attach: function (context, settings) {
+      var $tabs = $(context).find('[data-drupal-nav-tabs]');
+      if ($tabs.length) {
+        var notSmartPhone = window.matchMedia('(min-width: 300px)');
+        if (notSmartPhone.matches) {
+          $tabs.once('nav-tabs').each(init);
+        }
+      }
+    }
+  };
+
+})(jQuery, Drupal);
