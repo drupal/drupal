3a5c3b322a8de80ee223c80b4948a96b73cc9e1d
Dries <dries@buytaert.net>
2014-02-27 20:14:53 +0100

    Issue #2152073 by dmouse, sun, Xano: Bump Drupal core's PHP requirement to 5.4.2.

--- core/install.php
+++ core/install.php
@@ -23,16 +23,8 @@ define('MAINTENANCE_MODE', 'install');
 // The minimum version is specified explicitly, as DRUPAL_MINIMUM_PHP is not
 // yet available. It is defined in bootstrap.inc, but it is not possible to
 // load that file yet as it would cause a fatal error on older versions of PHP.
-if (version_compare(PHP_VERSION, '5.3.10') < 0) {
-  print 'Your PHP installation is too old. Drupal requires at least PHP 5.3.10. See the <a href="http://drupal.org/requirements">system requirements</a> page for more information.';
-  exit;
-}
-
-// Exit early if the PHP option safe_mode is enabled to avoid fatal errors.
-// @todo Remove this check once we require PHP > 5.4 as safe mode is deprecated
-// in PHP 5.3 and completely removed in PHP 5.4.
-if (ini_get('safe_mode')) {
-  print 'Your PHP installation has safe_mode enabled. Drupal requires the safe_mode option to be turned off. See the <a href="http://drupal.org/requirements">system requirements</a> page for more information.';
+if (version_compare(PHP_VERSION, '5.4.2') < 0) {
+  print 'Your PHP installation is too old. Drupal requires at least PHP 5.4.2. See the <a href="http://drupal.org/requirements">system requirements</a> page for more information.';
   exit;
 }
 
