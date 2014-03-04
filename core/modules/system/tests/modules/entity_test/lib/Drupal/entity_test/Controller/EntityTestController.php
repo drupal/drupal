7a08f39b8079e1d86cfcc4c89181aa0c5e4ddb48
webchick <webchick@24967.no-reply.drupal.org>
2014-03-04 12:20:40 -0800

    Issue #1987694 by ianthomas_uk, disasm, rlmumford, vijaycs85: Convert entity_test callbacks to a new style controller.

--- core/modules/system/tests/modules/entity_test/lib/Drupal/entity_test/Controller/EntityTestController.php
+++ core/modules/system/tests/modules/entity_test/lib/Drupal/entity_test/Controller/EntityTestController.php
@@ -7,31 +7,54 @@
 
 namespace Drupal\entity_test\Controller;
 
-use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Controller routines for entity_test routes.
  */
-class EntityTestController {
+class EntityTestController extends ControllerBase {
 
   /**
-   * @todo Remove entity_test_add()
+   * Displays the 'Add new entity_test' form.
+   *
+   * @param string $entity_type
+   *   Name of the entity type for which a create form should be displayed.
+   *
+   * @return array
+   *   The processed form for a new entity_test.
+   *
+   * @see \Drupal\entity_test\Routing\EntityTestRoutes::routes()
    */
   public function testAdd($entity_type) {
-    return entity_test_add($entity_type);
+    $entity = entity_create($entity_type, array());
+    $form = $this->entityFormBuilder()->getForm($entity);
+    $form['#title'] = $this->t('Create an @type', array('@type' => $entity_type));
+    return $form;
   }
 
   /**
-   * @todo Remove entity_test_edit()
+   * Displays the 'Edit existing entity_test' form.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request object to get entity type from.
+   *
+   * @return array
+   *   The processed form for the edited entity.
+   *
+   * @see \Drupal\entity_test\Routing\EntityTestRoutes::routes()
    */
   public function testEdit(Request $request) {
     $entity = $request->attributes->get($request->attributes->get('_entity_type'));
-    return entity_test_edit($entity);
+    $form = $this->entityFormBuilder()->getForm($entity);
+    $form['#title'] = $entity->label();
+    return $form;
   }
 
   /**
    * Returns an empty page.
+   *
+   * @see \Drupal\entity_test\Routing\EntityTestRoutes::routes()
    */
   public function testAdmin() {
     return '';
