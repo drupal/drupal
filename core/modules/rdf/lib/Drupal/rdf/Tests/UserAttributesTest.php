3001e93a139025a39efa77a3e3480ff9e35fd54b
Alex Pott <alex.a.pott@googlemail.com>
2014-03-04 12:50:20 +0000

    Issue #1992954 by lokapujya, sphism, cwells, djevans, Stefan Freudenberg, Gaelan: Trimming of long usernames results in incorrect RDF.

--- core/modules/rdf/lib/Drupal/rdf/Tests/UserAttributesTest.php
+++ core/modules/rdf/lib/Drupal/rdf/Tests/UserAttributesTest.php
@@ -48,59 +48,68 @@ class UserAttributesTest extends WebTestBase {
    * being used.
    */
   function testUserAttributesInMarkup() {
-    // Creates two users, one with access to user profiles.
+    // Creates users that should and should not be truncated
+    // by template_preprocess_username (20 characters)
+    // one of these users tests right on the cusp (20).
     $user1 = $this->drupalCreateUser(array('access user profiles'));
-    $user2 = $this->drupalCreateUser();
+
+    $authors = array(
+      $this->drupalCreateUser(array(), $this->randomName(30)),
+      $this->drupalCreateUser(array(), $this->randomName(20)),
+      $this->drupalCreateUser(array(), $this->randomName(5))
+    );
+
     $this->drupalLogin($user1);
 
-    $account_uri = url('user/' . $user2->id(), array('absolute' => TRUE));
+    foreach($authors as $author) {
+      $account_uri = url('user/' . $author->id(), array('absolute' => TRUE));
 
-    // Parses the user profile page where the default bundle mapping for user
-    // should be used.
-    $parser = new \EasyRdf_Parser_Rdfa();
-    $graph = new \EasyRdf_Graph();
-    $base_uri = url('<front>', array('absolute' => TRUE));
-    $parser->parse($graph, $this->drupalGet('user/' . $user2->id()), 'rdfa', $base_uri);
+      // Parses the user profile page where the default bundle mapping for user
+      // should be used.
+      $parser = new \EasyRdf_Parser_Rdfa();
+      $graph = new \EasyRdf_Graph();
+      $base_uri = url('<front>', array('absolute' => TRUE));
+      $parser->parse($graph, $this->drupalGet('user/' . $author->id()), 'rdfa', $base_uri);
 
-    // Inspects RDF graph output.
-    // User type.
-    $expected_value = array(
-      'type' => 'uri',
-      'value' => 'http://rdfs.org/sioc/ns#UserAccount',
-    );
-    $this->assertTrue($graph->hasProperty($account_uri, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', $expected_value), 'User type found in RDF output (sioc:UserAccount).');
-    // User name.
-    $expected_value = array(
-      'type' => 'literal',
-      'value' => $user2->getUsername(),
-    );
-    $this->assertTrue($graph->hasProperty($account_uri, 'http://xmlns.com/foaf/0.1/name', $expected_value), 'User name found in RDF output (foaf:name).');
+      // Inspects RDF graph output.
+      // User type.
+      $expected_value = array(
+        'type' => 'uri',
+        'value' => 'http://rdfs.org/sioc/ns#UserAccount',
+      );
+      $this->assertTrue($graph->hasProperty($account_uri, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', $expected_value), 'User type found in RDF output (sioc:UserAccount).');
+      // User name.
+      $expected_value = array(
+        'type' => 'literal',
+        'value' => $author->getUsername(),
+      );
+      $this->assertTrue($graph->hasProperty($account_uri, 'http://xmlns.com/foaf/0.1/name', $expected_value), 'User name found in RDF output (foaf:name).');
 
-    // User 2 creates a node.
-    $this->drupalLogin($user2);
-    $node = $this->drupalCreateNode(array('type' => 'article', 'promote' => 1));
-    $this->drupalLogin($user1);
+      // User creates a node.
+      $this->drupalLogin($author);
+      $node = $this->drupalCreateNode(array('type' => 'article', 'promote' => 1));
+      $this->drupalLogin($user1);
 
-    // Parses the user profile page where the default bundle mapping for user
-    // should be used.
-    $parser = new \EasyRdf_Parser_Rdfa();
-    $graph = new \EasyRdf_Graph();
-    $base_uri = url('<front>', array('absolute' => TRUE));
-    $parser->parse($graph, $this->drupalGet('node/' . $node->id()), 'rdfa', $base_uri);
+      // Parses the node created by the user.
+      $parser = new \EasyRdf_Parser_Rdfa();
+      $graph = new \EasyRdf_Graph();
+      $base_uri = url('<front>', array('absolute' => TRUE));
+      $parser->parse($graph, $this->drupalGet('node/' . $node->id()), 'rdfa', $base_uri);
 
-    // Ensures the default bundle mapping for user is used on the Authored By
-    // information on the node.
-    // User type.
-    $expected_value = array(
-      'type' => 'uri',
-      'value' => 'http://rdfs.org/sioc/ns#UserAccount',
-    );
-    $this->assertTrue($graph->hasProperty($account_uri, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', $expected_value), 'User type found in RDF output (sioc:UserAccount).');
-    // User name.
-    $expected_value = array(
-      'type' => 'literal',
-      'value' => $user2->getUsername(),
-    );
-    $this->assertTrue($graph->hasProperty($account_uri, 'http://xmlns.com/foaf/0.1/name', $expected_value), 'User name found in RDF output (foaf:name).');
+      // Ensures the default bundle mapping for user is used on the Authored By
+      // information on the node.
+      $expected_value = array(
+        'type' => 'uri',
+        'value' => 'http://rdfs.org/sioc/ns#UserAccount',
+      );
+      $this->assertTrue($graph->hasProperty($account_uri, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', $expected_value), 'User type found in RDF output (sioc:UserAccount).');
+      // User name.
+      $expected_value = array(
+        'type' => 'literal',
+        'value' => $author->getUsername(),
+      );
+      $this->assertTrue($graph->hasProperty($account_uri, 'http://xmlns.com/foaf/0.1/name', $expected_value), 'User name found in RDF output (foaf:name).');
+
+    }
   }
 }
