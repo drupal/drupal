3ed3cbe05564eed35a92428d6b5be277fdac4ea8
webchick <webchick@24967.no-reply.drupal.org>
2014-03-12 09:29:55 -0700

    Issue #2205161 by jibran, rteijeiro | sun: Convert node.js into a library.

--- core/modules/node/lib/Drupal/node/NodeFormController.php
+++ core/modules/node/lib/Drupal/node/NodeFormController.php
@@ -114,7 +114,7 @@ class NodeFormController extends ContentEntityFormController {
         'class' => array('node-form-revision-information'),
       ),
       '#attached' => array(
-        'js' => array(drupal_get_path('module', 'node') . '/node.js'),
+        'library' => array('node/drupal.node'),
       ),
       '#weight' => 20,
       '#optional' => TRUE,
@@ -152,8 +152,8 @@ class NodeFormController extends ContentEntityFormController {
         'class' => array('node-form-author'),
       ),
       '#attached' => array(
+        'library' => array('node/drupal.node'),
         'js' => array(
-          drupal_get_path('module', 'node') . '/node.js',
           array(
             'type' => 'setting',
             'data' => array('anonymous' => $user_config->get('anonymous')),
@@ -194,7 +194,7 @@ class NodeFormController extends ContentEntityFormController {
         'class' => array('node-form-options'),
       ),
       '#attached' => array(
-        'js' => array(drupal_get_path('module', 'node') . '/node.js'),
+        'library' => array('node/drupal.node'),
       ),
       '#weight' => 95,
       '#optional' => TRUE,
