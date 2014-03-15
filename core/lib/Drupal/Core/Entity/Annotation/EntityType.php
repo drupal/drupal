ddade5bf7948bda55d941894ef265f9678da8d50
Jennifer Hodgdon <yahgrp@poplarware.com>
2014-03-15 05:50:29 -0700

    Issue #2189497 by paulh: Add docs to EntityType annotation class to tell where to actually find the relevant docs

--- core/lib/Drupal/Core/Entity/Annotation/EntityType.php
+++ core/lib/Drupal/Core/Entity/Annotation/EntityType.php
@@ -12,6 +12,14 @@ use Drupal\Component\Annotation\Plugin;
 /**
  * Defines an Entity type annotation object.
  *
+ * Entity type plugins use an object-based annotation method, rather than an
+ * array-type annotation method (as commonly used on other annotation types).
+ * The annotation properties of entity types are found on
+ * \Drupal\Core\Entity\EntityType and are accessed using get/set methods defined
+ * in \Drupal\Core\Entity\EntityTypeInterface.
+ *
+ * @see \Drupal\Core\Entity\Annotation\EntityType
+ *
  * @Annotation
  */
 class EntityType extends Plugin {
