1adeb2ed3f3a2ccb664deb937af18445b55184db
webchick <webchick@24967.no-reply.drupal.org>
2014-03-31 08:10:19 -0700

    Issue #2229781 by sun, alexpott: Opcode cache utility breaking cli site install for PHP 5.5.

--- core/lib/Drupal/Component/Utility/OpCodeCache.php
+++ core/lib/Drupal/Component/Utility/OpCodeCache.php
@@ -29,7 +29,11 @@ class OpCodeCache {
     if (extension_loaded('Zend OPcache')) {
       opcache_invalidate($pathname, TRUE);
     }
-    if (extension_loaded('apc')) {
+    // If apcu extenstion is enabled in PHP 5.5 or greater it emulates apc.
+    // This is to provide an easy upgrade path if you are using apc's user
+    // caching however the emulation does not extend to opcode caching.
+    // Therefore we need to check if the function exists as well.
+    if (extension_loaded('apc') && function_exists('apc_delete_file')) {
       // apc_delete_file() throws a PHP warning in case the specified file was
       // not compiled yet.
       // @see http://php.net/manual/en/function.apc-delete-file.php
