7f00c591b0433b6ef40856427aa60a8d02b3fca6
Alex Pott <alex.a.pott@googlemail.com>
2014-04-09 04:49:22 -0400

    Issue #2218655 by sun, effulgentsia, jessebeach: Core expects $theme.settings default config file to exist.

---
+++ core/modules/system/lib/Drupal/system/Tests/Theme/ThemeSettingsTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\system\Tests\Theme\ThemeSettingsTest.
+ */
+
+namespace Drupal\system\Tests\Theme;
+
+use Drupal\Core\Extension\ExtensionDiscovery;
+use Drupal\simpletest\DrupalUnitTestBase;
+
+/**
+ * Tests theme settings functionality.
+ */
+class ThemeSettingsTest extends DrupalUnitTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('system');
+
+  /**
+   * List of discovered themes.
+   *
+   * @var array
+   */
+  protected $availableThemes;
+
+  public static function getInfo() {
+    return array(
+      'name' => 'Theme settings',
+      'description' => 'Tests theme settings functionality.',
+      'group' => 'Theme',
+    );
+  }
+
+  function setUp() {
+    parent::setUp();
+    // Theme settings rely on System module's system.theme.global configuration.
+    $this->installConfig(array('system'));
+
+    if (!isset($this->availableThemes)) {
+      $discovery = new ExtensionDiscovery();
+      $this->availableThemes = $discovery->scan('theme');
+    }
+  }
+
+  /**
+   * Tests that $theme.settings are imported and used as default theme settings.
+   */
+  function testDefaultConfig() {
+    $name = 'test_basetheme';
+    $path = $this->availableThemes[$name]->getPath();
+    $this->assertTrue(file_exists("$path/config/$name.settings.yml"));
+    $this->container->get('theme_handler')->enable(array($name));
+    $this->assertIdentical(theme_get_setting('base', $name), 'only');
+  }
+
+  /**
+   * Tests that the $theme.settings default config file is optional.
+   */
+  function testNoDefaultConfig() {
+    $name = 'stark';
+    $path = $this->availableThemes[$name]->getPath();
+    $this->assertFalse(file_exists("$path/config/$name.settings.yml"));
+    $this->container->get('theme_handler')->enable(array($name));
+    $this->assertNotNull(theme_get_setting('features.favicon', $name));
+  }
+
+}
