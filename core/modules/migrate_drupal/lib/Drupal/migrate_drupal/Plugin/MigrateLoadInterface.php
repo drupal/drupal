23b38b72943c3f790e28b16c65643ea132629945
Alex Pott <alex.a.pott@googlemail.com>
2014-04-14 13:04:21 +0100

    Issue #2190561 by chx, pcambra, benjy: Migrate in core: Add a load system for migrate plugins.

---
+++ core/modules/migrate_drupal/lib/Drupal/migrate_drupal/Plugin/MigrateLoadInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\migrate\Plugin\MigrateLoadInterface
+ */
+
+namespace Drupal\migrate_drupal\Plugin;
+
+use Drupal\Core\Entity\EntityStorageInterface;
+
+interface MigrateLoadInterface {
+
+  /**
+   * Load an additional migration.
+   *
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The migration storage controller.
+   * @param string $sub_id
+   *   For example, when loading d6_node:article, this will be article.
+   * @return \Drupal\migrate\Entity\MigrationInterface
+   */
+  public function load(EntityStorageInterface $storage, $sub_id);
+
+  /**
+   * Load additional migrations.
+   *
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The migration storage controller.
+   * @param array $sub_ids
+   *   For example, when loading d6_node:article, sub_id will be article.
+   *   If NULL then load all sub-migrations.
+   * @return \Drupal\migrate\Entity\MigrationInterface[]
+   */
+  public function loadMultiple(EntityStorageInterface $storage, array $sub_ids = NULL);
+
+}
---
+++ core/modules/migrate_drupal/lib/Drupal/migrate_drupal/Plugin/migrate/load/d6/LoadTermNode.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate_drupal\Plugin\migrate\load\d6\TermNode.
+ */
+
+namespace Drupal\migrate_drupal\Plugin\migrate\load\d6;
+
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\migrate\Entity\MigrationInterface;
+use Drupal\migrate\MigrateExecutable;
+use Drupal\migrate\MigrateMessage;
+use Drupal\migrate\Row;
+use Drupal\migrate_drupal\Plugin\migrate\load\LoadEntity;
+
+/**
+ * @PluginID("d6_term_node")
+ */
+class LoadTermNode extends LoadEntity {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, MigrationInterface $migration) {
+    $configuration['bundle_migration'] = 'd6_taxonomy_vocabulary';
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadMultiple(EntityStorageInterface $storage, array $sub_ids = NULL) {
+    /** @var \Drupal\migrate\Entity\MigrationInterface $bundle_migration */
+    $bundle_migration = $storage->load('d6_taxonomy_vocabulary');
+    $migrate_executable = new MigrateExecutable($bundle_migration, new MigrateMessage());
+    $process = array_intersect_key($bundle_migration->get('process'), $bundle_migration->getDestinationPlugin()->getIds());
+    $migrations = array();
+    $vid_map = array();
+    foreach ($bundle_migration->getIdMap() as $key => $value) {
+      $old_vid = unserialize($key)['sourceid1'];
+      $new_vid = $value['destid1'];
+      $vid_map[$old_vid] = $new_vid;
+    }
+    foreach ($bundle_migration->getSourcePlugin()->getIterator() as $source_row) {
+      $row = new Row($source_row, $source_row);
+      $migrate_executable->processRow($row, $process);
+      $old_vid = $source_row['vid'];
+      $new_vid = $row->getDestinationProperty('vid');
+      $vid_map[$old_vid] = $new_vid;
+    }
+    foreach ($vid_map as $old_vid => $new_vid) {
+      $values = $this->migration->toArray();
+      $migration_id = $this->migration->id() . ':' . $old_vid;
+      $values['id'] = $migration_id;
+      $values['source']['vid'] = $old_vid;
+      $values['process'][$new_vid] = 'tid';
+      $migrations[$migration_id] = $storage->create($values);;
+    }
+
+    return $migrations;
+  }
+
+}
