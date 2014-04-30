e96964286085e91e0c152ec6cb2fcbef4965459d
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2014-04-30 11:49:52 +0100

    Issue #2039021 by tim.plunkett, Berdir, Xano | akshay.swnt22: Convert ActionManager to extend DefaultPluginManager.

--- core/lib/Drupal/Core/Action/ActionManager.php
+++ core/lib/Drupal/Core/Action/ActionManager.php
@@ -7,31 +7,38 @@
 
 namespace Drupal\Core\Action;
 
-use Drupal\Component\Plugin\PluginManagerBase;
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Language\LanguageManager;
+use Drupal\Core\Plugin\DefaultPluginManager;
 use Drupal\Core\Plugin\Discovery\AlterDecorator;
 use Drupal\Core\Plugin\Discovery\AnnotatedClassDiscovery;
-use Drupal\Core\Plugin\Factory\ContainerFactory;
 
 /**
  * Provides an Action plugin manager.
  *
- * @see \Drupal\Core\Annotation\Operation
- * @see \Drupal\Core\Action\OperationInterface
+ * @see \Drupal\Core\Annotation\Action
+ * @see \Drupal\Core\Action\ActionInterface
  */
-class ActionManager extends PluginManagerBase {
+class ActionManager extends DefaultPluginManager {
 
   /**
-   * Constructs a ActionManager object.
+   * Constructs a new class instance.
    *
    * @param \Traversable $namespaces
    *   An object that implements \Traversable which contains the root paths
    *   keyed by the corresponding namespace to look for plugin implementations.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
+   *   Cache backend instance to use.
+   * @param \Drupal\Core\Language\LanguageManager $language_manager
+   *   The language manager.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler to invoke the alter hook with.
    */
-  public function __construct(\Traversable $namespaces) {
-    $this->discovery = new AnnotatedClassDiscovery('Plugin/Action', $namespaces, 'Drupal\Core\Annotation\Action');
-    $this->discovery = new AlterDecorator($this->discovery, 'action_info');
-
-    $this->factory = new ContainerFactory($this);
+  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, LanguageManager $language_manager, ModuleHandlerInterface $module_handler) {
+    parent::__construct('Plugin/Action', $namespaces, $module_handler, 'Drupal\Core\Annotation\Action');
+    $this->alterInfo('action_info');
+    $this->setCacheBackend($cache_backend, $language_manager, 'action_info');
   }
 
   /**
