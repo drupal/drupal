26eb01c06de7aea4134674799bbb5a5a56f28291
Alex Pott <alex.a.pott@googlemail.com>
2014-05-06 11:42:01 +0100

    Issue #2245721 by vijaycs85, YesCT: Add missing configuration schema in language component.

---
+++ core/modules/language/lib/Drupal/language/Tests/LanguageConfigSchemaTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\language\Tests\LanguageConfigSchemaTest.
+ */
+
+namespace Drupal\language\Tests;
+
+use Drupal\config\Tests\ConfigSchemaTestBase;
+
+/**
+ * Tests the language config schema.
+ */
+class LanguageConfigSchemaTest extends ConfigSchemaTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('language');
+
+  /**
+   * A user with administrative permissions.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $adminUser;
+
+  public static function getInfo() {
+    return array(
+      'name' => 'Language config schema',
+      'description' => 'Ensures the language config schema is correct.',
+      'group' => 'Language',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  function setUp() {
+    parent::setUp();
+
+    // Create user.
+    $this->adminUser = $this->drupalCreateUser(array('administer languages'));
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Tests whether the language config schema is valid.
+   */
+  function testValidLanguageConfigSchema() {
+    // Make sure no language configuration available by default.
+    $config_data = \Drupal::config('language.settings')->get();
+    $this->assertTrue(empty($config_data));
+
+    $settings_path = 'admin/config/regional/content-language';
+
+    // Enable translation for menu link.
+    $edit['entity_types[menu_link]'] = TRUE;
+    $edit['settings[menu_link][menu_link][settings][language][language_show]'] = TRUE;
+
+    // Enable translation for user.
+    $edit['entity_types[user]'] = TRUE;
+    $edit['settings[user][user][settings][language][language_show]'] = TRUE;
+    $edit['settings[user][user][settings][language][langcode]'] = 'en';
+
+    $this->drupalPostForm($settings_path, $edit, t('Save'));
+
+    $config_data = \Drupal::config('language.settings')->get();
+    // Make sure configuration saved correctly.
+    $this->assertTrue($config_data['entities']['menu_link']['menu_link']['language']['default_configuration']['language_show']);
+
+    $this->assertConfigSchema(\Drupal::service('config.typed'), 'language.settings', $config_data);
+  }
+
+}
