7982985ae6ddb6d73ecc0d6827d0d5964b212b09
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2014-05-08 15:05:22 +0100

    Issue #2226933 by mandar.harkare, mortendk: Remove odd, even, first & last classes from views-view-unformatted.html.twig.

--- core/modules/views/lib/Drupal/views/Tests/Plugin/StyleUnformattedTest.php
+++ core/modules/views/lib/Drupal/views/Tests/Plugin/StyleUnformattedTest.php
@@ -45,19 +45,6 @@ class StyleUnformattedTest extends StyleTestBase {
       $count++;
       $attributes = $row->attributes();
       $class = (string) $attributes['class'][0];
-      // Make sure that each row has a row css class.
-      $this->assertTrue(strpos($class, "views-row-$count") !== FALSE, 'Make sure that each row has a row css class.');
-      // Make sure that the odd/even classes are set right.
-      $odd_even = $count % 2 == 0 ? 'even' : 'odd';
-      $this->assertTrue(strpos($class, "views-row-$odd_even") !== FALSE, 'Make sure that the odd/even classes are set right.');
-
-      if ($count == 1) {
-        $this->assertTrue(strpos($class, "views-row-first") !== FALSE, 'Make sure that the first class is set right.');
-      }
-      elseif ($count == $count_result) {
-        $this->assertTrue(strpos($class, "views-row-last") !== FALSE, 'Make sure that the last class is set right.');
-
-      }
       $this->assertTrue(strpos($class, 'views-row') !== FALSE, 'Make sure that the views row class is set right.');
     }
   }
