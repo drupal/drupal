3eda9f7076d6dadab201a67165f5b77b6bf824a9
Alex Pott <alex.a.pott@googlemail.com>
2014-05-15 13:11:52 -0500

    Issue #2256355 by tim.plunkett: Make block plugins usable outside the block entity context.

--- core/modules/block/lib/Drupal/block/BlockManagerInterface.php
+++ core/modules/block/lib/Drupal/block/BlockManagerInterface.php
@@ -7,10 +7,12 @@
 
 namespace Drupal\block;
 
+use Drupal\Component\Plugin\PluginManagerInterface;
+
 /**
  * Provides an interface for the discovery and instantiation of block plugins.
  */
-interface BlockManagerInterface {
+interface BlockManagerInterface extends PluginManagerInterface {
 
   /**
    * Gets the names of all block categories.
