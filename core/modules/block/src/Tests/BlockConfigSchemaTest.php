0ad74d11f5300d3ecda4f36e486b03228e515842
Alex Pott <alex.a.pott@googlemail.com>
2014-06-03 17:52:38 -0500

    Issue #2274175 by tim.plunkett, vijaycs85: Block plugin schema should be defined separately from the entity.

---
+++ core/modules/block/src/Tests/BlockConfigSchemaTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\block\Tests\BlockConfigSchemaTest.
+ */
+
+namespace Drupal\block\Tests;
+
+use Drupal\block\Entity\Block;
+use Drupal\config\Tests\ConfigSchemaTestBase;
+
+/**
+ * Tests the block config schema.
+ */
+class BlockConfigSchemaTest extends ConfigSchemaTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = array('block', 'aggregator', 'book', 'custom_block', 'forum', 'statistics');
+
+  /**
+   * The typed config manager.
+   *
+   * @var \Drupal\Core\Config\TypedConfigManagerInterface
+   */
+  protected $typedConfig;
+
+  /**
+   * The block manager.
+   *
+   * @var \Drupal\block\BlockManagerInterface
+   */
+  protected $blockManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'name' => 'Block config schema',
+      'description' => '',
+      'group' => 'Block',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->typedConfig = \Drupal::service('config.typed');
+    $this->blockManager = \Drupal::service('plugin.manager.block');
+  }
+
+  /**
+   * Tests the block config schema for block plugins.
+   */
+  public function testBlockConfigSchema() {
+    foreach ($this->blockManager->getDefinitions() as $block_id => $definition) {
+      $id = strtolower($this->randomName());
+      $block = Block::create(array(
+        'id' => $id,
+        'theme' => 'stark',
+        'weight' => 00,
+        'status' => TRUE,
+        'region' => 'content',
+        'plugin' => $block_id,
+        'settings' => array(
+          'label' => $this->randomName(),
+          'provider' => 'system',
+          'label_display' => FALSE,
+        ),
+        'visibility' => array(),
+      ));
+      $block->save();
+
+      $config = \Drupal::config("block.block.$id");
+      $this->assertEqual($config->get('id'), $id);
+      $this->assertConfigSchema($this->typedConfig, $config->getName(), $config->get());
+    }
+  }
+
+}
