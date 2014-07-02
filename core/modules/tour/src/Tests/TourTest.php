bd9eef3f3da78e04344827e3752d4477903a51ff
Dries <dries@buytaert.net>
2014-07-02 16:57:28 -0400

    Issue #2289059 by martin107: Fixed duplicate assertion text is causing confusion.

--- core/modules/tour/src/Tests/TourTest.php
+++ core/modules/tour/src/Tests/TourTest.php
@@ -182,6 +182,6 @@ class TourTest extends TourTestBasic {
       ':data_id' => 'tour-test-1',
       ':text' => 'The first tip',
     ));
-    $this->assertEqual(count($elements), 0, 'Found English variant of tip 1.');
+    $this->assertEqual(count($elements), 0, 'Did not find English variant of tip 1.');
   }
 }
