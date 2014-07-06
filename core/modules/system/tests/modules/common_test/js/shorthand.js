0fb70c779fc754dd54b49ebeaf4ca76917b808a5
Alex Pott <alex.a.pott@googlemail.com>
2014-07-06 17:49:49 +0100

    Issue #2241165 by larowlan: Fixed Remove redundant shorthand.js file.

--- core/modules/system/tests/modules/common_test/js/shorthand.js
+++
@@ -1,9 +0,0 @@
-/**
- * JavaScript file for the 'Shorthand' plugin.
- *
- * This file is intentionally blank. It is used to test that nominating
- * JavaScript (and CSS) files in the shorthand format using hook_library_info().
- *
- * @see common_test_library_info()
- * @see \Drupal/system/Tests/Common/JavaScriptTest::testLibraryRender()
- */
