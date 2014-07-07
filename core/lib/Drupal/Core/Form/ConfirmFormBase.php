49d64895a2f94450f3d067e7792d8a98208b5006
Alex Pott <alex.a.pott@googlemail.com>
2014-07-07 12:24:52 +0100

    Issue #2294597 by Xano: Remove usage of t() from the confirm form base classes.

--- core/lib/Drupal/Core/Form/ConfirmFormBase.php
+++ core/lib/Drupal/Core/Form/ConfirmFormBase.php
@@ -16,21 +16,21 @@ abstract class ConfirmFormBase extends FormBase implements ConfirmFormInterface 
    * {@inheritdoc}
    */
   public function getDescription() {
-    return t('This action cannot be undone.');
+    return $this->t('This action cannot be undone.');
   }
 
   /**
    * {@inheritdoc}
    */
   public function getConfirmText() {
-    return t('Confirm');
+    return $this->t('Confirm');
   }
 
   /**
    * {@inheritdoc}
    */
   public function getCancelText() {
-    return t('Cancel');
+    return $this->t('Cancel');
   }
 
   /**
