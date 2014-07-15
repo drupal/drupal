8f94da5725333b3f2546a20581fb28fd4c5e5f7a
Alex Pott <alex.a.pott@googlemail.com>
2014-07-15 20:06:34 +0100

    Issue #2010246 by tim.plunkett, tkuldeep17, plopesc, InternetDevels, pfrenssen, googletorp: Convert update_manager_install_form, update_manager_update_form, update_manager_update_ready_form to the new form interface.

---
+++ core/modules/update/src/Form/UpdateManagerUpdate.php
@@ -0,0 +1,331 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\update\Form\UpdateManagerUpdate.
+ */
+
+namespace Drupal\update\Form;
+
+use Drupal\Component\Utility\String;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\State\StateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configure update settings for this site.
+ */
+class UpdateManagerUpdate extends FormBase {
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The Drupal state storage service.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * Constructs a new UpdateManagerUpdate object.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   * @param \Drupal\Core\State\StateInterface $state
+   *  The state service.
+   */
+  public function __construct(ModuleHandlerInterface $module_handler, StateInterface $state) {
+    $this->moduleHandler = $module_handler;
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormID() {
+    return 'update_manager_update_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('module_handler'),
+      $container->get('state')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state) {
+    $this->moduleHandler->loadInclude('update', 'inc', 'update.manager');
+
+    $last_markup = array(
+      '#theme' => 'update_last_check',
+      '#last' => $this->state->get('update.last_check') ?: 0,
+    );
+    $form['last_check'] = array(
+      '#markup' => drupal_render($last_markup),
+    );
+
+    if (!_update_manager_check_backends($form, 'update')) {
+      return $form;
+    }
+
+    $available = update_get_available(TRUE);
+    if (empty($available)) {
+      $form['message'] = array(
+        '#markup' => $this->t('There was a problem getting update information. Try again later.'),
+      );
+      return $form;
+    }
+
+    $form['#attached']['library'][] = 'update/drupal.update.admin';
+
+    // This will be a nested array. The first key is the kind of project, which
+    // can be either 'enabled', 'disabled', 'manual' (projects which require
+    // manual updates, such as core). Then, each subarray is an array of
+    // projects of that type, indexed by project short name, and containing an
+    // array of data for cells in that project's row in the appropriate table.
+    $projects = array();
+
+    // This stores the actual download link we're going to update from for each
+    // project in the form, regardless of if it's enabled or disabled.
+    $form['project_downloads'] = array('#tree' => TRUE);
+    $this->moduleHandler->loadInclude('update', 'inc', 'update.compare');
+    $project_data = update_calculate_project_data($available);
+    foreach ($project_data as $name => $project) {
+      // Filter out projects which are up to date already.
+      if ($project['status'] == UPDATE_CURRENT) {
+        continue;
+      }
+      // The project name to display can vary based on the info we have.
+      if (!empty($project['title'])) {
+        if (!empty($project['link'])) {
+          $project_name = l($project['title'], $project['link']);
+        }
+        else {
+          $project_name = String::checkPlain($project['title']);
+        }
+      }
+      elseif (!empty($project['info']['name'])) {
+        $project_name = String::checkPlain($project['info']['name']);
+      }
+      else {
+        $project_name = String::checkPlain($name);
+      }
+      if ($project['project_type'] == 'theme' || $project['project_type'] == 'theme-disabled') {
+        $project_name .= ' ' . $this->t('(Theme)');
+      }
+
+      if (empty($project['recommended'])) {
+        // If we don't know what to recommend they upgrade to, we should skip
+        // the project entirely.
+        continue;
+      }
+
+      $recommended_release = $project['releases'][$project['recommended']];
+      $recommended_version = $recommended_release['version'] . ' ' . l($this->t('(Release notes)'), $recommended_release['release_link'], array('attributes' => array('title' => $this->t('Release notes for @project_title', array('@project_title' => $project['title'])))));
+      if ($recommended_release['version_major'] != $project['existing_major']) {
+        $recommended_version .= '<div title="Major upgrade warning" class="update-major-version-warning">' . $this->t('This update is a major version update which means that it may not be backwards compatible with your currently running version.  It is recommended that you read the release notes and proceed at your own risk.') . '</div>';
+      }
+
+      // Create an entry for this project.
+      $entry = array(
+        'title' => $project_name,
+        'installed_version' => $project['existing_version'],
+        'recommended_version' => $recommended_version,
+      );
+
+      switch ($project['status']) {
+        case UPDATE_NOT_SECURE:
+        case UPDATE_REVOKED:
+          $entry['title'] .= ' ' . $this->t('(Security update)');
+          $entry['#weight'] = -2;
+          $type = 'security';
+          break;
+
+        case UPDATE_NOT_SUPPORTED:
+          $type = 'unsupported';
+          $entry['title'] .= ' ' . $this->t('(Unsupported)');
+          $entry['#weight'] = -1;
+          break;
+
+        case UPDATE_UNKNOWN:
+        case UPDATE_NOT_FETCHED:
+        case UPDATE_NOT_CHECKED:
+        case UPDATE_NOT_CURRENT:
+          $type = 'recommended';
+          break;
+
+        default:
+          // Jump out of the switch and onto the next project in foreach.
+          continue 2;
+      }
+
+      // Use the project title for the tableselect checkboxes.
+      $entry['title'] = array('data' => array(
+        '#title' => $entry['title'],
+        '#markup' => $entry['title'],
+      ));
+      $entry['#attributes'] = array('class' => array('update-' . $type));
+
+      // Drupal core needs to be upgraded manually.
+      $needs_manual = $project['project_type'] == 'core';
+
+      if ($needs_manual) {
+        // There are no checkboxes in the 'Manual updates' table so it will be
+        // rendered by _theme('table'), not _theme('tableselect'). Since the data
+        // formats are incompatible, we convert now to the format expected by
+        // _theme('table').
+        unset($entry['#weight']);
+        $attributes = $entry['#attributes'];
+        unset($entry['#attributes']);
+        $entry = array(
+          'data' => $entry,
+        ) + $attributes;
+      }
+      else {
+        $form['project_downloads'][$name] = array(
+          '#type' => 'value',
+          '#value' => $recommended_release['download_link'],
+        );
+      }
+
+      // Based on what kind of project this is, save the entry into the
+      // appropriate subarray.
+      switch ($project['project_type']) {
+        case 'core':
+          // Core needs manual updates at this time.
+          $projects['manual'][$name] = $entry;
+          break;
+
+        case 'module':
+        case 'theme':
+          $projects['enabled'][$name] = $entry;
+          break;
+
+        case 'module-disabled':
+        case 'theme-disabled':
+          $projects['disabled'][$name] = $entry;
+          break;
+      }
+    }
+
+    if (empty($projects)) {
+      $form['message'] = array(
+        '#markup' => $this->t('All of your projects are up to date.'),
+      );
+      return $form;
+    }
+
+    $headers = array(
+      'title' => array(
+        'data' => $this->t('Name'),
+        'class' => array('update-project-name'),
+      ),
+      'installed_version' => $this->t('Installed version'),
+      'recommended_version' => $this->t('Recommended version'),
+    );
+
+    if (!empty($projects['enabled'])) {
+      $form['projects'] = array(
+        '#type' => 'tableselect',
+        '#header' => $headers,
+        '#options' => $projects['enabled'],
+      );
+      if (!empty($projects['disabled'])) {
+        $form['projects']['#prefix'] = '<h2>' . $this->t('Enabled') . '</h2>';
+      }
+    }
+
+    if (!empty($projects['disabled'])) {
+      $form['disabled_projects'] = array(
+        '#type' => 'tableselect',
+        '#header' => $headers,
+        '#options' => $projects['disabled'],
+        '#weight' => 1,
+        '#prefix' => '<h2>' . $this->t('Disabled') . '</h2>',
+      );
+    }
+
+    // If either table has been printed yet, we need a submit button and to
+    // validate the checkboxes.
+    if (!empty($projects['enabled']) || !empty($projects['disabled'])) {
+      $form['actions'] = array('#type' => 'actions');
+      $form['actions']['submit'] = array(
+        '#type' => 'submit',
+        '#value' => $this->t('Download these updates'),
+      );
+    }
+
+    if (!empty($projects['manual'])) {
+      $prefix = '<h2>' . $this->t('Manual updates required') . '</h2>';
+      $prefix .= '<p>' . $this->t('Updates of Drupal core are not supported at this time.') . '</p>';
+      $form['manual_updates'] = array(
+        '#type' => 'table',
+        '#header' => $headers,
+        '#rows' => $projects['manual'],
+        '#prefix' => $prefix,
+        '#weight' => 120,
+      );
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, array &$form_state) {
+    if (!empty($form_state['values']['projects'])) {
+      $enabled = array_filter($form_state['values']['projects']);
+    }
+    if (!empty($form_state['values']['disabled_projects'])) {
+      $disabled = array_filter($form_state['values']['disabled_projects']);
+    }
+    if (empty($enabled) && empty($disabled)) {
+      $this->setFormError('projects', $form_state, $this->t('You must select at least one project to update.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    $this->moduleHandler->loadInclude('update', 'inc', 'update.manager');
+    $projects = array();
+    foreach (array('projects', 'disabled_projects') as $type) {
+      if (!empty($form_state['values'][$type])) {
+        $projects = array_merge($projects, array_keys(array_filter($form_state['values'][$type])));
+      }
+    }
+    $operations = array();
+    foreach ($projects as $project) {
+      $operations[] = array(
+        'update_manager_batch_project_get',
+        array(
+          $project,
+          $form_state['values']['project_downloads'][$project],
+        ),
+      );
+    }
+    $batch = array(
+      'title' => $this->t('Downloading updates'),
+      'init_message' => $this->t('Preparing to download selected updates'),
+      'operations' => $operations,
+      'finished' => 'update_manager_download_batch_finished',
+      'file' => drupal_get_path('module', 'update') . '/update.manager.inc',
+    );
+    batch_set($batch);
+  }
+
+}
---
+++ core/modules/update/src/Form/UpdateReady.php
@@ -0,0 +1,148 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\update\Form\UpdateReady.
+ */
+
+namespace Drupal\update\Form;
+
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\FileTransfer\Local;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\State\StateInterface;
+use Drupal\Core\Updater\Updater;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Configure update settings for this site.
+ */
+class UpdateReady extends FormBase {
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The state key value store.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * Constructs a new UpdateReady object.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The object that manages enabled modules in a Drupal installation.
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state key value store.
+   */
+  public function __construct(ModuleHandlerInterface $module_handler, StateInterface $state) {
+    $this->moduleHandler = $module_handler;
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormID() {
+    return 'update_manager_update_ready_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('module_handler'),
+      $container->get('state')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state) {
+    $this->moduleHandler->loadInclude('update', 'inc', 'update.manager');
+    if (!_update_manager_check_backends($form, 'update')) {
+      return $form;
+    }
+
+    $form['backup'] = array(
+      '#prefix' => '<strong>',
+      '#markup' => $this->t('Back up your database and site before you continue. <a href="@backup_url">Learn how</a>.', array('@backup_url' => url('http://drupal.org/node/22281'))),
+      '#suffix' => '</strong>',
+    );
+
+    $form['maintenance_mode'] = array(
+      '#title' => $this->t('Perform updates with site in maintenance mode (strongly recommended)'),
+      '#type' => 'checkbox',
+      '#default_value' => TRUE,
+    );
+
+    $form['actions'] = array('#type' => 'actions');
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Continue'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    // Store maintenance_mode setting so we can restore it when done.
+    $_SESSION['maintenance_mode'] = $this->state->get('system.maintenance_mode');
+    if ($form_state['values']['maintenance_mode'] == TRUE) {
+      $this->state->set('system.maintenance_mode', TRUE);
+    }
+
+    if (!empty($_SESSION['update_manager_update_projects'])) {
+      // Make sure the Updater registry is loaded.
+      drupal_get_updaters();
+
+      $updates = array();
+      $directory = _update_manager_extract_directory();
+
+      $projects = $_SESSION['update_manager_update_projects'];
+      unset($_SESSION['update_manager_update_projects']);
+
+      $project_real_location = NULL;
+      foreach ($projects as $project => $url) {
+        $project_location = $directory . '/' . $project;
+        $updater = Updater::factory($project_location);
+        $project_real_location = drupal_realpath($project_location);
+        $updates[] = array(
+          'project' => $project,
+          'updater_name' => get_class($updater),
+          'local_url' => $project_real_location,
+        );
+      }
+
+      // If the owner of the last directory we extracted is the same as the
+      // owner of our configuration directory (e.g. sites/default) where we're
+      // trying to install the code, there's no need to prompt for FTP/SSH
+      // credentials. Instead, we instantiate a Drupal\Core\FileTransfer\Local
+      // and invoke update_authorize_run_update() directly.
+      if (fileowner($project_real_location) == fileowner(conf_path())) {
+        $this->moduleHandler->loadInclude('update', 'inc', 'update.authorize');
+        $filetransfer = new Local(DRUPAL_ROOT);
+        update_authorize_run_update($filetransfer, $updates);
+      }
+      // Otherwise, go through the regular workflow to prompt for FTP/SSH
+      // credentials and invoke update_authorize_run_update() indirectly with
+      // whatever FileTransfer object authorize.php creates for us.
+      else {
+        system_authorized_init('update_authorize_run_update', drupal_get_path('module', 'update') . '/update.authorize.inc', array($updates), $this->t('Update manager'));
+        $form_state['redirect'] = system_authorized_get_url();
+      }
+    }
+  }
+
+}
