1b55c4f20d3da6a6dd6a7a90dc4d000c652de59e
Alex Pott <alex.a.pott@googlemail.com>
2014-07-15 11:59:47 +0100

    Issue #2302531 by tim.plunkett: Convert database_test_theme_tablesort to a class.

--- core/modules/system/tests/modules/database_test/src/Form/DatabaseTestForm.php
+++ core/modules/system/tests/modules/database_test/src/Form/DatabaseTestForm.php
@@ -7,16 +7,72 @@
 
 namespace Drupal\database_test\Form;
 
+use Drupal\Component\Utility\String;
+use Drupal\Core\Form\FormBase;
+
 /**
- * Temporary form controller for database_test module.
+ * Form controller for database_test module.
  */
-class DatabaseTestForm {
+class DatabaseTestForm extends FormBase {
 
   /**
-   * @todo Remove database_test_theme_tablesort().
+   * {@inheritdoc}
    */
-  public function testTablesortDefaultSort() {
-    return \Drupal::formBuilder()->getForm('database_test_theme_tablesort');
+  public function getFormId() {
+    return 'database_test_theme_tablesort';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state) {
+    $header = array(
+      'username' => array('data' => t('Username'), 'field' => 'u.name'),
+      'status' => array('data' => t('Status'), 'field' => 'u.status'),
+    );
+
+    $query = db_select('users', 'u');
+    $query->condition('u.uid', 0, '<>');
+
+    $count_query = clone $query;
+    $count_query->addExpression('COUNT(u.uid)');
+
+    $query = $query
+      ->extend('Drupal\Core\Database\Query\PagerSelectExtender')
+      ->extend('Drupal\Core\Database\Query\TableSortExtender');
+    $query
+      ->fields('u', array('uid'))
+      ->limit(50)
+      ->orderByHeader($header)
+      ->setCountQuery($count_query);
+    $uids = $query
+      ->execute()
+      ->fetchCol();
+
+    $options = array();
+
+    foreach (user_load_multiple($uids) as $account) {
+      $options[$account->id()] = array(
+        'title' => array('data' => array('#title' => String::checkPlain($account->getUsername()))),
+        'username' => String::checkPlain($account->getUsername()),
+        'status' =>  $account->isActive() ? t('active') : t('blocked'),
+      );
+    }
+
+    $form['accounts'] = array(
+      '#type' => 'tableselect',
+      '#header' => $header,
+      '#options' => $options,
+      '#empty' => t('No people available.'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
   }
 
 }
