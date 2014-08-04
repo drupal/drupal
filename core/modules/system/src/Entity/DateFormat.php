45d55e6a9ca5315a2a32b8d6d809ac7cc77262dd
webchick <drupal@webchick.net>
2014-08-04 09:52:46 -0700

    Issue #2314869 by Crell: Standardize DateFormat entity route names.

--- core/modules/system/src/Entity/DateFormat.php
+++ core/modules/system/src/Entity/DateFormat.php
@@ -33,8 +33,8 @@ use Drupal\system\DateFormatInterface;
  *   },
  *   admin_permission = "administer site configuration",
  *   links = {
- *     "delete-form" = "system.date_format_delete",
- *     "edit-form" = "system.date_format_edit"
+ *     "delete-form" = "entity.date_format.delete_form",
+ *     "edit-form" = "entity.date_format.edit_form"
  *   }
  * )
  */
