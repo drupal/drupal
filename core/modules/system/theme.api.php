ea8334982eda6810e67ef5a4f8d3704695e8d500
Alex Pott <alex.a.pott@googlemail.com>
2014-09-08 15:21:41 +0100

    Issue #2326093 by astrocling | jhodgdon: Need new section about Twig added to theme system overview.

--- core/modules/system/theme.api.php
+++ core/modules/system/theme.api.php
@@ -18,6 +18,19 @@
  * @link theme_render Theme system and Render API topic @endlink for more
  * information about render arrays and rendering.
  *
+ * @section sec_twig_theme Twig Templating Engine
+ * Drupal 8 utilizes the templating engine Twig. Twig offers developers a fast,
+ * secure, and flexible method for building templates for Drupal 8 sites. Twig
+ * also offers substantial usability improvements over PHPTemplate, and does
+ * not require front-end developers to know PHP to build and manipulate Drupal
+ * 8 themes.
+ *
+ * For further information on theming in Drupal 8 see
+ * https://www.drupal.org/theme-guide/8
+ *
+ * For further Twig documentation see
+ * http://twig.sensiolabs.org/doc/templates.html
+ *
  * @section sec_theme_hooks Theme Hooks
  * The theme system is invoked in drupal_render() by calling the internal
  * _theme() function, which operates on the concept of "theme hooks". Theme
