a8b92fe4f4b8d99982d4efcd64d55b96668e04a3
Alex Pott <alex.a.pott@googlemail.com>
2014-09-11 12:34:16 +0100

    Issue #2332983 by dawehner: Replace ban event subscriber with a ban stack.

---
+++ core/modules/ban/src/BanMiddleware.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ban\BanMiddleware.
+ */
+
+namespace Drupal\ban;
+
+use Drupal\Component\Utility\String;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * Provides a HTTP middleware to implement IP based banning.
+ */
+class BanMiddleware implements HttpKernelInterface {
+
+  /**
+   * The decorated kernel.
+   *
+   * @var \Symfony\Component\HttpKernel\HttpKernelInterface
+   */
+  protected $httpKernel;
+
+  /**
+   * The ban IP manager.
+   *
+   * @var \Drupal\ban\BanIpManagerInterface
+   */
+  protected $banIpManager;
+
+  /**
+   * Constructs a BanMiddleware object.
+   *
+   * @param \Symfony\Component\HttpKernel\HttpKernelInterface $http_kernel
+   *   The decorated kernel.
+   * @param \Drupal\ban\BanIpManagerInterface $manager
+   *   The ban IP manager.
+   */
+  public function __construct(HttpKernelInterface $http_kernel, BanIpManagerInterface $manager) {
+    $this->httpKernel = $http_kernel;
+    $this->banIpManager = $manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = TRUE) {
+    $ip = $request->getClientIp();
+    if ($this->banIpManager->isBanned($ip)) {
+      return new Response(String::format('Sorry @ip has been banned', ['@ip' => $ip]), 403);
+    }
+    return $this->httpKernel->handle($request, $type, $catch);
+  }
+
+}
---
+++ core/modules/ban/src/Tests/BanMiddlewareTest.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ban\Tests\BanMiddlewareTest.
+ */
+
+namespace Drupal\ban\Tests;
+
+use Drupal\ban\BanMiddleware;
+use Drupal\Tests\UnitTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * @coversDefaultClass \Drupal\ban\BanMiddleware
+ * @group ban
+ */
+class BanMiddlewareTest extends UnitTestCase {
+
+  /**
+   * The mocked wrapped kernel.
+   *
+   * @var \Symfony\Component\HttpKernel\HttpKernelInterface|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $kernel;
+
+  /**
+   * The mocked ban IP manager.
+   *
+   * @var \Drupal\ban\BanIpManagerInterface|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $banManager;
+
+  /**
+   * The tested ban middleware.
+   *
+   * @var \Drupal\ban\BanMiddleware
+   */
+  protected $banMiddleware;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->kernel = $this->getMock('Symfony\Component\HttpKernel\HttpKernelInterface');
+    $this->banManager = $this->getMock('Drupal\ban\BanIpManagerInterface');
+    $this->banMiddleware = new BanMiddleware($this->kernel, $this->banManager);
+  }
+
+  /**
+   * Tests a banned IP.
+   */
+  public function testBannedIp() {
+    $banned_ip = '17.0.0.2';
+    $this->banManager->expects($this->once())
+      ->method('isBanned')
+      ->with($banned_ip)
+      ->willReturn(TRUE);
+
+    $this->kernel->expects($this->never())
+      ->method('handle');
+
+    $request = Request::create('/test-path');
+    $request->server->set('REMOTE_ADDR', $banned_ip);
+    $response = $this->banMiddleware->handle($request);
+
+    $this->assertEquals(403, $response->getStatusCode());
+  }
+
+  /**
+   * Tests an unbanned IP.
+   */
+  public function testUnbannedIp() {
+    $unbanned_ip = '18.0.0.2';
+    $this->banManager->expects($this->once())
+      ->method('isBanned')
+      ->with($unbanned_ip)
+      ->willReturn(FALSE);
+
+    $request = Request::create('/test-path');
+    $request->server->set('REMOTE_ADDR', $unbanned_ip);
+    $expected_response = new Response(200);
+    $this->kernel->expects($this->once())
+      ->method('handle')
+      ->with($request, HttpKernelInterface::MASTER_REQUEST, TRUE)
+      ->willReturn($expected_response);
+
+    $response = $this->banMiddleware->handle($request);
+
+    $this->assertSame($expected_response, $response);
+  }
+
+}
