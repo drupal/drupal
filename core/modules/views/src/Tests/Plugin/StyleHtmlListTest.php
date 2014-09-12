0d1b898502a51f80a74a46b3aecb707ae6fa3c5e
Alex Pott <alex.a.pott@googlemail.com>
2014-09-12 12:05:00 +0100

    Issue #2197091 by kirby14, undertext, lhangea, hussainweb, ACF, smiro, visabhishek | ivanjaros: Fixed Class HTML attribute is present even when no class is assigned to the list.

---
+++ core/modules/views/src/Tests/Plugin/StyleHtmlListTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\views\Tests\Plugin\StyleHtmlListTest.
+ */
+
+namespace Drupal\views\Tests\Plugin;
+
+use Drupal\views\Tests\ViewUnitTestBase;
+use Drupal\views\Views;
+
+/**
+ * Tests the HTML list style plugin.
+ *
+ * @group views
+ * @see \Drupal\views\Plugin\views\style\HtmlList
+ */
+class StyleHtmlListTest extends ViewUnitTestBase {
+
+  /**
+   * Views used by this test.
+   *
+   * @var array
+   */
+  public static $testViews = array('test_style_html_list');
+
+  /**
+   * Make sure that the HTML list style markup is correct.
+   */
+  function testDefaultRowClasses() {
+    $view = Views::getView('test_style_html_list');
+    $output = $view->preview();
+    $output = drupal_render($output);
+
+    // Check that an empty class attribute is not added if the wrapper class is
+    // not set.
+    $this->assertTrue(strpos($output, '<div>') !== FALSE, 'Empty class is not added to DIV when class is not set');
+
+    // Check that an empty class attribute is not added if the list class is
+    // not set.
+    $this->assertTrue(strpos($output, '<ul>') !== FALSE, 'Empty class is not added to UL when class is not set');
+
+    // Set wrapper class and list class in style options.
+    $view->style_plugin->options['class'] = 'class';
+    $view->style_plugin->options['wrapper_class'] = 'wrapper-class';
+
+    $output = $view->preview();
+    $output = drupal_render($output);
+
+    // Check that class attribute is present if the wrapper class is set.
+    $this->assertTrue(strpos($output, '<div class="wrapper-class">') !== FALSE, 'Class is added to DIV');
+
+    // Check that class attribute is present if the list class is set.
+    $this->assertTrue(strpos($output, '<ul class="class">') !== FALSE, 'Class is added to UL');
+  }
+
+}
