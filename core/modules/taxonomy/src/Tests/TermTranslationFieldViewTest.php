ca513fb8784a26f531b821d4479faa669a0eb28b
Gabor Hojtsy <gabor@hojtsy.hu>
2014-09-28 19:51:27 +0200

    Issue #2345607 followup by webflo: The combination of #2345607 and #2344821 shortly after broke head.

--- core/modules/taxonomy/src/Tests/TermTranslationFieldViewTest.php
+++ core/modules/taxonomy/src/Tests/TermTranslationFieldViewTest.php
@@ -166,7 +166,7 @@ class TermTranslationFieldViewTest extends TaxonomyTestBase {
    */
   protected function setUpTermReferenceField() {
     entity_create('field_storage_config', array(
-      'name' => $this->termFieldName,
+      'field_name' => $this->termFieldName,
       'entity_type' => 'node',
       'type' => 'taxonomy_term_reference',
       'cardinality' => FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED,
