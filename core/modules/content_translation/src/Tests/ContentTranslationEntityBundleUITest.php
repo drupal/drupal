10341f431378c64ad5411d22d7f43c716342b4db
webchick <drupal@webchick.net>
2014-09-28 05:01:37 -0700

    Issue #2328161 by alexpott, vijaycs85 | Sutharsan: Fixed 'Enable Translation' checkbox default non-predictable for new bundles.

---
+++ core/modules/content_translation/src/Tests/ContentTranslationEntityBundleUITest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\entity\Tests\ContentTranslationEntityBundleUITest.
+ */
+
+namespace Drupal\content_translation\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests the content translation behaviours on entity bundle UI.
+ *
+ * @group content_translation
+ */
+class ContentTranslationEntityBundleUITest extends WebTestBase {
+
+  public static $modules = array('language', 'content_translation', 'node', 'comment', 'field_ui');
+
+  protected function setUp() {
+    parent::setUp();
+    $user = $this->drupalCreateUser(array('access administration pages', 'administer languages', 'administer content translation', 'administer content types'));
+    $this->drupalLogin($user);
+  }
+
+  /**
+   * Tests content types default translation behaviour.
+   */
+  public function testContentTypeUI() {
+    // Create first content type.
+    $this->drupalCreateContentType(array('type' => 'article'));
+    // Enable content translation.
+    $edit = array('language_configuration[content_translation]' => TRUE);
+    $this->drupalPostForm('admin/structure/types/manage/article', $edit, 'Save content type');
+
+    // Make sure add page does not inherit translation configuration from first
+    // content type.
+    $this->drupalGet('admin/structure/types/add');
+    $this->assertNoFieldChecked('edit-language-configuration-content-translation');
+  }
+
+}
