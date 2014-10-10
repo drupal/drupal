0803312d9a4597233462dc1cb3494e5c1f96428d
webchick <drupal@webchick.net>
2014-10-10 09:43:27 -0700

    Issue #2348783 by larowlan, Cottser, kugta | skein: Fixed drupal_set_message inherits previous style if set to 'status'.

---
+++ core/modules/system/src/Tests/Theme/MessageTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\system\Tests\Theme\MessageTest.
+ */
+
+namespace Drupal\system\Tests\Theme;
+
+use Drupal\simpletest\KernelTestBase;
+
+/**
+ * Tests built-in message theme functions.
+ *
+ * @group Theme
+ */
+class MessageTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = array('system');
+
+  /**
+   * Tests setting messages output.
+   */
+  function testMessages() {
+    drupal_set_message('An error occurred', 'error');
+    drupal_set_message('But then something nice happened');
+    $messages = array(
+      '#theme' => 'status_messages',
+    );
+    $this->render($messages);
+    $this->assertRaw('messages messages--error');
+    $this->assertRaw('messages messages--status');
+  }
+}
