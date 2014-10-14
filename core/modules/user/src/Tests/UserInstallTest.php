f1102d202bb1a78cc33a41e1a7bd59ed63c77591
webchick <drupal@webchick.net>
2014-10-13 21:36:15 -0700

    Issue #2355545 by lhangea, alexpott | YesCT: UserInstallTest testUserInstall() custom assert message not more helpful than the default.

--- core/modules/user/src/Tests/UserInstallTest.php
+++ core/modules/user/src/Tests/UserInstallTest.php
@@ -47,8 +47,8 @@ class UserInstallTest extends DrupalUnitTestBase {
 
     // Test that the anonymous and administrators languages are equal to the
     // site's default language.
-    $this->assertEqual($anon->langcode, \Drupal::languageManager()->getDefaultLanguage()->getId(), 'Anon user language is the default.');
-    $this->assertEqual($admin->langcode, \Drupal::languageManager()->getDefaultLanguage()->getId(), 'Admin user language is the default.');
+    $this->assertEqual($anon->langcode, \Drupal::languageManager()->getDefaultLanguage()->getId());
+    $this->assertEqual($admin->langcode, \Drupal::languageManager()->getDefaultLanguage()->getId());
 
     // Test that the administrator is active.
     $this->assertEqual($admin->status, 1);
