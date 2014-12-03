f28179d4035afba169378c333eb6c48421751140
Alex Pott <alex.a.pott@googlemail.com>
2014-12-03 16:32:27 +0000

    Issue #1074108 by pwolanin, webchick, BlakeLawson, jayeshanandani, Devin Carlson, harijari, David_Rothstein, c_lehel, Haza, oriol_e9g: Profile selection form not skipped if there is only one visible profile

---
+++ core/modules/system/src/Tests/Installer/SingleVisibleProfileTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\system\Tests\Installer\SingleVisibleProfileTest.
+ */
+
+namespace Drupal\system\Tests\Installer;
+
+use Drupal\Component\Serialization\Yaml;
+use Drupal\simpletest\InstallerTestBase;
+
+/**
+ * Tests distribution profile support.
+ *
+ * @group Installer
+ */
+class SingleVisibleProfileTest extends InstallerTestBase {
+
+  /**
+   * The installation profile to install.
+   *
+   * Not needed when only one is visible.
+   *
+   * @var string
+   */
+  protected $profile = NULL;
+
+  /**
+   * The install profile info.
+   *
+   * @var array
+   */
+  protected $info;
+
+  protected function setUp() {
+    $this->info = array(
+      'type' => 'profile',
+      'core' => \Drupal::CORE_COMPATIBILITY,
+      'name' => 'Override standard',
+      'hidden' => TRUE,
+    );
+    // File API functions are not available yet.
+    $path = $this->siteDirectory . '/profiles/standard';
+    mkdir($path, 0777, TRUE);
+    file_put_contents("$path/standard.info.yml", Yaml::encode($this->info));
+
+    parent::setUp();
+  }
+
+  /**
+   * Overrides InstallerTest::setUpProfile().
+   */
+  protected function setUpProfile() {
+    // This step is skipped, because there is only one visible profile.
+  }
+
+  /**
+   * Confirms that the installation succeeded.
+   */
+  public function testInstalled() {
+    $this->assertUrl('user/1');
+    $this->assertResponse(200);
+    // Confirm that we are logged-in after installation.
+    $this->assertText($this->root_user->getUsername());
+    // Confirm that the minimal profile was installed.
+    $this->assertEqual(drupal_get_profile(), 'minimal');
+  }
+
+}
