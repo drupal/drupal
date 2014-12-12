5f13519250683b34096802236a6790988d45d1b5
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2014-12-12 12:09:17 +0000

    Issue #2352081 by myforgedoteu, Devin Carlson, martin107: Prevent installation over an already installed database

---
+++ core/modules/system/src/Tests/Installer/InstallerExistingInstallationTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\system\Tests\Installer\InstallerExistingInstallationTest.
+ */
+
+namespace Drupal\system\Tests\Installer;
+
+use Drupal\simpletest\InstallerTestBase;
+
+/**
+ * Tests the installer with an existing Drupal installation.
+ *
+ * @group Installer
+ */
+class InstallerExistingInstallationTest extends InstallerTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * Verifies that Drupal can't be reinstalled while an existing installation is
+   * available.
+   */
+  public function testInstaller() {
+    // Verify that Drupal can't be immediately reinstalled.
+    $this->drupalGet($GLOBALS['base_url'] . '/core/install.php');
+    $this->assertRaw('Drupal already installed');
+
+    // Delete settings.php and attempt to reinstall again.
+    unlink($this->siteDirectory . '/settings.php');
+    $this->drupalGet($GLOBALS['base_url'] . '/core/install.php');
+    $this->setUpLanguage();
+    $this->setUpProfile();
+    $this->setUpSettings();
+    $this->assertRaw('Drupal already installed');
+  }
+
+}
