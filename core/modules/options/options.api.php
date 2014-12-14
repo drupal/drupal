ec4b6972f59cf50c3ff7db3a586709dd90cc9190
Alex Pott <alex.a.pott@googlemail.com>
2014-12-14 15:13:06 +0100

    Issue #2393061 by yched: Adjust phpdoc for callback_allowed_values_function()

--- core/modules/options/options.api.php
+++ core/modules/options/options.api.php
@@ -37,50 +37,62 @@ function hook_options_list_alter(array &$options, array $context) {
 }
 
 /**
- * Provides the allowed values for an options field or widget.
+ * Provides the allowed values for a 'list_*' field.
  *
  * Callback for options_allowed_values().
  *
+ * 'list_*' fields can specify a callback to define the set of their allowed
+ * values using the 'allowed_values_callback' storage setting.
+ *
+ * That function will be called:
+ *  - either in the context of a specific entity, which is then provided as the
+ *    $entity parameter,
+ *  - or for the field generally without the context of any specific entity or
+ *    entity bundle (typically, Views needing a list of values for an exposed
+ *    filter), in which case the $entity parameter is NULL.
+ * This lets the callback restrict the set of allowed values or adjust the
+ * labels depending on some conditions on the containing entity.
+ *
+ * For consistency, the set of values returned when an $entity is provided
+ * should be a subset of the values returned when no $entity is provided.
+ *
  * @param \Drupal\Core\Field\FieldStorageDefinitionInterface $definition
  *   The field storage definition.
- * @param \Drupal\Core\Entity\FieldableEntityInterface|NULL $entity
- *   (optional) A specific entity to use for either restricting the values or
- *   customizing the labels for particular bundles and entities. NULL when
- *   there is not a specific entity available, such as for Views filters.
- * @param bool $cacheable
- *   (optional) If $cacheable is FALSE, then the allowed values are not
- *   statically cached. See options_test_dynamic_values_callback() for an
- *   example of generating dynamic and uncached values. Defaults to TRUE.
+ * @param \Drupal\Core\Entity\FieldableEntityInterface|null $entity
+ *   (optional) The entity context if known, or NULL if the allowed values are
+ *   being collected without the context of a specific entity.
+ * @param bool &$cacheable
+ *   (optional) If an $entity is provided, the $cacheable parameter should be
+ *   modified by reference and set to FALSE if the set of allowed values
+ *   returned was specifically adjusted for that entity and cannot not be reused
+ *   for other entities. Defaults to TRUE.
  *
  * @return array
  *   The array of allowed values. Keys of the array are the raw stored values
  *   (number or text), values of the array are the display labels. If $entity
  *   is NULL, you should return the list of all the possible allowed values in
- *   any context so that other code (e.g. Views filters) can support the
- *   allowed values for all possible entities and bundles.
+ *   any context so that other code (e.g. Views filters) can support the allowed
+ *   values for all possible entities and bundles.
  *
  * @see options_allowed_values()
  * @see options_test_allowed_values_callback()
  * @see options_test_dynamic_values_callback()
  */
-function callback_allowed_values_function(FieldStorageDefinitionInterface $definition, FieldableEntityInterface $entity = NULL, $cacheable = TRUE) {
+function callback_allowed_values_function(FieldStorageDefinitionInterface $definition, FieldableEntityInterface $entity = NULL, &$cacheable = TRUE) {
   if (isset($entity) && ($entity->bundle() == 'not_a_programmer')) {
     $values = array(
-      'Group 1' => array(
-        1 => 'One',
-      ),
-      'Group 2' => array(
-        2 => 'Two',
-      ),
+      1 => 'One',
+      2 => 'Two',
     );
   }
   else {
     $values = array(
       'Group 1' => array(
         0 => 'Zero',
+        1 => 'One',
       ),
       'Group 2' => array(
-        1 => 'One',
+        2 => 'Two',
       ),
     );
   }
