b5e409a0f1e43a17899aa831171b049109b49bdb
webchick <drupal@webchick.net>
2014-12-29 12:23:54 -0800

    Issue #2397691 by alexpott: Random fail in Drupal\taxonomy\Tests\TermTest::testNodeTermCreationAndDeletion()

--- core/modules/taxonomy/src/Tests/TermTest.php
+++ core/modules/taxonomy/src/Tests/TermTest.php
@@ -205,11 +205,14 @@ class TermTest extends TaxonomyTestBase {
         ),
       ))
       ->save();
+    // Prefix the terms with a letter to ensure there is no clash in the first
+    // three letters.
+    // @see https://www.drupal.org/node/2397691
     $terms = array(
-      'term1' => $this->randomMachineName(),
-      'term2' => $this->randomMachineName(),
-      'term3' => $this->randomMachineName() . ', ' . $this->randomMachineName(),
-      'term4' => $this->randomMachineName(),
+      'term1' => 'a'. $this->randomMachineName(),
+      'term2' => 'b'. $this->randomMachineName(),
+      'term3' => 'c'. $this->randomMachineName() . ', ' . $this->randomMachineName(),
+      'term4' => 'd'. $this->randomMachineName(),
     );
 
     $edit = array();
