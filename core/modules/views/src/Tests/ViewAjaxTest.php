118abbcc6f9f2735b02e79d19226e47383ca2187
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2014-12-29 14:31:47 +0000

    Issue #2399195 by mondrake: Views setting 'Use Ajax' doesn't work

--- core/modules/views/src/Tests/ViewAjaxTest.php
+++ core/modules/views/src/Tests/ViewAjaxTest.php
@@ -35,6 +35,13 @@ class ViewAjaxTest extends ViewTestBase {
   public function testAjaxView() {
     $this->drupalGet('test_ajax_view');
 
+    $drupal_settings = $this->getDrupalSettings();
+    $this->assertTrue(isset($drupal_settings['views']['ajax_path']), 'The Ajax callback path is set in drupalSettings.');
+    $this->assertEqual(count($drupal_settings['views']['ajaxViews']), 1);
+    $view_entry = array_keys($drupal_settings['views']['ajaxViews'])[0];
+    $this->assertEqual($drupal_settings['views']['ajaxViews'][$view_entry]['view_name'], 'test_ajax_view', 'The view\'s ajaxViews array entry has the correct \'view_name\' key.');
+    $this->assertEqual($drupal_settings['views']['ajaxViews'][$view_entry]['view_display_id'], 'page_1', 'The view\'s ajaxViews array entry has the correct \'view_display_id\' key.');
+
     $data = array();
     $data['view_name'] = 'test_ajax_view';
     $data['view_display_id'] = 'test_ajax_view';
