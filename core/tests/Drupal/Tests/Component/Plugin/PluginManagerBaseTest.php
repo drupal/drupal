ba40d78ce6f873a9f2942827b4f37c60e801fa46
webchick <drupal@webchick.net>
2015-01-06 11:15:20 -0800

    Issue #2371531 by Mile23, YesCT, daffie: Expand unit testing for Drupal\Component\Plugin\PluginManagerBase

---
+++ core/tests/Drupal/Tests/Component/Plugin/PluginManagerBaseTest.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\Component\Plugin\PluginManagerBaseTest.
+ */
+
+namespace Drupal\Tests\Component\Plugin;
+
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\Component\Plugin\PluginManagerBase
+ * @group Plugin
+ */
+class PluginManagerBaseTest extends UnitTestCase {
+
+  /**
+   * A callback method for mocking FactoryInterface objects.
+   */
+  public function createInstanceCallback() {
+    $args = func_get_args();
+    $plugin_id = $args[0];
+    $configuration = $args[1];
+    if ('invalid' == $plugin_id) {
+      throw new PluginNotFoundException($plugin_id);
+    }
+    return array(
+      'plugin_id' => $plugin_id,
+      'configuration' => $configuration,
+    );
+  }
+
+  /**
+   * Generates a mocked FactoryInterface object with known properties.
+   */
+  public function getMockFactoryInterface($expects_count) {
+    $mock_factory = $this->getMockBuilder('Drupal\Component\Plugin\Factory\FactoryInterface')
+      ->setMethods(array('createInstance'))
+      ->getMockForAbstractClass();
+    $mock_factory->expects($this->exactly($expects_count))
+      ->method('createInstance')
+      ->willReturnCallback(array($this, 'createInstanceCallback'));
+    return $mock_factory;
+  }
+
+  /**
+   * Tests createInstance() with no fallback methods.
+   *
+   * @covers ::createInstance
+   */
+  public function testCreateInstance() {
+    $manager = $this->getMockBuilder('Drupal\Component\Plugin\PluginManagerBase')
+      ->getMockForAbstractClass();
+    // PluginManagerBase::createInstance() looks for a factory object and then
+    // calls createInstance() on it. So we have to mock a factory object.
+    $factory_ref = new \ReflectionProperty($manager, 'factory');
+    $factory_ref->setAccessible(TRUE);
+    $factory_ref->setValue($manager, $this->getMockFactoryInterface(1));
+
+    // Finally the test.
+    $configuration_array = array('config' => 'something');
+    $result = $manager->createInstance('valid', $configuration_array);
+    $this->assertEquals('valid', $result['plugin_id']);
+    $this->assertArrayEquals($configuration_array, $result['configuration']);
+  }
+
+  /**
+   * Tests createInstance() with a fallback method.
+   *
+   * @covers ::createInstance
+   */
+  public function testCreateInstanceFallback() {
+    // We use our special stub class which extends PluginManagerBase and also
+    // implements FallbackPluginManagerInterface.
+    $manager = new StubFallbackPluginManager();
+    // Put our stubbed factory on the base object.
+    $factory_ref = new \ReflectionProperty($manager, 'factory');
+    $factory_ref->setAccessible(TRUE);
+
+    // Set up the configuration array.
+    $configuration_array = array('config' => 'something');
+
+    // Test with fallback interface and valid plugin_id.
+    $factory_ref->setValue($manager, $this->getMockFactoryInterface(1));
+    $no_fallback_result = $manager->createInstance('valid', $configuration_array);
+    $this->assertEquals('valid', $no_fallback_result['plugin_id']);
+    $this->assertArrayEquals($configuration_array, $no_fallback_result['configuration']);
+
+    // Test with fallback interface and invalid plugin_id.
+    $factory_ref->setValue($manager, $this->getMockFactoryInterface(2));
+    $fallback_result = $manager->createInstance('invalid', $configuration_array);
+    $this->assertEquals('invalid_fallback', $fallback_result['plugin_id']);
+    $this->assertArrayEquals($configuration_array, $fallback_result['configuration']);
+  }
+
+}
---
+++ core/tests/Drupal/Tests/Component/Plugin/StubFallbackPluginManager.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\Component\Plugin\StubFallbackPluginManager.
+ */
+
+namespace Drupal\Tests\Component\Plugin;
+
+use Drupal\Component\Plugin\FallbackPluginManagerInterface;
+use Drupal\Component\Plugin\PluginManagerBase;
+
+/**
+ * Stubs \Drupal\Component\Plugin\FallbackPluginManagerInterface.
+ *
+ * We have to stub \Drupal\Component\Plugin\FallbackPluginManagerInterface for
+ * \Drupal\Tests\Component\Plugin\PluginManagerBaseTest so that we can
+ * implement ::getFallbackPluginId().
+ *
+ * We do this so we can have it just return the plugin ID passed to it, with
+ * '_fallback' appended.
+ */
+class StubFallbackPluginManager extends PluginManagerBase implements FallbackPluginManagerInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFallbackPluginId($plugin_id, array $configuration = array()) {
+    // Minimally implement getFallbackPluginId so that we can test it.
+    return $plugin_id . '_fallback';
+  }
+
+}
