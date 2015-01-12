9a2b86b27db401244ec24879ac16f555cf0e1ac9
Alex Pott <alex.a.pott@googlemail.com>
2015-01-12 14:24:42 +0000

    Issue #2402827 by kgoel: Extract ViewUnitTestBase and ViewTestBase::assertIdenti* methods into a trait

--- core/modules/views/src/Tests/ViewTestBase.php
+++ core/modules/views/src/Tests/ViewTestBase.php
@@ -23,6 +23,8 @@ use Drupal\views\ViewExecutable;
  */
 abstract class ViewTestBase extends WebTestBase {
 
+  use ViewResultAssertionTrait;
+
   /**
    * Modules to enable.
    *
@@ -66,111 +68,6 @@ abstract class ViewTestBase extends WebTestBase {
       $query->values($record);
     }
     $query->execute();
-  }
-
-  /**
-   * Verifies that a result set returned by a View matches expected values.
-   *
-   * The comparison is done on the string representation of the columns of the
-   * column map, taking the order of the rows into account, but not the order
-   * of the columns.
-   *
-   * @param \Drupal\views\ViewExecutable $view
-   *   An executed View.
-   * @param array $expected_result
-   *   An expected result set.
-   * @param array $column_map
-   *   (optional) An associative array mapping the columns of the result set
-   *   from the view (as keys) and the expected result set (as values).
-   * @param string $message
-   *   (optional) A custom message to display with the assertion. Defaults to
-   *   'Identical result set.'
-   *
-   * @return bool
-   *   TRUE if the assertion succeeded, or FALSE otherwise.
-   */
-  protected function assertIdenticalResultset(ViewExecutable $view, $expected_result, $column_map = array(), $message = 'Identical result set.') {
-    return $this->assertIdenticalResultsetHelper($view, $expected_result, $column_map, $message, 'assertIdentical');
-  }
-
-  /**
-   * Verifies that a result set returned by a View differs from certain values.
-   *
-   * Inverse of ViewsTestCase::assertIdenticalResultset().
-   *
-   * @param \Drupal\views\ViewExecutable $view
-   *   An executed View.
-   * @param array $expected_result
-   *   An expected result set.
-   * @param array $column_map
-   *   (optional) An associative array mapping the columns of the result set
-   *  from the view (as keys) and the expected result set (as values).
-   * @param string $message
-   *   (optional) A custom message to display with the assertion. Defaults to
-   *   'Non-identical result set.'
-   *
-   * @return bool
-   *   TRUE if the assertion succeeded, or FALSE otherwise.
-   */
-  protected function assertNotIdenticalResultset(ViewExecutable $view, $expected_result, $column_map = array(), $message = 'Non-identical result set.') {
-    return $this->assertIdenticalResultsetHelper($view, $expected_result, $column_map, $message, 'assertNotIdentical');
-  }
-
-  /**
-   * Performs View result assertions.
-   *
-   * This is a helper method for ViewTestBase::assertIdenticalResultset() and
-   * ViewTestBase::assertNotIdenticalResultset().
-   *
-   * @param \Drupal\views\ViewExecutable $view
-   *   An executed View.
-   * @param array $expected_result
-   *   An expected result set.
-   * @param array $column_map
-   *   An associative array mapping the columns of the result set
-   *   from the view (as keys) and the expected result set (as values).
-   * @param string $message
-   *   The message to display with the assertion.
-   * @param string $assert_method
-   *   The TestBase assertion method to use (either 'assertIdentical' or
-   *   'assertNotIdentical').
-   *
-   * @return bool
-   *   TRUE if the assertion succeeded, or FALSE otherwise.
-   *
-   * @see \Drupal\views\Tests\ViewTestBase::assertIdenticalResultset()
-   * @see \Drupal\views\Tests\ViewTestBase::assertNotIdenticalResultset()
-   */
-  protected function assertIdenticalResultsetHelper(ViewExecutable $view, $expected_result, $column_map, $message, $assert_method) {
-    // Convert $view->result to an array of arrays.
-    $result = array();
-    foreach ($view->result as $key => $value) {
-      $row = array();
-      foreach ($column_map as $view_column => $expected_column) {
-        // The comparison will be done on the string representation of the value.
-        $row[$expected_column] = (string) $value->$view_column;
-      }
-      $result[$key] = $row;
-    }
-
-    // Remove the columns we don't need from the expected result.
-    foreach ($expected_result as $key => $value) {
-      $row = array();
-      foreach ($column_map as $expected_column) {
-        // The comparison will be done on the string representation of the value.
-        $row[$expected_column] = (string) (is_object($value) ? $value->$expected_column : $value[$expected_column]);
-      }
-      $expected_result[$key] = $row;
-    }
-
-    // Reset the numbering of the arrays.
-    $result = array_values($result);
-    $expected_result = array_values($expected_result);
-
-    $this->verbose('<pre>Returned data set: ' . print_r($result, TRUE) . "\n\nExpected: ". print_r($expected_result, TRUE));
-
-    // Do the actual comparison.
-    return $this->$assert_method($result, $expected_result, $message);
   }
 
   /**
