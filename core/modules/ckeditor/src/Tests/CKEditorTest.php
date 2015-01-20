8b9b0d7e1724426dbbc43bcc6dc5a21f0006772e
Alex Pott <alex.a.pott@googlemail.com>
2015-01-20 16:37:11 +0000

    Issue #2064379 by tadityar, bneil: Remove ckeditor-iframe.css and load relevant Bartik CSS files for CKEditor's iframe mode

--- core/modules/ckeditor/src/Tests/CKEditorTest.php
+++ core/modules/ckeditor/src/Tests/CKEditorTest.php
@@ -265,7 +265,12 @@ class CKEditorTest extends KernelTestBase {
     // Enable the Bartik theme, which specifies a CKEditor stylesheet.
     \Drupal::service('theme_handler')->install(['bartik']);
     $this->config('system.theme')->set('default', 'bartik')->save();
-    $expected[] = file_create_url('core/themes/bartik/css/ckeditor-iframe.css');
+    $expected[] = file_create_url('core/themes/bartik/css/base/elements.css');
+    $expected[] = file_create_url('core/themes/bartik/css/base/typography.css');
+    $expected[] = file_create_url('core/themes/bartik/css/components/captions.css');
+    $expected[] = file_create_url('core/themes/bartik/css/components/content.css');
+    $expected[] = file_create_url('core/themes/bartik/css/components/list.css');
+    $expected[] = file_create_url('core/themes/bartik/css/components/table.css');
     $this->assertIdentical($expected, $this->ckeditor->buildContentsCssJSSetting($editor), '"contentsCss" configuration part of JS settings built correctly while a theme providing a CKEditor stylesheet exists.');
   }
 
