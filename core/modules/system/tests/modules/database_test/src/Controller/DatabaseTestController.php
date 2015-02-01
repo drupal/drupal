ed7f9d9eedbdecd1e075706c50a2e81933fd3660
Alex Pott <alex.a.pott@googlemail.com>
2015-02-01 10:01:38 +0000

    Issue #2066557 by mparker17, valthebald, chakrapani: Convert system.module's test's database_test.module's JSON functions to a new-style controller

--- core/modules/system/tests/modules/database_test/src/Controller/DatabaseTestController.php
+++ core/modules/system/tests/modules/database_test/src/Controller/DatabaseTestController.php
@@ -7,44 +7,144 @@
 
 namespace Drupal\database_test\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 /**
  * Controller routines for database_test routes.
  */
 class DatabaseTestController {
 
   /**
-   * @todo Remove database_test_db_query_temporary().
+   * Runs db_query_temporary() and outputs the table name and its number of rows.
+   *
+   * We need to test that the table created is temporary, so we run it here, in a
+   * separate menu callback request; After this request is done, the temporary
+   * table should automatically dropped.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function dbQueryTemporary() {
-    return database_test_db_query_temporary();
+    $table_name = db_query_temporary('SELECT age FROM {test}', array());
+    return new JsonResponse(array(
+      'table_name' => $table_name,
+      'row_count' => db_select($table_name)->countQuery()->execute()->fetchField(),
+    ));
   }
 
   /**
-   * @todo Remove database_test_even_pager_query().
+   * Runs a pager query and returns the results.
+   *
+   * This function does care about the page GET parameter, as set by the
+   * simpletest HTTP call.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function pagerQueryEven($limit) {
-    return database_test_even_pager_query($limit);
+    $query = db_select('test', 't');
+    $query
+      ->fields('t', array('name'))
+      ->orderBy('age');
+
+    // This should result in 2 pages of results.
+    $query = $query
+      ->extend('Drupal\Core\Database\Query\PagerSelectExtender')
+      ->limit($limit);
+
+    $names = $query->execute()->fetchCol();
+
+    return new JsonResponse(array(
+      'names' => $names,
+    ));
   }
 
   /**
-   * @todo Remove database_test_odd_pager_query().
+   * Runs a pager query and returns the results.
+   *
+   * This function does care about the page GET parameter, as set by the
+   * simpletest HTTP call.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function pagerQueryOdd($limit) {
-    return database_test_odd_pager_query($limit);
+    $query = db_select('test_task', 't');
+    $query
+      ->fields('t', array('task'))
+      ->orderBy('pid');
+
+    // This should result in 4 pages of results.
+    $query = $query
+      ->extend('Drupal\Core\Database\Query\PagerSelectExtender')
+      ->limit($limit);
+
+    $names = $query->execute()->fetchCol();
+
+    return new JsonResponse(array(
+      'names' => $names,
+    ));
   }
 
   /**
-   * @todo Remove database_test_tablesort().
+   * Runs a tablesort query and returns the results.
+   *
+   * This function does care about the page GET parameter, as set by the
+   * simpletest HTTP call.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function testTablesort() {
-    return database_test_tablesort();
+    $header = array(
+      'tid' => array('data' => t('Task ID'), 'field' => 'tid', 'sort' => 'desc'),
+      'pid' => array('data' => t('Person ID'), 'field' => 'pid'),
+      'task' => array('data' => t('Task'), 'field' => 'task'),
+      'priority' => array('data' => t('Priority'), 'field' => 'priority', ),
+    );
+
+    $query = db_select('test_task', 't');
+    $query
+      ->fields('t', array('tid', 'pid', 'task', 'priority'));
+
+    $query = $query
+      ->extend('Drupal\Core\Database\Query\TableSortExtender')
+      ->orderByHeader($header);
+
+    // We need all the results at once to check the sort.
+    $tasks = $query->execute()->fetchAll();
+
+    return new JsonResponse(array(
+      'tasks' => $tasks,
+    ));
   }
 
   /**
-   * @todo Remove database_test_tablesort_first().
+   * Runs a tablesort query with a second order_by after and returns the results.
+   *
+   * This function does care about the page GET parameter, as set by the
+   * simpletest HTTP call.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function testTablesortFirst() {
-    return database_test_tablesort_first();
-  }
+    $header = array(
+      'tid' => array('data' => t('Task ID'), 'field' => 'tid', 'sort' => 'desc'),
+      'pid' => array('data' => t('Person ID'), 'field' => 'pid'),
+      'task' => array('data' => t('Task'), 'field' => 'task'),
+      'priority' => array('data' => t('Priority'), 'field' => 'priority', ),
+    );
 
+    $query = db_select('test_task', 't');
+    $query
+      ->fields('t', array('tid', 'pid', 'task', 'priority'));
+
+    $query = $query
+      ->extend('Drupal\Core\Database\Query\TableSortExtender')
+      ->orderByHeader($header)
+      ->orderBy('priority');
+
+    // We need all the results at once to check the sort.
+    $tasks = $query->execute()->fetchAll();
+
+    return new JsonResponse(array(
+      'tasks' => $tasks,
+    ));
+  }
 }
