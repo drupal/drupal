5923e580695d0f25cd630a549e2e47ce75fcbc3a
Alex Pott <alex.a.pott@googlemail.com>
2015-02-02 13:36:19 +0000

    Issue #2415511 by eojthebrave: Add @Event documentation to all RenderEvents

--- core/lib/Drupal/Core/Render/RenderEvents.php
+++ core/lib/Drupal/Core/Render/RenderEvents.php
@@ -15,7 +15,15 @@ final class RenderEvents {
   /**
    * Name of the event when selecting a page display variant to use.
    *
+   * This event allows you to select a different page display variant to use
+   * when rendering a page. The event listener method receives a
+   * \Drupal\Core\Render\PageDisplayVariantSelectionEvent instance.
+   *
+   * @Event
+   *
    * @see \Drupal\Core\Render\PageDisplayVariantSelectionEvent
+   * @see \Drupal\Core\Render\MainContent\HtmlRenderer
+   * @see \Drupal\block\EventSubscriber\BlockPageDisplayVariantSubscriber
    */
   const SELECT_PAGE_DISPLAY_VARIANT = 'render.page_display_variant.select';
 
