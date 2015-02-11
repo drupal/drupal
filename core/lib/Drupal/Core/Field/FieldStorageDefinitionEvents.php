53ca7d6a94c68876319f4a8ff8d55d79d8469e1f
webchick <drupal@webchick.net>
2015-02-11 14:50:57 -0800

    Issue #2415507 by eojthebrave: Add @Event documentation to all FieldStorageDefinitionEvents

--- core/lib/Drupal/Core/Field/FieldStorageDefinitionEvents.php
+++ core/lib/Drupal/Core/Field/FieldStorageDefinitionEvents.php
@@ -13,21 +13,51 @@ namespace Drupal\Core\Field;
 final class FieldStorageDefinitionEvents {
 
   /**
-   * Event name for field storage definition creation.
+   * Name of the event triggered for field storage definition creation.
+   *
+   * This event allows you to respond to the creation of a new field storage
+   * definition. The event listener method receives a
+   * \Drupal\Core\Field\FieldStorageDefinitionEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Field\FieldStorageDefinitionEvent
+   * @see \Drupal\Core\Entity\EntityManager::onFieldStorageDefinitionCreate()
+   * @see \Drupal\Core\Field\FieldStorageDefinitionEventSubscriberTrait
    *
    * @var string
    */
   const CREATE = 'field_storage.definition.create';
 
   /**
-   * Event name for field storage definition update.
+   * Name of the event triggered for field storage definition update.
+   *
+   * This event allows you to respond anytime a field storage definition is
+   * updated. The event listener method receives a
+   * \Drupal\Core\Field\FieldStorageDefinitionEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Field\FieldStorageDefinitionEvent
+   * @see \Drupal\Core\Entity\EntityManager::onFieldStorageDefinitionUpdate()
+   * @see \Drupal\Core\Field\FieldStorageDefinitionEventSubscriberTrait
    *
    * @var string
    */
   const UPDATE = 'field_storage.definition.update';
 
   /**
-   * Event name for field storage definition deletion.
+   * Name of the event triggered for field storage definition deletion.
+   *
+   * This event allows you to respond anytime a field storage definition is
+   * deleted. The event listener method receives a
+   * \Drupal\Core\Field\FieldStorageDefinitionEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Field\FieldStorageDefinitionEvent
+   * @see \Drupal\Core\Entity\EntityManager::onFieldStorageDefinitionDelete()
+   * @see \Drupal\Core\Field\FieldStorageDefinitionEventSubscriberTrait
    *
    * @var string
    */
