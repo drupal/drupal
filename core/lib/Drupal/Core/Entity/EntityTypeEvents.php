c6036a58c4ad713ad5cff49cb04a4caf55b2764f
webchick <drupal@webchick.net>
2015-02-13 12:48:04 -0800

    Issue #2425379 by eojthebrave: Add @Event documentation to all EntityTypeEvents

--- core/lib/Drupal/Core/Entity/EntityTypeEvents.php
+++ core/lib/Drupal/Core/Entity/EntityTypeEvents.php
@@ -13,21 +13,54 @@ namespace Drupal\Core\Entity;
 final class EntityTypeEvents {
 
   /**
-   * Event name for entity type creation.
+   * The name of the event triggered when a new entity type is created.
+   *
+   * This event allows modules to react to a new entity type being created. The
+   * event listener method receives a \Drupal\Core\Entity\EntityTypeEvent
+   * instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Entity\EntityTypeEvent
+   * @see \Drupal\Core\Entity\EntityManager::onEntityTypeCreate()
+   * @see \Drupal\Core\Entity\EntityTypeEventSubscriberTrait
+   * @see \Drupal\views\EventSubscriber\ViewsEntitySchemaSubscriber::onEntityTypeCreate()
    *
    * @var string
    */
   const CREATE = 'entity_type.definition.create';
 
   /**
-   * Event name for entity type update.
+   * The name of the event triggered when an existing entity type is updated.
+   *
+   * This event allows modules to react whenever an existing entity type is
+   * updated. The event listener method receives a
+   * \Drupal\Core\Entity\EntityTypeEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Entity\EntityTypeEvent
+   * @see \Drupal\Core\Entity\EntityManager::onEntityTypeUpdate()
+   * @see \Drupal\Core\Entity\EntityTypeEventSubscriberTrait
+   * @see \Drupal\views\EventSubscriber\ViewsEntitySchemaSubscriber::onEntityTypeUpdate()
    *
    * @var string
    */
   const UPDATE = 'entity_type.definition.update';
 
   /**
-   * Event name for entity type deletion.
+   * The name of the event triggered when an existing entity type is deleted.
+   *
+   * This event allows modules to react whenever an existing entity type is
+   * deleted.  The event listener method receives a
+   * \Drupal\Core\Entity\EntityTypeEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Entity\EntityTypeEvent
+   * @see \Drupal\Core\Entity\EntityManager::onEntityTypeDelete()
+   * @see \Drupal\Core\Entity\EntityTypeEventSubscriberTrait
+   * @see \Drupal\views\EventSubscriber\ViewsEntitySchemaSubscriber::onEntityTypeDelete()
    *
    * @var string
    */
