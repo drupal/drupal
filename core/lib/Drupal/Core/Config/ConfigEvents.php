370e15e62d2650eabaa4ad5279b22ef389f7c57f
Alex Pott <alex.a.pott@googlemail.com>
2015-02-16 15:52:16 +0000

    Issue #2415505 by eojthebrave: Add @Event documentation to all ConfigEvents

--- core/lib/Drupal/Core/Config/ConfigEvents.php
+++ core/lib/Drupal/Core/Config/ConfigEvents.php
@@ -15,48 +15,104 @@ namespace Drupal\Core\Config;
 final class ConfigEvents {
 
   /**
-   * Name of event fired when saving the configuration object.
+   * Name of the event fired when saving a configuration object.
    *
+   * This event allows modules to perform an action whenever a configuration
+   * object is saved. The event listener method receives a
+   * \Drupal\Core\Config\ConfigCrudEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Config\ConfigCrudEvent
    * @see \Drupal\Core\Config\Config::save()
    * @see \Drupal\Core\Config\ConfigFactory::onConfigSave()
+   *
+   * @var string
    */
   const SAVE = 'config.save';
 
   /**
-   * Name of event fired when deleting the configuration object.
+   * Name of the event fired when deleting a configuration object.
    *
+   * This event allows modules to perform an action whenever a configuration
+   * object is deleted. The event listener method receives a
+   * \Drupal\Core\Config\ConfigCrudEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Config\ConfigCrudEvent
    * @see \Drupal\Core\Config\Config::delete()
+   * @see \Drupal\Core\Config\ConfigFactory::onConfigDelete()
+   *
+   * @var string
    */
   const DELETE = 'config.delete';
 
   /**
-   * Name of event fired when renaming a configuration object.
+   * Name of the event fired when renaming a configuration object.
    *
+   * This event allows modules to perform an action whenever a configuration
+   * object's name is changed. The event listener method receives a
+   * \Drupal\Core\Config\ConfigRenameEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Config\ConfigRenameEvent
    * @see \Drupal\Core\Config\ConfigFactoryInterface::rename().
+   *
+   * @var string
    */
   const RENAME = 'config.rename';
 
   /**
-   * Name of event fired when validating in the configuration import process.
+   * Name of the event fired when validating imported configuration.
    *
+   * This event allows modules to perform additional validation operations when
+   * configuration is being imported. The event listener method receives a
+   * \Drupal\Core\Config\ConfigImporterEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Config\ConfigImporterEvent
    * @see \Drupal\Core\Config\ConfigImporter::validate().
    * @see \Drupal\Core\EventSubscriber\ConfigImportSubscriber::onConfigImporterValidate().
+   *
+   * @var string
    */
   const IMPORT_VALIDATE = 'config.importer.validate';
 
   /**
-   * Name of event fired when when importing configuration to target storage.
+   * Name of the event fired when importing configuration to target storage.
    *
+   * This event allows modules to perform additional actions when configuration
+   * is imported. The event listener method receives a
+   * \Drupal\Core\Config\ConfigImporterEvent instance.
+   *
+   * @Event
+   *
+   * @see \Drupal\Core\Config\ConfigImporterEvent
    * @see \Drupal\Core\Config\ConfigImporter::import().
    * @see \Drupal\Core\EventSubscriber\ConfigSnapshotSubscriber::onConfigImporterImport().
+   *
+   * @var string
    */
   const IMPORT = 'config.importer.import';
 
   /**
-   * Name of event fired to collect information on all collections.
+   * Name of event fired to collect information on all config collections.
    *
-   * @see \Drupal\Core\Config\ConfigManager::getConfigCollectionInfo()
+   * This event allows modules to add to the list of configuration collections
+   * retrieved by \Drupal\Core\Config\ConfigManager::getConfigCollectionInfo().
+   * The event listener method receives a
+   * \Drupal\Core\Config\ConfigCollectionInfo instance.
+   *
+   * @Event
+   *
    * @see \Drupal\Core\Config\ConfigCollectionInfo
+   * @see \Drupal\Core\Config\ConfigManager::getConfigCollectionInfo()
+   * @see \Drupal\Core\Config\ConfigFactoryOverrideBase
+   *
+   * @var string
    */
   const COLLECTION_INFO = 'config.collection_info';
 
