bf48d49846f47836190e0f210c16fd56168f9185
Alex Pott <alex.a.pott@googlemail.com>
2015-02-18 12:56:07 +0000

    Revert "Issue #2427875 by daffie: SQLite Insert query does not account for INSERT FROM ... SELECT"
    This reverts commit d1a9a099e26aabbb7ef51b58763ddbffb29b090f.

--- core/lib/Drupal/Core/Database/Driver/sqlite/Insert.php
+++ core/lib/Drupal/Core/Database/Driver/sqlite/Insert.php
@@ -22,7 +22,7 @@ class Insert extends QueryInsert {
     if (!$this->preExecute()) {
       return NULL;
     }
-    if (count($this->insertFields) || !empty($this->fromQuery)) {
+    if (count($this->insertFields)) {
       return parent::execute();
     }
     else {
@@ -35,10 +35,7 @@ class Insert extends QueryInsert {
     $comments = $this->connection->makeComment($this->comments);
 
     // Produce as many generic placeholders as necessary.
-    $placeholders = array();
-    if (!empty($this->insertFields)) {
-      $placeholders = array_fill(0, count($this->insertFields), '?');
-    }
+    $placeholders = array_fill(0, count($this->insertFields), '?');
 
     // If we're selecting from a SelectQuery, finish building the query and
     // pass it back, as any remaining options are irrelevant.
