de0c4fb8540bc0cc5dfe674c0a7bbcb6abf3964e
Alex Pott <alex.a.pott@googlemail.com>
2015-02-24 21:56:18 +0000

    Revert "Issue #2433009 by hussainweb, grom358, daffie, larowlan, alexpott, pcambra, benjy, jibran, phenaproxima, moshe weitzman, nick_schuch: Add Mink, with Goutte driver, to core"
    This reverts commit 2b96082da5ee869741691e2fee24037152d9ee0c.

--- core/vendor/behat/mink/driver-testsuite/web-fixtures/sub-folder/cookie_page1.php
+++
@@ -1,4 +0,0 @@
-<?php
-$cookieAtRootPath = false;
-$cookieValue = 'srv_var_is_set_sub_folder';
-require_once __DIR__ . '/../' . basename(__FILE__);
--- core/vendor/composer/autoload_psr4.php
+++ core/vendor/composer/autoload_psr4.php
@@ -14,5 +14,4 @@ return array(
     'Drupal\\Driver\\' => array($baseDir . '/../drivers/lib/Drupal/Driver'),
     'Drupal\\Core\\' => array($baseDir . '/lib/Drupal/Core'),
     'Drupal\\Component\\' => array($baseDir . '/lib/Drupal/Component'),
-    'Behat\\Mink\\' => array($vendorDir . '/behat/mink/src'),
 );
