35f1560690e738da9dfcec7caf3ab4dc04332a60
Alex Pott <alex.a.pott@googlemail.com>
2015-03-01 12:58:39 +0000

    Issue #2017471 by rodrigoaguilera, balagan, clemens.tolboom, Kristen Pol, Outi, rpayanm, danylevskyi, mon_franco, Antti J. Salminen, aloyr, thlafon, Marc Hannaford, kreatIL, oussema, David Hernández: Multilingual tour for language section

---
+++ core/modules/language/src/Tests/LanguageTourTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\language\Tests\LanguageTourTest.
+ */
+
+namespace Drupal\language\Tests;
+
+use Drupal\tour\Tests\TourTestBase;
+
+/**
+ * Tests tour functionality.
+ *
+ * @group tour
+ */
+class LanguageTourTest extends TourTestBase {
+
+  /**
+   * An admin user with administrative permissions for views.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $adminUser;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('language', 'tour');
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->adminUser = $this->drupalCreateUser(array('administer languages', 'access tour'));
+    $this->drupalLogin($this->adminUser);
+  }
+
+  /**
+   * Tests language tour tip availability.
+   */
+  public function testLanguageTour() {
+    $this->drupalGet('admin/config/regional/language');
+    $this->assertTourTips();
+  }
+
+  /**
+   * Go to add language page and check the tour tooltips.
+   */
+  public function testLanguageAddTour() {
+    $this->drupalGet('admin/config/regional/language/add');
+    $this->assertTourTips();
+  }
+
+  /**
+   * Go to edit language page and check the tour tooltips.
+   */
+  public function testLanguageEditTour() {
+    $this->drupalGet('admin/config/regional/language/edit/en');
+    $this->assertTourTips();
+  }
+
+}
