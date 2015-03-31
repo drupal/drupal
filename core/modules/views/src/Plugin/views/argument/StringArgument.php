c127131f4c9e1b995c70fef0a89be27248512d9e
Alex Pott <alex.a.pott@googlemail.com>
2015-03-31 16:30:19 +0100

    Issue #2462175 by amateescu: SQLite: Fix case sensitivity in Views' string argument plugin

--- core/modules/views/src/Plugin/views/argument/StringArgument.php
+++ core/modules/views/src/Plugin/views/argument/StringArgument.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\views\Plugin\views\argument;
 
+use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\ViewExecutable;
 use Drupal\views\Plugin\views\display\DisplayPluginBase;
@@ -176,7 +177,15 @@ class StringArgument extends ArgumentPluginBase {
    * $this->ensureMyTable() MUST have been called prior to this.
    */
   public function getFormula() {
-    return "SUBSTRING($this->tableAlias.$this->realField, 1, " . intval($this->options['limit']) . ")";
+    $formula = "SUBSTRING($this->tableAlias.$this->realField, 1, " . intval($this->options['limit']) . ")";
+
+    // Support case-insensitive substring comparisons for SQLite by using the
+    // 'NOCASE_UTF8' collation.
+    // @see Drupal\Core\Database\Driver\sqlite\Connection::open()
+    if (Database::getConnection()->databaseType() == 'sqlite' && $this->options['case'] != 'none') {
+      $formula .= ' COLLATE NOCASE_UTF8';
+    }
+    return $formula;
   }
 
   /**
