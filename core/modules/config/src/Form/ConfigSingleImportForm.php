2745b6127f45be1b4ec542309dc5f23e9b14f4a7
Alex Pott <alex.a.pott@googlemail.com>
2015-03-31 16:11:48 +0100

    Issue #2462289 by TravisCarden: Grammar error in config single import prompt

--- core/modules/config/src/Form/ConfigSingleImportForm.php
+++ core/modules/config/src/Form/ConfigSingleImportForm.php
@@ -107,7 +107,7 @@ class ConfigSingleImportForm extends ConfirmFormBase {
       $question = $this->t('Are you sure you want to update the %name @type?', $args);
     }
     else {
-      $question = $this->t('Are you sure you want to create new %name @type?', $args);
+      $question = $this->t('Are you sure you want to create a new %name @type?', $args);
     }
     return $question;
   }
