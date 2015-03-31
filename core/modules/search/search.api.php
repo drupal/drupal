f4a9d47a5d6cda8ce5d2abafcd2e98184023a7d9
Alex Pott <alex.a.pott@googlemail.com>
2015-03-31 15:42:56 +0100

    Issue #2459325 by jhodgdon: Document that language is not set on search keyword preprocessing

--- core/modules/search/search.api.php
+++ core/modules/search/search.api.php
@@ -14,7 +14,8 @@
  * Preprocess text for search.
  *
  * This hook is called to preprocess both the text added to the search index
- * and the keywords users have submitted for searching.
+ * and the keywords users have submitted for searching. The same processing
+ * needs to be applied to both so that searches will find matches.
  *
  * Possible uses:
  * - Adding spaces between words of Chinese or Japanese text.
@@ -22,14 +23,23 @@
  *   instance, walk, walked, walking, and walks in searching.
  * - Expanding abbreviations and acronymns that occur in text.
  *
- * @param $text
+ * @param string $text
  *   The text to preprocess. This is a single piece of plain text extracted
  *   from between two HTML tags or from the search query. It will not contain
  *   any HTML entities or HTML tags.
- * @param $langcode
- *   The language code of the entity that has been found.
+ * @param string|null $langcode
+ *   The language code for the language the text is in, if known. When this hook
+ *   is invoked during search indexing, the language will most likely be known
+ *   and passed in. This is left up to the search plugin;
+ *   \Drupal\node\Plugin\Search\NodeSearch does pass in the node
+ *   language. However, when this hook is invoked during searching, in order to
+ *   let a module apply the same preprocessing to the search keywords and
+ *   indexed text so they will match, $langcode will be NULL. A hook
+ *   implementation can call the getCurrentLanguage() method on the
+ *   'language_manager' service to determine the current language and act
+ *   accordingly.
  *
- * @return
+ * @return string
  *   The text after preprocessing. Note that if your module decides not to
  *   alter the text, it should return the original text. Also, after
  *   preprocessing, words in the text should be separated by a space.
@@ -37,9 +47,14 @@
  * @ingroup search
  */
 function hook_search_preprocess($text, $langcode = NULL) {
+  // If the language is not set, get it from the language manager.
+  if (!isset($langcode)) {
+    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+  }
+
   // If the langcode is set to 'en' then add variations of the word "testing"
   // which can also be found during English language searches.
-  if (isset($langcode) && $langcode == 'en') {
+  if ($langcode == 'en') {
     // Add the alternate verb forms for the word "testing".
     if ($text == 'we are testing') {
       $text .= ' test tested';
