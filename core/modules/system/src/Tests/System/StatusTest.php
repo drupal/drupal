6435e2ee5d5f0480ac483de4b7cbeb48d114d672
Alex Pott <alex.a.pott@googlemail.com>
2015-04-08 22:06:00 +0100

    Issue #2464605 by willzyx, Dom.: Regression: no link to admin/reports/status/php

--- core/modules/system/src/Tests/System/StatusTest.php
+++ core/modules/system/src/Tests/System/StatusTest.php
@@ -7,7 +7,9 @@
 
 namespace Drupal\system\Tests\System;
 
+use Drupal\Core\Url;
 use Drupal\simpletest\WebTestBase;
+use Drupal\system\SystemRequirements;
 
 /**
  * Tests output on the status overview page.
@@ -35,6 +37,28 @@ class StatusTest extends WebTestBase {
     // Go to Administration.
     $this->drupalGet('admin/reports/status');
     $this->assertResponse(200, 'The status page is reachable.');
+
+    $phpversion = phpversion();
+    $this->assertText($phpversion, 'Php version is shown on the page.');
+
+    // Checks if the suggestion to update to php 5.5.21 or 5.6.5 for disabling
+    // multiple statements is present when necessary.
+    if (\Drupal::database()->driver() === 'mysql' && !SystemRequirements::phpVersionWithPdoDisallowMultipleStatements($phpversion)) {
+      $this->assertText(t('PHP (multiple statement disabling)'));
+    }
+    else {
+      $this->assertNoText(t('PHP (multiple statement disabling)'));
+    }
+
+    if (function_exists('phpinfo')) {
+      $this->assertLinkByHref(Url::fromRoute('system.php')->toString());
+    }
+    else {
+      $this->assertNoLinkByHref(Url::fromRoute('system.php')->toString());
+    }
+
+    $this->drupalGet('admin/reports/status/php');
+    $this->assertResponse(200, 'The phpinfo page is reachable.');
   }
 
 }
