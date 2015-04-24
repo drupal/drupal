e39a32842072bfbaa2b15c5284625ff63ebc4a08
xjm <xjm@65776.no-reply.drupal.org>
2015-04-23 20:47:50 -0500

    Issue #2121863 by googletorp, drubb, jhodgdon: There is no FileTransferInterface

--- core/lib/Drupal/Core/Updater/Updater.php
+++ core/lib/Drupal/Core/Updater/Updater.php
@@ -9,7 +9,7 @@ namespace Drupal\Core\Updater;
 
 use Drupal\Component\Utility\Unicode;
 use Drupal\Core\FileTransfer\FileTransferException;
-use Drupal\Core\FileTransfer\FileTransferInterface;
+use Drupal\Core\FileTransfer\FileTransfer;
 
 /**
  * Defines the base class for Updaters used in Drupal.
@@ -167,7 +167,7 @@ class Updater {
   /**
    * Updates a Drupal project and returns a list of next actions.
    *
-   * @param \Drupal\Core\FileTransfer\FileTransferInterface $filetransfer
+   * @param \Drupal\Core\FileTransfer\FileTransfer $filetransfer
    *   Object that is a child of FileTransfer. Used for moving files
    *   to the server.
    * @param array $overrides
@@ -226,7 +226,7 @@ class Updater {
   /**
    * Installs a Drupal project, returns a list of next actions.
    *
-   * @param \Drupal\Core\FileTransfer\FileTransferInterface $filetransfer
+   * @param \Drupal\Core\FileTransfer\FileTransfer $filetransfer
    *   Object that is a child of FileTransfer.
    * @param array $overrides
    *   An array of settings to override defaults; see self::getInstallArgs().
@@ -264,7 +264,7 @@ class Updater {
   /**
    * Makes sure the installation parent directory exists and is writable.
    *
-   * @param \Drupal\Core\FileTransfer\FileTransferInterface $filetransfer
+   * @param \Drupal\Core\FileTransfer\FileTransfer $filetransfer
    *   Object which is a child of FileTransfer.
    * @param string $directory
    *   The installation directory to prepare.
@@ -309,7 +309,7 @@ class Updater {
   /**
    * Ensures that a given directory is world readable.
    *
-   * @param \Drupal\Core\FileTransfer\FileTransferInterface $filetransfer
+   * @param \Drupal\Core\FileTransfer\FileTransfer $filetransfer
    *   Object which is a child of FileTransfer.
    * @param string $path
    *   The file path to make world readable.
@@ -327,16 +327,16 @@ class Updater {
   /**
    * Performs a backup.
    *
-   * @param \Drupal\Core\FileTransfer\FileTransferInterface $filetransfer
+   * @param \Drupal\Core\FileTransfer\FileTransfer $filetransfer
    *   Object which is a child of FileTransfer.
    * @param string $from
    *   The file path to copy from.
    * @param string $to
    *   The file path to copy to.
    *
-   * @todo Not implemented.
+   * @todo Not implemented: https://www.drupal.org/node/2474355
    */
-  public function makeBackup(FileTransferInterface $filetransfer, $from, $to) {
+  public function makeBackup(FileTransfer $filetransfer, $from, $to) {
   }
 
   /**
--- core/lib/Drupal/Core/Updater/UpdaterFileTransferException.php
+++ core/lib/Drupal/Core/Updater/UpdaterFileTransferException.php
@@ -9,10 +9,10 @@ namespace Drupal\Core\Updater;
 
 /**
  * Defines a child class of Drupal\Core\Updater\UpdaterException that indicates
- * a Drupal\Core\FileTransfer\FileTransferInterface exception.
+ * a Drupal\Core\FileTransfer\FileTransfer exception.
  *
- * We have to catch Drupal\Core\FileTransfer\FileTransferInterface exceptions
- * and wrap those in t(), since Drupal\Core\FileTransfer\FileTransferInterface
+ * We have to catch Drupal\Core\FileTransfer\FileTransfer exceptions
+ * and wrap those in t(), since Drupal\Core\FileTransfer\FileTransfer
  * is so low-level that it doesn't use any Drupal APIs and none of the strings
  * are translated.
  */
