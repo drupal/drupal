fc0c278b3fde1ce1151d4b1a47a7d676b6d30add
webchick <drupal@webchick.net>
2015-05-02 15:59:16 -0700

    Issue #2476225 by pixel5, cilefen, disasm, jhodgdon, TravisCarden: Doxygen on SuspendQueueException mentions non-existent callback_queue_worker()

--- core/lib/Drupal/Core/Queue/SuspendQueueException.php
+++ core/lib/Drupal/Core/Queue/SuspendQueueException.php
@@ -10,11 +10,11 @@ namespace Drupal\Core\Queue;
 /**
  * Exception class to throw to indicate that a cron queue should be skipped.
  *
- * An implementation of callback_queue_worker() may throw this class of
- * exception to indicate that processing of the whole queue should be skipped.
- * This should be thrown rather than a normal Exception if the problem
- * encountered by the queue worker is such that it can be deduced that workers
- * of subsequent items would encounter it too. For example, if a remote site
- * that the queue worker depends on appears to be inaccessible.
+ * An implementation of \Drupal\Core\Queue\QueueWorkerInterface::processItem()
+ * throws this class of exception to indicate that processing of the whole queue
+ * should be skipped. This should be thrown rather than a normal Exception if
+ * the problem encountered by the queue worker is such that it can be deduced
+ * that workers of subsequent items would encounter it too. For example, if a
+ * remote site that the queue worker depends on appears to be inaccessible.
  */
 class SuspendQueueException extends \RuntimeException {}
