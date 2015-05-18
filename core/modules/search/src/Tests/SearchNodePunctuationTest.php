9ac248bff26d9ec1b7fde1d630cd1a22d840ed8e
Alex Pott <alex.a.pott@googlemail.com>
2015-05-18 13:52:57 +0100

    Issue #2060553 by jhodgdon, manningpete: Search result snippet displayes HTML entities

--- core/modules/search/src/Tests/SearchNodePunctuationTest.php
+++ core/modules/search/src/Tests/SearchNodePunctuationTest.php
@@ -8,7 +8,7 @@
 namespace Drupal\search\Tests;
 
 /**
- * Tests search functionality with punctuation.
+ * Tests search functionality with punctuation and HTML entities.
  *
  * @group search
  */
@@ -31,10 +31,11 @@ class SearchNodePunctuationTest extends SearchTestBase {
   }
 
   /**
-   * Tests that search returns results with punctuation in the search phrase.
+   * Tests that search works with punctuation and HTML entities.
    */
   function testPhraseSearchPunctuation() {
     $node = $this->drupalCreateNode(array('body' => array(array('value' => "The bunny's ears were fluffy."))));
+    $node2 = $this->drupalCreateNode(array('body' => array(array('value' => 'Dignissim Aliquam &amp; Quieligo meus natu quae quia te. Damnum&copy; erat&mdash; neo pneum. Facilisi feugiat ibidem ratis.'))));
 
     // Update the search index.
     $this->container->get('plugin.manager.search')->createInstance('node_search')->updateIndex();
@@ -51,5 +52,16 @@ class SearchNodePunctuationTest extends SearchTestBase {
     // Check if the author is linked correctly to the user profile page.
     $username = $node->getOwner()->getUsername();
     $this->assertLink($username);
+
+    // Search for "&" and verify entities are not broken up in the output.
+    $edit = array('keys' => '&');
+    $this->drupalPostForm('search/node', $edit, t('Search'));
+    $this->assertNoRaw('<strong>&</strong>amp;');
+    $this->assertText('You must include at least one positive keyword');
+
+    $edit = array('keys' => '&amp;');
+    $this->drupalPostForm('search/node', $edit, t('Search'));
+    $this->assertNoRaw('<strong>&</strong>amp;');
+    $this->assertText('You must include at least one positive keyword');
   }
 }
