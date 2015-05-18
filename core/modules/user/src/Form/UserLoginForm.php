9f403fbe7d55586730b23955a2f426aa0b3ec03c
Alex Pott <alex.a.pott@googlemail.com>
2015-05-18 14:34:43 +0100

    Issue #2487808 by JesseSturgis, Scott Weston: autocorrect="off" and autocapitalize="off" in login form are deprecated and may be not enough

--- core/modules/user/src/Form/UserLoginForm.php
+++ core/modules/user/src/Form/UserLoginForm.php
@@ -102,8 +102,8 @@ class UserLoginForm extends FormBase {
       '#description' => $this->t('Enter your @s username.', array('@s' => $config->get('name'))),
       '#required' => TRUE,
       '#attributes' => array(
-        'autocorrect' => 'off',
-        'autocapitalize' => 'off',
+        'autocorrect' => 'none',
+        'autocapitalize' => 'none',
         'spellcheck' => 'false',
         'autofocus' => 'autofocus',
       ),
