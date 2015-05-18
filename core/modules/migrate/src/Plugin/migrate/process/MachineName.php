f52cc182358fb786c25e0d6bf02bf6771dceb969
Alex Pott <alex.a.pott@googlemail.com>
2015-05-18 06:13:01 +0100

    Issue #2488126 by phenaproxima: MachineName process plugin should use injected transliteration service

--- core/modules/migrate/src/Plugin/migrate/process/MachineName.php
+++ core/modules/migrate/src/Plugin/migrate/process/MachineName.php
@@ -7,10 +7,13 @@
 
 namespace Drupal\migrate\Plugin\migrate\process;
 
+use Drupal\Component\Transliteration\TransliterationInterface;
 use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Row;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * This plugin creates a machine name.
@@ -23,34 +26,50 @@ use Drupal\migrate\Row;
  *   id = "machine_name"
  * )
  */
-class MachineName extends ProcessPluginBase {
+class MachineName extends ProcessPluginBase implements ContainerFactoryPluginInterface {
 
   /**
-   * @var \Drupal\Core\Transliteration\PhpTransliteration
+   * @var \Drupal\Component\Transliteration\TransliterationInterface
    */
   protected $transliteration;
+
+  /**
+   * Constructs a MachineName plugin.
+   *
+   * @param array $configuration
+   *  The plugin configuration.
+   * @param string $plugin_id
+   *  The plugin ID.
+   * @param mixed $plugin_definition
+   *  The plugin definition.
+   * @param \Drupal\Component\Transliteration\TransliterationInterface $transliteration
+   *  The transliteration service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, TransliterationInterface $transliteration) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->transliteration = $transliteration;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('transliteration')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $new_value = $this->getTransliteration()->transliterate($value, LanguageInterface::LANGCODE_DEFAULT, '_');
+    $new_value = $this->transliteration->transliterate($value, LanguageInterface::LANGCODE_DEFAULT, '_');
     $new_value = strtolower($new_value);
     $new_value = preg_replace('/[^a-z0-9_]+/', '_', $new_value);
     return preg_replace('/_+/', '_', $new_value);
-  }
-
-  /**
-   * Get the transliteration object.
-   *
-   * @return \Drupal\Core\Transliteration\PhpTransliteration
-   *   The transliteration object.
-   */
-  protected function getTransliteration() {
-    if (!isset($this->transliteration)) {
-      $this->transliteration = \Drupal::transliteration();
-    }
-    return $this->transliteration;
   }
 
 }
--- core/modules/migrate/tests/src/Unit/process/MachineNameTest.php
+++ core/modules/migrate/tests/src/Unit/process/MachineNameTest.php
@@ -6,7 +6,7 @@
 
 namespace Drupal\Tests\migrate\Unit\process;
 
-use Drupal\migrate\Plugin\migrate\process\TestMachineName;
+use Drupal\migrate\Plugin\migrate\process\MachineName;
 
 /**
  * Tests the machine name process plugin.
@@ -57,20 +57,9 @@ class MachineNameTest extends MigrateProcessTestCase {
       ->with($human_name)
       ->will($this->returnValue($human_name_ascii . 'aeo'));
 
-    $plugin = new TestMachineName(array(), 'machine_name', array());
-    $plugin->setTransliteration($this->transliteration);
+    $plugin = new MachineName(array(), 'machine_name', array(), $this->transliteration);
     $value = $plugin->transform($human_name, $this->migrateExecutable, $this->row, 'destinationproperty');
     $this->assertEquals($expected_result, $value);
   }
 
-}
-
-namespace Drupal\migrate\Plugin\migrate\process;
-
-use Drupal\Component\Transliteration\TransliterationInterface;
-
-class TestMachineName extends MachineName {
-  public function setTransliteration(TransliterationInterface $transliteration) {
-    $this->transliteration = $transliteration;
-  }
 }
