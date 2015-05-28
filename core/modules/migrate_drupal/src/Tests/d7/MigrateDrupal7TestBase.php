27acbbbc739860a5b654f956e17ae36eb516f3f6
webchick <drupal@webchick.net>
2015-05-27 23:00:31 -0700

    Issue #2495755 by phenaproxima: Create MigrateDrupal7TestBase

---
+++ core/modules/migrate_drupal/src/Tests/d7/MigrateDrupal7TestBase.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\migrate_drupal\Tests\d7\MigrateDrupal7TestBase.
+ */
+
+namespace Drupal\migrate_drupal\Tests\d7;
+
+use Drupal\migrate_drupal\Tests\MigrateDrupalTestBase;
+
+/**
+ * Base class for Drupal 7 migration tests.
+ */
+abstract class MigrateDrupal7TestBase extends MigrateDrupalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDumpDirectory() {
+    return parent::getDumpDirectory() . '/d7';
+  }
+
+}
