ac8605bf32775da78ac41cc115d650a4ffef7249
Alex Pott <alex.a.pott@googlemail.com>
2015-06-23 12:30:54 -0500

    Issue #2505197 by Arskiainen, jaimeguzman, tstoeckler: Add a @see + docs about language_element_info_alter() to LanguageSelect

--- core/lib/Drupal/Core/Render/Element/LanguageSelect.php
+++ core/lib/Drupal/Core/Render/Element/LanguageSelect.php
@@ -12,7 +12,11 @@ use Drupal\Core\Language\LanguageInterface;
 /**
  * Provides a form element for selecting a language.
  *
- * @see \Drupal\Core\Render\Element\Select
+ * This does not render an actual form element, but always returns the value of
+ * the default language. It is then extended by Language module via
+ * language_element_info_alter() to provide a proper language selector.
+ *
+ * @see language_element_info_alter()
  *
  * @FormElement("language_select")
  */
