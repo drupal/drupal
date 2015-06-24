b90319a601f9bb381f5c7eee70663fe7af61075d
Alex Pott <alex.a.pott@googlemail.com>
2015-06-23 22:04:00 -0500

    Issue #2278965 by jhedstrom: Add a warning to the installer for MySQL if an InnoDB-compatible engine is not selected

--- core/lib/Drupal/Core/Database/Driver/mysql/Install/Tasks.php
+++ core/lib/Drupal/Core/Database/Driver/mysql/Install/Tasks.php
@@ -34,6 +34,10 @@ class Tasks extends InstallTasks {
         'The %name database server must support utf8mb4 character encoding to work with Drupal. Make sure to use a database server that supports utf8mb4 character encoding, such as MySQL/MariaDB/Percona versions 5.5.3 and up.',
       ),
     );
+    $this->tasks[] = array(
+      'arguments' => array(),
+      'function' => 'ensureInnoDbAvailable',
+    );
   }
 
   /**
@@ -106,4 +110,15 @@ class Tasks extends InstallTasks {
 
     return $form;
   }
+
+  /**
+   * Ensure that InnoDB is available.
+   */
+  function ensureInnoDbAvailable() {
+    $engines = Database::getConnection()->query('SHOW ENGINES')->fetchAllKeyed();
+    if (isset($engines['MyISAM']) && $engines['MyISAM'] == 'DEFAULT' && !isset($engines['InnoDB'])) {
+      $this->fail(t('The MyISAM storage engine is not supported.'));
+    }
+  }
+
 }
