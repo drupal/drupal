760e9d5ec6f744e1bf5dacdeb9d4c10ac6f29fe7
Alex Pott <alex.a.pott@googlemail.com>
2015-07-04 00:50:39 +0100

    Issue #2509628 by andypost: TranslationStatusForm::buildForm() should not use drupal_render()

--- core/modules/locale/src/Form/TranslationStatusForm.php
+++ core/modules/locale/src/Form/TranslationStatusForm.php
@@ -99,7 +99,7 @@ class TranslationStatusForm extends FormBase {
           ),
           'status' => array(
             'class' => array('description', 'priority-low'),
-            'data' => drupal_render($locale_translation_update_info),
+            'data' => $locale_translation_update_info,
           ),
         );
         if (!empty($update['not_found'])) {
