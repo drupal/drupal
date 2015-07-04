2a317f41fd6e63e9cad205b737789b4757789916
xjm <xjm@65776.no-reply.drupal.org>
2015-07-04 15:38:00 +0100

    Issue #2511584 by jhedstrom, rpayanm: Move NodeFormButtonsTest::assertButtons() to a trait

--- core/modules/node/src/Tests/NodeFormButtonsTest.php
+++ core/modules/node/src/Tests/NodeFormButtonsTest.php
@@ -14,6 +14,8 @@ namespace Drupal\node\Tests;
  */
 class NodeFormButtonsTest extends NodeTestBase {
 
+  use AssertButtonsTrait;
+
   /**
    * A normal logged in user.
    *
@@ -133,43 +135,5 @@ class NodeFormButtonsTest extends NodeTestBase {
     $this->drupalPostForm('node/add/article', $edit, t('Save'));
     $node_3 = $node_storage->load(3);
     $this->assertFalse($node_3->isPublished(), 'Node is unpublished');
-  }
-
-  /**
-   * Assert method to verify the buttons in the dropdown element.
-   *
-   * @param array $buttons
-   *   A collection of buttons to assert for on the page.
-   * @param bool $dropbutton
-   *   Whether to check if the buttons are in a dropbutton widget or not.
-   */
-  public function assertButtons($buttons, $dropbutton = TRUE) {
-
-    // Try to find a Save button.
-    $save_button = $this->xpath('//input[@type="submit"][@value="Save"]');
-
-    // Verify that the number of buttons passed as parameters is
-    // available in the dropbutton widget.
-    if ($dropbutton) {
-      $i = 0;
-      $count = count($buttons);
-
-      // Assert there is no save button.
-      $this->assertTrue(empty($save_button));
-
-      // Dropbutton elements.
-      $elements = $this->xpath('//div[@class="dropbutton-wrapper"]//input[@type="submit"]');
-      $this->assertEqual($count, count($elements));
-      foreach ($elements as $element) {
-        $value = isset($element['value']) ? (string) $element['value'] : '';
-        $this->assertEqual($buttons[$i], $value);
-        $i++;
-      }
-    }
-    else {
-      // Assert there is a save button.
-      $this->assertTrue(!empty($save_button));
-      $this->assertNoRaw('dropbutton-wrapper');
-    }
   }
 }
