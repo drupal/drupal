48aa09b423ee78e7279684062f98920697c8dda3
Alex Pott <alex.a.pott@googlemail.com>
2015-07-07 14:10:40 +0100

    Issue #2516866 by willzyx: Faulty dependency to RedirectDestination in Drupal\menu_ui\Plugin\Menu\LocalAction\LocalActionDefault

--- core/modules/menu_ui/src/Plugin/Menu/LocalAction/MenuLinkAdd.php
+++ core/modules/menu_ui/src/Plugin/Menu/LocalAction/MenuLinkAdd.php
@@ -8,7 +8,7 @@
 namespace Drupal\menu_ui\Plugin\Menu\LocalAction;
 
 use Drupal\Core\Menu\LocalActionDefault;
-use Drupal\Core\Routing\RedirectDestination;
+use Drupal\Core\Routing\RedirectDestinationInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Routing\RouteProviderInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -21,7 +21,7 @@ class MenuLinkAdd extends LocalActionDefault {
   /**
    * The redirect destination.
    *
-   * @var \Drupal\Core\Routing\RedirectDestination
+   * @var \Drupal\Core\Routing\RedirectDestinationInterface
    */
   private $redirectDestination;
 
@@ -36,10 +36,10 @@ class MenuLinkAdd extends LocalActionDefault {
    *   The plugin implementation definition.
    * @param \Drupal\Core\Routing\RouteProviderInterface $route_provider
    *   The route provider to load routes by name.
-   * @param \Drupal\Core\Routing\RedirectDestination $redirect_destination
+   * @param \Drupal\Core\Routing\RedirectDestinationInterface $redirect_destination
    *   The redirect destination.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteProviderInterface $route_provider, RedirectDestination $redirect_destination) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteProviderInterface $route_provider, RedirectDestinationInterface $redirect_destination) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $route_provider);
 
     $this->redirectDestination = $redirect_destination;
