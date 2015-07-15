0827c34fb369af1accb45863e7874da9e3b9c609
xjm <xjm@65776.no-reply.drupal.org>
2015-07-15 13:09:04 +0100

    Issue #2533034 by chx, webflo: AliasWhitelistInterface doxygen missing

--- core/lib/Drupal/Core/Path/AliasWhitelistInterface.php
+++ core/lib/Drupal/Core/Path/AliasWhitelistInterface.php
@@ -9,4 +9,12 @@ namespace Drupal\Core\Path;
 
 use Drupal\Core\Cache\CacheCollectorInterface;
 
+/**
+ * Cache the alias whitelist.
+ *
+ * The whitelist contains the first element of the router paths of all
+ * aliases. For example, if /node/12345 has an alias then "node" is added to
+ * the whitelist. This optimization allows skipping the lookup for every
+ * /user/{user} path if "user" is not in the whitelist.
+ */
 interface AliasWhitelistInterface extends CacheCollectorInterface {}
