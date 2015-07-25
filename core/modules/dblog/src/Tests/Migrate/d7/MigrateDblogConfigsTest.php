251ca21b3cb2ce56ed63c3799ed48d311bea6188
Alex Pott <alex.a.pott@googlemail.com>
2015-07-25 10:39:10 +0100

    Issue #2500499 by phenaproxima, bdimaggio: Migration path for dblog 7.x

---
+++ core/modules/dblog/src/Tests/Migrate/d7/MigrateDblogConfigsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\dblog\Tests\Migrate\d7\MigrateDblogConfigsTest.
+ */
+
+namespace Drupal\dblog\Tests\Migrate\d7;
+
+use Drupal\migrate_drupal\Tests\d7\MigrateDrupal7TestBase;
+
+/**
+ * Upgrade variables to dblog.settings.yml.
+ *
+ * @group dblog
+ */
+class MigrateDblogConfigsTest extends MigrateDrupal7TestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['dblog'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installConfig(static::$modules);
+    $this->loadDumps(['Variable.php']);
+    $this->executeMigration('d7_dblog_settings');
+  }
+
+  /**
+   * Tests migration of dblog variables to dblog.settings.yml.
+   */
+  public function testDblogSettings() {
+    $config = $this->config('dblog.settings');
+    $this->assertIdentical(1000, $config->get('row_limit'));
+  }
+
+}
