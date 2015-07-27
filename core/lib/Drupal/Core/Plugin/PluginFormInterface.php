b116b58102046a6df66b04d650590cf37178d0ab
Alex Pott <alex.a.pott@googlemail.com>
2015-07-27 13:43:56 +0100

    Issue #2532970 by Xano, tim.plunkett, jhodgdon: PluginFormInterface must document $form means the plugin's own elements

--- core/lib/Drupal/Core/Plugin/PluginFormInterface.php
+++ core/lib/Drupal/Core/Plugin/PluginFormInterface.php
@@ -10,18 +10,7 @@ namespace Drupal\Core\Plugin;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Provides an interface for a plugin that contains a form.
- *
- * Plugin forms are usually contained in other forms. In order to know where the
- * plugin form is located in the parent form, #parents and #array_parents must
- * be known, but these are not available during the initial build phase. In
- * order to have these properties available when building the plugin form's
- * elements, let buildConfigurationForm() return a form element that has a
- * #process callback and build the rest of the form in the callback. By the time
- * the callback is executed, the element's #parents and #array_parents
- * properties will have been set by the form API. For more documentation on
- * #parents and #array_parents, see
- * https://api.drupal.org/api/drupal/developer!topics!forms_api_reference.html/8.
+ * Provides an interface for an embeddable plugin form.
  *
  * @ingroup plugin_api
  */
@@ -30,10 +19,21 @@ interface PluginFormInterface {
   /**
    * Form constructor.
    *
+   * Plugin forms are embedded in other forms. In order to know where the plugin
+   * form is located in the parent form, #parents and #array_parents must be
+   * known, but these are not available during the initial build phase. In order
+   * to have these properties available when building the plugin form's
+   * elements, let this method return a form element that has a #process
+   * callback and build the rest of the form in the callback. By the time the
+   * callback is executed, the element's #parents and #array_parents properties
+   * will have been set by the form API. For more documentation on #parents and
+   * #array_parents, see
+   * https://api.drupal.org/api/drupal/developer!topics!forms_api_reference.html/8.
+   *
    * @param array $form
-   *   An associative array containing the structure of the form.
+   *   An associative array containing the initial structure of the plugin form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
+   *   The current state of the complete form.
    *
    * @return array
    *   The form structure.
@@ -44,9 +44,10 @@ interface PluginFormInterface {
    * Form validation handler.
    *
    * @param array $form
-   *   An associative array containing the structure of the form.
+   *   An associative array containing the structure of the plugin form as built
+   *   by static::buildConfigurationForm().
    * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
+   *   The current state of the complete form.
    */
   public function validateConfigurationForm(array &$form, FormStateInterface $form_state);
 
@@ -54,9 +55,10 @@ interface PluginFormInterface {
    * Form submission handler.
    *
    * @param array $form
-   *   An associative array containing the structure of the form.
+   *   An associative array containing the structure of the plugin form as built
+   *   by static::buildConfigurationForm().
    * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   The current state of the form.
+   *   The current state of the complete form.
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state);
 
