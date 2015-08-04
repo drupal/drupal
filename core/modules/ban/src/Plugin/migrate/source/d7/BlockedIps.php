2b9b8d0e9abe370bdcf9536416efb1303f587fab
webchick <drupal@webchick.net>
2015-08-04 07:59:57 -0700

    Issue #2503049 by phenaproxima, quietone, joelpittet: Migrate D7 blocked IPs to D8

---
+++ core/modules/ban/src/Plugin/migrate/source/d7/BlockedIps.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ban\Plugin\migrate\source\d7\BlockedIps.
+ */
+
+namespace Drupal\ban\Plugin\migrate\source\d7;
+
+use Drupal\migrate_drupal\Plugin\migrate\source\DrupalSqlBase;
+
+/**
+ * Drupal 7 blocked IPs from database.
+ *
+ * @MigrateSource(
+ *   id = "d7_blocked_ips",
+ *   source_provider = "system"
+ * )
+ */
+class BlockedIps extends DrupalSqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    return $this->select('blocked_ips', 'bi')->fields('bi', ['ip']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return [
+      'ip' => $this->t('The blocked IP address.'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return ['ip' => ['type' => 'string']];
+  }
+
+}
---
+++ core/modules/ban/src/Tests/Migrate/d7/MigrateBlockedIPsTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ban\Tests\Migrate\d7\MigrateBlockedIPsTest.
+ */
+
+namespace Drupal\ban\Tests\Migrate\d7;
+
+use Drupal\config\Tests\SchemaCheckTestTrait;
+use Drupal\migrate_drupal\Tests\d7\MigrateDrupal7TestBase;
+
+/**
+ * Migrate blocked IPs.
+ *
+ * @group ban
+ */
+class MigrateBlockedIPsTest extends MigrateDrupal7TestBase {
+
+  use SchemaCheckTestTrait;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['ban'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installSchema('ban', ['ban_ip']);
+    $this->loadDumps(['BlockedIps.php']);
+    $this->executeMigration('d7_blocked_ips');
+  }
+
+  /**
+   * Tests migration of blocked IPs.
+   */
+  public function testBlockedIPs() {
+    $this->assertTrue(\Drupal::service('ban.ip_manager')->isBanned('111.111.111.111'));
+  }
+
+}
