2c5bee43d5089f6e294480cf3defc10ec8ef7b1b
Andy T. <andytruong@users.noreply.github.com>
2015-08-14 10:59:21 +0700

    Update default.settings.php
    Disable email sending by default

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -565,3 +565,6 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
+# Disable email sending by default
+$conf["mail_system"]["default-system"] = "TestingMailSystem";
