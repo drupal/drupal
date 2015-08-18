bc7d4ebb775032abdc611d2f401f903b33dc0d48
Alex Pott <alex.a.pott@googlemail.com>
2015-08-18 23:57:39 +0100

    Issue #2548135 by hussainweb, neclimdul: Upgrade to Symfony 2.7.3

--- core/vendor/symfony/http-kernel/Kernel.php
+++ core/vendor/symfony/http-kernel/Kernel.php
@@ -60,11 +60,11 @@ abstract class Kernel implements KernelInterface, TerminableInterface
     protected $startTime;
     protected $loadClassCache;
 
-    const VERSION = '2.7.2';
-    const VERSION_ID = '20702';
+    const VERSION = '2.7.3';
+    const VERSION_ID = '20703';
     const MAJOR_VERSION = '2';
     const MINOR_VERSION = '7';
-    const RELEASE_VERSION = '2';
+    const RELEASE_VERSION = '3';
     const EXTRA_VERSION = '';
 
     const END_OF_MAINTENANCE = '05/2018';
