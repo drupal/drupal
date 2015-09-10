44097522094a648cde2a186f34ecb2542d3f685d
webchick <drupal@webchick.net>
2015-09-09 23:23:39 -0700

    Issue #2510310 by cilefen, jeroenbegyn, borisson_, er.pushpinderrana, jhodgdon, stefan.r, alexpott: Improve hook_system_info_alter documentation to mention ModuleUninstallValidatorInterface

--- core/lib/Drupal/Core/Extension/ModuleUninstallValidatorInterface.php
+++ core/lib/Drupal/Core/Extension/ModuleUninstallValidatorInterface.php
@@ -9,11 +9,29 @@ namespace Drupal\Core\Extension;
 
 /**
  * Common interface for module uninstall validators.
+ *
+ * A module uninstall validator must implement this interface and be defined in
+ * a Drupal @link container service @endlink that is tagged
+ * module_install.uninstall_validator.
  */
 interface ModuleUninstallValidatorInterface {
 
   /**
    * Determines the reasons a module can not be uninstalled.
+   *
+   * Example implementation:
+   * @code
+   * public function validate($module) {
+   *   $entity_types = $this->entityManager->getDefinitions();
+   *   $reasons = array();
+   *   foreach ($entity_types as $entity_type) {
+   *     if ($module == $entity_type->getProvider() && $entity_type instanceof ContentEntityTypeInterface && $this->entityManager->getStorage($entity_type->id())->hasData()) {
+   *       $reasons[] = $this->t('There is content for the entity type: @entity_type', array('@entity_type' => $entity_type->getLabel()));
+   *     }
+   *   }
+   *   return $reasons;
+   * }
+   * @endcode
    *
    * @param string $module
    *   A module name.
