22cd7b8f85b20c179ebedf9eb971aaa1536b0c36
Alex Pott <alex.a.pott@googlemail.com>
2015-09-10 12:17:38 +0100

    Issue #2407739 by mortendk, davidhernandez, rteijeiro, lduerig, sivaji@knackforge.com: Remove classes from system templates r*.html.twig

--- core/modules/quickedit/js/quickedit.js
+++ core/modules/quickedit/js/quickedit.js
@@ -369,9 +369,10 @@
     var entityElement = $(fieldElement).closest(entityElementSelector);
     // In the case of a full entity view page, the entity title is rendered
     // outside of "the entity DOM node": it's rendered as the page title. So in
-    // this case, we must find the entity in the mandatory "content" region.
+    // this case, we find the main element of the page and use it as a full
+    // entity ancestor.
     if (entityElement.length === 0) {
-      entityElement = $('.region-content').find(entityElementSelector);
+      entityElement = $('.js-quickedit-main-content').find(entityElementSelector);
     }
     var entityInstanceID = entityElement
       .get(0)
