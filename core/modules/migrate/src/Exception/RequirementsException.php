14c552f355b1d9ebebe7c67f775cdd0708d1a7a0
webchick <drupal@webchick.net>
2015-09-11 14:29:36 -0700

    Issue #2549783 by lostkangaroo, phenaproxima: RequirementsException gives foreach error when single requirement present

--- core/modules/migrate/src/Exception/RequirementsException.php
+++ core/modules/migrate/src/Exception/RequirementsException.php
@@ -60,6 +60,10 @@ class RequirementsException extends \RuntimeException {
   public function getRequirementsString() {
     $output = '';
     foreach ($this->requirements as $requirement_type => $requirements) {
+      if (!is_array($requirements)) {
+        $requirements = array($requirements);
+      }
+
       foreach ($requirements as $value) {
         $output .= "$requirement_type: $value. ";
       }
