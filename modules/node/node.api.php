cb7888f53db00c184dfd947ffae210f9bb737ba1
David Rothstein <drothstein@gmail.com>
2015-10-14 15:47:32 -0400

    Issue #2325055 by darol100, joshi.rohit100, jhodgdon, Bevan: hook_prepare() example is not useful

--- modules/node/node.api.php
+++ modules/node/node.api.php
@@ -1084,19 +1084,9 @@ function hook_delete($node) {
  * @ingroup node_api_hooks
  */
 function hook_prepare($node) {
-  $file = file_save_upload($field_name, _image_filename($file->filename, NULL, TRUE));
-  if ($file) {
-    if (!image_get_info($file->uri)) {
-      form_set_error($field_name, t('Uploaded file is not a valid image'));
-      return;
-    }
+  if (!isset($node->mymodule_value)) {
+    $node->mymodule_value = 'foo';
   }
-  else {
-    return;
-  }
-  $node->images['_original'] = $file->uri;
-  _image_build_derivatives($node, TRUE);
-  $node->new_file = TRUE;
 }
 
 /**
