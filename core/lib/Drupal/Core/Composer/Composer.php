fff902e1a9facca9434afc818fdb57495c78b00f
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2015-10-14 14:25:10 +0100

    Issue #2592367 by alexpott: PHPUnit is broken - we have dependencies on Symfony event dispatcher tests

--- core/lib/Drupal/Core/Composer/Composer.php
+++ core/lib/Drupal/Core/Composer/Composer.php
@@ -53,7 +53,8 @@ class Composer {
     'symfony/debug' => ['Tests'],
     'symfony/dependency-injection' => ['Tests'],
     'symfony/dom-crawler' => ['Tests'],
-    'symfony/event-dispatcher' => ['Tests'],
+    // @see \Drupal\Tests\Component\EventDispatcher\ContainerAwareEventDispatcherTest
+    // 'symfony/event-dispatcher' => ['Tests'],
     'symfony/http-foundation' => ['Tests'],
     'symfony/http-kernel' => ['Tests'],
     'symfony/process' => ['Tests'],
