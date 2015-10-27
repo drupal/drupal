b336472386508ea4221167f8ca75bd92af01d794
Alex Pott <alex.a.pott@googlemail.com>
2015-10-27 12:26:11 +0000

    Issue #2601162 by heykarthikwithu: ShortcutSetStorage::__construct documentation of @param StorageInterface is not required

--- core/modules/shortcut/src/ShortcutSetStorage.php
+++ core/modules/shortcut/src/ShortcutSetStorage.php
@@ -35,8 +35,6 @@ class ShortcutSetStorage extends ConfigEntityStorage implements ShortcutSetStora
    *   The entity info for the entity type.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory service.
-   * @param \Drupal\Core\Config\StorageInterface $config_storage
-   *   The config storage service.
    * @param \Drupal\Component\Uuid\UuidInterface $uuid_service
    *   The UUID service.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
