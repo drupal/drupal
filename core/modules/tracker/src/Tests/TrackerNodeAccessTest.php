b408fba061487ce86a87319a10e152bd0ccd9472
Alex Pott <alex.a.pott@googlemail.com>
2015-10-27 15:24:13 +0000

    Issue #2602442 by mcdruid: typo in \Drupal\tracker\Tests\TrackerNodeAccessTest - drupalCreateuser

--- core/modules/tracker/src/Tests/TrackerNodeAccessTest.php
+++ core/modules/tracker/src/Tests/TrackerNodeAccessTest.php
@@ -45,7 +45,7 @@ class TrackerNodeAccessTest extends WebTestBase {
     $access_user = $this->drupalCreateUser(array('node test view', 'access user profiles'));
 
     // Create user without node test view permission.
-    $no_access_user = $this->drupalCreateuser(array('access user profiles'));
+    $no_access_user = $this->drupalCreateUser(array('access user profiles'));
 
     $this->drupalLogin($access_user);
 
