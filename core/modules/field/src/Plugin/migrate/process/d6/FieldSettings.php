4d9ce53bfbc9e74270212f8cedec961a9f6cceff
webchick <drupal@webchick.net>
2015-10-30 10:13:09 -0400

    Issue #2590105 by webflo, phenaproxima, snehi, alexpott: Fix undefined variable in d6_field migration

--- core/modules/field/src/Plugin/migrate/process/d6/FieldSettings.php
+++ core/modules/field/src/Plugin/migrate/process/d6/FieldSettings.php
@@ -44,6 +44,7 @@ class FieldSettings extends ProcessPluginBase {
   public function getSettings($field_type, $global_settings) {
     $max_length = isset($global_settings['max_length']) ? $global_settings['max_length'] : '';
     $max_length = empty($max_length) ? 255 : $max_length;
+    $allowed_values = [];
     if (isset($global_settings['allowed_values'])) {
       $list = explode("\n", $global_settings['allowed_values']);
       $list = array_map('trim', $list);
@@ -61,9 +62,6 @@ class FieldSettings extends ProcessPluginBase {
         default:
           $allowed_values = $list;
       }
-    }
-    else {
-      $allowed_values = '';
     }
 
     $settings = array(
