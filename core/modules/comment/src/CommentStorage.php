e13d1bcda079c9dd8285628bbd079a68dec1b60f
Alex Pott <alex.a.pott@googlemail.com>
2015-11-02 16:44:53 +0000

    Issue #2602410 by xaiwant: Invalid @param name in core/modules/comment/src/CommentStorage.php

--- core/modules/comment/src/CommentStorage.php
+++ core/modules/comment/src/CommentStorage.php
@@ -42,10 +42,10 @@ class CommentStorage extends SqlContentEntityStorage implements CommentStorageIn
    *   The database connection to be used.
    * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
    *   The entity manager.
-   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
-   *   Cache backend instance to use.
    * @param \Drupal\Core\Session\AccountInterface $current_user
    *   The current user.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
+   *   Cache backend instance to use.
    * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
    *   The language manager.
    */
