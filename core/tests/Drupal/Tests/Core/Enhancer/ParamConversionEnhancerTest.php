0c12b72e0d87e385e72cd893a8245ba48eef0435
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2015-11-10 12:51:43 +0000

    Issue #2605250 by tim.plunkett: ParamConverterManager::convert() should run once per request object

--- core/tests/Drupal/Tests/Core/Enhancer/ParamConversionEnhancerTest.php
+++ core/tests/Drupal/Tests/Core/Enhancer/ParamConversionEnhancerTest.php
@@ -59,12 +59,18 @@ class ParamConversionEnhancerTest extends UnitTestCase {
     $expected['id'] = 'something_better!';
     $expected['_raw_variables'] = new ParameterBag($raw_variables);
 
-    $this->paramConverterManager->expects($this->any())
+    $this->paramConverterManager->expects($this->once())
       ->method('convert')
       ->with($this->isType('array'))
       ->will($this->returnValue($expected));
 
     $result = $this->paramConversionEnhancer->enhance($defaults, new Request());
+
+    $this->assertEquals($expected, $result);
+
+    // Now run with the results as the new defaults to ensure that the
+    // conversion is just run once.
+    $result = $this->paramConversionEnhancer->enhance($result, new Request());
 
     $this->assertEquals($expected, $result);
   }
