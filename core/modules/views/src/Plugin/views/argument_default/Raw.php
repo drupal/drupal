af813036688eefaf6ccb87af66c155a29082dfa0
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2015-11-11 12:10:45 +0000

    Issue #2569117 by othermachines, Lendude, cilefen: "Source path ... has to start with a slash" exception when using contextual filter

--- core/modules/views/src/Plugin/views/argument_default/Raw.php
+++ core/modules/views/src/Plugin/views/argument_default/Raw.php
@@ -112,11 +112,15 @@ class Raw extends ArgumentDefaultPluginBase implements CacheableDependencyInterf
    * {@inheritdoc}
    */
   public function getArgument() {
-    $path = trim($this->currentPath->getPath($this->view->getRequest()), '/');
+    // Don't trim the leading slash since getAliasByPath() requires it.
+    $path = rtrim($this->currentPath->getPath($this->view->getRequest()), '/');
     if ($this->options['use_alias']) {
       $path = $this->aliasManager->getAliasByPath($path);
     }
     $args = explode('/', $path);
+    // Drop the empty first element created by the leading slash since the path
+    // component index doesn't take it into account.
+    array_shift($args);
     if (isset($args[$this->options['index']])) {
       return $args[$this->options['index']];
     }
