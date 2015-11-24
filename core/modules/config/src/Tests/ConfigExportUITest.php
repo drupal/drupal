7c7504ccd0313054bdb27532bcc4842eaf7d2aeb
Alex Pott <alex.a.pott@googlemail.com>
2015-11-24 11:52:09 +0000

    Issue #2409547 by znerol: Remove config_export_test.module

--- core/modules/config/src/Tests/ConfigExportUITest.php
+++ core/modules/config/src/Tests/ConfigExportUITest.php
@@ -23,13 +23,20 @@ class ConfigExportUITest extends WebTestBase {
    *
    * @var array
    */
-  public static $modules = array('config', 'config_test', 'config_export_test');
+  public static $modules = array('config', 'config_test');
 
   /**
    * {@inheritdoc}
    */
   protected function setUp() {
     parent::setUp();
+
+    // Set up an override.
+    $settings['config']['system.maintenance']['message'] = (object) array(
+      'value' => 'Foo',
+      'required' => TRUE,
+    );
+    $this->writeSettings($settings);
 
     $this->drupalLogin($this->drupalCreateUser(array('export configuration')));
   }
