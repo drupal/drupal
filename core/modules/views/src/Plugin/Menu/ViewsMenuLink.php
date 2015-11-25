a45315980400b5cfc48f647153012c39fd73a206
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2015-11-25 22:02:51 +0000

    Issue #2579695 by willzyx: ViewsMenuLink should use parent contructor instead of set manually the member variables
    (cherry picked from commit 8aead032bb01322b1f311846863600f4f5bee33c)

--- core/modules/views/src/Plugin/Menu/ViewsMenuLink.php
+++ core/modules/views/src/Plugin/Menu/ViewsMenuLink.php
@@ -70,9 +70,7 @@ class ViewsMenuLink extends MenuLinkBase implements ContainerFactoryPluginInterf
    *   The view executable factory
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entity_manager, ViewExecutableFactory $view_executable_factory) {
-    $this->configuration = $configuration;
-    $this->pluginId = $plugin_id;
-    $this->pluginDefinition = $plugin_definition;
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
 
     $this->entityManager = $entity_manager;
     $this->viewExecutableFactory = $view_executable_factory;
