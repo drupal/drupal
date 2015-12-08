2ddd4bf65d00a4bed40b276e63bd90d2bbda4585
Jennifer Hodgdon <yahgrp@poplarware.com>
2015-12-08 09:52:54 -0800

    Issue #2626518 by michaellenahan: Mention sites/default/settings.php in example.settings.local.php

--- sites/example.settings.local.php
+++ sites/example.settings.local.php
@@ -5,10 +5,13 @@
  * Local development override configuration feature.
  *
  * To activate this feature, copy and rename it such that its path plus
- * filename is 'sites/example.com/settings.local.php', where example.com
- * is the name of your site. Then, go to the bottom of
- * 'sites/example.com/settings.php' and uncomment the commented lines that
- * mention 'settings.local.php'.
+ * filename is 'sites/default/settings.local.php'. Then, go to the bottom of
+ * 'sites/default/settings.php' and uncomment the commented lines that mention
+ * 'settings.local.php'.
+ *
+ * If you are using a site name in the path, such as 'sites/example.com', copy
+ * this file to 'sites/example.com/settings.local.php', and uncomment the lines
+ * at the bottom of 'sites/example.com/settings.php'.
  */
 
 /**
