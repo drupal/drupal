3e4354cdbcc029a72b9a6f4ddca85dd35166aabc
Alex Pott <alex.a.pott@googlemail.com>
2015-12-09 16:25:28 +0000

    Issue #2629772 by webflo: Update mink-phantomjs-driver to a tagged release
    (cherry picked from commit 96808b3a386485b674051662ad6592c3bf5fa9ea)

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -41,15 +41,19 @@ class ComposerAutoloaderInitDrupal8
         $loader->register(true);
 
         $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $file) {
-            composerRequireDrupal8($file);
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequireDrupal8($fileIdentifier, $file);
         }
 
         return $loader;
     }
 }
 
-function composerRequireDrupal8($file)
+function composerRequireDrupal8($fileIdentifier, $file)
 {
-    require $file;
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+    }
 }
