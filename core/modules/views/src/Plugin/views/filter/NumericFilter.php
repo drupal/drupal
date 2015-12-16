5ea2d135f19738f7b06c8ea3200cece39a7ae4c8
Alex Pott <alex.a.pott@googlemail.com>
2015-12-16 11:40:42 +0000

    Issue #2480719 by Lendude, aerozeppelin, Xano, dawehner: Missing label and description for exposed numeric filter when using 'between' filter

--- core/modules/views/src/Plugin/views/filter/NumericFilter.php
+++ core/modules/views/src/Plugin/views/filter/NumericFilter.php
@@ -199,9 +199,10 @@ class NumericFilter extends FilterPluginBase {
     if ($which == 'all' || $which == 'minmax') {
       $form['value']['min'] = array(
         '#type' => 'textfield',
-        '#title' => !$exposed ? $this->t('Min') : '',
+        '#title' => !$exposed ? $this->t('Min') : $this->exposedInfo()['label'],
         '#size' => 30,
         '#default_value' => $this->value['min'],
+        '#description' => !$exposed ? '' : $this->exposedInfo()['description']
       );
       $form['value']['max'] = array(
         '#type' => 'textfield',
