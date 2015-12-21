af1363466a96cea02786905dfc832bead856928b
Alex Pott <alex.a.pott@googlemail.com>
2015-12-21 13:52:38 +0000

    Issue #2401953 by Lendude, dawehner: Database exception for View with Combined field filter with fields with no query alias
    (cherry picked from commit 05b3abf260b2e796c3375fbdb635d5dca352b2d6)

--- core/modules/user/src/Tests/Views/BulkFormTest.php
+++ core/modules/user/src/Tests/Views/BulkFormTest.php
@@ -30,7 +30,7 @@ class BulkFormTest extends UserTestBase {
    *
    * @var array
    */
-  public static $testViews = array('test_user_bulk_form');
+  public static $testViews = array('test_user_bulk_form', 'test_user_bulk_form_combine_filter');
 
   /**
    * Tests the user bulk form.
@@ -130,4 +130,14 @@ class BulkFormTest extends UserTestBase {
     $this->assertOption('edit-action', $action_id);
   }
 
+  /**
+   * Tests the user bulk form with a combined field filter on the bulk column.
+   */
+  public function testBulkFormCombineFilter() {
+    // Add a user.
+    $account = entity_load('user', $this->users[0]->id());
+    $view = Views::getView('test_user_bulk_form_combine_filter');
+    $errors = $view->validate();
+    $this->assertEqual(reset($errors['default']), t('Field %field set in %filter is not usable for this filter type. Combined field filter only works for simple fields.', array('%field' => 'User: Bulk update', '%filter' => 'Global: Combine fields filter')));
+  }
 }
