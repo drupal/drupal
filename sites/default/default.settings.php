74926123dd26749d88022b594f8576a56a0abca5
Andy T <andytruong@users.noreply.github.com>
2016-01-02 11:44:02 +1000

    Update default.settings.php

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -584,3 +584,8 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash sign to enable.
  */
 # $conf['theme_debug'] = TRUE;
+
+$local_settings = dirname(__FILE__) . '/local.settings.php';
+if (file_exists($local_settings)) {
+  require_once $local_settings;  
+}
