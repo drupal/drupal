5cba573865c13cf8fa30e14a84f0b0afc6d11fc4
Alex Pott <alex.a.pott@googlemail.com>
2016-01-08 11:41:14 +0000

    Issue #2644734 by znerol: Replace confusing comment in PagerSelectExtender::execute()
    (cherry picked from commit 79ee24dd9f668ba7ee2d7e2c58179ce38f58af88)

--- core/lib/Drupal/Core/Database/Query/PagerSelectExtender.php
+++ core/lib/Drupal/Core/Database/Query/PagerSelectExtender.php
@@ -64,10 +64,9 @@ class PagerSelectExtender extends SelectExtender {
    * to it.
    */
   public function execute() {
-
-    // Add convenience tag to mark that this is an extended query. We have to
-    // do this in the constructor to ensure that it is set before preExecute()
-    // gets called.
+    // By calling preExecute() here, we force it to preprocess the extender
+    // object rather than just the base query object. That means
+    // hook_query_alter() gets access to the extended object.
     if (!$this->preExecute($this)) {
       return NULL;
     }
