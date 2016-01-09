6ea3c65baab490bc9c6a1ef231aa187fe5edc315
sanket_markan <sanketmarkan@gmail.com>
2016-01-09 11:12:40 +0530

    Resolved bug Upload multiple images at the same time breaks the display

--- core/modules/image/src/Plugin/Field/FieldWidget/ImageWidget.php
+++ core/modules/image/src/Plugin/Field/FieldWidget/ImageWidget.php
@@ -170,20 +170,14 @@ class ImageWidget extends FileWidget {
       );
 
       // Determine image dimensions.
-    //  if (isset($element['#value']['width']) && isset($element['#value']['height'])) {
-      //  $variables['width'] = $element['#value']['width'];
-      //  $variables['height'] = $element['#value']['height'];
-    //  }
-    //  else {
+      if ($image->isValid()) {
         $image = \Drupal::service('image.factory')->get($file->getFileUri());
-        if ($image->isValid()) {
-          $variables['width'] = $image->getWidth();
-          $variables['height'] = $image->getHeight();
-        }
-        else {
-          $variables['width'] = $variables['height'] = NULL;
-        }
-    //  }
+        $variables['width'] = $image->getWidth();
+        $variables['height'] = $image->getHeight();
+      }
+      else {
+        $variables['width'] = $variables['height'] = NULL;
+      }
 
       $element['preview'] = array(
         '#weight' => -10,
