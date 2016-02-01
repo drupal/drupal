1999e1d9d4163c632e60633c0cda240afec56622
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-02-01 19:17:33 +0900

    Issue #2267039 by aerozeppelin, joachim: UnsupportedDataTypeConfigException doesn't say which config file the problem is
    (cherry picked from commit 40bf8be87c4613ce44a0c78d682e656e17f66c5e)

--- core/modules/config/src/Tests/Storage/FileStorageTest.php
+++ core/modules/config/src/Tests/Storage/FileStorageTest.php
@@ -9,6 +9,7 @@ namespace Drupal\config\Tests\Storage;
 
 use Drupal\Component\Serialization\Yaml;
 use Drupal\Core\Config\FileStorage;
+use Drupal\Core\Config\UnsupportedDataTypeConfigException;
 
 /**
  * Tests FileStorage operations.
@@ -74,6 +75,21 @@ class FileStorageTest extends ConfigStorageTestBase {
     $storage_absolute_path = new FileStorage($absolute_path);
     $config_files = $storage_absolute_path->listAll();
     $this->assertIdentical($config_files, $expected_files, 'Absolute path, two config files found.');
+  }
+
+  /**
+   * Test UnsupportedDataTypeConfigException displays path of
+   * erroneous file during read.
+   */
+  public function testReadUnsupportedDataTypeConfigException() {
+    file_put_contents($this->storage->getFilePath('core.extension'), PHP_EOL . 'foo : [bar}', FILE_APPEND);
+    try {
+      $config_parsed = $this->storage->read('core.extension');
+    }
+    catch (UnsupportedDataTypeConfigException $e) {
+      $this->pass('Exception thrown when trying to read a field containing invalid data type.');
+      $this->assertTrue((strpos($e->getMessage(), $this->storage->getFilePath('core.extension')) !== FALSE), 'Erroneous file path is displayed.');
+    }
   }
 
 }
