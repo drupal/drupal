854898258d67abf385973ba9d9a71474362128e2
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-02-03 15:15:58 +0900

    Issue #1232572 by deviantintegral, joachim, cweagans, kenorb, bleen, webflo, ceng, Elijah Lynn, sriharsha.uppuluri: Add a variable to disable fixing file permissions in drupal_install_fix_file()

--- sites/example.settings.local.php
+++ sites/example.settings.local.php
@@ -93,3 +93,15 @@ $settings['extension_discovery_scan_tests'] = TRUE;
  * using these parameters in a request to rebuild.php.
  */
 $settings['rebuild_access'] = TRUE;
+
+/**
+ * Skip file system permissions hardening.
+ *
+ * The system module will periodically check the permissions of your site's
+ * site directory to ensure that it is not writable by the website user. For
+ * sites that are managed with a version control system, this can cause problems
+ * when files in that directory such as settings.php are updated, because the
+ * user pulling in the changes won't have permissions to modify files in the
+ * directory.
+ */
+$settings['skip_permissions_hardening'] = TRUE;
