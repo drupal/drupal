fc24a2ea55bcebb87a33931ad98067e64a76d29d
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-02-09 09:38:11 +0900

    Issue #2615790 by Xano: Field item properties do not prevent the services they contain from being serialized
    (cherry picked from commit 28c003ba8853906d53d8a22794ba9caf4b105bf7)

---
+++ core/tests/Drupal/Tests/Core/TypedData/TypedDataTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\Core\TypedData\TypedDataTest.
+ */
+
+namespace Drupal\Tests\Core\TypedData;
+
+use Drupal\Core\TypedData\DataDefinitionInterface;
+use Drupal\Core\TypedData\TypedData;
+use Drupal\Core\TypedData\TypedDataManagerInterface;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\Core\TypedData\TypedData
+ *
+ * @group TypedData
+ */
+class TypedDataTest extends UnitTestCase {
+
+  /**
+   * @covers ::__sleep
+   */
+  public function testSleep() {
+    $data_definition = $this->getMock(DataDefinitionInterface::class);
+    /** @var \Drupal\Core\TypedData\TypedData $typed_data */
+    $typed_data = $this->getMockForAbstractClass(TypedData::class, [$data_definition]);
+    $string_translation = $this->getStringTranslationStub();
+    $typed_data->setStringTranslation($string_translation);
+    $typed_data_manager = $this->getMock(TypedDataManagerInterface::class);
+    $typed_data->setTypedDataManager($typed_data_manager);
+    $serialized_typed_data = serialize($typed_data);
+    $this->assertNotContains(get_class($string_translation), $serialized_typed_data);
+    $this->assertNotContains(get_class($typed_data_manager), $serialized_typed_data);
+  }
+
+}
