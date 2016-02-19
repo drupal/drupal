6c3e7da2802658bb0afeaa911cfb0e6781e8e690
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-02-19 13:36:03 +0900

    Issue #2226455 by quietone, ultimike, brockfanning: Migrated URL aliases not working until nodes are re-saved

---
+++ core/modules/path/src/Plugin/migrate/process/d6/UrlAliasLanguage.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\path\Plugin\migrate\process\d6\UrlAliasLanguage.
+ */
+
+namespace Drupal\path\Plugin\migrate\process\d6;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Drupal\Core\Language\LanguageInterface;
+
+/**
+ * Url alias language code process.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "d6_url_alias_language"
+ * )
+ */
+class UrlAliasLanguage extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $language = reset($value);
+    $langcode = ($language === '') ? LanguageInterface::LANGCODE_NOT_SPECIFIED : $language;
+    return $langcode;
+  }
+
+}
