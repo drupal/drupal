822a21f8bc92a1de2f722b28a5912f12a0700230
Alex Pott <alex.a.pott@googlemail.com>
2016-02-20 10:22:52 +0530

    Issue #2393387 by mondrake, gnuget, tim.plunkett: Add test for editing image effect when configuration form is Ajax enabled
    (cherry picked from commit 208be7834b70a170f313fbf7e12b8c1ce1ede784)

--- core/modules/image/src/Tests/ImageAdminStylesTest.php
+++ core/modules/image/src/Tests/ImageAdminStylesTest.php
@@ -292,6 +292,47 @@ class ImageAdminStylesTest extends ImageFieldTestBase {
   }
 
   /**
+   * Tests editing Ajax-enabled image effect forms.
+   */
+  public function testAjaxEnabledEffectForm() {
+    $admin_path = 'admin/config/media/image-styles';
+
+    // Setup a style to be created and effects to add to it.
+    $style_name = strtolower($this->randomMachineName(10));
+    $style_label = $this->randomString();
+    $style_path = $admin_path . '/manage/' . $style_name;
+    $effect_edit = [
+      'data[test_parameter]' => 100,
+    ];
+
+    // Add style form.
+    $edit = [
+      'name' => $style_name,
+      'label' => $style_label,
+    ];
+    $this->drupalPostForm($admin_path . '/add', $edit, t('Create new style'));
+    $this->assertRaw(t('Style %name was created.', ['%name' => $style_label]));
+
+    // Add two Ajax-enabled test effects.
+    $this->drupalPostForm($style_path, ['new' => 'image_module_test_ajax'], t('Add'));
+    $this->drupalPostForm(NULL, $effect_edit, t('Add effect'));
+    $this->drupalPostForm($style_path, ['new' => 'image_module_test_ajax'], t('Add'));
+    $this->drupalPostForm(NULL, $effect_edit, t('Add effect'));
+
+    // Load the saved image style.
+    $style = ImageStyle::load($style_name);
+
+    // Edit back the effects.
+    foreach ($style->getEffects() as $uuid => $effect) {
+      $effect_path = $admin_path . '/manage/' . $style_name . '/effects/' . $uuid;
+      $this->drupalGet($effect_path);
+      $this->drupalPostAjaxForm(NULL, $effect_edit, ['op' => t('Ajax refresh')]);
+      $this->drupalPostForm(NULL, $effect_edit, t('Update effect'));
+    }
+
+  }
+
+  /**
    * Test deleting a style and choosing a replacement style.
    */
   function testStyleReplacement() {
