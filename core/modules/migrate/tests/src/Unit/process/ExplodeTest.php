4e39681b6a15ceb26b33fc247b75844f0b7c29de
Alex Pott <alex.a.pott@googlemail.com>
2016-02-21 12:56:13 +0530

    Issue #2575101 by quietone, jcnventura, jgrubb, hussainweb, chr.fritsch, heddn, benjy: Add an explode/separator process plugin
    (cherry picked from commit e740b7060f11ba21a3433099522f82f5bb26be01)

---
+++ core/modules/migrate/tests/src/Unit/process/ExplodeTest.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\migrate\Unit\process\ExplodeTest.
+ */
+
+namespace Drupal\Tests\migrate\Unit\process;
+
+use Drupal\migrate\Plugin\migrate\process\Explode;
+use Drupal\migrate\Plugin\migrate\process\Concat;
+
+/**
+ * Tests the Explode process plugin.
+ *
+ * @group migrate
+ */
+class ExplodeTest extends MigrateProcessTestCase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    $configuration = [
+      'delimiter' => ',',
+    ];
+    $this->plugin = new Explode($configuration, 'map', []);
+    parent::setUp();
+  }
+
+  /**
+   * Test explode transform process works.
+   */
+  public function testTransform() {
+    $value = $this->plugin->transform('foo,bar,tik', $this->migrateExecutable, $this->row, 'destinationproperty');
+    $this->assertSame($value, ['foo', 'bar', 'tik']);
+  }
+
+  /**
+   * Test explode transform process works with a limit.
+   */
+  public function testTransformLimit() {
+    $plugin = new Explode(['delimiter' => '_', 'limit' => 2], 'map', []);
+    $value = $plugin->transform('foo_bar_tik', $this->migrateExecutable, $this->row, 'destinationproperty');
+    $this->assertSame($value, ['foo', 'bar_tik']);
+  }
+
+  /**
+   * Test if the explode process can be chained with a handles_multiple process.
+   */
+  public function testChainedTransform() {
+    $exploded = $this->plugin->transform('foo,bar,tik', $this->migrateExecutable, $this->row, 'destinationproperty');
+
+    $concat = new Concat([], 'map', []);
+    $concatenated = $concat->transform($exploded, $this->migrateExecutable, $this->row, 'destinationproperty');
+    $this->assertSame($concatenated, 'foobartik');
+  }
+
+  /**
+   * Test explode fails properly on non-strings.
+   *
+   * @expectedException \Drupal\migrate\MigrateException
+   *
+   * @expectedExceptionMessage is not a string
+   */
+  public function testExplodeWithNonString() {
+    $this->plugin->transform(['foo'], $this->migrateExecutable, $this->row, 'destinationproperty');
+  }
+
+  /**
+   * Test explode fails with empty delimiter.
+   *
+   * @expectedException \Drupal\migrate\MigrateException
+   *
+   * @expectedExceptionMessage delimiter is empty
+   */
+  public function testExplodeWithEmptyDelimiter() {
+    $plugin = new Explode(['delimiter' => ''], 'map', []);
+    $plugin->transform('foo,bar', $this->migrateExecutable, $this->row, 'destinationproperty');
+  }
+
+}
