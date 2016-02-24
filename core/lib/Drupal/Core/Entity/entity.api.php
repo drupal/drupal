2b6e538fa7325abcc2a91590b5322f2e77102068
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-02-24 13:09:37 +0900

    Issue #2673918 by David Hernández: hook_entity_view_mode_info_alter still shows hook_entity_view_mode_info, that has been removed
    (cherry picked from commit aa44ae429ff06e9eb62a7278f2c9c034031445c3)

--- core/lib/Drupal/Core/Entity/entity.api.php
+++ core/lib/Drupal/Core/Entity/entity.api.php
@@ -699,7 +699,6 @@ function hook_entity_type_alter(array &$entity_types) {
  *
  * @see \Drupal\Core\Entity\EntityManagerInterface::getAllViewModes()
  * @see \Drupal\Core\Entity\EntityManagerInterface::getViewModes()
- * @see hook_entity_view_mode_info()
  */
 function hook_entity_view_mode_info_alter(&$view_modes) {
   $view_modes['user']['full']['status'] = TRUE;
