50ca110791f081b1832d94c3d71567431d902f4c
effulgentsia <alex.bronstein@acquia.com>
2016-03-02 15:17:20 -0800

    Issue #2678770 by alexpott, tstoeckler, Gábor Hojtsy: Random fail with third party shortcut settings schema on seven theme

---
+++ core/modules/shortcut/tests/src/Kernel/ShortcutSevenIntegrationTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\shortcut\Kernel\ShortcutSevenIntegrationTest.
+ */
+
+namespace Drupal\Tests\shortcut\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests shortcut_install() and shortcut_uninstall().
+ *
+ * @group shortcut
+ */
+class ShortcutSevenIntegrationTest extends KernelTestBase {
+
+  public static $modules = ['system'];
+
+  /**
+   * Tests shortcut_install() and shortcut_uninstall().
+   */
+  public function testInstallUninstall() {
+    // Install seven.
+    \Drupal::service('theme_installer')->install(['seven']);
+    $this->assertNull($this->config('seven.settings')->get('third_party_settings.shortcut'), 'There are no shortcut settings in seven.settings.');
+
+    \Drupal::service('module_installer')->install(['shortcut']);
+    $this->assertTrue($this->config('seven.settings')->get('third_party_settings.shortcut.module_link'), 'The shortcut module_link setting is in seven.settings.');
+
+    \Drupal::service('module_installer')->uninstall(['shortcut']);
+    $this->assertNull($this->config('seven.settings')->get('third_party_settings.shortcut'), 'There are no shortcut settings in seven.settings.');
+  }
+
+}
