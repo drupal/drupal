4b74a16386ec4a895c0bb2a45668a8fbc32d07b8
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-03-07 08:06:25 +0900

    Issue #2678620 by stBorchert: HandlerBase::breakString does not work with decimal values
    (cherry picked from commit 3d91345eb3711326802b6846c61a6a0a843e536c)

--- core/modules/views/src/Plugin/views/argument/NumericArgument.php
+++ core/modules/views/src/Plugin/views/argument/NumericArgument.php
@@ -67,7 +67,7 @@ class NumericArgument extends ArgumentPluginBase {
     }
 
     if (!empty($this->options['break_phrase'])) {
-      $break = static::breakString($this->argument, TRUE);
+      $break = static::breakString($this->argument, FALSE);
       $this->value = $break->value;
       $this->operator = $break->operator;
     }
@@ -100,7 +100,7 @@ class NumericArgument extends ArgumentPluginBase {
     $this->ensureMyTable();
 
     if (!empty($this->options['break_phrase'])) {
-      $break = static::breakString($this->argument, TRUE);
+      $break = static::breakString($this->argument, FALSE);
       $this->value = $break->value;
       $this->operator = $break->operator;
     }
