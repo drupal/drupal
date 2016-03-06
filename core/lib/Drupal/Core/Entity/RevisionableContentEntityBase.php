6315ac4392a497fc00bbe0c82fe2828b2731f5f0
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-03-07 08:53:00 +0900

    Issue #2666808 by dawehner, amateescu: Add a revision log interface

---
+++ core/lib/Drupal/Core/Entity/RevisionableContentEntityBase.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Core\Entity\RevisionableContentEntityBase.
+ */
+
+namespace Drupal\Core\Entity;
+
+/**
+ * Provides a content entity with extended support for revisions.
+ *
+ * In addition to the parent entity class, base fields and methods for
+ * accessing the revision log message, revision owner and the revision creation
+ * time are provided.
+ *
+ * @ingroup entity_api
+ */
+abstract class RevisionableContentEntityBase extends ContentEntityBase implements RevisionLogInterface {
+
+  use RevisionLogEntityTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields += static::revisionLogBaseFieldDefinitions($entity_type);
+    return $fields;
+  }
+
+}
