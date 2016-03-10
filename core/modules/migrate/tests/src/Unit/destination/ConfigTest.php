39b98468a328740e9b2f51e4b71011cda3f7c914
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-03-10 12:46:41 +0900

    Issue #2225477 by quietone: Add migrate sources and destinations for D6 i18n variables
    (cherry picked from commit 054d153ff9247ad2cd6d2268ef2386635fd8351f)

--- core/modules/migrate/tests/src/Unit/destination/ConfigTest.php
+++ core/modules/migrate/tests/src/Unit/destination/ConfigTest.php
@@ -49,9 +49,72 @@ class ConfigTest extends UnitTestCase {
       ->disableOriginalConstructor()
       ->getMock();
     $row->expects($this->once())
+      ->method('hasDestinationProperty')
+      ->will($this->returnValue(FALSE));
+    $row->expects($this->any())
       ->method('getRawDestination')
       ->will($this->returnValue($source));
-    $destination = new Config(array('config_name' => 'd8_config'), 'd8_config', array('pluginId' => 'd8_config'), $migration, $config_factory);
+    $language_manager = $this->getMockBuilder('Drupal\language\ConfigurableLanguageManagerInterface')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $language_manager->expects($this->never())
+      ->method('getLanguageConfigOverride')
+      ->with('fr', 'd8_config')
+      ->will($this->returnValue($config));
+    $destination = new Config(array('config_name' => 'd8_config'), 'd8_config', array('pluginId' => 'd8_config'), $migration, $config_factory, $language_manager);
+    $destination_id = $destination->import($row);
+    $this->assertEquals($destination_id, ['d8_config']);
+  }
+
+  /**
+   * Test the import method.
+   */
+  public function testLanguageImport() {
+    $source = array(
+      'langcode' => 'mi',
+    );
+    $migration = $this->getMockBuilder('Drupal\migrate\Entity\Migration')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $config = $this->getMockBuilder('Drupal\Core\Config\Config')
+      ->disableOriginalConstructor()
+      ->getMock();
+    foreach ($source as $key => $val) {
+      $config->expects($this->once())
+        ->method('set')
+        ->with($this->equalTo($key), $this->equalTo($val))
+        ->will($this->returnValue($config));
+    }
+    $config->expects($this->once())
+      ->method('save');
+    $config->expects($this->any())
+      ->method('getName')
+      ->willReturn('d8_config');
+    $config_factory = $this->getMock('Drupal\Core\Config\ConfigFactoryInterface');
+    $config_factory->expects($this->once())
+      ->method('getEditable')
+      ->with('d8_config')
+      ->will($this->returnValue($config));
+    $row = $this->getMockBuilder('Drupal\migrate\Row')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $row->expects($this->once())
+      ->method('hasDestinationProperty')
+      ->will($this->returnValue($source));
+    $row->expects($this->any())
+      ->method('getRawDestination')
+      ->will($this->returnValue($source));
+    $row->expects($this->any())
+      ->method('getDestinationProperty')
+      ->will($this->returnValue($source['langcode']));
+    $language_manager = $this->getMockBuilder('Drupal\language\ConfigurableLanguageManagerInterface')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $language_manager->expects($this->any())
+      ->method('getLanguageConfigOverride')
+      ->with('mi', 'd8_config')
+      ->will($this->returnValue($config));
+    $destination = new Config(array('config_name' => 'd8_config'), 'd8_config', array('pluginId' => 'd8_config'), $migration, $config_factory, $language_manager);
     $destination_id = $destination->import($row);
     $this->assertEquals($destination_id, ['d8_config']);
   }
