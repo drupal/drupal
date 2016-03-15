6af13e33d26144b15052cd44173946771004b3e4
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-03-15 09:32:12 +0900

    Issue #2669590 by Lendude, joe_carvajal: IntegrityConstraintViolationException using an entity reference in a view with a entity reference display
    (cherry picked from commit 0ec00a5012edb7b6e0f5acf9c7748aecdff099e3)

--- core/modules/views/src/Tests/Plugin/DisplayEntityReferenceTest.php
+++ core/modules/views/src/Tests/Plugin/DisplayEntityReferenceTest.php
@@ -10,6 +10,7 @@ namespace Drupal\views\Tests\Plugin;
 use Drupal\entity_test\Entity\EntityTest;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\field\Tests\EntityReference\EntityReferenceTestTrait;
 use Drupal\views\Views;
 
 /**
@@ -20,6 +21,8 @@ use Drupal\views\Views;
  * @see \Drupal\views\Plugin\views\display\EntityReference
  */
 class DisplayEntityReferenceTest extends PluginTestBase {
+
+  use EntityReferenceTestTrait;
 
   /**
    * Views used by this test.
@@ -41,6 +44,13 @@ class DisplayEntityReferenceTest extends PluginTestBase {
    * @var string
    */
   protected $fieldName;
+
+  /**
+   * The used entity reference field name in the test.
+   *
+   * @var string
+   */
+  protected $entityRefFieldName;
 
   /**
    * The field storage.
@@ -79,6 +89,10 @@ class DisplayEntityReferenceTest extends PluginTestBase {
       'bundle' => 'entity_test',
     ]);
     $this->field->save();
+
+    // Add an entity reference field to reference the same base table.
+    $this->entityRefFieldName = 'field_test_entity_ref_entity_ref';
+    $this->createEntityReferenceField('entity_test', 'entity_test', $this->entityRefFieldName, NULL, 'entity_test');
 
     // Create some entities to search. Add a common string to the name and
     // the text field in two entities so we can test that we can search in both.
@@ -128,6 +142,59 @@ class DisplayEntityReferenceTest extends PluginTestBase {
     $this->executeView($view);
 
     // Test that we have searched in both fields.
+    $this->assertEqual(count($view->result), 2, 'Search returned two rows');
+    $view->destroy();
+
+    // Add a relationship and a field using that relationship.
+    $this->drupalPostForm('admin/structure/views/nojs/add-handler/test_display_entity_reference/default/relationship', ['name[entity_test.user_id]' => TRUE], t('Add and configure relationships'));
+    $this->drupalPostForm(NULL, [], t('Apply'));
+
+    $this->drupalPostForm('admin/structure/views/nojs/add-handler/test_display_entity_reference/default/field', ['name[users_field_data.uid]' => TRUE], t('Add and configure fields'));
+    $this->drupalPostForm(NULL, [], t('Apply'));
+
+    // Add the new field to the search fields.
+    $this->drupalPostForm('admin/structure/views/nojs/display/test_display_entity_reference/entity_reference_1/style_options', ['style_options[search_fields][uid]' => 'uid'], t('Apply'));
+    $this->drupalPostForm(NULL, [], t('Save'));
+
+    // Test that the search still works with the ralated field.
+    $view = Views::getView('test_display_entity_reference');
+    $view->setDisplay('entity_reference_1');
+
+    // Add the required settings to test a search operation.
+    $options = [
+      'match' => '2',
+      'match_operator' => 'CONTAINS',
+      'limit' => 0,
+      'ids' => NULL,
+    ];
+    $view->display_handler->setOption('entity_reference_options', $options);
+
+    $this->executeView($view);
+
+    // Run validation when using a relationship to the same base table.
+    $this->assertEqual(count($view->result), 2, 'Search returned two rows');
+    $view->destroy();
+
+    $this->drupalPostForm('admin/structure/views/nojs/add-handler/test_display_entity_reference/default/relationship', ['name[entity_test__field_test_entity_ref_entity_ref.field_test_entity_ref_entity_ref]' => TRUE], t('Add and configure relationships'));
+    $this->drupalPostForm(NULL, [], t('Apply'));
+
+    $this->drupalPostForm(NULL, [], t('Save'));
+
+    // Test that the search still works with the related field.
+    $view = Views::getView('test_display_entity_reference');
+    $view->setDisplay('entity_reference_1');
+
+    // Add IDs to trigger validation.
+    $options = [
+      'match' => '1',
+      'match_operator' => 'CONTAINS',
+      'limit' => 0,
+      'ids' => [1, 2],
+    ];
+    $view->display_handler->setOption('entity_reference_options', $options);
+
+    $this->executeView($view);
+
     $this->assertEqual(count($view->result), 2, 'Search returned two rows');
   }
 
