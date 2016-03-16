e8ae668964fe014d3e025377fb7d1c5c798f926b
Alex Pott <alex.a.pott@googlemail.com>
2016-03-16 17:28:57 +0000

    Revert "Issue #2469713 by dawehner, pfrenssen, alexpott, elachlan, Mixologic, larowlan, hussainweb, benjy, jibran, Wim Leers, isntall: Step 2: Create a JavaScriptTestBase using PhantomJs Driver/Binary"
    This reverts commit 9c9fae71c23c39b9bb7a54bd0f9844022925b02f.

--- core/modules/simpletest/src/TestDiscovery.php
+++ core/modules/simpletest/src/TestDiscovery.php
@@ -123,7 +123,6 @@ class TestDiscovery {
       $this->testNamespaces["Drupal\\Tests\\$name\\Unit\\"][] = "$base_path/tests/src/Unit";
       $this->testNamespaces["Drupal\\Tests\\$name\\Kernel\\"][] = "$base_path/tests/src/Kernel";
       $this->testNamespaces["Drupal\\Tests\\$name\\Functional\\"][] = "$base_path/tests/src/Functional";
-      $this->testNamespaces["Drupal\\Tests\\$name\\FunctionalJavascript\\"][] = "$base_path/tests/src/FunctionalJavascript";
     }
 
     foreach ($this->testNamespaces as $prefix => $paths) {
