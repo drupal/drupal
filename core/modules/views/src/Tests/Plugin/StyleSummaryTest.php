1d13c5f1a1b2c6d604ea2ce1c0f408a3bebf9547
Alex Pott <alex.a.pott@googlemail.com>
2016-03-27 16:04:00 +0100

    Issue #2685021 by Lendude, cilefen, r0nn1ef, andypost: Fatal error: $this when not in object context in views.theme.inc

--- core/modules/views/src/Tests/Plugin/StyleSummaryTest.php
+++ core/modules/views/src/Tests/Plugin/StyleSummaryTest.php
@@ -20,7 +20,7 @@ class StyleSummaryTest extends ViewTestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = ['entity_test'];
+  public static $modules = ['entity_test', 'views_ui'];
 
   /**
    * {@inheritdoc}
@@ -48,6 +48,9 @@ class StyleSummaryTest extends ViewTestBase {
         $entity->save();
       }
     }
+
+    $views_user = $this->drupalCreateUser(['administer views']);
+    $this->drupalLogin($views_user);
   }
 
   /**
@@ -72,6 +75,71 @@ class StyleSummaryTest extends ViewTestBase {
     $this->clickLink('type1');
     $entries = $this->cssSelect('div.view-content div.views-row');
     $this->assertEqual(2, count($entries));
+
+    // Add a base path to the summary settings.
+    $edit = [
+      'options[summary][options][default_summary][base_path]' => 'test-summary',
+    ];
+    $this->drupalPostForm('admin/structure/views/nojs/handler/test_summary/page_1/argument/type', $edit, t('Apply'));
+    $this->drupalPostForm(NULL, [], t('Save'));
+
+    // Test that the links still work.
+    $this->drupalGet('test-summary');
+    $this->clickLink('type1');
+    $entries = $this->cssSelect('div.view-content div.views-row');
+    $this->assertEqual(2, count($entries));
+
+    // Change the summary display to an unformatted list displaying 3 items.
+    $edit = [
+      'options[summary][format]' => 'unformatted_summary',
+      'options[summary][options][unformatted_summary][override]' => '1',
+      'options[summary][options][unformatted_summary][items_per_page]' => '3',
+    ];
+    $this->drupalPostForm('admin/structure/views/nojs/handler/test_summary/page_1/argument/type', $edit, t('Apply'));
+    $this->drupalPostForm(NULL, [], t('Save'));
+
+    $this->drupalGet('admin/structure/views/nojs/handler/test_summary/page_1/argument/type');
+    $this->drupalGet('test-summary');
+
+    $summary_list = $this->cssSelect('.views-summary-unformatted');
+    $this->assertEqual(3, count($summary_list));
+
+    foreach ($summary_list as $summary_list_item) {
+      $this->assertEqual('(5)', trim((string) $summary_list_item));
+    }
+
+    $summary_links = $this->cssSelect('.views-summary-unformatted a');
+    $this->assertEqual(3, count($summary_links));
+    foreach ($summary_links as $index => $summary_link) {
+      $this->assertEqual('type' . $index, trim((string) $summary_link));
+    }
+
+    $this->clickLink('type1');
+    $entries = $this->cssSelect('div.view-content div.views-row');
+    $this->assertEqual(2, count($entries));
+
+    // Add a base path to the summary settings.
+    $edit = [
+      'options[summary][options][unformatted_summary][base_path]' => 'test-summary',
+    ];
+    $this->drupalPostForm('admin/structure/views/nojs/handler/test_summary/page_1/argument/type', $edit, t('Apply'));
+    $this->drupalPostForm(NULL, [], t('Save'));
+
+    // Test that the links still work.
+    $this->drupalGet('test-summary');
+    $this->clickLink('type1');
+    $entries = $this->cssSelect('div.view-content div.views-row');
+    $this->assertEqual(2, count($entries));
+
+    // Set base_path to an unknown path and test that the links lead to the
+    // front page.
+    $edit = [
+      'options[summary][options][unformatted_summary][base_path]' => 'unknown-path',
+    ];
+    $this->drupalPostForm('admin/structure/views/nojs/handler/test_summary/page_1/argument/type', $edit, t('Apply'));
+    $this->drupalPostForm(NULL, [], t('Save'));
+    $this->drupalGet('test-summary');
+    $this->assertLinkByHref('/');
   }
 
 }
