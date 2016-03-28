ce7e21fcf5aa30ab65296799d1255f9e295d34c9
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-03-29 08:40:25 +0900

    Issue #2688668 by Berdir, bojanz: Register the new entity revision param enhancer/converter as services
    (cherry picked from commit fff38959a1d5e567f1c39fc6504280826167234f)

--- core/lib/Drupal/Core/Entity/Controller/EntityViewController.php
+++ core/lib/Drupal/Core/Entity/Controller/EntityViewController.php
@@ -104,8 +104,25 @@ class EntityViewController implements ContainerInjectionInterface {
     $page['#entity_type'] = $_entity->getEntityTypeId();
     $page['#' . $page['#entity_type']] = $_entity;
 
-
     return $page;
+  }
+
+  /**
+   * Provides a page to render a single entity revision.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $_entity_revision
+   *   The Entity to be rendered. Note this variable is named $_entity_revision
+   *   rather than $entity to prevent collisions with other named placeholders
+   *   in the route.
+   * @param string $view_mode
+   *   (optional) The view mode that should be used to display the entity.
+   *   Defaults to 'full'.
+   *
+   * @return array
+   *   A render array.
+   */
+  public function viewRevision(EntityInterface $_entity_revision, $view_mode = 'full') {
+    return $this->view($_entity_revision, $view_mode);
   }
 
 }
