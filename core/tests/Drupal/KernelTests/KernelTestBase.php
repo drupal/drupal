5f65d69356f7960a7c4dd755a75a2d8b28912f6b
Alex Pott <alex.a.pott@googlemail.com>
2016-03-31 14:42:00 +0100

    Issue #2661470 by andypost, lauriii: KernelTestBase::render passes wrong values as a parameter for BareHtmlPageRenderer::renderBarePage

--- core/tests/Drupal/KernelTests/KernelTestBase.php
+++ core/tests/Drupal/KernelTests/KernelTestBase.php
@@ -981,9 +981,7 @@ abstract class KernelTestBase extends \PHPUnit_Framework_TestCase implements Ser
 
     // Use the bare HTML page renderer to render our links.
     $renderer = $this->container->get('bare_html_page_renderer');
-    $response = $renderer->renderBarePage(
-      $elements, '', $this->container->get('theme.manager')->getActiveTheme()->getName()
-    );
+    $response = $renderer->renderBarePage($elements, '', 'maintenance_page');
 
     // Glean the content from the response object.
     $content = $response->getContent();
