0964a8ff7b894150f7ef0e4721d3c7b5855bfcde
Alex Pott <alex.a.pott@googlemail.com>
2016-04-26 19:40:26 +0100

    Issue #2711963 by jibran: Modernized ToolbarIntegrationTest
    (cherry picked from commit 7aab1f5c3912901334b028fa74b00f194594c038)

--- core/modules/toolbar/tests/src/FunctionalJavascript/ToolbarIntegrationTest.php
+++ core/modules/toolbar/tests/src/FunctionalJavascript/ToolbarIntegrationTest.php
@@ -23,30 +23,29 @@ class ToolbarIntegrationTest extends JavascriptTestBase {
     $admin_user = $this->drupalCreateUser([
       'access toolbar',
       'administer site configuration',
-      'access content overview'
+      'access content overview',
     ]);
     $this->drupalLogin($admin_user);
 
     $this->drupalGet('<front>');
+    $page = $this->getSession()->getPage();
 
     // Test that it is possible to toggle the toolbar tray.
-    $this->assertElementVisible('#toolbar-link-system-admin_content', 'Toolbar tray is open by default.');
-    $this->click('#toolbar-item-administration');
-    $this->assertElementNotVisible('#toolbar-link-system-admin_content', 'Toolbar tray is closed after clicking the "Manage" button.');
-    $this->click('#toolbar-item-administration');
-    $this->assertElementVisible('#toolbar-link-system-admin_content', 'Toolbar tray is visible again after clicking the "Manage" button a second time.');
+    $content = $page->findLink('Content');
+    $this->assertTrue($content->isVisible(), 'Toolbar tray is open by default.');
+    $page->clickLink('Manage');
+    $this->assertFalse($content->isVisible(), 'Toolbar tray is closed after clicking the "Manage" link.');
+    $page->clickLink('Manage');
+    $this->assertTrue($content->isVisible(), 'Toolbar tray is visible again after clicking the "Manage" button a second time.');
 
     // Test toggling the toolbar tray between horizontal and vertical.
-    $this->assertElementVisible('#toolbar-item-administration-tray.toolbar-tray-horizontal', 'Toolbar tray is horizontally oriented by default.');
-    $this->assertElementNotPresent('#toolbar-item-administration-tray.toolbar-tray-vertical', 'Toolbar tray is not vertically oriented by default.');
+    $tray = $page->findById('toolbar-item-administration-tray');
+    $this->assertFalse($tray->hasClass('toolbar-tray-vertical'), 'Toolbar tray is not vertically oriented by default.');
+    $page->pressButton('Vertical orientation');
+    $this->assertTrue($tray->hasClass('toolbar-tray-vertical'), 'After toggling the orientation the toolbar tray is now displayed vertically.');
 
-    $this->click('#toolbar-item-administration-tray button.toolbar-icon-toggle-vertical');
-    $this->assertJsCondition('jQuery("#toolbar-item-administration-tray").hasClass("toolbar-tray-vertical")');
-    $this->assertElementVisible('#toolbar-item-administration-tray.toolbar-tray-vertical', 'After toggling the orientation the toolbar tray is now displayed vertically.');
-
-    $this->click('#toolbar-item-administration-tray button.toolbar-icon-toggle-horizontal');
-    $this->assertJsCondition('jQuery("#toolbar-item-administration-tray").hasClass("toolbar-tray-horizontal")');
-    $this->assertElementVisible('#toolbar-item-administration-tray.toolbar-tray-horizontal', 'After toggling the orientation a second time the toolbar tray is displayed horizontally again.');
+    $page->pressButton('Horizontal orientation');
+    $this->assertTrue($tray->hasClass('toolbar-tray-horizontal'), 'After toggling the orientation a second time the toolbar tray is displayed horizontally again.');
   }
 
 }
