1003490b9936ad5ad9403fa2ab4fa27c6af17efe
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-04-28 21:46:32 +0100

    Issue #2707261 by alexpott, jhodgdon: Calling moduleInvokeAll in Help block is wrong

--- core/modules/help/src/Tests/HelpBlockTest.php
+++ core/modules/help/src/Tests/HelpBlockTest.php
@@ -14,7 +14,7 @@ class HelpBlockTest extends WebTestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = ['help', 'help_page_test', 'block'];
+  public static $modules = ['help', 'help_page_test', 'block', 'more_help_page_test'];
 
   /**
    * The help block instance.
@@ -39,6 +39,12 @@ class HelpBlockTest extends WebTestBase {
     $this->drupalGet('help_page_test/no_help');
     // The help block should not appear when there is no help.
     $this->assertNoText($this->helpBlock->label());
+
+    // Ensure that if two hook_help() implementations both return a render array
+    // the output is as expected.
+    $this->drupalGet('help_page_test/test_array');
+    $this->assertText('Help text from more_help_page_test_help module.');
+    $this->assertText('Help text from help_page_test_help module.');
   }
 
 }
