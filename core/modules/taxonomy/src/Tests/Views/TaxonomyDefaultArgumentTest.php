4325df9defe2773e225953691b6ed2f6385f515b
Alex Pott <alex.a.pott@googlemail.com>
2016-05-31 10:59:11 +0100

    Issue #2728137 by dawehner, amateescu: \Drupal\taxonomy\Plugin\views\argument_default\Tid doesn't work with views selection plugin
    (cherry picked from commit f809699778a87ee9020240b8775b994bf9d312b2)

--- core/modules/taxonomy/src/Tests/Views/TaxonomyDefaultArgumentTest.php
+++ core/modules/taxonomy/src/Tests/Views/TaxonomyDefaultArgumentTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\taxonomy\Tests\Views;
 
+use Drupal\field\Entity\FieldConfig;
 use Drupal\views\Views;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
@@ -24,6 +25,38 @@ class TaxonomyDefaultArgumentTest extends TaxonomyTestBase {
    * Tests the relationship.
    */
   public function testNodePath() {
+    $view = Views::getView('taxonomy_default_argument_test');
+
+    $request = Request::create($this->nodes[0]->url());
+    $request->server->set('SCRIPT_NAME', $GLOBALS['base_path'] . 'index.php');
+    $request->server->set('SCRIPT_FILENAME', 'index.php');
+
+    $response = $this->container->get('http_kernel')
+      ->handle($request, HttpKernelInterface::SUB_REQUEST);
+    $view->setRequest($request);
+    $view->setResponse($response);
+
+    $view->initHandlers();
+    $expected = implode(',', array($this->term1->id(), $this->term2->id()));
+    $this->assertEqual($expected, $view->argument['tid']->getDefaultArgument());
+    $view->destroy();
+  }
+
+  public function testNodePathWithViewSelection() {
+    // Change the term entity reference field to use a view as selection plugin.
+    \Drupal::service('module_installer')->install(['entity_reference_test']);
+
+    $field_name = 'field_' . $this->vocabulary->id();
+    $field = FieldConfig::loadByName('node', 'article', $field_name);
+    $field->setSetting('handler', 'views');
+    $field->setSetting('handler_settings', [
+      'view' => [
+        'view_name' => 'test_entity_reference',
+        'display_name' => 'entity_reference_1',
+      ],
+    ]);
+    $field->save();
+
     $view = Views::getView('taxonomy_default_argument_test');
 
     $request = Request::create($this->nodes[0]->url());
