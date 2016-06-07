0d9a9c986cb74f39488bf6ff43999e217d8245ec
Alex Pott <alex.a.pott@googlemail.com>
2016-06-07 12:12:37 +0100

    Issue #2735045 by klausi, dawehner, jibran: Convert StandardTest to BrowserTestBase by introducing a AssertLegacyTrait and new assertions on the WebAssert
    (cherry picked from commit a4bf1e9ab1d5c103b8b5a8ad5baee22bc854b2de)

--- core/tests/Drupal/Tests/WebAssert.php
+++ core/tests/Drupal/Tests/WebAssert.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\Tests;
 
+use Behat\Mink\Exception\ExpectationException;
 use Behat\Mink\WebAssert as MinkWebAssert;
 use Behat\Mink\Element\TraversableElement;
 use Behat\Mink\Exception\ElementNotFoundException;
+use Drupal\Component\Utility\Html;
 
 /**
  * Defines a class with methods for asserting presence of elements during tests.
@@ -62,6 +64,85 @@ class WebAssert extends MinkWebAssert {
     }
 
     return $node;
+  }
+
+  /**
+   * Pass if the page title is the given string.
+   *
+   * @param string $expected_title
+   *   The string the page title should be.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   *   Thrown when element doesn't exist, or the title is a different one.
+   */
+  public function titleEquals($expected_title) {
+    $title_element = $this->session->getPage()->find('css', 'title');
+    if (!$title_element) {
+      throw new ExpectationException('No title element found on the page', $this->session);
+    }
+    $actual_title = $title_element->getText();
+    $this->assert($expected_title === $actual_title, 'Title found');
+  }
+
+  /**
+   * Passes if a link with the specified label is found.
+   *
+   * An optional link index may be passed.
+   *
+   * @param string|\Drupal\Component\Render\MarkupInterface $label
+   *   Text between the anchor tags.
+   * @param int $index
+   *   Link position counting from zero.
+   * @param string $message
+   *   (optional) A message to display with the assertion. Do not translate
+   *   messages: use strtr() to embed variables in the message text, not
+   *   t(). If left blank, a default message will be displayed.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   *   Thrown when element doesn't exist, or the link label is a different one.
+   */
+  public function linkExists($label, $index = 0, $message = '') {
+    // Cast MarkupInterface objects to string.
+    $label = (string) $label;
+    $message = ($message ? $message : strtr('Link with label %label found.', ['%label' => $label]));
+    $links = $this->session->getPage()->findAll('named', ['link', $label]);
+    if (empty($links[$index])) {
+      throw new ExpectationException($message);
+    }
+    $this->assert($links[$index] !== NULL, $message);
+  }
+
+  /**
+   * Passes if the raw text IS NOT found escaped on the loaded page.
+   *
+   * Raw text refers to the raw HTML that the page generated.
+   *
+   * @param string $raw
+   *   Raw (HTML) string to look for.
+   */
+  public function assertNoEscaped($raw) {
+    $this->pageTextNotContains(Html::escape($raw));
+  }
+
+  /**
+   * Asserts a condition.
+   *
+   * The parent method is overridden because it is a private method.
+   *
+   * @param bool $condition
+   *   The condition.
+   * @param string $message
+   *   The success message.
+   *
+   * @throws \Behat\Mink\Exception\ExpectationException
+   *   When the condition is not fulfilled.
+   */
+  public function assert($condition, $message) {
+    if ($condition) {
+      return;
+    }
+
+    throw new ExpectationException($message, $this->session->getDriver());
   }
 
 }
