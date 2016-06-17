2bb1b8d039e5bb9e9be1624f1a8b870dee7fc2d5
Alex Pott <alex.a.pott@googlemail.com>
2016-06-17 11:14:14 +0100

    Issue #2742211 by jsdix: Doxygen typo for $cache variable
    (cherry picked from commit 671a41e6580bf2dc46631314875a5a600b00309d)

--- core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorage.php
+++ core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorage.php
@@ -150,7 +150,7 @@ class SqlContentEntityStorage extends ContentEntityStorageBase implements SqlEnt
    *   The database connection to be used.
    * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
    *   The entity manager.
-   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
    *   The cache backend to be used.
    * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
    *   The language manager.
