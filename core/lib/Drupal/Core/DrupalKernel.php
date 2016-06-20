6c230338e3abd58b6bf5c210c5bf5ab384fae523
Ivan Milyukov <ivanmilyukov@gmail.com>
2016-06-20 12:43:51 +0300

    Added absolute path to core.services.yml
    It's needed when I try assign {root}/web/index.php instead  {root}/index.php like symfony application.

--- core/lib/Drupal/Core/DrupalKernel.php
+++ core/lib/Drupal/Core/DrupalKernel.php
@@ -575,7 +575,7 @@ class DrupalKernel implements DrupalKernelInterface, TerminableInterface {
       'app' => array(),
       'site' => array(),
     );
-    $this->serviceYamls['app']['core'] = 'core/core.services.yml';
+    $this->serviceYamls['app']['core'] = $this->root . '/core/core.services.yml';
     $this->serviceProviderClasses['app']['core'] = 'Drupal\Core\CoreServiceProvider';
 
     // Retrieve enabled modules and register their namespaces.
