60a17522f4bf1323af54c32b115289a475afcaa6
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-06-23 23:24:36 +0100

    Issue #2742103 by ekes, dawehner, amateescu: SqlContentEntityStorageSchema::getDedicatedTableSchema() ignores 'unique keys' from the schema definition

--- core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorageSchema.php
+++ core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorageSchema.php
@@ -1837,6 +1837,24 @@ class SqlContentEntityStorageSchema implements DynamicallyFieldableEntityStorage
       }
     }
 
+    // Add unique keys.
+    foreach ($schema['unique keys'] as $index_name => $columns) {
+      $real_name = $this->getFieldIndexName($storage_definition, $index_name);
+      foreach ($columns as $column_name) {
+        // Unique keys can be specified as either a column name or an array with
+        // column name and length. Allow for either case.
+        if (is_array($column_name)) {
+          $data_schema['unique keys'][$real_name][] = array(
+            $table_mapping->getFieldColumnName($storage_definition, $column_name[0]),
+            $column_name[1],
+          );
+        }
+        else {
+          $data_schema['unique keys'][$real_name][] = $table_mapping->getFieldColumnName($storage_definition, $column_name);
+        }
+      }
+    }
+
     // Add foreign keys.
     foreach ($schema['foreign keys'] as $specifier => $specification) {
       $real_name = $this->getFieldIndexName($storage_definition, $specifier);
