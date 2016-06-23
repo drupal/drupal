2660334e4f6f26c1f1390ec6f9e8fa20519383f8
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-06-23 23:24:36 +0100

    Issue #2742103 by ekes, dawehner, amateescu: SqlContentEntityStorageSchema::getDedicatedTableSchema() ignores 'unique keys' from the schema definition
    (cherry picked from commit 60a17522f4bf1323af54c32b115289a475afcaa6)

--- core/tests/Drupal/Tests/Core/Entity/Sql/SqlContentEntityStorageSchemaTest.php
+++ core/tests/Drupal/Tests/Core/Entity/Sql/SqlContentEntityStorageSchemaTest.php
@@ -820,6 +820,16 @@ class SqlContentEntityStorageSchemaTest extends UnitTestCase {
           'length' => 32,
           'not null' => FALSE,
         ),
+        'area' => array(
+          'type' => 'int',
+          'unsigned' => TRUE,
+          'not null' => TRUE,
+        ),
+        'depth' => array(
+          'type' => 'int',
+          'unsigned' => TRUE,
+          'not null' => TRUE,
+        ),
       ),
       'foreign keys' => array(
         'color' => array(
@@ -829,8 +839,14 @@ class SqlContentEntityStorageSchemaTest extends UnitTestCase {
           ),
         ),
       ),
-      'unique keys' => array(),
-      'indexes' => array(),
+      'unique keys' => array(
+        'area' => array('area'),
+        'shape' => array(array('shape', 10)),
+      ),
+      'indexes' => array(
+        'depth' => array('depth'),
+        'color' => array(array('color', 3)),
+      ),
     ));
 
     $field_storage = $this->storageDefinitions[$field_name];
@@ -905,11 +921,27 @@ class SqlContentEntityStorageSchemaTest extends UnitTestCase {
             'length' => 32,
             'not null' => FALSE,
           ),
+          $field_name . '_area' => array(
+            'type' => 'int',
+            'unsigned' => TRUE,
+            'not null' => TRUE,
+          ),
+          $field_name . '_depth' => array(
+            'type' => 'int',
+            'unsigned' => TRUE,
+            'not null' => TRUE,
+          ),
         ),
         'primary key' => array('entity_id', 'deleted', 'delta', 'langcode'),
         'indexes' => array(
           'bundle' => array('bundle'),
           'revision_id' => array('revision_id'),
+          $field_name . '_depth' => array($field_name . '_depth'),
+          $field_name . '_color' => array(array($field_name . '_color', 3)),
+        ),
+        'unique keys' => array(
+           $field_name . '_area' => array($field_name . '_area'),
+           $field_name . '_shape' => array(array($field_name . '_shape', 10)),
         ),
         'foreign keys' => array(
           $field_name . '_color' => array(
