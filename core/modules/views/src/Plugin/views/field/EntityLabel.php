bbcd3bd74dd3c9a986c852bd09ec05a12a025649
Alex Pott <alex.a.pott@googlemail.com>
2016-06-24 13:35:09 +0200

    Issue #2676552 by justAChris, mbovan, chr.fritsch, gaborpeter, mariancalinro, alexpott, Berdir: File usage view breaks if an entity uses a file that has no canonical link template
    (cherry picked from commit 705b75ca9322577aa236b92ae488a7b73792b6a6)

--- core/modules/views/src/Plugin/views/field/EntityLabel.php
+++ core/modules/views/src/Plugin/views/field/EntityLabel.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\views\Plugin\views\field;
 
+use Drupal\Core\Entity\EntityMalformedException;
 use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\Exception\UndefinedLinkTemplateException;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\ResultRow;
 use Drupal\views\ViewExecutable;
@@ -105,8 +107,16 @@ class EntityLabel extends FieldPluginBase {
     $entity = $this->loadedReferencers[$type][$value];
 
     if (!empty($this->options['link_to_entity'])) {
-      $this->options['alter']['make_link'] = TRUE;
-      $this->options['alter']['url'] = $entity->urlInfo();
+      try {
+        $this->options['alter']['url'] = $entity->toUrl();
+        $this->options['alter']['make_link'] = TRUE;
+      }
+      catch (UndefinedLinkTemplateException $e) {
+        $this->options['alter']['make_link'] = FALSE;
+      }
+      catch (EntityMalformedException $e) {
+        $this->options['alter']['make_link'] = FALSE;
+      }
     }
 
     return $this->sanitizeValue($entity->label());
