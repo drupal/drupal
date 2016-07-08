5c4f93fda4ef4967fc49c89556a9f51585b101f6
Alex Pott <alex.a.pott@googlemail.com>
2016-07-08 11:03:24 +0100

    Issue #2761865 by catch, alexpott: Update from 8.13 to 8.14 breaks site seemingly permanently

---
+++ core/modules/user/src/MigratePassword.php
@@ -0,0 +1,5 @@
+<?php
+// @codingStandardsIgnoreFile
+// This file is intentionally empty so that it overwrites when sites are
+// updated from a zip/tarball without deleting the /core folder first.
+// @todo: remove in 8.3.x
