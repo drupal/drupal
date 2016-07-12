7aebc4824927b165ec4bb0a79a41c67be5e5e0b3
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-07-12 10:23:59 +0100

    Issue #2595535 by tstoeckler, swentel, vijaycs85, alvar0hurtad0, esolitos: Show helpful message (do not fatal!) when configuration files have different source language codes and cannot be translated
    (cherry picked from commit fe94d15d309398663d7de6a64c6551dfea6772bf)

--- core/modules/config_translation/src/Access/ConfigTranslationFormAccess.php
+++ core/modules/config_translation/src/Access/ConfigTranslationFormAccess.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\config_translation\Access;
 
+use Drupal\config_translation\ConfigMapperInterface;
+use Drupal\config_translation\Exception\ConfigMapperLanguageException;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -12,27 +14,68 @@ use Drupal\Core\Session\AccountInterface;
 class ConfigTranslationFormAccess extends ConfigTranslationOverviewAccess {
 
   /**
-   * {@inheritdoc}
+   * Checks access to the overview based on permissions and translatability.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route_match to check against.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account to check access for.
+   * @param string $langcode
+   *   The language code of the target language.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
    */
   public function access(RouteMatchInterface $route_match, AccountInterface $account, $langcode = NULL) {
-    // For the translation forms we have a target language, so we need some
-    // checks in addition to the checks performed for the translation overview.
-    $base_access = parent::access($route_match, $account);
-    if ($base_access->isAllowed()) {
+    $mapper = $this->getMapperFromRouteMatch($route_match);
+
+    try {
+      $source_langcode = $mapper->getLangcode();
+      $source_language = $this->languageManager->getLanguage($source_langcode);
+
       $target_language = $this->languageManager->getLanguage($langcode);
 
-      // Make sure that the target language is not locked, and that the target
-      // language is not the original submission language. Although technically
-      // configuration can be overlaid with translations in the same language,
-      // that is logically not a good idea.
-      $access =
-        !empty($target_language) &&
-        !$target_language->isLocked() &&
-        (empty($this->sourceLanguage) || ($target_language->getId() != $this->sourceLanguage->getId()));
-
-      return $base_access->andIf(AccessResult::allowedIf($access));
+      return $this->doCheckAccess($account, $mapper, $source_language, $target_language);
     }
-    return $base_access;
+    catch (ConfigMapperLanguageException $exception) {
+      return AccessResult::forbidden();
+    }
+  }
+
+  /**
+   * Checks access given an account, configuration mapper, and source language.
+   *
+   * In addition to the checks performed by
+   * ConfigTranslationOverviewAccess::doCheckAccess() this makes sure the target
+   * language is not locked and the target language is not the source language.
+   *
+   * Although technically configuration can be overlaid with translations in the
+   * same language, that is logically not a good idea.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account to check access for.
+   * @param \Drupal\config_translation\ConfigMapperInterface $mapper
+   *   The configuration mapper to check access for.
+   * @param \Drupal\Core\Language\LanguageInterface|null $source_language
+   *   The source language to check for, if any.
+   * @param \Drupal\Core\Language\LanguageInterface|null $target_language
+   *   The target language to check for, if any.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The result of the access check.
+   *
+   * @see \Drupal\config_translation\Access\ConfigTranslationOverviewAccess::doCheckAccess()
+   */
+  protected function doCheckAccess(AccountInterface $account, ConfigMapperInterface $mapper, $source_language = NULL, $target_language = NULL) {
+    $base_access_result = parent::doCheckAccess($account, $mapper, $source_language);
+
+    $access =
+      $target_language &&
+      !$target_language->isLocked() &&
+      (!$source_language || ($target_language->getId() !== $source_language->getId()));
+
+    return $base_access_result->andIf(AccessResult::allowedIf($access));
+
   }
 
 }
