3857af8b3e00a04ceddc5173304da06cfb838bb1
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-07-12 12:18:37 +0100

    Issue #2761451 by Wim Leers: Document bootstrap cache bin's purpose
    (cherry picked from commit e17761e462437f701feb79539bce4d562d80255b)

--- core/core.api.php
+++ core/core.api.php
@@ -461,7 +461,8 @@
  * caches.
  *
  * Other common cache bins are the following:
- *   - bootstrap: Small caches needed for the bootstrap on every request.
+ *   - bootstrap: Data needed from the beginning to the end of most requests,
+ *     that has a very strict limit on variations and is invalidated rarely.
  *   - render: Contains cached HTML strings like cached pages and blocks, can
  *     grow to large size.
  *   - data: Contains data that can vary by path or similar context.
