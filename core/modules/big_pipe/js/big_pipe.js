1136c178c2070a4314c9cd203cdcadb92ceb7388
Alex Pott <alex.a.pott@googlemail.com>
2016-07-19 15:05:56 +0100

    Issue #2723705 by Wim Leers, catch: Reduce the 200ms BigPipe placeholder interval to 50ms and make it overridable
    (cherry picked from commit 91987586b8fce970eb24683eca4bff6dff38435d)

--- core/modules/big_pipe/js/big_pipe.js
+++ core/modules/big_pipe/js/big_pipe.js
@@ -89,7 +89,10 @@
     }, interval);
   }
 
-  var interval = 200;
+  // The frequency with which to check for newly arrived BigPipe placeholders.
+  // Hence 50 ms means we check 20 times per second. Setting this to 100 ms or
+  // more would cause the user to see content appear noticeably slower.
+  var interval = drupalSettings.bigPipeInterval || 50;
   // The internal ID to contain the watcher service.
   var timeoutID;
 
