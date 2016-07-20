d317f6b34bdb380002af5c661d4d36cfd1495c30
Alex Pott <alex.a.pott@googlemail.com>
2016-07-20 12:08:04 +0100

    Issue #2348137 by daffie: Enable WAL journal mode by default for SQLite database

--- core/lib/Drupal/Core/Database/Driver/sqlite/Connection.php
+++ core/lib/Drupal/Core/Database/Driver/sqlite/Connection.php
@@ -124,6 +124,16 @@ class Connection extends DatabaseConnection {
     // Create a user-space case-insensitive collation with UTF-8 support.
     $pdo->sqliteCreateCollation('NOCASE_UTF8', array('Drupal\Component\Utility\Unicode', 'strcasecmp'));
 
+    // Set SQLite init_commands if not already defined. Enable the Write-Ahead
+    // Logging (WAL) for SQLite. See https://www.drupal.org/node/2348137 and
+    // https://www.sqlite.org/wal.html.
+    $connection_options += array(
+      'init_commands' => array(),
+    );
+    $connection_options['init_commands'] += array(
+      'wal' => "PRAGMA journal_mode=WAL",
+    );
+
     // Execute sqlite init_commands.
     if (isset($connection_options['init_commands'])) {
       $pdo->exec(implode('; ', $connection_options['init_commands']));
