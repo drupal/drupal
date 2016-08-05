937b9f228d2822b4a0c1a401398089aa52cc0c06
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-08-05 15:58:10 +0100

    Issue #2553733 by alexpott, dawehner, Wim Leers, jibran: BrowserTestBase should be adding the config schema checker like WebTestBase

---
+++ core/tests/Drupal/FunctionalTests/Core/Config/SchemaConfigListenerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\FunctionalTests\Core\Config;
+
+use Drupal\Tests\BrowserTestBase;
+use Drupal\Tests\Traits\Core\Config\SchemaConfigListenerTestTrait;
+
+/**
+ * Tests the functionality of ConfigSchemaChecker in KernelTestBase tests.
+ *
+ * @group config
+ */
+class SchemaConfigListenerTest extends BrowserTestBase {
+
+  use SchemaConfigListenerTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = array('config_test');
+
+}
---
+++ core/tests/Drupal/Tests/Traits/Core/Config/SchemaConfigListenerTestTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\Tests\Traits\Core\Config;
+
+use \Drupal\Core\Config\Schema\SchemaIncompleteException;
+
+/**
+ * Adds a test for the configuration schema checker use in tests.
+ */
+trait SchemaConfigListenerTestTrait {
+
+  /**
+   * Tests \Drupal\Core\Config\Testing\ConfigSchemaChecker.
+   */
+  public function testConfigSchemaChecker() {
+    // Test a non-existing schema.
+    $message = 'Expected SchemaIncompleteException thrown';
+    try {
+      $this->config('config_schema_test.schemaless')->set('foo', 'bar')->save();
+      $this->fail($message);
+    }
+    catch (SchemaIncompleteException $e) {
+      $this->pass($message);
+      $this->assertEqual('No schema for config_schema_test.schemaless', $e->getMessage());
+    }
+
+    // Test a valid schema.
+    $message = 'Unexpected SchemaIncompleteException thrown';
+    $config = $this->config('config_test.types')->set('int', 10);
+    try {
+      $config->save();
+      $this->pass($message);
+    }
+    catch (SchemaIncompleteException $e) {
+      $this->fail($message);
+    }
+
+    // Test an invalid schema.
+    $message = 'Expected SchemaIncompleteException thrown';
+    $config = $this->config('config_test.types')
+      ->set('foo', 'bar')
+      ->set('array', 1);
+    try {
+      $config->save();
+      $this->fail($message);
+    }
+    catch (SchemaIncompleteException $e) {
+      $this->pass($message);
+      $this->assertEqual('Schema errors for config_test.types with the following errors: config_test.types:array variable type is integer but applied schema class is Drupal\Core\Config\Schema\Sequence, config_test.types:foo missing schema', $e->getMessage());
+    }
+
+  }
+
+}
