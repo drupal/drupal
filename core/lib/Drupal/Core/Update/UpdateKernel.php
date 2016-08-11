27b2c4ff1b114d5e33e4a46bed373f529b9bc91a
Alex Pott <alex.a.pott@googlemail.com>
2016-08-11 14:16:15 +0100

    Issue #2781953 by vpshah86, fgm: Update.php still refers to $update_free_access

--- core/lib/Drupal/Core/Update/UpdateKernel.php
+++ core/lib/Drupal/Core/Update/UpdateKernel.php
@@ -182,7 +182,7 @@ class UpdateKernel extends DrupalKernel {
     $db_update_access = $this->getContainer()->get('access_check.db_update');
 
     if (!Settings::get('update_free_access', FALSE) && !$db_update_access->access($account)->isAllowed()) {
-      throw new AccessDeniedHttpException('In order to run update.php you need to either be logged in as admin or have set $update_free_access in your settings.php.');
+      throw new AccessDeniedHttpException('In order to run update.php you need to either be logged in as admin or have set $settings[\'update_free_access\'] in your settings.php.');
     }
   }
 
