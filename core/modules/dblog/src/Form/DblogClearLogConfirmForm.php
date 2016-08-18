0a8686acb32ffe109c1f22b2582214bce626847f
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-08-18 09:16:17 +0100

    Issue #2494221 by david_garcia, dagmar: Slow Log/Watchdog clear when there are lots of data

--- core/modules/dblog/src/Form/DblogClearLogConfirmForm.php
+++ core/modules/dblog/src/Form/DblogClearLogConfirmForm.php
@@ -65,7 +65,7 @@ class DblogClearLogConfirmForm extends ConfirmFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $_SESSION['dblog_overview_filter'] = array();
-    $this->connection->delete('watchdog')->execute();
+    $this->connection->truncate('watchdog')->execute();
     drupal_set_message($this->t('Database log cleared.'));
     $form_state->setRedirectUrl($this->getCancelUrl());
   }
