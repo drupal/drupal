462c71961fbe5e2b71bed85ca41b5a573bf88c5c
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-08-19 14:26:08 +0100

    Issue #2740983 by VitalyM, alexpott: Configuration system doesn't allow importing a single item from a non-default collection
    (cherry picked from commit d97ed166bd3a335fd7cf94d6b5e0a715955364d2)

--- core/modules/config/src/StorageReplaceDataWrapper.php
+++ core/modules/config/src/StorageReplaceDataWrapper.php
@@ -44,6 +44,7 @@ class StorageReplaceDataWrapper implements StorageInterface {
   public function __construct(StorageInterface $storage, $collection = StorageInterface::DEFAULT_COLLECTION) {
     $this->storage = $storage;
     $this->collection = $collection;
+    $this->replacementData[$collection] = [];
   }
 
   /**
@@ -104,7 +105,7 @@ class StorageReplaceDataWrapper implements StorageInterface {
       $this->replacementData[$this->collection][$new_name] = $this->replacementData[$this->collection][$name];
       unset($this->replacementData[$this->collection][$name]);
     }
-    return $this->rename($name, $new_name);
+    return $this->storage->rename($name, $new_name);
   }
 
   /**
@@ -164,8 +165,10 @@ class StorageReplaceDataWrapper implements StorageInterface {
    * {@inheritdoc}
    */
   public function createCollection($collection) {
-    $this->collection = $collection;
-    return $this->storage->createCollection($collection);
+    return new static(
+      $this->storage->createCollection($collection),
+      $collection
+    );
   }
 
   /**
