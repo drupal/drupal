73306d92f4ae75ff141e3d54eb2769dbb2a365ee
Alex Pott <alex.a.pott@googlemail.com>
2016-08-23 20:06:45 +0100

    Issue #2782967 by mikeker: FormTest::testFieldFormUnlimitedRequired claims to test for visually-hidden, but doesn't

--- core/modules/field/src/Tests/FormTest.php
+++ core/modules/field/src/Tests/FormTest.php
@@ -334,7 +334,7 @@ class FormTest extends FieldTestBase {
     $this->assertTrue(isset($element[0]), 'Required symbol added field label.');
     // Check that the label of the field input is visually hidden and contains
     // the field title and an indication of the delta for a11y.
-    $element = $this->xpath('//label[@for=:for and contains(@class, "js-form-required") and contains(text(), :value)]', array(':for' => 'edit-field-unlimited-0-value', ':value' => $this->field['label'] . ' (value 1)'));
+    $element = $this->xpath('//label[@for=:for and contains(@class, "visually-hidden") and contains(text(), :value)]', array(':for' => 'edit-field-unlimited-0-value', ':value' => $this->field['label'] . ' (value 1)'));
     $this->assertTrue(isset($element[0]), 'Required symbol not added for field input.');
   }
 
