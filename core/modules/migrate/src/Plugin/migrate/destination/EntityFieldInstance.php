f73729a3c9bd634d8bf2602ec53d247c8dddced7
Alex Pott <alex.a.pott@googlemail.com>
2016-08-26 14:22:16 +0100

    Issue #2706951 by Jo Fitzgerald: Field instance migration rollback fails
    (cherry picked from commit 7f5305b02629a9e79b25d506d949637629b939c4)

--- core/modules/migrate/src/Plugin/migrate/destination/EntityFieldInstance.php
+++ core/modules/migrate/src/Plugin/migrate/destination/EntityFieldInstance.php
@@ -21,4 +21,12 @@ class EntityFieldInstance extends EntityConfigBase {
     return $ids;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function rollback(array $destination_identifier) {
+    $destination_identifier = implode('.', $destination_identifier);
+    parent::rollback(array($destination_identifier));
+  }
+
 }
