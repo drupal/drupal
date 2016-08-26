7c11b0f4be2c41448851a7cabfb5cbf5d857d38e
Alex Pott <alex.a.pott@googlemail.com>
2016-08-26 14:26:55 +0100

    Issue #2213671 by jhedstrom, richardcanoe, andypost, david_garcia, Antonnavi, dagmar, aleevas, dawehner, alexpott: Log completely unusable when an entry has corrupt serialized data
    (cherry picked from commit 72c887b8f3c338dc05d56e6b2151b2c8ca9bad91)

--- core/modules/dblog/src/Controller/DbLogController.php
+++ core/modules/dblog/src/Controller/DbLogController.php
@@ -343,14 +343,18 @@ class DbLogController extends ControllerBase {
    */
   public function formatMessage($row) {
     // Check for required properties.
-    if (isset($row->message) && isset($row->variables)) {
+    if (isset($row->message, $row->variables)) {
+      $variables = @unserialize($row->variables);
       // Messages without variables or user specified text.
-      if ($row->variables === 'N;') {
+      if ($variables === NULL) {
         $message = Xss::filterAdmin($row->message);
+      }
+      elseif (!is_array($variables)) {
+        $message = $this->t('Log data is corrupted and cannot be unserialized: @message', ['@message' => Xss::filterAdmin($row->message)]);
       }
       // Message to translate with injected variables.
       else {
-        $message = $this->t(Xss::filterAdmin($row->message), unserialize($row->variables));
+        $message = $this->t(Xss::filterAdmin($row->message), $variables);
       }
     }
     else {
