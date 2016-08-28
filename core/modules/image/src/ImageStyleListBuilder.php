f37eba61fcfa6cb5cf1795a32d7415e486b4a7d6
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-08-28 22:11:52 +0100

    Issue #2490290 by willzyx, Mile23, alexpott, tim.plunkett: Subclasses of EntityListBuilder incorrectly override the #empty message

--- core/modules/image/src/ImageStyleListBuilder.php
+++ core/modules/image/src/ImageStyleListBuilder.php
@@ -4,10 +4,7 @@ namespace Drupal\image;
 
 use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\Core\Entity\EntityTypeInterface;
-use Drupal\Core\Routing\UrlGeneratorInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Url;
 
 /**
  * Defines a class to build a listing of image style entities.
@@ -15,40 +12,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @see \Drupal\image\Entity\ImageStyle
  */
 class ImageStyleListBuilder extends ConfigEntityListBuilder {
-
-  /**
-   * The URL generator.
-   *
-   * @var \Drupal\Core\Routing\UrlGeneratorInterface
-   */
-  protected $urlGenerator;
-
-  /**
-   * Constructs a new ImageStyleListBuilder object.
-   *
-   * @param EntityTypeInterface $entity_type
-   *   The entity type definition.
-   * @param \Drupal\Core\Entity\EntityStorageInterface $image_style_storage
-   *   The image style entity storage class.
-   * @param \Drupal\Core\Routing\UrlGeneratorInterface $url_generator
-   *   The URL generator.
-   */
-  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $image_style_storage, UrlGeneratorInterface $url_generator) {
-    parent::__construct($entity_type, $image_style_storage);
-    $this->urlGenerator = $url_generator;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
-    return new static(
-      $entity_type,
-      $container->get('entity.manager')->getStorage($entity_type->id()),
-      $container->get('url_generator'),
-      $container->get('string_translation')
-    );
-  }
 
   /**
    * {@inheritdoc}
@@ -87,7 +50,7 @@ class ImageStyleListBuilder extends ConfigEntityListBuilder {
   public function render() {
     $build = parent::render();
     $build['table']['#empty'] = $this->t('There are currently no styles. <a href=":url">Add a new one</a>.', [
-      ':url' => $this->urlGenerator->generateFromRoute('image.style_add'),
+      ':url' => Url::fromRoute('image.style_add')->toString(),
     ]);
     return $build;
   }
