a46d123bc708f8254562aa56d6ca49c1d17a6bcb
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-08-29 07:56:30 +0100

    Issue #2786195 by larowlan: Deleted block is not removed from the block list

--- core/modules/block_content/src/Entity/BlockContent.php
+++ core/modules/block_content/src/Entity/BlockContent.php
@@ -104,9 +104,15 @@ class BlockContent extends ContentEntityBase implements BlockContentInterface {
    */
   public function postSave(EntityStorageInterface $storage, $update = TRUE) {
     parent::postSave($storage, $update);
+    static::invalidateBlockPluginCache();
+  }
 
-    // Invalidate the block cache to update custom block-based derivatives.
-    \Drupal::service('plugin.manager.block')->clearCachedDefinitions();
+  /**
+   * {@inheritdoc}
+   */
+  public static function postDelete(EntityStorageInterface $storage, array $entities) {
+    parent::postDelete($storage, $entities);
+    static::invalidateBlockPluginCache();
   }
 
   /**
@@ -235,6 +241,14 @@ class BlockContent extends ContentEntityBase implements BlockContentInterface {
   public function setRevisionLog($revision_log) {
     $this->set('revision_log', $revision_log);
     return $this;
+  }
+
+  /**
+   * Invalidates the block plugin cache after changes and deletions.
+   */
+  protected static function invalidateBlockPluginCache() {
+    // Invalidate the block cache to update custom block-based derivatives.
+    \Drupal::service('plugin.manager.block')->clearCachedDefinitions();
   }
 
 }
---
+++ core/modules/block_content/tests/src/Kernel/BlockContentDeletionTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\Tests\block_content\Kernel;
+
+use Drupal\block_content\Entity\BlockContent;
+use Drupal\block_content\Entity\BlockContentType;
+use Drupal\Component\Plugin\PluginBase;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests that deleting a block clears the cached definitions.
+ *
+ * @group block_content
+ */
+class BlockContentDeletionTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['block', 'block_content', 'system', 'user'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+    $this->installSchema('system', ['sequence']);
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('block_content');
+  }
+
+  /**
+   * Tests deleting a block_content updates the discovered block plugin.
+   */
+  public function testDeletingBlockContentShouldClearPluginCache() {
+    // Create a block content type.
+    $block_content_type = BlockContentType::create([
+      'id' => 'spiffy',
+      'label' => 'Mucho spiffy',
+      'description' => "Provides a block type that increases your site's spiffiness by upto 11%",
+    ]);
+    $block_content_type->save();
+    // And a block content entity.
+    $block_content = BlockContent::create([
+      'info' => 'Spiffy prototype',
+      'type' => 'spiffy',
+    ]);
+    $block_content->save();
+
+    // Make sure the block content provides a derivative block plugin in the
+    // block repository.
+    /** @var \Drupal\Core\Block\BlockManagerInterface $block_manager */
+    $block_manager = $this->container->get('plugin.manager.block');
+    $plugin_id = 'block_content' . PluginBase::DERIVATIVE_SEPARATOR . $block_content->uuid();
+    $this->assertTrue($block_manager->hasDefinition($plugin_id));
+
+    // Now delete the block content entity.
+    $block_content->delete();
+    // The plugin should no longer exist.
+    $this->assertFalse($block_manager->hasDefinition($plugin_id));
+  }
+
+}
