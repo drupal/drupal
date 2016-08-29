482ae5084ede0a3306b97af15aef21c5f4d4cdff
Alex Pott <alex.a.pott@googlemail.com>
2016-08-29 06:56:46 -0500

    Issue #2764699 by catch, lainosantos: class "Drupal\user\UserServiceProvider" not found
    (cherry picked from commit bfd9b82de8d13170a2e95e51c39f4d52a9f5cacf)

--- core/modules/user/src/UserServiceProvider.php
+++ core/modules/user/src/UserServiceProvider.php
@@ -1,5 +1,8 @@
 <?php
 // @codingStandardsIgnoreFile
-// This file is intentionally empty so that it overwrites when sites are
+// This class is intentionally empty so that it overwrites when sites are
 // updated from a zip/tarball without deleting the /core folder first.
 // @todo: remove in 8.3.x
+//
+namespace Drupal\user;
+class UserServiceProvider {}
