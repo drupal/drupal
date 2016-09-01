07c10807ccb828479d122aa46580a923e2301348
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-09-01 12:24:16 +0100

    Issue #2117251 by tim.plunkett, markdorison, dawehner: Convert _access_tracker_own_information to a one-off access checker

--- core/modules/tracker/src/Controller/TrackerUserRecent.php
+++ core/modules/tracker/src/Controller/TrackerUserRecent.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\tracker\Controller;
 
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\user\UserInterface;
 
 /**
@@ -16,6 +18,22 @@ class TrackerUserRecent extends ControllerBase {
   public function getContent(UserInterface $user) {
     module_load_include('inc', 'tracker', 'tracker.pages');
     return tracker_page($user);
+  }
+
+  /**
+   * Checks access for the users recent content tracker page.
+   *
+   * @param \Drupal\user\UserInterface $user
+   *   The user being viewed.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The account viewing the page.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   The access result.
+   */
+  public function checkAccess(UserInterface $user, AccountInterface $account) {
+    return AccessResult::allowedIf($account->isAuthenticated() && $user->id() == $account->id())
+      ->cachePerUser();
   }
 
 }
