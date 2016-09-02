a4bc5fb6d8f1602a8423790b9be76552c3d1af5c
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-09-02 12:06:40 +0100

    Issue #2654628 by gnuget, lokapujya: Deprecate PluginInterface

--- core/modules/views/src/Plugin/views/PluginInterface.php
+++ core/modules/views/src/Plugin/views/PluginInterface.php
@@ -4,6 +4,9 @@ namespace Drupal\views\Plugin\views;
 
 use Drupal\Component\Plugin\PluginInspectionInterface;
 
+/**
+ * @deprecated as of Drupal 8.3.x, will be removed in Drupal 9.0.0
+ */
 interface PluginInterface extends PluginInspectionInterface {
 
 }
