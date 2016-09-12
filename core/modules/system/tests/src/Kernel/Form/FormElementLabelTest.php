abb19f57c3a9921f5094d2d6c45c9625f6c1559c
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-09-12 14:18:07 +0100

    Issue #2774195 by lauriii, claudiu.cristea: Passing attributes to field element label is not possible

---
+++ core/modules/system/tests/src/Kernel/Form/FormElementLabelTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\Tests\system\Kernel\Form;
+
+use Drupal\KernelTests\KernelTestBase;
+use Symfony\Component\CssSelector\CssSelectorConverter;
+
+/**
+ * Tests for form_element_label theme hook.
+ *
+ * @group Form
+ */
+class FormElementLabelTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['system'];
+
+  /**
+   * Ensures that attributes can be placed for form element label.
+   */
+  public function testAttributes() {
+    $render_array = [
+      '#type' => 'label',
+      '#attributes' => ['class' => ['kitten']],
+      '#title' => 'Kittens',
+      '#title_display' => 'above',
+    ];
+    $css_selector_converter = new CssSelectorConverter();
+    $this->render($render_array);
+    $elements = $this->xpath($css_selector_converter->toXPath('.kitten'));
+    $this->assertCount(1, $elements);
+  }
+
+}
