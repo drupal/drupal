441357f2864ffd9d78b8c14b31356cdbfd1622fa
Alex Pott <alex.a.pott@googlemail.com>
2016-09-19 11:44:18 +0100

    Issue #2795615 by GoZ: Rename LanguageUILanguageNegotiationTest::runTest to LanguageUILanguageNegotiationTest::doRunTest

--- core/modules/language/src/Tests/LanguageUILanguageNegotiationTest.php
+++ core/modules/language/src/Tests/LanguageUILanguageNegotiationTest.php
@@ -136,7 +136,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       'http_header' => $http_header_browser_fallback,
       'message' => 'SELECTED: UI language is switched based on selected language.',
     );
-    $this->runTest($test);
+    $this->doRunTest($test);
 
     // An invalid language is selected.
     $this->config('language.negotiation')->set('selected_langcode', NULL)->save();
@@ -148,7 +148,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       'http_header' => $http_header_browser_fallback,
       'message' => 'SELECTED > DEFAULT: UI language is switched based on selected language.',
     );
-    $this->runTest($test);
+    $this->doRunTest($test);
 
     // No selected language is available.
     $this->config('language.negotiation')->set('selected_langcode', $langcode_unknown)->save();
@@ -160,7 +160,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       'http_header' => $http_header_browser_fallback,
       'message' => 'SELECTED > DEFAULT: UI language is switched based on selected language.',
     );
-    $this->runTest($test);
+    $this->doRunTest($test);
 
     $tests = array(
       // Default, browser preference should have no influence.
@@ -211,7 +211,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
     );
 
     foreach ($tests as $test) {
-      $this->runTest($test);
+      $this->doRunTest($test);
     }
 
     // Unknown language prefix should return 404.
@@ -240,7 +240,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       'http_header' => array(),
       'message' => 'USER > DEFAULT: no preferred user language setting, the UI language is default',
     );
-    $this->runTest($test);
+    $this->doRunTest($test);
 
     // Set preferred langcode for user to unknown language.
     $account = $this->loggedInUser;
@@ -255,7 +255,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       'http_header' => array(),
       'message' => 'USER > DEFAULT: invalid preferred user language setting, the UI language is default',
     );
-    $this->runTest($test);
+    $this->doRunTest($test);
 
     // Set preferred langcode for user to non default.
     $account->preferred_langcode = $langcode;
@@ -269,7 +269,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       'http_header' => array(),
       'message' => 'USER > DEFAULT: defined preferred user language setting, the UI language is based on user setting',
     );
-    $this->runTest($test);
+    $this->doRunTest($test);
 
     // Set preferred admin langcode for user to NULL.
     $account->preferred_admin_langcode = NULL;
@@ -283,7 +283,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       'http_header' => array(),
       'message' => 'USER ADMIN > DEFAULT: no preferred user admin language setting, the UI language is default',
     );
-    $this->runTest($test);
+    $this->doRunTest($test);
 
     // Set preferred admin langcode for user to unknown language.
     $account->preferred_admin_langcode = $langcode_unknown;
@@ -297,7 +297,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       'http_header' => array(),
       'message' => 'USER ADMIN > DEFAULT: invalid preferred user admin language setting, the UI language is default',
     );
-    $this->runTest($test);
+    $this->doRunTest($test);
 
     // Set preferred admin langcode for user to non default.
     $account->preferred_admin_langcode = $langcode;
@@ -311,7 +311,7 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       'http_header' => array(),
       'message' => 'USER ADMIN > DEFAULT: defined preferred user admin language setting, the UI language is based on user setting',
     );
-    $this->runTest($test);
+    $this->doRunTest($test);
 
     // Go by session preference.
     $language_negotiation_session_param = $this->randomMachineName();
@@ -337,11 +337,11 @@ class LanguageUILanguageNegotiationTest extends WebTestBase {
       ),
     );
     foreach ($tests as $test) {
-      $this->runTest($test);
+      $this->doRunTest($test);
     }
   }
 
-  protected function runTest($test) {
+  protected function doRunTest($test) {
     $test += array('path_options' => []);
     if (!empty($test['language_negotiation'])) {
       $method_weights = array_flip($test['language_negotiation']);
