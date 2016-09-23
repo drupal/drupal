c9c12c59dbbd4647a0e5b44d9a6be43e1727d58f
Alex Pott <alex.a.pott@googlemail.com>
2016-09-23 23:56:53 +0100

    Issue #2802719 by maxocub: Publishing status option migrate to empty checkbox

---
+++ core/modules/node/tests/src/Kernel/Migrate/d6/MigrateNodeSettingStatusTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\Tests\node\Kernel\Migrate\d6;
+
+use Drupal\Core\Field\Entity\BaseFieldOverride;
+use Drupal\Tests\migrate_drupal\Kernel\d6\MigrateDrupal6TestBase;
+
+/**
+ * @group migrate_drupal_6
+ */
+class MigrateNodeSettingStatusTest extends MigrateDrupal6TestBase {
+
+  public static $modules = ['node', 'text'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installConfig(['node']);
+    $this->executeMigration('d6_node_type');
+    $this->executeMigration('d6_node_setting_status');
+  }
+
+  /**
+   * Tests migration of the publishing status checkbox's settings.
+   */
+  public function testMigration() {
+    $this->assertIdentical('Publishing status', BaseFieldOverride::load('node.article.status')->label());
+  }
+
+}
