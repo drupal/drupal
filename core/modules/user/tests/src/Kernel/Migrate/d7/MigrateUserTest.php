3d7c5304db0c2b27fe2241ce4a921eb533237f89
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-09-27 12:50:29 +0100

    Issue #2674152 by Jo Fitzgerald, hussainweb: D7 User fields content not migrating first time

--- core/modules/user/tests/src/Kernel/Migrate/d7/MigrateUserTest.php
+++ core/modules/user/tests/src/Kernel/Migrate/d7/MigrateUserTest.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\Tests\user\Kernel\Migrate\d7;
 
+use Drupal\comment\Entity\CommentType;
+use Drupal\node\Entity\NodeType;
+use Drupal\taxonomy\Entity\Vocabulary;
 use Drupal\Tests\migrate_drupal\Kernel\d7\MigrateDrupal7TestBase;
 use Drupal\user\Entity\User;
 use Drupal\user\RoleInterface;
@@ -17,7 +20,18 @@ class MigrateUserTest extends MigrateDrupal7TestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = ['file', 'image'];
+  public static $modules = [
+    'comment',
+    'datetime',
+    'file',
+    'image',
+    'link',
+    'node',
+    'system',
+    'taxonomy',
+    'telephone',
+    'text',
+  ];
 
   /**
    * {@inheritdoc}
@@ -27,12 +41,40 @@ class MigrateUserTest extends MigrateDrupal7TestBase {
 
     // Prepare to migrate user pictures as well.
     $this->installEntitySchema('file');
+    $this->createType('page');
+    $this->createType('article');
+    $this->createType('blog');
+    $this->createType('book');
+    $this->createType('forum');
+    $this->createType('test_content_type');
+    Vocabulary::create(['vid' => 'test_vocabulary'])->save();
     $this->executeMigrations([
       'user_picture_field',
       'user_picture_field_instance',
       'd7_user_role',
+      'd7_field',
+      'd7_field_instance',
       'd7_user',
     ]);
+  }
+
+  /**
+   * Creates a node type with a corresponding comment type.
+   *
+   * @param string $id
+   *   The node type ID.
+   */
+  protected function createType($id) {
+    NodeType::create([
+      'type' => $id,
+      'label' => $this->randomString(),
+    ])->save();
+
+    CommentType::create([
+      'id' => 'comment_node_' . $id,
+      'label' => $this->randomString(),
+      'target_entity_type_id' => 'node',
+    ])->save();
   }
 
   /**
@@ -60,8 +102,10 @@ class MigrateUserTest extends MigrateDrupal7TestBase {
    *   Role IDs the user account is expected to have.
    * @param bool $has_picture
    *   Whether the user is expected to have a picture attached.
+   * @param int $field_integer
+   *   The value of the integer field.
    */
-  protected function assertEntity($id, $label, $mail, $password, $access, $login, $blocked, $langcode, $init, array $roles = [RoleInterface::AUTHENTICATED_ID], $has_picture = FALSE) {
+  protected function assertEntity($id, $label, $mail, $password, $access, $login, $blocked, $langcode, $init, array $roles = [RoleInterface::AUTHENTICATED_ID], $has_picture = FALSE, $field_integer = NULL) {
     /** @var \Drupal\user\UserInterface $user */
     $user = User::load($id);
     $this->assertTrue($user instanceof UserInterface);
@@ -80,6 +124,10 @@ class MigrateUserTest extends MigrateDrupal7TestBase {
     $this->assertIdentical($roles, $user->getRoles());
     $this->assertIdentical($has_picture, !$user->user_picture->isEmpty());
     $this->assertIdentical($password, $user->getPassword());
+    if (!is_null($field_integer)) {
+      $this->assertTrue($user->hasField('field_integer'));
+      $this->assertEquals($field_integer, $user->field_integer->value);
+    }
   }
 
   /**
@@ -87,7 +135,7 @@ class MigrateUserTest extends MigrateDrupal7TestBase {
    */
   public function testUser() {
     $password = '$S$DGFZUE.FhrXbe4y52eC7p0ZVRGD/gOPtVctDlmC89qkujnBokAlJ';
-    $this->assertEntity(2, 'Odo', 'odo@local.host', $password, '0', '0', FALSE, '', 'odo@local.host');
+    $this->assertEntity(2, 'Odo', 'odo@local.host', $password, '0', '0', FALSE, 'en', 'odo@local.host', [RoleInterface::AUTHENTICATED_ID], FALSE, 99);
 
     // Ensure that the user can authenticate.
     $this->assertEquals(2, \Drupal::service('user.auth')->authenticate('Odo', 'a password'));
