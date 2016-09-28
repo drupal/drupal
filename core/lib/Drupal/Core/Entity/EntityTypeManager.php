380bc5a44b78539a849fe648698f1fcf1cef740d
Alex Pott <alex.a.pott@googlemail.com>
2016-09-28 13:26:58 +0100

    Issue #2804525 by shashikant_chauhan, joachim: EntityTypeManager should mention hook_entity_type_build()

--- core/lib/Drupal/Core/Entity/EntityTypeManager.php
+++ core/lib/Drupal/Core/Entity/EntityTypeManager.php
@@ -17,13 +17,21 @@ use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 /**
  * Manages entity type plugin definitions.
  *
- * Each entity type definition array is set in the entity type's
- * annotation and altered by hook_entity_type_alter().
+ * Each entity type definition array is set in the entity type's annotation and
+ * altered by hook_entity_type_alter().
+ *
+ * Do not use hook_entity_type_alter() hook to add information to entity types,
+ * unless one of the following is true:
+ * - You are filling in default values.
+ * - You need to dynamically add information only in certain circumstances.
+ * - Your hook needs to run after hook_entity_type_build() implementations.
+ * Use hook_entity_type_build() instead in all other cases.
  *
  * @see \Drupal\Core\Entity\Annotation\EntityType
  * @see \Drupal\Core\Entity\EntityInterface
  * @see \Drupal\Core\Entity\EntityTypeInterface
  * @see hook_entity_type_alter()
+ * @see hook_entity_type_build()
  */
 class EntityTypeManager extends DefaultPluginManager implements EntityTypeManagerInterface, ContainerAwareInterface {
 
