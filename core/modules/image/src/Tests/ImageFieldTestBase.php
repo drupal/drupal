66741e368b1bbb2ab2348a0028dbc9dc2b749f25
Alex Pott <alex.a.pott@googlemail.com>
2016-10-03 14:01:46 +0100

    Issue #2782309 by samuel.mortenson, martin107: Refactor File and Image related image field creation logic into a new trait

--- core/modules/image/src/Tests/ImageFieldTestBase.php
+++ core/modules/image/src/Tests/ImageFieldTestBase.php
@@ -2,9 +2,8 @@
 
 namespace Drupal\image\Tests;
 
-use Drupal\field\Entity\FieldConfig;
+use Drupal\Tests\image\Kernel\ImageFieldCreationTrait;
 use Drupal\simpletest\WebTestBase;
-use Drupal\field\Entity\FieldStorageConfig;
 
 /**
  * TODO: Test the following functions.
@@ -23,6 +22,8 @@ use Drupal\field\Entity\FieldStorageConfig;
  * This class provides methods specifically for testing Image's field handling.
  */
 abstract class ImageFieldTestBase extends WebTestBase {
+
+  use ImageFieldCreationTrait;
 
   /**
    * Modules to enable.
@@ -49,62 +50,6 @@ abstract class ImageFieldTestBase extends WebTestBase {
 
     $this->adminUser = $this->drupalCreateUser(array('access content', 'access administration pages', 'administer site configuration', 'administer content types', 'administer node fields', 'administer nodes', 'create article content', 'edit any article content', 'delete any article content', 'administer image styles', 'administer node display'));
     $this->drupalLogin($this->adminUser);
-  }
-
-  /**
-   * Create a new image field.
-   *
-   * @param string $name
-   *   The name of the new field (all lowercase), exclude the "field_" prefix.
-   * @param string $type_name
-   *   The node type that this field will be added to.
-   * @param array $storage_settings
-   *   A list of field storage settings that will be added to the defaults.
-   * @param array $field_settings
-   *   A list of instance settings that will be added to the instance defaults.
-   * @param array $widget_settings
-   *   Widget settings to be added to the widget defaults.
-   * @param array $formatter_settings
-   *   Formatter settings to be added to the formatter defaults.
-   * @param string $description
-   *   A description for the field.
-   */
-  function createImageField($name, $type_name, $storage_settings = array(), $field_settings = array(), $widget_settings = array(), $formatter_settings = array(), $description = '') {
-    FieldStorageConfig::create(array(
-      'field_name' => $name,
-      'entity_type' => 'node',
-      'type' => 'image',
-      'settings' => $storage_settings,
-      'cardinality' => !empty($storage_settings['cardinality']) ? $storage_settings['cardinality'] : 1,
-    ))->save();
-
-    $field_config = FieldConfig::create([
-      'field_name' => $name,
-      'label' => $name,
-      'entity_type' => 'node',
-      'bundle' => $type_name,
-      'required' => !empty($field_settings['required']),
-      'settings' => $field_settings,
-      'description' => $description,
-    ]);
-    $field_config->save();
-
-    entity_get_form_display('node', $type_name, 'default')
-      ->setComponent($name, array(
-        'type' => 'image_image',
-        'settings' => $widget_settings,
-      ))
-      ->save();
-
-    entity_get_display('node', $type_name, 'default')
-      ->setComponent($name, array(
-        'type' => 'image',
-        'settings' => $formatter_settings,
-      ))
-      ->save();
-
-    return $field_config;
-
   }
 
   /**
