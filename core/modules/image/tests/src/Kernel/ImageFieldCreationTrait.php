18da00650a6f4e4f22ef9458e724fbda0edab77a
Alex Pott <alex.a.pott@googlemail.com>
2016-10-03 14:01:46 +0100

    Issue #2782309 by samuel.mortenson, martin107: Refactor File and Image related image field creation logic into a new trait
    (cherry picked from commit 66741e368b1bbb2ab2348a0028dbc9dc2b749f25)

---
+++ core/modules/image/tests/src/Kernel/ImageFieldCreationTrait.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\Tests\image\Kernel;
+
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+
+/**
+ * Provides a helper method for creating Image fields.
+ */
+trait ImageFieldCreationTrait {
+
+  /**
+   * Create a new image field.
+   *
+   * @param string $name
+   *   The name of the new field (all lowercase), exclude the "field_" prefix.
+   * @param string $type_name
+   *   The node type that this field will be added to.
+   * @param array $storage_settings
+   *   (optional) A list of field storage settings that will be added to the
+   *   defaults.
+   * @param array $field_settings
+   *   (optional) A list of instance settings that will be added to the instance
+   *   defaults.
+   * @param array $widget_settings
+   *   (optional) Widget settings to be added to the widget defaults.
+   * @param array $formatter_settings
+   *   (optional) Formatter settings to be added to the formatter defaults.
+   * @param string $description
+   *   (optional) A description for the field. Defaults to ''.
+   */
+  protected function createImageField($name, $type_name, $storage_settings = array(), $field_settings = array(), $widget_settings = array(), $formatter_settings = array(), $description = '') {
+    FieldStorageConfig::create(array(
+      'field_name' => $name,
+      'entity_type' => 'node',
+      'type' => 'image',
+      'settings' => $storage_settings,
+      'cardinality' => !empty($storage_settings['cardinality']) ? $storage_settings['cardinality'] : 1,
+    ))->save();
+
+    $field_config = FieldConfig::create([
+      'field_name' => $name,
+      'label' => $name,
+      'entity_type' => 'node',
+      'bundle' => $type_name,
+      'required' => !empty($field_settings['required']),
+      'settings' => $field_settings,
+      'description' => $description,
+    ]);
+    $field_config->save();
+
+    entity_get_form_display('node', $type_name, 'default')
+      ->setComponent($name, array(
+        'type' => 'image_image',
+        'settings' => $widget_settings,
+      ))
+      ->save();
+
+    entity_get_display('node', $type_name, 'default')
+      ->setComponent($name, array(
+        'type' => 'image',
+        'settings' => $formatter_settings,
+      ))
+      ->save();
+
+    return $field_config;
+  }
+
+}
