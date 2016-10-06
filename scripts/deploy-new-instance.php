565c3a4a7d67b887791ee445978dab53cff2e6f0
Pavel Prischepa <pprischepa@gmail.com>
2016-10-06 16:54:20 +0700

    #1

--- scripts/deploy-new-instance.php
+++ scripts/deploy-new-instance.php
@@ -2,7 +2,7 @@
 
 define('MY_ORG_ID', '53deb661-3694-4336-8bd9-f4bc683ea360');
 
-require_once __DIR__ . './vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $api = new Wodby\Api($_SERVER['WODBY_API_TOKEN'], new GuzzleHttp\Client());
 
