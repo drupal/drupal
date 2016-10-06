5a1722eb07758d41d468e3bc0643ae9fc365638a
Pavel Prischepa <pprischepa@gmail.com>
2016-10-06 16:57:31 +0700

    #1

--- scripts/deploy-new-instance.php
+++ scripts/deploy-new-instance.php
@@ -6,6 +6,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $api = new Wodby\Api($_SERVER['WODBY_API_TOKEN'], new GuzzleHttp\Client());
 
-$apps = $api->application()->loadAll('MY_ORG_ID');
+$apps = $api->application()->loadAll(MY_ORG_ID);
 
 var_dump($apps);
