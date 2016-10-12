71f27581963cca00da3fb0ca0b3ec86ab0e22284
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-10-12 12:10:08 +0100

    Issue #2664530 by Lendude, slasher13, catch, alexpott, andrew.trebble: Views Combined fields filter with Contains

--- core/modules/views/src/Plugin/views/filter/Combine.php
+++ core/modules/views/src/Plugin/views/filter/Combine.php
@@ -3,6 +3,7 @@
 namespace Drupal\views\Plugin\views\filter;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Database\Database;
 
 /**
  * Filter handler which allows to search on multiple fields.
@@ -136,6 +137,40 @@ class Combine extends StringFilter {
   protected function opContains($expression) {
     $placeholder = $this->placeholder();
     $this->query->addWhereExpression($this->options['group'], "$expression LIKE $placeholder", array($placeholder => '%' . db_like($this->value) . '%'));
+  }
+
+  /**
+   * Filters by one or more words.
+   *
+   * By default opContainsWord uses add_where, that doesn't support complex
+   * expressions.
+   *
+   * @param string $expression
+   */
+  protected function opContainsWord($expression) {
+    $placeholder = $this->placeholder();
+
+    // Don't filter on empty strings.
+    if (empty($this->value)) {
+      return;
+    }
+
+    // Match all words separated by spaces or sentences encapsulated by double
+    // quotes.
+    preg_match_all(static::WORDS_PATTERN, ' ' . $this->value, $matches, PREG_SET_ORDER);
+
+    // Switch between the 'word' and 'allwords' operator.
+    $type = $this->operator == 'word' ? 'OR' : 'AND';
+    $group = $this->query->setWhereGroup($type);
+    $operator = Database::getConnection()->mapConditionOperator('LIKE');
+    $operator = isset($operator['operator']) ? $operator['operator'] : 'LIKE';
+
+    foreach ($matches as $match_key => $match) {
+      $temp_placeholder = $placeholder . '_' . $match_key;
+      // Clean up the user input and remove the sentence delimiters.
+      $word = trim($match[2], ',?!();:-"');
+      $this->query->addWhereExpression($group, "$expression $operator $temp_placeholder", array($temp_placeholder => '%' . Database::getConnection()->escapeLike($word) . '%'));
+    }
   }
 
   protected function opStartsWith($expression) {
