2f7b4e3ec2f1bd8af6f679a5bc13f0dd1906d3c6
Alex Pott <alex.a.pott@googlemail.com>
2016-10-15 06:32:58 +0100

    Issue #2807845 by quietone: Convert Ban's Migrate source tests to new base class
    (cherry picked from commit 9baedb6e843da12539081c06027e4e5d9bcf240a)

---
+++ core/modules/ban/tests/src/Kernel/Plugin/migrate/source/d7/BlockedIpsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\Tests\ban\Kernel\Plugin\migrate\source\d7;
+
+use Drupal\Tests\migrate\Kernel\MigrateSqlSourceTestBase;
+
+/**
+ * Tests D7 blocked_ip source plugin.
+ *
+ * @covers \Drupal\ban\Plugin\migrate\source\d7\BlockedIps
+ * @group ban
+ */
+class BlockedIpsTest extends MigrateSqlSourceTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['ban', 'migrate_drupal'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function providerSource() {
+    $tests = [];
+
+    $tests[0]['source_data']['blocked_ips'] = [
+      [
+        'iid' => 1,
+        'ip' => '127.0.0.1',
+      ]
+    ];
+    $tests[0]['expected_data'] = [
+      [
+        'ip' => '127.0.0.1',
+      ],
+    ];
+    return $tests;
+  }
+
+}
