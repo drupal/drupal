3412a90351eb699c0e100d8f52f21a76ab23dccb
Alex Pott <alex.a.pott@googlemail.com>
2016-10-15 06:38:31 +0100

    Issue #2807891 by svendecabooter: Convert Filter's Migrate source tests to new base class
    (cherry picked from commit dd953d4848d8a6044860a839a626c010f699db2c)

---
+++ core/modules/filter/tests/src/Kernel/Plugin/migrate/source/d6/FilterFormatTest.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Drupal\Tests\filter\Kernel\Plugin\migrate\source\d6;
+
+use Drupal\Tests\migrate\Kernel\MigrateSqlSourceTestBase;
+
+/**
+ * Tests D6 filter format source plugin.
+ *
+ * @covers \Drupal\filter\Plugin\migrate\source\d6\FilterFormat
+ *
+ * @group filter
+ */
+class FilterFormatTest extends MigrateSqlSourceTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['filter', 'migrate_drupal'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function providerSource() {
+    $tests = [];
+
+    // The source data.
+    $tests[0]['source_data']['filter_formats'] = [
+      [
+        'format' => 1,
+        'name' => 'Filtered HTML',
+        'roles' => ',1,2,',
+        'cache' => 1,
+      ],
+      [
+        'format' => 2,
+        'name' => 'Full HTML',
+        'roles' => '',
+        'cache' => 1,
+      ],
+      [
+        'format' => 4,
+        'name' => 'Example Custom Format',
+        'roles' => '4',
+        'cache' => 1,
+      ],
+    ];
+    $tests[0]['source_data']['filters'] = [
+      [
+        'fid' => 1,
+        'format' => 1,
+        'module' => 'filter',
+        'delta' => 2,
+        'weight' => 0,
+      ],
+      [
+        'fid' => 2,
+        'format' => 1,
+        'module' => 'filter',
+        'delta' => 0,
+        'weight' => 1,
+      ],
+      [
+        'fid' => 3,
+        'format' => 1,
+        'module' => 'filter',
+        'delta' => 1,
+        'weight' => 2,
+      ],
+      [
+        'fid' => 4,
+        'format' => 2,
+        'module' => 'filter',
+        'delta' => 2,
+        'weight' => 0,
+      ],
+      [
+        'fid' => 5,
+        'format' => 2,
+        'module' => 'filter',
+        'delta' => 1,
+        'weight' => 1,
+      ],
+      [
+        'fid' => 6,
+        'format' => 2,
+        'module' => 'filter',
+        'delta' => 3,
+        'weight' => 10,
+      ],
+      [
+        'fid' => 7,
+        'format' => 4,
+        'module' => 'markdown',
+        'delta' => 1,
+        'weight' => 10,
+      ],
+    ];
+
+    // The expected results.
+    $tests[0]['expected_data'] = [
+      [
+        'format' => 1,
+        'name' => 'Filtered HTML',
+        'roles' => [1, 2],
+        'cache' => 1,
+        'filters' => [
+          [
+            'module' => 'filter',
+            'delta' => 2,
+            'weight' => 0,
+            'settings' => [],
+          ],
+          [
+            'module' => 'filter',
+            'delta' => 0,
+            'weight' => 1,
+            'settings' => [],
+          ],
+          [
+            'module' => 'filter',
+            'delta' => 1,
+            'weight' => 2,
+            'settings' => [],
+          ],
+        ],
+      ],
+      [
+        'format' => 2,
+        'name' => 'Full HTML',
+        'roles' => [],
+        'cache' => 1,
+        'filters' => [
+          [
+            'module' => 'filter',
+            'delta' => 2,
+            'weight' => 0,
+            'settings' => [],
+          ],
+          [
+            'module' => 'filter',
+            'delta' => 1,
+            'weight' => 1,
+            'settings' => [],
+          ],
+          [
+            'module' => 'filter',
+            'delta' => 3,
+            'weight' => 10,
+            'settings' => [],
+          ],
+        ],
+      ],
+      [
+        'format' => 4,
+        'name' => 'Example Custom Format',
+        'roles' => [4],
+        'cache' => 1,
+        'filters' => [
+          // This custom format uses a filter defined by a contrib module.
+          [
+            'module' => 'markdown',
+            'delta' => 1,
+            'weight' => 10,
+            'settings' => [],
+          ],
+        ],
+      ],
+    ];
+
+    return $tests;
+  }
+
+}
---
+++ core/modules/filter/tests/src/Kernel/Plugin/migrate/source/d7/FilterFormatTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Drupal\Tests\filter\Kernel\Plugin\migrate\source\d7;
+
+use Drupal\Tests\migrate\Kernel\MigrateSqlSourceTestBase;
+
+/**
+ * Tests D7 filter format source plugin.
+ *
+ * @covers \Drupal\filter\Plugin\migrate\source\d7\FilterFormat
+ *
+ * @group filter
+ */
+class FilterFormatTest extends MigrateSqlSourceTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['filter', 'migrate_drupal'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function providerSource() {
+    $tests = [];
+
+    // The source data.
+    $tests[0]['source_data']['filter_format'] = [
+      [
+        'format' => 'custom_text_format',
+        'name' => 'Custom Text format',
+        'cache' => 1,
+        'status' => 1,
+        'weight' => 0,
+      ],
+      [
+        'format' => 'full_html',
+        'name' => 'Full HTML',
+        'cache' => 1,
+        'status' => 1,
+        'weight' => 1,
+      ],
+    ];
+    $tests[0]['source_data']['filter'] = [
+      [
+        'format' => 'custom_text_format',
+        'module' => 'filter',
+        'name' => 'filter_autop',
+        'weight' => 0,
+        'status' => 1,
+        'settings' => serialize(array()),
+      ],
+      [
+        'format' => 'custom_text_format',
+        'module' => 'filter',
+        'name' => 'filter_html',
+        'weight' => 1,
+        'status' => 1,
+        'settings' => serialize(array()),
+      ],
+      [
+        'format' => 'full_html',
+        'module' => 'filter',
+        'name' => 'filter_url',
+        'weight' => 0,
+        'status' => 1,
+        'settings' => serialize(array()),
+      ],
+    ];
+
+    // The expected results.
+    $tests[0]['expected_data'] = [
+      [
+        'format' => 'custom_text_format',
+        'name' => 'Custom Text format',
+        'cache' => 1,
+        'status' => 1,
+        'weight' => 0,
+        'filters' => [
+          'filter_autop' => [
+            'format' => 'custom_text_format',
+            'module' => 'filter',
+            'name' => 'filter_autop',
+            'weight' => 0,
+            'status' => 1,
+            'settings' => [],
+          ],
+          'filter_html' => [
+            'format' => 'custom_text_format',
+            'module' => 'filter',
+            'name' => 'filter_html',
+            'weight' => 1,
+            'status' => 1,
+            'settings' => [],
+          ],
+        ],
+      ],
+      [
+        'format' => 'full_html',
+        'name' => 'Full HTML',
+        'cache' => 1,
+        'status' => 1,
+        'weight' => 1,
+        'filters' => [
+          'filter_url' => [
+            'format' => 'full_html',
+            'module' => 'filter',
+            'name' => 'filter_url',
+            'weight' => 0,
+            'status' => 1,
+            'settings' => [],
+          ],
+        ],
+      ],
+    ];
+
+    return $tests;
+  }
+
+}
