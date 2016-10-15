6523934194025debab081a813f8c5fcb765086b7
Alex Pott <alex.a.pott@googlemail.com>
2016-10-15 06:42:02 +0100

    Issue #2807939 by erozqba, svendecabooter: Convert System's Migrate source tests to new base class

---
+++ core/modules/system/tests/src/Kernel/Plugin/migrate/source/MenuTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\Tests\system\Kernel\Plugin\migrate\source;
+
+use Drupal\Tests\migrate\Kernel\MigrateSqlSourceTestBase;
+
+/**
+ * Tests menu source plugin.
+ *
+ * @covers Drupal\system\Plugin\migrate\source\Menu
+ *
+ * @group system
+ */
+class MenuTest extends MigrateSqlSourceTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['system', 'migrate_drupal'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function providerSource() {
+    $tests = [];
+
+    // The source data.
+    $tests[0]['source_data']['menu_custom'] = [
+      [
+        'menu_name' => 'menu-name-1',
+        'title' => 'menu custom value 1',
+        'description' => 'menu custom description value 1',
+      ],
+      [
+        'menu_name' => 'menu-name-2',
+        'title' => 'menu custom value 2',
+        'description' => 'menu custom description value 2',
+      ],
+    ];
+
+    // The expected results are identical to the source data.
+    $tests[0]['expected_data'] = $tests[0]['source_data']['menu_custom'];
+
+    return $tests;
+  }
+
+}
