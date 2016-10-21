b840cbc5a2280ec3820921534d1d75867ed02c40
Alex Pott <alex.a.pott@googlemail.com>
2016-10-21 06:29:37 -0700

    Issue #2807863 by quietone, phenaproxima: Convert Block_content's Migrate source tests to new base class
    (cherry picked from commit 7d70af542e560774617ddd2ff8816e8bae5fc0f8)

---
+++ core/modules/block_content/tests/src/Kernel/Plugin/migrate/source/d7/BlockCustomTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\Tests\block_content\Kernel\Plugin\migrate\source\d7;
+
+use Drupal\Tests\migrate\Kernel\MigrateSqlSourceTestBase;
+
+/**
+ * Tests d7_block_custom source plugin.
+ *
+ * @covers \Drupal\block_content\Plugin\migrate\source\d7\BlockCustom
+ * @group block_content
+ */
+class BlockCustomTest extends MigrateSqlSourceTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['block_content', 'migrate_drupal'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function providerSource() {
+    $tests = [];
+
+    $tests[0]['source_data']['block_custom'] = [
+      [
+        'bid' => '1',
+        'body' => "I don't feel creative enough to write anything clever here.",
+        'info' => 'Meh',
+        'format' => 'filtered_html',
+      ],
+    ];
+    // The expected results are identical to the source data.
+    $tests[0]['expected_data'] = $tests[0]['source_data']['block_custom'];
+
+    return $tests;
+  }
+
+}
