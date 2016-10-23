553122bd0ded24dd0efa1725273a749bae1de636
Alex Pott <alex.a.pott@googlemail.com>
2016-10-23 12:43:14 -0700

    Issue #2798941 by mikeryan: Remove dead code in MigratePluginManager
    (cherry picked from commit 20303228e2fd83fc8cc57c31748d3b417626a14a)

--- core/modules/migrate/src/Plugin/MigratePluginManager.php
+++ core/modules/migrate/src/Plugin/MigratePluginManager.php
@@ -41,8 +41,7 @@ class MigratePluginManager extends DefaultPluginManager implements MigratePlugin
    *   'Drupal\Component\Annotation\PluginID'.
    */
   public function __construct($type, \Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler, $annotation = 'Drupal\Component\Annotation\PluginID') {
-    $plugin_interface = isset($plugin_interface_map[$type]) ? $plugin_interface_map[$type] : NULL;
-    parent::__construct("Plugin/migrate/$type", $namespaces, $module_handler, $plugin_interface, $annotation);
+    parent::__construct("Plugin/migrate/$type", $namespaces, $module_handler, NULL, $annotation);
     $this->alterInfo('migrate_' . $type . '_info');
     $this->setCacheBackend($cache_backend, 'migrate_plugins_' . $type);
   }
