74312bb7588780ca93c074c4441b2893c199403b
Alex Pott <alex.a.pott@googlemail.com>
2016-10-23 12:47:31 -0700

    Issue #2807937 by erozqba, svendecabooter, phenaproxima: Convert Shortcut's Migrate source tests to new base class
    (cherry picked from commit c0fda56d2ca43ed39a696b71dbaaef76e8e78e29)

---
+++ core/modules/shortcut/tests/src/Kernel/Plugin/migrate/source/d7/ShortcutTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\Tests\shortcut\Kernel\Plugin\migrate\source\d7;
+
+use Drupal\Tests\migrate\Kernel\MigrateSqlSourceTestBase;
+
+/**
+ * Tests D7 Shortcut source plugin.
+ *
+ * @covers Drupal\shortcut\Plugin\migrate\source\d7\Shortcut
+ *
+ * @group shortcut
+ */
+class ShortcutTest extends MigrateSqlSourceTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['shortcut', 'migrate_drupal'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function providerSource() {
+    $tests = [];
+
+    // The source data.
+    $tests[0]['source_data']['menu_links'] = [
+      [
+        'menu_name' => 'shortcut-set-2',
+        'mlid' => '473',
+        'plid' => '0',
+        'link_path' => 'admin/people',
+        'router_path' => 'admin/people',
+        'link_title' => 'People',
+        'options' => 'a:0:{}',
+        'module' => 'menu',
+        'hidden' => '0',
+        'external' => '0',
+        'has_children' => '0',
+        'expanded' => '0',
+        'weight' => '-50',
+        'depth' => '1',
+        'customized' => '0',
+        'p1' => '473',
+        'p2' => '0',
+        'p3' => '0',
+        'p4' => '0',
+        'p5' => '0',
+        'p6' => '0',
+        'p7' => '0',
+        'p8' => '0',
+        'p9' => '0',
+        'updated' => '0',
+      ]
+    ];
+
+    // The expected results.
+    $tests[0]['expected_data'] = [
+      [
+        'mlid' => '473',
+        'menu_name' => 'shortcut-set-2',
+        'link_path' => 'admin/people',
+        'link_title' => 'People',
+        'weight' => '-50',
+      ],
+    ];
+
+    return $tests;
+  }
+
+}
