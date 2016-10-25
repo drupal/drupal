271817660d096afb2c4cd647de8e6e248b3134e9
Alex Pott <alex.a.pott@googlemail.com>
2016-10-25 11:48:16 -0700

    Issue #2819479 by shashikant_chauhan, joachim: Fully qualify the FieldItemListInterface class name in its docs so it links
    (cherry picked from commit e0463d0ee488f622a9165d935cf5234a09a1b2fc)

--- core/lib/Drupal/Core/Field/FieldItemListInterface.php
+++ core/lib/Drupal/Core/Field/FieldItemListInterface.php
@@ -22,6 +22,8 @@ use Drupal\Core\TypedData\ListInterface;
  *
  * When implementing this interface which extends Traversable, make sure to list
  * IteratorAggregate or Iterator before this interface in the implements clause.
+ *
+ * @see \Drupal\Core\Field\FieldItemInterface
  */
 interface FieldItemListInterface extends ListInterface, AccessibleInterface {
 
