2c81806c4cb79c7a46ad1c263454d76333f6d018
Alex Pott <alex.a.pott@googlemail.com>
2016-10-26 12:13:27 -0700

    Issue #2785133 by tedbow, effulgentsia: Simplify OpenOffCanvasDialogCommand
    (cherry picked from commit 3a363f619e3c53183c10f1e8fe1f61f8603a6ec3)

--- core/modules/outside_in/src/Ajax/OpenOffCanvasDialogCommand.php
+++ core/modules/outside_in/src/Ajax/OpenOffCanvasDialogCommand.php
@@ -14,8 +14,10 @@ class OpenOffCanvasDialogCommand extends OpenDialogCommand {
   /**
    * Constructs an OpenOffCanvasDialogCommand object.
    *
-   * Drupal provides a built-in offcanvas tray for this purpose, so no selector
-   * needs to be provided.
+   * The off-canvas dialog differs from the normal modal provided by
+   * OpenDialogCommand in that a off-canvas has built in positioning and
+   * behaviours. Drupal provides a built-in off-canvas tray for this purpose,
+   * so the selector is hard-coded in the call to the parent constructor.
    *
    * @param string $title
    *   The title of the dialog.
@@ -46,15 +48,10 @@ class OpenOffCanvasDialogCommand extends OpenDialogCommand {
    * {@inheritdoc}
    */
   public function render() {
-    return [
-      'command' => 'openDialog',
-      'selector' => '#drupal-offcanvas',
-      'settings' => $this->settings,
-      'data' => $this->getRenderedContent(),
-      'dialogOptions' => $this->dialogOptions,
-      'effect' => 'fade',
-      'speed' => 1000,
-    ];
+    $build = parent::render();
+    $build['effect'] = 'fade';
+    $build['speed'] = 1000;
+    return $build;
   }
 
 }
