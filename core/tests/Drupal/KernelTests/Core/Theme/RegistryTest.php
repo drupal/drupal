2b74c933618dcc59c2c55bbe41502dd3c71d212d
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-10-26 20:41:50 +0100

    Issue #2754929 by joelpittet, markcarver: Internal path(s) missing for "html" and "page" theme hook suggestions on <front>

--- core/tests/Drupal/KernelTests/Core/Theme/RegistryTest.php
+++ core/tests/Drupal/KernelTests/Core/Theme/RegistryTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\KernelTests\Core\Theme;
 
+use Drupal\Core\Path\CurrentPathStack;
+use Drupal\Core\Path\PathMatcherInterface;
 use Drupal\Core\Theme\Registry;
 use Drupal\Core\Utility\ThemeRegistry;
 use Drupal\KernelTests\KernelTestBase;
@@ -155,6 +157,39 @@ class RegistryTest extends KernelTestBase {
     $registry = new Registry(\Drupal::root(), \Drupal::cache(), \Drupal::lock(), \Drupal::moduleHandler(), $theme_handler, \Drupal::service('theme.initialization'), 'test_theme');
     $registry->setThemeManager(\Drupal::theme());
     $this->assertEqual('value', $registry->get()['theme_test_template_test']['variables']['additional']);
+  }
+
+  /**
+   * Tests front node theme suggestion generation.
+   */
+  public function testThemeSuggestions() {
+    // Mock the current page as the front page.
+    /** @var PathMatcherInterface $path_matcher */
+    $path_matcher = $this->prophesize(PathMatcherInterface::class);
+    $path_matcher->isFrontPage()->willReturn(TRUE);
+    $this->container->set('path.matcher', $path_matcher->reveal());
+    /** @var CurrentPathStack $path_matcher */
+    $path_current = $this->prophesize(CurrentPathStack::class);
+    $path_current->getPath()->willReturn('/node/1');
+    $this->container->set('path.current', $path_current->reveal());
+
+    // Check suggestions provided through hook_theme_suggestions_html().
+    $suggestions = \Drupal::moduleHandler()->invokeAll('theme_suggestions_html', [[]]);
+    $this->assertSame([
+      'html__node',
+      'html__node__%',
+      'html__node__1',
+      'html__front',
+    ], $suggestions, 'Found expected html node suggestions.');
+
+    // Check suggestions provided through hook_theme_suggestions_page().
+    $suggestions = \Drupal::moduleHandler()->invokeAll('theme_suggestions_page', [[]]);
+    $this->assertSame([
+      'page__node',
+      'page__node__%',
+      'page__node__1',
+      'page__front',
+    ], $suggestions, 'Found expected page node suggestions.');
   }
 
 }
