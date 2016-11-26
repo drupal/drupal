46ff3bb2b3605a92b3effd7a118f8d96c3506030
Alex Pott <alex.a.pott@googlemail.com>
2016-11-26 22:30:04 +0000

    Issue #2829588 by damiankloip, Fabianx, markcarver, dawehner, catch: Provide a "url.path.is_front" cache context to allow contributed themes to move global "is_front" variable back

---
+++ core/tests/Drupal/Tests/Core/Cache/Context/IsFrontPathCacheContextTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\Tests\Core\Cache\Context;
+
+use Drupal\Core\Cache\Context\IsFrontPathCacheContext;
+use Drupal\Core\Path\PathMatcherInterface;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\Core\Cache\Context\IsFrontPathCacheContext
+ * @group Cache
+ */
+class IsFrontPathCacheContextTest extends UnitTestCase {
+
+  /**
+   * @covers ::getContext
+   */
+  public function testGetContextFront() {
+    $cache_context = new IsFrontPathCacheContext($this->createPathMatcher(TRUE)->reveal());
+    $this->assertSame('is_front.1', $cache_context->getContext());
+  }
+
+  /**
+   * @covers ::getContext
+   */
+  public function testGetContextNotFront() {
+    $cache_context = new IsFrontPathCacheContext($this->createPathMatcher(FALSE)->reveal());
+    $this->assertSame('is_front.0', $cache_context->getContext());
+  }
+
+  /**
+   * Creates a PathMatcherInterface prophecy.
+   *
+   * @param bool $is_front
+   *
+   * @return \Prophecy\Prophecy\ObjectProphecy
+   */
+  protected function createPathMatcher($is_front) {
+    $path_matcher = $this->prophesize(PathMatcherInterface::class);
+    $path_matcher->isFrontPage()
+      ->willReturn($is_front);
+
+    return $path_matcher;
+  }
+
+}
