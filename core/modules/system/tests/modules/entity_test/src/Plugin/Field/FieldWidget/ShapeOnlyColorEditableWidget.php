740ccca37f6f3255e8afcf3246ce8cd7de05a12e
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-12-02 13:38:53 +0000

    Issue #2811841 by hchonov, tstoeckler, Berdir: Add test coverage ensuring user input is mapped on the correct form elements when elements are reordered

---
+++ core/modules/system/tests/modules/entity_test/src/Plugin/Field/FieldWidget/ShapeOnlyColorEditableWidget.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\entity_test\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'shape_only_color_editable_widget' widget.
+ *
+ * @FieldWidget(
+ *   id = "shape_only_color_editable_widget",
+ *   label = @Translation("Shape widget with only color editable property"),
+ *   field_types = {
+ *     "shape"
+ *   },
+ * )
+ */
+class ShapeOnlyColorEditableWidget extends WidgetBase  {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element['shape'] = [
+      '#type' => 'hidden',
+      '#value' => $items[$delta]->shape
+    ];
+
+    $element['color'] = [
+      '#type' => 'textfield',
+      '#default_value' => isset($items[$delta]->color) ? $items[$delta]->color : NULL,
+      '#size' => 255,
+    ];
+
+    return $element;
+  }
+
+}
