fbafc69241f95e117d6d6140d1f08ec2fef348c2
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-12-06 11:54:06 +0000

    Issue #2726803 by hussainweb, jmuzz, Jo Fitzgerald: Field formatters with names different than their field type can not be migrated

--- core/modules/field/src/Plugin/migrate/source/d7/FieldInstancePerViewMode.php
+++ core/modules/field/src/Plugin/migrate/source/d7/FieldInstancePerViewMode.php
@@ -24,8 +24,15 @@ class FieldInstancePerViewMode extends DrupalSqlBase {
       $data = unserialize($field_instance['data']);
       // We don't need to include the serialized data in the returned rows.
       unset($field_instance['data']);
+
       foreach ($data['display'] as $view_mode => $info) {
-        $rows[] = array_merge($field_instance, $info, array('view_mode' => $view_mode));
+        // Rename type to formatter_type in the info array.
+        $info['formatter_type'] = $info['type'];
+        unset($info['type']);
+
+        $rows[] = array_merge($field_instance, $info, [
+          'view_mode' => $view_mode,
+        ]);
       }
     }
     return new \ArrayIterator($rows);
@@ -35,8 +42,11 @@ class FieldInstancePerViewMode extends DrupalSqlBase {
    * {@inheritdoc}
    */
   public function query() {
-    return $this->select('field_config_instance', 'fci')
-      ->fields('fci', array('entity_type', 'bundle', 'field_name', 'data'));
+    $query = $this->select('field_config_instance', 'fci')
+      ->fields('fci', ['entity_type', 'bundle', 'field_name', 'data'])
+      ->fields('fc', ['type']);
+    $query->join('field_config', 'fc', 'fc.field_name = fci.field_name');
+    return $query;
   }
 
   /**
@@ -49,7 +59,8 @@ class FieldInstancePerViewMode extends DrupalSqlBase {
       'field_name' => $this->t('Machine name of the field.'),
       'view_mode' => $this->t('The original machine name of the view mode.'),
       'label' => $this->t('The display label of the field.'),
-      'type' => $this->t('The formatter ID.'),
+      'type' => $this->t('The field ID.'),
+      'formatter_type' => $this->t('The formatter ID.'),
       'settings' => $this->t('Array of formatter-specific settings.'),
       'module' => $this->t('The module providing the formatter.'),
       'weight' => $this->t('Display weight of the field.'),
