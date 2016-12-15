d0271bdc4f222545f507de3af7672086d1040282
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2016-12-15 21:33:01 +0000

    Issue #2828438 by Adita, Sam152, rachel_norfolk, timmillwood, jp.stacey: Exception when adding tab to a node managed by content moderation
    (cherry picked from commit debf5fcc140bb7459a7c894ea0e3e64e09b4f856)

--- core/modules/content_moderation/src/Plugin/Menu/EditTab.php
+++ core/modules/content_moderation/src/Plugin/Menu/EditTab.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\content_moderation\Plugin\Menu;
 
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Menu\LocalTaskDefault;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
@@ -25,9 +27,9 @@ class EditTab extends LocalTaskDefault implements ContainerFactoryPluginInterfac
   protected $moderationInfo;
 
   /**
-   * The entity.
+   * The entity if determinable from the route or FALSE.
    *
-   * @var \Drupal\Core\Entity\ContentEntityInterface
+   * @var \Drupal\Core\Entity\ContentEntityInterface|FALSE
    */
   protected $entity;
 
@@ -69,8 +71,8 @@ class EditTab extends LocalTaskDefault implements ContainerFactoryPluginInterfac
    * {@inheritdoc}
    */
   public function getRouteParameters(RouteMatchInterface $route_match) {
-    // Override the node here with the latest revision.
-    $this->entity = $route_match->getParameter($this->pluginDefinition['entity_type_id']);
+    $entity_parameter = $route_match->getParameter($this->pluginDefinition['entity_type_id']);
+    $this->entity = $entity_parameter instanceof ContentEntityInterface ? $route_match->getParameter($this->pluginDefinition['entity_type_id']) : FALSE;
     return parent::getRouteParameters($route_match);
   }
 
@@ -78,8 +80,8 @@ class EditTab extends LocalTaskDefault implements ContainerFactoryPluginInterfac
    * {@inheritdoc}
    */
   public function getTitle() {
-    if (!$this->moderationInfo->isModeratedEntity($this->entity)) {
-      // Moderation isn't enabled.
+    // If the entity couldn't be loaded or moderation isn't enabled.
+    if (!$this->entity || !$this->moderationInfo->isModeratedEntity($this->entity)) {
       return parent::getTitle();
     }
 
@@ -96,8 +98,10 @@ class EditTab extends LocalTaskDefault implements ContainerFactoryPluginInterfac
     // @todo https://www.drupal.org/node/2779933 write a test for this.
     $tags = parent::getCacheTags();
     // Tab changes if node or node-type is modified.
-    $tags = array_merge($tags, $this->entity->getCacheTags());
-    $tags[] = $this->entity->getEntityType()->getBundleEntityType() . ':' . $this->entity->bundle();
+    if ($this->entity) {
+      $tags = array_merge($tags, $this->entity->getCacheTags());
+      $tags[] = $this->entity->getEntityType()->getBundleEntityType() . ':' . $this->entity->bundle();
+    }
     return $tags;
   }
 
---
+++ core/modules/content_moderation/tests/src/Functional/LocalTaskTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\Tests\content_moderation\Functional;
+
+use Drupal\simpletest\ContentTypeCreationTrait;
+use Drupal\simpletest\NodeCreationTrait;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test the content moderation local task.
+ *
+ * @group content_moderation
+ */
+class LocalTaskTest extends BrowserTestBase {
+
+  use ContentTypeCreationTrait;
+  use NodeCreationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'content_moderation_test_local_task',
+    'content_moderation',
+    'block',
+  ];
+
+  /**
+   * A test node.
+   *
+   * @var \Drupal\node\NodeInterface
+   */
+  protected $testNode;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->drupalPlaceBlock('local_tasks_block', ['id' => 'tabs_block']);
+    $this->drupalLogin($this->createUser(['bypass node access']));
+
+    $node_type = $this->createContentType();
+    $node_type
+      ->setThirdPartySetting('content_moderation', 'enabled', TRUE)
+      ->save();
+    $this->testNode = $this->createNode([
+      'type' => $node_type->id(),
+      'moderation_state' => 'draft',
+    ]);
+  }
+
+  /**
+   * Tests local tasks behave with content_moderation enabled.
+   */
+  public function testLocalTasks() {
+    $this->drupalGet(sprintf('node/%s', $this->testNode->id()));
+    $this->assertTasks(TRUE);
+
+    $this->clickLink('Task Without Upcast Node');
+    $this->assertTasks(FALSE);
+  }
+
+  /**
+   * Assert the correct tasks appear.
+   */
+  protected function assertTasks($with_upcast_node) {
+    $this->assertSession()->linkExists('View');
+    $this->assertSession()->linkExists('Task Without Upcast Node');
+    $this->assertSession()->linkExists($with_upcast_node ? 'Edit draft' : 'Edit');
+    $this->assertSession()->linkExists('Delete');
+  }
+
+}
