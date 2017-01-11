c021387ca50d3fd98ba3538fb2ab63d350891fb7
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-01-11 22:12:49 +0000

    Issue #2714497 by Peacog, mangy.fox, riteshsarraf, Sophie.SK: Pre-existing 'book' content type prevents book module installation

---
+++ core/modules/book/src/Tests/BookInstallTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\book\Tests;
+
+use Drupal\simpletest\WebTestBase;
+use Drupal\Core\Config\PreExistingConfigException;
+
+/**
+ * Test installation of Book module.
+ *
+ * @group book
+ */
+class BookInstallTest extends WebTestBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  public static $modules = array('node');
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * Test Book install with pre-existing content type.
+   *
+   * Tests that Book module can be installed if content type with machine name
+   * 'book' already exists.
+   */
+  public function testBookInstallWithPreexistingContentType() {
+    // Create a 'book' content type.
+    $this->drupalCreateContentType(['type' => 'book']);
+
+    // Install the Book module.
+    try {
+      $this->container->get('module_installer')->install(['book']);
+    } catch (PreExistingConfigException $e) {
+      $this->fail("Expected exception thrown trying to install Book module: " . $e->getMessage());
+    }
+  }
+
+}
--- core/modules/migrate_drupal_ui/src/Tests/MigrateUpgradeTestBase.php
+++ core/modules/migrate_drupal_ui/src/Tests/MigrateUpgradeTestBase.php
@@ -175,6 +175,7 @@ abstract class MigrateUpgradeTestBase extends WebTestBase {
       }
     }
     \Drupal::service('module_installer')->install(['forum']);
+    \Drupal::service('module_installer')->install(['book']);
   }
 
   /**
