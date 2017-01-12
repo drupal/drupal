3900e343b1b037d9e94e251e0ed7d788261467d5
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-01-12 20:02:55 +0000

    Issue #2842982 by alexpott: Fix Drupal\system\Tests\System\UncaughtExceptionTest in PHP7.1

--- core/modules/system/src/Tests/System/UncaughtExceptionTest.php
+++ core/modules/system/src/Tests/System/UncaughtExceptionTest.php
@@ -113,7 +113,12 @@ class UncaughtExceptionTest extends WebTestBase {
    * Tests a missing dependency on a service.
    */
   public function testMissingDependency() {
-    $this->expectedExceptionMessage = 'Argument 1 passed to Drupal\error_service_test\LonelyMonkeyClass::__construct() must be an instance of Drupal\Core\Database\Connection, non';
+    if (version_compare(PHP_VERSION, '7.1') < 0) {
+      $this->expectedExceptionMessage = 'Argument 1 passed to Drupal\error_service_test\LonelyMonkeyClass::__construct() must be an instance of Drupal\Core\Database\Connection, non';
+    }
+    else {
+      $this->expectedExceptionMessage = 'Too few arguments to function Drupal\error_service_test\LonelyMonkeyClass::__construct(), 0 passed';
+    }
     $this->drupalGet('broken-service-class');
     $this->assertResponse(500);
 
