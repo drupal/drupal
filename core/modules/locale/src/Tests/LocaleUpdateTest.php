3c951044493271b2450403eedf9e236cbaa16ac0
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-01-18 10:52:03 +0000

    Issue #2157927 by alexpott: Intermittent test fails in LocaleUpdateTest::testUpdateImportSourceRemote()
    (cherry picked from commit 9ab013370acd4a62a2a616c4f4da04beeacd099f)

--- core/modules/locale/src/Tests/LocaleUpdateTest.php
+++ core/modules/locale/src/Tests/LocaleUpdateTest.php
@@ -136,7 +136,7 @@ class LocaleUpdateTest extends LocaleUpdateBase {
     // Check the status on the Available translation status page.
     $this->assertRaw('<label for="edit-langcodes-de" class="visually-hidden">Update German</label>', 'German language found');
     $this->assertText('Updates for: Contributed module one, Contributed module two, Custom module one, Locale test', 'Updates found');
-    $this->assertText('Contributed module one (' . format_date($this->timestampNow, 'html_date') . ')', 'Updates for Contrib module one');
+    $this->assertText('Contributed module one (' . format_date($this->timestampNew, 'html_date') . ')', 'Updates for Contrib module one');
     $this->assertText('Contributed module two (' . format_date($this->timestampNew, 'html_date') . ')', 'Updates for Contrib module two');
 
     // Execute the translation update.
