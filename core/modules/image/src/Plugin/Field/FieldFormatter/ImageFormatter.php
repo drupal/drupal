80ffa8c7c900173ac34082abb1af809dacdcf187
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-01-25 13:34:00 +0000

    Issue #2844213 by andrewmacpherson: Undocumented $image_style_storage constructor argument in ImageFormatter

--- core/modules/image/src/Plugin/Field/FieldFormatter/ImageFormatter.php
+++ core/modules/image/src/Plugin/Field/FieldFormatter/ImageFormatter.php
@@ -63,6 +63,8 @@ class ImageFormatter extends ImageFormatterBase implements ContainerFactoryPlugi
    *   Any third party settings settings.
    * @param \Drupal\Core\Session\AccountInterface $current_user
    *   The current user.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $image_style_storage
+   *   The image style storage.
    */
   public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, AccountInterface $current_user, EntityStorageInterface $image_style_storage) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
