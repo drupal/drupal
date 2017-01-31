aaf99e1e449d61a33b12c5c9d6981ecf728cf263
Alex Pott <alex.a.pott@googlemail.com>
2017-01-31 00:30:00 +0000

    Issue #2744463 by Mile23, dawehner, pfrenssen, joelpittet, tstoeckler, alexpott, klausi: Add phpcs, coder 8.2.8 as --dev requirements for drupal/core

--- core/lib/Drupal/Core/Composer/Composer.php
+++ core/lib/Drupal/Core/Composer/Composer.php
@@ -6,6 +6,7 @@ use Drupal\Component\PhpStorage\FileStorage;
 use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 use Composer\Semver\Constraint\Constraint;
+use PHP_CodeSniffer;
 
 /**
  * Provides static functions for composer script events.
@@ -18,6 +19,7 @@ class Composer {
     'behat/mink' => ['tests', 'driver-testsuite'],
     'behat/mink-browserkit-driver' => ['tests'],
     'behat/mink-goutte-driver' => ['tests'],
+    'drupal/coder' => ['coder_sniffer/Drupal/Test', 'coder_sniffer/DrupalPractice/Test'],
     'doctrine/cache' => ['tests'],
     'doctrine/collections' => ['tests'],
     'doctrine/common' => ['tests'],
@@ -132,6 +134,28 @@ class Composer {
 </configuration>
 EOT;
       file_put_contents($webconfig_file, $lines . "\n");
+    }
+  }
+
+  /**
+   * Configures phpcs if present.
+   *
+   * @param \Composer\Script\Event $event
+   */
+  public static function configurePhpcs(Event $event) {
+    // Grab the local repo which tells us what's been installed.
+    $local_repository = $event->getComposer()
+      ->getRepositoryManager()
+      ->getLocalRepository();
+    // Make sure both phpcs and coder are installed.
+    $phpcs_package = $local_repository->findPackage('squizlabs/php_codesniffer', '*');
+    $coder_package = $local_repository->findPackage('drupal/coder', '*');
+    if (!empty($phpcs_package) && !empty($coder_package)) {
+      $config = $event->getComposer()->getConfig();
+      $vendor_dir = $config->get('vendor-dir');
+      // Set phpcs' installed_paths config to point to our coder_sniffer
+      // directory.
+      PHP_CodeSniffer::setConfigData('installed_paths', $vendor_dir . '/drupal/coder/coder_sniffer');
     }
   }
 
