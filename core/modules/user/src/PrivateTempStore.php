890bb76fcd25350d7fe9eb7861d751fa181b5993
Alex Pott <alex.a.pott@googlemail.com>
2017-02-07 13:18:22 +0000

    Issue #2845666 by dpi, ritzz, dawehner: *TempStore's missing @throws documentation

--- core/modules/user/src/PrivateTempStore.php
+++ core/modules/user/src/PrivateTempStore.php
@@ -112,6 +112,9 @@ class PrivateTempStore {
    *   The key of the data to store.
    * @param mixed $value
    *   The data to store.
+   *
+   * @throws \Drupal\user\TempStoreException
+   *   Thrown when a lock for the backend storage could not be acquired.
    */
   public function set($key, $value) {
     $key = $this->createkey($key);
@@ -161,6 +164,9 @@ class PrivateTempStore {
    * @return bool
    *   TRUE if the object was deleted or does not exist, FALSE if it exists but
    *   is not owned by $this->owner.
+   *
+   * @throws \Drupal\user\TempStoreException
+   *   Thrown when a lock for the backend storage could not be acquired.
    */
   public function delete($key) {
     $key = $this->createkey($key);
--- core/modules/user/src/SharedTempStore.php
+++ core/modules/user/src/SharedTempStore.php
@@ -164,6 +164,9 @@ class SharedTempStore {
    * @return bool
    *   TRUE if the data was set, or FALSE if it already exists and is not owned
    *   by $this->user.
+   *
+   * @throws \Drupal\user\TempStoreException
+   *   Thrown when a lock for the backend storage could not be acquired.
    */
   public function setIfOwner($key, $value) {
     if ($this->setIfNotExists($key, $value)) {
@@ -185,6 +188,9 @@ class SharedTempStore {
    *   The key of the data to store.
    * @param mixed $value
    *   The data to store.
+   *
+   * @throws \Drupal\user\TempStoreException
+   *   Thrown when a lock for the backend storage could not be acquired.
    */
   public function set($key, $value) {
     if (!$this->lockBackend->acquire($key)) {
@@ -228,6 +234,9 @@ class SharedTempStore {
    *
    * @param string $key
    *   The key of the data to delete.
+   *
+   * @throws \Drupal\user\TempStoreException
+   *   Thrown when a lock for the backend storage could not be acquired.
    */
   public function delete($key) {
     if (!$this->lockBackend->acquire($key)) {
@@ -251,6 +260,9 @@ class SharedTempStore {
    * @return bool
    *   TRUE if the object was deleted or does not exist, FALSE if it exists but
    *   is not owned by $this->owner.
+   *
+   * @throws \Drupal\user\TempStoreException
+   *   Thrown when a lock for the backend storage could not be acquired.
    */
   public function deleteIfOwner($key) {
     if (!$object = $this->storage->get($key)) {
