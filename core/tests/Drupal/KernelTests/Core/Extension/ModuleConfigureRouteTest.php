dc7fd78d172108392c5c488bcc1237eb534673f2
Alex Pott <alex.a.pott@googlemail.com>
2017-02-08 21:40:41 +0000

    Issue #2848553 by Sam152, Jaesin, dawehner: Make sure all 'configure' links are valid routes
    (cherry picked from commit a3c32ebb6feb2f4fee66679f6e1636e383199d19)

---
+++ core/tests/Drupal/KernelTests/Core/Extension/ModuleConfigureRouteTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\KernelTests\Core\Extension;
+
+use Drupal\KernelTests\FileSystemModuleDiscoveryDataProviderTrait;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests the configure route for core modules.
+ *
+ * @group Module
+ */
+class ModuleConfigureRouteTest extends KernelTestBase {
+
+  use FileSystemModuleDiscoveryDataProviderTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['system', 'user'];
+
+  /**
+   * @var \Drupal\Core\Routing\RouteProviderInterface
+   */
+  protected $routeProvider;
+
+  /**
+   * An array of module info.
+   *
+   * @var array
+   */
+  protected $moduleInfo;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->routeProvider = \Drupal::service('router.route_provider');
+    $this->moduleInfo = system_rebuild_module_data();
+  }
+
+  /**
+   * Test the module configure routes exist.
+   *
+   * @dataProvider coreModuleListDataProvider
+   */
+  function testModuleConfigureRoutes($module) {
+    $module_info = $this->moduleInfo[$module]->info;
+    if (isset($module_info['configure'])) {
+      $this->container->get('module_installer')->install([$module]);
+      $route = $this->routeProvider->getRouteByName($module_info['configure']);
+      $this->assertNotEmpty($route, sprintf('The configure route for the "%s" module was found.', $module));
+    }
+  }
+
+}
---
+++ core/tests/Drupal/KernelTests/FileSystemModuleDiscoveryDataProviderTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\KernelTests;
+
+/**
+ * A trait used in testing for providing a list of modules in a dataProvider.
+ */
+trait FileSystemModuleDiscoveryDataProviderTrait {
+
+  /**
+   * A data provider that lists every module in core.
+   *
+   * @return array
+   *   An array of module names to test.
+   */
+  public function coreModuleListDataProvider() {
+    $module_dirs = array_keys(iterator_to_array(new \FilesystemIterator(__DIR__ . '/../../../modules/')));
+    $module_names = array_map(function($path) {
+      return str_replace(__DIR__ . '/../../../modules/', '', $path);
+    }, $module_dirs);
+    $modules_keyed = array_combine($module_names, $module_names);
+
+    $data = array_map(function ($module) {
+      return [$module];
+    }, $modules_keyed);
+
+    return $data;
+  }
+
+}
