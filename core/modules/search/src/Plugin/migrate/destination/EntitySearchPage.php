dec943021c1f55b53d4a729c4d735626e71ed531
xjm <xjm@65776.no-reply.drupal.org>
2017-02-11 19:44:24 -0600

    Revert "Issue #2587063 by quietone, Jo Fitzgerald, jhodgdon, phenaproxima, alexpott, mikeryan: Variable to config: search_active_modules [d7]"
    This reverts commit 08e8437bdd96f778e764224c520553270ea56e98.

--- core/modules/search/src/Plugin/migrate/destination/EntitySearchPage.php
+++ core/modules/search/src/Plugin/migrate/destination/EntitySearchPage.php
@@ -3,12 +3,8 @@
 namespace Drupal\search\Plugin\migrate\destination;
 
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\migrate\Plugin\migrate\destination\EntityConfigBase;
-use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\Row;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @MigrateDestination(
@@ -16,72 +12,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class EntitySearchPage extends EntityConfigBase {
-
-  /**
-   * The module handler.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
-   */
-  protected $moduleHandler;
-
-  /**
-   * Constructs a new EntitySearchPage.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param MigrationInterface $migration
-   *   The migration.
-   * @param EntityStorageInterface $storage
-   *   The storage for this entity type.
-   * @param array $bundles
-   *   The list of bundles this entity type has.
-   * @param ModuleHandlerInterface $module_handler
-   *   The module handler.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, EntityStorageInterface $storage, array $bundles, ModuleHandlerInterface $module_handler) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration, $storage, $bundles);
-    $this->moduleHandler = $module_handler;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
-    $entity_type_id = static::getEntityTypeId($plugin_id);
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $migration,
-      $container->get('entity.manager')->getStorage($entity_type_id),
-      array_keys($container->get('entity.manager')->getBundleInfo($entity_type_id)),
-      $container->get('module_handler')
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function import(Row $row, array $old_destination_id_values = array()) {
-    if ($this->moduleHandler->moduleExists($row->getDestinationProperty('module'))) {
-      parent::import($row, $old_destination_id_values);
-      $id = $row->getDestinationProperty('id');
-      /** @var \Drupal\search\Entity\SearchPage $search_page */
-      $search_page = $this->storage->load($id);
-      if ($row->getDestinationProperty('status')) {
-        $search_page->enable()->save();
-      }
-      else {
-        $search_page->disable()->save();
-      }
-      return [$id];
-    }
-    return [];
-  }
 
   /**
    * Updates the entity with the contents of a row.
@@ -93,10 +23,7 @@ class EntitySearchPage extends EntityConfigBase {
    */
   protected function updateEntity(EntityInterface $entity, Row $row) {
     $entity->setPlugin($row->getDestinationProperty('plugin'));
-    // The user_search plugin does not have a setConfiguration() method.
-    if (method_exists($entity->getPlugin(), 'setConfiguration')) {
-      $entity->getPlugin()->setConfiguration($row->getDestinationProperty('configuration'));
-    }
+    $entity->getPlugin()->setConfiguration($row->getDestinationProperty('configuration'));
   }
 
 }
