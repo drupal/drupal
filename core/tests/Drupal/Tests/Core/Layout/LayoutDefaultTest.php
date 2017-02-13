5dc0e21c6570807e5002317b72664b91d8d44844
xjm <xjm@65776.no-reply.drupal.org>
2017-02-13 17:17:25 -0600

    Issue #2851648 by tim.plunkett, jibran: Layout regions should be in the order defined by the definition
    (cherry picked from commit fee5c769222707792e4e463e53ee4234b9de9b5e)

---
+++ core/tests/Drupal/Tests/Core/Layout/LayoutDefaultTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Drupal\Tests\Core\Layout;
+
+use Drupal\Core\Layout\LayoutDefault;
+use Drupal\Core\Layout\LayoutDefinition;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\Core\Layout\LayoutDefault
+ * @group Layout
+ */
+class LayoutDefaultTest extends UnitTestCase {
+
+  /**
+   * @covers ::build
+   * @dataProvider providerTestBuild
+   */
+  public function testBuild($regions, $expected) {
+    $definition = new LayoutDefinition([
+      'theme_hook' => 'layout',
+      'library' => 'core/drupal',
+      'regions' => [
+        'left' => [
+          'label' => 'Left',
+        ],
+        'right' => [
+          'label' => 'Right',
+        ],
+      ],
+    ]);
+    $expected += [
+      '#settings' => [],
+      '#layout' => $definition,
+      '#theme' => 'layout',
+      '#attached' => [
+        'library' => [
+          'core/drupal',
+        ],
+      ],
+    ];
+
+    $layout = new LayoutDefault([], '', $definition);
+    $this->assertSame($expected, $layout->build($regions));
+  }
+
+  /**
+   * Provides test data for ::testBuild().
+   */
+  public function providerTestBuild() {
+    $data = [];
+    // Empty regions are not added.
+    $data['right_only'] = [
+      [
+        'right' => [
+          'foo' => 'bar',
+        ],
+      ],
+      [
+        'right' => [
+          'foo' => 'bar',
+        ],
+      ],
+    ];
+    // Regions will be in the order defined by the layout.
+    $data['switched_order'] = [
+      [
+        'right' => [
+          'foo' => 'bar',
+        ],
+        'left' => [
+          'foo' => 'baz',
+        ],
+      ],
+      [
+        'left' => [
+          'foo' => 'baz',
+        ],
+        'right' => [
+          'foo' => 'bar',
+        ],
+      ],
+    ];
+    return $data;
+  }
+
+}
