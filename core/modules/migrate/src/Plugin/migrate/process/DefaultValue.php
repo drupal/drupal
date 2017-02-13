559e61a460d4bacd0f4734d277ccce473d546aef
Alex Pott <alex.a.pott@googlemail.com>
2017-02-13 14:04:38 +0000

    Issue #2845477 by gerzenstl, Jo Fitzgerald, quietone, phenaproxima, xjm: Add documentation to DefaultValue process plugin

--- core/modules/migrate/src/Plugin/migrate/process/DefaultValue.php
+++ core/modules/migrate/src/Plugin/migrate/process/DefaultValue.php
@@ -7,9 +7,38 @@ use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Row;
 
 /**
- * This plugin sets missing values on the destination.
+ * Returns a given default value if the input is empty.
  *
- * @link https://www.drupal.org/node/2135313 Online handbook documentation for default_value process plugin @endlink
+ * The default_value process plugin provides the ability to set a fixed default
+ * value. The plugin returns a default value if the input value is considered
+ * empty (NULL, FALSE, 0, '0', an empty string, or an empty array). The strict
+ * configuration key can be used to set the default only when the incoming
+ * value is NULL.
+ *
+ * Available configuration keys:
+ * - default_value: The fixed default value to apply.
+ * - strict: (optional) Use strict value checking. Defaults to false.
+ *   - FALSE: Apply default when input value is empty().
+ *   - TRUE: Apply default when input value is NULL.
+ *
+ * Example:
+ *
+ * @code
+ * process:
+ *   uid:
+ *     -
+ *       plugin: migration
+ *       migration: users
+ *       source: author
+ *     -
+ *       plugin: default_value
+ *       default_value: 44
+ * @endcode
+ *
+ * This will look up the source value of author in the users migration and if
+ * not found, set the destination property uid to 44.
+ *
+ * @see \Drupal\migrate\Plugin\MigrateProcessInterface
  *
  * @MigrateProcessPlugin(
  *   id = "default_value"
