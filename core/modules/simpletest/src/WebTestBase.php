bc0b00e254804e2a00ac5df1ee344fdeb234d599
Chris McCafferty <cilefen@gmail.com>
2017-03-15 16:19:51 -0400

    SA-CORE-2017-001 by alexpott, benjy, Berdir, casey, cilefen, dawehner, Heine, klausi, larowlan, Mixologic, mlhess, pwolanin, samuel.mortenson, Wim Leers, xjm, YesCT

--- core/modules/simpletest/src/WebTestBase.php
+++ core/modules/simpletest/src/WebTestBase.php
@@ -1301,7 +1301,7 @@ abstract class WebTestBase extends TestBase {
     }
 
     if ($path instanceof Url) {
-      $path = $path->toString();
+      $path = $path->setAbsolute()->toString(TRUE)->getGeneratedUrl();
     }
 
     $verbose = 'GET request to: ' . $path .
@@ -2588,7 +2588,7 @@ abstract class WebTestBase extends TestBase {
       $url_options = $path->getOptions();
       $options = $url_options + $options;
       $path->setOptions($options);
-      return $path->setAbsolute()->toString();
+      return $path->setAbsolute()->toString(TRUE)->getGeneratedUrl();
     }
     // The URL generator service is not necessarily available yet; e.g., in
     // interactive installer tests.
