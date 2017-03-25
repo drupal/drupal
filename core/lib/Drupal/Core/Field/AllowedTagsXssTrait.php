212818c13b5f97a0dd3803f81133fa5d311ee56c
Alex Pott <alex.a.pott@googlemail.com>
2017-03-25 10:43:05 +0000

    Issue #2504607 by juanjesustrigo, sasanikolic, mgifford, dawehner: Change the return of fieldFilterXss to FieldFilteredMarkup
    (cherry picked from commit 163b5102530afa9227191bfddb2fbf58fc8bf080)

--- core/lib/Drupal/Core/Field/AllowedTagsXssTrait.php
+++ core/lib/Drupal/Core/Field/AllowedTagsXssTrait.php
@@ -25,7 +25,7 @@ trait AllowedTagsXssTrait {
    * @param string $string
    *   The string with raw HTML in it.
    *
-   * @return \Drupal\Component\Utility\SafeMarkup
+   * @return \Drupal\Core\Field\FieldFilteredMarkup
    *   An XSS safe version of $string, or an empty string if $string is not
    *   valid UTF-8.
    */
