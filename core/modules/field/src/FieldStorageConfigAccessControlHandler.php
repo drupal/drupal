bfaa4f5ab6dace60e2da7b6508479874b50f2a79
Alex Pott <alex.a.pott@googlemail.com>
2017-03-27 11:39:13 +0100

    Issue #2843756 by vaplas, brentschuddinck, Wim Leers: EntityResource: Provide comprehensive test coverage for FieldStorageConfig entity
    (cherry picked from commit df2fd66d5999884504ab1d66f921aa6d0bf73711)

---
+++ core/modules/field/src/FieldStorageConfigAccessControlHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\field;
+
+/**
+ * Defines the access control handler for the field storage config entity type.
+ *
+ * @see \Drupal\field\Entity\FieldStorageConfig
+ */
+class FieldStorageConfigAccessControlHandler extends FieldConfigAccessControlHandler {}
