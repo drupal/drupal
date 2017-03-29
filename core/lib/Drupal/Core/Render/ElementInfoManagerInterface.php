b4e39cc9db8b4e3e8f252ddd6e58a3a0393cfd42
Alex Pott <alex.a.pott@googlemail.com>
2017-03-29 07:03:55 +0100

    Issue #2854909 by arshadcn: Update documentation for \Drupal\Core\Render\ElementInfoManagerInterface
    (cherry picked from commit 01e52a3dcc8543815b0b0a9cf120cf51b6e399dd)

--- core/lib/Drupal/Core/Render/ElementInfoManagerInterface.php
+++ core/lib/Drupal/Core/Render/ElementInfoManagerInterface.php
@@ -10,8 +10,8 @@ interface ElementInfoManagerInterface {
   /**
    * Retrieves the default properties for the defined element type.
    *
-   * Each of the form element types defined by this hook is assumed to have
-   * a matching theme hook, which should be registered with hook_theme() as
+   * Each of the element types defined by this hook is assumed to have a
+   * matching theme hook, which should be registered with hook_theme() as
    * normal.
    *
    * For more information about custom element types see the explanation at
