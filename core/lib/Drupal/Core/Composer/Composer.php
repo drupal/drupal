8c970f7e8776cff62e2ca6aaf3e9e7481de7e0db
Chris McCafferty <cilefen@gmail.com>
2017-04-11 08:20:07 -0400

    Issue #2851661 by alexpott, dawehner, xjm: Ensure that we're using the right ruleset for coding standards checking
    (cherry picked from commit 34bf287af7b75de7c28717f2ecfd88db0d3b0832)

--- core/lib/Drupal/Core/Composer/Composer.php
+++ core/lib/Drupal/Core/Composer/Composer.php
@@ -6,7 +6,6 @@ use Drupal\Component\PhpStorage\FileStorage;
 use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
 use Composer\Semver\Constraint\Constraint;
-use PHP_CodeSniffer;
 
 /**
  * Provides static functions for composer script events.
@@ -134,28 +133,6 @@ class Composer {
 </configuration>
 EOT;
       file_put_contents($webconfig_file, $lines . "\n");
-    }
-  }
-
-  /**
-   * Configures phpcs if present.
-   *
-   * @param \Composer\Script\Event $event
-   */
-  public static function configurePhpcs(Event $event) {
-    // Grab the local repo which tells us what's been installed.
-    $local_repository = $event->getComposer()
-      ->getRepositoryManager()
-      ->getLocalRepository();
-    // Make sure both phpcs and coder are installed.
-    $phpcs_package = $local_repository->findPackage('squizlabs/php_codesniffer', '*');
-    $coder_package = $local_repository->findPackage('drupal/coder', '*');
-    if (!empty($phpcs_package) && !empty($coder_package)) {
-      $config = $event->getComposer()->getConfig();
-      $vendor_dir = $config->get('vendor-dir');
-      // Set phpcs' installed_paths config to point to our coder_sniffer
-      // directory.
-      PHP_CodeSniffer::setConfigData('installed_paths', $vendor_dir . '/drupal/coder/coder_sniffer');
     }
   }
 
