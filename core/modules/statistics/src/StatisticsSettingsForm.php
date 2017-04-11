b8f16bba09b56102675d8d1f8bd6016d85443468
Alex Pott <alex.a.pott@googlemail.com>
2017-04-11 14:45:34 +0100

    Issue #2868442 by Adita: Wrong namespace in StatisticsSettingsForm constructor docblock

--- core/modules/statistics/src/StatisticsSettingsForm.php
+++ core/modules/statistics/src/StatisticsSettingsForm.php
@@ -21,7 +21,7 @@ class StatisticsSettingsForm extends ConfigFormBase {
   protected $moduleHandler;
 
   /**
-   * Constructs a \Drupal\user\StatisticsSettingsForm object.
+   * Constructs a \Drupal\statistics\StatisticsSettingsForm object.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The factory for configuration objects.
