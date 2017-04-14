55e309fc33895bf2d629825b08486c40d07212fc
Alex Pott <alex.a.pott@googlemail.com>
2017-04-14 07:36:40 +0100

    Issue #2862988 by timmillwood, catch: EntityOperations::entityPresave doesn't always set the correct default revision

--- core/modules/content_moderation/src/EntityOperations.php
+++ core/modules/content_moderation/src/EntityOperations.php
@@ -105,9 +105,10 @@ class EntityOperations implements ContainerInjectionInterface {
       /** @var \Drupal\content_moderation\ContentModerationState $current_state */
       $current_state = $workflow->getState($entity->moderation_state->value);
 
-      // This entity is default if it is new, the default revision, or the
-      // default revision is not published.
+      // This entity is default if it is new, a new translation, the default
+      // revision, or the default revision is not published.
       $update_default_revision = $entity->isNew()
+        || $entity->isNewTranslation()
         || $current_state->isDefaultRevisionState()
         || !$this->isDefaultRevisionPublished($entity, $workflow);
 
@@ -250,8 +251,8 @@ class EntityOperations implements ContainerInjectionInterface {
    * Check if the default revision for the given entity is published.
    *
    * The default revision is the same as the entity retrieved by "default" from
-   * the storage handler. If the entity is translated, use the default revision
-   * of the same language as the given entity.
+   * the storage handler. If the entity is translated, check if any of the
+   * translations are published.
    *
    * @param \Drupal\Core\Entity\EntityInterface $entity
    *   The entity being saved.
@@ -262,21 +263,22 @@ class EntityOperations implements ContainerInjectionInterface {
    *   TRUE if the default revision is published. FALSE otherwise.
    */
   protected function isDefaultRevisionPublished(EntityInterface $entity, WorkflowInterface $workflow) {
-    $storage = $this->entityTypeManager->getStorage($entity->getEntityTypeId());
-    $default_revision = $storage->load($entity->id());
+    $default_revision = $this->entityTypeManager->getStorage($entity->getEntityTypeId())->load($entity->id());
 
-    // Ensure we are comparing the same translation as the current entity.
+    // Ensure we are checking all translations of the default revision.
     if ($default_revision instanceof TranslatableInterface && $default_revision->isTranslatable()) {
-      // If there is no translation, then there is no default revision and is
-      // therefore not published.
-      if (!$default_revision->hasTranslation($entity->language()->getId())) {
-        return FALSE;
+      // Loop through each language that has a translation.
+      foreach ($default_revision->getTranslationLanguages() as $language) {
+        // Load the translated revision.
+        $language_revision = $default_revision->getTranslation($language->getId());
+        // Return TRUE if a translation with a published state is found.
+        if ($workflow->getState($language_revision->moderation_state->value)->isPublishedState()) {
+          return TRUE;
+        }
       }
-
-      $default_revision = $default_revision->getTranslation($entity->language()->getId());
     }
 
-    return $default_revision && $workflow->getState($default_revision->moderation_state->value)->isPublishedState();
+    return $workflow->getState($default_revision->moderation_state->value)->isPublishedState();
   }
 
 }
