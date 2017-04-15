d6a1aa64bf29c30f0067c7ef9e22370d8b4c9bbb
Alex Pott <alex.a.pott@googlemail.com>
2017-04-15 10:21:42 +0100

    Issue #2843763 by arshadcn, Wim Leers: EntityResource: Provide comprehensive test coverage for ContentLanguageSettings entity
    (cherry picked from commit b712a2d91f328ee3c79279cf82872a9991d5e5b3)

---
+++ core/modules/rest/tests/src/Functional/EntityResource/ContentLanguageSettings/ContentLanguageSettingsResourceTestBase.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\Tests\rest\Functional\EntityResource\ContentLanguageSettings;
+
+use Drupal\language\Entity\ContentLanguageSettings;
+use Drupal\node\Entity\NodeType;
+use Drupal\Tests\rest\Functional\EntityResource\EntityResourceTestBase;
+
+abstract class ContentLanguageSettingsResourceTestBase extends EntityResourceTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['language', 'node'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $entityTypeId = 'language_content_settings';
+
+  /**
+   * @var \Drupal\language\ContentLanguageSettingsInterface
+   */
+  protected $entity;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUpAuthorization($method) {
+    $this->grantPermissionsToTestedRole(['administer languages']);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function createEntity() {
+    // Create a "Camelids" node type.
+    $camelids = NodeType::create([
+      'name' => 'Camelids',
+      'type' => 'camelids',
+    ]);
+    $camelids->save();
+
+    $entity = ContentLanguageSettings::create([
+      'target_entity_type_id' => 'node',
+      'target_bundle' => 'camelids',
+    ]);
+    $entity->setDefaultLangcode('site_default')
+      ->save();
+
+    return $entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getExpectedNormalizedEntity() {
+    return [
+      'default_langcode' => 'site_default',
+      'dependencies' => [
+        'config' => [
+          'node.type.camelids',
+        ],
+      ],
+      'id' => 'node.camelids',
+      'langcode' => 'en',
+      'language_alterable' => FALSE,
+      'status' => TRUE,
+      'target_bundle' => 'camelids',
+      'target_entity_type_id' => 'node',
+      'uuid' => $this->entity->uuid(),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getNormalizedPostEntity() {
+    // @todo Update in https://www.drupal.org/node/2300677.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getExpectedCacheContexts() {
+    return [
+      'languages:language_interface',
+      'user.permissions',
+    ];
+  }
+
+}
