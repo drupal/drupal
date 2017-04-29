a6b84bfbf2090e19d7871561f3f20adede3e2a87
Alex Pott <alex.a.pott@googlemail.com>
2017-04-29 21:27:29 +0100

    Issue #2843756 by vaplas, Wim Leers, brentschuddinck, jamesdesq: EntityResource: Provide comprehensive test coverage for FieldStorageConfig entity

---
+++ core/modules/field/src/FieldStorageConfigAccessControlHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\field;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityAccessControlHandler;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Defines the access control handler for the field storage config entity type.
+ *
+ * @see \Drupal\field\Entity\FieldStorageConfig
+ */
+class FieldStorageConfigAccessControlHandler extends EntityAccessControlHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+    /** \Drupal\field\FieldStorageConfigInterface $entity */
+    if ($operation === 'delete') {
+      if ($entity->isLocked()) {
+        return AccessResult::forbidden()->addCacheableDependency($entity);
+      }
+      else {
+        return AccessResult::allowedIfHasPermission($account, 'administer ' . $entity->getTargetEntityTypeId() . ' fields')->addCacheableDependency($entity);
+      }
+    }
+    return AccessResult::allowedIfHasPermission($account, 'administer ' . $entity->getTargetEntityTypeId() . ' fields');
+  }
+
+}
---
+++ core/modules/field/tests/src/Unit/FieldConfigAccessControlHandlerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\Tests\field\Unit;
+
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\FieldConfigAccessControlHandler;
+
+/**
+ * Tests the field config access controller.
+ *
+ * @group field
+ *
+ * @coversDefaultClass \Drupal\field\FieldConfigAccessControlHandler
+ */
+class FieldConfigAccessControlHandlerTest extends FieldStorageConfigAccessControlHandlerTest {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->entity = new FieldConfig([
+      'field_name' => $this->fieldStorage->getName(),
+      'entity_type' => 'node',
+      'fieldStorage' => $this->fieldStorage,
+      'bundle' => 'test_bundle',
+      'field_type' => 'test_field',
+    ], 'node');
+
+    $this->accessControlHandler = new FieldConfigAccessControlHandler($this->entity->getEntityType());
+    $this->accessControlHandler->setModuleHandler($this->moduleHandler);
+  }
+
+  /**
+   * Ensures field config access is working properly.
+   */
+  public function testAccess() {
+    $this->assertAllowOperations([], $this->anon);
+    $this->assertAllowOperations(['view', 'update', 'delete'], $this->member);
+  }
+
+}
