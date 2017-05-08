699606e6036302783836884038007364b5ce6ab5
Gabor Hojtsy <gabor@hojtsy.hu>
2017-05-08 14:56:50 +0200

    Issue #2597680 by stimalsina, neclimdul, paryank, Derimagia, timodwhit, Dries, vinothg, heddn, benjy, effulgentsia: Some migrations have warnings when node types don't exist

--- core/modules/taxonomy/tests/src/Kernel/Migrate/d6/MigrateVocabularyEntityFormDisplayTest.php
+++ core/modules/taxonomy/tests/src/Kernel/Migrate/d6/MigrateVocabularyEntityFormDisplayTest.php
@@ -22,13 +22,24 @@ class MigrateVocabularyEntityFormDisplayTest extends MigrateDrupal6TestBase {
    */
   protected function setUp() {
     parent::setUp();
-    $this->migrateTaxonomy();
+
+    // Execute Dependency Migrations.
+    $this->migrateContentTypes();
+    $this->installEntitySchema('taxonomy_term');
+    $this->executeMigrations([
+      'd6_taxonomy_vocabulary',
+      'd6_vocabulary_field',
+      'd6_vocabulary_field_instance',
+      'd6_vocabulary_entity_display',
+    ]);
   }
 
   /**
    * Tests the Drupal 6 vocabulary-node type association to Drupal 8 migration.
    */
   public function testVocabularyEntityFormDisplay() {
+    $this->executeMigration('d6_vocabulary_entity_form_display');
+
     // Test that the field exists.
     $component = EntityFormDisplay::load('node.page.default')->getComponent('tags');
     $this->assertIdentical('options_select', $component['type']);
@@ -40,6 +51,25 @@ class MigrateVocabularyEntityFormDisplayTest extends MigrateDrupal6TestBase {
     $entity_form_display = EntityFormDisplay::load('node.story.default');
     $this->assertIdentical($entity_form_display->getComponent('vocabulary_1_i_0_')['type'], 'options_select');
     $this->assertIdentical($entity_form_display->getComponent('vocabulary_2_i_1_')['type'], 'entity_reference_autocomplete_tags');
+  }
+
+  /**
+   * Tests that vocabulary displays are ignored appropriately.
+   *
+   * Vocabulary displays should be ignored when they belong to node types which
+   * were not migrated.
+   */
+  public function testSkipNonExistentNodeType() {
+    // The "story" node type is migrated by d6_node_type but we need to pretend
+    // that it didn't occur, so record that in the map table.
+    $this->mockFailure('d6_node_type', ['type' => 'story']);
+
+    // d6_vocabulary_entity_form_display should skip over the "story" node type
+    // config because, according to the map table, it didn't occur.
+    $migration = $this->getMigration('d6_vocabulary_entity_form_display');
+
+    $this->executeMigration($migration);
+    $this->assertNull($migration->getIdMap()->lookupDestinationIds(['type' => 'story'])[0][0]);
   }
 
 }
--- core/modules/taxonomy/tests/src/Kernel/Migrate/d6/MigrateVocabularyFieldInstanceTest.php
+++ core/modules/taxonomy/tests/src/Kernel/Migrate/d6/MigrateVocabularyFieldInstanceTest.php
@@ -22,13 +22,23 @@ class MigrateVocabularyFieldInstanceTest extends MigrateDrupal6TestBase {
    */
   protected function setUp() {
     parent::setUp();
-    $this->migrateTaxonomy();
+
+    // Execute Dependency Migrations.
+    $this->migrateContentTypes();
+    $this->installEntitySchema('taxonomy_term');
+    $this->executeMigrations([
+      'd6_node_type',
+      'd6_taxonomy_vocabulary',
+      'd6_vocabulary_field',
+    ]);
   }
 
   /**
    * Tests the Drupal 6 vocabulary-node type association to Drupal 8 migration.
    */
   public function testVocabularyFieldInstance() {
+    $this->executeMigration('d6_vocabulary_field_instance');
+
     // Test that the field exists.
     $field_id = 'node.article.tags';
     $field = FieldConfig::load($field_id);
@@ -50,10 +60,29 @@ class MigrateVocabularyFieldInstanceTest extends MigrateDrupal6TestBase {
 
     $this->assertIdentical(['node', 'article', 'tags'], $this->getMigration('d6_vocabulary_field_instance')->getIdMap()->lookupDestinationID([4, 'article']));
 
-    // Test the the field vocabulary_1_i_0_
+    // Test the the field vocabulary_1_i_0_.
     $field_id = 'node.story.vocabulary_1_i_0_';
     $field = FieldConfig::load($field_id);
     $this->assertFalse($field->isRequired(), 'Field is not required');
+  }
+
+  /**
+   * Tests that vocabulary field instances are ignored appropriately.
+   *
+   * Vocabulary field instances should be ignored when they belong to node
+   * types which were not migrated.
+   */
+  public function testSkipNonExistentNodeType() {
+    // The "story" node type is migrated by d6_node_type but we need to pretend
+    // that it didn't occur, so record that in the map table.
+    $this->mockFailure('d6_node_type', ['type' => 'story']);
+
+    // d6_vocabulary_field_instance should skip over the "story" node type
+    // config because, according to the map table, it didn't occur.
+    $migration = $this->getMigration('d6_vocabulary_field_instance');
+
+    $this->executeMigration($migration);
+    $this->assertNull($migration->getIdMap()->lookupDestinationIds(['type' => 'story'])[0][0]);
   }
 
 }
