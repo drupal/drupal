fb8b2ce1f7027c9fb6b90450a2be91c8e42ddd63
Alex Pott <alex.a.pott@googlemail.com>
2017-05-12 10:58:42 +0100

    Issue #2746253 by borisson_, Leksat, eiriksm, alexpott: Configuration translation save triggers an undefined index notice
    (cherry picked from commit 240524f08c22a03d3cc0c7d9a63bd88e898c9f1f)

--- core/modules/config_translation/src/Tests/ConfigTranslationUiTest.php
+++ core/modules/config_translation/src/Tests/ConfigTranslationUiTest.php
@@ -12,6 +12,7 @@ use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\filter\Entity\FilterFormat;
 use Drupal\language\Entity\ConfigurableLanguage;
+use Drupal\node\Entity\NodeType;
 use Drupal\simpletest\WebTestBase;
 
 /**
@@ -1031,6 +1032,45 @@ class ConfigTranslationUiTest extends WebTestBase {
       ->get('config_translation_test.content')
       ->get('content');
     $this->assertEqual($expected, $actual);
+  }
+
+  /**
+   * Tests field translation for node fields.
+   */
+  public function testNodeFieldTranslation() {
+    NodeType::create(['type' => 'article', 'name' => 'Article'])->save();
+
+    $field_name = 'translatable_field';
+    $field_storage = FieldStorageConfig::create([
+      'field_name' => $field_name,
+      'entity_type' => 'node',
+      'type' => 'text',
+    ]);
+
+    $field_storage->setSetting('translatable_storage_setting', 'translatable_storage_setting');
+    $field_storage->save();
+    $field = FieldConfig::create([
+      'field_name' => $field_name,
+      'entity_type' => 'node',
+      'bundle' => 'article',
+    ]);
+    $field->save();
+
+    $this->drupalLogin($this->translatorUser);
+
+    $this->drupalGet("/entity_test/structure/article/fields/node.article.$field_name/translate");
+    $this->clickLink('Add');
+
+    $form_values = [
+      'translation[config_names][field.field.node.article.translatable_field][description]' => 'FR Help text.',
+      'translation[config_names][field.field.node.article.translatable_field][label]' => 'FR label',
+    ];
+    $this->drupalPostForm(NULL, $form_values, 'Save translation');
+    $this->assertText('Successfully saved French translation.');
+
+    // Check that the translations are saved.
+    $this->clickLink('Add');
+    $this->assertRaw('FR label');
   }
 
   /**
