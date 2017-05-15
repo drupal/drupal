e5ef38c03029d0f38eb459732cca35ac90a1e5bf
Alex Pott <alex.a.pott@googlemail.com>
2017-05-15 13:49:52 +0100

    Issue #2845364 by Wim Leers, EAnushan: Test coverage to show that as a non-administrator, you're able to modify the 'name' field, but only if you have the 'change own username' permission
    (cherry picked from commit d0fceeeda9c238995b14905a9b8254a356fa5c56)

--- core/modules/rest/tests/src/Functional/EntityResource/User/UserResourceTestBase.php
+++ core/modules/rest/tests/src/Functional/EntityResource/User/UserResourceTestBase.php
@@ -139,11 +139,11 @@ abstract class UserResourceTestBase extends EntityResourceTestBase {
 
     $this->initAuthentication();
     $this->provisionEntityResource();
-    $this->setUpAuthorization('PATCH');
 
     /** @var \Drupal\user\UserInterface $user */
     $user = static::$auth ? $this->account : User::load(0);
-    $original_normalization = array_diff_key($this->serializer->normalize($user, static::$format), ['changed' => TRUE]);
+    // @todo Remove the array_diff_key() call in https://www.drupal.org/node/2821077.
+    $original_normalization = array_diff_key($this->serializer->normalize($user, static::$format), ['created' => TRUE, 'changed' => TRUE, 'name' => TRUE]);
 
 
     // Since this test must be performed by the user that is being modified,
@@ -205,9 +205,52 @@ abstract class UserResourceTestBase extends EntityResourceTestBase {
 
 
     // Verify that we can log in with the new password.
+    $this->assertRpcLogin($user->getAccountName(), $new_password);
+
+
+    // Update password in $this->account, prepare for future requests.
+    $this->account->passRaw = $new_password;
+    $this->initAuthentication();
+    $request_options = [
+      RequestOptions::HEADERS => ['Content-Type' => static::$mimeType],
+    ];
+    $request_options = array_merge_recursive($request_options, $this->getAuthenticationRequestOptions('PATCH'));
+
+
+    // Test case 3: changing name.
+    $normalization = $original_normalization;
+    $normalization['name'] = [['value' => 'Cooler Llama']];
+    $request_options[RequestOptions::BODY] = $this->serializer->encode($normalization, static::$format);
+
+
+    // DX: 403 when modifying username without required permission.
+    $response = $this->request('PATCH', $url, $request_options);
+    $this->assertResourceErrorResponse(403, "Access denied on updating field 'name'.", $response);
+
+
+    $this->grantPermissionsToTestedRole(['change own username']);
+
+
+    // 200 for well-formed request.
+    $response = $this->request('PATCH', $url, $request_options);
+    $this->assertResourceResponse(200, FALSE, $response);
+
+    // Verify that we can log in with the new username.
+    $this->assertRpcLogin('Cooler Llama', $new_password);
+  }
+
+  /**
+   * Verifies that logging in with the given username and password works.
+   *
+   * @param string $username
+   *   The username to log in with.
+   * @param string $password
+   *   The password to log in with.
+   */
+  protected function assertRpcLogin($username, $password) {
     $request_body = [
-      'name' => $user->getAccountName(),
-      'pass' => $new_password,
+      'name' => $username,
+      'pass' => $password,
     ];
     $request_options = [
       RequestOptions::HEADERS => [],
