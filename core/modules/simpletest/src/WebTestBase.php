13ae357fc74606b36b39bb5a84f70142dbf3186f
Alex Pott <alex.a.pott@googlemail.com>
2017-05-20 21:21:48 +0100

    Issue #2795051 by legovaer, Lendude, dawehner: Move \Drupal\simpletest\WebTestBase::drupalBuildEntityView into a trait and make it available in BTB
    (cherry picked from commit b5742e6890f28ae9c8021cebf94c2af5fb79a319)

--- core/modules/simpletest/src/WebTestBase.php
+++ core/modules/simpletest/src/WebTestBase.php
@@ -9,16 +9,15 @@ use Drupal\Component\Utility\NestedArray;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Component\Utility\SafeMarkup;
 use Drupal\Core\Database\Database;
-use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\EventSubscriber\AjaxResponseSubscriber;
 use Drupal\Core\EventSubscriber\MainContentViewSubscriber;
-use Drupal\Core\Render\Element;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Session\AnonymousUserSession;
 use Drupal\Core\Test\AssertMailTrait;
 use Drupal\Core\Test\FunctionalTestSetupTrait;
 use Drupal\Core\Url;
 use Drupal\system\Tests\Cache\AssertPageCacheContextsAndTagsTrait;
+use Drupal\Tests\EntityViewTrait;
 use Drupal\Tests\Traits\Core\CronRunTrait;
 use Drupal\Tests\TestFileCreationTrait;
 use Drupal\Tests\XdebugRequestTrait;
@@ -59,6 +58,9 @@ abstract class WebTestBase extends TestBase {
   }
 
   use XdebugRequestTrait;
+  use EntityViewTrait {
+    buildEntityView as drupalBuildEntityView;
+  }
 
   /**
    * The profile to install as a basis for testing.
@@ -207,64 +209,6 @@ abstract class WebTestBase extends TestBase {
     parent::__construct($test_id);
     $this->skipClasses[__CLASS__] = TRUE;
     $this->classLoader = require DRUPAL_ROOT . '/autoload.php';
-  }
-
-  /**
-   * Builds the renderable view of an entity.
-   *
-   * Entities postpone the composition of their renderable arrays to #pre_render
-   * functions in order to maximize cache efficacy. This means that the full
-   * renderable array for an entity is constructed in drupal_render(). Some
-   * tests require the complete renderable array for an entity outside of the
-   * drupal_render process in order to verify the presence of specific values.
-   * This method isolates the steps in the render process that produce an
-   * entity's renderable array.
-   *
-   * @param \Drupal\Core\Entity\EntityInterface $entity
-   *   The entity to prepare a renderable array for.
-   * @param string $view_mode
-   *   (optional) The view mode that should be used to build the entity.
-   * @param null $langcode
-   *   (optional) For which language the entity should be prepared, defaults to
-   *   the current content language.
-   * @param bool $reset
-   *   (optional) Whether to clear the cache for this entity.
-   * @return array
-   *
-   * @see drupal_render()
-   */
-  protected function drupalBuildEntityView(EntityInterface $entity, $view_mode = 'full', $langcode = NULL, $reset = FALSE) {
-    $ensure_fully_built = function(&$elements) use (&$ensure_fully_built) {
-      // If the default values for this element have not been loaded yet, populate
-      // them.
-      if (isset($elements['#type']) && empty($elements['#defaults_loaded'])) {
-        $elements += \Drupal::service('element_info')->getInfo($elements['#type']);
-      }
-
-      // Make any final changes to the element before it is rendered. This means
-      // that the $element or the children can be altered or corrected before the
-      // element is rendered into the final text.
-      if (isset($elements['#pre_render'])) {
-        foreach ($elements['#pre_render'] as $callable) {
-          $elements = call_user_func($callable, $elements);
-        }
-      }
-
-      // And recurse.
-      $children = Element::children($elements, TRUE);
-      foreach ($children as $key) {
-        $ensure_fully_built($elements[$key]);
-      }
-    };
-
-    $render_controller = $this->container->get('entity.manager')->getViewBuilder($entity->getEntityTypeId());
-    if ($reset) {
-      $render_controller->resetCache([$entity->id()]);
-    }
-    $build = $render_controller->view($entity, $view_mode, $langcode);
-    $ensure_fully_built($build);
-
-    return $build;
   }
 
   /**
