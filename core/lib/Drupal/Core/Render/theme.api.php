b913e8625b5ff1dd7754279e004ea91bc1d24318
Alex Pott <alex.a.pott@googlemail.com>
2017-05-22 12:28:44 +0100

    Issue #2875443 by pankajsachdeva, pritish.kumar, brentgees: Remove D7 links from D8 Render API documentation
    (cherry picked from commit 84233bab05946e61a4a485986a7718f8cdebd5f6)

--- core/lib/Drupal/Core/Render/theme.api.php
+++ core/lib/Drupal/Core/Render/theme.api.php
@@ -225,12 +225,7 @@
  * For further information on the Theme and Render APIs, see:
  * - https://www.drupal.org/docs/8/theming
  * - https://www.drupal.org/developing/api/8/render
- * - https://www.drupal.org/node/722174
- * - https://www.drupal.org/node/933976
- * - https://www.drupal.org/node/930760
- *
- * @todo Check these links. Some are for Drupal 7, and might need updates for
- *   Drupal 8.
+ * - @link themeable Theme system overview @endlink.
  *
  * @section arrays Render arrays
  * The core structure of the Render API is the render array, which is a
