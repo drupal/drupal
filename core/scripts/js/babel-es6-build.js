bc12e67bdd0aae4af477846891470dab519b85f3
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-05-22 23:52:50 +0100

    Issue #2880013 by alexpott, droplet: Add command to check if .es6.js has been transpiled

--- core/scripts/js/babel-es6-build.js
+++ core/scripts/js/babel-es6-build.js
@@ -1,7 +1,14 @@
 /**
  * @file
  *
- * Compile *.es6.js files to ES5.
+ * Provides the build:js command to compile *.es6.js files to ES5.
+ *
+ * Run build:js with --file to only parse a specific file. Using the --check
+ * flag build:js can be run to check if files are compiled correctly.
+ * @example <caption>Only process misc/drupal.es6.js and misc/drupal.init.es6.js</caption
+ * yarn run build:js -- --file misc/drupal.es6.js --file misc/drupal.init.es6.js
+ * @example <caption>Check if all files have been compiled correctly</caption
+ * yarn run build:js -- --check
  *
  * @internal This file is part of the core javascript build process and is only
  * meant to be used in that context.
@@ -9,11 +16,10 @@
 
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
 const glob = require('glob');
-
+const argv = require('minimist')(process.argv.slice(2));
 const changeOrAdded = require('./changeOrAdded');
+const check = require('./check');
 const log = require('./log');
 
 // Match only on .es6.js files.
@@ -27,14 +33,15 @@ const processFiles = (error, filePaths) => {
     process.exitCode = 1;
   }
   // Process all the found files.
-  filePaths.forEach(changeOrAdded);
+  let callback = changeOrAdded;
+  if (argv.check) {
+    callback = check;
+  }
+  filePaths.forEach(callback);
 };
 
-// Run build:js with some special arguments to only parse specific files.
-// npm run build:js -- --files misc/drupal.es6.js misc/drupal.init.es6.js
-// Only misc/drupal.es6.js misc/drupal.init.es6.js will be processed.
-if (process.argv.length > 2 && process.argv[2] === '--files') {
-  processFiles(null, process.argv.splice(3, process.argv.length));
+if (argv.file) {
+  processFiles(null, [].concat(argv.file));
 }
 else {
   glob(fileMatch, globOptions, processFiles);
