3516dfcf2801e75e1415761bcc24921494e7081e
Alex Pott <alex.a.pott@googlemail.com>
2017-05-23 18:03:34 +0100

    Issue #2865213 by acbramley, Sam152, timmillwood: Moderation form submission does not set revision author
    (cherry picked from commit 3225473ddddf23fbfd9d22fe27190165a701c744)

--- core/modules/content_moderation/src/Form/EntityModerationForm.php
+++ core/modules/content_moderation/src/Form/EntityModerationForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\content_moderation\Form;
 
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\RevisionLogInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\content_moderation\ModerationInformationInterface;
@@ -127,8 +128,11 @@ class EntityModerationForm extends FormBase {
     $new_state = $form_state->getValue('new_state');
 
     $entity->set('moderation_state', $new_state);
-    $entity->revision_log = $form_state->getValue('revision_log');
 
+    if ($entity instanceof RevisionLogInterface) {
+      $entity->setRevisionLogMessage($form_state->getValue('revision_log'));
+      $entity->setRevisionUserId($this->currentUser()->id());
+    }
     $entity->save();
 
     drupal_set_message($this->t('The moderation state has been updated.'));
