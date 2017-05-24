4bd9426ae0efb91311f383c69e9888fa6841e124
Alex Pott <alex.a.pott@googlemail.com>
2017-05-24 22:26:39 +0100

    Issue #2881012 by Lendude, dawehner, michielnugter: Move generatePermutations out of testbase into a trait

--- core/modules/comment/tests/src/Unit/CommentLinkBuilderTest.php
+++ core/modules/comment/tests/src/Unit/CommentLinkBuilderTest.php
@@ -6,7 +6,7 @@ use Drupal\comment\CommentLinkBuilder;
 use Drupal\comment\Plugin\Field\FieldType\CommentItemInterface;
 use Drupal\Core\Url;
 use Drupal\node\NodeInterface;
-use Drupal\simpletest\TestBase;
+use Drupal\Tests\Traits\Core\GeneratePermutationsTrait;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -14,6 +14,8 @@ use Drupal\Tests\UnitTestCase;
  * @group comment
  */
 class CommentLinkBuilderTest extends UnitTestCase {
+
+  use GeneratePermutationsTrait;
 
   /**
    * Comment manager mock.
@@ -195,7 +197,7 @@ class CommentLinkBuilderTest extends UnitTestCase {
         'teaser', 'rss', 'full',
       ],
     ];
-    $permutations = TestBase::generatePermutations($combinations);
+    $permutations = $this->generatePermutations($combinations);
     foreach ($permutations as $combination) {
       $case = [
         $this->getMockNode(TRUE, $combination['comments'], $combination['form_location'], $combination['comment_count']),
