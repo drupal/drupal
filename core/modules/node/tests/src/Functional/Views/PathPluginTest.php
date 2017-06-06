bc653996b57c8578e092c9a09ff8e2103881584a
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-06-06 10:43:35 +0100

    Issue #2870453 by Lendude, michielnugter, dawehner: Convert web tests to browser tests for node module
    (cherry picked from commit 4416f7bb3b0c30e650e24612b2cd358c976b7689)

---
+++ core/modules/node/tests/src/Functional/Views/PathPluginTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\Tests\node\Functional\Views;
+
+use Drupal\views\Views;
+
+/**
+ * Tests the node row plugin.
+ *
+ * @group node
+ */
+class PathPluginTest extends NodeTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['node'];
+
+  /**
+   * Views used by this test.
+   *
+   * @var array
+   */
+  public static $testViews = ['test_node_path_plugin'];
+
+  /**
+   * Contains all nodes used by this test.
+   *
+   * @var Node[]
+   */
+  protected $nodes;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp($import_test_views = TRUE) {
+    parent::setUp($import_test_views);
+
+    $this->drupalCreateContentType(['type' => 'article']);
+
+    // Create two nodes.
+    for ($i = 0; $i < 2; $i++) {
+      $this->nodes[] = $this->drupalCreateNode(
+        [
+          'type' => 'article',
+          'body' => [
+            [
+              'value' => $this->randomMachineName(42),
+              'format' => filter_default_format(),
+              'summary' => $this->randomMachineName(),
+            ],
+          ],
+        ]
+      );
+    }
+  }
+
+  /**
+   * Tests the node path plugin.
+   */
+  public function testPathPlugin() {
+    /** @var \Drupal\Core\Render\RendererInterface $renderer */
+    $renderer = $this->container->get('renderer');
+    $view = Views::getView('test_node_path_plugin');
+    $view->initDisplay();
+    $view->setDisplay('page_1');
+    $view->initStyle();
+    $view->rowPlugin->options['view_mode'] = 'full';
+
+    // Test with view_mode full.
+    $output = $view->preview();
+    $output = $renderer->renderRoot($output);
+    foreach ($this->nodes as $node) {
+      $this->assertTrue(strpos($output, 'This is <strong>not escaped</strong> and this is ' . $node->link('the link')) !== FALSE, 'Make sure path field rewriting is not escaped.');
+    }
+  }
+
+}
---
+++ core/modules/node/tests/src/Functional/Views/RowPluginTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Drupal\Tests\node\Functional\Views;
+
+use Drupal\views\Views;
+
+/**
+ * Tests the node row plugin.
+ *
+ * @group node
+ * @see \Drupal\node\Plugin\views\row\NodeRow
+ */
+class RowPluginTest extends NodeTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['node'];
+
+  /**
+   * Views used by this test.
+   *
+   * @var array
+   */
+  public static $testViews = ['test_node_row_plugin'];
+
+  /**
+   * Contains all nodes used by this test.
+   *
+   * @var array
+   */
+  protected $nodes;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp($import_test_views = TRUE) {
+    parent::setUp($import_test_views);
+
+    $this->drupalCreateContentType(['type' => 'article']);
+
+    // Create two nodes.
+    for ($i = 0; $i < 2; $i++) {
+      $this->nodes[] = $this->drupalCreateNode(
+        [
+          'type' => 'article',
+          'body' => [
+            [
+              'value' => $this->randomMachineName(42),
+              'format' => filter_default_format(),
+              'summary' => $this->randomMachineName(),
+            ],
+          ],
+        ]
+      );
+    }
+  }
+
+  /**
+   * Tests the node row plugin.
+   */
+  public function testRowPlugin() {
+    /** @var \Drupal\Core\Render\RendererInterface $renderer */
+    $renderer = $this->container->get('renderer');
+    $view = Views::getView('test_node_row_plugin');
+    $view->initDisplay();
+    $view->setDisplay('page_1');
+    $view->initStyle();
+    $view->rowPlugin->options['view_mode'] = 'full';
+
+    // Test with view_mode full.
+    $output = $view->preview();
+    $output = $renderer->renderRoot($output);
+    foreach ($this->nodes as $node) {
+      $this->assertFalse(strpos($output, $node->body->summary) !== FALSE, 'Make sure the teaser appears in the output of the view.');
+      $this->assertTrue(strpos($output, $node->body->value) !== FALSE, 'Make sure the full text appears in the output of the view.');
+    }
+
+    // Test with teasers.
+    $view->rowPlugin->options['view_mode'] = 'teaser';
+    $output = $view->preview();
+    $output = $renderer->renderRoot($output);
+    foreach ($this->nodes as $node) {
+      $this->assertTrue(strpos($output, $node->body->summary) !== FALSE, 'Make sure the teaser appears in the output of the view.');
+      $this->assertFalse(strpos($output, $node->body->value) !== FALSE, 'Make sure the full text does not appears in the output of the view if teaser is set as viewmode.');
+    }
+  }
+
+}
