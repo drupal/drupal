e77fbeeca04711e62c076ae46d46162d139579dd
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-06-20 16:42:42 +0100

    Issue #2886801 by phenaproxima: Migrate D6 user reference values to D8
    (cherry picked from commit a6fb560a7a323c6ae154b0f53571b9b9943b0d86)

---
+++ core/modules/migrate_drupal/src/Plugin/migrate/field/UserReference.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\migrate_drupal\Plugin\migrate\field;
+
+use Drupal\migrate\Plugin\MigrationInterface;
+
+/**
+ * @MigrateField(
+ *   id = "userreference",
+ *   core = {6},
+ *   type_map = {
+ *     "userreference" = "entity_reference",
+ *   },
+ * )
+ */
+class UserReference extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFieldFormatterMap() {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processFieldValues(MigrationInterface $migration, $field_name, $data) {
+    $process = [
+      'plugin' => 'iterator',
+      'source' => $field_name,
+      'process' => [
+        'target_id' => [
+          'plugin' => 'migration_lookup',
+          'migration' => 'd6_user',
+          'source' => 'uid',
+        ],
+      ],
+    ];
+    $migration->setProcessOfProperty($field_name, $process);
+  }
+
+}
