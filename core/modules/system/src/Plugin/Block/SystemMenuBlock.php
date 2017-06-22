6eb59c81c096d2dc3e6b7653aabf5b0607e193d6
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-06-22 11:17:59 +0100

    Issue #2631468 by snufkin, johnmcc, DamienMcKenna, aaronbauman, kiwad, acrosman, esolitos, Tom Robert, Yogesh Pawar, acke: Menu subtrees in menu blocks show all subitems regardless of the active menu item

--- core/modules/system/src/Plugin/Block/SystemMenuBlock.php
+++ core/modules/system/src/Plugin/Block/SystemMenuBlock.php
@@ -135,6 +135,25 @@ class SystemMenuBlock extends BlockBase implements ContainerFactoryPluginInterfa
       $parameters->setMaxDepth(min($level + $depth - 1, $this->menuTree->maxDepth()));
     }
 
+    // For menu blocks with start level greater than 1, only show menu items
+    // from the current active trail. Adjust the root according to the current
+    // position in the menu in order to determine if we can show the subtree.
+    if ($level > 1) {
+      if (count($parameters->activeTrail) >= $level) {
+        // Active trail array is child-first. Reverse it, and pull the new menu
+        // root based on the parent of the configured start level.
+        $menu_trail_ids = array_reverse(array_values($parameters->activeTrail));
+        $menu_root = $menu_trail_ids[$level - 1];
+        $parameters->setRoot($menu_root)->setMinDepth(1);
+        if ($depth > 0) {
+          $parameters->setMaxDepth(min($level - 1 + $depth - 1, $this->menuTree->maxDepth()));
+        }
+      }
+      else {
+        return [];
+      }
+    }
+
     $tree = $this->menuTree->load($menu_name, $parameters);
     $manipulators = [
       ['callable' => 'menu.default_tree_manipulators:checkAccess'],
