e1e216138f5311a5dd6b96b5789ab2db9ac5fd5f
Lauri Eskola <lauriii@1078742.no-reply.drupal.org>
2017-07-07 09:27:15 +0300

    Issue #2891797 by FeyP: Document last parameter of Views Entity Bundle Filter constructor

--- core/modules/views/src/Plugin/views/filter/Bundle.php
+++ core/modules/views/src/Plugin/views/filter/Bundle.php
@@ -56,6 +56,8 @@ class Bundle extends InOperator {
    *   The plugin implementation definition.
    * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
    *   The entity manager.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $bundle_info_service
+   *   The bundle info service.
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entity_manager, EntityTypeBundleInfoInterface $bundle_info_service) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
