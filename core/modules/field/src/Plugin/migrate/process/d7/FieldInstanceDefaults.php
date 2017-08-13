4ff7617a62ae963732850dc6a41c50b39dcb62f1
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-08-13 14:14:37 +0900

    Issue #2891935 by maxocub, Jo Fitzgerald, heddn: Refactor D7 FieldInstance source plugin and other field instance related source plugins

--- core/modules/field/src/Plugin/migrate/process/d7/FieldInstanceDefaults.php
+++ core/modules/field/src/Plugin/migrate/process/d7/FieldInstanceDefaults.php
@@ -19,6 +19,14 @@ class FieldInstanceDefaults extends ProcessPluginBase {
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     list($default_value, $widget_settings) = $value;
     $widget_type = $widget_settings['type'];
+    $default_value = $default_value ?: [];
+
+    // In Drupal 7, the default value for email fields is stored in the key
+    // 'email' while in Drupal 8 it is stored in the key 'value'.
+    if ($widget_type == 'email_textfield' && $default_value) {
+      $default_value[0]['value'] = $default_value[0]['email'];
+      unset($default_value[0]['email']);
+    }
 
     $default = [];
 
