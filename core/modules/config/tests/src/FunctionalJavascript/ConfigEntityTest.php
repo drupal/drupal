91229428d931f71be62c29b5861d8bebdd5c0cd2
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-09-04 15:20:21 +0100

    Issue #2809471 by vaplas, michielnugter, dawehner, Lendude: Convert AJAX part of \Drupal\config\Tests\ConfigEntityTest::testCRUDUI to JavascriptTestBase
    (cherry picked from commit 8ec6e83fccadaccf83717f70d2c8f2a687fb7771)

---
+++ core/modules/config/tests/src/FunctionalJavascript/ConfigEntityTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\Tests\config\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
+
+/**
+ * Tests the Config operations through the UI.
+ *
+ * @group config
+ */
+class ConfigEntityTest extends JavascriptTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['config_test'];
+
+  /**
+   * Tests ajax operations through the UI on 'Add' page.
+   */
+  public function testAjaxOnAddPage() {
+    $this->drupalLogin($this->drupalCreateUser(['administer site configuration']));
+
+    $page = $this->getSession()->getPage();
+    $assert_session = $this->assertSession();
+
+    $this->drupalGet('admin/structure/config_test/add');
+    // Test that 'size value' field is not show initially, and it is show after
+    // selecting value in the 'size' field.
+    $this->assertNull($page->findField('size_value'));
+    $page->findField('size')->setValue('custom');
+    $this->assertNotNull($assert_session->waitForField('size_value'));
+  }
+
+}
