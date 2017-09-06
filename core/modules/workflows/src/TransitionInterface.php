3600228e6189fd188ab18806482ff47fdebba426
xjm <xjm@65776.no-reply.drupal.org>
2017-09-06 11:39:27 -0500

    Issue #2897130 followup by timmillwood, Wim Leers: Fix @internal documentation.
    (cherry picked from commit 1f6adcd816e0b5782197a17e42ec9a8f00885838)

--- core/modules/workflows/src/TransitionInterface.php
+++ core/modules/workflows/src/TransitionInterface.php
@@ -4,6 +4,12 @@ namespace Drupal\workflows;
 
 /**
  * A transition value object that describes the transition between two states.
+ *
+ * @internal
+ *   The TransitionInterface should only be used by Workflows and Content
+ *   Moderation.
+ *
+ * @todo Revisit the need for this in https://www.drupal.org/node/2902309.
  */
 interface TransitionInterface {
 
