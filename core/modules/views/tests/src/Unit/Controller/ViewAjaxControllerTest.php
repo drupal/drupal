6b9ffd82cedeca6f51a3e373d74b7538615192d9
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-09-11 14:06:45 +0100

    Issue #2904798 by dawehner, ahebrank: 8.3.7 Views SA patch should use ajaxEnabled() and not check option 'use_ajax' directly
    (cherry picked from commit 0f36528a5481e48593aac919515a68bc1c930c18)

--- core/modules/views/tests/src/Unit/Controller/ViewAjaxControllerTest.php
+++ core/modules/views/tests/src/Unit/Controller/ViewAjaxControllerTest.php
@@ -344,8 +344,7 @@ class ViewAjaxControllerTest extends UnitTestCase {
     $display_handler->expects($this->never())
       ->method('setOption');
     $display_handler->expects($this->any())
-      ->method('getOption')
-      ->with('use_ajax')
+      ->method('ajaxEnabled')
       ->willReturn($use_ajax);
 
     $display_collection = $this->getMockBuilder('Drupal\views\DisplayPluginCollection')
