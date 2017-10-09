152e49f6f6269ceaa3d6f9e8d09a5759ed9cc78f
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-10-09 13:24:37 +0100

    Issue #2914249 by maxocub, dillix, masipila, phenaproxima: Translated forum vocabulary migration creates duplicate forum vocabularies
    (cherry picked from commit a20201a5ab247d38735466394d6ef42e3f3197ef)

--- core/modules/taxonomy/src/Plugin/migrate/source/d7/Vocabulary.php
+++ core/modules/taxonomy/src/Plugin/migrate/source/d7/Vocabulary.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\taxonomy\Plugin\migrate\source\d7;
 
+use Drupal\migrate\Row;
 use Drupal\migrate_drupal\Plugin\migrate\source\DrupalSqlBase;
 
 /**
@@ -44,6 +45,20 @@ class Vocabulary extends DrupalSqlBase {
       'weight' => $this->t('The weight of the vocabulary in relation to other vocabularies.'),
       'machine_name' => $this->t('Unique machine name of the vocabulary.')
     ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    // If the vocabulary being migrated is the one defined in the
+    // 'forum_nav_vocabulary' variable, set the 'forum_vocabulary' source
+    // property to true so we know this is the vocabulary used by Forum.
+    if ($this->variableGet('forum_nav_vocabulary', 0) == $row->getSourceProperty('vid')) {
+      $row->setSourceProperty('forum_vocabulary', TRUE);
+    }
+
+    return parent::prepareRow($row);
   }
 
   /**
