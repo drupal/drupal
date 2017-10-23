66cbd7c9e5540611918b378592ba775ac4756741
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-10-23 13:53:06 +0100

    Issue #2864995 by amateescu, Dinesh18, Sam152: Allow entity query to query the latest revision

--- core/lib/Drupal/Core/Entity/Query/QueryInterface.php
+++ core/lib/Drupal/Core/Entity/Query/QueryInterface.php
@@ -255,6 +255,17 @@ interface QueryInterface extends AlterableInterface {
   public function currentRevision();
 
   /**
+   * Queries the latest revision.
+   *
+   * The latest revision is the most recent revision of an entity. This will be
+   * either the default revision, or a pending revision if one exists and it is
+   * newer than the default.
+   *
+   * @return $this
+   */
+  public function latestRevision();
+
+  /**
    * Queries all the revisions.
    *
    * @return $this
