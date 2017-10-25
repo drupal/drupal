06ca70fdba14010b14e189b840cc9b30c000a69b
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-10-25 11:56:02 +0100

    Issue #2913459 by Eric_A: drupal/core-class-finder missing in ComposerIntegrationTest

--- core/tests/Drupal/Tests/ComposerIntegrationTest.php
+++ core/tests/Drupal/Tests/ComposerIntegrationTest.php
@@ -47,6 +47,7 @@ class ComposerIntegrationTest extends UnitTestCase {
       $this->root . '/core/lib/Drupal/Component/Annotation',
       $this->root . '/core/lib/Drupal/Component/Assertion',
       $this->root . '/core/lib/Drupal/Component/Bridge',
+      $this->root . '/core/lib/Drupal/Component/ClassFinder',
       $this->root . '/core/lib/Drupal/Component/Datetime',
       $this->root . '/core/lib/Drupal/Component/DependencyInjection',
       $this->root . '/core/lib/Drupal/Component/Diff',
