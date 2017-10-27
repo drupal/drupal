fdce9b702e02772ef0915ac046555931b15bc27c
xjm <xjm@65776.no-reply.drupal.org>
2017-10-27 18:52:28 -0500

    Issue #2915673 by borisson_, alexpott, Gábor Hojtsy: Ignore coding standards in sites/* PHP files as they are for configuration and not real code

--- sites/example.settings.local.php
+++ sites/example.settings.local.php
@@ -1,5 +1,7 @@
 <?php
 
+// @codingStandardsIgnoreFile
+
 /**
  * @file
  * Local development override configuration feature.
--- sites/example.sites.php
+++ sites/example.sites.php
@@ -1,5 +1,7 @@
 <?php
 
+// @codingStandardsIgnoreFile
+
 /**
  * @file
  * Configuration file for multi-site support and directory aliasing feature.
