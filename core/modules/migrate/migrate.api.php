33818be2d810be751b11c81bdafcd300f15b055a
xjm <xjm@65776.no-reply.drupal.org>
2017-11-07 06:19:19 -0600

    Issue #2917883 by quietone, TR, Jo Fitzgerald, masipila, phenaproxima: Rename 'migration_templates' directories in core modules to 'migrations'

--- core/modules/migrate/migrate.api.php
+++ core/modules/migrate/migrate.api.php
@@ -38,9 +38,8 @@ use Drupal\migrate\Row;
  *
  * @section sec_migrations Migration plugins
  * Migration plugin definitions are stored in a module's 'migrations' directory.
- * For backwards compatibility we also scan the 'migration_templates' directory.
  * Examples of migration plugin definitions can be found in
- * 'core/modules/action/migration_templates'. The plugin class is
+ * 'core/modules/action/migrations'. The plugin class is
  * \Drupal\migrate\Plugin\Migration, with interface
  * \Drupal\migrate\Plugin\MigrationInterface. Migration plugins are managed by
  * the \Drupal\migrate\Plugin\MigrationPluginManager class. Migration plugins
---
+++ core/modules/migrate/tests/src/Kernel/Plugin/MigrationDirectoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\Tests\migrate\Kernel\Plugin;
+
+use Drupal\Tests\migrate_drupal\Kernel\MigrateDrupalTestBase;
+
+/**
+ * Tests that migrations exist in the migration_templates directory.
+ *
+ * @group migrate
+ */
+class MigrationDirectoryTest extends MigrateDrupalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['migration_directory_test'];
+
+  /**
+   * Tests that migrations in the migration_templates directory are created.
+   */
+  public function testMigrationDirectory() {
+    /** @var \Drupal\migrate\Plugin\MigrationPluginManager $plugin_manager */
+    $plugin_manager = $this->container->get('plugin.manager.migration');
+    // Tests that a migration in directory 'migration_templates' is discovered.
+    $this->assertTrue($plugin_manager->hasDefinition('migration_templates_test'));
+  }
+
+}
