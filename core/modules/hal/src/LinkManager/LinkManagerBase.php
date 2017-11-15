fd497ef04203b3dcf4ed783cb975b660a689e272
xjm <xjm@65776.no-reply.drupal.org>
2017-11-15 11:58:01 -0500

    Revert "Issue #2864816 by Wim Leers, dawehner, larowlan, tedbow: HAL LinkManager doesn't add 'url.site' cache context when needed"
    This reverts commit 66df3e1c8274e7bbea37f7f10d93bbe4815ea7d4.

--- core/modules/hal/src/LinkManager/LinkManagerBase.php
+++ core/modules/hal/src/LinkManager/LinkManagerBase.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\hal\LinkManager;
 
-use Drupal\rest\EventSubscriber\ResourceResponseSubscriber;
-
 /**
  * Defines an abstract base-class for HAL link manager objects.
  */
@@ -41,32 +39,17 @@ abstract class LinkManagerBase {
   /**
    * Gets the link domain.
    *
-   * @param array $context
-   *   Normalization/serialization context.
-   *
    * @return string
    *   The link domain.
-   *
-   * @see \Symfony\Component\Serializer\Normalizer\NormalizerInterface::normalize()
-   * @see \Symfony\Component\Serializer\SerializerInterface::serialize()
-   * @see \Drupal\rest\EventSubscriber\ResourceResponseSubscriber::SERIALIZATION_CONTEXT_CACHEABILITY
    */
-  protected function getLinkDomain(array $context = []) {
+  protected function getLinkDomain() {
     if (empty($this->linkDomain)) {
       if ($domain = $this->configFactory->get('hal.settings')->get('link_domain')) {
-        // Bubble the appropriate cacheability metadata whenever possible.
-        if (isset($context[ResourceResponseSubscriber::SERIALIZATION_CONTEXT_CACHEABILITY])) {
-          $context[ResourceResponseSubscriber::SERIALIZATION_CONTEXT_CACHEABILITY]->addCacheableDependency($this->configFactory->get('hal.settings'));
-        }
-        return rtrim($domain, '/');
+        $this->linkDomain = rtrim($domain, '/');
       }
       else {
-        // Bubble the relevant cacheability metadata whenever possible.
-        if (isset($context[ResourceResponseSubscriber::SERIALIZATION_CONTEXT_CACHEABILITY])) {
-          $context[ResourceResponseSubscriber::SERIALIZATION_CONTEXT_CACHEABILITY]->addCacheContexts(['url.site']);
-        }
         $request = $this->requestStack->getCurrentRequest();
-        return $request->getSchemeAndHttpHost() . $request->getBasePath();
+        $this->linkDomain = $request->getSchemeAndHttpHost() . $request->getBasePath();
       }
     }
     return $this->linkDomain;
