5564bba7d7705719e3bef731d6575df2b0ecf794
Gabor Hojtsy <gabor@hojtsy.hu>
2017-12-11 17:36:36 +0100

    Issue #2862745 by masipila, quietone: Add documentation to EntityFieldInstance destination plugin

--- core/modules/migrate/src/Plugin/migrate/destination/EntityFieldInstance.php
+++ core/modules/migrate/src/Plugin/migrate/destination/EntityFieldInstance.php
@@ -3,7 +3,43 @@
 namespace Drupal\migrate\Plugin\migrate\destination;
 
 /**
- * Provides entity field instance plugin.
+ * Provides destination plugin for field_config configuration entities.
+ *
+ * The Field API defines two primary data structures, FieldStorage and Field.
+ * A FieldStorage defines a particular type of data that can be attached to
+ * entities as a Field instance.
+ *
+ * The example below adds an instance of 'field_text_example' to 'article'
+ * bundle (node content type). The example uses the EmptySource source plugin
+ * and constant source values for the sake of simplicity. For an example on how
+ * the FieldStorage 'field_text_example' can be migrated, refer to
+ * \Drupal\migrate\Plugin\migrate\destination\EntityFieldStorageConfig.
+ * @code
+ * id: field_instance_example
+ * label: Field instance example
+ * source:
+ *   plugin: empty
+ *   constants:
+ *     entity_type: node
+ *     field_name: field_text_example
+ *     bundle: article
+ *     label: Text field example
+ *     translatable: true
+ *  process:
+ *    entity_type: constants/entity_type
+ *    field_name: constants/field_name
+ *    bundle: constants/bundle
+ *    label: constants/label
+ *     translatable: constants/translatable
+ *  destination:
+ *    plugin: entity:field_config
+ *  migration_dependencies:
+ *    required:
+ *      - field_storage_example
+ * @endcode
+ *
+ * @see \Drupal\field\Entity\FieldConfig
+ * @see \Drupal\field\Entity\FieldConfigBase
  *
  * @MigrateDestination(
  *   id = "entity:field_config"
