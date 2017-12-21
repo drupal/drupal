4e5b4b09bde155d50ed3b84ee1fa399347bb83ef
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2017-12-21 11:49:17 +0000

    Issue #2931368 by opdavies: Add missing param documentation for hook_migrate_prepare_row, document hook_migrate_MIGRATION_ID_prepare_row

--- core/modules/migrate/migrate.api.php
+++ core/modules/migrate/migrate.api.php
@@ -98,6 +98,13 @@ use Drupal\migrate\Row;
  *
  * hook_migrate_MIGRATION_ID_prepare_row() is also available.
  *
+ * @param \Drupal\migrate\Row $row
+ *   The row being imported.
+ * @param \Drupal\migrate\Plugin\MigrateSourceInterface $source
+ *   The source migration.
+ * @param \Drupal\migrate\Plugin\MigrationInterface $migration
+ *   The current migration.
+ *
  * @ingroup migration
  */
 function hook_migrate_prepare_row(Row $row, MigrateSourceInterface $source, MigrationInterface $migration) {
@@ -106,6 +113,28 @@ function hook_migrate_prepare_row(Row $row, MigrateSourceInterface $source, Migr
     if ($value) {
       $row->setSourceProperty('settings:mymodule:foo', unserialize($value));
     }
+  }
+}
+
+/**
+ * Allows adding data to a row for a migration with the specified ID.
+ *
+ * This provides the same functionality as hook_migrate_prepare_row() but
+ * removes the need to check the value of $migration->id().
+ *
+ * @param \Drupal\migrate\Row $row
+ *   The row being imported.
+ * @param \Drupal\migrate\Plugin\MigrateSourceInterface $source
+ *   The source migration.
+ * @param \Drupal\migrate\Plugin\MigrationInterface $migration
+ *   The current migration.
+ *
+ * @ingroup migration
+ */
+function hook_migrate_MIGRATION_ID_prepare_row(Row $row, MigrateSourceInterface $source, MigrationInterface $migration) {
+  $value = $source->getDatabase()->query('SELECT value FROM {variable} WHERE name = :name', [':name' => 'mymodule_filter_foo_' . $row->getSourceProperty('format')])->fetchField();
+  if ($value) {
+    $row->setSourceProperty('settings:mymodule:foo', unserialize($value));
   }
 }
 
