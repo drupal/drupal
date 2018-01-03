35b2990a1ba8ba455e478830266011702553c3b1
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-01-03 15:47:09 +0000

    Issue #2930715 by alexpott, dawehner: Recursive rebuild caused by installing admin_toolbar_tools module
    (cherry picked from commit 0d2a45adc2dd3c84ccd893875edf2820a4d5e824)

--- core/tests/Drupal/FunctionalTests/Routing/LazyRouteProviderInstallTest.php
+++ core/tests/Drupal/FunctionalTests/Routing/LazyRouteProviderInstallTest.php
@@ -23,6 +23,11 @@ class LazyRouteProviderInstallTest extends BrowserTestBase {
     // we cannot use ::assertEquals().
     $this->assertStringEndsWith('/admin', \Drupal::state()->get('Drupal\lazy_route_provider_install_test\PluginManager'));
     $this->assertStringEndsWith('/router_test/test1', \Drupal::state()->get('router_test_install'));
+    // If there is an exception thrown in rebuilding a route then the state
+    // 'lazy_route_provider_install_test_menu_links_discovered_alter' will be
+    // set.
+    // @see lazy_route_provider_install_test_menu_links_discovered_alter().
+    $this->assertEquals('success', \Drupal::state()->get('lazy_route_provider_install_test_menu_links_discovered_alter', NULL));
   }
 
 }
