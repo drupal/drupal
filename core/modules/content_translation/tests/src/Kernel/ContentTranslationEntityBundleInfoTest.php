82a20b9af8c3f410f184ea688a9a4afc240de818
effulgentsia <alex.bronstein@acquia.com>
2018-01-27 15:19:18 -0800

    Issue #2939909 by plach, xjm, Wim Leers: Ensure that hook_bundle_info_alter() implementations know whether bundles are translatable
    (cherry picked from commit bc1fc2f7bdd61a5c9f1fcb0cef41de95b5249257)

---
+++ core/modules/content_translation/tests/src/Kernel/ContentTranslationEntityBundleInfoTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\Tests\content_translation\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests the Content Translation bundle info logic.
+ *
+ * @group content_translation
+ */
+class ContentTranslationEntityBundleInfoTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['user', 'language', 'content_translation_test', 'content_translation', 'entity_test'];
+
+  /**
+   * The content translation manager.
+   *
+   * @var \Drupal\content_translation\ContentTranslationManagerInterface
+   */
+  protected $contentTranslationManager;
+
+  /**
+   * The bundle info service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeBundleInfo
+   */
+  protected $bundleInfo;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->contentTranslationManager = $this->container->get('content_translation.manager');
+    $this->bundleInfo = $this->container->get('entity_type.bundle.info');
+
+    $this->installEntitySchema('entity_test_mul');
+  }
+
+  /**
+   * Tests that modules can know whether bundles are translatable.
+   */
+  public function testHookInvocationOrder() {
+    $this->contentTranslationManager->setEnabled('entity_test_mul', 'entity_test_mul', TRUE);
+    $this->bundleInfo->clearCachedBundles();
+    $this->bundleInfo->getAllBundleInfo();
+
+    // Verify that the test module comes first in the module list, which would
+    // normally make its hook implementation to be invoked first.
+    /** @var \Drupal\Core\Extension\ModuleHandlerInterface $module_handler */
+    $module_handler = $this->container->get('module_handler');
+    $module_list = $module_handler->getModuleList();
+    $expected_modules = [
+      'content_translation_test',
+      'content_translation',
+    ];
+    $actual_modules = array_keys(array_intersect_key($module_list, array_flip($expected_modules)));
+    $this->assertEquals($expected_modules, $actual_modules);
+
+    // Check that the "content_translation_test" hook implementation has access
+    // to the "translatable" bundle info property.
+    /** @var \Drupal\Core\State\StateInterface $state */
+    $state = $this->container->get('state');
+    $this->assertTrue($state->get('content_translation_test.translatable'));
+  }
+
+}
