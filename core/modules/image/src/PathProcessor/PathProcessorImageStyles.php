bfa44123aa1d82aec105d409302deb56bae10c74
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-01-29 19:31:34 +0000

    Issue #2797639 by jzavrl, slashrsm, yobottehg, tassilogroeper, Yogesh Pawar, Berdir: For multi language private files FileDownloadController is used for the ImageStyle instead of ImageStyleDownloadController
    (cherry picked from commit 1bc9459a8e45a36115f2ad3c5b8df01874fa25d3)

--- core/modules/image/src/PathProcessor/PathProcessorImageStyles.php
+++ core/modules/image/src/PathProcessor/PathProcessorImageStyles.php
@@ -48,8 +48,11 @@ class PathProcessorImageStyles implements InboundPathProcessorInterface {
     if (strpos($path, '/' . $directory_path . '/styles/') === 0) {
       $path_prefix = '/' . $directory_path . '/styles/';
     }
-    elseif (strpos($path, '/system/files/styles/') === 0) {
+    // Check if the string '/system/files/styles/' exists inside the path,
+    // that means we have a case of private file's image style.
+    elseif (strpos($path, '/system/files/styles/') !== FALSE) {
       $path_prefix = '/system/files/styles/';
+      $path = substr($path, strpos($path, $path_prefix), strlen($path));
     }
     else {
       return $path;
