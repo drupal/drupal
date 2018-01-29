5e69700e0a55a19a31336fd4a850aac458898191
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-01-29 13:18:59 +0000

    Issue #2915398 by Sam152, amateescu, timmillwood, plach, mstef: The moderation_state field is not computed during the creation of a new entity translation
    (cherry picked from commit 56247da6c2220d008351b6a9c4f61e14af90d271)

--- core/modules/content_moderation/src/Plugin/Field/ModerationStateFieldItemList.php
+++ core/modules/content_moderation/src/Plugin/Field/ModerationStateFieldItemList.php
@@ -5,6 +5,7 @@ namespace Drupal\content_moderation\Plugin\Field;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityPublishedInterface;
 use Drupal\Core\Field\FieldItemList;
+use Drupal\Core\TypedData\ComputedItemListTrait;
 
 /**
  * A computed field that provides a content entity's moderation state.
@@ -13,6 +14,38 @@ use Drupal\Core\Field\FieldItemList;
  * moderation state content entity.
  */
 class ModerationStateFieldItemList extends FieldItemList {
+
+  use ComputedItemListTrait {
+    ensureComputedValue as traitEnsureComputedValue;
+    get as traitGet;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function computeValue() {
+    $moderation_state = $this->getModerationStateId();
+    // Do not store NULL values, in the case where an entity does not have a
+    // moderation workflow associated with it, we do not create list items for
+    // the computed field.
+    if ($moderation_state) {
+      // An entity can only have a single moderation state.
+      $this->list[0] = $this->createItem(0, $moderation_state);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function ensureComputedValue() {
+    // If the moderation state field is set to an empty value, always recompute
+    // the state. Empty is not a valid moderation state value, when none is
+    // present the default state is used.
+    if (!isset($this->list[0]) || $this->list[0]->isEmpty()) {
+      $this->valueComputed = FALSE;
+    }
+    $this->traitEnsureComputedValue();
+  }
 
   /**
    * Gets the moderation state ID linked to a content entity revision.
@@ -90,32 +123,7 @@ class ModerationStateFieldItemList extends FieldItemList {
     if ($index !== 0) {
       throw new \InvalidArgumentException('An entity can not have multiple moderation states at the same time.');
     }
-    $this->computeModerationFieldItemList();
-    return isset($this->list[$index]) ? $this->list[$index] : NULL;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getIterator() {
-    $this->computeModerationFieldItemList();
-    return parent::getIterator();
-  }
-
-  /**
-   * Recalculate the moderation field item list.
-   */
-  protected function computeModerationFieldItemList() {
-    // Compute the value of the moderation state.
-    $index = 0;
-    if (!isset($this->list[$index]) || $this->list[$index]->isEmpty()) {
-
-      $moderation_state = $this->getModerationStateId();
-      // Do not store NULL values in the static cache.
-      if ($moderation_state) {
-        $this->list[$index] = $this->createItem($index, $moderation_state);
-      }
-    }
+    return $this->traitGet($index);
   }
 
   /**
@@ -134,6 +142,7 @@ class ModerationStateFieldItemList extends FieldItemList {
     parent::setValue($values, $notify);
 
     if (isset($this->list[0])) {
+      $this->valueComputed = TRUE;
       $this->updateModeratedEntity($this->list[0]->value);
     }
   }
