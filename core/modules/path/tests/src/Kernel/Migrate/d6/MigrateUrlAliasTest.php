bf47414a25e8d8f1fb2b5101a2c18c6829d99627
Alex Pott <alex.a.pott@googlemail.com>
2018-02-06 09:11:00 +0000

    Issue #2927227 by Jo Fitzgerald, Olarin, ao2, quietone, heddn: d6_url_alias migration fails when the src path in D6 contains no slashes
    (cherry picked from commit 17a47b8e9393599d5bd5fc6c3b3dc55a57a42cb3)

--- core/modules/path/tests/src/Kernel/Migrate/d6/MigrateUrlAliasTest.php
+++ core/modules/path/tests/src/Kernel/Migrate/d6/MigrateUrlAliasTest.php
@@ -103,6 +103,14 @@ class MigrateUrlAliasTest extends MigrateDrupal6TestBase {
     ];
     $path = \Drupal::service('path.alias_storage')->load($conditions);
     $this->assertPath('3', $conditions, $path);
+
+    $path = \Drupal::service('path.alias_storage')->load(['alias' => '/source-noslash']);
+    $conditions = [
+      'source' => '/admin',
+      'alias' => '/source-noslash',
+      'langcode' => 'und',
+    ];
+    $this->assertPath('2', $conditions, $path);
   }
 
   /**
