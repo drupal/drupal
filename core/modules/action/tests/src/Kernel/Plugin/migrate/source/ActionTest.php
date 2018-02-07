ef5b216cf1750a8ace5be36071d008718b82713b
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-02-07 15:23:02 +0000

    Issue #2935951 by heddn, maxocub, quietone, Jo Fitzgerald, phenaproxima, alexpott, larowlan: Copy migrate source plugin from migrate_drupal_d8 into migrate_drupal

--- core/modules/action/tests/src/Kernel/Plugin/migrate/source/ActionTest.php
+++ core/modules/action/tests/src/Kernel/Plugin/migrate/source/ActionTest.php
@@ -15,7 +15,7 @@ class ActionTest extends MigrateSqlSourceTestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = ['action', 'migrate_drupal'];
+  public static $modules = ['action', 'migrate_drupal', 'system'];
 
   /**
    * {@inheritdoc}
