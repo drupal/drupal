5d1a3206249c739e92e4ea8ab96022372d2ae9c5
Alex Pott <alex.a.pott@googlemail.com>
2018-03-01 10:14:13 +0000

    Issue #565220 by voleger, Yogesh Pawar, gpk, alexpott: Fix Weight form element behavior
    (cherry picked from commit cf94492ee8673f2d0f62b760130ac8f1c623ae00)

---
+++ core/tests/Drupal/Tests/Core/Render/Element/WeightTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\Tests\Core\Render\Element;
+
+use Drupal\Core\Form\FormState;
+use Drupal\Core\Render\Element\Weight;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * @coversDefaultClass \Drupal\Core\Render\Element\Weight
+ * @group Render
+ */
+class WeightTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['system'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installConfig(['system']);
+  }
+
+  /**
+   * Test existing #default_value value in #options list.
+   *
+   * @covers ::processWeight
+   */
+  public function testProcessWeight() {
+    $element = [];
+    $form_state = new FormState();
+    $complete_form = [];
+
+    $element_object = new Weight([], 'weight', []);
+    $info = $element_object->getInfo();
+    $element += $info;
+
+    $element['#default_value'] = $element['#delta'] + 5;
+
+    Weight::processWeight($element, $form_state, $complete_form);
+
+    $this->assertTrue(
+      isset($element['#options'][$element['#default_value']]),
+      'Default value exists in the #options list'
+    );
+  }
+
+}
