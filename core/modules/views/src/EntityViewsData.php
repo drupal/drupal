7d7e37b0bc8bc46b48974530e8ec57cb78efc4e2
Lee Rowlands <lee.rowlands@previousnext.com.au>
2018-03-09 07:51:11 +1000

    Issue #2914287 by Sam152, Manuel Garcia: Entity links aren't added to views using the revision table as a base
    (cherry picked from commit 4bd5cab3f40d29c6fc6cfd43613d4cb9c68128fe)

--- core/modules/views/src/EntityViewsData.php
+++ core/modules/views/src/EntityViewsData.php
@@ -253,6 +253,9 @@ class EntityViewsData implements EntityHandlerInterface, EntityViewsDataInterfac
     }
 
     $this->addEntityLinks($data[$base_table]);
+    if ($views_revision_base_table) {
+      $this->addEntityLinks($data[$views_revision_base_table]);
+    }
 
     // Load all typed data definitions of all fields. This should cover each of
     // the entity base, revision, data tables.
