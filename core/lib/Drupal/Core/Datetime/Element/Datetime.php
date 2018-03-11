3d2d2a18a5c069c870731e3432f40fa85509ba07
Lee Rowlands <lee.rowlands@previousnext.com.au>
2018-03-12 07:34:19 +1000

    Issue #2941646 by mr.baileys: Documentation on datetime #date_increment is incorrect/misleading

--- core/lib/Drupal/Core/Datetime/Element/Datetime.php
+++ core/lib/Drupal/Core/Datetime/Element/Datetime.php
@@ -181,9 +181,13 @@ class Datetime extends DateElementBase {
    *     dynamic value that is that many years earlier or later than the current
    *     year at the time the form is displayed. Used in jQueryUI datepicker year
    *     range and HTML5 min/max date settings. Defaults to '1900:2050'.
-   *   - #date_increment: The increment to use for minutes and seconds, i.e.
-   *    '15' would show only :00, :15, :30 and :45. Used for HTML5 step values and
-   *     jQueryUI datepicker settings. Defaults to 1 to show every minute.
+   *   - #date_increment: The interval (step) to use when incrementing or
+   *     decrementing time, in seconds. For example, if this value is set to 30,
+   *     time increases (or decreases) in steps of 30 seconds (00:00:00,
+   *     00:00:30, 00:01:00, and so on.) If this value is a multiple of 60, the
+   *     "seconds"-component will not be shown in the input. Used for HTML5 step
+   *     values and jQueryUI datepicker settings. Defaults to 1 to show every
+   *     second.
    *   - #date_timezone: The local timezone to use when creating dates. Generally
    *     this should be left empty and it will be set correctly for the user using
    *     the form. Useful if the default value is empty to designate a desired
