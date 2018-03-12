12a44fc46eb68c9d6d03131c2cda3c2100a027cb
Alex Pott <alex.a.pott@googlemail.com>
2018-03-12 12:52:41 +0000

    Revert "-m"
    This reverts commit f9ca1c02be27d6c32beec7e4869ee31f326fc4e8.

--- core/modules/views/src/Plugin/views/display/EntityReference.php
+++ core/modules/views/src/Plugin/views/display/EntityReference.php
@@ -54,6 +54,9 @@ class EntityReference extends DisplayPluginBase {
     $options['row']['contains']['type'] = ['default' => 'entity_reference'];
     $options['defaults']['default']['row'] = FALSE;
 
+    // Make sure the query is not cached.
+    $options['defaults']['default']['cache'] = FALSE;
+
     // Set the display title to an empty string (not used in this display type).
     $options['title']['default'] = '';
     $options['defaults']['default']['title'] = FALSE;
@@ -62,12 +65,13 @@ class EntityReference extends DisplayPluginBase {
   }
 
   /**
-   * {@inheritdoc}
+   * Overrides \Drupal\views\Plugin\views\display\DisplayPluginBase::optionsSummary().
+   *
+   * Disable 'cache' and 'title' so it won't be changed.
    */
   public function optionsSummary(&$categories, &$options) {
     parent::optionsSummary($categories, $options);
-    // Disable 'title' so it won't be changed from the default set in
-    // \Drupal\views\Plugin\views\display\EntityReference::defineOptions.
+    unset($options['query']);
     unset($options['title']);
   }
 
