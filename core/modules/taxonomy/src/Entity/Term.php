2d0c3eb4753620d2ce25cb897655c819b77800ab
Alex Pott <alex.a.pott@googlemail.com>
2018-03-14 22:17:56 +0000

    Issue #2843060 by amateescu: Term field "parent" could not be handled properly by ContentEntityBase::hasTranslationChanges

--- core/modules/taxonomy/src/Entity/Term.php
+++ core/modules/taxonomy/src/Entity/Term.php
@@ -237,17 +237,4 @@ class Term extends ContentEntityBase implements TermInterface {
     return $this->bundle();
   }
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function getFieldsToSkipFromTranslationChangesCheck() {
-    // @todo the current implementation of the parent field makes it impossible
-    // for ::hasTranslationChanges() to correctly check the field for changes,
-    // so it is currently skipped from the comparision and has to be fixed by
-    // https://www.drupal.org/node/2843060.
-    $fields = parent::getFieldsToSkipFromTranslationChangesCheck();
-    $fields[] = 'parent';
-    return $fields;
-  }
-
 }
