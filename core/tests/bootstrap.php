ce1447d73d3fc3a771ffd0075f00d488b61dfcfa
Alex Pott <alex.a.pott@googlemail.com>
2018-03-14 23:16:46 +0000

    Issue #2687711 by chriscohen, anavarre: Drupal PHPUnit tests currently NEED the profiles directory to exist

--- core/tests/bootstrap.php
+++ core/tests/bootstrap.php
@@ -66,7 +66,7 @@ function drupal_phpunit_contrib_extension_directory_roots($root = NULL) {
     $paths[] = is_dir("$path/profiles") ? realpath("$path/profiles") : NULL;
     $paths[] = is_dir("$path/themes") ? realpath("$path/themes") : NULL;
   }
-  return array_filter($paths);
+  return array_filter($paths, 'file_exists');
 }
 
 /**
