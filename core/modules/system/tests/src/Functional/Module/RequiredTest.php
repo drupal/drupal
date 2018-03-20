0c2507efdb3cb7c989c90b2031a3fe38a4c67ce5
Alex Pott <alex.a.pott@googlemail.com>
2018-03-20 15:17:14 +0000

    Revert "Issue #2949834 by alexpott: RequiredTest is not testing what it thinks it is testing"
    This reverts commit e6040e58a0fbd21c5227765a149a6622d6fe1e49.

---
+++ core/modules/system/tests/src/Functional/Module/RequiredTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\Tests\system\Functional\Module;
+
+/**
+ * Attempt disabling of required modules.
+ *
+ * @group Module
+ */
+class RequiredTest extends ModuleTestBase {
+  /**
+   * Assert that core required modules cannot be disabled.
+   */
+  public function testDisableRequired() {
+    $module_info = system_get_info('module');
+    $this->drupalGet('admin/modules');
+    foreach ($module_info as $module => $info) {
+      // Check to make sure the checkbox for each required module is disabled
+      // and checked (or absent from the page if the module is also hidden).
+      if (!empty($info['required'])) {
+        $field_name = 'modules[' . $module . '][enable]';
+        if (empty($info['hidden'])) {
+          $this->assertFieldByXPath("//input[@name='$field_name' and @disabled='disabled' and @checked='checked']", '', format_string('Field @name was disabled and checked.', ['@name' => $field_name]));
+        }
+        else {
+          $this->assertNoFieldByName($field_name);
+        }
+      }
+    }
+  }
+
+}
