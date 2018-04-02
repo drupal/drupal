e3f39f98ccf41bec53322f07e83fd98d39c14f32
Alex Pott <alex.a.pott@googlemail.com>
2018-04-02 15:56:56 +0200

    Issue #2952962 by tim.plunkett, sureshcj, JordiK, Madis, cilefen: Block visibility setting tab for Roles not showing
    (cherry picked from commit 5d5af494fb229518228eb9317f0ff4b5808ea34b)

---
+++ core/tests/Drupal/KernelTests/Core/Plugin/ContextDefinitionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\KernelTests\Core\Plugin;
+
+use Drupal\Core\Plugin\Context\Context;
+use Drupal\Core\Plugin\Context\ContextDefinition;
+use Drupal\entity_test\Entity\EntityTest;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * @coversDefaultClass \Drupal\Core\Plugin\Context\ContextDefinition
+ * @group Plugin
+ */
+class ContextDefinitionTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['entity_test', 'user'];
+
+  /**
+   * @covers ::isSatisfiedBy
+   */
+  public function testIsSatisfiedBy() {
+    $this->installEntitySchema('user');
+
+    $value = EntityTest::create([]);
+    // Assert that the entity has at least one violation.
+    $this->assertNotEmpty($value->validate());
+    // Assert that these violations do not prevent it from satisfying the
+    // requirements of another object.
+    $requirement = new ContextDefinition('any');
+    $context = new Context(new ContextDefinition('entity:entity_test'), $value);
+    $this->assertTrue($requirement->isSatisfiedBy($context));
+  }
+
+}
