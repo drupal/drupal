9d60012a99df4ff312e3112a47d189bc7d92c4f2
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-04-06 00:17:59 +0100

    Issue #2939491 by hchonov, slasher13, alexpott, dawehner: Prevent memory leak and inconsistent entity references when serializing entity storages

--- core/lib/Drupal/Core/Entity/EntityStorageBase.php
+++ core/lib/Drupal/Core/Entity/EntityStorageBase.php
@@ -541,4 +541,16 @@ abstract class EntityStorageBase extends EntityHandlerBase implements EntityStor
    */
   abstract protected function getQueryServiceName();
 
+  /**
+   * {@inheritdoc}
+   */
+  public function __sleep() {
+    // In case the storage is being serialized then we prevent from serializing
+    // the static cache of entities together with it, as this could lead to a
+    // memory leak.
+    $vars = parent::__sleep();
+    unset($vars['entities']);
+    return $vars;
+  }
+
 }
