25d969081176fbc90ad3675d6d054d6da22286a5
Alex Pott <alex.a.pott@googlemail.com>
2018-04-11 20:48:46 +0100

    Issue #2959316 by dww, Lendude, alexpott, dawehner, xjm: Views argument validators that modify argument values aren't reflected in token replacement

--- core/modules/views/tests/src/Kernel/ViewExecutableTest.php
+++ core/modules/views/tests/src/Kernel/ViewExecutableTest.php
@@ -38,7 +38,7 @@ class ViewExecutableTest extends ViewsKernelTestBase {
    *
    * @var array
    */
-  public static $testViews = ['test_destroy', 'test_executable_displays'];
+  public static $testViews = ['test_destroy', 'test_executable_displays', 'test_argument_dependency'];
 
   /**
    * Properties that should be stored in the configuration.
@@ -523,6 +523,21 @@ class ViewExecutableTest extends ViewsKernelTestBase {
       ->getItemDefinition()
       ->getFieldDefinition();
     $this->assertEquals($nid_definition_before->getPropertyDefinitions(), $nid_definition_after->getPropertyDefinitions());
+  }
+
+  /**
+   * Tests if argument overrides by validators are propagated to tokens.
+   */
+  public function testArgumentValidatorValueOverride() {
+    $view = Views::getView('test_argument_dependency');
+    $view->setDisplay('page_1');
+    $view->setArguments(['1', 'this value should be replaced']);
+    $view->execute();
+    $expected = [
+      '{{ arguments.uid }}' => '1',
+      '{{ raw_arguments.uid }}' => '1',
+    ];
+    $this->assertEquals($expected, $view->build_info['substitutions']);
   }
 
 }
