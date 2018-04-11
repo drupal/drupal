37b9e45ac06b5d2e90af60a931cd0fc548756d1b
Alex Pott <alex.a.pott@googlemail.com>
2018-04-11 22:05:27 +0100

    Issue #2956814 by tim.plunkett: FileValidationConstraintValidator assumes that the file exists

--- core/modules/file/tests/src/Kernel/FileItemValidationTest.php
+++ core/modules/file/tests/src/Kernel/FileItemValidationTest.php
@@ -104,6 +104,18 @@ class FileItemValidationTest extends KernelTestBase {
     $this->assertEquals('The file is <em class="placeholder">2.93 KB</em> exceeding the maximum file size of <em class="placeholder">2 KB</em>.', (string) $result->get(0)->getMessage());
     $this->assertEquals('field_test_file.0', $result->get(1)->getPropertyPath());
     $this->assertEquals('Only files with the following extensions are allowed: <em class="placeholder">jpg|png</em>.', (string) $result->get(1)->getMessage());
+
+    // Refer to a file that does not exist.
+    $entity_test = EntityTest::create([
+      'uid' => $this->user->id(),
+      'field_test_file' => [
+        'target_id' => 2,
+      ],
+    ]);
+    $result = $entity_test->validate();
+    $this->assertCount(1, $result);
+    $this->assertEquals('field_test_file.0.target_id', $result->get(0)->getPropertyPath());
+    $this->assertEquals('The referenced entity (<em class="placeholder">file</em>: <em class="placeholder">2</em>) does not exist.', (string) $result->get(0)->getMessage());
   }
 
   /**
