fb4c903430d6be30da57a3dd9045362705d96286
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-04-30 12:12:35 +0100

    Issue #2955803 by zviryatko, andypost: Do not attach history for non-node entities in CommentViewBuilder::buildComponents()

---
+++ core/modules/comment/tests/src/Functional/CommentEntityTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\Tests\comment\Functional;
+
+use Drupal\comment\Entity\CommentType;
+use Drupal\comment\Plugin\Field\FieldType\CommentItemInterface;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\comment\CommentInterface;
+use Drupal\Tests\taxonomy\Functional\TaxonomyTestTrait;
+use Drupal\comment\Entity\Comment;
+
+/**
+ * Tests comments with other entities.
+ *
+ * @group comment
+ */
+class CommentEntityTest extends CommentTestBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  public static $modules = ['block', 'comment', 'node', 'history', 'field_ui', 'datetime', 'taxonomy'];
+
+  use TaxonomyTestTrait;
+
+  protected $vocab;
+  protected $commentType;
+
+  protected function setUp() {
+    parent::setUp();
+
+    $this->vocab = $this->createVocabulary();
+    $this->commentType = CommentType::create([
+      'id' => 'taxonomy_comment',
+      'label' => 'Taxonomy comment',
+      'description' => '',
+      'target_entity_type_id' => 'taxonomy_term',
+    ]);
+    $this->commentType->save();
+    $this->addDefaultCommentField(
+      'taxonomy_term',
+      $this->vocab->id(),
+      'field_comment',
+      CommentItemInterface::OPEN,
+      $this->commentType->id()
+    );
+  }
+
+  /**
+   * Tests CSS classes on comments.
+   */
+  public function testEntityChanges() {
+    $this->drupalLogin($this->webUser);
+    // Create a new node.
+    $term = $this->createTerm($this->vocab, ['uid' => $this->webUser->id()]);
+
+    // Add a comment.
+    /** @var \Drupal\comment\CommentInterface $comment */
+    $comment = Comment::create([
+      'entity_id' => $term->id(),
+      'entity_type' => 'taxonomy_term',
+      'field_name' => 'field_comment',
+      'uid' => $this->webUser->id(),
+      'status' => CommentInterface::PUBLISHED,
+      'subject' => $this->randomMachineName(),
+      'language' => LanguageInterface::LANGCODE_NOT_SPECIFIED,
+      'comment_body' => [LanguageInterface::LANGCODE_NOT_SPECIFIED => [$this->randomMachineName()]],
+    ]);
+    $comment->save();
+
+    // Request the node with the comment.
+    $this->drupalGet('taxonomy/term/' . $term->id());
+    $settings = $this->getDrupalSettings();
+    $this->assertFalse(isset($settings['ajaxPageState']['libraries']) && in_array('comment/drupal.comment-new-indicator', explode(',', $settings['ajaxPageState']['libraries'])), 'drupal.comment-new-indicator library is present.');
+    $this->assertFalse(isset($settings['history']['lastReadTimestamps']) && in_array($term->id(), array_keys($settings['history']['lastReadTimestamps'])), 'history.lastReadTimestamps is present.');
+  }
+
+}
