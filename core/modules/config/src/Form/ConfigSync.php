1eae4dc7dce7da6bc60c506487197a5d9f428117
Alex Pott <alex.a.pott@googlemail.com>
2018-04-30 16:41:49 +0100

    Issue #2950709 by idebr, Grimreaper: ConfigSync.php tries to include non-existing file config.admin.inc

--- core/modules/config/src/Form/ConfigSync.php
+++ core/modules/config/src/Form/ConfigSync.php
@@ -342,7 +342,6 @@ class ConfigSync extends FormBase {
           'init_message' => t('Starting configuration synchronization.'),
           'progress_message' => t('Completed step @current of @total.'),
           'error_message' => t('Configuration synchronization has encountered an error.'),
-          'file' => __DIR__ . '/../../config.admin.inc',
         ];
         foreach ($sync_steps as $sync_step) {
           $batch['operations'][] = [[get_class($this), 'processBatch'], [$config_importer, $sync_step]];
