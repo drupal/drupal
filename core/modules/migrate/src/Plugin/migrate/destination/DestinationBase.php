b2247f0e7e639f14ffdb1f9fe0f6f60b975ff685
Alex Pott <alex.a.pott@googlemail.com>
2018-05-11 10:58:43 +0100

    Issue #2971959 by juampynr: Typo at DestinationBase.php
    (cherry picked from commit ba42166da282f6c727d15869d0b3702dd309fc91)

--- core/modules/migrate/src/Plugin/migrate/destination/DestinationBase.php
+++ core/modules/migrate/src/Plugin/migrate/destination/DestinationBase.php
@@ -12,7 +12,7 @@ use Drupal\migrate\Plugin\RequirementsInterface;
 /**
  * Base class for migrate destination classes.
  *
- * Migrate destination plugins perfom the import operation of the migration.
+ * Migrate destination plugins perform the import operation of the migration.
  * Destination plugins extend this abstract base class. A destination plugin
  * must implement at least fields(), getIds() and import() methods. Destination
  * plugins can also support rollback operations. For more
