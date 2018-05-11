5972c776e6445d514b23dcbb6b281bd55df61488
Alex Pott <alex.a.pott@googlemail.com>
2018-05-11 10:16:02 +0100

    Issue #2832083 by rbayliss, pounard, alexpott, dawehner: ThemeRegistryLoader::findTemplate() breaks Twig_Loader_Filesystem signature

--- core/lib/Drupal/Core/Template/Loader/ThemeRegistryLoader.php
+++ core/lib/Drupal/Core/Template/Loader/ThemeRegistryLoader.php
@@ -36,8 +36,8 @@ class ThemeRegistryLoader extends \Twig_Loader_Filesystem {
    * @param bool $throw
    *   Whether to throw an exception when an error occurs.
    *
-   * @return string
-   *   The path to the template.
+   * @return string|false
+   *   The path to the template, or false if the template is not found.
    *
    * @throws \Twig_Error_Loader
    *   Thrown if a template matching $name cannot be found.
@@ -63,6 +63,8 @@ class ThemeRegistryLoader extends \Twig_Loader_Filesystem {
     if ($throw) {
       throw new \Twig_Error_Loader(sprintf('Unable to find template "%s" in the Drupal theme registry.', $name));
     }
+
+    return FALSE;
   }
 
 }
