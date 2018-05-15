6609757cc5e2d74b3e8ca8906ea4a90c82494448
Alex Pott <alex.a.pott@googlemail.com>
2018-05-15 21:28:08 +0100

    Issue #2939336 by dsdeiz, quietone: D6 User Profile Source Fields
    (cherry picked from commit 729b6308bc70de73d65eb93e61767e89b36ac888)

--- core/modules/user/src/Plugin/migrate/source/d6/User.php
+++ core/modules/user/src/Plugin/migrate/source/d6/User.php
@@ -33,14 +33,6 @@ class User extends DrupalSqlBase {
     // Add roles field.
     $fields['roles'] = $this->t('Roles');
 
-    // Profile fields.
-    if ($this->moduleExists('profile')) {
-      $fields += $this->select('profile_fields', 'pf')
-        ->fields('pf', ['name', 'title'])
-        ->execute()
-        ->fetchAllKeyed();
-    }
-
     return $fields;
   }
 
