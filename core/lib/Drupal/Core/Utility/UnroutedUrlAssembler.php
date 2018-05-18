a7fdfb64693d00bd7e094ccda45c3e58581b8bbe
Alex Pott <alex.a.pott@googlemail.com>
2018-05-18 11:13:12 +0100

    Issue #2955685 by Wim Leers, dawehner, mpdonadio, alexpott, borisson_: Unrouted URLs cannot have have overridden query or fragments

--- core/lib/Drupal/Core/Utility/UnroutedUrlAssembler.php
+++ core/lib/Drupal/Core/Utility/UnroutedUrlAssembler.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Core\Utility;
 
+use Drupal\Component\Utility\NestedArray;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\GeneratedUrl;
 use Drupal\Core\PathProcessor\OutboundPathProcessorInterface;
@@ -69,13 +70,18 @@ class UnroutedUrlAssembler implements UnroutedUrlAssemblerInterface {
    */
   protected function buildExternalUrl($uri, array $options = [], $collect_bubbleable_metadata = FALSE) {
     $this->addOptionDefaults($options);
-    // Split off the fragment.
-    if (strpos($uri, '#') !== FALSE) {
-      list($uri, $old_fragment) = explode('#', $uri, 2);
-      // If $options contains no fragment, take it over from the path.
-      if (isset($old_fragment) && !$options['fragment']) {
-        $options['fragment'] = '#' . $old_fragment;
-      }
+    // Split off the query & fragment.
+    $parsed = UrlHelper::parse($uri);
+    $uri = $parsed['path'];
+
+    $parsed += ['query' => []];
+    $options += ['query' => []];
+
+    $options['query'] = NestedArray::mergeDeep($parsed['query'], $options['query']);
+    ksort($options['query']);
+
+    if ($parsed['fragment'] && !$options['fragment']) {
+      $options['fragment'] = '#' . $parsed['fragment'];
     }
 
     if (isset($options['https'])) {
@@ -88,7 +94,7 @@ class UnroutedUrlAssembler implements UnroutedUrlAssemblerInterface {
     }
     // Append the query.
     if ($options['query']) {
-      $uri .= (strpos($uri, '?') !== FALSE ? '&' : '?') . UrlHelper::buildQuery($options['query']);
+      $uri .= '?' . UrlHelper::buildQuery($options['query']);
     }
     // Reassemble.
     $url = $uri . $options['fragment'];
