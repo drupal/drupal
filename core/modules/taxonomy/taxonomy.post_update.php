bc2d4bfeb62a876af4511cd1c2ffc273f4831345
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-06-28 22:55:49 +0100

    Issue #2977882 by Wim Leers, jibran, catch: Term entity type's "parent" field does not specify "target_bundles" setting: a term from *any* vocabulary can be chosen

--- core/modules/taxonomy/taxonomy.post_update.php
+++ core/modules/taxonomy/taxonomy.post_update.php
@@ -11,3 +11,10 @@
 function taxonomy_post_update_clear_views_data_cache() {
   // An empty update will flush caches.
 }
+
+/**
+ * Clear entity_bundle_field_definitions cache for new parent field settings.
+ */
+function taxonomy_post_update_clear_entity_bundle_field_definitions_cache() {
+  // An empty update will flush caches.
+}
