19100d948a4d3fa4c2563822b2149d4277ca7412
Alex Pott <alex.a.pott@googlemail.com>
2018-06-28 22:14:15 +0100

    Issue #2860760 by Jo Fitzgerald, heddn, quietone, alexpott: Match setup() functionality of MigrateFileTest with MigratePrivateFileTest
    (cherry picked from commit 4784ac3d0f65b9e18ea69948c9b2428b10c644ad)

--- core/modules/file/tests/src/Kernel/Migrate/d7/MigrateFileTest.php
+++ core/modules/file/tests/src/Kernel/Migrate/d7/MigrateFileTest.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\Tests\file\Kernel\Migrate\d7;
 
-use Drupal\file\Entity\File;
-use Drupal\file\FileInterface;
 use Drupal\Tests\migrate_drupal\Kernel\d7\MigrateDrupal7TestBase;
 
 /**
@@ -15,6 +13,9 @@ class MigrateFileTest extends MigrateDrupal7TestBase {
 
   use FileMigrationSetupTrait;
 
+  /**
+   * {@inheritdoc}
+   */
   public static $modules = ['file'];
 
   /**
@@ -22,44 +23,19 @@ class MigrateFileTest extends MigrateDrupal7TestBase {
    */
   protected function setUp() {
     parent::setUp();
-
     $this->fileMigrationSetup();
   }
 
   /**
-   * Tests a single file entity.
-   *
-   * @param int $id
-   *   The file ID.
-   * @param string $name
-   *   The expected file name.
-   * @param string $uri
-   *   The expected URI.
-   * @param string $mime
-   *   The expected MIME type.
-   * @param int $size
-   *   The expected file size.
-   * @param int $created
-   *   The expected creation time.
-   * @param int $changed
-   *   The expected modification time.
-   * @param int $uid
-   *   The expected owner ID.
+   * {@inheritdoc}
    */
-  protected function assertEntity($id, $name, $uri, $mime, $size, $created, $changed, $uid) {
-    /** @var \Drupal\file\FileInterface $file */
-    $file = File::load($id);
-    $this->assertTrue($file instanceof FileInterface);
-    $this->assertIdentical($name, $file->getFilename());
-    $this->assertIdentical($uri, $file->getFileUri());
-    $this->assertTrue(file_exists($uri));
-    $this->assertIdentical($mime, $file->getMimeType());
-    $this->assertIdentical($size, $file->getSize());
-    // isPermanent(), isTemporary(), etc. are determined by the status column.
-    $this->assertTrue($file->isPermanent());
-    $this->assertIdentical($created, $file->getCreatedTime());
-    $this->assertIdentical($changed, $file->getChangedTime());
-    $this->assertIdentical($uid, $file->getOwnerId());
+  protected function getFileMigrationInfo() {
+    return [
+      'path' => 'public://sites/default/files/cube.jpeg',
+      'size' => '3620',
+      'base_path' => 'public://',
+      'plugin_id' => 'd7_file',
+    ];
   }
 
   /**
