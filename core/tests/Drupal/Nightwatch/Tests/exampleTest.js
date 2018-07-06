ca5abe8c6f82ca73286b3af430767643b607a24e
Alex Pott <alex.a.pott@googlemail.com>
2018-07-06 15:18:38 +0100

    Issue #2983382 by ApacheEx, drpal: JS codestyle: spaced-comment

--- core/tests/Drupal/Nightwatch/Tests/exampleTest.js
+++ core/tests/Drupal/Nightwatch/Tests/exampleTest.js
@@ -15,17 +15,4 @@ module.exports = {
       .assert.containsText('body', 'Test page text')
       .drupalLogAndEnd({ onlyOnError: false });
   },
-  /**
-  'Example failing test': (browser) => {
-    browser
-      // Change this to point at a site which has some console errors, as the
-      // test site that was just installed doesn't.
-      .url('https://www./')
-      .waitForElementVisible('h1', 1000)
-      // Wait for some errors to build up.
-      .pause(5000)
-      .assert.containsText('h1', 'I\'m the operator with my pocket calculator')
-      .drupalLogAndEnd();
-  },
-  **/
 };
