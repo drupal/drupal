f0372ebff9c056343bae628f835f0e340149208c
Alex Pott <alex.a.pott@googlemail.com>
2018-07-16 11:52:45 +0100

    Issue #2976816 by msankhala: CachePluginBase::postRender docblock not using @code for code example
    (cherry picked from commit 68bb91c208a3ef866b83ada6badb898fb8a0374c)

--- core/modules/views/src/Plugin/views/cache/CachePluginBase.php
+++ core/modules/views/src/Plugin/views/cache/CachePluginBase.php
@@ -163,19 +163,22 @@ abstract class CachePluginBase extends PluginBase {
   /**
    * Post process any rendered data.
    *
-   * This can be valuable to be able to cache a view and still have some level of
-   * dynamic output. In an ideal world, the actual output will include HTML
+   * This can be valuable to be able to cache a view and still have some level
+   * of dynamic output. In an ideal world, the actual output will include HTML
    * comment based tokens, and then the post process can replace those tokens.
    *
    * Example usage. If it is known that the view is a node view and that the
    * primary field will be a nid, you can do something like this:
-   *
-   * <!--post-FIELD-NID-->
+   * @code
+   *   <!--post-FIELD-NID-->
+   * @endcode
    *
    * And then in the post render, create an array with the text that should
    * go there:
    *
-   * strtr($output, array('<!--post-FIELD-1-->', 'output for FIELD of nid 1');
+   * @code
+   *   strtr($output, array('<!--post-FIELD-1-->', 'output for FIELD of nid 1');
+   * @endcode
    *
    * All of the cached result data will be available in $view->result, as well,
    * so all ids used in the query should be discoverable.
