5adaf0617685ca3ee360eca01d4dc30805e20088
Alex Pott <alex.a.pott@googlemail.com>
2018-07-21 17:29:32 +0100

    Issue #2985716 by maxocub, masipila: Cannot save language negotiation settings after upgrade
    (cherry picked from commit 37e09ab49c8c4bee1ac2451edc104f224d8a258b)

--- core/modules/language/src/Plugin/migrate/source/Language.php
+++ core/modules/language/src/Plugin/migrate/source/Language.php
@@ -66,7 +66,7 @@ class Language extends DrupalSqlBase {
       // when the 'language_negotiation' variable is set to '3', and in D7, when
       // the 'locale_language_negotiation_url_part' variable is set to '1'.
       if ($this->variableGet('language_negotiation', 0) == 3 || $this->variableGet('locale_language_negotiation_url_part', 0) == 1) {
-        $row->setSourceProperty('domain_negotiation', TRUE);
+        $row->setSourceProperty('domain_negotiation_used', TRUE);
       }
     }
 
