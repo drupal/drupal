2f08f6de5e0a62c0f8ffab3ed26a4247d92e414c
Alex Pott <alex.a.pott@googlemail.com>
2018-07-22 13:51:10 +0100

    Issue #2809517 by martin107, Lendude: Convert AJAX part of \Drupal\system\Tests\Ajax\AjaxFormCacheTest to WebDriverTestBase

--- core/tests/Drupal/FunctionalJavascriptTests/Ajax/AjaxFormCacheTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Ajax/AjaxFormCacheTest.php
@@ -1,17 +1,23 @@
 <?php
 
-namespace Drupal\system\Tests\Ajax;
+namespace Drupal\FunctionalJavascriptTests\Ajax;
 
 use Drupal\Core\EventSubscriber\MainContentViewSubscriber;
 use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Url;
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
 
 /**
  * Tests the usage of form caching for AJAX forms.
  *
  * @group Ajax
  */
-class AjaxFormCacheTest extends AjaxTestBase {
+class AjaxFormCacheTest extends WebDriverTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['ajax_test', 'ajax_forms_test'];
 
   /**
    * Tests the usage of form cache for AJAX forms.
@@ -47,11 +53,27 @@ class AjaxFormCacheTest extends AjaxTestBase {
     $this->drupalPlaceBlock('ajax_forms_test_block');
 
     $this->drupalGet('');
-    $this->drupalPostAjaxForm(NULL, ['test1' => 'option1'], 'test1');
-    $this->assertOptionSelectedWithDrupalSelector('edit-test1', 'option1');
-    $this->assertOptionWithDrupalSelector('edit-test1', 'option3');
-    $this->drupalPostForm(NULL, ['test1' => 'option1'], 'Submit');
-    $this->assertText('Submission successful.');
+    $session = $this->getSession();
+
+    // Select first option and trigger ajax update.
+    $session->getPage()->selectFieldOption('edit-test1', 'option1');
+
+    // DOM update: The InsertCommand in the AJAX response changes the text
+    // in the option element to 'Option1!!!'.
+    $opt1_selector = $this->assertSession()->waitForElement('css', "select[data-drupal-selector='edit-test1'] option:contains('Option 1!!!')");
+    $this->assertNotEmpty($opt1_selector);
+    $this->assertTrue($opt1_selector->isSelected());
+
+    // Confirm option 3 exists.
+    $page = $session->getPage();
+    $opt3_selector = $page->find('xpath', '//select[@data-drupal-selector="edit-test1"]//option[@value="option3"]');
+    $this->assertNotEmpty($opt3_selector);
+
+    // Confirm success message appears after a submit.
+    $page->findButton('edit-submit')->click();
+    $this->assertSession()->waitForButton('edit-submit');
+    $updated_page = $session->getPage();
+    $updated_page->hasContent('Submission successful.');
   }
 
   /**
@@ -65,7 +87,16 @@ class AjaxFormCacheTest extends AjaxTestBase {
 
     $url = Url::fromRoute('entity.user.canonical', ['user' => $this->rootUser->id()], ['query' => ['foo' => 'bar']]);
     $this->drupalGet($url);
-    $this->drupalPostAjaxForm(NULL, ['test1' => 'option1'], 'test1');
+
+    $session = $this->getSession();
+    // Select first option and trigger ajax update.
+    $session->getPage()->selectFieldOption('edit-test1', 'option1');
+
+    // DOM update: The InsertCommand in the AJAX response changes the text
+    // in the option element to 'Option1!!!'.
+    $opt1_selector = $this->assertSession()->waitForElement('css', "option:contains('Option 1!!!')");
+    $this->assertNotEmpty($opt1_selector);
+
     $url->setOption('query', [
       'foo' => 'bar',
       FormBuilderInterface::AJAX_FORM_REQUEST => 1,
