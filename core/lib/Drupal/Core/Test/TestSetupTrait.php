e3458a701a997b21e3005a47d44cadf8a7a1104b
Francesco Placella <plach@183211.no-reply.drupal.org>
2018-08-07 12:09:45 +0200

    Issue #2987124 by alexpott, mondrake: Functional tests don't use SIMPLETEST_DB environmental variable as expected
    (cherry picked from commit bacd6578d3fd5e7342f25b0a408382be16e152e4)

--- core/lib/Drupal/Core/Test/TestSetupTrait.php
+++ core/lib/Drupal/Core/Test/TestSetupTrait.php
@@ -158,6 +158,9 @@ trait TestSetupTrait {
     // If the test is run with argument dburl then use it.
     $db_url = getenv('SIMPLETEST_DB');
     if (!empty($db_url)) {
+      // Ensure no existing database gets in the way. If a default database
+      // exists already it must be removed.
+      Database::removeConnection('default');
       $database = Database::convertDbUrlToConnectionInfo($db_url, isset($this->root) ? $this->root : DRUPAL_ROOT);
       Database::addConnectionInfo('default', 'default', $database);
     }
