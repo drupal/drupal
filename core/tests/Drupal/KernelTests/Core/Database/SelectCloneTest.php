74ba0f68db092437a166db938cd0f2ef071296bb
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-09-17 11:37:28 +0100

    Issue #2142107 by drunken monkey, gilsbert, David_Rothstein, mondrake, Kristi Wachter: Complex cloned query dependent on __toString() call

--- core/tests/Drupal/KernelTests/Core/Database/SelectCloneTest.php
+++ core/tests/Drupal/KernelTests/Core/Database/SelectCloneTest.php
@@ -22,6 +22,10 @@ class SelectCloneTest extends DatabaseTestBase {
     $query->condition('id', $subquery, 'IN');
 
     $clone = clone $query;
+
+    // Cloned query should have a different unique identifier.
+    $this->assertNotEquals($query->uniqueIdentifier(), $clone->uniqueIdentifier());
+
     // Cloned query should not be altered by the following modification
     // happening on original query.
     $subquery->condition('age', 25, '>');
@@ -32,6 +36,33 @@ class SelectCloneTest extends DatabaseTestBase {
     // Make sure the cloned query has not been modified
     $this->assertEqual(3, $clone_result, 'The cloned query returns the expected number of rows');
     $this->assertEqual(2, $query_result, 'The query returns the expected number of rows');
+  }
+
+  /**
+   * Tests that nested SELECT queries are cloned properly.
+   */
+  public function testNestedQueryCloning() {
+    $sub_query = $this->connection->select('test', 't');
+    $sub_query->addField('t', 'id', 'id');
+    $sub_query->condition('age', 28, '<');
+
+    $query = $this->connection->select($sub_query, 't');
+
+    $clone = clone $query;
+
+    // Cloned query should have a different unique identifier.
+    $this->assertNotEquals($query->uniqueIdentifier(), $clone->uniqueIdentifier());
+
+    // Cloned query should not be altered by the following modification
+    // happening on original query.
+    $sub_query->condition('age', 25, '>');
+
+    $clone_result = $clone->countQuery()->execute()->fetchField();
+    $query_result = $query->countQuery()->execute()->fetchField();
+
+    // Make sure the cloned query has not been modified.
+    $this->assertEquals(3, $clone_result, 'The cloned query returns the expected number of rows');
+    $this->assertEquals(2, $query_result, 'The query returns the expected number of rows');
   }
 
 }
