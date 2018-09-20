170c6a60f253a5cad29c9d83e862c7c84e990266
Alex Pott <alex.a.pott@googlemail.com>
2018-09-20 13:16:43 +0100

    Issue #2954776 by neclimdul, tim.plunkett, zenimagine, tedbow: Can not uninstall Field Layout while Layout Builder is installed
    (cherry picked from commit 68f9fd50e86339fda754386248d9f1378de64d6e)

---
+++ core/modules/field_layout/tests/src/Kernel/FieldLayoutUninstallTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\Tests\field_layout\Kernel;
+
+use Drupal\Tests\layout_builder\Kernel\LayoutBuilderCompatibilityTestBase;
+
+/**
+ * @group field_layout
+ */
+class FieldLayoutUninstallTest extends LayoutBuilderCompatibilityTestBase {
+
+  /**
+   * Ensures field layout can be uninstalled with layout builder enabled.
+   */
+  public function testFieldLayoutUninstall() {
+    // Setup user schema so user hook uninstall hook doesn't break.
+    $this->installSchema('user', 'users_data');
+
+    // Setup layout builder and same displays.
+    $this->installLayoutBuilder();
+
+    // Ensure install hook can handle displays without a layout.
+    $this->container->get('module_installer')->install(['field_layout']);
+
+    // Ensure uninstall hook can handle displays without a layout.
+    $this->container->get('module_installer')->uninstall(['field_layout']);
+  }
+
+}
