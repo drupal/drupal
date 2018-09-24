ac623bf91b009a359d4c2ec04042f7d7b1b2e127
Alex Pott <alex.a.pott@googlemail.com>
2018-09-24 08:55:20 +0100

    Issue #2954825 by rakesh.gectcr, tresti88, gnuget, borisson_, DamienGR, Prashant.c, amietpatial, alexpott, joachim, Berdir: Update the user.api.php documentation and add the correct type hints in the user's hooks implementations
    (cherry picked from commit 400413fafaa1652db81c923662d73c3e8363633a)

--- core/modules/user/user.api.php
+++ core/modules/user/user.api.php
@@ -5,6 +5,9 @@
  * Hooks provided by the User module.
  */
 
+use Drupal\Core\Session\AccountInterface;
+use Drupal\user\UserInterface;
+
 /**
  * @addtogroup hooks
  * @{
@@ -28,7 +31,7 @@
  *
  * @param array $edit
  *   The array of form values submitted by the user.
- * @param \Drupal\Core\Session\AccountInterface $account
+ * @param \Drupal\user\UserInterface $account
  *   The user object on which the operation is being performed.
  * @param string $method
  *   The account cancellation method.
@@ -36,7 +39,7 @@
  * @see user_cancel_methods()
  * @see hook_user_cancel_methods_alter()
  */
-function hook_user_cancel($edit, $account, $method) {
+function hook_user_cancel($edit, UserInterface $account, $method) {
   switch ($method) {
     case 'user_cancel_block_unpublish':
       // Unpublish nodes (current revisions).
@@ -120,7 +123,7 @@ function hook_user_cancel_methods_alter(&$methods) {
  * @see \Drupal\Core\Session\AccountInterface::getDisplayName()
  * @see sanitization
  */
-function hook_user_format_name_alter(&$name, $account) {
+function hook_user_format_name_alter(&$name, AccountInterface $account) {
   // Display the user's uid instead of name.
   if ($account->id()) {
     $name = t('User @uid', ['@uid' => $account->id()]);
@@ -130,10 +133,10 @@ function hook_user_format_name_alter(&$name, $account) {
 /**
  * The user just logged in.
  *
- * @param object $account
+ * @param \Drupal\user\UserInterface $account
  *   The user object on which the operation was just performed.
  */
-function hook_user_login($account) {
+function hook_user_login(UserInterface $account) {
   $config = \Drupal::config('system.date');
   // If the user has a NULL time zone, notify them to set a time zone.
   if (!$account->getTimezone() && $config->get('timezone.user.configurable') && $config->get('timezone.user.warn')) {
@@ -151,10 +154,10 @@ function hook_user_login($account) {
 /**
  * The user just logged out.
  *
- * @param $account
+ * @param \Drupal\Core\Session\AccountInterface $account
  *   The user object on which the operation was just performed.
  */
-function hook_user_logout($account) {
+function hook_user_logout(AccountInterface $account) {
   db_insert('logouts')
     ->fields([
       'uid' => $account->id(),
