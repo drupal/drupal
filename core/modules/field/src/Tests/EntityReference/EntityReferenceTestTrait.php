a97bb3aaf41b71f7ebcd0d7b9ce7319733fa4266
Alex Pott <alex.a.pott@googlemail.com>
2018-09-25 09:43:35 +0100

    Issue #3002011 by Mile23, Wim Leers, Berdir, gabesullice: Remove @trigger_error() for 8.6.x Drupal\field\Tests\EntityReference\EntityReferenceTestTrait deprecation

--- core/modules/field/src/Tests/EntityReference/EntityReferenceTestTrait.php
+++ core/modules/field/src/Tests/EntityReference/EntityReferenceTestTrait.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\field\Tests\EntityReference;
 
-@trigger_error(__NAMESPACE__ . '\EntityReferenceTestTrait is deprecated Drupal 8.6.2 for removal before 9.0.0. Use Drupal\Tests\field\Traits\EntityReferenceTestTrait instead. See https://www.drupal.org/node/2998888', E_USER_DEPRECATED);
-
 use Drupal\Tests\field\Traits\EntityReferenceTestTrait as nonDeprecatedEntityReferenceTestTrait;
 
 /**
