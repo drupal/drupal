855d0cf6d8cca2c5a5f27b2bd29e1b51dd561e91
Alex Pott <alex.a.pott@googlemail.com>
2018-09-27 16:25:39 +0100

    Issue #2686409 by mpdonadio, pjonckiere, claudiu.cristea, darrick, xSDx, bceyssens, tamasd, Ismail Cherri, joelpittet, dawehner, AdamPS, jhedstrom, JaceRider: Time Ago summary does not render on Manage Display for Timestamp and Datetime fields
    (cherry picked from commit 21ba3dfc6f8ffb695c4d71e0e7c4cbaf4a17d3ad)

--- core/lib/Drupal/Core/Field/Plugin/Field/FieldFormatter/TimestampAgoFormatter.php
+++ core/lib/Drupal/Core/Field/Plugin/Field/FieldFormatter/TimestampAgoFormatter.php
@@ -5,6 +5,7 @@ namespace Drupal\Core\Field\Plugin\Field\FieldFormatter;
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
@@ -104,7 +105,7 @@ class TimestampAgoFormatter extends FormatterBase implements ContainerFactoryPlu
    * {@inheritdoc}
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
-    $elements = parent::settingsForm($form, $form_state);
+    $form = parent::settingsForm($form, $form_state);
 
     $form['future_format'] = [
       '#type' => 'textfield',
@@ -120,7 +121,7 @@ class TimestampAgoFormatter extends FormatterBase implements ContainerFactoryPlu
       '#description' => $this->t('Use <em>@interval</em> where you want the formatted interval text to appear.'),
     ];
 
-    $elements['granularity'] = [
+    $form['granularity'] = [
       '#type' => 'number',
       '#title' => $this->t('Granularity'),
       '#description' => $this->t('How many time interval units should be shown in the formatted output.'),
@@ -129,7 +130,7 @@ class TimestampAgoFormatter extends FormatterBase implements ContainerFactoryPlu
       '#max' => 6,
     ];
 
-    return $elements;
+    return $form;
   }
 
   /**
@@ -138,10 +139,19 @@ class TimestampAgoFormatter extends FormatterBase implements ContainerFactoryPlu
   public function settingsSummary() {
     $summary = parent::settingsSummary();
 
-    $future_date = strtotime('1 year 1 month 1 week 1 day 1 hour 1 minute');
-    $past_date = strtotime('-1 year -1 month -1 week -1 day -1 hour -1 minute');
-    $summary[] = $this->t('Future date: %display', ['%display' => $this->formatTimestamp($future_date)]);
-    $summary[] = $this->t('Past date: %display', ['%display' => $this->formatTimestamp($past_date)]);
+    $future_date = new DrupalDateTime('1 year 1 month 1 week 1 day 1 hour 1 minute');
+    $past_date = new DrupalDateTime('-1 year -1 month -1 week -1 day -1 hour -1 minute');
+    $granularity = $this->getSetting('granularity');
+    $options = [
+      'granularity' => $granularity,
+      'return_as_object' => FALSE,
+    ];
+
+    $future_date_interval = new FormattableMarkup($this->getSetting('future_format'), ['@interval' => $this->dateFormatter->formatTimeDiffUntil($future_date->getTimestamp(), $options)]);
+    $past_date_interval = new FormattableMarkup($this->getSetting('past_format'), ['@interval' => $this->dateFormatter->formatTimeDiffSince($past_date->getTimestamp(), $options)]);
+
+    $summary[] = $this->t('Future date: %display', ['%display' => $future_date_interval]);
+    $summary[] = $this->t('Past date: %display', ['%display' => $past_date_interval]);
 
     return $summary;
   }
---
+++ core/modules/datetime/tests/src/Functional/DateTimeTimeAgoFormatterTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Drupal\Tests\datetime\Functional;
+
+use Drupal\Core\Entity\Entity\EntityFormDisplay;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests the functionality of DateTimeTimeAgoFormatter field formatter.
+ *
+ * @group field
+ */
+class DateTimeTimeAgoFormatterTest extends BrowserTestBase {
+
+  /**
+   * An array of display options to pass to entity_get_display().
+   *
+   * @var array
+   */
+  protected $displayOptions;
+
+  /**
+   * A field storage to use in this test class.
+   *
+   * @var \Drupal\field\Entity\FieldStorageConfig
+   */
+  protected $fieldStorage;
+
+  /**
+   * The field used in this test class.
+   *
+   * @var \Drupal\field\Entity\FieldConfig
+   */
+  protected $field;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['datetime', 'entity_test', 'field_ui'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $web_user = $this->drupalCreateUser([
+      'access administration pages',
+      'view test entity',
+      'administer entity_test content',
+      'administer entity_test fields',
+      'administer entity_test display',
+      'administer entity_test form display',
+      'view the administration theme',
+    ]);
+    $this->drupalLogin($web_user);
+
+    $field_name = 'field_datetime';
+    $type = 'datetime';
+    $widget_type = 'datetime_default';
+    $formatter_type = 'datetime_time_ago';
+
+    $this->fieldStorage = FieldStorageConfig::create([
+      'field_name' => $field_name,
+      'entity_type' => 'entity_test',
+      'type' => $type,
+    ]);
+    $this->fieldStorage->save();
+    $this->field = FieldConfig::create([
+      'field_storage' => $this->fieldStorage,
+      'bundle' => 'entity_test',
+      'required' => TRUE,
+    ]);
+    $this->field->save();
+
+    EntityFormDisplay::load('entity_test.entity_test.default')
+      ->setComponent($field_name, ['type' => $widget_type])
+      ->save();
+
+    $this->displayOptions = [
+      'type' => $formatter_type,
+      'label' => 'hidden',
+    ];
+
+    EntityViewDisplay::create([
+      'targetEntityType' => $this->field->getTargetEntityTypeId(),
+      'bundle' => $this->field->getTargetBundle(),
+      'mode' => 'full',
+      'status' => TRUE,
+    ])->setComponent($field_name, $this->displayOptions)
+      ->save();
+  }
+
+  /**
+   * Tests the formatter settings.
+   */
+  public function testSettings() {
+    $this->drupalGet('entity_test/structure/entity_test/display');
+
+    $edit = [
+      'fields[field_datetime][region]' => 'content',
+      'fields[field_datetime][type]' => 'datetime_time_ago',
+    ];
+    $this->drupalPostForm(NULL, $edit, t('Save'));
+
+    $this->drupalPostForm(NULL, [], 'field_datetime_settings_edit');
+    $edit = [
+      'fields[field_datetime][settings_edit_form][settings][future_format]' => 'ends in @interval',
+      'fields[field_datetime][settings_edit_form][settings][past_format]' => 'started @interval ago',
+      'fields[field_datetime][settings_edit_form][settings][granularity]' => 3,
+    ];
+    $this->drupalPostForm(NULL, $edit, 'Update');
+    $this->drupalPostForm(NULL, [], 'Save');
+
+    $this->assertSession()->pageTextContains('ends in 1 year 1 month 1 week');
+    $this->assertSession()->pageTextContains('started 1 year 1 month 1 week ago');
+  }
+
+}
