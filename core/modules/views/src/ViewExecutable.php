f6d914e704c216883684aff35576e36778e0d4af
Alex Pott <alex.a.pott@googlemail.com>
2018-10-12 22:43:22 +0100

    Issue #3004228 by mpp, Lendude, drunken monkey: Consistently use ViewExecutable::usePager()

--- core/modules/views/src/ViewExecutable.php
+++ core/modules/views/src/ViewExecutable.php
@@ -925,7 +925,7 @@ class ViewExecutable {
     if (!isset($this->pager)) {
       $this->pager = $this->display_handler->getPlugin('pager');
 
-      if ($this->pager->usePager()) {
+      if ($this->usePager()) {
         $this->pager->setCurrentPage($this->current_page);
       }
 
@@ -951,7 +951,7 @@ class ViewExecutable {
    *   The render array of the pager if it's set, blank string otherwise.
    */
   public function renderPager($exposed_input) {
-    if (!empty($this->pager) && $this->pager->usePager()) {
+    if ($this->usePager()) {
       return $this->pager->render($exposed_input);
     }
 
@@ -1417,7 +1417,7 @@ class ViewExecutable {
     }
 
     if ($cache->cacheGet('results')) {
-      if ($this->pager->usePager()) {
+      if ($this->usePager()) {
         $this->pager->total_items = $this->total_rows;
         $this->pager->updatePageInfo();
       }
