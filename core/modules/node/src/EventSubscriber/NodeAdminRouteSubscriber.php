f3f974cf11e2da75ebff73b78cad732ba6b73f6e
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-11-15 14:52:37 +0000

    Issue #2980237 by Upchuk, alexpott: Rebuilding routes when node.settings:use_admin_theme changes should be done in a config listener

--- core/modules/node/src/EventSubscriber/NodeAdminRouteSubscriber.php
+++ core/modules/node/src/EventSubscriber/NodeAdminRouteSubscriber.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\node\EventSubscriber;
 
+use Drupal\Core\Config\ConfigCrudEvent;
+use Drupal\Core\Config\ConfigEvents;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Routing\RouteBuilderInterface;
 use Drupal\Core\Routing\RouteSubscriberBase;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -19,13 +22,23 @@ class NodeAdminRouteSubscriber extends RouteSubscriberBase {
   protected $configFactory;
 
   /**
+   * The router builder.
+   *
+   * @var \Drupal\Core\Routing\RouteBuilderInterface
+   */
+  protected $routerBuilder;
+
+  /**
    * Constructs a new NodeAdminRouteSubscriber.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
+   * @param \Drupal\Core\Routing\RouteBuilderInterface $router_builder
+   *   The router builder service.
    */
-  public function __construct(ConfigFactoryInterface $config_factory) {
+  public function __construct(ConfigFactoryInterface $config_factory, RouteBuilderInterface $router_builder) {
     $this->configFactory = $config_factory;
+    $this->routerBuilder = $router_builder;
   }
 
   /**
@@ -39,6 +52,26 @@ class NodeAdminRouteSubscriber extends RouteSubscriberBase {
         }
       }
     }
+  }
+
+  /**
+   * Rebuilds the router when node.settings:use_admin_theme is changed.
+   *
+   * @param \Drupal\Core\Config\ConfigCrudEvent $event
+   */
+  public function onConfigSave(ConfigCrudEvent $event) {
+    if ($event->getConfig()->getName() === 'node.settings' && $event->isChanged('use_admin_theme')) {
+      $this->routerBuilder->setRebuildNeeded();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events = parent::getSubscribedEvents();
+    $events[ConfigEvents::SAVE][] = ['onConfigSave', 0];
+    return $events;
   }
 
 }
