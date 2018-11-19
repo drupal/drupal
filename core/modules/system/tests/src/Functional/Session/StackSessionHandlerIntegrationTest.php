6d3fbe9a6483ed73f302ce124971f9bd3d030097
Alex Pott <alex.a.pott@googlemail.com>
2018-11-19 10:21:19 +0000

    Issue #3002121 by Lendude, jibran, borisson_: Session: Convert system functional tests to phpunit

---
+++ core/modules/system/tests/src/Functional/Session/StackSessionHandlerIntegrationTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\Tests\system\Functional\Session;
+
+use Drupal\Core\EventSubscriber\MainContentViewSubscriber;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests the stacked session handler functionality.
+ *
+ * @group Session
+ */
+class StackSessionHandlerIntegrationTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['session_test'];
+
+  /**
+   * Tests a request.
+   */
+  public function testRequest() {
+    $options['query'][MainContentViewSubscriber::WRAPPER_FORMAT] = 'drupal_ajax';
+    $headers[] = 'X-Requested-With: XMLHttpRequest';
+    $actual_trace = json_decode($this->drupalGet('session-test/trace-handler', $options, $headers));
+    $sessionId = $this->getSessionCookies()->getCookieByName($this->getSessionName())->getValue();
+    $expect_trace = [
+      ['BEGIN', 'test_argument', 'open'],
+      ['BEGIN', NULL, 'open'],
+      ['END', NULL, 'open'],
+      ['END', 'test_argument', 'open'],
+      ['BEGIN', 'test_argument', 'read', $sessionId],
+      ['BEGIN', NULL, 'read', $sessionId],
+      ['END', NULL, 'read', $sessionId],
+      ['END', 'test_argument', 'read', $sessionId],
+      ['BEGIN', 'test_argument', 'write', $sessionId],
+      ['BEGIN', NULL, 'write', $sessionId],
+      ['END', NULL, 'write', $sessionId],
+      ['END', 'test_argument', 'write', $sessionId],
+      ['BEGIN', 'test_argument', 'close'],
+      ['BEGIN', NULL, 'close'],
+      ['END', NULL, 'close'],
+      ['END', 'test_argument', 'close'],
+    ];
+    $this->assertEqual($expect_trace, $actual_trace);
+  }
+
+}
