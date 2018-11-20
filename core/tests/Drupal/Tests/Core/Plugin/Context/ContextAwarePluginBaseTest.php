136dd9ed0b805b9d907c76429d0f4ed5df425df9
Alex Pott <alex.a.pott@googlemail.com>
2018-11-20 16:01:43 +0000

    Issue #3014851 by tim.plunkett: ContextAwarePluginBase::setContextValue() should delegate to ::setContext()
    (cherry picked from commit 7dcb927309abbd380ac0cc363ee4b0f0205f3456)

--- core/tests/Drupal/Tests/Core/Plugin/Context/ContextAwarePluginBaseTest.php
+++ core/tests/Drupal/Tests/Core/Plugin/Context/ContextAwarePluginBaseTest.php
@@ -2,11 +2,17 @@
 
 namespace Drupal\Tests\Core\Plugin\Context;
 
+use Drupal\Component\Plugin\Context\ContextInterface as ComponentContextInterface;
 use Drupal\Component\Plugin\Definition\ContextAwarePluginDefinitionInterface;
 use Drupal\Component\Plugin\Definition\ContextAwarePluginDefinitionTrait;
 use Drupal\Component\Plugin\Definition\PluginDefinition;
 use Drupal\Component\Plugin\Exception\ContextException;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\Plugin\Context\ContextDefinition;
 use Drupal\Core\Plugin\ContextAwarePluginBase;
+use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\TypedData\Plugin\DataType\StringData;
+use Drupal\Core\TypedData\TypedDataManagerInterface;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -46,6 +52,22 @@ class ContextAwarePluginBaseTest extends UnitTestCase {
     $this->plugin->getContextDefinition('person');
   }
 
+  /**
+   * @covers ::setContextValue
+   */
+  public function testSetContextValue() {
+    $typed_data_manager = $this->prophesize(TypedDataManagerInterface::class);
+    $container = new ContainerBuilder();
+    $container->set('typed_data_manager', $typed_data_manager->reveal());
+    \Drupal::setContainer($container);
+
+    $this->plugin->getPluginDefinition()->addContextDefinition('foo', new ContextDefinition('string'));
+
+    $this->assertFalse($this->plugin->setContextCalled);
+    $this->plugin->setContextValue('foo', new StringData(new DataDefinition(), 'bar'));
+    $this->assertTrue($this->plugin->setContextCalled);
+  }
+
 }
 
 class TestPluginDefinition extends PluginDefinition implements ContextAwarePluginDefinitionInterface {
@@ -54,4 +76,21 @@ class TestPluginDefinition extends PluginDefinition implements ContextAwarePlugi
 
 }
 
-class TestContextAwarePlugin extends ContextAwarePluginBase {}
+class TestContextAwarePlugin extends ContextAwarePluginBase {
+
+  /**
+   * Indicates if ::setContext() has been called or not.
+   *
+   * @var bool
+   */
+  public $setContextCalled = FALSE;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setContext($name, ComponentContextInterface $context) {
+    parent::setContext($name, $context);
+    $this->setContextCalled = TRUE;
+  }
+
+}
