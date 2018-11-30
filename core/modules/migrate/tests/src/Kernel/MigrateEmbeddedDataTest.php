30cfcd03d05e6511e2438e11b87a67ad5abcf2e7
Alex Pott <alex.a.pott@googlemail.com>
2018-11-30 09:48:15 +0000

    Issue #3017119 by phenaproxima: SourcePluginBase::next() calls Row constructor incorrectly

--- core/modules/migrate/tests/src/Kernel/MigrateEmbeddedDataTest.php
+++ core/modules/migrate/tests/src/Kernel/MigrateEmbeddedDataTest.php
@@ -45,6 +45,11 @@ class MigrateEmbeddedDataTest extends KernelTestBase {
     $results = [];
     /** @var \Drupal\migrate\Row $row */
     foreach ($source as $row) {
+      // The plugin should not mark any rows as stubs. We need to use
+      // assertSame() here because assertFalse() will pass falsy values (e.g.,
+      // empty arrays).
+      $this->assertSame(FALSE, $row->isStub());
+
       $data_row = $row->getSource();
       // The "data" row returned by getSource() also includes all source
       // configuration - we remove it so we see only the data itself.
