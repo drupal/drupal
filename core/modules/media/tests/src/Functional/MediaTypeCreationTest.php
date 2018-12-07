3ba6bc11689c4691063a46ac03ef933885f16f2b
Alex Pott <alex.a.pott@googlemail.com>
2018-12-07 15:37:22 +0100

    Issue #3018539 by phenaproxima, rodrigoaguilera, alexpott: Media types cannot be created in the UI without JavaScript

---
+++ core/modules/media/tests/src/Functional/MediaTypeCreationTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\Tests\media\Functional;
+
+use Drupal\media\Entity\MediaType;
+
+/**
+ * Ensures that media UI works correctly without JavaScript.
+ *
+ * @group media
+ */
+class MediaTypeCreationTest extends MediaFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = [
+    'media_test_source',
+  ];
+
+  /**
+   * Tests the media type creation form with only the mandatory options.
+   */
+  public function testMediaTypeCreationForm() {
+    $machine_name = mb_strtolower($this->randomMachineName());
+
+    $this->drupalGet('/admin/structure/media/add');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->fieldExists('label')->setValue($this->randomString());
+    $this->assertSession()->fieldExists('id')->setValue($machine_name);
+    $this->assertSession()->selectExists('source')->selectOption('test');
+    $this->assertSession()->buttonExists('Save')->press();
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->fieldValueEquals('Test config value', 'This is default value.');
+    $this->assertSession()->buttonExists('Save')->press();
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->addressEquals('admin/structure/media');
+
+    $this->assertInstanceOf(MediaType::class, MediaType::load($machine_name));
+  }
+
+}
