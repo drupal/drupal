017a22fa7853b41d1d0ccdb2bd300243a4875c4b
Alex Pott <alex.a.pott@googlemail.com>
2018-12-14 22:20:28 +0000

    Issue #3020579 by catch: TypeError: Argument 3 passed to Symfony\Component\HttpKernel\Event\FilterResponseEvent::__construct() must be of the type integer, string given [Symfony 4]
    (cherry picked from commit 96ef995291d1b19c9eaf7f098761271fe35efa4f)

--- core/tests/Drupal/Tests/Core/EventSubscriber/RssResponseRelativeUrlFilterTest.php
+++ core/tests/Drupal/Tests/Core/EventSubscriber/RssResponseRelativeUrlFilterTest.php
@@ -114,7 +114,7 @@ RSS;
     $event = new FilterResponseEvent(
       $this->prophesize(HttpKernelInterface::class)->reveal(),
       Request::create('/'),
-      'foo',
+      HttpKernelInterface::MASTER_REQUEST,
       new Response($content, 200, [
         'Content-Type' => 'application/rss+xml',
       ])
