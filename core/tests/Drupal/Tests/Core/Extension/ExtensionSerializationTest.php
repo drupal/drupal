451a7ba86490621396e037860f6db96598a8f9be
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2018-12-18 09:29:50 +0000

    Issue #2701829 by alexpott, andypost, Soul88, Graber, EduardoMadrid, dawehner, Berdir, jibran, pingwin4eg, httang12: Extension objects should not implement \Serializable

--- core/tests/Drupal/Tests/Core/Extension/ExtensionSerializationTest.php
+++ core/tests/Drupal/Tests/Core/Extension/ExtensionSerializationTest.php
@@ -7,12 +7,12 @@ use Drupal\Core\Extension\Extension;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 
 /**
- * Tests preferred use of service application root over DRUPAL_ROOT.
+ * Tests Extension serialization.
  *
  * @coversDefaultClass \Drupal\Core\Extension\Extension
  * @group Extension
  */
-class ExtensionUnserializedServiceAppRootUsageTest extends UnitTestCase {
+class ExtensionSerializationTest extends UnitTestCase {
 
   /**
    * Tests that the Extension class unserialize method uses the preferred root.
@@ -22,7 +22,8 @@ class ExtensionUnserializedServiceAppRootUsageTest extends UnitTestCase {
    * container's app.root and not the DRUPAL_ROOT constant if the service
    * container app.root is available.
    *
-   * @covers ::unserialize
+   * @covers ::__sleep
+   * @covers ::__wakeup
    */
   public function testServiceAppRouteUsage() {
     // The assumption of our test is that DRUPAL_ROOT is not defined.
@@ -33,9 +34,26 @@ class ExtensionUnserializedServiceAppRootUsageTest extends UnitTestCase {
     \Drupal::setContainer($container);
     // Instantiate an Extension object for testing unserialization.
     $extension = new Extension($container->get('app.root'), 'module', 'core/modules/system/system.info.yml', 'system.module');
-    $data = $extension->serialize();
-    $extension->unserialize($data);
+    $extension = unserialize(serialize($extension));
     $this->assertEquals('/dummy/app/root', $this->readAttribute($extension, 'root'));
+  }
+
+  /**
+   * Tests dynamically assigned public properties kept when serialized.
+   *
+   * @covers ::__sleep
+   * @covers ::__wakeup
+   */
+  public function testPublicProperties() {
+    $container = new ContainerBuilder();
+    // Set a dummy container app.root to test against.
+    $container->set('app.root', '/dummy/app/root');
+    \Drupal::setContainer($container);
+    $extension = new Extension($container->get('app.root'), 'module', 'core/modules/system/system.info.yml', 'system.module');
+    // Assign a public property dynamically.
+    $extension->test = 'foo';
+    $extension = unserialize(serialize($extension));
+    $this->assertSame('foo', $extension->test);
   }
 
 }
