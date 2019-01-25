cb244d0d6406776350996079b3ed4fa2bee7af85
Gábor Hojtsy <gabor@hojtsy.hu>
2019-01-25 18:35:03 +0100

    Issue #3027871 by catch, alexpott, jibran, Gábor Hojtsy, Berdir: [symfony 4] Symfony\Component\Validator\Constraints\Email::$strict property is deprecated since Symfony 4.1

--- core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/EmailConstraint.php
+++ core/lib/Drupal/Core/Validation/Plugin/Validation/Constraint/EmailConstraint.php
@@ -16,7 +16,25 @@ use Symfony\Component\Validator\Constraints\Email;
  */
 class EmailConstraint extends Email {
 
-  public $strict = TRUE;
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($options = []) {
+    // Since Symfony 4.1, the 'mode' property is used, for previous versions the
+    // 'strict' property. If the 'strict' property is set,
+    // \Symfony\Component\Validator\Constraints\EmailValidator will trigger
+    // a deprecation error, so only assign a value for versions of Symfony
+    // < 4.2. This compatibility layer can be removed once Drupal requires
+    // Symfony 4.2 or higher in https://www.drupal.org/node/3009219.
+    if (property_exists($this, 'mode')) {
+      $default_options = ['mode' => 'strict'];
+    }
+    else {
+      $default_options = ['strict' => TRUE];
+    }
+    $options += $default_options;
+    parent::__construct($options);
+  }
 
   /**
    * {@inheritdoc}
