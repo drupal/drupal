efefc9236c5f13b528d766714500a6fc48186d9c
Alex Pott <alex.a.pott@googlemail.com>
2019-01-31 00:19:59 +0000

    Issue #3017812 by ibustos, joachim: Language selector is immune to hook_entity_field_access in entity forms
    (cherry picked from commit f6674fa41767d54edfd9e1727d4d82598128e596)

---
+++ core/modules/language/tests/src/Functional/LanguageEntityFieldAccessHookTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\Tests\language\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests language picker compatibility with hook_entity_field_access.
+ *
+ * @group language
+ */
+class LanguageEntityFieldAccessHookTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'node',
+    'text',
+    'field',
+    'filter',
+    'language',
+    'language_entity_field_access_test',
+  ];
+
+  /**
+   * Tests compatibility with hook_entity_field_access().
+   */
+  public function testHookEntityFieldAccess() {
+    // Create an admin user and do the login.
+    $user = $this->drupalCreateUser([], NULL, TRUE);
+    $this->drupalLogin($user);
+
+    // Assess the field is not visible.
+    $this->drupalGet('node/add/page');
+    $this->assertSession()->fieldNotExists('langcode[0][value]');
+
+    $this->drupalLogout();
+  }
+
+}
