f458f5a290343438428b63a96a33cb63ee96a23b
Lee Rowlands <lee.rowlands@previousnext.com.au>
2019-02-06 20:04:17 +1000

    Issue #2135445 by dww, Sam152, jessebeach, Mile23, Kristen Pol, Wim Leers, larowlan: Toolbar displays Manage tab even if the user is not permitted to see it
    (cherry picked from commit 211c6641f86d5e9c3e4c9cee267bf44f8bf23a05)

--- core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayBlockFormTest.php
+++ core/modules/settings_tray/tests/src/FunctionalJavascript/SettingsTrayBlockFormTest.php
@@ -30,6 +30,7 @@ class SettingsTrayBlockFormTest extends SettingsTrayTestBase {
 
     $user = $this->createUser([
       'administer blocks',
+      'access administration pages',
       'access contextual links',
       'access toolbar',
       'administer nodes',
