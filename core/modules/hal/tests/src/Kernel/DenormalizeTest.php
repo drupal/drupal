d540d8d6357f558280fbd7ad841bb3cc9617b543
xjm <xjm@65776.no-reply.drupal.org>
2019-02-21 16:38:28 -0600

    Issue #3034695 by xjm, neclimdul: Fix coding standard errors from SA-CORE-2019-003

--- core/modules/hal/tests/src/Kernel/DenormalizeTest.php
+++ core/modules/hal/tests/src/Kernel/DenormalizeTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\hal\Kernel;
 
 use Drupal\Core\Url;
-use Drupal\entity_test\Entity\EntitySerializedField;
 use Drupal\field\Entity\FieldConfig;
 use Symfony\Component\Serializer\Exception\UnexpectedValueException;
 
