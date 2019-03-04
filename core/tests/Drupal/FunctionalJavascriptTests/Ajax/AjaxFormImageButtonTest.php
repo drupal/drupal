944b157e5bb95d993c3289fcaca8db88106e5e49
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2019-03-04 17:07:38 +0000

    Issue #2996404 by andrewmacpherson, Krzysztof Domański: Replace old keypress workaround in AjaxFormImageButtonTest

--- core/tests/Drupal/FunctionalJavascriptTests/Ajax/AjaxFormImageButtonTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Ajax/AjaxFormImageButtonTest.php
@@ -19,22 +19,14 @@ class AjaxFormImageButtonTest extends WebDriverTestBase {
   /**
    * Tests image buttons can be operated with the keyboard ENTER key.
    */
-  public function testAjaxImageButton() {
+  public function testAjaxImageButtonKeypressEnter() {
     // Get a Field UI manage-display page.
     $this->drupalGet('ajax_forms_image_button_form');
     $assertSession = $this->assertSession();
     $session = $this->getSession();
 
-    $enter_key_event = <<<JS
-jQuery('#edit-image-button')
-  .trigger(jQuery.Event('keypress', {
-    which: 13
-  }));
-JS;
-    // PhantomJS driver has buggy behavior with key events, we send a JavaScript
-    // key event instead.
-    // @todo: use WebDriver event when we remove PhantomJS driver.
-    $session->executeScript($enter_key_event);
+    $button = $session->getPage()->findButton('Edit');
+    $button->keyPress(13);
 
     $this->assertNotEmpty($assertSession->waitForElementVisible('css', '#ajax-1-more-div'), 'Page updated after image button pressed');
   }
