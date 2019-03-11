1e0ad95a0b346dce61711425be9cd40eb965f9da
Alex Pott <alex.a.pott@googlemail.com>
2019-03-11 13:00:57 +0000

    Revert "Issue #3035316 by seanB, andrewmacpherson, oknate, phenaproxima, rainbreaw: ‘Add media’ in MediaLibraryWidget should be a button, not a link"
    This reverts commit 58d6b4428839a2d4d7becbee9f5fc67eaf5e871e.

--- core/modules/media_library/src/Form/AddFormBase.php
+++ core/modules/media_library/src/Form/AddFormBase.php
@@ -12,6 +12,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\media\MediaInterface;
 use Drupal\media\MediaTypeInterface;
 use Drupal\media_library\Ajax\UpdateSelectionCommand;
+use Drupal\media_library\MediaLibraryState;
 use Drupal\media_library\MediaLibraryUiBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -419,7 +420,7 @@ abstract class AddFormBase extends FormBase {
     // contains the vertical tabs. Besides that, we also need to force the media
     // library to create a new instance of the media add form.
     // @see \Drupal\media_library\MediaLibraryUiBuilder::buildMediaTypeAddForm()
-    $state = $form_state->get('media_library_state');
+    $state = MediaLibraryState::fromRequest($this->getRequest());
     $state->remove('media_library_content');
     $state->set('_media_library_form_rebuild', TRUE);
     $library_ui = $this->libraryUiBuilder->buildUi($state);
