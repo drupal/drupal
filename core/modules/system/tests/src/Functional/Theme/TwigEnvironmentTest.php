f3183c02cf09ad9ece0bf725b19be8ff4e7f06e6
xjm <xjm@65776.no-reply.drupal.org>
2019-03-14 15:31:42 -0500

    Issue #3040210 by Spokje, phenaproxima, shaal, lauriii, hctom, a.dmitriiev, xjm, cilefen, eelkeblok: Updating twig/twig to v1.38.2 (included in Drupal 8.6.11) causes fatal error when embed tags are used in templates
    (cherry picked from commit 7e18d45eeee54b2c0e8789d91d0f8c2bb9aa63a7)

---
+++ core/modules/system/tests/src/Functional/Theme/TwigEnvironmentTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\Tests\system\Functional\Theme;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests Twig environment.
+ *
+ * @group Theme
+ */
+class TwigEnvironmentTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['twig_theme_test'];
+
+  /**
+   * Tests template class loading with Twig embed.
+   */
+  public function testTwigEmbed() {
+    $assert_session = $this->assertSession();
+    // Test the Twig embed tag.
+    $this->drupalGet('twig-theme-test/embed-tag');
+    $assert_session->statusCodeEquals(200);
+    $assert_session->responseContains('This line is from twig_theme_test/templates/twig-theme-test-embed-tag-embedded.html.twig');
+  }
+
+}
