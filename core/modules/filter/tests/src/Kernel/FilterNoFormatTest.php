937d6fed0f8d40c54f1ac38a531f87fec2a0aaf7
Alex Pott <alex.a.pott@googlemail.com>
2019-04-01 23:22:24 +0100

    Issue #3041096 by claudiu.cristea: Convert FilterNoFormatTest to a kernel test

--- core/modules/filter/tests/src/Kernel/FilterNoFormatTest.php
+++ core/modules/filter/tests/src/Kernel/FilterNoFormatTest.php
@@ -1,20 +1,18 @@
 <?php
 
-namespace Drupal\Tests\filter\Functional;
+namespace Drupal\Tests\filter\Kernel;
 
-use Drupal\Tests\BrowserTestBase;
+use Drupal\KernelTests\KernelTestBase;
 
 /**
  * Tests the behavior of check_markup() when it is called without text format.
  *
  * @group filter
  */
-class FilterNoFormatTest extends BrowserTestBase {
+class FilterNoFormatTest extends KernelTestBase {
 
   /**
-   * Modules to enable.
-   *
-   * @var array
+   * {@inheritdoc}
    */
   public static $modules = ['filter'];
 
@@ -25,13 +23,15 @@ class FilterNoFormatTest extends BrowserTestBase {
    * fallback format.
    */
   public function testCheckMarkupNoFormat() {
+    $this->installConfig(['filter']);
+
     // Create some text. Include some HTML and line breaks, so we get a good
     // test of the filtering that is applied to it.
     $text = "<strong>" . $this->randomMachineName(32) . "</strong>\n\n<div>" . $this->randomMachineName(32) . "</div>";
 
     // Make sure that when this text is run through check_markup() with no text
     // format, it is filtered as though it is in the fallback format.
-    $this->assertEqual(check_markup($text), check_markup($text, filter_fallback_format()), 'Text with no format is filtered the same as text in the fallback format.');
+    $this->assertEquals(check_markup($text), check_markup($text, filter_fallback_format()));
   }
 
 }
