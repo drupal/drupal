9e5e613d42980a23bce570faf96814030591e256
Alex Pott <alex.a.pott@googlemail.com>
2019-04-02 09:05:12 +0100

    Issue #3041727 by Manuel Garcia, claudiu.cristea, amateescu: Convert ViewRenderTest into a Kernel test

--- core/modules/views/tests/src/Kernel/ViewRenderTest.php
+++ core/modules/views/tests/src/Kernel/ViewRenderTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\Tests\views\Functional;
+namespace Drupal\Tests\views\Kernel;
 
 use Drupal\views\Views;
 
@@ -9,33 +9,26 @@ use Drupal\views\Views;
  *
  * @group views
  */
-class ViewRenderTest extends ViewTestBase {
+class ViewRenderTest extends ViewsKernelTestBase {
 
   /**
-   * Views used by this test.
-   *
-   * @var array
+   * {@inheritdoc}
    */
   public static $testViews = ['test_view_render'];
-
-  protected function setUp($import_test_views = TRUE) {
-    parent::setUp($import_test_views);
-
-    $this->enableViewsTestModule();
-  }
 
   /**
    * Tests render functionality.
    */
   public function testRender() {
-    \Drupal::state()->set('views_render.test', 0);
+    $state = $this->container->get('state');
+    $state->set('views_render.test', 0);
 
     // Make sure that the rendering just calls the preprocess function once.
     $view = Views::getView('test_view_render');
     $output = $view->preview();
     $this->container->get('renderer')->renderRoot($output);
 
-    $this->assertEqual(\Drupal::state()->get('views_render.test'), 1);
+    $this->assertEquals(1, $state->get('views_render.test'));
   }
 
 }
