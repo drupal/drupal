752ad1a4def5a75445febf55d4011d7cb7318f96
Lee Rowlands <lee.rowlands@previousnext.com.au>
2019-04-04 19:39:39 +1000

    Issue #3045349 by alexpott, mikelutz, mglaman: Lazy started sessions with session data are not saved with symfony/http-foundation 3.4.24
    (cherry picked from commit 3d7ea45cced9fcbb0a58f8357b63a7f6db415179)

--- core/lib/Drupal/Core/Session/SessionManager.php
+++ core/lib/Drupal/Core/Session/SessionManager.php
@@ -216,7 +216,9 @@ class SessionManager extends NativeSessionStorage implements SessionManagerInter
       throw new \InvalidArgumentException('The optional parameters $destroy and $lifetime of SessionManager::regenerate() are not supported currently');
     }
 
-    if ($this->isStarted()) {
+    // Only migrate the session if the session is really started and not only
+    // lazy started.
+    if ($this->started) {
       $old_session_id = $this->getId();
       // Save and close the old session. Call the parent method to avoid issue
       // with session destruction due to the session being considered obsolete.
@@ -338,6 +340,21 @@ class SessionManager extends NativeSessionStorage implements SessionManagerInter
       ->fields($fields)
       ->condition('sid', Crypt::hashBase64($old_session_id))
       ->execute();
+  }
+
+  /**
+   * Checks if the session is started.
+   *
+   * Beginning with symfony/http-foundation 3.4.24, the session will no longer
+   * save unless this method returns true. The parent method returns true if
+   * $this->started is true, but we need the session to also save if we lazy
+   * started, so we override isStarted() here.
+   *
+   * @return bool
+   *   True if started, false otherwise
+   */
+  public function isStarted() {
+    return parent::isStarted() || $this->startedLazy;
   }
 
 }
