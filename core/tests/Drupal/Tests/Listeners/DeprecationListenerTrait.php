37e405c63676505827afa0d8fbf12d47c2e0641f
Gábor Hojtsy <gabor@hojtsy.hu>
2019-05-14 15:09:45 +0200

    Issue #3052625 by Mile23, alexpott: DeprecationListenerTrait croaks on tests with no results
    (cherry picked from commit 50a5f02e2ee0dae6512bb600a9389b2301eb0918)

--- core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
+++ core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
@@ -175,12 +175,16 @@ trait DeprecationListenerTrait {
     // SymfonyTestsListenerTrait has its own error handler that needs to be
     // removed before this one.
     $test_result_object = $test->getTestResultObject();
-    $reflection_class = new \ReflectionClass($test_result_object);
-    $reflection_property = $reflection_class->getProperty('listeners');
-    $reflection_property->setAccessible(TRUE);
-    $listeners = $reflection_property->getValue($test_result_object);
-    $listeners[] = new AfterSymfonyListener();
-    $reflection_property->setValue($test_result_object, $listeners);
+    // It's possible that a test does not have a result object. This can happen
+    // when a test class does not have any test methods.
+    if ($test_result_object) {
+      $reflection_class = new \ReflectionClass($test_result_object);
+      $reflection_property = $reflection_class->getProperty('listeners');
+      $reflection_property->setAccessible(TRUE);
+      $listeners = $reflection_property->getValue($test_result_object);
+      $listeners[] = new AfterSymfonyListener();
+      $reflection_property->setValue($test_result_object, $listeners);
+    }
   }
 
 }
