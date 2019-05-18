ffd99ca739819ab1840805492e2dd9ebb211f637
Alex Pott <alex.a.pott@googlemail.com>
2019-05-18 15:44:09 +0200

    Issue #3055495 by Mile23, Lendude: Search page migration tests are misplaced kernel tests

--- core/modules/search/tests/src/Kernel/Migrate/d7/SearchPageTest.php
+++ core/modules/search/tests/src/Kernel/Migrate/d7/SearchPageTest.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Drupal\Tests\search\Unit\Plugin\migrate\source\d6;
+namespace Drupal\Tests\search\Kernel\Migrate\d7;
 
 use Drupal\Tests\migrate\Kernel\MigrateSqlSourceTestBase;
 
 /**
- * Tests D6 search page source plugin.
+ * Tests D7 search page source plugin.
  *
- * @covers \Drupal\search\Plugin\migrate\source\d6\SearchPage
+ * @covers \Drupal\search\Plugin\migrate\source\d7\SearchPage
  * @group search
  */
 class SearchPageTest extends MigrateSqlSourceTestBase {
@@ -24,6 +24,10 @@ class SearchPageTest extends MigrateSqlSourceTestBase {
     $tests[0]['source_data'] = [
       'variable' => [
         [
+          'name' => 'search_active_modules',
+          'value' => 'a:2:{s:4:"node";s:4:"node";s:4:"user";i:0;}',
+        ],
+        [
           'name' => 'node_rank_comments',
           'value' => 's:1:"5";',
         ],
@@ -36,6 +40,11 @@ class SearchPageTest extends MigrateSqlSourceTestBase {
         [
           'name' => 'node',
           'type' => 'module',
+          'status' => '0',
+        ],
+        [
+          'name' => 'user',
+          'type' => 'module',
           'status' => '1',
         ],
       ],
@@ -44,8 +53,15 @@ class SearchPageTest extends MigrateSqlSourceTestBase {
     $tests[0]['expected_data'] = [
       [
         'module' => 'node',
+        'status' => 'node',
+        'module_exists' => FALSE,
         'node_rank_comments' => '5',
         'node_rank_promote' => '1',
+      ],
+      [
+        'module' => 'user',
+        'status' => 0,
+        'module_exists' => TRUE,
       ],
     ];
 
