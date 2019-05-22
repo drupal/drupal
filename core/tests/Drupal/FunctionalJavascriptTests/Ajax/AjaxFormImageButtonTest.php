d5142d9faab14a0ea145d36cadfee387c4932f0c
Lauri Eskola <lauri.eskola@acquia.com>
2019-05-22 09:05:58 +0300

    Issue #3030363 by andrewmacpherson, Krzysztof Domański: Add test to assert ajax buttons are operable with the spacebar key
    (cherry picked from commit 9a3477e83b9ae358ba7288a842b571e85b8e1793)

--- core/tests/Drupal/FunctionalJavascriptTests/Ajax/AjaxFormImageButtonTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Ajax/AjaxFormImageButtonTest.php
@@ -31,4 +31,19 @@ class AjaxFormImageButtonTest extends WebDriverTestBase {
     $this->assertNotEmpty($assertSession->waitForElementVisible('css', '#ajax-1-more-div'), 'Page updated after image button pressed');
   }
 
+  /**
+   * Tests image buttons can be operated with the keyboard SPACE key.
+   */
+  public function testAjaxImageButtonKeypressSpace() {
+    // Get a Field UI manage-display page.
+    $this->drupalGet('ajax_forms_image_button_form');
+    $assertSession = $this->assertSession();
+    $session = $this->getSession();
+
+    $button = $session->getPage()->findButton('Edit');
+    $button->keyPress(32);
+
+    $this->assertNotEmpty($assertSession->waitForElementVisible('css', '#ajax-1-more-div'), 'Page updated after image button pressed');
+  }
+
 }
