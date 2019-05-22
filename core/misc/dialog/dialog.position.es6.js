3925713fc080338ca02f940aa5ba4f9a00d2d74d
Lauri Eskola <lauri.eskola@acquia.com>
2019-05-22 12:08:14 +0300

    Revert "Issue #2822778 by wengerk, bighappyface, joelpittet, Lendude, lauriii, vivekguptakota, Meenakshi.g, shashikant_chauhan, GoZ, ifrik, Manuel Garcia, Neograph734: Modal popups in views is not showing properly when toolbar tray is upon modal"
    This reverts commit 3257ff168bfefba0e8789d05ac31620a9c3d0af7.
    
    (cherry picked from commit 8f8ddab283776645856c2061e216e4ca8faa8ec6)

--- core/misc/dialog/dialog.position.es6.js
+++ core/misc/dialog/dialog.position.es6.js
@@ -129,9 +129,6 @@
           autoResize,
         );
       }
-      // Force the dialog & dialog-overlay to render on top.
-      $element.dialog('widget').css('zIndex', 601);
-      $('.ui-widget-overlay').css('zIndex', 600);
     },
     'dialog:beforeclose': function(event, dialog, $element) {
       $(window).off('.dialogResize');
