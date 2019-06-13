03965a9b4df55959f2ced2142a26a8ddf8962e34
Alex Pott <alex.a.pott@googlemail.com>
2019-06-13 17:06:01 +0300

    Revert "Issue #2979966 by quietone, PieterDC, masipila, heddn, Gábor Hojtsy: Migrate D7 i18n taxonomy term language"
    This reverts commit 379763d5e5430d2c7b2c102a522a07feceb1ec7d.

--- core/modules/taxonomy/src/Plugin/migrate/source/d7/Term.php
+++ core/modules/taxonomy/src/Plugin/migrate/source/d7/Term.php
@@ -32,9 +32,6 @@ class Term extends FieldableEntity {
       $query->condition('tv.machine_name', (array) $this->configuration['bundle'], 'IN');
     }
 
-    if ($this->getDatabase()->schema()->fieldExists('taxonomy_vocabulary', 'i18n_mode')) {
-      $query->addField('tv', 'i18n_mode');
-    }
     return $query;
   }
 
@@ -71,12 +68,6 @@ class Term extends FieldableEntity {
     $entity_translatable = $this->isEntityTranslatable('taxonomy_term') && in_array($vocabulary, $translatable_vocabularies, TRUE);
     $source_language = $this->getEntityTranslationSourceLanguage('taxonomy_term', $tid);
     $language = $entity_translatable && $source_language ? $source_language : $default_language['language'];
-
-    // If this is an i18n translation.
-    $i18n_mode = $row->get('i18n_mode');
-    if ($i18n_mode) {
-      $language = $row->get('language');
-    }
     $row->setSourceProperty('language', $language);
 
     // Get Field API field values.
