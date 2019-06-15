d7183eeaca9a11d7ec24a68a58de3b937f3d2245
Alex Pott <alex.a.pott@googlemail.com>
2019-06-15 20:36:28 +0100

    Issue #3056414 by yogeshmpawar, R.shaikh, joachim, John Cook: inaccurate property docs for Drupal\Core\Site\Settings

--- core/lib/Drupal/Core/Cache/DatabaseBackendFactory.php
+++ core/lib/Drupal/Core/Cache/DatabaseBackendFactory.php
@@ -22,7 +22,7 @@ class DatabaseBackendFactory implements CacheFactoryInterface {
   protected $checksumProvider;
 
   /**
-   * The settings array.
+   * The site settings.
    *
    * @var \Drupal\Core\Site\Settings
    */
@@ -36,7 +36,7 @@ class DatabaseBackendFactory implements CacheFactoryInterface {
    * @param \Drupal\Core\Cache\CacheTagsChecksumInterface $checksum_provider
    *   The cache tags checksum provider.
    * @param \Drupal\Core\Site\Settings $settings
-   *   (optional) The settings array.
+   *   (optional) The site settings.
    *
    * @throws \BadMethodCallException
    */
