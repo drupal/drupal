d77f9a680ed4482df5917f89521a52b92d428111
Alex Pott <alex.a.pott@googlemail.com>
2019-06-23 12:49:55 +0100

    Issue #2979044 by BR0kEN, alexpott: Unable to change menu items weight when the "system.site.weight_select_max" exceeded and "#pre_render" for "number" element is modified
    (cherry picked from commit d90d0ebbda976d7bd18b49bddef989de51d20ed5)

--- core/tests/Drupal/KernelTests/Core/Render/Element/WeightTest.php
+++ core/tests/Drupal/KernelTests/Core/Render/Element/WeightTest.php
@@ -3,6 +3,8 @@
 namespace Drupal\KernelTests\Core\Render\Element;
 
 use Drupal\Core\Form\FormState;
+use Drupal\Core\Render\Element\Number;
+use Drupal\Core\Render\Element\Select;
 use Drupal\Core\Render\Element\Weight;
 use Drupal\KernelTests\KernelTestBase;
 
@@ -15,7 +17,7 @@ class WeightTest extends KernelTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['system'];
+  protected static $modules = ['system', 'element_info_test'];
 
   /**
    * {@inheritdoc}
@@ -47,6 +49,78 @@ class WeightTest extends KernelTestBase {
       isset($element['#options'][$element['#default_value']]),
       'Default value exists in the #options list'
     );
+  }
+
+  /**
+   * Test transformation from "select" to "number" for MAX_DELTA + 1.
+   *
+   * @throws \Exception
+   *
+   * @covers ::processWeight
+   */
+  public function testProcessWeightSelectMax() {
+    $form_state = new FormState();
+    $definition = [
+      '#type' => 'weight',
+      '#delta' => $this->container
+        ->get('config.factory')
+        ->get('system.site')
+        ->get('weight_select_max'),
+      // Expected by the "doBuildForm()" method of "form_builder" service.
+      '#parents' => [],
+    ];
+
+    $assert = function ($type, array $element, array $expected) use ($form_state) {
+      // Pretend we have a form to trigger the "#process" callbacks.
+      $element = $this->container
+        ->get('form_builder')
+        ->doBuildForm(__FUNCTION__, $element, $form_state);
+
+      $expected['#type'] = $type;
+
+      foreach ($expected as $property => $value) {
+        static::assertSame($value, $element[$property]);
+      }
+
+      return $element;
+    };
+
+    // When the "#delta" is less or equal to maximum the "weight" must be
+    // rendered as a "select".
+    $select = $definition;
+    $assert('select', $select, [
+      '#process' => [
+        [Select::class, 'processSelect'],
+        [Select::class, 'processAjaxForm'],
+      ],
+      '#pre_render' => [
+        [Select::class, 'preRenderSelect'],
+      ],
+    ]);
+
+    $number = $definition;
+    // Increase "#delta" in order to start rendering "number" elements
+    // instead of "select".
+    $number['#delta']++;
+    // The "number" element definition has the "#pre_render" declaration by
+    // default. The "hook_element_info_alter()" allows to modify the definition
+    // of an element. We must be sure the standard "#pre_render" callbacks
+    // are presented (unless explicitly removed) even in a case when the array
+    // is modified by the alter hook.
+    $assert('number', $number, [
+      '#process' => [
+        [Number::class, 'processAjaxForm'],
+      ],
+      '#element_validate' => [
+        [Number::class, 'validateNumber'],
+      ],
+      '#pre_render' => [
+        [Number::class, 'preRenderNumber'],
+        // The custom callback is appended.
+        /* @see element_info_test_element_info_alter() */
+        'element_info_test_element_pre_render',
+      ],
+    ]);
   }
 
 }
