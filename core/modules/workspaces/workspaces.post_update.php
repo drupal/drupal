9f9d61a5dafd83e3bc9ba3fdf5180e15dec6e542
xjm <xjm@65776.no-reply.drupal.org>
2019-07-16 11:24:57 -0500

    SA-CORE-2019-008 by dwbotsch, xjm, mlhess, cilefen, greggles, drumm, alexpott, amateescu

---
+++ core/modules/workspaces/workspaces.post_update.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for the Workspaces module.
+ */
+
+/**
+ * Clear caches due to access changes.
+ */
+function workspaces_post_update_access_clear_caches() {
+}
