2c4789c54037ecca64f8b83913ebdbabe90fb307
catch <catch@35733.no-reply.drupal.org>
2019-07-23 13:33:19 +0100

    Issue #3060550 by Firass Ziedan: oembed link does not pass the URL parameter to the provider
    (cherry picked from commit 6a2df2601790984f2bd912126a22b7d751fc3a8e)

--- core/modules/media/src/OEmbed/UrlResolver.php
+++ core/modules/media/src/OEmbed/UrlResolver.php
@@ -173,7 +173,7 @@ class UrlResolver implements UrlResolverInterface {
     // provide extra parameters in the query string. For example, Instagram also
     // supports the 'omitscript' parameter.
     $this->moduleHandler->alter('oembed_resource_url', $parsed_url, $provider);
-    $resource_url = $parsed_url['path'] . '?' . UrlHelper::buildQuery($parsed_url['query']);
+    $resource_url = $parsed_url['path'] . '?' . rawurldecode(UrlHelper::buildQuery($parsed_url['query']));
 
     $this->urlCache[$url] = $resource_url;
     $this->cacheSet($cache_id, $resource_url);
