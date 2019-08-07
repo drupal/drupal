b0358ae8e76d1c937424a2e34b624203c85cbc6c
Ted Bowman <ted@tedbow.com>
2019-08-07 13:06:04 -0400

    assert driver class

--- core/modules/system/tests/src/FunctionalJavascript/DebugTest.php
+++ core/modules/system/tests/src/FunctionalJavascript/DebugTest.php
@@ -12,9 +12,13 @@ use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
 class DebugTest extends WebDriverTestBase {
 
   public function testXPath() {
+    $driver = $this->getSession()->getDriver();
+    $this->assertEquals('Drupal\FunctionalJavascriptTests\DrupalSelenium2Driver', get_class($driver));
     $this->drupalGet('user');
     $assert_session = $this->assertSession();
-    $this->assertNotEmpty($this->getSession()->getPage());
+    $page = $this->getSession()->getPage();
+    $this->assertEquals('//html', $page->getXpath());
+    $this->assertNotEmpty($driver->getContent());
     $assert_session->elementExists('xpath', '/body');
     $assert_session->elementNotExists('xpath', '/aksdjfajsdf');
     $assert_session->elementExists('css', 'body');
