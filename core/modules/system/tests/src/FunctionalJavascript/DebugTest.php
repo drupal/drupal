f6fb2e06ea605d28fdd2331980317b21e0709f46
Ted Bowman <ted@tedbow.com>
2019-08-07 12:51:48 -0400

    add passing debug test

---
+++ core/modules/system/tests/src/FunctionalJavascript/DebugTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\Tests\system\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+
+/**
+ * Class DebugTest
+ *
+ * @group debug
+ */
+class DebugTest extends WebDriverTestBase {
+
+  public function testXPath() {
+    $this->drupalGet('user');
+    $assert_session = $this->assertSession();
+    $assert_session->elementExists('xpath', '/body');
+    $assert_session->elementNotExists('xpath', '/aksdjfajsdf');
+    $assert_session->elementExists('css', 'body');
+  }
+
+
+}
