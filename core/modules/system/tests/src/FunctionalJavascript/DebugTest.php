143286a7b3d12fb682191473c788286f5753d6b8
Ted Bowman <ted@tedbow.com>
2019-08-07 13:26:45 -0400

    use static files

--- core/modules/system/tests/src/FunctionalJavascript/DebugTest.php
+++ core/modules/system/tests/src/FunctionalJavascript/DebugTest.php
@@ -14,15 +14,16 @@ class DebugTest extends WebDriverTestBase {
   public function testXPath() {
     $driver = $this->getSession()->getDriver();
     $this->assertEquals('Drupal\FunctionalJavascriptTests\DrupalSelenium2Driver', get_class($driver));
-    $this->drupalGet('user');
+    $this->drupalGet('debug.html');
     $assert_session = $this->assertSession();
     $page = $this->getSession()->getPage();
     $this->assertEquals('//html', $page->getXpath());
     $this->assertNotEmpty($driver->getContent());
+    //file_put_contents('/Users/ted.bowman/Sites/www/ted.html', $driver->getContent());
     /** @var \Behat\Mink\Element\NodeElement $body_element */
-    $body_element = $driver->find('//html/body')[0];
+    $body_element = $driver->find('//*[@id="findme"]')[0];
     self::assertNotEmpty($body_element);
-    $this->assertEquals('body', $body_element->getTagName());
+    $this->assertEquals('h1', $body_element->getTagName());
     $assert_session->elementExists('xpath', '/body');
     $assert_session->elementNotExists('xpath', '/aksdjfajsdf');
     $assert_session->elementExists('css', 'body');
