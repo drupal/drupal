ae590d934c61be2822cee761b4b5b57ce23c9a52
Ted Bowman <ted@tedbow.com>
2019-08-07 12:55:43 -0400

    check for page

--- core/modules/system/tests/src/FunctionalJavascript/DebugTest.php
+++ core/modules/system/tests/src/FunctionalJavascript/DebugTest.php
@@ -14,6 +14,7 @@ class DebugTest extends WebDriverTestBase {
   public function testXPath() {
     $this->drupalGet('user');
     $assert_session = $this->assertSession();
+    $this->assertNotEmpty($this->getSession()->getPage());
     $assert_session->elementExists('xpath', '/body');
     $assert_session->elementNotExists('xpath', '/aksdjfajsdf');
     $assert_session->elementExists('css', 'body');
