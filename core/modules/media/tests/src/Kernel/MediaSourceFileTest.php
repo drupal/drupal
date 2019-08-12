3237440708c56cfd50abe51cbbbd4ba509995db2
Lee Rowlands <lee.rowlands@previousnext.com.au>
2019-08-13 07:26:31 +1000

    Issue #2849413 by sam-elayyoub, idebr, tatarbj, B N Pandey, annetee, developmenticon, borisson_, Lendude: Class name must be a valid object or a string in core/modules/image/src/Entity/ImageStyle.php on line 174

--- core/modules/media/tests/src/Kernel/MediaSourceFileTest.php
+++ core/modules/media/tests/src/Kernel/MediaSourceFileTest.php
@@ -27,4 +27,21 @@ class MediaSourceFileTest extends MediaKernelTestBase {
     $this->assertCount(0, $result);
   }
 
+  /**
+   * Tests a media file can be deleted.
+   */
+  public function testFileDeletion() {
+    $mediaType = $this->createMediaType('file');
+    $media = $this->generateMedia('test.txt', $mediaType);
+    $media->save();
+
+    $source_field_name = $mediaType->getSource()
+      ->getSourceFieldDefinition($mediaType)
+      ->getName();
+    /** @var \Drupal\file\FileInterface $file */
+    $file = $media->get($source_field_name)->entity;
+    $file->delete();
+    $this->assertEmpty($this->container->get('entity_type.manager')->getStorage('file')->loadByProperties(['filename' => 'test.txt']));
+  }
+
 }
