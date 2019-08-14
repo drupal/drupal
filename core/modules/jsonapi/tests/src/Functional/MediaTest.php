9eca7bba0b76c1c808102da7352dfcdbc775c3d7
Lee Rowlands <lee.rowlands@previousnext.com.au>
2019-08-14 10:21:15 +1000

    Issue #2952522 by Wim Leers, gabesullice, e0ipso, Berdir: Remove @todo that is no longer relevant

--- core/modules/jsonapi/tests/src/Functional/MediaTest.php
+++ core/modules/jsonapi/tests/src/Functional/MediaTest.php
@@ -350,8 +350,6 @@ class MediaTest extends ResourceTestBase {
 
   /**
    * {@inheritdoc}
-   *
-   * @todo Determine if this override should be removed in https://www.drupal.org/project/jsonapi/issues/2952522
    */
   protected function getExpectedGetRelationshipDocumentData($relationship_field_name, EntityInterface $entity = NULL) {
     $data = parent::getExpectedGetRelationshipDocumentData($relationship_field_name, $entity);
