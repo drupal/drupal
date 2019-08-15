ece5db4017a17aea418c15fa781dd826a38cb93a
Lee Rowlands <lee.rowlands@previousnext.com.au>
2019-08-15 10:39:36 +1000

    Issue #3063179 by Lendude, shubham.prakash, daffie: Add test coverage for Drupal\views\Plugin\views\field\Counter::getValue()

--- core/modules/views/tests/src/Kernel/Handler/FieldCounterTest.php
+++ core/modules/views/tests/src/Kernel/Handler/FieldCounterTest.php
@@ -10,9 +10,6 @@ use Drupal\views\Views;
  * Tests the Drupal\views\Plugin\views\field\Counter handler.
  *
  * @group views
- *
- * @todo Write tests for pager in
- *   https://www.drupal.org/project/drupal/issues/3063179
  */
 class FieldCounterTest extends ViewsKernelTestBase {
 
@@ -86,6 +83,95 @@ class FieldCounterTest extends ViewsKernelTestBase {
     $counter = $view->style_plugin->getField(2, 'counter');
     $expected_number = 2 + $rand_start;
     $this->assertEqual($counter, (string) $expected_number, new FormattableMarkup('Make sure the expected number (@expected) patches with the rendered number (@counter)', ['@expected' => $expected_number, '@counter' => $counter]));
+  }
+
+  /**
+   * Tests the counter field when using a pager.
+   */
+  public function testPager() {
+    $view = Views::getView('test_view');
+    $view->setDisplay();
+    $view->displayHandlers->get('default')->overrideOption('fields', [
+      'counter' => [
+        'id' => 'counter',
+        'table' => 'views',
+        'field' => 'counter',
+        'relationship' => 'none',
+      ],
+      'name' => [
+        'id' => 'name',
+        'table' => 'views_test_data',
+        'field' => 'name',
+        'relationship' => 'none',
+      ],
+    ]);
+    $view->displayHandlers->get('default')->setOption('pager', [
+      'type' => 'mini',
+      'options' => ['items_per_page' => 1],
+    ]);
+
+    $view->preview();
+
+    $counter = $view->style_plugin->getField(0, 'counter');
+    $this->assertEquals('1', $counter);
+    $view->destroy();
+
+    // Go the the second page.
+    $view->setCurrentPage(1);
+    $view->preview();
+
+    $counter = $view->style_plugin->getField(0, 'counter');
+    $this->assertEquals('2', $counter);
+    $view->destroy();
+
+    // Go the the third page.
+    $view->setCurrentPage(2);
+    $view->preview();
+
+    $counter = $view->style_plugin->getField(0, 'counter');
+    $this->assertEquals('3', $counter);
+
+    $view->destroy();
+
+    // Test using the counter start option.
+    $counter_start = 1000000;
+    $view->setDisplay();
+    $view->displayHandlers->get('default')->overrideOption('fields', [
+      'counter' => [
+        'id' => 'counter',
+        'table' => 'views',
+        'field' => 'counter',
+        'relationship' => 'none',
+        'counter_start' => $counter_start,
+      ],
+      'name' => [
+        'id' => 'name',
+        'table' => 'views_test_data',
+        'field' => 'name',
+        'relationship' => 'none',
+      ],
+    ]);
+
+    $view->preview();
+
+    $counter = $view->style_plugin->getField(0, 'counter');
+    $this->assertEquals($counter_start, $counter);
+    $view->destroy();
+
+    // Go the the second page.
+    $view->setCurrentPage(1);
+    $view->preview();
+
+    $counter = $view->style_plugin->getField(0, 'counter');
+    $this->assertEquals($counter_start + 1, $counter);
+    $view->destroy();
+
+    // Go the the third page.
+    $view->setCurrentPage(2);
+    $view->preview();
+
+    $counter = $view->style_plugin->getField(0, 'counter');
+    $this->assertEquals($counter_start + 2, $counter);
   }
 
 }
