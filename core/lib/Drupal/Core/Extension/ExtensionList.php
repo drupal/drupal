cda61a576427d7699ad78c92e711ca842920a826
Lee Rowlands <lee.rowlands@previousnext.com.au>
2019-09-11 19:46:01 +1000

    Issue #3079444 by jdmoreno: ExtensionList::reset() doesn't document its return value

--- core/lib/Drupal/Core/Extension/ExtensionList.php
+++ core/lib/Drupal/Core/Extension/ExtensionList.php
@@ -157,6 +157,8 @@ abstract class ExtensionList {
    * We don't reset statically added filenames, as it is a static cache which
    * logically can't change. This is done for performance reasons of the
    * installer.
+   *
+   * @return $this
    */
   public function reset() {
     $this->extensions = NULL;
