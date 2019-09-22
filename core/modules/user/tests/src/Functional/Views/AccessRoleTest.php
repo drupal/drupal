d7892190739be9c93c9dc794717cccba1ca3e2cc
Lee Rowlands <lee.rowlands@previousnext.com.au>
2019-09-23 08:38:07 +1000

    Issue #3082287 by alexpott: \Drupal\user\Plugin\views\access\Role::access() does not conform to the base class documentation
    (cherry picked from commit 6f29f980301099132c799ab2e3025b4efe14e75d)

--- core/modules/user/tests/src/Functional/Views/AccessRoleTest.php
+++ core/modules/user/tests/src/Functional/Views/AccessRoleTest.php
@@ -50,8 +50,8 @@ class AccessRoleTest extends AccessTestBase {
     $this->assertTrue($access_plugin instanceof Role, 'Make sure the right class got instantiated.');
 
     // Test the access() method on the access plugin.
-    $this->assertFalse($executable->display_handler->access($this->webUser));
-    $this->assertTrue($executable->display_handler->access($this->normalUser));
+    $this->assertSame(FALSE, $executable->display_handler->access($this->webUser));
+    $this->assertSame(TRUE, $executable->display_handler->access($this->normalUser));
 
     $this->drupalLogin($this->webUser);
     $this->drupalGet('test-role');
