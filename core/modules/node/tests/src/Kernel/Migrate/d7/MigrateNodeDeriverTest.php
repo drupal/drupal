9c171f8d33bd5bf29e26ae721c6f00a11177ee46
Alex Pott <alex.a.pott@googlemail.com>
2019-09-28 14:16:17 +0100

    Issue #3083196 by iyyappan.govind, quietone, mikelutz: Move d7\NodeMigrateDeriverTest::testBuilder to d7\MigrateNodeDeriverTest::testBuilder

--- core/modules/node/tests/src/Kernel/Migrate/d7/MigrateNodeDeriverTest.php
+++ core/modules/node/tests/src/Kernel/Migrate/d7/MigrateNodeDeriverTest.php
@@ -35,4 +35,28 @@ class MigrateNodeDeriverTest extends MigrateDrupal7TestBase {
     $this->assertTrue($this->container->get('plugin.manager.migration')->hasDefinition('d7_node_translation:article'), "Node translation migrations exist after content_translation installed");
   }
 
+  /**
+   * Tests the d7_node node driver.
+   *
+   * @group node
+   */
+  public function testBuilder() {
+    $process = $this->getMigration('d7_node:test_content_type')->getProcess();
+    $this->assertIdentical('field_boolean', $process['field_boolean'][0]['source']);
+    $this->assertIdentical('field_email', $process['field_email'][0]['source']);
+    $this->assertIdentical('field_phone', $process['field_phone'][0]['source']);
+    $this->assertIdentical('field_date', $process['field_date'][0]['source']);
+    $this->assertIdentical('field_date_with_end_time', $process['field_date_with_end_time'][0]['source']);
+    $this->assertIdentical('field_file', $process['field_file'][0]['source']);
+    $this->assertIdentical('field_float', $process['field_float'][0]['source']);
+    $this->assertIdentical('field_images', $process['field_images'][0]['source']);
+    $this->assertIdentical('field_integer', $process['field_integer'][0]['source']);
+    $this->assertIdentical('field_link', $process['field_link'][0]['source']);
+    $this->assertIdentical('field_text_list', $process['field_text_list'][0]['source']);
+    $this->assertIdentical('field_integer_list', $process['field_integer_list'][0]['source']);
+    $this->assertIdentical('field_long_text', $process['field_long_text'][0]['source']);
+    $this->assertIdentical('field_term_reference', $process['field_term_reference'][0]['source']);
+    $this->assertIdentical('field_text', $process['field_text'][0]['source']);
+  }
+
 }
