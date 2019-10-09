bd5908ad2e208f32c838d9b6016fe99cfb835ce9
xjm <xjm@65776.no-reply.drupal.org>
2019-10-09 06:09:51 -0500

    Revert "Issue #2863188 by acbramley, Erik Frèrejean, Maouna, andypost, init90, larowlan, sathish.redcrackle, vasi1186, chr.fritsch, tstoeckler, phjou, jibran, hchonov, alexpott, catch: Hardcoded result size limit in the entity reference autocomplete widget"
    This reverts commit 438f38397b32e2badfcd9f2cc2059e2758a64faa.

--- core/modules/jsonapi/tests/src/Functional/EntityFormDisplayTest.php
+++ core/modules/jsonapi/tests/src/Functional/EntityFormDisplayTest.php
@@ -139,7 +139,6 @@ class EntityFormDisplayTest extends ResourceTestBase {
               'weight' => 5,
               'settings' => [
                 'match_operator' => 'CONTAINS',
-                'match_limit' => 10,
                 'size' => 60,
                 'placeholder' => '',
               ],
