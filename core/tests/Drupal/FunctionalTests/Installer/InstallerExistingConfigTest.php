9286e7cefbbbbe1d92dee6e8b2e523f13331cdf4
Gábor Hojtsy <gabor@hojtsy.hu>
2019-10-15 17:32:40 +0200

    Issue #3088012 by alexpott, mikelutz, Wim Leers, Berdir: InstallerExistingConfigTest shouldn't download translations from l.d.o
    (cherry picked from commit 48cfce30c42766481ddda3275e1dcce3e7b1bb68)

--- core/tests/Drupal/FunctionalTests/Installer/InstallerExistingConfigTest.php
+++ core/tests/Drupal/FunctionalTests/Installer/InstallerExistingConfigTest.php
@@ -12,12 +12,22 @@ class InstallerExistingConfigTest extends InstallerExistingConfigTestBase {
   /**
    * {@inheritdoc}
    */
-  public function setUpSite() {
+  protected function setUpLanguage() {
+    // Place a custom local translation in the translations directory.
+    mkdir($this->root . '/' . $this->siteDirectory . '/files/translations', 0777, TRUE);
+    file_put_contents($this->root . '/' . $this->siteDirectory . '/files/translations/drupal-8.0.0.fr.po', "msgid \"\"\nmsgstr \"\"\nmsgid \"Save and continue\"\nmsgstr \"Enregistrer et continuer\"");
+    parent::setUpLanguage();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUpSettings() {
     // The configuration is from a site installed in French.
     // So after selecting the profile the installer detects that the site must
     // be installed in French, thus we change the button translation.
     $this->translations['Save and continue'] = 'Enregistrer et continuer';
-    parent::setUpSite();
+    parent::setUpSettings();
   }
 
   /**
