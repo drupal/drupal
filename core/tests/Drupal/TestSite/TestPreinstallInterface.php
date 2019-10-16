57dc6b65d983cf34d7e2c406ce1a20b68e957a81
Gábor Hojtsy <gabor@hojtsy.hu>
2019-10-16 11:59:08 +0200

    Issue #3088135 by alexpott, mikelutz, Wim Leers, Berdir, heddn: TestSiteApplicationTest::testInstallInDifferentLanguage requires localize.drupal.org
    (cherry picked from commit 8556a4d55f43c995163a115db7b51036867b2759)

---
+++ core/tests/Drupal/TestSite/TestPreinstallInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\TestSite;
+
+/**
+ * Allows running code prior to a test site install.
+ *
+ * @see \Drupal\TestSite\Commands\TestSiteInstallCommand
+ */
+interface TestPreinstallInterface {
+
+  /**
+   * Runs code prior to a test site install.
+   *
+   * This method is run after FunctionalTestSetupTrait::prepareEnvironment()
+   * but before Drupal is installed. As such, there is limited setup of the
+   * environment and no Drupal API is available.
+   *
+   * @param string $db_prefix
+   *   The database prefix.
+   * @param string $site_directory
+   *   The site directory.
+   *
+   * @see \Drupal\TestSite\TestSiteInstallTestScript
+   */
+  public function preinstall($db_prefix, $site_directory);
+
+}
