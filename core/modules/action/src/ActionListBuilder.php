fd49b9e7906034ebca8df5f323de0fc5b2a6dd7f
Alex Pott <alex.a.pott@googlemail.com>
2019-10-24 14:39:51 +0100

    Issue #3088195 by acbramley: action_entity_info incorrectly referenced in ActionListBuilder

--- core/modules/action/src/ActionListBuilder.php
+++ core/modules/action/src/ActionListBuilder.php
@@ -13,7 +13,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * Defines a class to build a listing of action entities.
  *
  * @see \Drupal\system\Entity\Action
- * @see action_entity_info()
+ * @see action_entity_type_build()
  */
 class ActionListBuilder extends ConfigEntityListBuilder {
 
