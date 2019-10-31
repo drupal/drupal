70addd7c8e4bacdd26a90b2ae057f51484bae412
Alex Pott <alex.a.pott@googlemail.com>
2019-10-31 14:43:43 +0100

    Issue #3087482 by Mile23, greg.1.anderson, AaronMcHale, johndevman, mbaynton, alexpott: Add 'next steps' info to Composer installation output

---
+++ composer/Plugin/ProjectMessage/MessagePlugin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\Composer\Plugin\ProjectMessage;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+/**
+ * A Composer plugin to display a message after creating a project.
+ */
+class MessagePlugin implements PluginInterface, EventSubscriberInterface {
+
+  /**
+   * Composer object.
+   *
+   * @var \Composer\Composer
+   */
+  protected $composer;
+
+  /**
+   * IO object.
+   *
+   * @var \Composer\IO\IOInterface
+   */
+  protected $io;
+
+  /**
+   * Configuration.
+   *
+   * @var \Drupal\Composer\VendorHardening\Config
+   */
+  protected $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function activate(Composer $composer, IOInterface $io) {
+    $this->composer = $composer;
+    $this->io = $io;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      ScriptEvents::POST_CREATE_PROJECT_CMD => 'displayPostCreateMessage',
+      ScriptEvents::POST_INSTALL_CMD => 'displayPostCreateMessage',
+    ];
+  }
+
+  public function displayPostCreateMessage(Event $event) {
+    $message = new Message($this->composer->getPackage(), $event->getName());
+    if ($message = $message->getText()) {
+      $this->io->write($message);
+    }
+  }
+
+}
