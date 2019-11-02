e8b6250313dae020a850b1bf45ac351f0ba98ea6
Alex Pott <alex.a.pott@googlemail.com>
2019-11-02 06:52:57 +0100

    Issue #3004929 by mikelutz, heddn: Fix 'The Drupal\migrate\Plugin\migrate\process\Migration is deprecated in Drupal 8.4.0 and will be removed before Drupal 9.0.0. Instead, use Drupal\migrate\Plugin\migrate\process\MigrationLookup'
    (cherry picked from commit efc4dd9796179248545b94cfd5b7e63170a3becb)

--- core/modules/block/src/Plugin/migrate/process/BlockPluginId.php
+++ core/modules/block/src/Plugin/migrate/process/BlockPluginId.php
@@ -4,6 +4,7 @@ namespace Drupal\block\Plugin\migrate\process;
 
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\MigrateLookupInterface;
 use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\Plugin\MigrateProcessInterface;
@@ -19,12 +20,26 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class BlockPluginId extends ProcessPluginBase implements ContainerFactoryPluginInterface {
 
   /**
-   * The migration process plugin, configured for lookups in d6_custom_block
-   * and d7_custom_block.
+   * The migration process plugin.
+   *
+   * The plugin is configured for lookups in d6_custom_block and
+   * d7_custom_block.
    *
    * @var \Drupal\migrate\Plugin\MigrateProcessInterface
+   *
+   * @deprecated in drupal:8.8.x and is removed from drupal:9.0.0. Use
+   *   the migrate.lookup service instead.
+   *
+   * @see https://www.drupal.org/node/3047268
    */
   protected $migrationPlugin;
+
+  /**
+   * The migrate lookup service.
+   *
+   * @var \Drupal\migrate\MigrateLookupInterface
+   */
+  protected $migrateLookup;
 
   /**
    * The block_content entity storage handler.
@@ -34,12 +49,32 @@ class BlockPluginId extends ProcessPluginBase implements ContainerFactoryPluginI
   protected $blockContentStorage;
 
   /**
-   * {@inheritdoc}
+   * Constructs a BlockPluginId object.
+   *
+   * @param array $configuration
+   *   The plugin configuration.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The block content storage object.
+   * @param \Drupal\migrate\MigrateLookupInterface $migrate_lookup
+   *   The migrate lookup service.
    */
-  public function __construct(array $configuration, $plugin_id, array $plugin_definition, EntityStorageInterface $storage, MigrateProcessInterface $migration_plugin) {
+  // @codingStandardsIgnoreLine
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, EntityStorageInterface $storage, $migrate_lookup) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
+    if ($migrate_lookup instanceof MigrateProcessInterface) {
+      @trigger_error('Passing a migration process plugin as the fifth argument to ' . __METHOD__ . ' is deprecated in drupal:8.8.0 and will throw an error in drupal:9.0.0. Pass the migrate.lookup service instead. See https://www.drupal.org/node/3047268', E_USER_DEPRECATED);
+      $this->migrationPlugin = $migrate_lookup;
+      $migrate_lookup = \Drupal::service('migrate.lookup');
+    }
+    elseif (!$migrate_lookup instanceof MigrateLookupInterface) {
+      throw new \InvalidArgumentException("The fifth argument to " . __METHOD__ . " must be an instance of MigrateLookupInterface.");
+    }
     $this->blockContentStorage = $storage;
-    $this->migrationPlugin = $migration_plugin;
+    $this->migrateLookup = $migrate_lookup;
   }
 
   /**
@@ -47,18 +82,12 @@ class BlockPluginId extends ProcessPluginBase implements ContainerFactoryPluginI
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
     $entity_type_manager = $container->get('entity_type.manager');
-    $migration_configuration = [
-      'migration' => [
-        'd6_custom_block',
-        'd7_custom_block',
-      ],
-    ];
     return new static(
       $configuration,
       $plugin_id,
       $plugin_definition,
       $entity_type_manager->getDefinition('block_content') ? $entity_type_manager->getStorage('block_content') : NULL,
-      $container->get('plugin.manager.migrate.process')->createInstance('migration', $migration_configuration, $migration)
+      $container->get('migrate.lookup')
     );
   }
 
@@ -77,17 +106,35 @@ class BlockPluginId extends ProcessPluginBase implements ContainerFactoryPluginI
             return 'aggregator_feed_block';
           }
           break;
+
         case 'menu':
           return "system_menu_block:$delta";
+
         case 'block':
           if ($this->blockContentStorage) {
-            $block_id = $this->migrationPlugin
-              ->transform($delta, $migrate_executable, $row, $destination_property);
-            if ($block_id) {
+            // This BC layer is included because if the plugin constructor was
+            // called in the legacy way with a migration_lookup process plugin,
+            // it  may have been preconfigured with a different migration to
+            // look up against. While this is unlikely, for maximum BC we will
+            // continue to use the plugin to do the lookup if it is provided,
+            // and support for this will be removed in Drupal 9.
+            if ($this->migrationPlugin) {
+              $block_id = $this->migrationPlugin
+                ->transform($delta, $migrate_executable, $row, $destination_property);
+            }
+            else {
+              $lookup_result = $this->migrateLookup->lookup(['d6_custom_block', 'd7_custom_block'], [$delta]);
+              if ($lookup_result) {
+                $block_id = $lookup_result[0]['id'];
+              }
+            }
+
+            if (!empty($block_id)) {
               return 'block_content:' . $this->blockContentStorage->load($block_id)->uuid();
             }
           }
           break;
+
         default:
           break;
       }
--- core/modules/filter/tests/src/Kernel/Migrate/d6/FilterFormatPermissionTest.php
+++ core/modules/filter/tests/src/Kernel/Migrate/d6/FilterFormatPermissionTest.php
@@ -3,7 +3,9 @@
 namespace Drupal\Tests\filter\Kernel\Migrate\d6;
 
 use Drupal\filter\Plugin\migrate\process\d6\FilterFormatPermission;
+use Drupal\migrate\Plugin\MigrateProcessInterface;
 use Drupal\migrate\Plugin\Migration;
+use Drupal\migrate\Plugin\MigrationInterface;
 use Drupal\Tests\migrate_drupal\Kernel\MigrateDrupalTestBase;
 
 /**
@@ -23,10 +25,21 @@ class FilterFormatPermissionTest extends MigrateDrupalTestBase {
   public function testConfigurableFilterFormat() {
     $migration = Migration::create($this->container, [], 'custom_migration', []);
     $filterFormatPermissionMigration = FilterFormatPermission::create($this->container, ['migration' => 'custom_filter_format'], 'custom_filter_format', [], $migration);
-    $migrationPlugin = $this->readAttribute($filterFormatPermissionMigration, 'migrationPlugin');
-    $config = $this->readAttribute($migrationPlugin, 'configuration');
-
+    $config = $this->readAttribute($filterFormatPermissionMigration, 'configuration');
     $this->assertEquals($config['migration'], 'custom_filter_format');
+  }
+
+  /**
+   * Tests legacy plugin usage.
+   *
+   * @group legacy
+   *
+   * @expectedDeprecation Passing a migration process plugin as the fourth argument to Drupal\filter\Plugin\migrate\process\d6\FilterFormatPermission::__construct is deprecated in drupal:8.8.0 and will throw an error in drupal:9.0.0. Pass the migrate.lookup service instead. See https://www.drupal.org/node/3047268
+   */
+  public function testLegacyConstruct() {
+    $process_plugin = $this->prophesize(MigrateProcessInterface::class)->reveal();
+    $plugin = new FilterFormatPermission([], '', [], $this->prophesize(MigrationInterface::class)->reveal(), $process_plugin);
+    $this->assertSame($process_plugin, $this->readAttribute($plugin, 'migrationPlugin'));
   }
 
 }
