9451d021eedad66e22fab83b0729292d59d2197e
Francesco Placella <plach@183211.no-reply.drupal.org>
2019-11-05 11:25:27 +0100

    Issue #2993642 by AdamPS, Pancho, jonathanshaw: Mechanism to disable preprocessing of base fields in taxonomy and aggregator entity types so they can be configured via the field UI

--- core/modules/aggregator/src/ItemViewBuilder.php
+++ core/modules/aggregator/src/ItemViewBuilder.php
@@ -19,7 +19,12 @@ class ItemViewBuilder extends EntityViewBuilder {
       $bundle = $entity->bundle();
       $display = $displays[$bundle];
 
-      if ($display->getComponent('description')) {
+      // By default, the description field is exposed as a pseudo-field
+      // rendered in this function. However it can optionally be rendered
+      // directly using a field formatter. Skip rendering here if a field
+      // formatter type is set.
+      $component = $display->getComponent('description');
+      if ($component && !isset($component['type'])) {
         $build[$id]['description'] = [
           '#markup' => $entity->getDescription(),
           '#allowed_tags' => _aggregator_allowed_tags(),
