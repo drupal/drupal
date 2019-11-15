5e9f0067eb7bcd5bbec1a504ad4cc6f484033494
Alex Pott <alex.a.pott@googlemail.com>
2019-11-15 09:35:00 +0000

    Issue #3055363 by hoanns, yogeshmpawar, alexpott, borisson_: EntityDefinitionUpdateManager: Show field_name when field has no label
    (cherry picked from commit f66921a6624b4020ee9931ad4738522e1334addb)

--- core/lib/Drupal/Core/Entity/EntityDefinitionUpdateManager.php
+++ core/lib/Drupal/Core/Entity/EntityDefinitionUpdateManager.php
@@ -150,15 +150,15 @@ class EntityDefinitionUpdateManager implements EntityDefinitionUpdateManagerInte
         foreach ($change_list['field_storage_definitions'] as $field_name => $change) {
           switch ($change) {
             case static::DEFINITION_CREATED:
-              $summary[$entity_type_id][] = $this->t('The %field_name field needs to be installed.', ['%field_name' => $storage_definitions[$field_name]->getLabel()]);
+              $summary[$entity_type_id][] = $this->t('The %field_name field needs to be installed.', ['%field_name' => $storage_definitions[$field_name]->getLabel() ?: $field_name]);
               break;
 
             case static::DEFINITION_UPDATED:
-              $summary[$entity_type_id][] = $this->t('The %field_name field needs to be updated.', ['%field_name' => $storage_definitions[$field_name]->getLabel()]);
+              $summary[$entity_type_id][] = $this->t('The %field_name field needs to be updated.', ['%field_name' => $storage_definitions[$field_name]->getLabel() ?: $field_name]);
               break;
 
             case static::DEFINITION_DELETED:
-              $summary[$entity_type_id][] = $this->t('The %field_name field needs to be uninstalled.', ['%field_name' => $original_storage_definitions[$field_name]->getLabel()]);
+              $summary[$entity_type_id][] = $this->t('The %field_name field needs to be uninstalled.', ['%field_name' => $original_storage_definitions[$field_name]->getLabel() ?: $field_name]);
               break;
           }
         }
