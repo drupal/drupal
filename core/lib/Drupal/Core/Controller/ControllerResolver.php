00d5589c22d3517ba2312eddd3c88caa6f246dc8
catch <catch@35733.no-reply.drupal.org>
2019-11-20 22:53:15 +0000

    Issue #2830631 by alexpott, cilefen, yunke, andypost: Remove code that tries to use _raw_variables for route argument resolution as it does not work
    (cherry picked from commit e669b4e863a08438169fb04b5703e8800fe4d2dd)

--- core/lib/Drupal/Core/Controller/ControllerResolver.php
+++ core/lib/Drupal/Core/Controller/ControllerResolver.php
@@ -131,13 +131,9 @@ class ControllerResolver extends BaseControllerResolver implements ControllerRes
     // to ensure it is removed in Drupal 9.
     @trigger_error(sprintf('%s is deprecated as of 8.6.0 and will be removed in 9.0. Inject the "http_kernel.controller.argument_resolver" service instead.', __METHOD__, ArgumentResolverInterface::class), E_USER_DEPRECATED);
     $attributes = $request->attributes->all();
-    $raw_parameters = $request->attributes->has('_raw_variables') ? $request->attributes->get('_raw_variables') : [];
     $arguments = [];
     foreach ($parameters as $param) {
       if (array_key_exists($param->name, $attributes)) {
-        $arguments[] = $attributes[$param->name];
-      }
-      elseif (array_key_exists($param->name, $raw_parameters)) {
         $arguments[] = $attributes[$param->name];
       }
       elseif ($param->getClass() && $param->getClass()->isInstance($request)) {
