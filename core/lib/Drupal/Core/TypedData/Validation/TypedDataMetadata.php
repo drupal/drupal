000c392060d839974a32ba19590c3fb295ee1783
Alex Pott <alex.a.pott@googlemail.com>
2019-11-22 14:15:50 +0000

    Issue #3093825 by Meenakshi.g, shashikant_chauhan, Chi: Remove TypedDataMetadata::accept() method
    (cherry picked from commit 7bfb9b723f10a610b335268e4bfe27ca0b3c00dc)

--- core/lib/Drupal/Core/TypedData/Validation/TypedDataMetadata.php
+++ core/lib/Drupal/Core/TypedData/Validation/TypedDataMetadata.php
@@ -3,11 +3,9 @@
 namespace Drupal\Core\TypedData\Validation;
 
 use Drupal\Core\TypedData\TypedDataInterface;
-use Symfony\Component\Validator\Exception\BadMethodCallException;
 use Symfony\Component\Validator\Mapping\CascadingStrategy;
 use Symfony\Component\Validator\Mapping\MetadataInterface;
 use Symfony\Component\Validator\Mapping\TraversalStrategy;
-use Symfony\Component\Validator\ValidationVisitorInterface;
 
 /**
  * Validator metadata for typed data objects.
@@ -31,13 +29,6 @@ class TypedDataMetadata implements MetadataInterface {
    */
   public function __construct(TypedDataInterface $typed_data) {
     $this->typedData = $typed_data;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function accept(ValidationVisitorInterface $visitor, $typed_data, $group, $propertyPath) {
-    throw new BadMethodCallException('Not supported.');
   }
 
   /**
