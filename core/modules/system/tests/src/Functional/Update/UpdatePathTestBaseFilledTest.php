ef54880529f6b5f275cec1649cd73c6012582a9e
catch <catch@35733.no-reply.drupal.org>
2019-11-25 12:39:51 +0000

    Issue #3089900 by jibran, alexpott, catch, Berdir, amateescu: Drupal 8.8/8.9/9.0 update test coverage
    (cherry picked from commit c2e3f0b3232d4d328b6530c9048ab5f87e96d4d7)

--- core/modules/system/tests/src/Functional/Update/UpdatePathTestBaseFilledTest.php
+++ core/modules/system/tests/src/Functional/Update/UpdatePathTestBaseFilledTest.php
@@ -26,7 +26,7 @@ class UpdatePathTestBaseFilledTest extends UpdatePathTestBaseTest {
    */
   protected function setDatabaseDumpFiles() {
     parent::setDatabaseDumpFiles();
-    $this->databaseDumpFiles[0] = __DIR__ . '/../../../../tests/fixtures/update/drupal-8.filled.standard.php.gz';
+    $this->databaseDumpFiles[0] = __DIR__ . '/../../../../tests/fixtures/update/drupal-8.8.0.filled.standard.php.gz';
   }
 
   /**
