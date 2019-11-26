b28eaa79ed6b9aa05be0247f4cbe5f7d1df4051f
Alex Pott <alex.a.pott@googlemail.com>
2019-11-26 17:09:00 +0000

    Issue #2941102 by gease, martin107, kfritsche, hchonov, alexpott: Form has become outdated after login (CSRF token race condition)

---
+++ core/modules/system/tests/modules/csrf_race_test/src/Controller/TestController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\csrf_race_test\Controller;
+
+use Drupal\Core\Access\CsrfTokenGenerator;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Controller to test concurrent CSRF token generation.
+ */
+class TestController extends ControllerBase {
+
+  /**
+   * Token generator service.
+   *
+   * @var \Drupal\Core\Access\CsrfTokenGenerator
+   */
+  protected $tokenGenerator;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('csrf_token')
+    );
+  }
+
+  /**
+   * Controller constructor.
+   */
+  public function __construct(CsrfTokenGenerator $token_generator) {
+    $this->tokenGenerator = $token_generator;
+  }
+
+  /**
+   * Helper page to load jQuery in test.
+   *
+   * @return array
+   *   Empty page with jQuery.
+   */
+  public function testMethod() {
+    return [
+      '#markup' => '',
+      '#attached' => [
+        'library' => 'core/jquery',
+      ],
+    ];
+  }
+
+  /**
+   * Just return generated CSRF token for concurrent requests.
+   *
+   * We delay the response to the first request to make sure the second request
+   * is made when the first is not yet finished.
+   *
+   * @return \Symfony\Component\HttpFoundation\Response
+   *   CSRF token.
+   */
+  public function getCsrfToken($num) {
+    sleep($num);
+    return new JsonResponse($this->tokenGenerator->get());
+  }
+
+}
