4e306db93d7e6021b2f87a21b830c44c01c469eb
Alex Pott <alex.a.pott@googlemail.com>
2019-11-27 22:23:22 +0000

    Issue #3092418 by ravi.shankar, Chi: Fix broken references in lanuage module
    (cherry picked from commit 3316724f0d2eeb30a010ecfec933fe9d45bdb14c)

--- core/modules/language/src/LanguageAccessControlHandler.php
+++ core/modules/language/src/LanguageAccessControlHandler.php
@@ -10,7 +10,7 @@ use Drupal\Core\Session\AccountInterface;
 /**
  * Defines the access control handler for the language entity type.
  *
- * @see \Drupal\language\Entity\Language
+ * @see \Drupal\language\Entity\ConfigurableLanguage
  */
 class LanguageAccessControlHandler extends EntityAccessControlHandler {
 
