7f779ce7b882a11034eb7e72a07ed68c5a7fb225
Alex Pott <alex.a.pott@googlemail.com>
2019-11-29 12:17:26 +0000

    Issue #3056617 by jcisio, tstoeckler, alexpott: NullStorage does not support collection
    (cherry picked from commit cd325f157a20348b47d3841238e8c807836714fc)

--- core/lib/Drupal/Core/Config/NullStorage.php
+++ core/lib/Drupal/Core/Config/NullStorage.php
@@ -19,6 +19,24 @@ namespace Drupal\Core\Config;
 class NullStorage implements StorageInterface {
 
   /**
+   * The storage collection.
+   *
+   * @var string
+   */
+  protected $collection;
+
+  /**
+   * Constructs a new NullStorage.
+   *
+   * @param string $collection
+   *   (optional) The collection to store configuration in. Defaults to the
+   *   default collection.
+   */
+  public function __construct($collection = StorageInterface::DEFAULT_COLLECTION) {
+    $this->collection = $collection;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function exists($name) {
@@ -92,13 +110,14 @@ class NullStorage implements StorageInterface {
    * {@inheritdoc}
    */
   public function createCollection($collection) {
-    // No op.
+    return new static($collection);
   }
 
   /**
    * {@inheritdoc}
    */
   public function getAllCollectionNames() {
+    // Returns only non empty collections.
     return [];
   }
 
@@ -106,7 +125,7 @@ class NullStorage implements StorageInterface {
    * {@inheritdoc}
    */
   public function getCollectionName() {
-    return '';
+    return $this->collection;
   }
 
 }
