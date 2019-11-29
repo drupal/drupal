3408494d71fc94cd16f180ff75a703db8554823a
catch <catch@35733.no-reply.drupal.org>
2019-11-29 15:52:14 +0000

    Issue #3097443 by andypost, longwave: Remove all @deprecated code in the Search module

--- core/modules/search/src/SearchPageListBuilder.php
+++ core/modules/search/src/SearchPageListBuilder.php
@@ -73,15 +73,11 @@ class SearchPageListBuilder extends DraggableListBuilder implements FormInterfac
    * @param \Drupal\search\SearchIndexInterface $search_index
    *   The search index.
    */
-  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, SearchPluginManager $search_manager, ConfigFactoryInterface $config_factory, MessengerInterface $messenger, SearchIndexInterface $search_index = NULL) {
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, SearchPluginManager $search_manager, ConfigFactoryInterface $config_factory, MessengerInterface $messenger, SearchIndexInterface $search_index) {
     parent::__construct($entity_type, $storage);
     $this->configFactory = $config_factory;
     $this->searchManager = $search_manager;
     $this->messenger = $messenger;
-    if (!$search_index) {
-      @trigger_error('Calling SearchPageListBuilder::__construct() without the $search_index argument is deprecated in drupal:8.8.0 and is required in drupal:9.0.0. See https://www.drupal.org/node/3075696', E_USER_DEPRECATED);
-      $search_index = \Drupal::service('search.index');
-    }
     $this->searchIndex = $search_index;
   }
 
