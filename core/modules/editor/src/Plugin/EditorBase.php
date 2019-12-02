6251b3edbdc7367ce46a88005b817389e197b2b8
catch <catch@35733.no-reply.drupal.org>
2019-12-02 11:35:04 +0000

    Issue #3092458 by Wim Leers, longwave, kim.pepper: Remove editor.module BC layers

--- core/modules/editor/src/Plugin/EditorBase.php
+++ core/modules/editor/src/Plugin/EditorBase.php
@@ -3,7 +3,6 @@
 namespace Drupal\editor\Plugin;
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Form\SubformStateInterface;
 use Drupal\Core\Plugin\PluginBase;
 
 /**
@@ -33,13 +32,6 @@ abstract class EditorBase extends PluginBase implements EditorPluginInterface {
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    if (method_exists($this, 'settingsForm')) {
-      @trigger_error(get_called_class() . "::settingsForm is deprecated since version 8.3.x. Rename the implementation 'buildConfigurationForm'. See https://www.drupal.org/node/2819753", E_USER_DEPRECATED);
-      if ($form_state instanceof SubformStateInterface) {
-        $form_state = $form_state->getCompleteFormState();
-      }
-      return $this->settingsForm($form, $form_state, $form_state->get('editor'));
-    }
     return $form;
   }
 
@@ -47,26 +39,12 @@ abstract class EditorBase extends PluginBase implements EditorPluginInterface {
    * {@inheritdoc}
    */
   public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
-    if (method_exists($this, 'settingsFormValidate')) {
-      @trigger_error(get_called_class() . "::settingsFormValidate is deprecated since version 8.3.x. Rename the implementation 'validateConfigurationForm'. See https://www.drupal.org/node/2819753", E_USER_DEPRECATED);
-      if ($form_state instanceof SubformStateInterface) {
-        $form_state = $form_state->getCompleteFormState();
-      }
-      $this->settingsFormValidate($form, $form_state);
-    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-    if (method_exists($this, 'settingsFormSubmit')) {
-      @trigger_error(get_called_class() . "::settingsFormSubmit is deprecated since version 8.3.x. Rename the implementation 'submitConfigurationForm'. See https://www.drupal.org/node/2819753", E_USER_DEPRECATED);
-      if ($form_state instanceof SubformStateInterface) {
-        $form_state = $form_state->getCompleteFormState();
-      }
-      $this->settingsFormSubmit($form, $form_state);
-    }
   }
 
 }
