75df2611dc65dc0993342a5ca46c10e0572765f8
Alex Pott <alex.a.pott@googlemail.com>
2020-01-22 11:13:21 +0000

    Issue #3064523 by andypost, Kartagis, Sutharsan: Add t() context to "Order" string for views module

--- core/modules/views/src/Plugin/views/sort/SortPluginBase.php
+++ core/modules/views/src/Plugin/views/sort/SortPluginBase.php
@@ -169,7 +169,7 @@ abstract class SortPluginBase extends HandlerBase implements CacheableDependency
     $options = $this->sortOptions();
     if (!empty($options)) {
       $form['order'] = [
-        '#title' => $this->t('Order'),
+        '#title' => $this->t('Order', [], ['context' => 'Sort order']),
         '#type' => 'radios',
         '#options' => $options,
         '#default_value' => $this->options['order'],
