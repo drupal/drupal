af399700a4fe94fc1ca35c3e3683078029ac0924
Alex Pott <alex.a.pott@googlemail.com>
2020-01-29 22:24:19 +0000

    Issue #3108109 by bnjmnm, lauriii: Decouple Classy libraries from Claro

---
+++ core/themes/claro/css/classy/components/link.css
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Style another element as a link.
+ */
+
+button.link {
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+  border: 0;
+  background: transparent;
+  font-size: 1em;
+}
+label button.link {
+  font-weight: bold;
+}
