96e280c8007034301a47305cc1533312d9ba4075
catch <catch@35733.no-reply.drupal.org>
2020-02-04 09:36:19 +0000

    Issue #3097751 by longwave, kim.pepper, Berdir: Remove all @deprecated code in the filter module

--- core/modules/filter/src/Plugin/migrate/process/d6/FilterFormatPermission.php
+++ core/modules/filter/src/Plugin/migrate/process/d6/FilterFormatPermission.php
@@ -3,10 +3,9 @@
 namespace Drupal\filter\Plugin\migrate\process\d6;
 
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\MigrateLookupInterface;
 use Drupal\migrate\Plugin\MigrationInterface;
-use Drupal\migrate\MigrateExecutableInterface;
-use Drupal\migrate\Plugin\MigrateProcessInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -20,18 +19,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class FilterFormatPermission extends ProcessPluginBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The Migration process plugin.
-   *
-   * @var \Drupal\migrate\Plugin\MigrateProcessInterface
-   *
-   * @deprecated in drupal:8.8.x and is removed from drupal:9.0.0. Use
-   *   the migrate.lookup service instead.
-   *
-   * @see https://www.drupal.org/node/3047268
-   */
-  protected $migrationPlugin;
 
   /**
    * The migrate lookup service.
@@ -54,20 +41,10 @@ class FilterFormatPermission extends ProcessPluginBase implements ContainerFacto
    * @param \Drupal\migrate\MigrateLookupInterface $migrate_lookup
    *   The migrate lookup service.
    */
-  // @codingStandardsIgnoreLine
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, $migrate_lookup) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration, MigrateLookupInterface $migrate_lookup) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
-    if ($migrate_lookup instanceof MigrateProcessInterface) {
-      @trigger_error('Passing a migration process plugin as the fourth argument to ' . __METHOD__ . ' is deprecated in drupal:8.8.0 and will throw an error in drupal:9.0.0. Pass the migrate.lookup service instead. See https://www.drupal.org/node/3047268', E_USER_DEPRECATED);
-      $this->migrationPlugin = $migrate_lookup;
-      $migrate_lookup = \Drupal::service('migrate.lookup');
-    }
-    elseif (!$migrate_lookup instanceof MigrateLookupInterface) {
-      throw new \InvalidArgumentException("The fifth argument to " . __METHOD__ . " must be an instance of MigrateLookupInterface.");
-    }
     $this->migration = $migration;
     $this->migrateLookup = $migrate_lookup;
-
   }
 
   /**
@@ -93,24 +70,9 @@ class FilterFormatPermission extends ProcessPluginBase implements ContainerFacto
     $migration = isset($this->configuration['migration']) ? $this->configuration['migration'] : 'd6_filter_format';
     if ($formats = $row->getSourceProperty("filter_permissions:$rid")) {
       foreach ($formats as $format) {
-
-        // This BC layer is included because if the plugin constructor was
-        // called in the legacy way with a migration_lookup process plugin, it
-        // may have been preconfigured with a different migration to look up
-        // against. While this is unlikely, for maximum BC we will continue to
-        // use the plugin to do the lookup if it is provided, and support for
-        // this will be removed in Drupal 9.
-        if ($this->migrationPlugin) {
-          $new_id = $this->migrationPlugin->transform($format, $migrate_executable, $row, $destination_property);
-        }
-        else {
-          $lookup_result = $this->migrateLookup->lookup($migration, [$format]);
-          if ($lookup_result) {
-            $new_id = $lookup_result[0]['format'];
-          }
-        }
-        if (!empty($new_id)) {
-          $value[] = 'use text format ' . $new_id;
+        $lookup_result = $this->migrateLookup->lookup($migration, [$format]);
+        if ($lookup_result) {
+          $value[] = 'use text format ' . $lookup_result[0]['format'];
         }
       }
     }
