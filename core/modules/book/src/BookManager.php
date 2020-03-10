0537629fa4a731cdd2553931e7b0efdd7888e394
Alex Pott <alex.a.pott@googlemail.com>
2020-03-10 23:31:00 +0000

    Issue #3010378 by benjifisher, piotrkonefal: BookManager::buildItems() is slow because it loads nodes
    (cherry picked from commit 702b8f1aa868e0760614a384d267b8d90b87f1f2)

--- core/modules/book/src/BookManager.php
+++ core/modules/book/src/BookManager.php
@@ -594,8 +594,7 @@ class BookManager implements BookManagerInterface {
       // Allow book-specific theme overrides.
       $element['attributes'] = new Attribute();
       $element['title'] = $data['link']['title'];
-      $node = $this->entityTypeManager->getStorage('node')->load($data['link']['nid']);
-      $element['url'] = $node->toUrl();
+      $element['url'] = 'entity:node/' . $data['link']['nid'];
       $element['localized_options'] = !empty($data['link']['localized_options']) ? $data['link']['localized_options'] : [];
       $element['localized_options']['set_active_class'] = TRUE;
       $element['below'] = $data['below'] ? $this->buildItems($data['below']) : [];
