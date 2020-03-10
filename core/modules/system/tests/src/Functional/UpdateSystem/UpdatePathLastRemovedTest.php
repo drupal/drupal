fd11c5a42cb026e7d51ef2112af83f28070f5f81
Alex Pott <alex.a.pott@googlemail.com>
2020-03-10 22:15:20 +0000

    Issue #3098475 by Berdir, catch, TravisCarden, xjm, benjifisher, alexpott, larowlan, webchick, dww: Add more strict checking of hook_update_last_removed() and better explanation

--- core/modules/system/tests/src/Functional/UpdateSystem/UpdatePathLastRemovedTest.php
+++ core/modules/system/tests/src/Functional/UpdateSystem/UpdatePathLastRemovedTest.php
@@ -47,21 +47,21 @@ class UpdatePathLastRemovedTest extends BrowserTestBase {
   }
 
   /**
-   * Test that a module with a too old schema version can not be updated.
+   * Tests that a module with a too old schema version can not be updated.
    */
   public function testLastRemovedVersion() {
     drupal_set_installed_schema_version('update_test_last_removed', 8000);
     drupal_set_installed_schema_version('system', 8804);
 
-    // Access the update page with too old versions for system and the test
-    // module, only the generic core message should be shown.
+    // Access the update page with a schema version that is too old for system
+    // and the test module, only the generic core message should be shown.
     $this->drupalLogin($this->updateUser);
     $this->drupalGet($this->updateUrl);
     $assert_session = $this->assertSession();
     $assert_session->pageTextContains('Requirements problem');
     $assert_session->pageTextContains('The version of Drupal you are trying to update from is too old');
     $assert_session->pageTextContains('Updating to Drupal 9 is only supported from Drupal version 8.8.0 or higher. If you are trying to update from an older version, first update to the latest version of Drupal 8');
-    $assert_session->pageTextNotContains('Unsupported schema version: Update test with update_last_removed implementation');
+    $assert_session->pageTextNotContains('Unsupported schema version: Update test with hook_update_last_removed() implementation');
 
     $assert_session->linkNotExists('Continue');
 
@@ -72,8 +72,8 @@ class UpdatePathLastRemovedTest extends BrowserTestBase {
 
     $assert_session->pageTextNotContains('The version of Drupal you are trying to update from is too old');
 
-    $assert_session->pageTextContains('Unsupported schema version: Update test with update_last_removed implementation');
-    $assert_session->pageTextContains('The installed version of the Update test with update_last_removed implementation module is too old to update. Update to an intermediate version first (last removed version: 8002, installed version: 8000).');
+    $assert_session->pageTextContains('Unsupported schema version: Update test with hook_update_last_removed() implementation');
+    $assert_session->pageTextContains('The installed version of the Update test with hook_update_last_removed() implementation module is too old to update. Update to an intermediate version first (last removed version: 8002, installed version: 8000).');
     $assert_session->linkNotExists('Continue');
 
     // Set the expected schema version for the node and test module, updates are
