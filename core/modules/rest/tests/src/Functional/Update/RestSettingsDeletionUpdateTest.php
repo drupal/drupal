ce1885d16c4a0420d8defcd39a516441ceb5ec59
catch <catch@35733.no-reply.drupal.org>
2020-03-10 22:26:23 +0000

    Issue #3119027 by Berdir, andypost: Use filled dump for RestSettingsDeletionUpdateTest

--- core/modules/rest/tests/src/Functional/Update/RestSettingsDeletionUpdateTest.php
+++ core/modules/rest/tests/src/Functional/Update/RestSettingsDeletionUpdateTest.php
@@ -16,8 +16,7 @@ class RestSettingsDeletionUpdateTest extends UpdatePathTestBase {
    */
   protected function setDatabaseDumpFiles() {
     $this->databaseDumpFiles = [
-      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-8.8.0.bare.standard.php.gz',
-      __DIR__ . '/../../../fixtures/update/rest-module-installed.php',
+      __DIR__ . '/../../../../../system/tests/fixtures/update/drupal-8.8.0.filled.standard.php.gz',
     ];
   }
 
@@ -25,9 +24,6 @@ class RestSettingsDeletionUpdateTest extends UpdatePathTestBase {
    * Ensures that update hook is run for "rest" module.
    */
   public function testUpdate() {
-    // @todo Remove this in https://www.drupal.org/project/drupal/issues/3095333.
-    \Drupal::entityDefinitionUpdateManager()->installEntityType(\Drupal::entityTypeManager()->getDefinition('rest_resource_config'));
-
     $rest_settings = $this->config('rest.settings');
     $this->assertFalse($rest_settings->isNew());
 
