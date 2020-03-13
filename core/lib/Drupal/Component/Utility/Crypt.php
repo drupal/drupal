d2d8fc93d73d16ddd063f4e7e081ad361e164337
Alex Pott <alex.a.pott@googlemail.com>
2020-03-13 22:34:35 +0000

    Issue #3118581 by kiamlaluno: The documentation for Crypt::randomBytesBase64() is wrong about the output length

--- core/lib/Drupal/Component/Utility/Crypt.php
+++ core/lib/Drupal/Component/Utility/Crypt.php
@@ -58,7 +58,8 @@ class Crypt {
    *   The number of random bytes to fetch and base64 encode.
    *
    * @return string
-   *   The base64 encoded result will have a length of up to 4 * $count.
+   *   A base-64 encoded string, with + replaced with -, / with _ and any =
+   *   padding characters removed.
    */
   public static function randomBytesBase64($count = 32) {
     return str_replace(['+', '/', '='], ['-', '_', ''], base64_encode(random_bytes($count)));
