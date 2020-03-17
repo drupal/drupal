b080f52432c538133ea1e3c41f4572d816c10086
Alex Pott <alex.a.pott@googlemail.com>
2020-03-17 12:16:25 +0000

    Revert "Issue #3103529 by alexpott, mcdruid, Chris Burge, greg.1.anderson, rfay, anavarre, catch, Gábor Hojtsy: Drupal 8.8.1+ and 9 can fail to install in the web browser due to cache pollution"
    This reverts commit ca13a977b0e2c13c79f37e6e93dbef94dc275913.

--- core/tests/Drupal/FunctionalTests/Installer/InstallerExistingSettingsTest.php
+++ core/tests/Drupal/FunctionalTests/Installer/InstallerExistingSettingsTest.php
@@ -55,17 +55,6 @@ class InstallerExistingSettingsTest extends InstallerTestBase {
   }
 
   /**
-   * Visits the interactive installer.
-   */
-  protected function visitInstaller() {
-    // Should redirect to the installer.
-    $this->drupalGet($GLOBALS['base_url']);
-    // Ensure no database tables have been created yet.
-    $this->assertSame([], Database::getConnection()->schema()->findTables('%'));
-    $this->assertSession()->addressEquals($GLOBALS['base_url'] . '/core/install.php');
-  }
-
-  /**
    * {@inheritdoc}
    */
   protected function setUpSettings() {
