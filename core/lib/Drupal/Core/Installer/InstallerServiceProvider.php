f14155d513dcfee029acd868e6f65795138c46c4
catch <catch@35733.no-reply.drupal.org>
2020-03-31 15:15:19 +0100

    Issue #2488350 by alexpott, Wim Leers, Dimiter, catch, dawehner, Berdir, webchick: Switch to a memory cache backend during installation

--- core/lib/Drupal/Core/Installer/InstallerServiceProvider.php
+++ core/lib/Drupal/Core/Installer/InstallerServiceProvider.php
@@ -14,7 +14,7 @@ use Symfony\Component\DependencyInjection\Reference;
  * $conf['container_service_providers'] and required to prevent various services
  * from trying to retrieve data from storages that do not exist yet.
  */
-class InstallerServiceProvider implements ServiceProviderInterface, ServiceModifierInterface {
+class InstallerServiceProvider extends NormalInstallerServiceProvider implements ServiceProviderInterface, ServiceModifierInterface {
 
   /**
    * {@inheritdoc}
@@ -26,10 +26,6 @@ class InstallerServiceProvider implements ServiceProviderInterface, ServiceModif
     $container->register('config.storage', 'Drupal\Core\Config\InstallStorage');
 
     // Replace services with in-memory implementations.
-    $definition = $container->getDefinition('cache_factory');
-    $definition->setClass('Drupal\Core\Cache\MemoryBackendFactory');
-    $definition->setArguments([]);
-    $definition->setMethodCalls([]);
     $container
       ->register('keyvalue', 'Drupal\Core\KeyValueStore\KeyValueMemoryFactory');
     $container
@@ -37,20 +33,12 @@ class InstallerServiceProvider implements ServiceProviderInterface, ServiceModif
 
     // Replace services with no-op implementations.
     $container
-      ->register('lock', 'Drupal\Core\Lock\NullLockBackend');
-    $container
       ->register('url_generator', 'Drupal\Core\Routing\NullGenerator')
       ->addArgument(new Reference('request_stack'));
     $container
       ->register('path_processor_manager', 'Drupal\Core\PathProcessor\NullPathProcessorManager');
     $container
       ->register('router.dumper', 'Drupal\Core\Routing\NullMatcherDumper');
-
-    // Remove the cache tags invalidator tag from the cache tags storage, so
-    // that we don't call it when cache tags are invalidated very early in the
-    // installer.
-    $container->getDefinition('cache_tags.invalidator.checksum')
-      ->clearTag('cache_tags_invalidator');
 
     // Replace the route builder with an empty implementation.
     // @todo Convert installer steps into routes; add an installer.routing.yml.
@@ -60,8 +48,7 @@ class InstallerServiceProvider implements ServiceProviderInterface, ServiceModif
       // we don't need to ship with a custom proxy class.
       ->setLazy(FALSE);
 
-    // Use a performance optimised module extension list.
-    $container->getDefinition('extension.list.module')->setClass('Drupal\Core\Installer\InstallerModuleExtensionList');
+    parent::register($container);
   }
 
   /**
--- core/lib/Drupal/Core/Installer/NormalInstallerServiceProvider.php
+++ core/lib/Drupal/Core/Installer/NormalInstallerServiceProvider.php
@@ -2,11 +2,23 @@
 
 namespace Drupal\Core\Installer;
 
+use Drupal\Core\Cache\MemoryBackendFactory;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
+use Drupal\Core\Lock\NullLockBackend;
 
 /**
- * Service provider for the non early installer environment.
+ * Service provider for the installer environment.
+ *
+ * This class is manually added by install_begin_request() via
+ * $conf['container_service_providers'] and optimizes the container for
+ * installation.
+ *
+ * Note that the early installer environment requires the further customisations
+ * in InstallerServiceProvider.
+ *
+ * @see install_begin_request()
+ * @see \Drupal\Core\Installer\InstallerServiceProvider
  */
 class NormalInstallerServiceProvider implements ServiceProviderInterface {
 
@@ -14,10 +26,29 @@ class NormalInstallerServiceProvider implements ServiceProviderInterface {
    * {@inheritdoc}
    */
   public function register(ContainerBuilder $container) {
-    // Use a performance optimised module extension list.
-    $container->getDefinition('extension.list.module')->setClass('Drupal\Core\Installer\InstallerModuleExtensionList');
-    $container->getDefinition('extension.list.theme')->setClass('Drupal\Core\Installer\InstallerThemeExtensionList');
-    $container->getDefinition('extension.list.theme_engine')->setClass('Drupal\Core\Installer\InstallerThemeEngineExtensionList');
+    // Replace cache services with in-memory implementations. The results in
+    // less queries to set caches which will only be cleared on the next module
+    // install.
+    $definition = $container->getDefinition('cache_factory');
+    $definition->setClass(MemoryBackendFactory::class);
+    $definition->setArguments([]);
+    $definition->setMethodCalls([]);
+
+    // Replace lock service with no-op implementation as Drupal installation can
+    // only occur in a single thread and the site should not be publicly
+    // available.
+    $container
+      ->register('lock', NullLockBackend::class);
+
+    // Remove the cache tags invalidator tag from the cache tags storage, so
+    // that we don't call it when cache tags are invalidated in the installer.
+    $container->getDefinition('cache_tags.invalidator.checksum')
+      ->clearTag('cache_tags_invalidator');
+
+    // Use performance-optimized extension lists.
+    $container->getDefinition('extension.list.module')->setClass(InstallerModuleExtensionList::class);
+    $container->getDefinition('extension.list.theme')->setClass(InstallerThemeExtensionList::class);
+    $container->getDefinition('extension.list.theme_engine')->setClass(InstallerThemeEngineExtensionList::class);
   }
 
 }
