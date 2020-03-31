3614f4d928e9eed0b976ad7484c99fa241b46c63
catch <catch@35733.no-reply.drupal.org>
2020-03-31 13:14:04 +0100

    Issue #3122511 by matsbla, neelam_wadhwani, jungle: Add missing link to delete form in local tasks for terms

--- core/modules/taxonomy/tests/src/Unit/Menu/TaxonomyLocalTasksTest.php
+++ core/modules/taxonomy/tests/src/Unit/Menu/TaxonomyLocalTasksTest.php
@@ -23,7 +23,7 @@ class TaxonomyLocalTasksTest extends LocalTaskIntegrationTestBase {
    */
   public function testTaxonomyPageLocalTasks($route, $subtask = []) {
     $tasks = [
-      0 => ['entity.taxonomy_term.canonical', 'entity.taxonomy_term.edit_form'],
+      0 => ['entity.taxonomy_term.canonical', 'entity.taxonomy_term.edit_form', 'entity.taxonomy_term.delete_form'],
     ];
     if ($subtask) {
       $tasks[] = $subtask;
