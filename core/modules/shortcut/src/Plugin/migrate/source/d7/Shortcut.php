2ee60874c5221c92c5e18fe4208a9fda4cbd9935
catch <catch@35733.no-reply.drupal.org>
2020-04-17 12:25:31 +0100

    Issue #3127960 by Kartagis: Incorrectly put underscore in string
    (cherry picked from commit a4ddcd27c6e39bd1c34c46459a0ab1c2aef553c6)

--- core/modules/shortcut/src/Plugin/migrate/source/d7/Shortcut.php
+++ core/modules/shortcut/src/Plugin/migrate/source/d7/Shortcut.php
@@ -31,7 +31,7 @@ class Shortcut extends DrupalSqlBase {
   public function fields() {
     return [
       'mlid' => $this->t("The menu.mlid primary key for this menu item (= shortcut link)."),
-      'menu_name' => $this->t("The menu_name (= set name) for this shortcut link."),
+      'menu_name' => $this->t("The menu name (= set name) for this shortcut link."),
       'link_path' => $this->t("The link for this shortcut."),
       'link_title' => $this->t("The title for this shortcut."),
       'weight' => $this->t("The weight for this shortcut"),
