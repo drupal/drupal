023ef0412cd4003bc8ca92b360d03c1a12f37eb9
Alex Pott <alex.a.pott@googlemail.com>
2020-05-02 13:11:18 +0100

    Issue #3131258 by longwave, mondrake, dww: Remove redundant $message from assertFile(Not)Exists and assertDirectory(Not)Exists calls

--- core/modules/file/tests/src/Functional/FileFieldAnonymousSubmissionTest.php
+++ core/modules/file/tests/src/Functional/FileFieldAnonymousSubmissionTest.php
@@ -93,7 +93,7 @@ class FileFieldAnonymousSubmissionTest extends FileFieldTestBase {
       $this->assertNotEqual($nid, 0, 'The node ID was extracted from the URL.');
       $node = Node::load($nid);
       $this->assertNotEqual($node, NULL, 'The node was loaded successfully.');
-      $this->assertFileExists(File::load($node->field_image->target_id)->getFileUri(), 'The image was uploaded successfully.');
+      $this->assertFileExists(File::load($node->field_image->target_id)->getFileUri());
     }
   }
 
@@ -167,7 +167,7 @@ class FileFieldAnonymousSubmissionTest extends FileFieldTestBase {
       $this->assertNotEqual($nid, 0, 'The node ID was extracted from the URL.');
       $node = Node::load($nid);
       $this->assertNotEqual($node, NULL, 'The node was loaded successfully.');
-      $this->assertFileExists(File::load($node->field_image->target_id)->getFileUri(), 'The image was uploaded successfully.');
+      $this->assertFileExists(File::load($node->field_image->target_id)->getFileUri());
     }
   }
 
