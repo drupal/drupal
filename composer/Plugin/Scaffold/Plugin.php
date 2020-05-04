349ae785257efd9bc701b17261174067bee124b0
catch <catch@35733.no-reply.drupal.org>
2020-05-04 11:05:38 +0100

    Issue #3126566 by greg.1.anderson, jungle, tedbow, hussainweb, Kristen Pol, alexpott: Allow Drupal to work with Composer 2

--- composer/Plugin/Scaffold/Plugin.php
+++ composer/Plugin/Scaffold/Plugin.php
@@ -63,6 +63,18 @@ class Plugin implements PluginInterface, EventSubscriberInterface, Capable {
   /**
    * {@inheritdoc}
    */
+  public function deactivate(Composer $composer, IOInterface $io) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function uninstall(Composer $composer, IOInterface $io) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getCapabilities() {
     return [CommandProvider::class => ScaffoldCommandProvider::class];
   }
