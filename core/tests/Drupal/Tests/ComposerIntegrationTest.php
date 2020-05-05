ed61692243cac43a835362f65baeb4e280785448
xjm <xjm@65776.no-reply.drupal.org>
2020-05-05 07:53:37 -0500

    Issue #3032686 by longwave, jibran, xjm, alexpott, Berdir, amateescu: Remove references to unused packages in Drupal 9's vendor hardening
    (cherry picked from commit 572e554aa94f450d56aeecf97fc0a8d09346eecc)

--- core/tests/Drupal/Tests/ComposerIntegrationTest.php
+++ core/tests/Drupal/Tests/ComposerIntegrationTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests;
 
+use Drupal\Composer\Plugin\VendorHardening\Config;
+use Drupal\Core\Composer\Composer;
 use Drupal\Tests\Composer\ComposerIntegrationTrait;
 use Symfony\Component\Yaml\Yaml;
 
@@ -240,5 +242,37 @@ class ComposerIntegrationTest extends UnitTestCase {
     return md5(json_encode($relevantContent));
   }
   // @codingStandardsIgnoreEnd
+
+  /**
+   * Tests the vendor cleanup utilities do not have obsolete packages listed.
+   *
+   * @dataProvider providerTestVendorCleanup
+   */
+  public function testVendorCleanup($class, $property) {
+    $lock = json_decode(file_get_contents($this->root . '/composer.lock'), TRUE);
+    $packages = [];
+    foreach (array_merge($lock['packages'], $lock['packages-dev']) as $package) {
+      $packages[] = $package['name'];
+    }
+
+    $reflection = new \ReflectionProperty($class, $property);
+    $reflection->setAccessible(TRUE);
+    $config = $reflection->getValue();
+    foreach (array_keys($config) as $package) {
+      $this->assertContains(strtolower($package), $packages);
+    }
+  }
+
+  /**
+   * Data provider for the vendor cleanup utility classes.
+   *
+   * @return array[]
+   */
+  public function providerTestVendorCleanup() {
+    return [
+      [Composer::class, 'packageToCleanup'],
+      [Config::class, 'defaultConfig'],
+    ];
+  }
 
 }
