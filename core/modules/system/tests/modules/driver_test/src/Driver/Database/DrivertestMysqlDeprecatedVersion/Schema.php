19e17d5570b61718734da56539138432c8670a61
catch <catch@35733.no-reply.drupal.org>
2020-05-14 15:25:41 +0100

    Issue #3135629 by alexpott, dww, daffie, jungle, Rkumar, balsama, xjm: Minimum MySQL version requirement is not confirmed when upgrading existing sites from Drupal 8 to Drupal 9
    (cherry picked from commit cefc84f9b4d16fd1a8e8f756d9620aa6ec95fa7e)

---
+++ core/modules/system/tests/modules/driver_test/src/Driver/Database/DrivertestMysqlDeprecatedVersion/Schema.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\driver_test\Driver\Database\DrivertestMysqlDeprecatedVersion;
+
+use Drupal\Core\Database\Driver\mysql\Schema as CoreSchema;
+
+/**
+ * MySQL test implementation of \Drupal\Core\Database\Schema.
+ */
+class Schema extends CoreSchema {}
---
+++ core/modules/system/tests/modules/driver_test/src/Driver/Database/DrivertestMysqlDeprecatedVersion/Upsert.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\driver_test\Driver\Database\DrivertestMysqlDeprecatedVersion;
+
+use Drupal\Core\Database\Driver\mysql\Upsert as CoreUpsert;
+
+/**
+ * MySQL test implementation of \Drupal\Core\Database\Query\Upsert.
+ */
+class Upsert extends CoreUpsert {}
