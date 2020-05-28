9800cd529af201f5835ffdaa683d0b676109e682
catch <catch@35733.no-reply.drupal.org>
2020-05-28 12:09:12 +0100

    Issue #1831560 by acbramley, CRZDEV, mvwensen, fago, David_Rothstein, harsha012, Lendude, mrinalini9, Devin Carlson, borisson_, Gábor Hojtsy: Remove Html::resetSeenIds() call during form processing
    (cherry picked from commit bc62d349ba1547c5def8609df6cfe3317e6df376)

--- core/lib/Drupal/Core/Form/FormBuilder.php
+++ core/lib/Drupal/Core/Form/FormBuilder.php
@@ -385,6 +385,17 @@ class FormBuilder implements FormBuilderInterface, FormValidatorInterface, FormS
       $form_state->setCached();
     }
 
+    // \Drupal\Component\Utility\Html::getUniqueId() maintains a cache of
+    // element IDs it has seen, so it can prevent duplicates. We want to be
+    // sure we reset that cache when a form is processed, so scenarios that
+    // result in the form being built behind the scenes and again for the
+    // browser don't increment all the element IDs needlessly.
+    if (!FormState::hasAnyErrors()) {
+      // We only reset HTML ID's when there are no validation errors as this can
+      // cause ID collisions with other forms on the page otherwise.
+      Html::resetSeenIds();
+    }
+
     // If only parts of the form will be returned to the browser (e.g., Ajax or
     // RIA clients), or if the form already had a new build ID regenerated when
     // it was retrieved from the form cache, reuse the existing #build_id.
@@ -575,16 +586,6 @@ class FormBuilder implements FormBuilderInterface, FormValidatorInterface, FormS
         $form_state->setTriggeringElement(reset($buttons));
       }
       $this->formValidator->validateForm($form_id, $form, $form_state);
-
-      // \Drupal\Component\Utility\Html::getUniqueId() maintains a cache of
-      // element IDs it has seen, so it can prevent duplicates. We want to be
-      // sure we reset that cache when a form is processed, so scenarios that
-      // result in the form being built behind the scenes and again for the
-      // browser don't increment all the element IDs needlessly.
-      if (!FormState::hasAnyErrors()) {
-        // In case of errors, do not break HTML IDs of other forms.
-        Html::resetSeenIds();
-      }
 
       // If there are no errors and the form is not rebuilding, submit the form.
       if (!$form_state->isRebuilding() && !FormState::hasAnyErrors()) {
