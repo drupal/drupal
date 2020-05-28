c9c0fc1e590a005976af811e6f833d4d17dd91fc
Alex Pott <alex.a.pott@googlemail.com>
2020-05-28 14:58:37 +0100

    Issue #3133798 by Beakerboy, daffie: Semicolon removed from query even when it is allowed

--- core/lib/Drupal/Core/Database/Connection.php
+++ core/lib/Drupal/Core/Database/Connection.php
@@ -725,8 +725,12 @@ abstract class Connection {
         // semicolon) is not allowed unless the option is set.  Allowing
         // semicolons should only be needed for special cases like defining a
         // function or stored procedure in SQL. Trim any trailing delimiter to
-        // minimize false positives.
-        $query = rtrim($query, ";  \t\n\r\0\x0B");
+        // minimize false positives unless delimiter is allowed.
+        $trim_chars = "  \t\n\r\0\x0B";
+        if (empty($options['allow_delimiter_in_query'])) {
+          $trim_chars .= ';';
+        }
+        $query = rtrim($query, $trim_chars);
         if (strpos($query, ';') !== FALSE && empty($options['allow_delimiter_in_query'])) {
           throw new \InvalidArgumentException('; is not supported in SQL strings. Use only one statement at a time.');
         }
