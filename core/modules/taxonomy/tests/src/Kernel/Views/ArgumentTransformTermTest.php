0946a51ccd5d8dd09de412f2a3cd8d303d2c1125
Alex Pott <alex.a.pott@googlemail.com>
2020-05-30 21:22:37 +0100

    Issue #2710407 by bobbygryzynger, dww, Kingdutch, Krzysztof Domański, jp.stacey, johnny5th, tea.time, joachim, andileco, Lendude, xjm, percoction: Option for 'Transform dashes in URL to spaces in term name filter values' on term arguments doesn't affect the query
    (cherry picked from commit 75d67d4ce8deafee645bb29387c0969d93a62da7)

---
+++ core/modules/taxonomy/tests/src/Kernel/Views/ArgumentTransformTermTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\Tests\taxonomy\Kernel\Views;
+
+use Drupal\views\Views;
+
+/**
+ * Tests taxonomy term argument transformation.
+ *
+ * @group taxonomy
+ *
+ * @see \Drupal\taxonomy\Plugin\views\argument_validator\TermName
+ */
+class ArgumentTransformTermTest extends TaxonomyTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $testViews = ['test_argument_transform_term'];
+
+  /**
+   * Tests term argument transformation of hyphens and spaces.
+   *
+   * @dataProvider termArgumentTransformationProvider
+   *
+   * @param string $name
+   *   The name of the taxonomy term to use for the test.
+   */
+  public function testTermArgumentTransformation($name) {
+    /** @var \Drupal\taxonomy\TermInterface $term */
+    $term = $this->createTerm(['name' => $name]);
+
+    /** @var \Drupal\views\ViewExecutable $view */
+    $view = Views::getView('test_argument_transform_term');
+    $view->initHandlers();
+
+    /** @var string $hyphenated_term */
+    $hyphenated_term = str_replace(' ', '-', $term->label());
+    $this->assertTrue($view->argument['tid']->setArgument($hyphenated_term));
+    // Assert hyphens are converted back to spaces.
+    $this->assertEquals($term->label(), $view->argument['tid']->argument);
+  }
+
+  /**
+   * Provides data for testTermArgumentTransformation().
+   *
+   * @return array[]
+   *   Test data.
+   */
+  public function termArgumentTransformationProvider() {
+    return [
+      'space in the middle' => [
+        'name' => $this->randomMachineName() . ' ' . $this->randomMachineName(),
+      ],
+      'space at the start' => [
+        'name' => ' ' . $this->randomMachineName(),
+      ],
+      'space at the end' => [
+        'name' => $this->randomMachineName() . ' ',
+      ],
+    ];
+  }
+
+}
