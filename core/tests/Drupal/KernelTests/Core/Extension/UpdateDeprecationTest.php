0334414b314b6b66622b7edb9544c956ec175f4c
Alex Pott <alex.a.pott@googlemail.com>
2020-06-23 11:34:39 +0100

    Issue #3150726 by andypost: Deprecate update_check_incompatibility as unused

---
+++ core/tests/Drupal/KernelTests/Core/Extension/UpdateDeprecationTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\KernelTests\Core\Extension;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests deprecated update.inc functions.
+ *
+ * @group legacy
+ * @group extension
+ */
+class UpdateDeprecationTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // Include the legacy update.inc file.
+    include_once $this->root . '/core/includes/update.inc';
+  }
+
+  /**
+   * Tests update_check_incompatibility() function.
+   *
+   * @expectedDeprecation update_check_incompatibility() is deprecated in drupal:9.1.0 and is removed from drupal:10.0.0. No direct replacement is provided. See https://www.drupal.org/node/3150727
+   */
+  public function testUpdateCheckIncompatibility() {
+    $this->assertTrue(update_check_incompatibility('incompatible_module'));
+    $this->assertFalse(update_check_incompatibility('system'));
+  }
+
+}
