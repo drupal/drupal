929f86c32423d4f08010548e803b59012871d793
xjm <xjm@65776.no-reply.drupal.org>
2020-06-23 00:16:46 -0500

    Issue #3138791 by jungle, sja112, xjm, mohrerao: Fix "bubbleable" relevant typos in core
    (cherry picked from commit fb33384dc66e122980e38b3d7c33ce79356b207e)

--- core/modules/system/tests/src/Functional/System/TokenReplaceWebTest.php
+++ core/modules/system/tests/src/Functional/System/TokenReplaceWebTest.php
@@ -38,7 +38,7 @@ class TokenReplaceWebTest extends BrowserTestBase {
     $this->assertCacheTags(['node:1', 'rendered', 'user:2']);
     $this->assertCacheContexts(['languages:language_interface', 'theme', 'url.query_args:' . MainContentViewSubscriber::WRAPPER_FORMAT, 'user']);
 
-    $this->drupalGet('token-test-without-bubleable-metadata/' . $node->id());
+    $this->drupalGet('token-test-without-bubbleable-metadata/' . $node->id());
     $this->assertText("Tokens: {$node->id()} {$account->id()}");
     $this->assertCacheTags(['node:1', 'rendered', 'user:2']);
     $this->assertCacheContexts(['languages:language_interface', 'theme', 'url.query_args:' . MainContentViewSubscriber::WRAPPER_FORMAT, 'user']);
