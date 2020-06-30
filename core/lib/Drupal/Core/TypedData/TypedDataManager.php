d5a8ea01e175052b5a9e7f84f06070975a387f7b
catch <catch@35733.no-reply.drupal.org>
2020-06-30 14:34:57 +0100

    Issue #3142893 by hchonov, Raunak.singh, kishor_kolekar, alexpott, tstoeckler, catch, kfritsche, johnwebdev: Memory leak - typed data prototypes for field items are not re-used like intended
    (cherry picked from commit ce5d85281b53bb683c6e0c9cdd27a14368584091)

--- core/lib/Drupal/Core/TypedData/TypedDataManager.php
+++ core/lib/Drupal/Core/TypedData/TypedDataManager.php
@@ -169,7 +169,15 @@ class TypedDataManager extends DefaultPluginManager implements TypedDataManagerI
       $parts[] = json_encode($settings);
     }
     // Property path for the requested data object.
-    $parts[] = $object->getPropertyPath() . '.' . $property_name;
+    $parts[] = $object->getPropertyPath();
+    // Only property instances of complex data types should be cached by the
+    // property name, as they represent different properties. Properties of list
+    // data types are the items of the list and the property name represents
+    // only the delta in that list and not an unique property, which is why all
+    // items should use the same prototype.
+    if ($object instanceof ComplexDataInterface) {
+      $parts[] = $property_name;
+    }
     $key = implode(':', $parts);
 
     // Create the prototype if needed.
