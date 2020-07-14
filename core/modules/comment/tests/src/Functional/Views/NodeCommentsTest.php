08b33b573141da6c423a29d62148d261a8dc868a
Alex Pott <alex.a.pott@googlemail.com>
2020-07-14 17:03:33 +0100

    Issue #2801929 by Lendude, ridhimaabrol24, Sutharsan, gilles.koffmann, geek-merlin, dawehner: View loses records after adding comment count field
    (cherry picked from commit 0ee1b660dc1638bfa4ab902bd5831548b2dc162f)

--- core/modules/comment/tests/src/Functional/Views/NodeCommentsTest.php
+++ core/modules/comment/tests/src/Functional/Views/NodeCommentsTest.php
@@ -26,7 +26,7 @@ class NodeCommentsTest extends CommentTestBase {
    *
    * @var array
    */
-  public static $testViews = ['test_new_comments'];
+  public static $testViews = ['test_new_comments', 'test_comment_count'];
 
   /**
    * Test the new comments field plugin.
@@ -36,6 +36,32 @@ class NodeCommentsTest extends CommentTestBase {
     $this->assertSession()->statusCodeEquals(200);
     $new_comments = $this->cssSelect(".views-field-new-comments a:contains('1')");
     $this->assertCount(1, $new_comments, 'Found the number of new comments for a certain node.');
+  }
+
+  /**
+   * Test the comment count field.
+   */
+  public function testCommentCount() {
+    $this->drupalGet('test-comment-count');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertCount(2, $this->cssSelect('.views-row'));
+    $comment_count_with_comment = $this->cssSelect(".views-field-comment-count span:contains('1')");
+    $this->assertCount(1, $comment_count_with_comment);
+    $comment_count_without_comment = $this->cssSelect(".views-field-comment-count span:contains('0')");
+    $this->assertCount(1, $comment_count_without_comment);
+
+    // Create a content type with no comment field, and add a node.
+    $this->drupalCreateContentType(['type' => 'no_comment', 'name' => t('No comment page')]);
+    $this->nodeUserPosted = $this->drupalCreateNode(['type' => 'no_comment']);
+    $this->drupalGet('test-comment-count');
+
+    // Test that the node with no comment field is also shown.
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertCount(3, $this->cssSelect('.views-row'));
+    $comment_count_with_comment = $this->cssSelect(".views-field-comment-count span:contains('1')");
+    $this->assertCount(1, $comment_count_with_comment);
+    $comment_count_without_comment = $this->cssSelect(".views-field-comment-count span:contains('0')");
+    $this->assertCount(2, $comment_count_without_comment);
   }
 
 }
