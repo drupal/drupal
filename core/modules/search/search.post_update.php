920d1708c9b21c91d063df189075c429aa438fd3
catch <catch@35733.no-reply.drupal.org>
2020-07-14 13:16:26 +0100

    Issue #3151364 by Charlie ChX Negyesi, amateescu, alexpott, jhodgdon: diacritics are not removed from ǢǣǼǽǮǯ

--- core/modules/search/search.post_update.php
+++ core/modules/search/search.post_update.php
@@ -13,3 +13,14 @@ function search_removed_post_updates() {
     'search_post_update_block_page' => '9.0.0',
   ];
 }
+
+/**
+ * Mark everything for reindexing after diacritics removal rule change.
+ */
+function search_post_update_reindex_after_diacritics_rule_change() {
+  $search_page_repository = \Drupal::service('search.search_page_repository');
+  foreach ($search_page_repository->getIndexableSearchPages() as $entity) {
+    $entity->getPlugin()->markForReindex();
+  }
+  return t("Content has been marked for re-indexing for all active search pages. Searching will continue to work, but new content won't be indexed until all existing content has been re-indexed.");
+}
