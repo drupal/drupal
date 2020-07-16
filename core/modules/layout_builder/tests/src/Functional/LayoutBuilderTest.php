761fd76a293e66a16397001cc78013bf0aba5fbe
Alex Pott <alex.a.pott@googlemail.com>
2020-07-16 22:28:11 +0100

    Issue #3091309 by tim.plunkett, godotislate, paulocs, thursday_bw, alexpott, TwiiK: Broken context-aware block plugins throw an unexpected exception
    (cherry picked from commit 57af0a04c142695ee239972361528b942f705808)

--- core/modules/layout_builder/tests/src/Functional/LayoutBuilderTest.php
+++ core/modules/layout_builder/tests/src/Functional/LayoutBuilderTest.php
@@ -68,6 +68,39 @@ class LayoutBuilderTest extends BrowserTestBase {
   }
 
   /**
+   * Tests deleting a field in-use by an overridden layout.
+   */
+  public function testDeleteField() {
+    $assert_session = $this->assertSession();
+    $page = $this->getSession()->getPage();
+
+    $this->drupalLogin($this->drupalCreateUser([
+      'configure any layout',
+      'administer node fields',
+    ]));
+
+    // Enable layout builder overrides.
+    LayoutBuilderEntityViewDisplay::load('node.bundle_with_section_field.default')
+      ->enableLayoutBuilder()
+      ->setOverridable()
+      ->save();
+
+    // Ensure there is a layout override.
+    $this->drupalGet('node/1/layout');
+    $page->pressButton('Save layout');
+
+    // Delete one of the fields in use.
+    $this->drupalGet('admin/structure/types/manage/bundle_with_section_field/fields/node.bundle_with_section_field.body/delete');
+    $page->pressButton('Delete');
+
+    // The node should still be accessible.
+    $this->drupalGet('node/1');
+    $assert_session->statusCodeEquals(200);
+    $this->drupalGet('node/1/layout');
+    $assert_session->statusCodeEquals(200);
+  }
+
+  /**
    * Tests Layout Builder overrides without access to edit the default layout.
    */
   public function testOverridesWithoutDefaultsAccess() {
