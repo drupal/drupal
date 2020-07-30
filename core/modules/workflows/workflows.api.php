537e1903182f2b15cb61d2dcdcc5ac9935eeec20
catch <catch@35733.no-reply.drupal.org>
2020-07-30 09:08:09 +0100

    Issue #3022551 by kiamlaluno, oknate, govind.maloo: The definite article is wrongly placed after a possessive pronoun

--- core/modules/workflows/workflows.api.php
+++ core/modules/workflows/workflows.api.php
@@ -8,10 +8,11 @@
 /**
  * @defgroup workflow_type_plugins Workflow Type Plugins
  * @{
- * Any module harnessing Workflows module must define a Workflow Type Plugin.
- * This allows the module to tailor the workflow to its specific need. For
- * example, Content Moderation module uses its the Workflow Type Plugin to link
- * workflows to entities. On their own, workflows are a stand-alone concept. It
- * takes a module such as Content Moderation to give the workflow context.
+ * Any module harnessing the Workflows module must define a Workflow Type
+ * Plugin. This allows the module to tailor the workflow to its specific need.
+ * For example, the Content Moderation module uses its Workflow Type Plugin to
+ * link workflows to entities.
+ * On their own, workflows are a stand-alone concept. It takes a module such as
+ * Content Moderation to give the workflow context.
  * @}
  */
