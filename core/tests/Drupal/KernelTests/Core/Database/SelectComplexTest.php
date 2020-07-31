bcc48076e322e19dacb5849329174801bcef5887
Alex Pott <alex.a.pott@googlemail.com>
2020-07-31 15:55:38 +0100

    Issue #3158270 by Hardik_Patel_12, siddhant.bhosale, paulocs: Unused local variables in SelectComplexTest file
    (cherry picked from commit fde0132b020176c5d473d0aa609d9b8110534eff)

--- core/tests/Drupal/KernelTests/Core/Database/SelectComplexTest.php
+++ core/tests/Drupal/KernelTests/Core/Database/SelectComplexTest.php
@@ -161,8 +161,8 @@ class SelectComplexTest extends DatabaseTestBase {
    */
   public function testRangeUndo() {
     $query = $this->connection->select('test');
-    $name_field = $query->addField('test', 'name');
-    $age_field = $query->addField('test', 'age', 'age');
+    $query->addField('test', 'name');
+    $query->addField('test', 'age', 'age');
     $query->range(0, 2);
     $query->range(NULL, NULL);
     $query_result = $query->countQuery()->execute()->fetchField();
