610682e945ff6376fd7f2d2cef297a1d60bafd99
Alex Pott <alex.a.pott@googlemail.com>
2020-07-31 15:19:13 +0100

    Issue #3099662 by jibran, dpi, jptaranto, larowlan, nod_: Allow ckeditor_stylesheets to refer to a Drupal root URL

--- core/modules/ckeditor/tests/src/Functional/CKEditorLoadingTest.php
+++ core/modules/ckeditor/tests/src/Functional/CKEditorLoadingTest.php
@@ -242,6 +242,14 @@ class CKEditorLoadingTest extends BrowserTestBase {
       'core/modules/system/tests/themes/test_ckeditor_stylesheets_relative/css/yokotsoko.css',
     ];
     $this->assertIdentical($expected, _ckeditor_theme_css('test_ckeditor_stylesheets_relative'));
+
+    // Case 4: Install theme which has a Drupal root CSS URL.
+    $theme_installer->install(['test_ckeditor_stylesheets_drupal_root']);
+    $this->config('system.theme')->set('default', 'test_ckeditor_stylesheets_drupal_root')->save();
+    $expected = [
+      'core/modules/system/tests/themes/test_ckeditor_stylesheets_drupal_root/css/yokotsoko.css',
+    ];
+    $this->assertIdentical($expected, _ckeditor_theme_css('test_ckeditor_stylesheets_drupal_root'));
   }
 
   protected function getThingsToCheck() {
