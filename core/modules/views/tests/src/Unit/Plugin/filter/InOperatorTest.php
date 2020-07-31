264a36a671593d7a6e75723c1d5deef4fe744731
Lee Rowlands <lee.rowlands@previousnext.com.au>
2020-07-31 11:51:17 +1000

    Issue #3074595 by Lendude, ravi.shankar, sime, jian he, Kristen Pol, longwave: var_export only returns if the second parameter set to TRUE
    (cherry picked from commit 98856bb10ce5701fa27fb34d5b9e53c0ace0d6cc)

---
+++ core/modules/views/tests/src/Unit/Plugin/filter/InOperatorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\Tests\views\Unit\Plugin\filter;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\views\Plugin\views\filter\InOperator;
+
+/**
+ * @coversDefaultClass \Drupal\views\Plugin\views\filter\InOperator
+ * @group views
+ */
+class InOperatorTest extends UnitTestCase {
+
+  /**
+   * @covers ::validate
+   */
+  public function testValidate() {
+    $definition = [
+      'title' => 'Is InOperator Test',
+      'group' => 'Test',
+      'options callback' => '\Drupal\Tests\views\Unit\Plugin\filter\InOperatorTest::validate_options_callback',
+    ];
+    $filter = new InOperator([], 'in_operator', $definition);
+    $filter->value = 'string';
+    $filter->operator = 'in';
+    $translation_stub = $this->getStringTranslationStub();
+    $filter->setStringTranslation($translation_stub);
+    $errors = $filter->validate();
+    $this->assertSame('The value &#039;string&#039; is not an array for in on filter: ' . $filter->adminLabel(TRUE), (string) $errors[0]);
+  }
+
+  /**
+   * @return array
+   */
+  public static function validate_options_callback() {
+    return ['Yes', 'No'];
+  }
+
+}
