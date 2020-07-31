18623b27c1de10780a6a379dc9f573428dedb687
Alex Pott <alex.a.pott@googlemail.com>
2020-07-31 13:17:49 +0100

    Issue #3113986 by acbramley, msuthars: Can't show 'revision author' on Media views
    (cherry picked from commit ca0f252c4c4f4294ce5d8d3c1d9a7ac02ad9bca6)

--- core/modules/media/src/MediaViewsData.php
+++ core/modules/media/src/MediaViewsData.php
@@ -18,6 +18,13 @@ class MediaViewsData extends EntityViewsData {
     $data['media_field_data']['table']['wizard_id'] = 'media';
     $data['media_field_revision']['table']['wizard_id'] = 'media_revision';
 
+    $data['media_revision']['revision_user']['help'] = $this->t('The user who created the revision.');
+    $data['media_revision']['revision_user']['relationship']['label'] = $this->t('revision user');
+    $data['media_revision']['revision_user']['filter']['id'] = 'user_name';
+
+    $data['media_revision']['table']['join']['media_field_data']['left_field'] = 'vid';
+    $data['media_revision']['table']['join']['media_field_data']['field'] = 'vid';
+
     $data['media_field_data']['status_extra'] = [
       'title' => $this->t('Published status or admin user'),
       'help' => $this->t('Filters out unpublished media if the current user cannot view it.'),
