f96378ac5600d11162c8339f2bde7482dda53b12
Alex Pott <alex.a.pott@googlemail.com>
2020-08-08 00:38:25 +0100

    Issue #2303323 by dww, quietone, tadityar, iyyappan.govind, vsujeetkumar, er.pushpinderrana, amitgoyal, jp.stacey, joshi.rohit100, jungle, alexpott, Berdir, Alan D.: update_delete_file_if_stale() must return a value or update_manager_file_get() will always fetch remote files

--- core/modules/update/tests/src/Kernel/UpdateDeleteFileIfStaleTest.php
+++ core/modules/update/tests/src/Kernel/UpdateDeleteFileIfStaleTest.php
@@ -27,18 +27,30 @@ class UpdateDeleteFileIfStaleTest extends KernelTestBase {
 
     $file_name = $file_system->saveData($this->randomMachineName(), 'public://');
     $this->assertNotNull($file_name);
+    $file_path = $file_system->realpath($file_name);
 
-    // During testing the file change and the stale checking occurs in the same
+    // During testing, the file change and the stale checking occurs in the same
     // request, so the beginning of request will be before the file changes and
     // REQUEST_TIME - $filectime is negative or zero. Set the maximum age to a
-    // number even smaller than that.
+    // number greater than that.
+    $this->config('system.file')
+      ->set('temporary_maximum_age', 100000)
+      ->save();
+
+    // First test that the file is not stale and thus not deleted.
+    $deleted = update_delete_file_if_stale($file_path);
+    $this->assertFalse($deleted);
+    $this->assertFileExists($file_path);
+
+    // Set the maximum age to a number smaller than REQUEST_TIME - $filectime.
     $this->config('system.file')
       ->set('temporary_maximum_age', -100000)
       ->save();
 
-    $file_path = $file_system->realpath($file_name);
-    update_delete_file_if_stale($file_path);
-
+    // Now attempt to delete the file; as it should be considered stale, this
+    // attempt should succeed.
+    $deleted = update_delete_file_if_stale($file_path);
+    $this->assertTrue($deleted);
     $this->assertFileNotExists($file_path);
   }
 
