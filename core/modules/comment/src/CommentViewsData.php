234af3fcc7eae944a5c9d233fe58812b08265c42
catch <catch@35733.no-reply.drupal.org>
2020-08-14 10:08:00 +0100

    Issue #3163425 by shetpooja04, paulocs: Unused variable $fields in comment module, CommentViewsData.php

--- core/modules/comment/src/CommentViewsData.php
+++ core/modules/comment/src/CommentViewsData.php
@@ -200,7 +200,7 @@ class CommentViewsData extends EntityViewsData {
       if ($type == 'comment' || !$entity_type->entityClassImplements(ContentEntityInterface::class) || !$entity_type->getBaseTable()) {
         continue;
       }
-      if ($fields = \Drupal::service('comment.manager')->getFields($type)) {
+      if (\Drupal::service('comment.manager')->getFields($type)) {
         $data['comment_field_data'][$type] = [
           'relationship' => [
             'title' => $entity_type->getLabel(),
