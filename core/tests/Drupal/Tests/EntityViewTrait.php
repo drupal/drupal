fecfd281715b6ce88d4a49c8a231ba964da53376
Lee Rowlands <lee.rowlands@previousnext.com.au>
2020-08-24 10:53:05 +1000

    Issue #3163924 by quietone, Berdir: buildEntityView calls resetCache with an array of ids not entities

--- core/tests/Drupal/Tests/EntityViewTrait.php
+++ core/tests/Drupal/Tests/EntityViewTrait.php
@@ -28,14 +28,12 @@ trait EntityViewTrait {
    * @param null $langcode
    *   (optional) For which language the entity should be prepared, defaults to
    *   the current content language.
-   * @param bool $reset
-   *   (optional) Whether to clear the cache for this entity.
    *
    * @return array
    *
    * @see \Drupal\Core\Render\RendererInterface::render()
    */
-  protected function buildEntityView(EntityInterface $entity, $view_mode = 'full', $langcode = NULL, $reset = FALSE) {
+  protected function buildEntityView(EntityInterface $entity, $view_mode = 'full', $langcode = NULL) {
     $ensure_fully_built = function (&$elements) use (&$ensure_fully_built) {
       // If the default values for this element have not been loaded yet, populate
       // them.
@@ -60,9 +58,6 @@ trait EntityViewTrait {
     };
 
     $render_controller = $this->container->get('entity_type.manager')->getViewBuilder($entity->getEntityTypeId());
-    if ($reset) {
-      $render_controller->resetCache([$entity->id()]);
-    }
     $build = $render_controller->view($entity, $view_mode, $langcode);
     $ensure_fully_built($build);
 
