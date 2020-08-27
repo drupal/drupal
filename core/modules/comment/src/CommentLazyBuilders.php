5e5959e79e3bc7753510f4354f69a7eff46bb68e
catch <catch@35733.no-reply.drupal.org>
2020-08-27 10:25:44 +0100

    Issue #2614720 by quietone, roderik, ofry, Dylan Donkersgoed, Deepak Goyal, samiullah, jonathanshaw, benjifisher, larowlan, Rewted, Ruedische, KlemenDEV, ao2, abramm, vadim.hirbu, HiMyNameIsSeb, dionsj, mgp_novicell, scalas89, rollins, dmytro-aragorn, hchonov, mayurjadhav, paranojik, nplowman: Fatal errors while loading/building orphaned comments
    (cherry picked from commit 9beb71e61014eb31fef2ab0312572ff1ea1b3a82)

--- core/modules/comment/src/CommentLazyBuilders.php
+++ core/modules/comment/src/CommentLazyBuilders.php
@@ -137,9 +137,9 @@ class CommentLazyBuilders implements TrustedCallbackInterface {
     if (!$is_in_preview) {
       /** @var \Drupal\comment\CommentInterface $entity */
       $entity = $this->entityTypeManager->getStorage('comment')->load($comment_entity_id);
-      $commented_entity = $entity->getCommentedEntity();
-
-      $links['comment'] = $this->buildLinks($entity, $commented_entity);
+      if ($commented_entity = $entity->getCommentedEntity()) {
+        $links['comment'] = $this->buildLinks($entity, $commented_entity);
+      }
 
       // Allow other modules to alter the comment links.
       $hook_context = [
