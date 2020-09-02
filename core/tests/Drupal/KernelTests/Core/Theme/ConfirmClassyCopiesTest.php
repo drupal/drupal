d91112e0b8735fde9e35f7a1ebedc5bc0e1134b2
catch <catch@35733.no-reply.drupal.org>
2020-09-02 10:28:08 +0100

    Issue #3164871 by lauriii, bandanasharma, hudri: Incorrect radio button spacing due needless twig template

--- core/tests/Drupal/KernelTests/Core/Theme/ConfirmClassyCopiesTest.php
+++ core/tests/Drupal/KernelTests/Core/Theme/ConfirmClassyCopiesTest.php
@@ -306,7 +306,6 @@ class ConfirmClassyCopiesTest extends KernelTestBase {
             'rdf-metadata.html.twig',
             'help-section.html.twig',
             'progress-bar.html.twig',
-            'radios.html.twig',
             'item-list.html.twig',
             'aggregator-feed.html.twig',
             'item-list--search-results.html.twig',
