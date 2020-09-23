2eebc472ad9fac667ef8c6f260d3e148009af85d
catch <catch@35733.no-reply.drupal.org>
2020-09-23 14:21:23 +0100

    Issue #2132773 by g-brodiei, killes@www.drop.org, DevJoJodae, jonathanshaw, dawehner: Don't add term_access tag if SQL rewriting off
    (cherry picked from commit 50e0c6f2b4a00a8de04c5c95306f6717503b5d12)

--- core/modules/taxonomy/tests/src/Functional/Views/RelationshipNodeTermDataTest.php
+++ core/modules/taxonomy/tests/src/Functional/Views/RelationshipNodeTermDataTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\taxonomy\Functional\Views;
 
 use Drupal\views\Views;
+use Drupal\views\ViewExecutable;
 
 /**
  * Tests the taxonomy term on node relationship handler.
@@ -58,6 +59,54 @@ class RelationshipNodeTermDataTest extends TaxonomyTestBase {
     $this->assertIdentical($expected, $view->getDependencies());
     $this->executeView($view, [$this->term1->id(), $this->term2->id()]);
     $this->assertIdenticalResultset($view, $expected_result, $column_map);
+  }
+
+  /**
+   * Tests that the 'taxonomy_term_access' tag is added to the Views query.
+   */
+  public function testTag() {
+    // Change the view to test relation limited by vocabulary.
+    $this->config('views.view.test_taxonomy_node_term_data')
+      ->set('display.default.display_options.relationships.term_node_tid.vids', ['views_testing_tags'])
+      ->save();
+    $view = Views::getView('test_taxonomy_node_term_data');
+    $this->executeView($view, [$this->term1->id()]);
+
+    // By default, view has taxonomy_term_access tag.
+    $this->assertQueriesTermAccessTag($view, TRUE);
+
+    // The term_access tag is not set if disable_sql_rewrite is set.
+    $view = Views::getView('test_taxonomy_node_term_data');
+    $display = $view->getDisplay();
+    $display_options = $display->getOption('query');
+    $display_options['options']['disable_sql_rewrite'] = TRUE;
+    $display->setOption('query', $display_options);
+    $view->save();
+    $this->executeView($view, [$this->term1->id()]);
+
+    $this->assertQueriesTermAccessTag($view, FALSE);
+  }
+
+  /**
+   * Assert views queries have taxonomy_term_access tag.
+   *
+   * @param \Drupal\views\ViewExecutable $view
+   *   The View to check for the term access tag.
+   * @param bool $hasTag
+   *   The expected existence of taxonomy_term_access tag.
+   */
+  protected function assertQueriesTermAccessTag(ViewExecutable $view, $hasTag) {
+    $main_query = $view->build_info['query'];
+    $count_query = $view->build_info['count_query'];
+
+    foreach ([$main_query, $count_query] as $query) {
+      $tables = $query->getTables();
+      foreach ($tables as $join_table) {
+        if (is_object($join_table['table'])) {
+          $this->assertSame($join_table['table']->hasTag('taxonomy_term_access'), $hasTag);
+        }
+      }
+    }
   }
 
 }
