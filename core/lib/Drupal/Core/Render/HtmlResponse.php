ebf61730dcf89825623e6c3109065ecf4d419954
Gábor Hojtsy <gabor@hojtsy.hu>
2020-09-24 13:19:14 +0200

    Issue #3172410 by mikelutz, andypost, catch, longwave: Make HtmlResponse compatible with Symfony 5

--- core/lib/Drupal/Core/Render/HtmlResponse.php
+++ core/lib/Drupal/Core/Render/HtmlResponse.php
@@ -8,12 +8,13 @@ use Drupal\Core\Cache\CacheableResponseTrait;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * A response that contains and can expose cacheability metadata and attachments.
+ * Response that contains and can expose cacheability metadata and attachments.
  *
  * Supports Drupal's caching concepts: cache tags for invalidation and cache
  * contexts for variations.
  *
- * Supports Drupal's idea of #attached metadata: libraries, settings, http_header and html_head.
+ * Supports Drupal's idea of #attached metadata: libraries, settings,
+ * http_header and html_head.
  *
  * @see \Drupal\Core\Cache\CacheableResponse
  * @see \Drupal\Core\Render\AttachmentsInterface
@@ -25,7 +26,30 @@ class HtmlResponse extends Response implements CacheableResponseInterface, Attac
   use AttachmentsTrait;
 
   /**
-   * {@inheritdoc}
+   * Constructs a HtmlResponse object.
+   *
+   * @param array|string $content
+   *   The response content, see setContent().
+   * @param int $status
+   *   The response status code.
+   * @param array $headers
+   *   An array of response headers.
+   *
+   * @throws \InvalidArgumentException
+   *   When the HTTP status code is not valid.
+   */
+  public function __construct($content = '', int $status = 200, array $headers = []) {
+    parent::__construct('', $status, $headers);
+    $this->setContent($content);
+  }
+
+  /**
+   * Sets the response content.
+   *
+   * @param mixed $content
+   *   Content that can be cast to string, or a render array.
+   *
+   * @return $this
    */
   public function setContent($content) {
     // A render array can automatically be converted to a string and set the
