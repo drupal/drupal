9b0a971a8e58a27bd6e33f16788617f7b9200a94
Alex Pott <alex.a.pott@googlemail.com>
2020-09-28 12:31:10 +0100

    Issue #2949400 by ayushmishra206, chandrashekhar_srijan, bandanasharma, marvil07, AkashkumarOSL, quietone, mikelutz: MigrateProcessInterface documents ProcessPluginBase behaviour
    (cherry picked from commit a677705b3be5e3659b5d99682f830f1aa4f6065c)

--- core/modules/migrate/src/ProcessPluginBase.php
+++ core/modules/migrate/src/ProcessPluginBase.php
@@ -12,10 +12,18 @@ use Drupal\migrate\Plugin\MigrateProcessInterface;
  * transform a human provided name into a machine name, look up an identifier
  * in a previous migration and so on.
  *
+ * Process plugins extending this class can use any number of methods, thus
+ * offering different alternative ways of processing. In this case, the
+ * transform() method should not be implemented, and the plugin configuration
+ * must provide the name of the method to be called via the "method" key. Each
+ * method must have the same signature as transform().
+ *
  * @see https://www.drupal.org/node/2129651
  * @see \Drupal\migrate\Plugin\MigratePluginManager
  * @see \Drupal\migrate\Plugin\MigrateProcessInterface
  * @see \Drupal\migrate\Annotation\MigrateProcessPlugin
+ * @see \Drupal\migrate\Plugin\migrate\process\SkipOnEmpty
+ * @see d7_field_formatter_settings.yml
  * @see plugin_api
  *
  * @ingroup migration
