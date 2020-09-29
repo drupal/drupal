bd8685e6eab059dcf7389f41748d45a6251eabb6
Lee Rowlands <lee.rowlands@previousnext.com.au>
2020-09-29 13:10:04 +1000

    Issue #2586013 by g-brodiei, pameeela, ayushmishra206, Vidushi Mehta, willzyx, LoMo, larowlan, longwave, Lendude, catch: [backport] node_views_analyze() is never executed because it is in the wrong inc file

---
+++ core/modules/node/tests/src/Functional/Views/NodeViewsAnalyzeTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\Tests\node\Functional\Views;
+
+/**
+ * Tests node_views_analyze().
+ *
+ * @group node
+ */
+class NodeViewsAnalyzeTest extends NodeTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['views_ui', 'node_test_views'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Views used by this test.
+   *
+   * @var array
+   */
+  public static $testViews = ['test_node_views_analyze'];
+
+  /**
+   * Tests the implementation of node_views_analyze().
+   */
+  public function testNodeViewsAnalyze() {
+    // Create user with permission to view analyze message on views_ui.
+    $admin_user = $this->createUser(['administer views']);
+
+    $this->drupalLogin($admin_user);
+
+    // Access to views analyze page.
+    $this->drupalGet('admin/structure/views/nojs/analyze/test_node_views_analyze/page_1');
+
+    // Should return 200 with correct permission.
+    $this->assertSession()->statusCodeEquals(200);
+
+    $this->assertSession()->responseContains('has set node/% as path. This will not produce what you want. If you want to have multiple versions of the node view, use Layout Builder.');
+  }
+
+}
