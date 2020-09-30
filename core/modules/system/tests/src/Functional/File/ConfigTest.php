1e5b9c7698b17c49fee02665f3901cf1a191634c
Alex Pott <alex.a.pott@googlemail.com>
2020-10-01 00:24:36 +0100

    Issue #3173976 by shetpooja04: Remove unused variable $file_path in ConfigTest.php, system module

--- core/modules/system/tests/src/Functional/File/ConfigTest.php
+++ core/modules/system/tests/src/Functional/File/ConfigTest.php
@@ -32,7 +32,6 @@ class ConfigTest extends BrowserTestBase {
     // Set the file paths to non-default values.
     // The respective directories are created automatically
     // upon form submission.
-    $file_path = $this->publicFilesDirectory;
     $fields = [
       'file_default_scheme' => 'private',
     ];
