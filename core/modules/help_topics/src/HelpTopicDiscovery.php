a77d83e30b0d1f030fa41d61ac89654f6d9cd9cb
Alex Pott <alex.a.pott@googlemail.com>
2020-10-14 09:23:47 +0100

    Issue #3085972 by andypost, jhodgdon: Replace Drupal\help_topics\FrontMatter with Drupal\Component\Utility\FrontMatter

--- core/modules/help_topics/src/HelpTopicDiscovery.php
+++ core/modules/help_topics/src/HelpTopicDiscovery.php
@@ -5,6 +5,7 @@ namespace Drupal\help_topics;
 use Drupal\Component\Discovery\DiscoveryException;
 use Drupal\Component\FileCache\FileCacheFactory;
 use Drupal\Component\FileSystem\RegexDirectoryIterator;
+use Drupal\Component\FrontMatter\FrontMatter;
 use Drupal\Component\Plugin\Discovery\DiscoveryInterface;
 use Drupal\Component\Plugin\Discovery\DiscoveryTrait;
 use Drupal\Component\Serialization\Exception\InvalidDataTypeException;
@@ -118,7 +119,7 @@ class HelpTopicDiscovery implements DiscoveryInterface {
         // Get the rest of the plugin definition from front matter contained in
         // the help topic Twig file.
         try {
-          $front_matter = FrontMatter::load(file_get_contents($file), Yaml::class)->getData();
+          $front_matter = FrontMatter::create(file_get_contents($file), Yaml::class)->getData();
         }
         catch (InvalidDataTypeException $e) {
           throw new DiscoveryException(sprintf('Malformed YAML in help topic "%s": %s.', $file, $e->getMessage()));
--- core/modules/help_topics/src/HelpTopicTwigLoader.php
+++ core/modules/help_topics/src/HelpTopicTwigLoader.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\help_topics;
 
+use Drupal\Component\FrontMatter\FrontMatter;
 use Drupal\Component\Serialization\Exception\InvalidDataTypeException;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Extension\ThemeHandlerInterface;
@@ -77,14 +78,14 @@ class HelpTopicTwigLoader extends FilesystemLoader {
       // "serializer.yaml" service. This allows the core serializer to utilize
       // core related functionality which isn't available as the standalone
       // component based serializer.
-      $front_matter = FrontMatter::load($contents, Yaml::class);
+      $front_matter = new FrontMatter($contents, Yaml::class);
 
       // Reconstruct the content if there is front matter data detected. Prepend
       // the source with {% line \d+ %} to inform Twig that the source code
       // actually starts on a different line past the front matter data. This is
       // particularly useful when used in error reporting.
       if ($front_matter->getData() && ($line = $front_matter->getLine())) {
-        $contents = "{% line $line %}" . $front_matter->getCode();
+        $contents = "{% line $line %}" . $front_matter->getContent();
       }
     }
     catch (InvalidDataTypeException $e) {
