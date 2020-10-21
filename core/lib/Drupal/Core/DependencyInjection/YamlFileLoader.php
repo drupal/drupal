29397a9f47b3e7ea9236f698932679652b5f99bc
Alex Pott <alex.a.pott@googlemail.com>
2020-10-21 22:11:45 +0100

    Issue #3069026 by anmolgoyal74, Pooja Ganjage, mikelutz, longwave, alexpott, xjm: Fix Call to deprecated method addAutowiringType() of class Symfony\Component\DependencyInjection\Definition in YamlFileLoader

--- core/lib/Drupal/Core/DependencyInjection/YamlFileLoader.php
+++ core/lib/Drupal/Core/DependencyInjection/YamlFileLoader.php
@@ -292,24 +292,6 @@ class YamlFileLoader
             $definition->setAutowired($service['autowire']);
         }
 
-        if (isset($service['autowiring_types'])) {
-            if (is_string($service['autowiring_types'])) {
-                $definition->addAutowiringType($service['autowiring_types']);
-            } else {
-                if (!is_array($service['autowiring_types'])) {
-                    throw new InvalidArgumentException(sprintf('Parameter "autowiring_types" must be a string or an array for service "%s" in %s. Check your YAML syntax.', $id, $file));
-                }
-
-                foreach ($service['autowiring_types'] as $autowiringType) {
-                    if (!is_string($autowiringType)) {
-                        throw new InvalidArgumentException(sprintf('A "autowiring_types" attribute must be of type string for service "%s" in %s. Check your YAML syntax.', $id, $file));
-                    }
-
-                    $definition->addAutowiringType($autowiringType);
-                }
-            }
-        }
-
         $this->container->setDefinition($id, $definition);
     }
 
