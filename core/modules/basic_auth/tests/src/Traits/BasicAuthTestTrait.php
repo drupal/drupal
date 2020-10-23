f60d2589b2b764aae94db2aa84e19c3d4e0016c5
Alex Pott <alex.a.pott@googlemail.com>
2020-10-23 23:47:03 +0100

    Issue #3178273 by longwave: BasicAuthTestTrait::basicAuthPostForm() does not work

--- core/modules/basic_auth/tests/src/Traits/BasicAuthTestTrait.php
+++ core/modules/basic_auth/tests/src/Traits/BasicAuthTestTrait.php
@@ -27,35 +27,6 @@ trait BasicAuthTestTrait {
   }
 
   /**
-   * Executes a form submission using basic authentication.
-   *
-   * @param string $path
-   *   Location of the post form.
-   * @param array $edit
-   *   Field data in an associative array.
-   * @param string $submit
-   *   Value of the submit button whose click is to be emulated.
-   * @param string $username
-   *   The username to use for basic authentication.
-   * @param string $password
-   *   The password to use for basic authentication.
-   * @param array $options
-   *   Options to be forwarded to the url generator.
-   * @param string $form_html_id
-   *   (optional) HTML ID of the form to be submitted.
-   * @param string $extra_post
-   *   (optional) A string of additional data to append to the POST submission.
-   *
-   * @return string
-   *   The retrieved HTML string.
-   *
-   * @see \Drupal\simpletest\WebTestBase::drupalPostForm()
-   */
-  protected function basicAuthPostForm($path, $edit, $submit, $username, $password, array $options = [], $form_html_id = NULL, $extra_post = NULL) {
-    return $this->drupalPostForm($path, $edit, $submit, $options, $this->getBasicAuthHeaders($username, $password), $form_html_id, $extra_post);
-  }
-
-  /**
    * Returns HTTP headers that can be used for basic authentication in Curl.
    *
    * @param string $username
