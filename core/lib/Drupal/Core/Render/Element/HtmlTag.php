f1789c0cb17609d0b7ef9bbcb06f0017fbf16e8d
catch <catch@35733.no-reply.drupal.org>
2020-10-26 09:11:41 +0000

    Issue #3095113 by gapple, longwave, ravi.shankar, Deepak Goyal, anmolgoyal74, Sahana _N, geek-merlin, catch, lauriii, Meenakshi.g: Deprecate IE conditional comments support
    (cherry picked from commit a60c3a47f629c2ee754a6f44fc442fed08042c21)

--- core/lib/Drupal/Core/Render/Element/HtmlTag.php
+++ core/lib/Drupal/Core/Render/Element/HtmlTag.php
@@ -150,6 +150,8 @@ class HtmlTag extends RenderElement {
       return $element;
     }
 
+    @trigger_error('Support for IE Conditional Comments is deprecated in drupal:9.1.0 and is removed from drupal:10.0.0. See https://www.drupal.org/node/3102997', E_USER_DEPRECATED);
+
     // Determine the conditional comment expression for Internet Explorer to
     // evaluate.
     if ($browsers['IE'] === TRUE) {
--- core/tests/Drupal/Tests/Core/Render/Element/HtmlTagTest.php
+++ core/tests/Drupal/Tests/Core/Render/Element/HtmlTagTest.php
@@ -206,22 +206,13 @@ class HtmlTagTest extends RendererTestBase {
     $element = [
       '#tag' => 'link',
     ];
-    $tags['link'] = [HtmlTag::preRenderConditionalComments($element), '<link />' . "\n"];
-
-    // Conditional link.
-    $element = [
-      '#tag' => 'link',
-      '#browsers' => [
-        'IE' => TRUE,
-        '!IE' => FALSE,
-      ],
-    ];
-    $tags['conditional-link'] = [HtmlTag::preRenderConditionalComments($element), "\n" . '<!--[if IE]>' . "\n" . '<link />' . "\n" . '<![endif]-->' . "\n"];
+    $tags['link'] = [$element, '<link />' . "\n"];
 
     return $tags;
   }
 
   /**
+   * @group legacy
    * @covers ::preRenderConditionalComments
    * @dataProvider providerPreRenderConditionalComments
    */
