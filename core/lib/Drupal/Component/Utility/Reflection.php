4fd67a5fc2e1eab275c1361e039e1e8b382ec588
catch <catch@35733.no-reply.drupal.org>
2020-10-26 13:23:10 +0000

    Issue #3156542 by alexpott, voleger, andypost, Charlie ChX Negyesi, Gábor Hojtsy, longwave: \ReflectionParameter::getClass() is deprecated in PHP 8.0
    (cherry picked from commit cb96a97be3cef01d6e7d1b378c8e6759916e85de)

---
+++ core/lib/Drupal/Component/Utility/Reflection.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\Component\Utility;
+
+/**
+ * Provides helper methods for reflection.
+ */
+final class Reflection {
+
+  /**
+   * Gets the parameter's class name.
+   *
+   * @param \ReflectionParameter $parameter
+   *   The parameter.
+   *
+   * @return string|null
+   *   The parameter's class name or NULL if the parameter is not a class.
+   */
+  public static function getParameterClassName(\ReflectionParameter $parameter) : ?string {
+    $name = NULL;
+    if ($parameter->hasType() && !$parameter->getType()->isBuiltin()) {
+      $name = $parameter->getType()->getName();
+      $lc_name = strtolower($name);
+      switch ($lc_name) {
+        case 'self':
+          return $parameter->getDeclaringClass()->getName();
+
+        case 'parent':
+          return ($parent = $parameter->getDeclaringClass()->getParentClass()) ? $parent->name : NULL;
+      }
+    }
+    return $name;
+  }
+
+}
