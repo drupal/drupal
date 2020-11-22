2c757bac860490a1959f5dba511fc0295918c978
catch <catch@35733.no-reply.drupal.org>
2020-11-22 21:21:19 +0000

    Issue #3098281 by greg.1.anderson, alexpott, heddn, Mile23, xjm, jungle, longwave: Ensure that 'composer update' evaluates dependencies using the correct PHP version

--- core/tests/Drupal/Tests/Composer/ComposerTest.php
+++ core/tests/Drupal/Tests/Composer/ComposerTest.php
@@ -25,4 +25,17 @@ class ComposerTest extends UnitTestCase {
     }
   }
 
+  /**
+   * Ensure that the configured php version matches the minimum php version.
+   *
+   * Also ensure that the minimum php version in the root-level composer.json
+   * file exactly matches DRUPAL_MINIMUM_PHP.
+   */
+  public function testEnsurePhpConfiguredVersion() {
+    $composer_json = json_decode(file_get_contents($this->root . '/composer.json'), TRUE);
+    $composer_core_json = json_decode(file_get_contents($this->root . '/core/composer.json'), TRUE);
+    $this->assertEquals(DRUPAL_MINIMUM_PHP, $composer_json['config']['platform']['php'], 'The DRUPAL_MINIMUM_PHP constant should always be exactly the same as the config.platform.php in the root composer.json.');
+    $this->assertEquals($composer_core_json['require']['php'], '>=' . $composer_json['config']['platform']['php'], 'The config.platform.php configured version in the root composer.json file should always be exactly the same as the minimum php version configured in core/composer.json.');
+  }
+
 }
