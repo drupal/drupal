752c8ffb47280f233c2eaf024f1c2b853cb48d90
catch <catch@35733.no-reply.drupal.org>
2020-11-30 12:11:48 +0000

    Issue #3176394 by huzooka, Wim Leers, quietone: Do not migrate comment related configurations if "comment" wasn't enabled on the source site
    (cherry picked from commit 20895bdc9745ae27151cfd494637f8539787cc9b)

--- core/modules/comment/tests/src/Kernel/Migrate/d7/MigrateCommentTypeTest.php
+++ core/modules/comment/tests/src/Kernel/Migrate/d7/MigrateCommentTypeTest.php
@@ -19,14 +19,6 @@ class MigrateCommentTypeTest extends MigrateDrupal7TestBase {
   protected static $modules = ['node', 'comment', 'text'];
 
   /**
-   * {@inheritdoc}
-   */
-  protected function setUp(): void {
-    parent::setUp();
-    $this->migrateCommentTypes();
-  }
-
-  /**
    * Asserts a comment type entity.
    *
    * @param string $id
@@ -45,6 +37,8 @@ class MigrateCommentTypeTest extends MigrateDrupal7TestBase {
    * Tests the migrated comment types.
    */
   public function testMigration() {
+    $this->migrateCommentTypes();
+
     $comment_fields = [
       'comment' => 'Default comment setting',
       'comment_default_mode' => 'Default display mode',
@@ -65,6 +59,91 @@ class MigrateCommentTypeTest extends MigrateDrupal7TestBase {
     $this->assertEntity('comment_node_page', 'Basic page comment');
     $this->assertEntity('comment_node_test_content_type', 'Test content type comment');
     $this->assertEntity('comment_node_a_thirty_two_char', 'Test long name comment');
+  }
+
+  /**
+   * Tests comment type migration without node or / and comment on source.
+   *
+   * Usually, MigrateDumpAlterInterface::migrateDumpAlter() should be used when
+   * the source fixture needs to be changed in a Migrate kernel test, but that
+   * would end in three additional tests and an extra overhead in maintenance.
+   *
+   * @param string[] $disabled_source_modules
+   *   List of the modules to disable in the source Drupal database.
+   * @param string[][] $expected_messages
+   *   List of the expected migration messages, keyed by the message type.
+   *   Message type should be "status" "warning" or "error".
+   *
+   * @dataProvider providerTestNoCommentTypeMigration
+   */
+  public function testNoCommentTypeMigration(array $disabled_source_modules, array $expected_messages) {
+    if (!empty($disabled_source_modules)) {
+      $this->sourceDatabase->update('system')
+        ->condition('name', $disabled_source_modules, 'IN')
+        ->fields(['status' => 0])
+        ->execute();
+    }
+
+    $this->startCollectingMessages();
+    $this->migrateCommentTypes();
+
+    $expected_messages += [
+      'status' => [],
+      'warning' => [],
+      'error' => [],
+    ];
+    $actual_messages = $this->migrateMessages + [
+      'status' => [],
+      'warning' => [],
+      'error' => [],
+    ];
+
+    foreach ($expected_messages as $type => $expected_messages_by_type) {
+      $this->assertEquals(count($expected_messages_by_type), count($actual_messages[$type]));
+      // Cast the actual messages to string.
+      $actual_messages_by_type = array_reduce($actual_messages[$type], function (array $carry, $actual_message) {
+        $carry[] = (string) $actual_message;
+        return $carry;
+      }, []);
+      $missing_expected_messages_by_type = array_diff($expected_messages_by_type, $actual_messages_by_type);
+      $unexpected_messages_by_type = array_diff($actual_messages_by_type, $expected_messages_by_type);
+      $this->assertEmpty($unexpected_messages_by_type, sprintf('No additional messages are present with type "%s". This expectation is wrong, because there are additional messages present: "%s"', $type, implode('", "', $unexpected_messages_by_type)));
+      $this->assertEmpty($missing_expected_messages_by_type, sprintf('Every expected messages are present with type "%s". This expectation is wrong, because the following messages aren\'t present: "%s"', $type, implode('", "', $missing_expected_messages_by_type)));
+    }
+
+    $this->assertEmpty(CommentType::loadMultiple());
+  }
+
+  /**
+   * Provides test cases for ::testNoCommentTypeMigration().
+   */
+  public function providerTestNoCommentTypeMigration() {
+    return [
+      'Node module is disabled in source' => [
+        'Disabled source modules' => ['node'],
+        'Expected messages' => [
+          'error' => [
+            'Migration d7_comment_type did not meet the requirements. The node module is not enabled in the source site. source_module_additional: node.',
+          ],
+        ],
+      ],
+      'Comment module is disabled in source' => [
+        'Disabled source modules' => ['comment'],
+        'Expected messages' => [
+          'error' => [
+            'Migration d7_comment_type did not meet the requirements. The module comment is not enabled in the source site. source_module: comment.',
+          ],
+        ],
+      ],
+      'Node and comment modules are disabled in source' => [
+        'Disabled source modules' => ['comment', 'node'],
+        'Expected messages' => [
+          'error' => [
+            'Migration d7_comment_type did not meet the requirements. The module comment is not enabled in the source site. source_module: comment.',
+          ],
+        ],
+      ],
+    ];
   }
 
 }
