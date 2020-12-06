32f249ae69589be6c741838bd4089426a20abb9b
catch <catch@35733.no-reply.drupal.org>
2020-12-06 20:21:38 +0000

    Issue #2916376 by rlmumford, Sweetchuck, tim.plunkett: The @ContextDefinition annotation always kills the label and the description

--- core/lib/Drupal/Core/Annotation/ContextDefinition.php
+++ core/lib/Drupal/Core/Annotation/ContextDefinition.php
@@ -3,7 +3,6 @@
 namespace Drupal\Core\Annotation;
 
 use Drupal\Component\Annotation\Plugin;
-use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * @defgroup plugin_context Annotation for context definition
@@ -106,7 +105,7 @@ class ContextDefinition extends Plugin {
     // used in the classes they pass to.
     foreach (['label', 'description'] as $key) {
       // @todo Remove this workaround in https://www.drupal.org/node/2362727.
-      if (isset($values[$key]) && $values[$key] instanceof TranslatableMarkup) {
+      if (isset($values[$key]) && $values[$key] instanceof Translation) {
         $values[$key] = (string) $values[$key]->get();
       }
       else {
