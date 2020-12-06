e8a0c86a6ae99c577bba9f3bdbb2a22f8f387377
Alex Pott <alex.a.pott@googlemail.com>
2020-12-06 23:21:47 +0000

    Issue #3048962 by Sam152, anmolgoyal74, paulocs, EclipseGc, abarrios, scotwith1t, Lendude: Moderation State fields generate bad sample data
    (cherry picked from commit 486bab8b49cd759d5b5d30e7564cf043b4711b91)

--- core/modules/content_moderation/tests/src/Kernel/ModerationStateFieldItemListTest.php
+++ core/modules/content_moderation/tests/src/Kernel/ModerationStateFieldItemListTest.php
@@ -7,6 +7,7 @@ use Drupal\language\Entity\ConfigurableLanguage;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
 use Drupal\Tests\content_moderation\Traits\ContentModerationTestTrait;
+use Drupal\Tests\user\Traits\UserCreationTrait;
 use Drupal\workflows\Entity\Workflow;
 
 /**
@@ -17,6 +18,7 @@ use Drupal\workflows\Entity\Workflow;
 class ModerationStateFieldItemListTest extends KernelTestBase {
 
   use ContentModerationTestTrait;
+  use UserCreationTrait;
 
   /**
    * {@inheritdoc}
@@ -42,6 +44,7 @@ class ModerationStateFieldItemListTest extends KernelTestBase {
     parent::setUp();
 
     $this->installSchema('node', 'node_access');
+    $this->installSchema('system', 'sequences');
     $this->installEntitySchema('node');
     $this->installEntitySchema('user');
     $this->installEntitySchema('content_moderation_state');
@@ -399,6 +402,23 @@ class ModerationStateFieldItemListTest extends KernelTestBase {
     $translation = $node->getTranslation('de');
     $this->assertEquals('published', $node->moderation_state->value);
     $this->assertEquals('published', $translation->moderation_state->value);
+  }
+
+  /**
+   * Test generating sample values for entities with a moderation state.
+   */
+  public function testModerationStateSampleValues() {
+    $this->container->get('current_user')->setAccount(
+      $this->createUser([
+        'use editorial transition create_new_draft',
+        'use editorial transition publish',
+      ])
+    );
+    $sample = $this->container->get('entity_type.manager')
+      ->getStorage('node')
+      ->createWithSampleValues('example');
+    $this->assertCount(0, $sample->validate());
+    $this->assertEquals('draft', $sample->moderation_state->value);
   }
 
   /**
