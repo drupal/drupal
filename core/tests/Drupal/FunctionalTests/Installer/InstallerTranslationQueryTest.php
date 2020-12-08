cce00b6b521aa3aafe9e3b6a4e221df4a221b17a
catch <catch@35733.no-reply.drupal.org>
2020-12-08 11:16:31 +0000

    Issue #3184632 by longwave: Convert assertions involving use of xpath on submit inputs to WebAssert
    (cherry picked from commit e0c8ce151872d43621e68d12ba41b82d07257c97)

--- core/tests/Drupal/FunctionalTests/Installer/InstallerTranslationQueryTest.php
+++ core/tests/Drupal/FunctionalTests/Installer/InstallerTranslationQueryTest.php
@@ -37,8 +37,7 @@ class InstallerTranslationQueryTest extends InstallerTestBase {
 
     // The language should have been automatically detected, all following
     // screens should be translated already.
-    $elements = $this->xpath('//input[@type="submit"]/@value');
-    $this->assertEqual(current($elements)->getText(), 'Save and continue de');
+    $this->assertSession()->buttonExists('Save and continue de');
     $this->translations['Save and continue'] = 'Save and continue de';
 
     // Check the language direction.
