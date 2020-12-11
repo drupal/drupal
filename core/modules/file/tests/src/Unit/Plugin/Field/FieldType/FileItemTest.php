3685a8801c301b94c80f6b20b1f580a5ea3263de
Florent Torregrosa <florent.torregrosa@smile.fr>
2020-12-11 11:18:39 +0100

    Issue #2359675: File field's Maximum upload size always passes validation

---
+++ core/modules/file/tests/src/Unit/Plugin/Field/FieldType/FileItemTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\file\Unit\Plugin\Field\FieldType\FileItemTest.
+ */
+
+namespace Drupal\Tests\file\Unit\Plugin\Field\FieldType {
+
+use Drupal\file\Plugin\Field\FieldType\FileItem;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\file\Plugin\Field\FieldType\FileItem
+ *
+ * @group file
+ */
+class FileItemTest extends UnitTestCase {
+
+  /**
+   * Data provider for ::testValidateMaxFilesize
+   */
+  public function providerTestValidateMaxFilesize() {
+    return [
+      // Valid.
+      [5, TRUE],
+      ['5M', TRUE],
+      ['5Mb', TRUE],
+      ['5Gb', TRUE],
+      // Invalid.
+      ['five', FALSE],
+      ['fifty megabytes', FALSE],
+    ];
+  }
+
+  /**
+   * @covers ::validateMaxFilesize
+   *
+   * @dataProvider providerTestValidateMaxFilesize
+   */
+  public function testValidateMaxFilesize($filesize, $valid) {
+    $form_state = $this->getMock('\Drupal\Core\Form\FormStateInterface');
+    // If this is valid, then setError should not be called.
+    $form_state->expects($valid ? $this->never() : $this->once())
+      ->method('setError');
+    $element['#value'] = $filesize;
+    $element['#title'] = 'title';
+    FileItem::validateMaxFilesize($element, $form_state);
+  }
+
+}
+
+}
+
+namespace {
+  use Drupal\Component\Utility\String;
+
+  if (!function_exists('t')) {
+    function t($string, array $args = []) {
+      return String::format($string, $args);
+    }
+  }
+}
