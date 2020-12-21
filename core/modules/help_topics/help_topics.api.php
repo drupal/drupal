39dec106dfbdfa27931c4e82fc4bf7776d6a2f70
Alex Pott <alex.a.pott@googlemail.com>
2020-12-21 05:53:58 +0000

    Issue #3065632 by jhodgdon, andypost: Add more developer docs for Help Topics
    (cherry picked from commit 4aba8c47463be28b013f456fac3035817642dcf0)

--- core/modules/help_topics/help_topics.api.php
+++ core/modules/help_topics/help_topics.api.php
@@ -6,6 +6,36 @@
  */
 
 /**
+ * @defgroup help_docs Help and documentation
+ * @{
+ * Documenting modules, themes, and install profiles
+ *
+ * @section sec_topics Help Topics
+ * Modules, themes, and install profiles can have a subdirectory help_topics
+ * that contains one or more Help Topics, to provide help to administrative
+ * users. These are shown on the main admin/help page. See
+ * @link https://www.drupal.org/docs/develop/documenting-your-project/help-topic-standards Help Topic Standards @endlink
+ * for more information.
+ *
+ * @section sec_hook hook_help
+ * Modules can implement hook_help() to provide a module overview (shown on the
+ * main admin/help page). This hook implementation can also provide help text
+ * that is shown in the Help block at the top of administrative pages. See the
+ * hook_help() documentation and
+ * @link https://www.drupal.org/docs/develop/documenting-your-project/help-text-standards Help text standards @endlink
+ * for more information.
+ *
+ * @section sec_tour Tours
+ * Modules can provide tours of administrative pages by creating tour config
+ * files and placing them in their config/optional subdirectory. See
+ * @link https://www.drupal.org/docs/8/api/tour-api/overview Tour API overview @endlink
+ * for more information. The contributed
+ * @link https://www.drupal.org/project/tour_ui Tour UI module @endlink
+ * can also be used to create tour config files.
+ * @}
+ */
+
+/**
  * @addtogroup hooks
  * @{
  */
