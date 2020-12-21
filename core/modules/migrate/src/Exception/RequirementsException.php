d61ce3017a18a11a80148264e3cfdf19aa2bd411
Alex Pott <alex.a.pott@googlemail.com>
2020-12-21 07:25:58 +0000

    Issue #3147135 by longwave: Fix 'Drupal.Classes.UseGlobalClass' coding standard

--- core/modules/migrate/src/Exception/RequirementsException.php
+++ core/modules/migrate/src/Exception/RequirementsException.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\migrate\Exception;
 
-use Exception;
-
 /**
  * Defines an exception thrown when a migration does not meet the requirements.
  *
@@ -30,7 +28,7 @@ class RequirementsException extends \RuntimeException {
    * @param \Exception $previous
    *   (optional) The previous exception used for the exception chaining.
    */
-  public function __construct($message = "", array $requirements = [], $code = 0, Exception $previous = NULL) {
+  public function __construct($message = "", array $requirements = [], $code = 0, \Exception $previous = NULL) {
     parent::__construct($message, $code, $previous);
 
     $this->requirements = $requirements;
