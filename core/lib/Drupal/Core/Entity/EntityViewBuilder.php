fca86a96717361d04e31f044466b681304cac4c0
Alex Pott <alex.a.pott@googlemail.com>
2020-12-22 16:47:32 +0000

    Issue #3133386 by jyotimishra123, fgm: Incorrect error text in EntityViewBuilder constructor

--- core/lib/Drupal/Core/Entity/EntityViewBuilder.php
+++ core/lib/Drupal/Core/Entity/EntityViewBuilder.php
@@ -108,7 +108,7 @@ class EntityViewBuilder extends EntityHandlerBase implements EntityHandlerInterf
     $this->languageManager = $language_manager;
     $this->themeRegistry = $theme_registry ?: \Drupal::service('theme.registry');
     if (!$entity_display_repository) {
-      @trigger_error('Calling EntityViewBuilder::__construct() with the $entity_repository argument is supported in drupal:8.7.0 and will be required before drupal:9.0.0. See https://www.drupal.org/node/2549139.', E_USER_DEPRECATED);
+      @trigger_error('Calling EntityViewBuilder::__construct() with the $entity_display_repository argument is supported in drupal:8.7.0 and will be required before drupal:9.0.0. See https://www.drupal.org/node/2549139.', E_USER_DEPRECATED);
       $entity_display_repository = \Drupal::service('entity_display.repository');
     }
     $this->entityDisplayRepository = $entity_display_repository;
