dd164ae66b3c320bd41819989501a819eae01939
catch <catch@35733.no-reply.drupal.org>
2021-01-11 20:09:41 +0000

    Issue #3159073 by amateescu, andypost, daffie: Use the new UPSERT capability from SQLite 3.24

--- core/lib/Drupal/Core/Database/Driver/pgsql/Upsert.php
+++ core/lib/Drupal/Core/Database/Driver/pgsql/Upsert.php
@@ -100,8 +100,8 @@ class Upsert extends QueryUpsert {
 
     // Default fields are always placed first for consistency.
     $insert_fields = array_merge($this->defaultFields, $this->insertFields);
-    $insert_fields = array_map(function ($f) {
-      return $this->connection->escapeField($f);
+    $insert_fields = array_map(function ($field) {
+      return $this->connection->escapeField($field);
     }, $insert_fields);
 
     $query = $comments . 'INSERT INTO {' . $this->table . '} (' . implode(', ', $insert_fields) . ') VALUES ';
@@ -114,6 +114,8 @@ class Upsert extends QueryUpsert {
 
     $update = [];
     foreach ($insert_fields as $field) {
+      // The "excluded." prefix causes the field to refer to the value for field
+      // that would have been inserted had there been no conflict.
       $update[] = "$field = EXCLUDED.$field";
     }
 
