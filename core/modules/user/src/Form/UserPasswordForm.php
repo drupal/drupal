5f0435769a07348b89bbb27e96c2b67fb6e12a39
Alex Pott <alex.a.pott@googlemail.com>
2021-01-16 10:54:57 +0000

    Issue #1521996 by quietone, dww, Sophie.SK, Carsten Müller, Spokje, kim.pepper, izus, mjpa, ashu1629, AlanHDev, mangy.fox, Vivek Panicker, robpowell, jozzy_a, tar_inet, ravi.shankar, sokru, zuhair_ak, cjgratacos, aerozeppelin, darrenwh, lomasr, luismagr, mohit1604, ronald.garcia, matteodem, webchuck, gordon, nginex, jain_deepak, Aunion, ankitgarg, Elendev, Kristen Pol, alexpott, YesCT, dpi, no2e, larowlan, longwave, markpavlitski, acbramley, C_Logemann, cilefen, claudiu.cristea, penyaskito, David_Rothstein, Leeteq, anagp, jlbellido, pdrake, klausi: Password reset form reveals whether an email or username is in use

--- core/modules/user/src/Form/UserPasswordForm.php
+++ core/modules/user/src/Form/UserPasswordForm.php
@@ -2,12 +2,15 @@
 
 namespace Drupal\user\Form;
 
+use Drupal\Component\Utility\EmailValidatorInterface;
 use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\Flood\FloodInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Render\Element\Email;
+use Drupal\Core\TypedData\TypedDataManagerInterface;
 use Drupal\user\UserInterface;
 use Drupal\user\UserStorageInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -43,6 +46,20 @@ class UserPasswordForm extends FormBase {
   protected $flood;
 
   /**
+   * The typed data manager.
+   *
+   * @var \Drupal\Core\TypedData\TypedDataManagerInterface
+   */
+  protected $typedDataManager;
+
+  /**
+   * The email validator service.
+   *
+   * @var \Drupal\Component\Utility\EmailValidatorInterface
+   */
+  protected $emailValidator;
+
+  /**
    * Constructs a UserPasswordForm object.
    *
    * @param \Drupal\user\UserStorageInterface $user_storage
@@ -53,12 +70,26 @@ class UserPasswordForm extends FormBase {
    *   The config factory.
    * @param \Drupal\Core\Flood\FloodInterface $flood
    *   The flood service.
+   * @param \Drupal\Core\TypedData\TypedDataManagerInterface $typed_data_manager
+   *   The typed data manager.
+   * @param \Drupal\Component\Utility\EmailValidatorInterface $email_validator
+   *   The email validator service.
    */
-  public function __construct(UserStorageInterface $user_storage, LanguageManagerInterface $language_manager, ConfigFactory $config_factory, FloodInterface $flood) {
+  public function __construct(UserStorageInterface $user_storage, LanguageManagerInterface $language_manager, ConfigFactory $config_factory, FloodInterface $flood, TypedDataManagerInterface $typed_data_manager = NULL, EmailValidatorInterface $email_validator = NULL) {
     $this->userStorage = $user_storage;
     $this->languageManager = $language_manager;
     $this->configFactory = $config_factory;
     $this->flood = $flood;
+    if (is_null($typed_data_manager)) {
+      @trigger_error('Calling ' . __METHOD__ . ' without the $typed_data_manager argument is deprecated in drupal:9.2.0 and will be required in drupal:10.0.0. See https://www.drupal.org/node/3189310', E_USER_DEPRECATED);
+      $typed_data_manager = \Drupal::typedDataManager();
+    }
+    $this->typedDataManager = $typed_data_manager;
+    if (is_null($email_validator)) {
+      @trigger_error('Calling ' . __METHOD__ . ' without the $email_validator argument is deprecated in drupal:9.2.0 and will be required in drupal:10.0.0. See https://www.drupal.org/node/3189310', E_USER_DEPRECATED);
+      $email_validator = \Drupal::service('email.validator');
+    }
+    $this->emailValidator = $email_validator;
   }
 
   /**
@@ -69,7 +100,9 @@ class UserPasswordForm extends FormBase {
       $container->get('entity_type.manager')->getStorage('user'),
       $container->get('language_manager'),
       $container->get('config.factory'),
-      $container->get('flood')
+      $container->get('flood'),
+      $container->get('typed_data_manager'),
+      $container->get('email.validator')
     );
   }
 
@@ -133,7 +166,20 @@ class UserPasswordForm extends FormBase {
       return;
     }
     $this->flood->register('user.password_request_ip', $flood_config->get('ip_window'));
+    // First, see if the input is possibly valid as a username.
     $name = trim($form_state->getValue('name'));
+    $definition = BaseFieldDefinition::create('string')
+      ->addConstraint('UserName', []);
+    $data = $this->typedDataManager->create($definition);
+    $data->setValue($name);
+    $violations = $data->validate();
+    // Usernames have a maximum length shorter than email addresses. Only print
+    // this error if the input is not valid as a username or email address.
+    if ($violations->count() > 0 && !$this->emailValidator->isValid($name)) {
+      $form_state->setErrorByName('name', $this->t("The username or email address is invalid."));
+      return;
+    }
+
     // Try to load by email.
     $users = $this->userStorage->loadByProperties(['mail' => $name]);
     if (empty($users)) {
@@ -141,26 +187,17 @@ class UserPasswordForm extends FormBase {
       $users = $this->userStorage->loadByProperties(['name' => $name]);
     }
     $account = reset($users);
-    if ($account && $account->id()) {
-      // Blocked accounts cannot request a new password.
-      if (!$account->isActive()) {
-        $form_state->setErrorByName('name', $this->t('%name is blocked or has not been activated yet.', ['%name' => $name]));
+    // Blocked accounts cannot request a new password.
+    if ($account && $account->id() && $account->isActive()) {
+      // Register flood events based on the uid only, so they apply for any
+      // IP address. This allows them to be cleared on successful reset (from
+      // any IP).
+      $identifier = $account->id();
+      if (!$this->flood->isAllowed('user.password_request_user', $flood_config->get('user_limit'), $flood_config->get('user_window'), $identifier)) {
+        return;
       }
-      else {
-        // Register flood events based on the uid only, so they apply for any
-        // IP address. This allows them to be cleared on successful reset (from
-        // any IP).
-        $identifier = $account->id();
-        if (!$this->flood->isAllowed('user.password_request_user', $flood_config->get('user_limit'), $flood_config->get('user_window'), $identifier)) {
-          $form_state->setErrorByName('name', $this->t('Too many password recovery requests for this account. It is temporarily blocked. Try again later or contact the site administrator.'));
-          return;
-        }
-        $this->flood->register('user.password_request_user', $flood_config->get('user_window'), $identifier);
-        $form_state->setValueForElement(['#parents' => ['account']], $account);
-      }
-    }
-    else {
-      $form_state->setErrorByName('name', $this->t('%name is not recognized as a username or an email address.', ['%name' => $name]));
+      $this->flood->register('user.password_request_user', $flood_config->get('user_window'), $identifier);
+      $form_state->setValueForElement(['#parents' => ['account']], $account);
     }
   }
 
@@ -169,12 +206,29 @@ class UserPasswordForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $account = $form_state->getValue('account');
-    // Mail one time login URL and instructions using current language.
-    $mail = _user_mail_notify('password_reset', $account);
-    if (!empty($mail)) {
-      $this->logger('user')->notice('Password reset instructions mailed to %name at %email.', ['%name' => $account->getAccountName(), '%email' => $account->getEmail()]);
-      $this->messenger()->addStatus($this->t('Further instructions have been sent to your email address.'));
+    if ($account) {
+      // Mail one time login URL and instructions using current language.
+      $mail = _user_mail_notify('password_reset', $account);
+      if (!empty($mail)) {
+        $this->logger('user')
+          ->notice('Password reset instructions mailed to %name at %email.', [
+            '%name' => $account->getAccountName(),
+            '%email' => $account->getEmail(),
+          ]);
+      }
     }
+    else {
+      $this->logger('user')
+        ->notice('Password reset form was submitted with an unknown or inactive account: %name.', [
+          '%name' => $form_state->getValue('name'),
+        ]);
+    }
+    // Make sure the status text is displayed even if no email was sent. This
+    // message is deliberately the same as the success message for privacy.
+    $this->messenger()
+      ->addStatus($this->t('If %identifier is a valid account, an email will be sent with instructions to reset your password.', [
+        '%identifier' => $form_state->getValue('name'),
+      ]));
 
     $form_state->setRedirect('<front>');
   }
