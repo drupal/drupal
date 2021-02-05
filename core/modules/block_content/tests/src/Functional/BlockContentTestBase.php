bd41f89e84b14580ecbd97d4112d4895fde654e6
Alex Pott <alex.a.pott@googlemail.com>
2021-02-05 13:09:23 +0000

    Issue #2693485 by juhog, paulocs, Gogowitsch, Hardik_Patel_12, YesCT, Mirroar, Lal_, ravi.shankar, abhisekmazumdar, tim.plunkett, longwave, esolitos, TravisCarden, lapek, jhuhta, adriancid, samiullah, adalbertov, cilefen, mohit1604, Abhijith S, John Cook, alexpott, dawehner, pameeela, larowlan, catch, leisurman: Content types are ordered by machine name on /node/add page (+ similar issues with other entities)
    (cherry picked from commit f0eaa97e135a708e645ad47e7b9ad772cf873713)

--- core/modules/block_content/tests/src/Functional/BlockContentTestBase.php
+++ core/modules/block_content/tests/src/Functional/BlockContentTestBase.php
@@ -91,20 +91,40 @@ abstract class BlockContentTestBase extends BrowserTestBase {
   /**
    * Creates a custom block type (bundle).
    *
-   * @param string $label
-   *   The block type label.
+   * @param array|string $values
+   *   The value to create the block content type. If $values is an array
+   *   it should be like: ['id' => 'foo', 'label' => 'Foo']. If $values
+   *   is a string, it will be considered that it represents the label.
    * @param bool $create_body
    *   Whether or not to create the body field
    *
    * @return \Drupal\block_content\Entity\BlockContentType
    *   Created custom block type.
    */
-  protected function createBlockContentType($label, $create_body = FALSE) {
-    $bundle = BlockContentType::create([
-      'id' => $label,
-      'label' => $label,
-      'revision' => FALSE,
-    ]);
+  protected function createBlockContentType($values, $create_body = FALSE) {
+    if (is_array($values)) {
+      if (!isset($values['id'])) {
+        do {
+          $id = strtolower($this->randomMachineName(8));
+        } while (BlockContentType::load($id));
+      }
+      else {
+        $id = $values['id'];
+      }
+      $values += [
+        'id' => $id,
+        'label' => $id,
+        'revision' => FALSE,
+      ];
+      $bundle = BlockContentType::create($values);
+    }
+    else {
+      $bundle = BlockContentType::create([
+        'id' => $values,
+        'label' => $values,
+        'revision' => FALSE,
+      ]);
+    }
     $bundle->save();
     if ($create_body) {
       block_content_add_body_field($bundle->id());
