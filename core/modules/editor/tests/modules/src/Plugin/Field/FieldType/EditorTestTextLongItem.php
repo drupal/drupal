f90925e5ea082b31f1de79a5838aa20be04a4318
Alex Pott <alex.a.pott@googlemail.com>
2021-02-05 14:11:45 +0000

    Issue #2857444 by nedjo, jofitz, gaurav.kapoor, Wim Leers: Editor module fails to track usage of files embedded in non-core fields

---
+++ core/modules/editor/tests/modules/src/Plugin/Field/FieldType/EditorTestTextLongItem.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\editor_test\Plugin\Field\FieldType;
+
+use Drupal\text\Plugin\Field\FieldType\TextLongItem;
+
+/**
+ * Plugin implementation of the 'editor_test_text_long' field type.
+ *
+ * @FieldType(
+ *   id = "editor_test_text_long",
+ *   label = @Translation("Filter test text (formatted, long)"),
+ *   description = @Translation("This field stores a long text with a text format."),
+ *   category = @Translation("Text"),
+ *   default_widget = "text_textarea",
+ *   default_formatter = "text_default"
+ * )
+ */
+class EditorTestTextLongItem extends TextLongItem {
+
+}
