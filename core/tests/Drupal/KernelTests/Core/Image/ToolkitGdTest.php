80805c78473f2d6a1b2a8ae85ff14afeeb4d2b4b
Alex Pott <alex.a.pott@googlemail.com>
2021-02-08 10:12:19 +0000

    Issue #2340699 by mondrake, andypost, jofitz, Jelle_S, Yiannis.Tzikas.Ziogos, kiamlaluno, fietserwin, alexpott, Mixologic, attiks, anavarre: Let GDToolkit support WEBP image format

--- core/tests/Drupal/KernelTests/Core/Image/ToolkitGdTest.php
+++ core/tests/Drupal/KernelTests/Core/Image/ToolkitGdTest.php
@@ -109,7 +109,7 @@ class ToolkitGdTest extends KernelTestBase {
     $this->assertEqual('gd', $this->imageFactory->getToolkitId(), 'The image factory is set to use the \'gd\' image toolkit.');
 
     // Test the list of supported extensions.
-    $expected_extensions = ['png', 'gif', 'jpeg', 'jpg', 'jpe'];
+    $expected_extensions = ['png', 'gif', 'jpeg', 'jpg', 'jpe', 'webp'];
     $supported_extensions = $this->imageFactory->getSupportedExtensions();
     $this->assertEqual($expected_extensions, array_intersect($expected_extensions, $supported_extensions));
 
@@ -121,6 +121,7 @@ class ToolkitGdTest extends KernelTestBase {
       'jpeg' => IMAGETYPE_JPEG,
       'jpg' => IMAGETYPE_JPEG,
       'jpe' => IMAGETYPE_JPEG,
+      'webp' => IMAGETYPE_WEBP,
     ];
     $image = $this->imageFactory->get();
     foreach ($expected_image_types as $extension => $expected_image_type) {
@@ -138,6 +139,7 @@ class ToolkitGdTest extends KernelTestBase {
       'image-test.gif',
       'image-test-no-transparency.gif',
       'image-test.jpg',
+      'img-test.webp',
     ];
 
     // Setup a list of tests to perform on each type.
@@ -210,6 +212,13 @@ class ToolkitGdTest extends KernelTestBase {
         'width' => 40,
         'height' => 20,
         'arguments' => ['extension' => 'png'],
+        'corners' => $default_corners,
+      ],
+      'convert_webp' => [
+        'function' => 'convert',
+        'width' => 40,
+        'height' => 20,
+        'arguments' => ['extension' => 'webp'],
         'corners' => $default_corners,
       ],
     ];
@@ -387,7 +396,7 @@ class ToolkitGdTest extends KernelTestBase {
     }
 
     // Test creation of image from scratch, and saving to storage.
-    foreach ([IMAGETYPE_PNG, IMAGETYPE_GIF, IMAGETYPE_JPEG] as $type) {
+    foreach ([IMAGETYPE_PNG, IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_WEBP] as $type) {
       $image = $this->imageFactory->get();
       $image->createNew(50, 20, image_type_to_extension($type, FALSE), '#ffff00');
       $file = 'from_null' . image_type_to_extension($type);
