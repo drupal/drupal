c05867bd29eb7443d6b93e8fd0d2cdf5fb495b9c
Alex Pott <alex.a.pott@googlemail.com>
2021-03-02 11:58:56 +0000

    Issue #3184650 by geek-merlin, quietone, benjifisher, alexpott: ContentEntity migration source adds revision ID as source key, incompatible with Drupal 8.8 and earlier

--- core/modules/migrate_drupal/src/Plugin/migrate/source/ContentEntity.php
+++ core/modules/migrate_drupal/src/Plugin/migrate/source/ContentEntity.php
@@ -30,14 +30,24 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   of this bundle.
  * - include_translations: (optional) Indicates if the entity translations
  *   should be included, defaults to TRUE.
+ * - add_revision_id: (optional) Indicates if the revision key is added to the
+ *   source IDs, defaults to TRUE.
  *
  * Examples:
  *
- * This will return all nodes, from every bundle and every translation. It does
- * not return all revisions, just the default one.
+ * This will return the default revision for all nodes, from every bundle and
+ * every translation. The revision key is added to the source IDs.
  * @code
  * source:
  *   plugin: content_entity:node
+ * @endcode
+ *
+ * This will return the default revision for all nodes, from every bundle and
+ * every translation. The revision key is not added to the source IDs.
+ * @code
+ * source:
+ *   plugin: content_entity:node
+ *   add_revision_id: false
  * @endcode
  *
  * This will only return nodes of type 'article' in their default language.
@@ -93,6 +103,7 @@ class ContentEntity extends SourcePluginBase implements ContainerFactoryPluginIn
   protected $defaultConfiguration = [
     'bundle' => NULL,
     'include_translations' => TRUE,
+    'add_revision_id' => TRUE,
   ];
 
   /**
@@ -265,7 +276,7 @@ class ContentEntity extends SourcePluginBase implements ContainerFactoryPluginIn
   public function getIds() {
     $id_key = $this->entityType->getKey('id');
     $ids[$id_key] = $this->getDefinitionFromEntity($id_key);
-    if ($this->entityType->isRevisionable()) {
+    if ($this->configuration['add_revision_id'] && $this->entityType->isRevisionable()) {
       $revision_key = $this->entityType->getKey('revision');
       $ids[$revision_key] = $this->getDefinitionFromEntity($revision_key);
     }
