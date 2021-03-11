11154c23a3e905b13b46852748e743a581111dec
catch <catch@35733.no-reply.drupal.org>
2021-03-11 14:34:58 +0000

    Issue #3202787 by richardbporter, longwave: Update @todo comment in FunctionalTestSetupTrait

--- core/lib/Drupal/Core/Test/FunctionalTestSetupTrait.php
+++ core/lib/Drupal/Core/Test/FunctionalTestSetupTrait.php
@@ -188,7 +188,7 @@ trait FunctionalTestSetupTrait {
    * @see TestBase::prepareEnvironment()
    * @see TestBase::restoreEnvironment()
    *
-   * @todo Fix https://www.drupal.org/node/2021959 so that module enable/disable
+   * @todo Fix https://www.drupal.org/node/2941757 so that module enable/disable
    *   changes are immediately reflected in \Drupal::getContainer(). Until then,
    *   tests can invoke this workaround when requiring services from newly
    *   enabled modules to be immediately available in the same request.
