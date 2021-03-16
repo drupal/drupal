cc0730d1bf28d47df6155aa31f3c220819f2c249
Lee Rowlands <lee.rowlands@previousnext.com.au>
2021-03-17 09:32:33 +1000

    Revert "Issue #3199730 by mohit_aghera, Abhijith S, AJV009, Gauravmahlawat, shriaas2898, pameeela, roman-yrv, ranjith_kumar_k_u, Kumar Ashutosh, ilgnerfagundes: Views block description is double-escaped if display name is set"
    This reverts commit fd9f7081cb27235eacce759fcca463f4d5465a57.

--- core/modules/block/tests/src/Kernel/BlockInterfaceTest.php
+++ core/modules/block/tests/src/Kernel/BlockInterfaceTest.php
@@ -60,7 +60,7 @@ class BlockInterfaceTest extends KernelTestBase {
       'admin_label' => [
         '#type' => 'item',
         '#title' => t('Block description'),
-        '#markup' => $definition['admin_label'],
+        '#plain_text' => $definition['admin_label'],
       ],
       'label' => [
         '#type' => 'textfield',
