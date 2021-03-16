82e82fdac4239e461c2abd9f9c156fe8431142b5
catch <catch@35733.no-reply.drupal.org>
2021-03-16 11:30:41 +0000

    Issue #3067609 by quietone, alexpott, justafish, sheanhoxie, flocondetoile, heddn: Fix config schema for links and migration of link default values
    (cherry picked from commit c2cbb6fbac051dd7f920c535354f526ffbfc10ff)

--- core/modules/field/src/Plugin/migrate/process/d6/FieldInstanceDefaults.php
+++ core/modules/field/src/Plugin/migrate/process/d6/FieldInstanceDefaults.php
@@ -54,7 +54,7 @@ class FieldInstanceDefaults extends ProcessPluginBase {
       case 'link':
         if (!empty($widget_settings['default_value'][0]['url'])) {
           $default['title'] = $widget_settings['default_value'][0]['title'];
-          $default['url'] = $widget_settings['default_value'][0]['url'];
+          $default['uri'] = $widget_settings['default_value'][0]['url'];
           $default['options'] = ['attributes' => []];
         }
         break;
