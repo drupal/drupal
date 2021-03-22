f2ba88bce3eead9e3fae69c16114ef2ed13ebc70
catch <catch@35733.no-reply.drupal.org>
2021-03-22 10:03:07 +0000

    Issue #3034324 by BlacKICEUA, nanak: Incorrect PHPDoc comment for public function FormStateInterface::has($property)

--- core/lib/Drupal/Core/Form/FormStateInterface.php
+++ core/lib/Drupal/Core/Form/FormStateInterface.php
@@ -210,7 +210,7 @@ interface FormStateInterface {
   /**
    * Determines if an arbitrary property is present.
    *
-   * @param string $property
+   * @param string|array $property
    *   Properties are often stored as multi-dimensional associative arrays. If
    *   $property is a string, it will return isset($storage[$property]). If
    *   $property is an array, each element of the array will be used as a nested
