8cef0f37c4784e9a0a353fabc59a94b5a08fa407
catch <catch@35733.no-reply.drupal.org>
2021-03-25 12:29:19 +0000

    Issue #3204353 by pragati_kanade, ravi.shankar, Chi, longwave: hook_link_alter() wrong example

--- core/lib/Drupal/Core/Menu/menu.api.php
+++ core/lib/Drupal/Core/Menu/menu.api.php
@@ -485,7 +485,9 @@ function hook_system_breadcrumb_alter(\Drupal\Core\Breadcrumb\Breadcrumb &$bread
  */
 function hook_link_alter(&$variables) {
   // Add a warning to the end of route links to the admin section.
-  if (isset($variables['route_name']) && strpos($variables['route_name'], 'admin') !== FALSE) {
+  /** @var \Drupal\Core\Url $url */
+  $url = $variables['url'];
+  if ($url->isRouted() && strpos($url->getRouteName(), 'admin') !== FALSE) {
     $variables['text'] = t('@text (Warning!)', ['@text' => $variables['text']]);
   }
 }
