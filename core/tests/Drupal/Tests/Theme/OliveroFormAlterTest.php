3c515fa6b3257f923fe3a23995768d9b7316d4e4
Matt Glaman <nmd.matt@gmail.com>
2021-03-27 11:14:47 -0500

    Add test coverage for search specific changes

--- core/tests/Drupal/Tests/Theme/OliveroFormAlterTest.php
+++ core/tests/Drupal/Tests/Theme/OliveroFormAlterTest.php
@@ -2,8 +2,18 @@
 
 namespace Drupal\Tests\Theme;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\Form\FormState;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Routing\UrlGeneratorInterface;
+use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\form_test\Form\FormTestValidateRequiredForm;
+use Drupal\search\Form\SearchBlockForm;
+use Drupal\search\Form\SearchPageForm;
+use Drupal\search\Plugin\SearchInterface;
+use Drupal\search\SearchPageInterface;
+use Drupal\search\SearchPageRepositoryInterface;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -13,8 +23,19 @@ use Drupal\Tests\UnitTestCase;
  */
 final class OliveroFormAlterTest extends UnitTestCase {
 
+  /**
+   * {@inheritdoc}
+   */
   public function setUp(): void {
     parent::setUp();
+
+    $string_translation = $this->prophesize(TranslationInterface::class)->reveal();
+    $url_generator = $this->prophesize(UrlGeneratorInterface::class)->reveal();
+    $container = new ContainerBuilder();
+    $container->set('url_generator', $url_generator);
+    $container->set('string_translation', $string_translation);
+    \Drupal::setContainer($container);
+
     require_once __DIR__ . '/../../../../themes/olivero/olivero.theme';
   }
 
@@ -30,6 +51,44 @@ final class OliveroFormAlterTest extends UnitTestCase {
     self::assertEquals($expected_form, $form);
   }
 
+  /**
+   * Tests specific alters for the `search_block_form` form.
+   */
+  public function testSearchBlockFormAlter() {
+    $form_state = new FormState();
+    $search_block_form = new SearchBlockForm(
+      $this->prophesize(SearchPageRepositoryInterface::class)->reveal(),
+      $this->prophesize(ConfigFactoryInterface::class)->reveal(),
+      $this->prophesize(RendererInterface::class)->reveal()
+    );
+    $form = $search_block_form->buildForm([], $form_state, '123');
+    olivero_form_alter($form, $form_state, $search_block_form->getFormId());
+
+    self::assertEquals(t('Search by keyword or phrase.'), $form['keys']['#attributes']['placeholder']);
+  }
+
+  /**
+   * Tests specific alters for the `search_form` form.
+   */
+  public function testSearchPageFormAlter() {
+    $form_state = new FormState();
+    $search_block_form = new SearchPageForm();
+    $search_page = $this->prophesize(SearchPageInterface::class);
+    $search_page->id()->willReturn(12345);
+    $search_page->getPlugin()->willReturn(
+      $this->prophesize(SearchInterface::class)->reveal()
+    );
+    $form = $search_block_form->buildForm([], $form_state, $search_page->reveal());
+    olivero_form_alter($form, $form_state, $search_block_form->getFormId());
+
+    self::assertEquals(t('Search by keyword or phrase.'), $form['basic']['keys']['#attributes']['placeholder']);
+    self::assertContains('button--primary', $form['basic']['submit']['#attributes']['class']);
+    self::assertContains('button--primary', $form['advanced']['submit']['#attributes']['class']);
+  }
+
+  /**
+   * Data provider to test classes added to the submit action of a form.
+   */
   public function dataForFormAlterTest() {
     // If only one button, class is added to submit.
     yield [
