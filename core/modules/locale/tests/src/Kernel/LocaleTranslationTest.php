f6658ba995db9f48ee3735137a5c8c65cd6001f0
Alex Pott <alex.a.pott@googlemail.com>
2021-04-13 13:41:25 +0100

    Issue #3199428 by clayfreeman, quietone: Remove testing the internals of DependencySerializationTrait from LocaleTranslationTest

--- core/modules/locale/tests/src/Kernel/LocaleTranslationTest.php
+++ core/modules/locale/tests/src/Kernel/LocaleTranslationTest.php
@@ -29,20 +29,6 @@ class LocaleTranslationTest extends KernelTestBase {
     $this->assertNotNull($translation);
     $unserialized = unserialize(serialize($translation));
     $this->assertInstanceOf(LocaleTranslation::class, $unserialized);
-
-    // Ensure that all properties on the unserialized object match the original.
-    $translation_reflection = new \ReflectionObject($translation);
-    $unserialized_reflection = new \ReflectionObject($unserialized);
-    // Ignore the '_serviceIds' property in the comparison.
-    $properties = array_filter($translation_reflection->getProperties(), function ($value) {
-      return $value->getName() !== '_serviceIds';
-    });
-    foreach ($properties as $value) {
-      $value->setAccessible(TRUE);
-      $unserialized_property = $unserialized_reflection->getProperty($value->getName());
-      $unserialized_property->setAccessible(TRUE);
-      $this->assertEquals($unserialized_property->getValue($unserialized), $value->getValue($translation));
-    }
   }
 
 }
