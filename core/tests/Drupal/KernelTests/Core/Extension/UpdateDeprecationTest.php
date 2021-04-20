a0575a8c47ef9e43abb1c501fe4dd4f64801225b
voleger <voleger@3260314.no-reply.drupal.org>
2021-04-20 20:46:45 +0000

    Revert changes to already deprecated functionality

--- core/tests/Drupal/KernelTests/Core/Extension/UpdateDeprecationTest.php
+++ core/tests/Drupal/KernelTests/Core/Extension/UpdateDeprecationTest.php
@@ -28,7 +28,7 @@ class UpdateDeprecationTest extends KernelTestBase {
    * Tests update_check_incompatibility() function.
    */
   public function testUpdateCheckIncompatibility() {
-    $this->expectDeprecation('update_check_incompatibility() is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. No direct replacement is provided. See https://www.drupal.org/node/3150727');
+    $this->expectDeprecation('update_check_incompatibility() is deprecated in drupal:9.1.0 and is removed from drupal:10.0.0. No direct replacement is provided. See https://www.drupal.org/node/3150727');
     $this->assertTrue(update_check_incompatibility('incompatible_module'));
     $this->assertFalse(update_check_incompatibility('system'));
   }
