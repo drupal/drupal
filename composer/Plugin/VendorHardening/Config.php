031c2a295e66d589a3dd6b6f2f6399cf7899c810
catch <catch@35733.no-reply.drupal.org>
2021-04-22 16:46:23 +0100

    Issue #3209459 by longwave, catch, andypost: Switch to friends of behat MinkBrowserKitDriver for PHP 8 and Symfony 5 compatibility

--- composer/Plugin/VendorHardening/Config.php
+++ composer/Plugin/VendorHardening/Config.php
@@ -21,7 +21,6 @@ class Config {
    */
   protected static $defaultConfig = [
     'behat/mink' => ['tests', 'driver-testsuite'],
-    'behat/mink-browserkit-driver' => ['tests'],
     'behat/mink-goutte-driver' => ['tests'],
     'behat/mink-selenium2-driver' => ['tests'],
     'composer/composer' => ['bin'],
@@ -33,6 +32,7 @@ class Config {
     'easyrdf/easyrdf' => ['scripts'],
     'egulias/email-validator' => ['documentation', 'tests'],
     'fabpot/goutte' => ['Goutte/Tests'],
+    'friends-of-behat/mink-browserkit-driver' => ['tests'],
     'guzzlehttp/promises' => ['tests'],
     'guzzlehttp/psr7' => ['tests'],
     'instaclick/php-webdriver' => ['doc', 'test'],
