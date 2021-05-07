a5d1bc6e89ec673f487858a65a9364022802dae4
Phéna Proxima <adam@phenaproxima.net>
2021-05-07 11:36:47 -0400

    Test DELETE method with media entities

--- core/modules/media/tests/src/Functional/UserCancellationTest.php
+++ core/modules/media/tests/src/Functional/UserCancellationTest.php
@@ -5,6 +5,7 @@ namespace Drupal\Tests\media\Functional;
 use Drupal\media\Entity\Media;
 use Drupal\Tests\user\Traits\UserCancellationTrait;
 use Drupal\user\CancellationHandlerInterface;
+use Drupal\user\UserInterface;
 
 /**
  * Tests how media items react to user cancellation.
@@ -24,36 +25,49 @@ class UserCancellationTest extends MediaFunctionalTestBase {
    * Tests how media items react to user cancellation.
    */
   public function testUserCancellation(): void {
-    $media_type = $this->createMediaType('test');
+    $this->createMediaType('test', [
+      'id' => 'test',
+    ]);
     $alice = $this->drupalCreateUser();
     $bob = $this->drupalCreateUser();
+    $charlie = $this->drupalCreateUser();
 
-    $aliceMedia = Media::create([
-      'bundle' => $media_type->id(),
-      'uid' => $alice->id(),
-    ]);
-    $aliceMedia->save();
-    $this->assertSame($alice->id(), $aliceMedia->getOwnerId());
-    $this->assertTrue($aliceMedia->isPublished());
+    $alice_media = $this->createMediaForUser($alice);
+    $bob_media = $this->createMediaForUser($bob);
+    $charlie_media = $this->createMediaForUser($charlie);
 
-    $bobMedia = Media::create([
-      'bundle' => $media_type->id(),
-      'uid' => $bob->id(),
-    ]);
-    $bobMedia->save();
-    $this->assertSame($bob->id(), $bobMedia->getOwnerId());
-    $this->assertTrue($bobMedia->isPublished());
+    $original_charlie_media_vid = $charlie_media->getRevisionId();
+    $charlie_media->setNewRevision();
+    $charlie_media->save();
 
     $this->drupalLogin($this->rootUser);
     $this->cancelUser($alice, CancellationHandlerInterface::METHOD_BLOCK_UNPUBLISH);
-    $aliceMedia = Media::load($aliceMedia->id());
-    $this->assertFalse($aliceMedia->isPublished());
-    $this->assertSame($alice->id(), $aliceMedia->getOwnerId());
+    $alice_media = Media::load($alice_media->id());
+    $this->assertFalse($alice_media->isPublished());
+    $this->assertSame($alice->id(), $alice_media->getOwnerId());
 
     $this->cancelUser($bob, CancellationHandlerInterface::METHOD_REASSIGN);
-    $bobMedia = Media::load($bobMedia->id());
-    $this->assertTrue($bobMedia->isPublished());
-    $this->assertTrue($bobMedia->getOwner()->isAnonymous());
+    $bob_media = Media::load($bob_media->id());
+    $this->assertTrue($bob_media->isPublished());
+    $this->assertTrue($bob_media->getOwner()->isAnonymous());
+
+    $this->cancelUser($charlie, CancellationHandlerInterface::METHOD_DELETE);
+    /** @var \Drupal\media\MediaStorage $media_storage */
+    $media_storage = $this->container->get('entity_type.manager')
+      ->getStorage('media');
+    $this->assertNull($media_storage->load($charlie_media->id()));
+    $this->assertNull($media_storage->loadRevision($original_charlie_media_vid));
+  }
+
+  protected function createMediaForUser(UserInterface $user): Media {
+    $media = Media::create([
+      'bundle' => 'test',
+      'uid' => $user->id(),
+    ]);
+    $media->save();
+    $this->assertSame($user->id(), $media->getOwnerId());
+    $this->assertTrue($media->isPublished());
+    return $media;
   }
 
 }
