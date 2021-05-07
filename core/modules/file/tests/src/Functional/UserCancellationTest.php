f029c5834baba99aa0ca5aec4d2dbfc8e7a31440
Phéna Proxima <adam@phenaproxima.net>
2021-05-07 11:02:44 -0400

    Test DELETE method with files

--- core/modules/file/tests/src/Functional/UserCancellationTest.php
+++ core/modules/file/tests/src/Functional/UserCancellationTest.php
@@ -35,22 +35,29 @@ class UserCancellationTest extends BrowserTestBase {
   public function testUserCancellation(): void {
     $alice = $this->drupalCreateUser();
     $bob = $this->drupalCreateUser();
+    $charlie = $this->drupalCreateUser();
 
     $uri = uniqid('public://') . '.txt';
     file_put_contents($uri, $this->getRandomGenerator()->paragraphs());
     $this->assertFileExists($uri);
+    $values = ['uri' => $uri];
 
-    $aliceFile = File::create(['uri' => $uri])->setOwner($alice);
+    $aliceFile = File::create($values)->setOwner($alice);
     $aliceFile->save();
 
-    $bobFile = File::create(['uri' => $uri])->setOwner($bob);
+    $bobFile = File::create($values)->setOwner($bob);
     $bobFile->save();
+
+    $charlieFile = File::create($values)->setOwner($charlie);
+    $charlieFile->save();
 
     $this->drupalLogin($this->rootUser);
     $this->cancelUser($alice, CancellationHandlerInterface::METHOD_BLOCK_UNPUBLISH);
     $this->assertSame($alice->id(), File::load($aliceFile->id())->getOwnerId());
     $this->cancelUser($bob, CancellationHandlerInterface::METHOD_REASSIGN);
     $this->assertTrue(File::load($bobFile->id())->getOwner()->isAnonymous());
+    $this->cancelUser($charlie, CancellationHandlerInterface::METHOD_DELETE);
+    $this->assertNull(File::load($charlieFile->id()));
   }
 
 }
