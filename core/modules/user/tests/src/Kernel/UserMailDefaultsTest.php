95edf33af0c11d1320b2e0b0f97e568631cb238b
catch <catch@35733.no-reply.drupal.org>
2021-05-10 12:05:56 +0100

    Issue #3212034 by longwave, quietone: Account emails are missing newlines due to malformed YAML

---
+++ core/modules/user/tests/src/Kernel/UserMailDefaultsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\Tests\user\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests that user.mail default settings are parsed correctly.
+ *
+ * @group user
+ */
+class UserMailDefaultsTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['user', 'system'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->installConfig(['user']);
+  }
+
+  /**
+   * Tests that each user mail contains blank lines.
+   *
+   * @dataProvider userMailsProvider
+   */
+  public function testMailDefaults($key) {
+    $body = $this->config('user.mail')->get("$key.body");
+    $this->assertStringContainsString("\n\n", $body);
+  }
+
+  /**
+   * Data provider for user mail testing.
+   *
+   * @return array
+   *   Array of arrays containing the set of user mail configuration keys.
+   */
+  public function userMailsProvider() {
+    return [
+      ['cancel_confirm'],
+      ['password_reset'],
+      ['status_activated'],
+      ['status_blocked'],
+      ['status_canceled'],
+      ['register_admin_created'],
+      ['register_no_approval_required'],
+      ['register_pending_approval'],
+      ['register_pending_approval_admin'],
+    ];
+  }
+
+}
