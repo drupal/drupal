09189c4dcf51038ddbac4a7a8d35487515197412
bnjmnm <benm@umich.edu>
2021-05-10 08:55:22 -0400

    update skipped deprecation doc

--- core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
+++ core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
@@ -137,7 +137,7 @@ trait DeprecationListenerTrait {
       'assertDirectoryNotIsWritable() is deprecated and will be removed in PHPUnit 10. Refactor your code to use assertDirectoryIsNotWritable() instead.',
       'assertFileNotIsWritable() is deprecated and will be removed in PHPUnit 10. Refactor your code to use assertFileIsNotWritable() instead.',
       'The at() matcher has been deprecated. It will be removed in PHPUnit 10. Please refactor your test to not rely on the order in which methods are invoked.',
-      // Messages triggered because the autocomplete shim is enabled by default.
+      // Warning that occurs as the autocomplete shim is enabled by default.
       'The jQuery UI markup structure is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. Use the API provided by core/a11y_autocomplete instead. See https://www.drupal.org/node/3083715',
     ];
   }
