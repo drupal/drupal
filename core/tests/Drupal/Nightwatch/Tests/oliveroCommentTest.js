2d28d287c5d7f2f9b5b115bf84767b4756745565
Brian Perry <contact.briaperry@gmail.com>
2021-05-15 17:55:21 -0500

    Issue 3174107: Nightwatch coverage for Olivero comments

--- core/tests/Drupal/Nightwatch/Tests/oliveroCommentTest.js
+++ core/tests/Drupal/Nightwatch/Tests/oliveroCommentTest.js
@@ -1,3 +1,6 @@
+const commentTitleSelector = 'h2.comments__title';
+const commentCountSelector = 'h2.comments__title .comments__count';
+
 module.exports = {
   '@tags': ['core', 'olivero'],
   before(browser) {
@@ -5,52 +8,7 @@ module.exports = {
       .drupalInstall({
         setupFile:
           'core/tests/Drupal/TestSite/TestSiteOliveroInstallTestScript.php',
-        installProfile: 'minimal',
-      })
-      .drupalLoginAsAdmin(() => {
-        browser
-          .drupalRelativeURL('/admin/modules')
-          .setValue('input[type="search"]', 'Comment')
-          .waitForElementVisible('input[name="modules[comment][enable]"]', 1000)
-          .click('input[name="modules[comment][enable]"]')
-          .clearValue('input[type="search"]')
-          .setValue('input[type="search"]', 'Field UI')
-          .waitForElementVisible(
-            'input[name="modules[field_ui][enable]"]',
-            1000,
-          )
-          .click('input[name="modules[field_ui][enable]"]')
-          .click('input[type="submit"]'); // Submit module form.
-        browser
-          .drupalRelativeURL('/admin/structure/comment/types/add')
-          .setValue('input[name="label"]', 'Default')
-          .waitForElementVisible('span.machine-name-label', 1000)
-          .click('select[name="target_entity_type_id"] option[value="node"]')
-          .click('input[type="submit"]');
-        browser
-          .drupalRelativeURL('/admin/structure/types/add')
-          .setValue('input[name="name"]', 'Article')
-          .waitForElementVisible('span.machine-name-label', 1000)
-          .click('input[type="submit"]');
-        browser
-          .drupalRelativeURL(
-            '/admin/structure/types/manage/article/fields/add-field',
-          )
-          .setValue('select[name="new_storage_type"]', 'comment')
-          .setValue('input[name="label"]', 'Comments')
-          .waitForElementVisible('span.machine-name-label', 1000)
-          .click('input[type="submit"]');
-        browser
-          .drupalRelativeURL('/admin/config/development/performance')
-          .click('input#edit-clear');
-        browser
-          .drupalRelativeURL('/node/add/article')
-          .setValue('input[name="title[0][value]"]', 'Test article')
-          .setValue(
-            'textarea[name="body[0][value]"]',
-            'Body for the test article',
-          )
-          .click('input[type="submit"]');
+        installProfile: 'standard',
       })
       .drupalCreateUser({
         name: 'user',
@@ -66,12 +24,18 @@ module.exports = {
   after(browser) {
     browser.drupalUninstall();
   },
-  'create comment and comment counts': (browser) => {
+  'Article without comments should not display count': (browser) => {
     browser
       .drupalRelativeURL('/node/1')
-      .setValue('input[name="subject[0][value]"]', 'A test comment!')
-      .click('input[type="submit"]')
-      .assert.containsText('h2.comments__title', 'Comments')
-      .assert.containsText('h2.comments__title .comments__count', '1');
+      .assert.containsText('body', 'Article without comments')
+      .assert.not.elementPresent(commentTitleSelector);
+  },
+  'Article with comments should display count': (browser) => {
+    browser
+      .drupalRelativeURL('/node/2')
+      .assert.containsText('body', 'Article with comments')
+      .assert.elementPresent(commentTitleSelector)
+      .assert.elementPresent(commentCountSelector)
+      .assert.containsText(commentCountSelector, '2');
   },
 };
