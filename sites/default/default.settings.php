0cd014fc591e4af1b86ad7469bd32b4536c3c860
bnjmnm <benm@umich.edu>
2021-05-17 09:02:33 -0400

    remove whitespace

--- sites/default/default.settings.php
+++ sites/default/default.settings.php
@@ -802,4 +802,3 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-
