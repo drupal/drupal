a6124cc0b61abaf152c77e9b9b47a0a5fcdd2f2b
Alex Pott <alex.a.pott@googlemail.com>
2021-05-17 09:09:24 +0100

    Issue #3190815 by Wim Leers, quietone, NickDickinsonWilde, benjifisher, mikelutz, larowlan: Source count caching broken: impossible to enable source count caching for SqlBase-based source plugins (plus, unneeded cache I/O)

---
+++ core/modules/migrate/tests/modules/migrate_sql_count_cache_test/src/Plugin/migrate/source/SqlCountCache.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\migrate_sql_count_cache_test\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Source plugin for Sql count cache test.
+ *
+ * @MigrateSource(
+ *   id = "sql_count_cache"
+ * )
+ */
+class SqlCountCache extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    return [
+      'id' => t('Id'),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'id' => [
+        'type' => 'integer',
+      ],
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    return $this->select('source_table', 's')->fields('s', ['id']);
+  }
+
+}
