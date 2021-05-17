1c94be4a7b3bccca87f4580486df3ee9da0b3344
catch <catch@35733.no-reply.drupal.org>
2021-05-17 20:34:57 +0100

    Issue #3126747 by phenaproxima, tim.plunkett, TR, Spokje: ContextDefinition::create() can no longer be used with an entitytype-specific datatype (like entity:user)

--- core/tests/Drupal/KernelTests/Core/Plugin/ContextDefinitionTest.php
+++ core/tests/Drupal/KernelTests/Core/Plugin/ContextDefinitionTest.php
@@ -4,6 +4,7 @@ namespace Drupal\KernelTests\Core\Plugin;
 
 use Drupal\Core\Plugin\Context\ContextDefinition;
 use Drupal\Core\Plugin\Context\EntityContext;
+use Drupal\Core\Plugin\Context\EntityContextDefinition;
 use Drupal\entity_test\Entity\EntityTest;
 use Drupal\KernelTests\KernelTestBase;
 
@@ -41,6 +42,13 @@ class ContextDefinitionTest extends KernelTestBase {
     $this->expectException(\AssertionError::class);
     $this->expectExceptionMessage('assert(strpos($data_type, \'entity:\') !== 0 || $this instanceof EntityContextDefinition)');
     new ContextDefinition('entity:entity_test');
+  }
+
+  /**
+   * @covers ::create
+   */
+  public function testCreateWithEntityDataType() {
+    $this->assertInstanceOf(EntityContextDefinition::class, ContextDefinition::create('entity:user'));
   }
 
 }
