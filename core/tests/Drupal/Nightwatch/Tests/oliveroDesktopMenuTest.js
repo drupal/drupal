703f752af0546856612ee71e5c3bd44d6ca83795
Lauri Eskola <lauri.eskola@acquia.com>
2021-05-19 17:54:44 +0300

    Issue #3210443 by mherchel, bnjmnm, andy-blum, guilhermevp, Gauravmahlawat, Rinku Jacob 13: Olivero: Focus after submenu close via ESC key
    (cherry picked from commit 7d68989ba098224d11d59a527225ecd526cbc96e)

--- core/tests/Drupal/Nightwatch/Tests/oliveroDesktopMenuTest.js
+++ core/tests/Drupal/Nightwatch/Tests/oliveroDesktopMenuTest.js
@@ -93,4 +93,67 @@ module.exports = {
         'false',
       );
   },
+  'Verify parent <button> focus on ESC in wide navigation': (browser) => {
+    browser
+      // Verify functionality on regular link's button.
+      .drupalRelativeURL('/node')
+      .waitForElementVisible(headerNavSelector)
+      .click(`[aria-controls="${linkSubMenuId}"]`)
+      .waitForElementVisible(`#${linkSubMenuId}`)
+      .keys(browser.Keys.TAB)
+      .pause(50)
+      .execute(
+        // eslint-disable-next-line func-names, prefer-arrow-callback, no-shadow
+        function (linkSubMenuId) {
+          return document.activeElement.matches(`#${linkSubMenuId} *`);
+        },
+        [linkSubMenuId],
+        (result) => {
+          browser.assert.ok(result.value);
+        },
+      )
+      .keys(browser.Keys.ESCAPE)
+      .pause(50)
+      .execute(
+        // eslint-disable-next-line func-names, prefer-arrow-callback, no-shadow
+        function (linkSubMenuId) {
+          return document.activeElement.matches(
+            `[aria-controls="${linkSubMenuId}"]`,
+          );
+        },
+        [linkSubMenuId],
+        (result) => {
+          browser.assert.ok(result.value);
+        },
+      )
+      // Verify functionality on route:<button> button.
+      .click(`[aria-controls="${buttonSubMenuId}"]`)
+      .waitForElementVisible(`#${buttonSubMenuId}`)
+      .keys(browser.Keys.TAB)
+      .pause(50)
+      .execute(
+        // eslint-disable-next-line func-names, prefer-arrow-callback, no-shadow
+        function (buttonSubMenuId) {
+          return document.activeElement.matches(`#${buttonSubMenuId} *`);
+        },
+        [buttonSubMenuId],
+        (result) => {
+          browser.assert.ok(result.value);
+        },
+      )
+      .keys(browser.Keys.ESCAPE)
+      .pause(50)
+      .execute(
+        // eslint-disable-next-line func-names, prefer-arrow-callback, no-shadow
+        function (buttonSubMenuId) {
+          return document.activeElement.matches(
+            `[aria-controls="${buttonSubMenuId}"]`,
+          );
+        },
+        [buttonSubMenuId],
+        (result) => {
+          browser.assert.ok(result.value);
+        },
+      );
+  },
 };
