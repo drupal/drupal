ed4a55c4e58e40fdc86a8a62a5ea45c91e054110
Lauri Eskola <lauri.eskola@acquia.com>
2021-05-21 16:11:05 +0300

    Issue #3173012 by mherchel, kiran.kadam911, andy-blum, Gauravmahlawat, ankithashetty, djsagar, proeung: Olivero header-search-wide.pcss.css and header-search-narrow.pcss.css adjustments
    (cherry picked from commit 25de8095658cd48fa741fa07feab811b90e97060)

--- core/themes/olivero/js/search.js
+++ core/themes/olivero/js/search.js
@@ -6,8 +6,8 @@
 **/
 
 (function (Drupal) {
-  var searchWideButton = document.querySelector('.header-nav__search-button');
-  var searchWideWrapper = document.querySelector('.search-wide__wrapper');
+  var searchWideButton = document.querySelector('[data-drupal-selector="block-search-wide-button"]');
+  var searchWideWrapper = document.querySelector('[data-drupal-selector="block-search-wide-wrapper"]');
 
   function searchIsVisible() {
     return searchWideWrapper.classList.contains('is-active');
@@ -44,9 +44,9 @@
     }
   });
   document.addEventListener('click', function (e) {
-    if (e.target.matches('.header-nav__search-button, .header-nav__search-button *')) {
+    if (e.target.matches('[data-drupal-selector="block-search-wide-button"], [data-drupal-selector="block-search-wide-button"] *')) {
       toggleSearchVisibility(!searchIsVisible());
-    } else if (searchIsVisible() && !e.target.matches('.search-wide__wrapper, .search-wide__wrapper *')) {
+    } else if (searchIsVisible() && !e.target.matches('[data-drupal-selector="block-search-wide-wrapper"], [data-drupal-selector="block-search-wide-wrapper"] *')) {
       toggleSearchVisibility(false);
     }
   });
