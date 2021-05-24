9ec511c812ecfc46d9d652c15c250fa2456254bd
Alex Pott <alex.a.pott@googlemail.com>
2021-05-24 12:05:48 +0100

    Issue #1870006 by nod_, BarisW, alwaysworking, Renrhaf, pk188, andrewmacpherson, ranjith_kumar_k_u, vikashsoni, Gauravmahlawat, wolffereast, Wim Leers, jessebeach, xjm: HTML5 validation with table sticky header is misaligned over the toolbar

--- core/misc/tableheader.js
+++ core/misc/tableheader.js
@@ -90,6 +90,7 @@
     tableHeight: null,
     stickyVisible: false,
     createSticky: function createSticky() {
+      this.$html = $('html');
       var $stickyHeader = this.$originalHeader.clone(true);
       this.$stickyTable = $('<table class="sticky-header"></table>').css({
         visibility: 'hidden',
@@ -110,6 +111,7 @@
         css.left = "".concat(this.tableOffset.left - offsetLeft, "px");
       }
 
+      this.$html.css('scroll-padding-top', displace.offsets.top + (this.stickyVisible ? this.$stickyTable.height() : 0));
       return this.$stickyTable.css(css);
     },
     checkStickyVisible: function checkStickyVisible() {
