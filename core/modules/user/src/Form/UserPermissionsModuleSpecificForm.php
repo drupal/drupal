5d9faa4f1bb13ba5bd479c5f398b3709b090572e
Benji Fisher <benji@FisherFam.org>
2021-05-28 19:36:11 -0400

    Add a module-specific version of the Permissions form
    Compare with UserPermissionsRoleSpecificForm.

---
+++ core/modules/user/src/Form/UserPermissionsModuleSpecificForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\user\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\user\RoleInterface;
+
+/**
+ * Provides the user permissions administration form for a specific role.
+ *
+ * @internal
+ */
+class UserPermissionsModuleSpecificForm extends UserPermissionsForm {
+
+  /**
+   * Builds the user permissions administration form for a specific module(s).
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   * @param string $modules
+   *   (optional) One or more module names, comma-separated.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $modules = '') {
+    $this->moduleList = explode(',', $modules);
+    return parent::buildForm($form, $form_state);
+  }
+
+}
