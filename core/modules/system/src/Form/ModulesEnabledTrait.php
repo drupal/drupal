5c770f5444569abbf0f4b97efe0aebf80a8ddfe4
Benji Fisher <benji@FisherFam.org>
2021-05-30 16:56:44 -0400

    Convert a multi-line ternary to if/else

--- core/modules/system/src/Form/ModulesEnabledTrait.php
+++ core/modules/system/src/Form/ModulesEnabledTrait.php
@@ -28,19 +28,22 @@ trait ModulesEnabledTrait {
     $module_names = implode(', ', array_values($modules));
     $t_args = ['%name' => $module_names, '%names' => $module_names];
 
-    $message = $url->access($this->currentUser())
-      ? $this->formatPlural(
+    if ($url->access($this->currentUser())) {
+      $message = $this->formatPlural(
         count($modules),
         'Module %name has been enabled. Configure <a href=":link">related permissions</a>.',
         '@count modules have been enabled: %names. Configure <a href=":link">related permissions</a>.',
         $t_args + [':link' => $url->toString()]
-      )
-      : $this->formatPlural(
+      );
+    }
+    else {
+      $message = $this->formatPlural(
         count($modules),
         'Module %name has been enabled.',
         '@count modules have been enabled: %names.',
         $t_args
       );
+    }
 
     return $message;
   }
