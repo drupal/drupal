9306f0691440f8773e65da96a8b322dd63aea257
Alex Pott <alex.a.pott@googlemail.com>
2021-06-02 17:42:53 +0100

    Revert "Issue #3162016 by longwave, andypost, Hardik_Patel_12, catch, alexpott, daffie, Kristen Pol: [Symfony 6] Retrieving a non-string value from "Symfony\Component\HttpFoundation\InputBag::get()" is deprecated"
    This reverts commit 8b8fc1eff777e020e7199a8f5f2e1f0a403e070c.

--- core/modules/contextual/src/ContextualController.php
+++ core/modules/contextual/src/ContextualController.php
@@ -60,15 +60,15 @@ class ContextualController implements ContainerInjectionInterface {
    * @see contextual_preprocess()
    */
   public function render(Request $request) {
-    if (!$request->request->has('ids')) {
+    $ids = $request->request->get('ids');
+    if (!isset($ids)) {
       throw new BadRequestHttpException('No contextual ids specified.');
     }
-    $ids = $request->request->all('ids');
 
-    if (!$request->request->has('tokens')) {
+    $tokens = $request->request->get('tokens');
+    if (!isset($tokens)) {
       throw new BadRequestHttpException('No contextual ID tokens specified.');
     }
-    $tokens = $request->request->all('tokens');
 
     $rendered = [];
     foreach ($ids as $key => $id) {
