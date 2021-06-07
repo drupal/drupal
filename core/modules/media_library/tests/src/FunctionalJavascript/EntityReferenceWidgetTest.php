25ae2b497643472ebcfe82100a6c8df4801228a1
Jeroen Tubex <jeroen.tubex@intracto.com>
2021-06-07 07:32:40 +0200

    Move test to functional test

--- core/modules/media_library/tests/src/FunctionalJavascript/EntityReferenceWidgetTest.php
+++ core/modules/media_library/tests/src/FunctionalJavascript/EntityReferenceWidgetTest.php
@@ -450,26 +450,4 @@ class EntityReferenceWidgetTest extends MediaLibraryTestBase {
     $assert_session->pageTextNotContains('Snake');
   }
 
-  /**
-   * Test that a required media field has a clear error message.
-   */
-  public function testEmptyErrorMessage() {
-    $assert_session = $this->assertSession();
-    $page = $this->getSession()->getPage();
-
-    // Make field_unlimited_media required.
-    $field_config = FieldConfig::loadByName('node', 'basic_page', 'field_unlimited_media');
-    $field_config->setRequired(TRUE)->save();
-
-    // Visit a node create page.
-    $this->drupalGet('node/add/basic_page');
-
-    $page->fillField('title[0][value]', 'My page');
-    $page->pressButton('Save');
-
-    // Check that a clear error message is shown.
-    $assert_session->pageTextNotContains('This value should not be null');
-    $assert_session->pageTextContains(sprintf('%s field is required.', $field_config->label()));
-  }
-
 }
