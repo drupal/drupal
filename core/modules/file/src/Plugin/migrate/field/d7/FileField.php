267c53ef833f4ef2189818ffd338dceea633d942
Wim Leers <wim.leers@acquia.com>
2021-06-14 13:06:04 +0200

    Fix mistakes I made while rebasing.

--- core/modules/file/src/Plugin/migrate/field/d7/FileField.php
+++ core/modules/file/src/Plugin/migrate/field/d7/FileField.php
@@ -19,7 +19,7 @@ class FileField extends D6FileField {
    * {@inheritdoc}
    */
   public function getFieldWidgetMap() {
-    return [
+    return parent::getFieldWidgetMap() + [
       'file_mfw' => 'file_generic',
       'filefield_widget' => 'file_generic',
       'media_generic' => 'file_generic',
