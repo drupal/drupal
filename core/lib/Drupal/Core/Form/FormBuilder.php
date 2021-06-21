8cc548c09eb5500689aaab9679781ee70e1fd7e0
catch <catch@35733.no-reply.drupal.org>
2021-06-21 11:56:25 +0100

    Issue #3219541 by tunic, longwave: Remove redundant call $this->requestStack->getCurrentRequest() in FormBuilder::buildForm

--- core/lib/Drupal/Core/Form/FormBuilder.php
+++ core/lib/Drupal/Core/Form/FormBuilder.php
@@ -302,7 +302,6 @@ class FormBuilder implements FormBuilderInterface, FormValidatorInterface, FormS
     }
 
     // If this form is an AJAX request, disable all form redirects.
-    $request = $this->requestStack->getCurrentRequest();
     if ($ajax_form_request = $request->query->has(static::AJAX_FORM_REQUEST)) {
       $form_state->disableRedirect();
     }
