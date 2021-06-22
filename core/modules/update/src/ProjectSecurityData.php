cragr/drupal-lab

Replace uses of the array data

core/modules/update/src/ProjectSecurityData.php
@@ -217,14 +217,15 @@ final class ProjectSecurityData {
   private function getAdditionalSecurityCoveredMinors($security_covered_version) {
     $security_covered_version_major = ModuleVersion::createFromVersionString($security_covered_version)->getMajorVersion();
     $security_covered_version_minor = $this->getSemanticMinorVersion($security_covered_version);
-    foreach ($this->releases as $release) {
-      $release_version = ModuleVersion::createFromVersionString($release['version']);
-      if ($release_version->getMajorVersion() === $security_covered_version_major && $release['status'] === 'published' && !$release_version->getVersionExtra()) {
+    foreach ($this->releases as $release_info) {
+      $release = ProjectRelease::createFromArray($release_info);
+      $release_version = ModuleVersion::createFromVersionString($release->getVersion());
+      if ($release_version->getMajorVersion() === $security_covered_version_major && !$release->isUnpublished() && !$release_version->getVersionExtra()) {
         // The releases are ordered with the most recent releases first.
         // Therefore, if we have found a published, official release with the
         // same major version as $security_covered_version, then this release
         // can be used to determine the latest minor.
-        $latest_minor = $this->getSemanticMinorVersion($release['version']);
+        $latest_minor = $this->getSemanticMinorVersion($release->getVersion());
         break;
       }
     }

drupal/drupal

core/modules/update/src/ProjectSecurityData.php
@@ -219,14 +219,15 @@ final class ProjectSecurityData {
   private function getAdditionalSecurityCoveredMinors($security_covered_version) {
     $security_covered_version_major = ExtensionVersion::createFromVersionString($security_covered_version)->getMajorVersion();
     $security_covered_version_minor = $this->getSemanticMinorVersion($security_covered_version);
-    foreach ($this->releases as $release) {
-      $release_version = ExtensionVersion::createFromVersionString($release['version']);
-      if ($release_version->getMajorVersion() === $security_covered_version_major && $release['status'] === 'published' && !$release_version->getVersionExtra()) {
+    foreach ($this->releases as $release_info) {
+      $release = ProjectRelease::createFromArray($release_info);
+      $release_version = ExtensionVersion::createFromVersionString($release->getVersion());
+      if ($release_version->getMajorVersion() === $security_covered_version_major && !$release->isUnpublished() && !$release_version->getVersionExtra()) {
         // The releases are ordered with the most recent releases first.
         // Therefore, if we have found a published, official release with the
         // same major version as $security_covered_version, then this release
         // can be used to determine the latest minor.
-        $latest_minor = $this->getSemanticMinorVersion($release['version']);
+        $latest_minor = $this->getSemanticMinorVersion($release->getVersion());
         break;
       }
     }