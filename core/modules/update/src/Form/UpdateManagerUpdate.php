cragr/drupal-lab

replace more uses of array data

core/modules/update/src/Form/UpdateManagerUpdate.php
@@ -8,6 +8,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Link;
 use Drupal\Core\State\StateInterface;
 use Drupal\Core\Url;
+use Drupal\update\ProjectRelease;
 use Drupal\update\UpdateFetcherInterface;
 use Drupal\update\UpdateManagerInterface;
 use Drupal\update\ModuleVersion;
@@ -140,9 +141,9 @@ class UpdateManagerUpdate extends FormBase {
         continue;
       }
 
-      $recommended_release = $project['releases'][$project['recommended']];
+      $recommended_release = ProjectRelease::createFromArray($project['releases'][$project['recommended']]);
       $recommended_version = '{{ release_version }} (<a href="{{ release_link }}" title="{{ project_title }}">{{ release_notes }}</a>)';
-      $recommended_version_parser = ModuleVersion::createFromVersionString($recommended_release['version']);
+      $recommended_version_parser = ModuleVersion::createFromVersionString($recommended_release->getVersion());
       if ($recommended_version_parser->getMajorVersion() != $project['existing_major']) {
         $recommended_version .= '<div title="{{ major_update_warning_title }}" class="update-major-version-warning">{{ major_update_warning_text }}</div>';
       }
@@ -151,8 +152,8 @@ class UpdateManagerUpdate extends FormBase {
         '#type' => 'inline_template',
         '#template' => $recommended_version,
         '#context' => [
-          'release_version' => $recommended_release['version'],
-          'release_link' => $recommended_release['release_link'],
+          'release_version' => $recommended_release->getVersion(),
+          'release_link' => $recommended_release->getUrl(),
           'project_title' => $this->t('Release notes for @project_title', ['@project_title' => $project['title']]),
           'major_update_warning_title' => $this->t('Major upgrade warning'),
           'major_update_warning_text' => $this->t('This update is a major version update which means that it may not be backwards compatible with your currently running version. It is recommended that you read the release notes and proceed at your own risk.'),
@@ -207,11 +208,10 @@ class UpdateManagerUpdate extends FormBase {
 
       // If the recommended release for a contributed project is not compatible
       // with the currently installed version of core, list that project in a
-      // separate table. To determine if the release is compatible, we inspect
-      // the 'core_compatible' key from the release info array. If it's not
-      // defined, it means we can't determine compatibility requirements (or
-      // we're looking at core), so we assume it is compatible.
-      $compatible = $recommended_release['core_compatible'] ?? TRUE;
+      // separate table. If it's not defined, it means we can't determine
+      // compatibility requirements (or we're looking at core), so we assume it
+      // is compatible.
+      $compatible = $recommended_release->isCoreCompatible() ?? TRUE;
 
       if ($needs_manual) {
         $this->removeCheckboxFromRow($entry);
@@ -220,19 +220,19 @@ class UpdateManagerUpdate extends FormBase {
       elseif (!$compatible) {
         $this->removeCheckboxFromRow($entry);
         // If the release has a core_compatibility_message, inject it.
-        if (!empty($recommended_release['core_compatibility_message'])) {
+        if ($core_compatibility_message = $recommended_release->getCoreCompatibilityMessage()) {
           // @todo In https://www.drupal.org/project/drupal/issues/3121769
           //   refactor this into something theme-friendly so we don't have a
           //   classless <div> here.
           $entry['data']['recommended_version']['data']['#template'] .= ' <div>{{ core_compatibility_message }}</div>';
-          $entry['data']['recommended_version']['data']['#context']['core_compatibility_message'] = $recommended_release['core_compatibility_message'];
+          $entry['data']['recommended_version']['data']['#context']['core_compatibility_message'] = $core_compatibility_message;
         }
         $projects['not-compatible'][$name] = $entry;
       }
       else {
         $form['project_downloads'][$name] = [
           '#type' => 'value',
-          '#value' => $recommended_release['download_link'],
+          '#value' => $recommended_release->getDownloadUrl(),
         ];
 
         // Based on what kind of project this is, save the entry into the

drupal/drupal

core/modules/update/src/Form/UpdateManagerUpdate.php
@@ -9,6 +9,7 @@ use Drupal\Core\Link;
 use Drupal\Core\State\StateInterface;
 use Drupal\Core\Url;
 use Drupal\Core\Extension\ExtensionVersion;
+use Drupal\update\ProjectRelease;
 use Drupal\update\UpdateFetcherInterface;
 use Drupal\update\UpdateManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -140,9 +141,9 @@ class UpdateManagerUpdate extends FormBase {
         continue;
       }
 
-      $recommended_release = $project['releases'][$project['recommended']];
+      $recommended_release = ProjectRelease::createFromArray($project['releases'][$project['recommended']]);
       $recommended_version = '{{ release_version }} (<a href="{{ release_link }}" title="{{ project_title }}">{{ release_notes }}</a>)';
-      $recommended_version_parser = ExtensionVersion::createFromVersionString($recommended_release['version']);
+      $recommended_version_parser = ExtensionVersion::createFromVersionString($recommended_release->getVersion());
       if ($recommended_version_parser->getMajorVersion() != $project['existing_major']) {
         $recommended_version .= '<div title="{{ major_update_warning_title }}" class="update-major-version-warning">{{ major_update_warning_text }}</div>';
       }
@@ -151,8 +152,8 @@ class UpdateManagerUpdate extends FormBase {
         '#type' => 'inline_template',
         '#template' => $recommended_version,
         '#context' => [
-          'release_version' => $recommended_release['version'],
-          'release_link' => $recommended_release['release_link'],
+          'release_version' => $recommended_release->getVersion(),
+          'release_link' => $recommended_release->getUrl(),
           'project_title' => $this->t('Release notes for @project_title', ['@project_title' => $project['title']]),
           'major_update_warning_title' => $this->t('Major upgrade warning'),
           'major_update_warning_text' => $this->t('This update is a major version update which means that it may not be backwards compatible with your currently running version. It is recommended that you read the release notes and proceed at your own risk.'),
@@ -207,11 +208,10 @@ class UpdateManagerUpdate extends FormBase {
 
       // If the recommended release for a contributed project is not compatible
       // with the currently installed version of core, list that project in a
-      // separate table. To determine if the release is compatible, we inspect
-      // the 'core_compatible' key from the release info array. If it's not
-      // defined, it means we can't determine compatibility requirements (or
-      // we're looking at core), so we assume it is compatible.
-      $compatible = $recommended_release['core_compatible'] ?? TRUE;
+      // separate table. If it's not defined, it means we can't determine
+      // compatibility requirements (or we're looking at core), so we assume it
+      // is compatible.
+      $compatible = $recommended_release->isCoreCompatible() ?? TRUE;
 
       if ($needs_manual) {
         $this->removeCheckboxFromRow($entry);
@@ -220,19 +220,19 @@ class UpdateManagerUpdate extends FormBase {
       elseif (!$compatible) {
         $this->removeCheckboxFromRow($entry);
         // If the release has a core_compatibility_message, inject it.
-        if (!empty($recommended_release['core_compatibility_message'])) {
+        if ($core_compatibility_message = $recommended_release->getCoreCompatibilityMessage()) {
           // @todo In https://www.drupal.org/project/drupal/issues/3121769
           //   refactor this into something theme-friendly so we don't have a
           //   classless <div> here.
           $entry['data']['recommended_version']['data']['#template'] .= ' <div>{{ core_compatibility_message }}</div>';
-          $entry['data']['recommended_version']['data']['#context']['core_compatibility_message'] = $recommended_release['core_compatibility_message'];
+          $entry['data']['recommended_version']['data']['#context']['core_compatibility_message'] = $core_compatibility_message;
         }
         $projects['not-compatible'][$name] = $entry;
       }
       else {
         $form['project_downloads'][$name] = [
           '#type' => 'value',
-          '#value' => $recommended_release['download_link'],
+          '#value' => $recommended_release->getDownloadUrl(),
         ];
 
         // Based on what kind of project this is, save the entry into the