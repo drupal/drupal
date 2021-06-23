8a4a2001aa39653f20dff10d0ac2bccd30ac182e
Dave Long <dave@longwaveconsulting.com>
2021-06-23 10:31:04 +0100

    Add Doctrine copyright notice and cspell fix.

--- core/lib/Drupal/Component/Reflection/StaticReflectionMethod.php
+++ core/lib/Drupal/Component/Reflection/StaticReflectionMethod.php
@@ -1,4 +1,28 @@
 <?php
+// phpcs:ignoreFile
+
+/**
+ * @file
+ *
+ * This class is a near-copy of
+ * Doctrine\Common\Reflection\StaticReflectionMethod, which is part of the
+ * Doctrine project: <http://www.doctrine-project.org>. It was copied from
+ * version 1.2.2.
+ *
+ * Original copyright:
+ *
+ * Copyright (c) 2006-2015 Doctrine Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
 
 namespace Drupal\Component\Reflection;
 
