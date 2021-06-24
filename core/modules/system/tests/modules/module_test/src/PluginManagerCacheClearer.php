b85bafd909c0b529ede9a36d20f3ef969e778502
Lee Rowlands <lee.rowlands@previousnext.com.au>
2021-06-24 14:30:05 +1000

    Issue #3218660 by alexpott: help_topics module can break during module uninstall
    (cherry picked from commit 257efe54397bef0fc57c823740835d9ad60c9330)

---
+++ core/modules/system/tests/modules/module_test/src/PluginManagerCacheClearer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\module_test;
+
+use Drupal\Core\Plugin\DefaultPluginManager;
+use Drupal\Core\State\StateInterface;
+
+/**
+ * Helps test module uninstall.
+ */
+class PluginManagerCacheClearer extends DefaultPluginManager {
+
+  /**
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * An optional service dependency.
+   *
+   * @var object|null
+   */
+  protected $optionalService;
+
+  /**
+   * PluginManagerCacheClearer constructor.
+   *
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state service for recording what happens.
+   * @param null $optional_service
+   *   An optional service for testing.
+   */
+  public function __construct(StateInterface $state, $optional_service = NULL) {
+    $this->state = $state;
+    $this->optionalService = $optional_service;
+  }
+
+  /**
+   * Tests call to CachedDiscoveryInterface::clearCachedDefinitions().
+   *
+   * @see \Drupal\Component\Plugin\Discovery\CachedDiscoveryInterface::clearCachedDefinitions()
+   */
+  public function clearCachedDefinitions() {
+    $this->state->set(self::class, isset($this->optionalService));
+  }
+
+}
