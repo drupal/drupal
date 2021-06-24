6379f4ed080456e169a91944631e500d430c1e1f
Claudiu Cristea <clau.cristea@gmail.com>
2021-06-24 18:55:01 +0300

    Use ::expectDeprecation() instead of annotation

--- core/tests/Drupal/KernelTests/Core/Updater/DrupalGetUpdatersDeprecationTest.php
+++ core/tests/Drupal/KernelTests/Core/Updater/DrupalGetUpdatersDeprecationTest.php
@@ -16,13 +16,14 @@ class DrupalGetUpdatersDeprecationTest extends KernelTestBase {
   protected static $modules = ['system'];
 
   /**
-   * @expectedDeprecation drupal_get_updaters() is deprecated in drupal:9.1.0 and is removed from drupal:10.0.0. Use \Drupal\Core\Updater\Updater::getUpdaterRegistry() instead. See https://www.drupal.org/node/3047258
-   * @expectedDeprecation Using drupal_static_reset() with 'drupal_get_updaters' as parameter is deprecated in drupal:9.1.0 and is removed from drupal:10.0.0. Use \Drupal\Core\Updater\Updater::resetRegistryCache() instead. See https://www.drupal.org/node/3047258
    * @see drupal_get_updaters()
+   * @see drupal_static_reset()
    */
   public function testDrupalGetUpdatersDeprecation() {
+    $this->expectDeprecation('drupal_get_updaters() is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. Use \Drupal\Core\Updater\Updater::getUpdaterRegistry() instead. See https://www.drupal.org/node/3047258');
     $updaters = drupal_get_updaters();
     $this->assertSame(['module', 'theme'], array_keys($updaters));
+    $this->expectDeprecation("Using drupal_static_reset() with 'drupal_get_updaters' as parameter is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. Use \Drupal\Core\Updater\Updater::resetRegistryCache() instead. See https://www.drupal.org/node/3047258");
     drupal_static_reset('drupal_get_updaters');
   }
 
