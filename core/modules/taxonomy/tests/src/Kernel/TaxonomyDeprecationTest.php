828b01248f7bf83c6194f14579110bffc3edc8f7
Claudiu Cristea <clau.cristea@gmail.com>
2021-06-25 18:02:37 +0300

    Fix the deprecation messages

--- core/modules/taxonomy/tests/src/Kernel/TaxonomyDeprecationTest.php
+++ core/modules/taxonomy/tests/src/Kernel/TaxonomyDeprecationTest.php
@@ -22,7 +22,7 @@ class TaxonomyDeprecationTest extends KernelTestBase {
   public function testTaxonomyVocabularyGetNamesDeprecation() {
     $this->expectDeprecation("taxonomy_vocabulary_get_names() is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. Use \Drupal::entityTypeManager()->getStorage('taxonomy_vocabulary')->loadMultiple() instead, to get a list of vocabulary entities keyed by vocabulary ID. See https://www.drupal.org/node/3039041");
     taxonomy_vocabulary_get_names();
-    $this->expectDeprecation("Using drupal_static_reset() with 'taxonomy_vocabulary_get_names' as parameter is deprecated in Drupal 9.3.0 and is removed from Drupal 10.0.0. There's no replacement for this usage. See https://www.drupal.org/node/3039041");
+    $this->expectDeprecation("Using drupal_static_reset() with 'taxonomy_vocabulary_get_names' as parameter is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. There is no replacement for this usage. See https://www.drupal.org/node/3039041");
     drupal_static_reset('taxonomy_vocabulary_get_names');
   }
 
