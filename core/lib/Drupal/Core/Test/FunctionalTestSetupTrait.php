883787ecfb14601a40164f168a2d81fedb84d8a4
catch <catch@35733.no-reply.drupal.org>
2021-06-25 06:58:05 +0100

    Issue #3217374 by bbrala, daffie: SIMPLETEST_BASE_URL does not validate scheme

--- core/lib/Drupal/Core/Test/FunctionalTestSetupTrait.php
+++ core/lib/Drupal/Core/Test/FunctionalTestSetupTrait.php
@@ -564,7 +564,7 @@ trait FunctionalTestSetupTrait {
    * Sets up the base URL based upon the environment variable.
    *
    * @throws \Exception
-   *   Thrown when no SIMPLETEST_BASE_URL environment variable is provided.
+   *   Thrown when no SIMPLETEST_BASE_URL environment variable is provided or uses an invalid scheme.
    */
   protected function setupBaseUrl() {
     global $base_url;
@@ -583,6 +583,13 @@ trait FunctionalTestSetupTrait {
     $host = $parsed_url['host'] . (isset($parsed_url['port']) ? ':' . $parsed_url['port'] : '');
     $path = isset($parsed_url['path']) ? rtrim(rtrim($parsed_url['path']), '/') : '';
     $port = isset($parsed_url['port']) ? $parsed_url['port'] : 80;
+
+    $valid_url_schemes = ['http', 'https'];
+    if (!in_array(strtolower($parsed_url['scheme']), $valid_url_schemes, TRUE)) {
+      throw new \Exception(
+        'You must provide valid scheme for the SIMPLETEST_BASE_URL environment variable. Valid schema are: http, https.'
+      );
+    }
 
     $this->baseUrl = $base_url;
 
