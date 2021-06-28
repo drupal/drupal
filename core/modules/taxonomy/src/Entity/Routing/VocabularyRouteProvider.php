cfca6ce40da44f97b1968b5ebb7d5cb6acb4b25e
xjm <xjm@65776.no-reply.drupal.org>
2021-06-28 18:13:29 -0500

    Issue #3037157 by phenaproxima, sudiptadas19, mohit_aghera, xjm, manuel.adan, Abhijith S, tanubansal, joachim, larowlan, tim.plunkett, catch: Vocabulary name not translated in page title of the taxonomy overview and reset forms

--- core/modules/taxonomy/src/Entity/Routing/VocabularyRouteProvider.php
+++ core/modules/taxonomy/src/Entity/Routing/VocabularyRouteProvider.php
@@ -51,6 +51,11 @@ class VocabularyRouteProvider extends AdminHtmlRouteProvider {
     $route->setDefault('_title', 'Reset');
     $route->setRequirement('_permission', $entity_type->getAdminPermission());
     $route->setOption('_admin_route', TRUE);
+    $route->setOption('parameters', [
+      'taxonomy_vocabulary' => [
+        'with_config_overrides' => TRUE,
+      ],
+    ]);
 
     return $route;
   }
@@ -66,10 +71,15 @@ class VocabularyRouteProvider extends AdminHtmlRouteProvider {
    */
   protected function getOverviewPageRoute(EntityTypeInterface $entity_type) {
     $route = new Route('/admin/structure/taxonomy/manage/{taxonomy_vocabulary}/overview');
-    $route->setDefault('_title_callback', 'Drupal\taxonomy\Controller\TaxonomyController::vocabularyTitle');
+    $route->setDefault('_title_callback', '\Drupal\Core\Entity\Controller\EntityController::title');
     $route->setDefault('_form', 'Drupal\taxonomy\Form\OverviewTerms');
     $route->setRequirement('_entity_access', 'taxonomy_vocabulary.access taxonomy overview');
     $route->setOption('_admin_route', TRUE);
+    $route->setOption('parameters', [
+      'taxonomy_vocabulary' => [
+        'with_config_overrides' => TRUE,
+      ],
+    ]);
 
     return $route;
   }
