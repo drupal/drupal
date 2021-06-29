63de964336e8aaeeba4019ddb9fd626f57df06a3
bnjmnm <benm@umich.edu>
2021-06-29 11:23:22 -0400

    update docs

--- core/misc/active-link.es6.js
+++ core/misc/active-link.es6.js
@@ -8,10 +8,9 @@
    * Append is-active class.
    *
    * The link is only active if its path corresponds to the current path, the
-   * language of the linked path is equal to the current language, and if the
-   * query parameters of the link equal those of the current request, since the
-   * same request with different query parameters may yield a different page
-   * (e.g. pagers, exposed View filters).
+   * language of the linked path is equal to the current language, and - for
+   * elements with the data-drupal-link-query attribute - the value of that
+   * attribute must match the query parameters of the current request.
    *
    * Does not discriminate based on element type, so allows you to set the
    * is-active class on any element: a, li…
