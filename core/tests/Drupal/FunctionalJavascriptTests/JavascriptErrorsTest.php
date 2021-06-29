65137d3bcba63af4d413f984639a5695b9fb13dc
tedbow <tedbow@240860.no-reply.drupal.org>
2021-06-29 16:28:49 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- core/tests/Drupal/FunctionalJavascriptTests/JavascriptErrorsTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/JavascriptErrorsTest.php
@@ -25,7 +25,7 @@ class JavascriptErrorsTest extends WebDriverTestBase {
    */
   public function testJavascriptErrors(): void {
     $this->expectDeprecation('Not failing JavaScript test for JavaScript errors is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. This test had the following JavaScript errors: Error: A manually thrown error.');
-    // Visit page that will throw a Javascript console error.
+    // Visit page that will throw a JavaScript console error.
     $this->drupalGet('js_errors_test');
     // Ensure that errors from previous page loads will be
     // detected.
