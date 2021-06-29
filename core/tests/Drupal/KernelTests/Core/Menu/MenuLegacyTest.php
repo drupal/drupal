c53309c0bfcc85486643099e0f909b92a11c1ae3
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2021-06-29 15:24:10 +0000

    Apply 2 suggestion(s) to 1 file(s)

--- core/tests/Drupal/KernelTests/Core/Menu/MenuLegacyTest.php
+++ core/tests/Drupal/KernelTests/Core/Menu/MenuLegacyTest.php
@@ -5,7 +5,7 @@ namespace Drupal\KernelTests\Core\Menu;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
- * Deprecation tests cases for the menu layer.
+ * Deprecation test cases for the menu layer.
  *
  * @group legacy
  */
@@ -14,7 +14,7 @@ class MenuLegacyTest extends KernelTestBase {
   /**
    * Tests deprecation of the menu_list_system_menus() function.
    */
-  public function testListSystemMenus() {
+  public function testListSystemMenus(): void {
     $this->expectDeprecation('menu_list_system_menus() is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. Use \Drupal\system\Entity\Menu::loadMultiple() instead. See https://www.drupal.org/node/1882552');
     $this->assertSame([
       'tools' => 'Tools',
