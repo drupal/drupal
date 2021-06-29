e182007f0b608732a1d5c44c99afdbaf4653b82f
Alex Pott <alex.a.pott@googlemail.com>
2021-06-29 22:14:59 +0100

    Issue #3174107 by brianperry, mglaman, mtift, alexpott, mradcliffe, ravi.shankar, capysara, cathwaldron, kostyashupenko, mherchel:  Add additional testing coverage for Olivero

---
+++ core/tests/Drupal/Nightwatch/Tests/Olivero/oliveroCommentTest.js
@@ -0,0 +1,41 @@
+const commentTitleSelector = 'h2.comments__title';
+const commentCountSelector = 'h2.comments__title .comments__count';
+
+module.exports = {
+  '@tags': ['core', 'olivero'],
+  before(browser) {
+    browser
+      .drupalInstall({
+        setupFile:
+          'core/tests/Drupal/TestSite/TestSiteOliveroInstallTestScript.php',
+        installProfile: 'minimal',
+      })
+      .drupalCreateUser({
+        name: 'user',
+        password: '123',
+        permissions: [
+          'access comments',
+          'post comments',
+          'skip comment approval',
+        ],
+      })
+      .drupalLogin({ name: 'user', password: '123' });
+  },
+  after(browser) {
+    browser.drupalUninstall();
+  },
+  'Article without comments should not display count': (browser) => {
+    browser
+      .drupalRelativeURL('/node/1')
+      .assert.containsText('body', 'Article without comments')
+      .assert.not.elementPresent(commentTitleSelector);
+  },
+  'Article with comments should display count': (browser) => {
+    browser
+      .drupalRelativeURL('/node/2')
+      .assert.containsText('body', 'Article with comments')
+      .assert.elementPresent(commentTitleSelector)
+      .assert.elementPresent(commentCountSelector)
+      .assert.containsText(commentCountSelector, '2');
+  },
+};
