cragr/drupal-lab

Move _drupal_flush_css_js function into the service

core/modules/ckeditor/tests/src/Functional/CKEditorLoadingTest.php
@@ -169,11 +169,11 @@ class CKEditorLoadingTest extends BrowserTestBase {
     // comparing the setting sent with the page with the current query string.
     $settings = $this->getDrupalSettings();
     $expected = $settings['ckeditor']['timestamp'];
-    $this->assertIdentical($expected, \Drupal::state()->get('system.css_js_query_string'), "CKEditor scripts cache-busting string is correct before flushing all caches.");
+    $this->assertIdentical($expected, \Drupal::service('cache.query_string')->get(), "CKEditor scripts cache-busting string is correct before flushing all caches.");
     // Flush all caches then make sure that $settings['ckeditor']['timestamp']
     // still matches.
     Rebuilder::rebuildAll();
-    $this->assertIdentical($expected, \Drupal::state()->get('system.css_js_query_string'), "CKEditor scripts cache-busting string is correct after flushing all caches.");
+    $this->assertIdentical($expected, \Drupal::service('cache.query_string')->get(), "CKEditor scripts cache-busting string is correct after flushing all caches.");
   }
 
   /**

drupal/drupal

core/modules/ckeditor/tests/src/Functional/CKEditorLoadingTest.php
@@ -171,11 +171,11 @@ class CKEditorLoadingTest extends BrowserTestBase {
     // comparing the setting sent with the page with the current query string.
     $settings = $this->getDrupalSettings();
     $expected = $settings['ckeditor']['timestamp'];
-    $this->assertSame($expected, \Drupal::state()->get('system.css_js_query_string'), "CKEditor scripts cache-busting string is correct before flushing all caches.");
+    $this->assertSame($expected, \Drupal::service('cache.query_string')->get(), "CKEditor scripts cache-busting string is correct before flushing all caches.");
     // Flush all caches then make sure that $settings['ckeditor']['timestamp']
     // still matches.
     Rebuilder::rebuildAll();
-    $this->assertSame($expected, \Drupal::state()->get('system.css_js_query_string'), "CKEditor scripts cache-busting string is correct after flushing all caches.");
+    $this->assertSame($expected, \Drupal::service('cache.query_string')->get(), "CKEditor scripts cache-busting string is correct after flushing all caches.");
   }
 
   /**