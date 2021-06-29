1b96c5e5ac10d9ec6278ae5944038b120bbf7901
Phéna Proxima <adam@phenaproxima.net>
2021-06-29 12:12:30 -0400

    Fix more JavaScript nits

--- core/tests/Drupal/FunctionalJavascriptTests/JavascriptErrorsTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/JavascriptErrorsTest.php
@@ -24,7 +24,7 @@ class JavascriptErrorsTest extends WebDriverTestBase {
    * Tests that JavaScript console errors will result in a deprecation warning.
    */
   public function testJavascriptErrors(): void {
-    $this->expectDeprecation('Not failing Javascript test for Javascript errors is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. This test had the following Javascript errors: Error: A manually thrown error.');
+    $this->expectDeprecation('Not failing JavaScript test for JavaScript errors is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. This test had the following JavaScript errors: Error: A manually thrown error.');
     // Visit page that will throw a Javascript console error.
     $this->drupalGet('js_errors_test');
     // Ensure that errors from previous page loads will be
--- core/tests/Drupal/FunctionalJavascriptTests/WebDriverTestBase.php
+++ core/tests/Drupal/FunctionalJavascriptTests/WebDriverTestBase.php
@@ -17,7 +17,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 abstract class WebDriverTestBase extends BrowserTestBase {
 
   /**
-   * Determines if a test should fail on Javascript console errors.
+   * Determines if a test should fail on JavaScript console errors.
    *
    * @var bool
    */
@@ -119,7 +119,7 @@ abstract class WebDriverTestBase extends BrowserTestBase {
         $errors = $this->getSession()->evaluateScript("JSON.parse(sessionStorage.getItem('js_testing_log_test.errors') || JSON.stringify([]))");
         if (!empty($errors)) {
           $all_errors = implode("\n", $errors);
-          @trigger_error("Not failing Javascript test for Javascript errors is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. This test had the following Javascript errors: $all_errors. See https://www.drupal.org/node/3221100", E_USER_DEPRECATED);
+          @trigger_error("Not failing JavaScript test for Javascript errors is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. This test had the following JavaScript errors: $all_errors. See https://www.drupal.org/node/3221100", E_USER_DEPRECATED);
         }
       }
 
