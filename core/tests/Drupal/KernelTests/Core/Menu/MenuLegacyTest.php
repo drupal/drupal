150b8596a2922239a36ea1de76eaca6034d1abcf
Ted Bowman <ted@tedbow.com>
2021-06-29 12:15:30 -0400

    add testMenuUiGetMenus

--- core/tests/Drupal/KernelTests/Core/Menu/MenuLegacyTest.php
+++ core/tests/Drupal/KernelTests/Core/Menu/MenuLegacyTest.php
@@ -12,6 +12,11 @@ use Drupal\KernelTests\KernelTestBase;
 class MenuLegacyTest extends KernelTestBase {
 
   /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['system', 'user'];
+
+  /**
    * Tests deprecation of the menu_list_system_menus() function.
    */
   public function testListSystemMenus(): void {
@@ -23,6 +28,15 @@ class MenuLegacyTest extends KernelTestBase {
       'main' => 'Main navigation',
       'footer' => 'Footer menu',
     ], menu_list_system_menus());
+  }
+
+  /**
+   * Tests deprecation of the menu_ui_get_menus() function.
+   */
+  public function testMenuUiGetMenus(): void {
+    $this->container->get('module_installer')->install(['menu_ui']);
+    $this->expectDeprecation('menu_ui_get_menus() is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. Use \Drupal\system\Entity\Menu::loadMultiple() instead. See https://www.drupal.org/node/1882552');
+    $this->assertSame([], menu_ui_get_menus());
   }
 
 }
