a0868de4a7a91d9a0f9da0936313fa91ced0429b
Claudiu Cristea <clau.cristea@gmail.com>
2021-07-03 17:42:12 +0300

    Add deprecation message testing

--- core/tests/Drupal/KernelTests/Core/Bootstrap/ResettableStaticTest.php
+++ core/tests/Drupal/KernelTests/Core/Bootstrap/ResettableStaticTest.php
@@ -8,6 +8,7 @@ use Drupal\KernelTests\KernelTestBase;
  * Tests that drupal_static() and drupal_static_reset() work.
  *
  * @group Bootstrap
+ * @group legacy
  */
 class ResettableStaticTest extends KernelTestBase {
 
@@ -19,6 +20,7 @@ class ResettableStaticTest extends KernelTestBase {
    */
   public function testDrupalStatic() {
     $name = __CLASS__ . '_' . __METHOD__;
+    $this->expectDeprecation("Calling drupal_static() with '{$name}' as first argument is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. There is no replacement for this usage. See https://www.drupal.org/node/2661204");
     $var = &drupal_static($name, 'foo');
     $this->assertEquals('foo', $var, 'Variable returned by drupal_static() was set to its default.');
 
@@ -28,6 +30,7 @@ class ResettableStaticTest extends KernelTestBase {
     drupal_static_reset($name);
     $this->assertEquals('foo', $var, 'Variable was reset after first invocation of name-specific reset.');
     $var = 'bar';
+    $this->expectDeprecation("Calling drupal_static_reset() with '{$name}' as argument is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. See https://www.drupal.org/node/2661204");
     drupal_static_reset($name);
     $this->assertEquals('foo', $var, 'Variable was reset after second invocation of name-specific reset.');
     $var = 'bar';
@@ -36,6 +39,10 @@ class ResettableStaticTest extends KernelTestBase {
     $var = 'bar';
     drupal_static_reset();
     $this->assertEquals('foo', $var, 'Variable was reset after second invocation of global reset.');
+
+    // Check that authorized callers don't trigger deprecation errors.
+    drupal_static('_batch_needs_update', 'foo');
+    drupal_static_reset('_batch_needs_update');
   }
 
 }
