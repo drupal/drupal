099307094f167dca8489fea09072d6cd12032be0
Clay Freeman <git@clayfreeman.com>
2021-07-08 12:49:28 -0500

    attempt to fix ProjectSecurityRequirement construction

--- core/modules/update/src/ProjectSecurityRequirement.php
+++ core/modules/update/src/ProjectSecurityRequirement.php
@@ -116,9 +116,9 @@ final class ProjectSecurityRequirement {
       list($major, $minor) = explode('.', $project_data['existing_version']);
       $existing_version = "$major.$minor";
       $next_version = "$major." . ((int) $minor + 1);
-      return new static($project_data['title'], $security_coverage_info, $existing_version, $next_version);
+      return new static($project_data['title'], $security_coverage_info, $existing_version, $next_version, \Drupal::time());
     }
-    return new static($project_data['title'], $security_coverage_info);
+    return new static($project_data['title'], $security_coverage_info, NULL, NULL, \Drupal::time());
   }
 
   /**
