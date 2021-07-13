88a28b9336ab74ab8f76580b724cfeaa02752ca6
Alex Pott <alex.a.pott@googlemail.com>
2021-07-13 01:36:03 +0100

    Remove deprecation skipping

--- core/lib/Drupal/Core/Test/HttpClientMiddleware/TestHttpClientMiddleware.php
+++ core/lib/Drupal/Core/Test/HttpClientMiddleware/TestHttpClientMiddleware.php
@@ -39,7 +39,7 @@ class TestHttpClientMiddleware {
                 foreach ($header_values as $header_value) {
                   $parameters = unserialize(urldecode($header_value));
                   if (count($parameters) === 3) {
-                    if ($parameters[1] === 'User deprecated function' || $parameters[1] === 'Deprecated function') {
+                    if ($parameters[1] === 'User deprecated function') {
                       // Fire the same deprecation message to allow it to be
                       // collected by
                       // \Symfony\Bridge\PhpUnit\DeprecationErrorHandler::collectDeprecations().
