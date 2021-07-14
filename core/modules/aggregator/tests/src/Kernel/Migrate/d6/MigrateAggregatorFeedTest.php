15074c9bf60af98cc950d9783f138c8eea4ec9c3
Alex Pott <alex.a.pott@googlemail.com>
2021-07-14 01:01:37 +0100

    Fix core/modules/aggregator/tests/src/Kernel/Migrate/d6 tests

--- core/modules/aggregator/tests/src/Kernel/Migrate/d6/MigrateAggregatorFeedTest.php
+++ core/modules/aggregator/tests/src/Kernel/Migrate/d6/MigrateAggregatorFeedTest.php
@@ -35,14 +35,14 @@ class MigrateAggregatorFeedTest extends MigrateDrupal6TestBase {
     $this->assertSame('Know Your Meme', $feed->title->value);
     $this->assertSame('en', $feed->language()->getId());
     $this->assertSame('http://knowyourmeme.com/newsfeed.rss', $feed->url->value);
-    $this->assertSame('900', $feed->refresh->value);
-    $this->assertSame('1387659487', $feed->checked->value);
-    $this->assertSame('0', $feed->queued->value);
+    $this->assertSame(900, (int) $feed->refresh->value);
+    $this->assertSame(1387659487, (int) $feed->checked->value);
+    $this->assertSame(0, (int) $feed->queued->value);
     $this->assertSame('http://knowyourmeme.com', $feed->link->value);
     $this->assertSame('New items added to the News Feed', $feed->description->value);
     $this->assertSame('http://b.thumbs.redditmedia.com/harEHsUUZVajabtC.png', $feed->image->value);
     $this->assertSame('"213cc1365b96c310e92053c5551f0504"', $feed->etag->value);
-    $this->assertSame('0', $feed->modified->value);
+    $this->assertSame(0, (int) $feed->modified->value);
   }
 
 }
