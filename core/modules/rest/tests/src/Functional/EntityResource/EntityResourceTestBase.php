6d74b7851b9b37c13cc164d8e75b512aeb1b5983
Alex Pott <alex.a.pott@googlemail.com>
2021-07-16 13:30:16 +0100

    Fix \Drupal\Tests\aggregator\Functional\Hal\FeedHalJsonAnonTest

--- core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
+++ core/modules/rest/tests/src/Functional/EntityResource/EntityResourceTestBase.php
@@ -1428,7 +1428,9 @@ abstract class EntityResourceTestBase extends ResourceTestBase {
           }
           // Fields are stored in the database, when read they are represented
           // as strings in PHP memory.
-          $expected_stored_data = static::castToString($expected_stored_data);
+          if (PHP_VERSION_ID < 80100) {
+            $expected_stored_data = static::castToString($expected_stored_data);
+          }
         }
         $this->assertEntityArraySubset($expected_stored_data, $modified_entity->get($field_name)->getValue());
       }
