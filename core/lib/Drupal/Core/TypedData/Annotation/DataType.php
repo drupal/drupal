593b09f97a825b8df8ff514e17a23f7a3fa6a122
catch <catch@35733.no-reply.drupal.org>
2021-08-06 07:57:28 +0100

    Issue #2047119 by fago, msankhala, LinL: Remove deprecated documentation in DataType annotation
    (cherry picked from commit 5d7ae0590544c8c9548e28854b71ad22dd03216a)

--- core/lib/Drupal/Core/TypedData/Annotation/DataType.php
+++ core/lib/Drupal/Core/TypedData/Annotation/DataType.php
@@ -10,11 +10,11 @@ use Drupal\Component\Annotation\Plugin;
  * The typed data API allows modules to support any kind of data based upon
  * pre-defined primitive types and interfaces for complex data and lists.
  *
- * Defined data types may map to one of the pre-defined primitive types in
- * \Drupal\Core\TypedData\Primitive or may be complex data types, containing on
- * or more data properties. Typed data objects for complex data types have to
- * implement the \Drupal\Core\TypedData\ComplexDataInterface. Further interface
- * that may be implemented are:
+ * Defined data types may map to one of the pre-defined primitive types below
+ * \Drupal\Core\TypedData\Type or may be complex data types, containing
+ * one or more data properties. Typed data objects for complex data types have
+ * to implement the \Drupal\Core\TypedData\ComplexDataInterface. Further
+ * interfaces that may be implemented are:
  *  - \Drupal\Core\Access\AccessibleInterface
  *  - \Drupal\Core\TypedData\TranslatableInterface
  *
@@ -81,16 +81,6 @@ class DataType extends Plugin {
    * @var string
    */
   public $list_definition_class = '\Drupal\Core\TypedData\ListDataDefinition';
-
-  /**
-   * The pre-defined primitive type that this data type maps to.
-   *
-   * If set, it must be a constant defined by \Drupal\Core\TypedData\Primitive
-   * such as \Drupal\Core\TypedData\Primitive::STRING.
-   *
-   * @var string
-   */
-  public $primitive_type;
 
   /**
    * An array of validation constraints for this type.
