e1ab113380cc4936ce9934085ede2867eff67dbd
catch <catch@35733.no-reply.drupal.org>
2021-09-07 15:46:36 +0100

    Issue #2794261 by longwave, andypost, Oleksiy, Mile23, pguillard, sorlov, voleger, jurgenhaas, Chi, borisson_, paulocs, Wim Leers, Berdir: Deprecate render() function in common.inc

--- core/modules/views/src/Plugin/views/field/EntityField.php
+++ core/modules/views/src/Plugin/views/field/EntityField.php
@@ -931,7 +931,7 @@ class EntityField extends FieldPluginBase implements CacheableDependencyInterfac
   }
 
   public function render_item($count, $item) {
-    return render($item['rendered']);
+    return $this->renderer->render($item['rendered']);
   }
 
   protected function documentSelfTokens(&$tokens) {
