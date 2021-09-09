e13e5590edf6be489da5b06c2500e9b07e7a5ee7
Lee Rowlands <lee.rowlands@previousnext.com.au>
2021-09-09 10:00:01 +1000

    Issue #3104980 by danflanagan8, acbramley: layout_builder_system_breadcrumb_alter doesn't check for a null route object
    (cherry picked from commit ef493a71d5e4a74e8809a3a778985df08d48d4bf)

---
+++ core/modules/layout_builder/tests/src/Kernel/LayoutBuilderBreadcrumbAlterTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\Tests\layout_builder\Kernel;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Routing\NullRouteMatch;
+use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
+
+/**
+ * Tests layout_builder_system_breadcrumb_alter().
+ *
+ * @group layout_builder
+ */
+class LayoutBuilderBreadcrumbAlterTest extends EntityKernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'layout_builder',
+    'layout_discovery',
+  ];
+
+  /**
+   * Check that there are no errors when alter called with null route match.
+   */
+  public function testBreadcrumbAlterNullRouteMatch() {
+    $breadcrumb = new Breadcrumb();
+    $route_match = new NullRouteMatch();
+    layout_builder_system_breadcrumb_alter($breadcrumb, $route_match, []);
+  }
+
+}
