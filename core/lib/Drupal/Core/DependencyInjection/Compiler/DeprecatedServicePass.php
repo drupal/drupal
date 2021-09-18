57a900bbdb7144ec1200c48d19c76449f190b4b3
Lee Rowlands <lee.rowlands@previousnext.com.au>
2021-09-18 11:25:03 +1000

    Issue #3231393 by catch, daffie, longwave: [Symfony 6] Symfony\Component\DependencyInjection\Alias::getDeprecationMessage() and Symfony\Component\DependencyInjection\Definition::getDeprecationMessage() method is deprecated, use getDeprecation()

--- core/lib/Drupal/Core/DependencyInjection/Compiler/DeprecatedServicePass.php
+++ core/lib/Drupal/Core/DependencyInjection/Compiler/DeprecatedServicePass.php
@@ -19,12 +19,26 @@ class DeprecatedServicePass implements CompilerPassInterface {
     $deprecated_services = [];
     foreach ($container->getDefinitions() as $service_id => $definition) {
       if ($definition->isDeprecated()) {
-        $deprecated_services[$service_id] = $definition->getDeprecationMessage($service_id);
+        // @todo: remove when Drupal requires Symfony 5. See
+        // https://www.drupal.org/project/drupal/issues/3197482
+        if (method_exists($definition, 'getDeprecation')) {
+          $deprecated_services[$service_id] = $definition->getDeprecation($service_id)['message'];
+        }
+        else {
+          $deprecated_services[$service_id] = $definition->getDeprecationMessage($service_id);
+        }
       }
     }
     foreach ($container->getAliases() as $service_id => $definition) {
       if ($definition->isDeprecated()) {
-        $deprecated_services[$service_id] = $definition->getDeprecationMessage($service_id);
+        // @todo: remove when Drupal requires Symfony 5. See
+        // https://www.drupal.org/project/drupal/issues/3197482
+        if (method_exists($definition, 'getDeprecation')) {
+          $deprecated_services[$service_id] = $definition->getDeprecation($service_id)['message'];
+        }
+        else {
+          $deprecated_services[$service_id] = $definition->getDeprecationMessage($service_id);
+        }
       }
     }
     $container->setParameter('_deprecated_service_list', $deprecated_services);
