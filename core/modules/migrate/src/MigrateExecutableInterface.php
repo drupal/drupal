faa874be00621b6263ae099067a4f9cae2d90b3c
catch <catch@35733.no-reply.drupal.org>
2021-09-29 15:49:54 +0100

    Issue #3222844 by guilhermevp, paulocs, dhirendra.mishra, joachim, quietone, longwave: Add documentation for the return value of MigrateExecutableInterface::import()

--- core/modules/migrate/src/MigrateExecutableInterface.php
+++ core/modules/migrate/src/MigrateExecutableInterface.php
@@ -8,6 +8,13 @@ interface MigrateExecutableInterface {
 
   /**
    * Performs an import operation - migrate items from source to destination.
+   *
+   * @return int
+   *   Returns a value indicating the status of the import operation.
+   *   The possible values are the 'RESULT_' constants defined
+   *   in MigrationInterface.
+   *
+   * @see \Drupal\migrate\Plugin\MigrationInterface
    */
   public function import();
 
