f8f27526a993682322b7d950efb95baeaa2f7f62
Lee Rowlands <lee.rowlands@previousnext.com.au>
2021-09-30 11:52:51 +1000

    Issue #3239859 by alexpott, longwave: \Drupal\Core\Template\Loader\ThemeRegistryLoader::getCacheKey() causes deprecations on PHP 8.1

--- core/lib/Drupal/Core/Template/Loader/ThemeRegistryLoader.php
+++ core/lib/Drupal/Core/Template/Loader/ThemeRegistryLoader.php
@@ -69,4 +69,13 @@ class ThemeRegistryLoader extends FilesystemLoader {
     return FALSE;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheKey($name) {
+    // The parent implementation does unnecessary work that triggers
+    // deprecations in PHP 8.1.
+    return $this->findTemplate($name) ?: '';
+  }
+
 }
