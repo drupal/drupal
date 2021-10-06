0223496952a42bd0e393fd6b0159b4c5f5f05986
Alex Pott <alex.a.pott@googlemail.com>
2021-10-06 20:50:03 +0100

    Issue #3240911 by andypost: language_test_page_top() causes deprecations on PHP 8.1

--- core/modules/language/src/ConfigurableLanguageManagerInterface.php
+++ core/modules/language/src/ConfigurableLanguageManagerInterface.php
@@ -98,7 +98,7 @@ interface ConfigurableLanguageManagerInterface extends LanguageManagerInterface 
    *   (optional) The language type; e.g., the interface or the content
    *   language.
    *
-   * @return string
+   * @return string|null
    *   The negotiated language method ID.
    */
   public function getNegotiatedLanguageMethod($type = LanguageInterface::TYPE_INTERFACE);
