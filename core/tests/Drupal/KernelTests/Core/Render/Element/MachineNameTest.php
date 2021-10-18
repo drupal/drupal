4ba14b66af8d01a6705029c490ab81e0788a6d44
Alex Pott <alex.a.pott@googlemail.com>
2021-10-18 15:19:33 +0100

    Issue #990218 by Lendude, sun, tim.plunkett, mr.baileys, joachim: Machine name field throws notices if before source field
    (cherry picked from commit 25718d36d4aa819b8baea61d2c213239aaaaef8f)

---
+++ core/tests/Drupal/KernelTests/Core/Render/Element/MachineNameTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\KernelTests\Core\Render\Element;
+
+use Drupal\Core\Form\FormInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * @coversDefaultClass \Drupal\Core\Render\Element\MachineName
+ * @group Render
+ */
+class MachineNameTest extends KernelTestBase implements FormInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['system'];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return __CLASS__;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $element = [
+      '#id' => 'test',
+      '#type' => 'machine_name',
+      '#machine_name' => [
+        'source' => [
+          'test_source',
+        ],
+      ],
+      '#name' => 'test_machine_name',
+      '#default_value' => NULL,
+    ];
+
+    $complete_form = [
+      'test_machine_name' => $element,
+      'test_source' => [
+        '#type' => 'textfield',
+      ],
+    ];
+    return $complete_form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * Tests the order of the machine name field and the source.
+   */
+  public function testMachineNameOrderException() {
+    $this->expectException(\LogicException::class);
+    $this->expectErrorMessage('The machine name element "test_machine_name" is defined before the source element "test_source", it must be defined after or the source element must specify an id.');
+    $form = \Drupal::formBuilder()->getForm($this);
+    $this->render($form);
+  }
+
+}
