b7e8bd496e89337046ee940b3b5691a719c31174
catch <catch@35733.no-reply.drupal.org>
2021-10-29 11:32:32 +0100

    Issue #3244855 by nod_, alexpott, catch: Regression in Drupal 9.3.x due to jQuery.once deprecation

--- core/misc/jquery.once.bc.es6.js
+++ core/misc/jquery.once.bc.es6.js
@@ -12,18 +12,40 @@
  */
 
 (($, once) => {
+  const deprecatedMessageSuffix = `is deprecated in Drupal 9.3.0 and will be removed in Drupal 10.0.0. Use the core/once library instead. See https://www.drupal.org/node/3158256`;
+
+  // Trigger a deprecation error when using jQuery.once methods.
+  const originalJQOnce = $.fn.once;
+  const originalJQRemoveOnce = $.fn.removeOnce;
+  // Do not deprecate findOnce because it is used internally by jQuery.once().
+
+  $.fn.once = function jQueryOnce(id) {
+    Drupal.deprecationError({
+      message: `jQuery.once() ${deprecatedMessageSuffix}`,
+    });
+    return originalJQOnce.apply(this, [id]);
+  };
+  $.fn.removeOnce = function jQueryRemoveOnce(id) {
+    Drupal.deprecationError({
+      message: `jQuery.removeOnce() ${deprecatedMessageSuffix}`,
+    });
+    return originalJQRemoveOnce.apply(this, [id]);
+  };
+
   // We'll replace the whole library so keep a version in cache for later.
   const drupalOnce = once;
 
   // When calling once(), also populate jQuery.once registry.
   function augmentedOnce(id, selector, context) {
-    $(selector, context).once(id);
+    // Do not trigger deprecation warnings for the BC layer calls.
+    originalJQOnce.apply($(selector, context), [id]);
     return drupalOnce(id, selector, context);
   }
 
   // When calling once.remove(), also remove it from jQuery.once registry.
   function remove(id, selector, context) {
-    $(selector, context).removeOnce(id);
+    // Do not trigger deprecation warnings for the BC layer calls.
+    originalJQRemoveOnce.apply($(selector, context), [id]);
     return drupalOnce.remove(id, selector, context);
   }
 
--- core/misc/jquery.once.bc.js
+++ core/misc/jquery.once.bc.js
@@ -6,15 +6,33 @@
 **/
 
 (function ($, once) {
+  var deprecatedMessageSuffix = "is deprecated in Drupal 9.3.0 and will be removed in Drupal 10.0.0. Use the core/once library instead. See https://www.drupal.org/node/3158256";
+  var originalJQOnce = $.fn.once;
+  var originalJQRemoveOnce = $.fn.removeOnce;
+
+  $.fn.once = function jQueryOnce(id) {
+    Drupal.deprecationError({
+      message: "jQuery.once() ".concat(deprecatedMessageSuffix)
+    });
+    return originalJQOnce.apply(this, [id]);
+  };
+
+  $.fn.removeOnce = function jQueryRemoveOnce(id) {
+    Drupal.deprecationError({
+      message: "jQuery.removeOnce() ".concat(deprecatedMessageSuffix)
+    });
+    return originalJQRemoveOnce.apply(this, [id]);
+  };
+
   var drupalOnce = once;
 
   function augmentedOnce(id, selector, context) {
-    $(selector, context).once(id);
+    originalJQOnce.apply($(selector, context), [id]);
     return drupalOnce(id, selector, context);
   }
 
   function remove(id, selector, context) {
-    $(selector, context).removeOnce(id);
+    originalJQRemoveOnce.apply($(selector, context), [id]);
     return drupalOnce.remove(id, selector, context);
   }
 
