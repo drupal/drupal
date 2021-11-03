c4debe701242cbc8237469b4b042ac4d5a307023
Alex Pott <alex.a.pott@googlemail.com>
2021-11-03 10:51:09 +0000

    Issue #3087332 by quietone, huzooka, amateescu, danflanagan8: Deprecate the 'd6_url_alias_language' migration process plugin
    (cherry picked from commit 04f2578519405e5bcf91fe281985402331c8b386)

--- core/modules/path/src/Plugin/migrate/process/d6/UrlAliasLanguage.php
+++ core/modules/path/src/Plugin/migrate/process/d6/UrlAliasLanguage.php
@@ -11,10 +11,24 @@ use Drupal\Core\Language\LanguageInterface;
  * Url alias language code process.
  *
  * @MigrateProcessPlugin(
- *   id = "d6_url_alias_language"
+ *   id = "d6_url_alias_language",
+ *   no_ui = TRUE
  * )
+ *
+ * @deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. No direct
+ *   replacement is provided.
+ *
+ * @see https://www.drupal.org/node/3219051
  */
 class UrlAliasLanguage extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    @trigger_error(__CLASS__ . ' is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. No direct replacement is provided. See https://www.drupal.org/node/3219051', E_USER_DEPRECATED);
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
 
   /**
    * {@inheritdoc}
