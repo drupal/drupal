87c12b1301a8e3356612c3e757871f6fd27a0806
mcdruid <drew@mcdruid.co.uk>
2021-11-25 07:41:14 +0000

    Issue #3026560 by alexpott, generalredneck, pbirk, plach, xjm, Hitby, kevinquillen, Alex Monaghan, greg.1.anderson, littlepixiez, bartvig: After upgrade to 7.63 still get TYPO3 phar error for drush

--- misc/typo3/phar-stream-wrapper/src/PharStreamWrapper.php
+++ misc/typo3/phar-stream-wrapper/src/PharStreamWrapper.php
@@ -156,6 +156,17 @@ class PharStreamWrapper
         );
     }
 
+    /**
+     * @param int $cast_as
+     */
+    public function stream_cast($cast_as)
+    {
+        throw new Exception(
+            'Method stream_select() cannot be used',
+            1530103999
+        );
+    }
+
     public function stream_close()
     {
         $this->invokeInternalStreamWrapper(
