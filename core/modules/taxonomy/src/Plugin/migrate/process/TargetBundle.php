88868d814e2c8734d42e648b0613d04393efe760
catch <catch@35733.no-reply.drupal.org>
2022-01-02 10:33:17 +0000

    Issue #3042533 by quietone, danflanagan8: D6 taxonomy term fields are not migrated with allowed vocabularies
    (cherry picked from commit 66449600c6daa3e709462fbb178492f845e389cb)

---
+++ core/modules/taxonomy/src/Plugin/migrate/process/TargetBundle.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\taxonomy\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Converts a Drupal 6 vocabulary ID to a target bundle array.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "target_bundle"
+ * )
+ */
+class TargetBundle extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $target_bundle = [];
+    $vid = $row->get('@_vid');
+    $target_bundle[$vid] = $vid;
+    return $target_bundle;
+  }
+
+}
