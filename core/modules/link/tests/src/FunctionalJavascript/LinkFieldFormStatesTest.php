a555205277089140865f59856efbf4deb89877bf
catch <catch@35733.no-reply.drupal.org>
2022-01-04 11:26:23 +0000

    Issue #2879293 by digitaldonkey, tobiasb, Mac_Weber, paulocs, Meenakshi_j, stefan.korn, ranjith_kumar_k_u, longwave, alexpott: Make Link URI required if there is Link Text input
    (cherry picked from commit c944285b83c56902d1d50c0481ded7ba571b5e95)

---
+++ core/modules/link/tests/src/FunctionalJavascript/LinkFieldFormStatesTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\link\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+
+/**
+ * Tests link field form states functionality.
+ *
+ * @group link
+ */
+class LinkFieldFormStatesTest extends WebDriverTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'entity_test',
+    'link',
+    'node',
+    'link_test_base_field',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->drupalLogin($this->drupalCreateUser([
+      'administer entity_test content',
+    ]));
+  }
+
+  /**
+   * @dataProvider linkFieldFormStatesData
+   */
+  public function testLinkFieldFormStates(string $uri, string $title) {
+    $this->drupalGet('entity_test/add');
+    $session = $this->assertSession();
+    $session->elementNotExists('css', '#edit-links-0-uri[required]');
+    $session->elementNotExists('css', '#edit-links-0-title[required]');
+
+    $page = $this->getSession()->getPage();
+
+    if ($uri !== '') {
+      $page->fillField('links[0][uri]', $uri);
+      $session->elementNotExists('css', '#edit-links-0-uri[required]');
+      $session->elementExists('css', '#edit-links-0-title[required]');
+    }
+    else {
+      $page->fillField('links[0][title]', $title);
+      $session->elementExists('css', '#edit-links-0-uri[required]');
+      $session->elementNotExists('css', '#edit-links-0-title[required]');
+    }
+  }
+
+  /**
+   * Provides data for ::testLinkFieldJSFormStates.
+   */
+  public function linkFieldFormStatesData() {
+    return [
+      'Fill uri, keep title empty' => [
+        'https://drupal.org',
+        '',
+      ],
+      'Fill title, keep uri empty' => [
+        '',
+        'https://drupal.org',
+      ],
+    ];
+  }
+
+}
