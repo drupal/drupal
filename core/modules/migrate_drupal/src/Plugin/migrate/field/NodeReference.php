c5bd67a8d1e8e2ef80b999e6990c710c80644c03
catch <catch@35733.no-reply.drupal.org>
2022-01-12 12:39:14 +0000

    Issue #3258014 by alexpott: Fix plugin deprecations

--- core/modules/migrate_drupal/src/Plugin/migrate/field/NodeReference.php
+++ core/modules/migrate_drupal/src/Plugin/migrate/field/NodeReference.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\migrate_drupal\Plugin\migrate\field;
 
-@trigger_error('The ' . __NAMESPACE__ . '\NodeReference is deprecated in drupal:9.1.0 and will be removed from drupal:10.0.0. Instead use \Drupal\migrate_drupal\Plugin\migrate\field\d6\NodeReference. See https://www.drupal.org/node/3159537.', E_USER_DEPRECATED);
-
 use Drupal\migrate_drupal\Plugin\migrate\field\d6\NodeReference as NonLegacyNodeReference;
 
 /**
@@ -13,4 +11,14 @@ use Drupal\migrate_drupal\Plugin\migrate\field\d6\NodeReference as NonLegacyNode
  * @see https://www.drupal.org/node/3159537
  */
 class NodeReference extends NonLegacyNodeReference {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    @trigger_error('The ' . __NAMESPACE__ . '\NodeReference is deprecated in drupal:9.1.0 and will be removed from drupal:10.0.0. Instead use \Drupal\migrate_drupal\Plugin\migrate\field\d6\NodeReference. See https://www.drupal.org/node/3159537.', E_USER_DEPRECATED);
+
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
 }
