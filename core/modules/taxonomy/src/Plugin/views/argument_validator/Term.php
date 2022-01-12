02a261ea9113f1871263007f64418fba162508a0
catch <catch@35733.no-reply.drupal.org>
2022-01-12 12:39:14 +0000

    Issue #3258014 by alexpott: Fix plugin deprecations
    (cherry picked from commit c5bd67a8d1e8e2ef80b999e6990c710c80644c03)

--- core/modules/taxonomy/src/Plugin/views/argument_validator/Term.php
+++ core/modules/taxonomy/src/Plugin/views/argument_validator/Term.php
@@ -2,8 +2,8 @@
 
 namespace Drupal\taxonomy\Plugin\views\argument_validator;
 
-@trigger_error('The ' . __NAMESPACE__ . '\Term is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. Use \Drupal\views\Plugin\views\argument_validator\Entity instead. See https://www.drupal.org/node/3221870', E_USER_DEPRECATED);
-
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\views\Plugin\views\argument_validator\Entity;
 
 /**
@@ -15,5 +15,14 @@ use Drupal\views\Plugin\views\argument_validator\Entity;
  * @see https://www.drupal.org/node/3221870
  */
 class Term extends Entity {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityTypeBundleInfoInterface $entity_type_bundle_info) {
+    @trigger_error('The ' . __NAMESPACE__ . '\Term is deprecated in drupal:9.3.0 and is removed from drupal:10.0.0. Use \Drupal\views\Plugin\views\argument_validator\Entity instead. See https://www.drupal.org/node/3221870', E_USER_DEPRECATED);
+
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $entity_type_bundle_info);
+  }
 
 }
