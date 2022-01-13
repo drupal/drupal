fc90d0c8990f3eb7ff861f557373aa8caf38106b
catch <catch@35733.no-reply.drupal.org>
2022-01-13 14:45:58 +0000

    Issue #2979588 by longwave, alexpott, Erik Frèrejean, catch, daffie: Deprecate Laminas\Feed reader and writer services

--- core/tests/Drupal/KernelTests/Core/LegacyServiceTest.php
+++ core/tests/Drupal/KernelTests/Core/LegacyServiceTest.php
@@ -30,4 +30,41 @@ class LegacyServiceTest extends KernelTestBase {
     $this->assertSame($this->container->get('app.root'), (string) $this->container->getParameter('app.root'));
   }
 
+  /**
+   * Tests the deprecated Laminas Feed services.
+   *
+   * @dataProvider providerLaminasFeedServices
+   */
+  public function testLaminasFeedServices($type, $service, $class) {
+    $service = "feed.$type.$service";
+    $this->expectDeprecation("The \"$service\" service is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. You should use \\Drupal::service('feed.bridge.$type')->get('$class') instead. See https://www.drupal.org/node/2979042");
+    $this->assertIsObject($this->container->get($service));
+  }
+
+  public function providerLaminasFeedServices() {
+    return [
+      ['reader', 'dublincoreentry', 'DublinCore\Entry'],
+      ['reader', 'dublincorefeed', 'DublinCore\Feed'],
+      ['reader', 'contententry', 'Content\Entry'],
+      ['reader', 'atomentry', 'Atom\Entry'],
+      ['reader', 'atomfeed', 'Atom\Feed'],
+      ['reader', 'slashentry', 'Slash\Entry'],
+      ['reader', 'wellformedwebentry', 'WellFormedWeb\Entry'],
+      ['reader', 'threadentry', 'Thread\Entry'],
+      ['reader', 'podcastentry', 'Podcast\Entry'],
+      ['reader', 'podcastfeed', 'Podcast\Feed'],
+      ['writer', 'atomrendererfeed', 'Atom\Renderer\Feed'],
+      ['writer', 'contentrendererentry', 'Content\Renderer\Entry'],
+      ['writer', 'dublincorerendererentry', 'DublinCore\Renderer\Entry'],
+      ['writer', 'dublincorerendererfeed', 'DublinCore\Renderer\Feed'],
+      ['writer', 'itunesentry', 'ITunes\Entry'],
+      ['writer', 'itunesfeed', 'ITunes\Feed'],
+      ['writer', 'itunesrendererentry', 'ITunes\Renderer\Entry'],
+      ['writer', 'itunesrendererfeed', 'ITunes\Renderer\Feed'],
+      ['writer', 'slashrendererentry', 'Slash\Renderer\Entry'],
+      ['writer', 'threadingrendererentry', 'Threading\Renderer\Entry'],
+      ['writer', 'wellformedwebrendererentry', 'WellFormedWeb\Renderer\Entry'],
+    ];
+  }
+
 }
