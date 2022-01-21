f5dc2cbd699a7cc0c1a9f81c94ee0b767fa2df88
Alex Pott <alex.a.pott@googlemail.com>
2022-01-21 11:45:00 +0000

    Issue #3259744 by Spokje: PHPUnit 9.5.12 (released 2022-01-21) throws unhandled deprecation notice on "Drupal\Tests\Listeners\DrupalListener"

--- core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
+++ core/tests/Drupal/Tests/Listeners/DeprecationListenerTrait.php
@@ -136,6 +136,7 @@ trait DeprecationListenerTrait {
       "The \"PHPUnit\Framework\TestSuite\" class is considered internal This class is not covered by the backward compatibility promise for PHPUnit. It may change without further notice. You should not use it from \"Drupal\Tests\TestSuites\TestSuiteBase\".",
       // PHPUnit 9.
       "The \"PHPUnit\TextUI\DefaultResultPrinter\" class is considered internal This class is not covered by the backward compatibility promise for PHPUnit. It may change without further notice. You should not use it from \"Drupal\Tests\Listeners\HtmlOutputPrinter\".",
+      "The \"Drupal\Tests\Listeners\DrupalListener\" class implements \"PHPUnit\Framework\TestListener\" that is deprecated.",
       // Guzzle/PSR-7
       "Method \"Psr\Http\Message\StreamInterface::getMetadata()\" will return \"mixed\" as of its next major version. Doing the same in implementation \"GuzzleHttp\Psr7\Stream\" will be required when upgrading.",
     ];
