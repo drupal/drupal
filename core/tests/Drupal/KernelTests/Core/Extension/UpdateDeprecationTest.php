241aa0d5ebd878738ec815a807d66e01ac731267
catch <catch@35733.no-reply.drupal.org>
2022-01-28 22:35:59 +0000

    Issue #3210931 by andypost: Remove deprecated update.inc functions

--- core/tests/Drupal/KernelTests/Core/Extension/UpdateDeprecationTest.php
+++ core/tests/Drupal/KernelTests/Core/Extension/UpdateDeprecationTest.php
@@ -25,25 +25,6 @@ class UpdateDeprecationTest extends KernelTestBase {
   }
 
   /**
-   * Tests update_check_incompatibility() function.
-   */
-  public function testUpdateCheckIncompatibility() {
-    $this->expectDeprecation('update_check_incompatibility() is deprecated in drupal:9.1.0 and is removed from drupal:10.0.0. No direct replacement is provided. See https://www.drupal.org/node/3150727');
-    $this->assertTrue(update_check_incompatibility('incompatible_module'));
-    $this->assertFalse(update_check_incompatibility('system'));
-  }
-
-  /**
-   * Tests update_set_schema() function.
-   */
-  public function testUpdateSetSchema() {
-    $this->expectDeprecation('update_set_schema() is deprecated in drupal:9.2.0 and is removed from drupal:10.0.0. No direct replacement is provided. See https://www.drupal.org/node/3210925');
-    update_set_schema('update_test_schema', 8003);
-    // Ensure schema has changed.
-    $this->assertEquals(8003, \Drupal::keyValue('system.schema')->get('update_test_schema'));
-  }
-
-  /**
    * Deprecation testing for drupal_get_schema_versions function.
    *
    * @see drupal_get_schema_versions()
