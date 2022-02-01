09c149e887d92d4d900a348f9b4a5e511184f392
Alex Pott <alex.a.pott@googlemail.com>
2022-02-01 12:57:44 +0000

    Issue #3254347 by murilohp, joachim, danflanagan8, quietone: Add the process plugin ID to migration exception message
    (cherry picked from commit f21dc6690eee3c648579a9e651af0664c0172dc3)

--- core/modules/field/tests/src/Kernel/Migrate/d7/MigrateFieldTest.php
+++ core/modules/field/tests/src/Kernel/Migrate/d7/MigrateFieldTest.php
@@ -207,7 +207,7 @@ class MigrateFieldTest extends MigrateDrupal7TestBase {
     $migration = $this->getMigration('d7_field');
     $messages = iterator_to_array($migration->getIdMap()->getMessages());
     $this->assertCount(5, $messages);
-    $msg = "d7_field:type: Can't migrate field 'field_event' with 'todate' settings. Enable the datetime_range module. See https://www.drupal.org/docs/8/upgrade/known-issues-when-upgrading-from-drupal-6-or-7-to-drupal-8#datetime";
+    $msg = "d7_field:type:process_field: Can't migrate field 'field_event' with 'todate' settings. Enable the datetime_range module. See https://www.drupal.org/docs/8/upgrade/known-issues-when-upgrading-from-drupal-6-or-7-to-drupal-8#datetime";
     $this->assertSame($messages[4]->message, $msg);
   }
 
