611d3abc728dd1735e2a8761886fd8b1532a1dbf
catch <catch@35733.no-reply.drupal.org>
2022-02-02 14:35:46 +0000

    Issue #3261262 by mondrake: Remove PHPUnit 8 warnings conversion to deprecations

--- core/tests/Drupal/Tests/Traits/PhpUnitWarnings.php
+++ core/tests/Drupal/Tests/Traits/PhpUnitWarnings.php
@@ -21,21 +21,6 @@ trait PhpUnitWarnings {
    * @var string[]
    */
   private static $deprecationWarnings = [
-    'Using assertContains() with string haystacks is deprecated and will not be supported in PHPUnit 9. Refactor your test to use assertStringContainsString() or assertStringContainsStringIgnoringCase() instead.',
-    'Using assertNotContains() with string haystacks is deprecated and will not be supported in PHPUnit 9. Refactor your test to use assertStringNotContainsString() or assertStringNotContainsStringIgnoringCase() instead.',
-    'assertArraySubset() is deprecated and will be removed in PHPUnit 9.',
-    'assertInternalType() is deprecated and will be removed in PHPUnit 9. Refactor your test to use assertIsArray(), assertIsBool(), assertIsFloat(), assertIsInt(), assertIsNumeric(), assertIsObject(), assertIsResource(), assertIsString(), assertIsScalar(), assertIsCallable(), or assertIsIterable() instead.',
-    'readAttribute() is deprecated and will be removed in PHPUnit 9.',
-    'getObjectAttribute() is deprecated and will be removed in PHPUnit 9.',
-    'The optional $canonicalize parameter of assertEquals() is deprecated and will be removed in PHPUnit 9. Refactor your test to use assertEqualsCanonicalizing() instead.',
-    'assertAttributeEquals() is deprecated and will be removed in PHPUnit 9.',
-    'assertAttributeSame() is deprecated and will be removed in PHPUnit 9.',
-    'assertAttributeInstanceOf() is deprecated and will be removed in PHPUnit 9.',
-    'assertAttributeEmpty() is deprecated and will be removed in PHPUnit 9.',
-    'The optional $ignoreCase parameter of assertContains() is deprecated and will be removed in PHPUnit 9.',
-    'The optional $ignoreCase parameter of assertNotContains() is deprecated and will be removed in PHPUnit 9.',
-    'expectExceptionMessageRegExp() is deprecated in PHPUnit 8 and will be removed in PHPUnit 9.',
-    'expectExceptionMessageRegExp() is deprecated in PHPUnit 8 and will be removed in PHPUnit 9. Use expectExceptionMessageMatches() instead.',
     // Warning for testing.
     'Test warning for \Drupal\Tests\PhpUnitWarningsTest::testAddWarning()',
     // PHPUnit 9.
@@ -64,12 +49,6 @@ trait PhpUnitWarnings {
     if (in_array($warning, self::$deprecationWarnings, TRUE)) {
       // Convert listed PHPUnit deprecations into E_USER_DEPRECATED and prevent
       // each from being raised as a test warning.
-      @trigger_error($warning, E_USER_DEPRECATED);
-      return;
-    }
-
-    // assertInternalType() has many similar deprecation warnings.
-    if (preg_match('/^assertInternalType\(\) is deprecated and will be removed in PHPUnit 9. Refactor your test to use assert.*\(\) instead.$/', $warning)) {
       @trigger_error($warning, E_USER_DEPRECATED);
       return;
     }
