9e6fedbdf80a1451b900f7770b7af0fa788a44ed
Alex Pott <alex.a.pott@googlemail.com>
2022-02-07 10:34:42 +0000

    Issue #3260781 by andypost: Remove deprecated module.inc functions

--- core/tests/Drupal/KernelTests/Core/Extension/ModuleLegacyTest.php
+++ core/tests/Drupal/KernelTests/Core/Extension/ModuleLegacyTest.php
@@ -22,14 +22,4 @@ class ModuleLegacyTest extends KernelTestBase {
 
   }
 
-  /**
-   * Test deprecation of module_load_install() function.
-   */
-  public function testModuleLoadInstall() {
-    $this->assertFalse(\Drupal::moduleHandler()->moduleExists('node'), 'The Node module is not installed');
-    $this->expectDeprecation('module_load_install() is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. Instead, you should use \Drupal::moduleHandler()->loadInclude($module, \'install\'). Note, the replacement no longer allows including code from uninstalled modules. See https://www.drupal.org/project/drupal/issues/2010380');
-    $filename = module_load_install('node');
-    $this->assertStringEndsWith("node.install", $filename);
-  }
-
 }
