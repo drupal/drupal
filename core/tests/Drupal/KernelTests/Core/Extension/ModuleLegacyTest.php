a87084f9f6a957c6c6b9826ce0385e83e9a36bd0
catch <catch@35733.no-reply.drupal.org>
2022-02-07 20:33:11 +0000

    Issue #3262805 by andypost, ravi.shankar, voleger, catch: Deprecate drupal_required_modules()

--- core/tests/Drupal/KernelTests/Core/Extension/ModuleLegacyTest.php
+++ core/tests/Drupal/KernelTests/Core/Extension/ModuleLegacyTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\KernelTests\Core\Extension;
 
+use Drupal\Core\Extension\ExtensionDiscovery;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
@@ -30,6 +31,26 @@ class ModuleLegacyTest extends KernelTestBase {
     $this->expectDeprecation('module_load_install() is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. Instead, you should use \Drupal::moduleHandler()->loadInclude($module, \'install\'). Note, the replacement no longer allows including code from uninstalled modules. See https://www.drupal.org/project/drupal/issues/2010380');
     $filename = module_load_install('node');
     $this->assertStringEndsWith("node.install", $filename);
+  }
+
+  /**
+   * Test deprecation of drupal_required_modules() function.
+   */
+  public function testDrupalRequiredModules() {
+    $this->expectDeprecation("drupal_required_modules() is deprecated in drupal:9.4.0 and is removed from drupal:10.0.0. There's no replacement. See https://www.drupal.org/node/3262811");
+    /** @var \Drupal\Core\Extension\InfoParserInterface $parser */
+    $parser = \Drupal::service('info_parser');
+    $listing = new ExtensionDiscovery(\Drupal::root());
+    $files = $listing->scan('module');
+    // Empty as there's no install profile.
+    $required = [];
+    foreach ($files as $name => $file) {
+      $info = $parser->parse($file->getPathname());
+      if (!empty($info) && !empty($info['required']) && $info['required']) {
+        $required[] = $name;
+      }
+    }
+    $this->assertSame($required, drupal_required_modules());
   }
 
 }
