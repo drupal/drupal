122c48f0d120b63a02593e6d722a62758b6119ab
Lauri Eskola <lauri.eskola@acquia.com>
2022-02-22 14:50:08 +0200

    Issue #3265230 by bnjmnm, Wim Leers: Refactor ie11.filter.warnings.es6.js to simpler structure & other improvements

--- core/modules/ckeditor5/js/ie11.filter.warnings.js
+++ core/modules/ckeditor5/js/ie11.filter.warnings.js
@@ -9,7 +9,7 @@
   Drupal.behaviors.ckEditor5warn = {
     attach: function attach() {
       const isIE11 = Modernizr.mq('(-ms-high-contrast: active), (-ms-high-contrast: none)');
-      const editorSelect = once('editor-select', document.querySelector('#filter-format-edit-form #edit-editor-editor, #filter-format-add-form #edit-editor-editor'));
+      const editorSelect = once('editor-ie11-warning', '[data-drupal-selector="filter-format-edit-form"] [data-drupal-selector="edit-editor-editor"], [data-drupal-selector="filter-format-add-form"] [data-drupal-selector="edit-editor-editor"]');
 
       if (typeof editorSelect[0] !== 'undefined') {
         const select = editorSelect[0];
@@ -18,51 +18,51 @@
         const selectMessages = new Drupal.Message(selectMessageContainer);
         const editorSettings = document.querySelector('#editor-settings-wrapper');
 
-        const ck5Warning = () => {
+        const addIE11Warning = () => {
           selectMessages.add(Drupal.t('CKEditor 5 is not compatible with Internet Explorer. Text fields using CKEditor 5 will fall back to plain HTML editing without CKEditor for users of Internet Explorer.'), {
-            type: 'warning'
+            type: 'warning',
+            id: 'ie_11_warning'
           });
 
           if (isIE11) {
             selectMessages.add(Drupal.t('Text editor toolbar settings are not available in Internet Explorer. They will be available in other <a href="@supported-browsers">supported browsers</a>.', {
               '@supported-browsers': 'https://www.drupal.org/docs/system-requirements/browser-requirements'
             }), {
-              type: 'error'
+              type: 'error',
+              id: 'ie_11_error'
             });
             editorSettings.hidden = true;
           }
         };
 
         const updateWarningStatus = () => {
-          if (select.value === 'ckeditor5' && !select.classList.contains('error')) {
-            ck5Warning();
+          if (select.value === 'ckeditor5' && !select.hasAttribute('data-error-switching-to-ckeditor5')) {
+            addIE11Warning();
           } else {
-            editorSettings.hidden = false;
-            selectMessages.clear();
-          }
-        };
+            if (selectMessages.select('ie_11_warning')) {
+              selectMessages.remove('ie_11_warning');
+            }
 
-        const selectChangeHandler = () => {
-          let editorSelectObserver = null;
-
-          function whenSelectAttributeChanges(mutations) {
-            for (let i = 0; i < mutations.length; i++) {
-              if (mutations[i].type === 'attributes' && mutations[i].attributeName === 'disabled' && !select.disabled) {
-                updateWarningStatus();
-                editorSelectObserver.disconnect();
-              }
+            if (selectMessages.select('ie_11_error')) {
+              selectMessages.remove('ie_11_error');
             }
           }
-
-          editorSelectObserver = new MutationObserver(whenSelectAttributeChanges);
-          editorSelectObserver.observe(select, {
-            attributes: true,
-            attributeOldValue: true
-          });
         };
 
         updateWarningStatus();
-        select.addEventListener('change', selectChangeHandler);
+        const editorSelectObserver = new MutationObserver(mutations => {
+          for (let i = 0; i < mutations.length; i++) {
+            const switchToCKEditor5Complete = mutations[i].type === 'attributes' && mutations[i].attributeName === 'disabled' && !select.disabled;
+            const fixedErrorsPreventingSwitchToCKEditor5 = mutations[i].type === 'attributes' && mutations[i].attributeName === 'data-error-switching-to-ckeditor5' && !select.hasAttribute('data-error-switching-to-ckeditor5');
+
+            if (switchToCKEditor5Complete || fixedErrorsPreventingSwitchToCKEditor5) {
+              updateWarningStatus();
+            }
+          }
+        });
+        editorSelectObserver.observe(select, {
+          attributes: true
+        });
       }
     }
   };
