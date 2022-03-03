77657fb6a31cc1a6032bcf41811e747553191ede
Lauri Eskola <lauri.eskola@acquia.com>
2022-03-03 12:24:18 +0200

    Issue #3260032 by longwave, bnjmnm, Wim Leers, samuel.mortenson: CKEditor 5 adds ie11.user.warnings library to every page, triggering a FOUC even for anonymous users

--- core/modules/quickedit/tests/src/FunctionalJavascript/CKEditor5IntegrationTest.php
+++ core/modules/quickedit/tests/src/FunctionalJavascript/CKEditor5IntegrationTest.php
@@ -205,9 +205,6 @@ class CKEditor5IntegrationTest extends QuickEditJavascriptTestBase {
 
     $this->drupalGet('node/' . $node->id());
 
-    // Confirm that the JavaScript that generates IE11 warnings loads.
-    $assert_session->elementExists('css', 'script[src*="ckeditor5/js/ie11.user.warnings.js"]');
-
     // Initial state.
     $this->awaitQuickEditForEntity('node', 1);
     $this->assertEntityInstanceStates([
@@ -228,6 +225,9 @@ class CKEditor5IntegrationTest extends QuickEditJavascriptTestBase {
       'node/1/title/en/full' => 'candidate',
       'node/1/body/en/full' => 'candidate',
     ]);
+
+    // Confirm that the JavaScript that generates IE11 warnings loads.
+    $assert_session->elementExists('css', 'script[src*="ckeditor5/js/ie11.user.warnings.js"]');
 
     // Click the body field.
     hold_test_response(TRUE);
