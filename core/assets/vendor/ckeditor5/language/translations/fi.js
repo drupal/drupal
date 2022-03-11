5e2c9139682cb043e7615f449fec04b33921a870
xjm <xjm@65776.no-reply.drupal.org>
2022-03-11 16:36:06 -0600

    Issue #3269064 by lauriii, xjm, Wim Leers: Update to CKEditor 5 v33.0.0

---
+++ core/assets/vendor/ckeditor5/language/translations/fi.js
@@ -0,0 +1,1 @@
+!function(i){const a=i.fi=i.fi||{};a.dictionary=Object.assign(a.dictionary||{},{"Choose language":"Valitse kieli",Language:"Kieli","Remove language":"Poista kieli"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
---
+++ core/assets/vendor/ckeditor5/remove-format/translations/el.js
@@ -0,0 +1,1 @@
+!function(i){const n=i.el=i.el||{};n.dictionary=Object.assign(n.dictionary||{},{"Remove Format":"Απομάκρυνση μορφοποίησης"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
