b02f8158ddf23f2d321a37bc5b2681138e967ea6
Alex Pott <alex.a.pott@googlemail.com>
2022-03-17 17:21:51 +0000

    Issue #3266308 by dww, murilohp, Vinodhini.E, alexpott, Kristen Pol: %extensions placeholder not extension names printed on the Status report warning about obsolete extensions

--- core/modules/system/tests/src/Functional/System/StatusTest.php
+++ core/modules/system/tests/src/Functional/System/StatusTest.php
@@ -129,6 +129,24 @@ class StatusTest extends BrowserTestBase {
     $session->pageTextNotContains('Deprecated modules found: Deprecated module.');
     $this->assertSession()->elementNotExists('xpath', "//a[contains(@href, 'http://example.com/deprecated')]");
 
+    // Make sure there are no warnings about obsolete modules.
+    $session->pageTextNotContains('Obsolete extensions enabled');
+    $session->pageTextNotContains('Obsolete extensions found: System obsolete status test.');
+
+    // Install an obsolete module. Normally this isn't possible, so write to
+    // configuration directly.
+    $this->config('core.extension')->set('module.system_status_obsolete_test', 0)->save();
+    $this->rebuildAll();
+    $this->drupalGet('admin/reports/status');
+    $session->pageTextContains('Obsolete extensions enabled');
+    $session->pageTextContains('Obsolete extensions found: System obsolete status test.');
+
+    // Make sure the warning is gone after uninstalling the module.
+    $module_installer->uninstall(['system_status_obsolete_test']);
+    $this->drupalGet('admin/reports/status');
+    $session->pageTextNotContains('Obsolete extensions enabled');
+    $session->pageTextNotContains('Obsolete extensions found: System obsolete status test.');
+
     // Install deprecated theme and confirm warning message is displayed.
     $theme_installer = \Drupal::service('theme_installer');
     $theme_installer->install(['test_deprecated_theme']);
