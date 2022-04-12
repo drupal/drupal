5e70a26c63a8b40b129bfaf979dbef04e5639dbb
catch <catch@35733.no-reply.drupal.org>
2022-04-12 16:59:02 +0100

    Issue #3274767 by nod_, lauriii: Update to CKEditor 5 v34.0.0

---
+++ core/assets/vendor/ckeditor5/basic-styles/translations/jv.js
@@ -0,0 +1,1 @@
+!function(i){const n=i.jv=i.jv||{};n.dictionary=Object.assign(n.dictionary||{},{Bold:"Kandhel",Code:"Kode",Italic:"Miring",Strikethrough:"Seratan dicoret",Subscript:"",Superscript:"",Underline:"Garis ngandhap"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
---
+++ core/assets/vendor/ckeditor5/language/translations/en-au.js
@@ -0,0 +1,1 @@
+!function(a){const n=a["en-au"]=a["en-au"]||{};n.dictionary=Object.assign(n.dictionary||{},{"Choose language":"Choose language",Language:"Language","Remove language":"Remove language"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
