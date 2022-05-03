5d068a48dd23fdfc44f2e01bad6454660f42c7a2
Alex Pott <alex.a.pott@googlemail.com>
2022-05-03 13:05:54 +0100

    Revert "Issue #3230541 by cliddell, jday, yogeshmpawar, neclimdul, cmlara, Charlie ChX Negyesi: Queue items only reserved by cron for 1 second"
    This reverts commit 7a35abe96ebdda29a3141d59d512a7cd198425ad.

--- core/lib/Drupal/Core/Queue/DatabaseQueue.php
+++ core/lib/Drupal/Core/Queue/DatabaseQueue.php
@@ -241,7 +241,7 @@ class DatabaseQueue implements ReliableQueueInterface, QueueGarbageCollectionInt
     try {
       // Clean up the queue for failed batches.
       $this->connection->delete(static::TABLE_NAME)
-        ->condition('created', \Drupal::time()->getRequestTime() - 864000, '<')
+        ->condition('created', REQUEST_TIME - 864000, '<')
         ->condition('name', 'drupal_batch:%', 'LIKE')
         ->execute();
 
@@ -252,7 +252,7 @@ class DatabaseQueue implements ReliableQueueInterface, QueueGarbageCollectionInt
           'expire' => 0,
         ])
         ->condition('expire', 0, '<>')
-        ->condition('expire', \Drupal::time()->getRequestTime(), '<')
+        ->condition('expire', REQUEST_TIME, '<')
         ->execute();
     }
     catch (\Exception $e) {
