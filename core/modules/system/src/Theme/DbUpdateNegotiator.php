1015c1b327c92bb069f61a2a95488f42a3d74d1b
Lauri Eskola <lauri.eskola@acquia.com>
2022-05-10 17:40:31 +0300

    Issue #3279640 by alexpott, Spokje, mherchel, lauriii, catch: Standard install profile uses Olivero for update.php

--- core/modules/system/src/Theme/DbUpdateNegotiator.php
+++ core/modules/system/src/Theme/DbUpdateNegotiator.php
@@ -3,6 +3,7 @@
 namespace Drupal\system\Theme;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ThemeHandlerInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Site\Settings;
 use Drupal\Core\Theme\ThemeNegotiatorInterface;
@@ -20,13 +21,27 @@ class DbUpdateNegotiator implements ThemeNegotiatorInterface {
   protected $configFactory;
 
   /**
+   * The theme handler.
+   *
+   * @var \Drupal\Core\Extension\ThemeHandlerInterface
+   */
+  protected $themeHandler;
+
+  /**
    * Constructs a DbUpdateNegotiator.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
+   * @param \Drupal\Core\Extension\ThemeHandlerInterface|null $theme_handler
+   *   The theme handler.
    */
-  public function __construct(ConfigFactoryInterface $config_factory) {
+  public function __construct(ConfigFactoryInterface $config_factory, ThemeHandlerInterface $theme_handler = NULL) {
     $this->configFactory = $config_factory;
+    if ($theme_handler === NULL) {
+      @trigger_error('Calling ' . __METHOD__ . '() without the $theme_handler argument is deprecated in drupal:9.4.0 and $theme_handler argument will be required in drupal:10.0.0. See https://www.drupal.org/node/3279699', E_USER_DEPRECATED);
+      $theme_handler = \Drupal::service('theme_handler');
+    }
+    $this->themeHandler = $theme_handler;
   }
 
   /**
@@ -40,10 +55,9 @@ class DbUpdateNegotiator implements ThemeNegotiatorInterface {
    * {@inheritdoc}
    */
   public function determineActiveTheme(RouteMatchInterface $route_match) {
-    $custom_theme = Settings::get('maintenance_theme', 'seven');
+    $custom_theme = Settings::get('maintenance_theme');
     if (!$custom_theme) {
-      $config = $this->configFactory->get('system.theme');
-      $custom_theme = $config->get('default');
+      $custom_theme = $this->themeHandler->themeExists('claro') ? 'claro' : 'seven';
     }
 
     return $custom_theme;
