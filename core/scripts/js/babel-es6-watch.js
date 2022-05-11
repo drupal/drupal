eb3ef8c00ac3561d0aa1d95b9402ebbc1a72d56c
Alex Pott <alex.a.pott@googlemail.com>
2022-05-11 19:11:16 +0100

    Issue #3278246 by lauriii, alexpott, nod_, justafish: Deprecate core/scripts/js/babel-es6-build.js for removal from 10.0.x
    (cherry picked from commit 8620063ae01376186157672fa3814657bfec770e)

--- core/scripts/js/babel-es6-watch.js
+++ core/scripts/js/babel-es6-watch.js
@@ -16,6 +16,8 @@ const chokidar = require('chokidar');
 const changeOrAdded = require('./changeOrAdded');
 const log = require('./log');
 
+console.warn('⚠️  yarn `watch:js` command is deprecated in drupal:9.4.0 and will be removed from drupal:10.0.0. This command is no longer needed in Drupal 10.0.0 once https://www.drupal.org/project/drupal/issues/3278415 is committed.️');
+
 // Match only on .es6.js files.
 const fileMatch = './**/*.es6.js';
 // Ignore everything in node_modules
