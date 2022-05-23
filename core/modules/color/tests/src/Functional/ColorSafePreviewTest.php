f32465f3a4cda1cbdfb0346cf8133945da2f381e
Alex Pott <alex.a.pott@googlemail.com>
2022-05-23 09:20:43 +0100

    Issue #3270936 by Spokje, quietone, andypost, lauriii: Deprecate Color module

--- core/modules/color/tests/src/Functional/ColorSafePreviewTest.php
+++ core/modules/color/tests/src/Functional/ColorSafePreviewTest.php
@@ -9,6 +9,7 @@ use Drupal\Tests\BrowserTestBase;
  * Tests sanitizing color preview loaded from theme.
  *
  * @group color
+ * @group legacy
  */
 class ColorSafePreviewTest extends BrowserTestBase {
 
