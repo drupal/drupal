7d151186343cfaded017985bb2e28d61f70b75e8
catch <catch@35733.no-reply.drupal.org>
2022-05-26 11:11:18 +0100

    Issue #3282395 by alexpott, mallezie: Latest versions of PHPStan and mglaman/phpstan-drupal cannot find PhpUnitVersionDependentTestCompatibilityTrait
    (cherry picked from commit a40e8436a2a34ac15c2712ab8bcff10feef7dc30)

--- core/tests/Drupal/Tests/PhpUnitCompatibilityTrait.php
+++ core/tests/Drupal/Tests/PhpUnitCompatibilityTrait.php
@@ -7,15 +7,15 @@ use Drupal\TestTools\PhpUnitCompatibility\RunnerVersion;
 // In order to manage different method signatures between PHPUnit versions, we
 // dynamically load a compatibility trait dependent on the PHPUnit runner
 // version.
-if (!trait_exists(PhpUnitVersionDependentTestCompatibilityTrait::class, FALSE)) {
-  class_alias("Drupal\TestTools\PhpUnitCompatibility\PhpUnit" . RunnerVersion::getMajor() . "\TestCompatibilityTrait", PhpUnitVersionDependentTestCompatibilityTrait::class);
-}
+class_alias("Drupal\TestTools\PhpUnitCompatibility\PhpUnit" . RunnerVersion::getMajor() . "\TestCompatibilityTrait", 'Drupal\Tests\PhpUnitCompatibilityTrait');
 
-/**
- * Makes Drupal's test API forward compatible with multiple versions of PHPUnit.
- */
-trait PhpUnitCompatibilityTrait {
+// Allow static analysis to find a trait.
+if (FALSE) {
 
-  use PhpUnitVersionDependentTestCompatibilityTrait;
+  /**
+   * Makes Drupal's test API forward compatible with multiple versions of PHPUnit.
+   */
+  trait PhpUnitCompatibilityTrait {
+  }
 
 }
