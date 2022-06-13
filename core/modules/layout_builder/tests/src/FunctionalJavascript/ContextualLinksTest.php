dd16e9e8bb6952b32fa6c88011c0981b2495ea08
catch <catch@35733.no-reply.drupal.org>
2022-06-13 08:31:59 +0100

    Issue #3285193 by Lendude, xjm: Temporarily skip random test failures that hide real test failures, part 4

--- core/modules/layout_builder/tests/src/FunctionalJavascript/ContextualLinksTest.php
+++ core/modules/layout_builder/tests/src/FunctionalJavascript/ContextualLinksTest.php
@@ -127,6 +127,7 @@ class ContextualLinksTest extends WebDriverTestBase {
    * @internal
    */
   protected function assertCorrectContextualLinksInUi(): void {
+    $this->markTestSkipped("Skipped temporarily for random fails.");
     $assert_session = $this->assertSession();
     $page = $this->getSession()->getPage();
     $this->assertNotEmpty($assert_session->waitForElementVisible('css', '.block-views-blocktest-block-view-block-2'));
