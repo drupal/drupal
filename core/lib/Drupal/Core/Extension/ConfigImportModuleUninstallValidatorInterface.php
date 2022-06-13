a2ca087f0bbea75441ae5418837a4aa37b5df4c1
catch <catch@35733.no-reply.drupal.org>
2022-06-13 11:40:11 +0100

    Issue #2392815 by alexpott, pooja saraah, bircher, cilefen, pratik_specbee, catch, fago: Module uninstall validators are not used to validate a config import
    (cherry picked from commit 7fbc4c4ba7816cf03133d27bb9e46f7d78e2be47)

---
+++ core/lib/Drupal/Core/Extension/ConfigImportModuleUninstallValidatorInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\Core\Extension;
+
+use Drupal\Core\Config\StorageInterface;
+
+/**
+ * Special interface for module uninstall validators for configuration import.
+ *
+ * A module uninstall validator that needs different functionality prior to a
+ * configuration import should implement this interface and be defined in
+ * a Drupal @link container service @endlink that is tagged
+ * module_install.uninstall_validator.
+ */
+interface ConfigImportModuleUninstallValidatorInterface extends ModuleUninstallValidatorInterface {
+
+  /**
+   * Determines reasons a module can not be uninstalled prior to config import.
+   *
+   * @param string $module
+   *   A module name.
+   * @param \Drupal\Core\Config\StorageInterface $source_storage
+   *   Storage object used to read configuration that is about to be imported.
+   *
+   * @return string[]
+   *   An array of reasons the module can not be uninstalled, empty if it can.
+   *   Each reason should not end with any punctuation since multiple reasons
+   *   can be displayed together.
+   *
+   * @see \Drupal\Core\EventSubscriber\ConfigImportSubscriber::validateModules()
+   */
+  public function validateConfigImport(string $module, StorageInterface $source_storage): array;
+
+}
--- core/lib/Drupal/Core/ProxyClass/Extension/ModuleRequiredByThemesUninstallValidator.php
+++ core/lib/Drupal/Core/ProxyClass/Extension/ModuleRequiredByThemesUninstallValidator.php
@@ -12,7 +12,7 @@ namespace Drupal\Core\ProxyClass\Extension {
      *
      * @see \Drupal\Component\ProxyBuilder
      */
-    class ModuleRequiredByThemesUninstallValidator implements \Drupal\Core\Extension\ModuleUninstallValidatorInterface
+    class ModuleRequiredByThemesUninstallValidator implements \Drupal\Core\Extension\ConfigImportModuleUninstallValidatorInterface
     {
 
         use \Drupal\Core\DependencyInjection\DependencySerializationTrait;
@@ -73,6 +73,14 @@ namespace Drupal\Core\ProxyClass\Extension {
         public function validate($module)
         {
             return $this->lazyLoadItself()->validate($module);
+        }
+
+        /**
+         * {@inheritdoc}
+         */
+        public function validateConfigImport(string $module, \Drupal\Core\Config\StorageInterface $source_storage): array
+        {
+            return $this->lazyLoadItself()->validateConfigImport($module, $source_storage);
         }
 
         /**
