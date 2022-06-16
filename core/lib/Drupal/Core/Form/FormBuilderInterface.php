40c2597c83279aa54051c91e7437444d446794e2
Alex Pott <alex.a.pott@googlemail.com>
2022-06-17 00:43:25 +0100

    Issue #2747273 by quietone, mayurjadhav, talhaparacha: Example given on FormBuilder::submitForm API Page is not working
    (cherry picked from commit d06b0a8f29e43d174e051d45008cd67c8f6aff29)

--- core/lib/Drupal/Core/Form/FormBuilderInterface.php
+++ core/lib/Drupal/Core/Form/FormBuilderInterface.php
@@ -138,15 +138,11 @@ interface FormBuilderInterface {
    *
    * For example:
    * @code
-   * // register a new user
+   * // Set the administrator role to 'content_editor'.
+   * $values['user_admin_role'] = 'content_editor';
    * $form_state = new FormState();
-   * $values['name'] = 'some-user';
-   * $values['mail'] = 'someuser@example.com';
-   * $values['pass']['pass1'] = 'password';
-   * $values['pass']['pass2'] = 'password';
-   * $values['op'] = t('Create new account');
    * $form_state->setValues($values);
-   * \Drupal::formBuilder()->submitForm('user_register_form', $form_state);
+   * \Drupal::formBuilder()->submitForm(RoleSettingsForm::class, $form_state);
    * @endcode
    *
    * @param \Drupal\Core\Form\FormInterface|string $form_arg
