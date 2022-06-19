fc14cbc68591d94744228d2bf7fb13e212305b26
Alex Pott <alex.a.pott@googlemail.com>
2022-06-19 17:14:06 +0100

    Issue #3283498 by Mile23, alexpott: Scaffold ReplaceOp::copyScaffold() throws an error for empty files

--- core/tests/Drupal/Tests/Composer/Plugin/Scaffold/Integration/ReplaceOpTest.php
+++ core/tests/Drupal/Tests/Composer/Plugin/Scaffold/Integration/ReplaceOpTest.php
@@ -39,4 +39,26 @@ class ReplaceOpTest extends TestCase {
     $this->assertStringContainsString('Copy [web-root]/robots.txt from assets/robots.txt', $output);
   }
 
+  /**
+   * @covers ::process
+   */
+  public function testEmptyFile() {
+    $fixtures = new Fixtures();
+    $destination = $fixtures->destinationPath('[web-root]/empty_file.txt');
+    $source = $fixtures->sourcePath('empty-file', 'empty_file.txt');
+    $options = ScaffoldOptions::create([]);
+    $sut = new ReplaceOp($source, TRUE);
+    // Assert that there is no target file before we run our test.
+    $this->assertFileDoesNotExist($destination->fullPath());
+    // Test the system under test.
+    $sut->process($destination, $fixtures->io(), $options);
+    // Assert that the target file was created.
+    $this->assertFileExists($destination->fullPath());
+    // Assert the target contained the contents from the correct scaffold file.
+    $this->assertSame('', file_get_contents($destination->fullPath()));
+    // Confirm that expected output was written to our io fixture.
+    $output = $fixtures->getOutput();
+    $this->assertStringContainsString('Copy [web-root]/empty_file.txt from assets/empty_file.txt', $output);
+  }
+
 }
