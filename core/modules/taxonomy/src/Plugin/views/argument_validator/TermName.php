f462b560dc3932e5677ad098e4a46d96aec3a47d
catch <catch@35733.no-reply.drupal.org>
2022-07-10 10:00:14 +0900

    Issue #2494617 by Lendude, danflanagan8, Bès, Shreya Shetty, stBorchert, Boobaa, DuaelFr, joey-santiago, drubb, nikitagupta, dawehner, alexpott: TermName views argument_validator is not working as expected
    (cherry picked from commit 091a83df7bd0e382a3ca135fb09a6900cd8275bc)

--- core/modules/taxonomy/src/Plugin/views/argument_validator/TermName.php
+++ core/modules/taxonomy/src/Plugin/views/argument_validator/TermName.php
@@ -66,22 +66,22 @@ class TermName extends Entity {
       $argument = str_replace('-', ' ', $argument);
       $this->argument->argument = $argument;
     }
-    $terms = $this->termStorage->loadByProperties(['name' => $argument]);
-
-    if (!$terms) {
-      // Returned empty array no terms with the name.
-      return FALSE;
+    // If bundles is set then restrict the loaded terms to the given bundles.
+    if (!empty($this->options['bundles'])) {
+      $terms = $this->termStorage->loadByProperties(['name' => $argument, 'vid' => $this->options['bundles']]);
+    }
+    else {
+      $terms = $this->termStorage->loadByProperties(['name' => $argument]);
     }
 
-    // Not knowing which term will be used if more than one is returned check
-    // each one.
+    // $terms are already bundle tested but we need to test access control.
     foreach ($terms as $term) {
-      if (!$this->validateEntity($term)) {
-        return FALSE;
+      if ($this->validateEntity($term)) {
+        return TRUE;
       }
     }
 
-    return TRUE;
+    return FALSE;
   }
 
 }
