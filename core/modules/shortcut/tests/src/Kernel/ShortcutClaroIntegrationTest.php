4d6ae45b5b37460ed503b56232d2c48c8367214e
Lauri Eskola <lauri.eskola@acquia.com>
2022-07-13 11:54:01 +0300

    Issue #3281440 by _shY, nod_: Update Shortcut tests to not use Bartik and Seven
    (cherry picked from commit 02656d5d012c2154bb62a7ccacd169372db42ecd)

---
+++ core/modules/shortcut/tests/src/Kernel/ShortcutClaroIntegrationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\Tests\shortcut\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests shortcut_install() and shortcut_uninstall().
+ *
+ * @group shortcut
+ */
+class ShortcutClaroIntegrationTest extends KernelTestBase {
+
+  protected static $modules = ['system'];
+
+  /**
+   * Tests shortcut_install() and shortcut_uninstall().
+   */
+  public function testInstallUninstall() {
+    // Install claro.
+    \Drupal::service('theme_installer')->install(['claro']);
+    $this->assertNull($this->config('claro.settings')->get('third_party_settings.shortcut'), 'There are no shortcut settings in claro.settings.');
+
+    \Drupal::service('module_installer')->install(['shortcut']);
+    $this->assertTrue($this->config('claro.settings')->get('third_party_settings.shortcut.module_link'), 'The shortcut module_link setting is in claro.settings.');
+
+    \Drupal::service('module_installer')->uninstall(['shortcut']);
+    $this->assertNull($this->config('claro.settings')->get('third_party_settings.shortcut'), 'There are no shortcut settings in claro.settings.');
+  }
+
+}
