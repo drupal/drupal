59a925f3434de7c07a2907f983384690b4a62421
catch <catch@35733.no-reply.drupal.org>
2022-07-13 15:44:13 +0900

    Issue #3021898 by longwave, alexpott, AaronBauman, daffie, opdavies: Support _defaults key in service.yml files for public, tags and autowire settings
    (cherry picked from commit 3d682d1796dadb30de1b412c5069adc01043d01e)

---
+++ core/tests/Drupal/KernelTests/Core/DependencyInjection/ServicesDefaultsTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\KernelTests\Core\DependencyInjection;
+
+use Drupal\KernelTests\KernelTestBase;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+
+/**
+ * Tests services _defaults definition.
+ *
+ * @group DependencyInjection
+ */
+class ServicesDefaultsTest extends KernelTestBase {
+
+  protected static $modules = ['services_defaults_test'];
+
+  /**
+   * Tests that 'services_defaults_test.service' has its dependencies injected.
+   */
+  public function testAutowiring() {
+    // Ensure interface autowiring works.
+    $this->assertSame(
+      $this->container->get('Drupal\services_defaults_test\TestInjection'),
+      $this->container->get('Drupal\services_defaults_test\TestService')->getTestInjection()
+    );
+    // Ensure defaults autowire works.
+    $this->assertSame(
+      $this->container->get('Drupal\services_defaults_test\TestInjection2'),
+      $this->container->get('Drupal\services_defaults_test\TestService')->getTestInjection2()
+    );
+
+    // Ensure that disabling autowiring works.
+    $this->assertNotSame(
+      $this->container->get('Drupal\services_defaults_test\TestInjection'),
+      $this->container->get('services_default_test.no_autowire')->getTestInjection()
+    );
+    $this->assertSame(
+      $this->container->get('services_default_test.no_autowire.arg'),
+      $this->container->get('services_default_test.no_autowire')->getTestInjection()
+    );
+    $this->assertSame(
+      $this->container->get('Drupal\services_defaults_test\TestInjection2'),
+      $this->container->get('services_default_test.no_autowire')->getTestInjection2()
+    );
+
+  }
+
+  /**
+   * Tests that default tags for 'services_defaults_test.service' are applied.
+   */
+  public function testDefaultTags() {
+    // Ensure default tags work.
+    $testServiceDefinition = $this->container->getDefinition('Drupal\services_defaults_test\TestService');
+    $testInjection1Definition = $this->container->getDefinition('Drupal\services_defaults_test\TestInjection');
+    $testInjection2Definition = $this->container->getDefinition('Drupal\services_defaults_test\TestInjection2');
+
+    $this->assertTrue($testServiceDefinition->hasTag('foo.tag1'));
+    $this->assertTrue($testServiceDefinition->hasTag('bar.tag2'));
+    $this->assertSame([['test' => 123]], $testServiceDefinition->getTag('bar.tag2'));
+    $this->assertTrue($testServiceDefinition->hasTag('bar.tag3'));
+    $this->assertSame([['value' => NULL]], $testServiceDefinition->getTag('bar.tag3'));
+
+    $this->assertSame($testServiceDefinition->getTags(), $testInjection1Definition->getTags());
+
+    // Ensure overridden tag works.
+    $this->assertTrue($testInjection2Definition->hasTag('zee.bang'));
+    $this->assertTrue($testInjection2Definition->hasTag('foo.tag1'));
+    $this->assertTrue($testInjection2Definition->hasTag('bar.tag2'));
+    $this->assertSame([['test' => 321], ['test' => 123]], $testInjection2Definition->getTag('bar.tag2'));
+    $this->assertTrue($testInjection2Definition->hasTag('bar.tag3'));
+    $this->assertSame([['value' => NULL]], $testInjection2Definition->getTag('bar.tag3'));
+  }
+
+  /**
+   * Tests that service from 'services_defaults_test.service' is private.
+   */
+  public function testPrivateServices() {
+    // Ensure default and overridden public flag works.
+    $this->expectException(ServiceNotFoundException::class);
+    $this->container->getDefinition('Drupal\services_defaults_test\TestPrivateService');
+  }
+
+}
--- core/tests/Drupal/Tests/Core/DependencyInjection/YamlFileLoaderTest.php
+++ core/tests/Drupal/Tests/Core/DependencyInjection/YamlFileLoaderTest.php
@@ -58,4 +58,118 @@ YAML;
     $this->assertSame('Drupal\Core\ExampleClass', $builder->getDefinition('Drupal\Core\ExampleClass')->getClass());
   }
 
+  /**
+   * @dataProvider providerTestExceptions
+   */
+  public function testExceptions($yml, $message) {
+    vfsStream::setup('drupal', NULL, [
+      'modules' => [
+        'example' => [
+          'example.yml' => $yml,
+        ],
+      ],
+    ]);
+
+    $builder = new ContainerBuilder();
+    $yaml_file_loader = new YamlFileLoader($builder);
+
+    $this->expectException(\InvalidArgumentException::class);
+    $this->expectExceptionMessage($message);
+    $yaml_file_loader->load('vfs://drupal/modules/example/example.yml');
+  }
+
+  public function providerTestExceptions() {
+    return [
+      '_defaults must be an array' => [<<<YAML
+services:
+  _defaults: string
+YAML,
+        'Service "_defaults" key must be an array, "string" given in "vfs://drupal/modules/example/example.yml".',
+      ],
+      'invalid _defaults key' => [<<<YAML
+services:
+  _defaults:
+    invalid: string
+YAML,
+        'The configuration key "invalid" cannot be used to define a default value in "vfs://drupal/modules/example/example.yml". Allowed keys are "public", "tags", "autowire".',
+      ],
+      'default tags must be an array' => [<<<YAML
+services:
+  _defaults:
+    tags: string
+YAML,
+        'Parameter "tags" in "_defaults" must be an array in "vfs://drupal/modules/example/example.yml". Check your YAML syntax.',
+      ],
+      'default tags must have a name' => [<<<YAML
+services:
+  _defaults:
+    tags:
+      - {}
+YAML,
+        'A "tags" entry in "_defaults" is missing a "name" key in "vfs://drupal/modules/example/example.yml".',
+      ],
+      'default tag name must not be empty' => [<<<YAML
+services:
+  _defaults:
+    tags:
+      - ''
+YAML,
+        'The tag name in "_defaults" must be a non-empty string in "vfs://drupal/modules/example/example.yml".',
+      ],
+      'default tag name must be a string' => [<<<YAML
+services:
+  _defaults:
+    tags:
+      - 123
+YAML,
+        'The tag name in "_defaults" must be a non-empty string in "vfs://drupal/modules/example/example.yml".',
+      ],
+      'default tag attribute must be scalar' => [<<<YAML
+services:
+  _defaults:
+    tags:
+      - { name: tag, value: [] }
+YAML,
+        'Tag "tag", attribute "value" in "_defaults" must be of a scalar-type in "vfs://drupal/modules/example/example.yml". Check your YAML syntax.',
+      ],
+      'tags must be an array' => [<<<YAML
+services:
+  service:
+    tags: string
+YAML,
+        'Parameter "tags" must be an array for service "service" in "vfs://drupal/modules/example/example.yml". Check your YAML syntax.',
+      ],
+      'tags must have a name' => [<<<YAML
+services:
+  service:
+    tags:
+      - {}
+YAML,
+        'A "tags" entry is missing a "name" key for service "service" in "vfs://drupal/modules/example/example.yml".',
+      ],
+      'tag name must not be empty' => [<<<YAML
+services:
+  service:
+    tags:
+      - ''
+YAML,
+        'The tag name for service "service" in "vfs://drupal/modules/example/example.yml" must be a non-empty string.',
+      ],
+      'tag attribute must be scalar' => [<<<YAML
+services:
+  service:
+    tags:
+      - { name: tag, value: [] }
+YAML,
+        'A "tags" attribute must be of a scalar-type for service "service", tag "tag", attribute "value" in "vfs://drupal/modules/example/example.yml". Check your YAML syntax.',
+      ],
+      'service must be array or @service' => [<<<YAML
+services:
+  service: string
+YAML,
+        'A service definition must be an array or a string starting with "@" but string found for service "service" in vfs://drupal/modules/example/example.yml. Check your YAML syntax.',
+      ],
+    ];
+  }
+
 }
