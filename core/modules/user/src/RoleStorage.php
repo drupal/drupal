d12f31e0e151b3c11a23d006c9b1350bf40f9a06
catch <catch@35733.no-reply.drupal.org>
2022-07-19 17:24:04 +0900

    Issue #2563995 by Temoor, Rakhi Soni, andypost: Remove duplicated check for isAdmin() in RoleStorage::isPermissionInRoles()
    (cherry picked from commit 4ae63c59a0afc108b056cfc872171b021c6c8eb7)

--- core/modules/user/src/RoleStorage.php
+++ core/modules/user/src/RoleStorage.php
@@ -13,16 +13,14 @@ class RoleStorage extends ConfigEntityStorage implements RoleStorageInterface {
    * {@inheritdoc}
    */
   public function isPermissionInRoles($permission, array $rids) {
-    $has_permission = FALSE;
     foreach ($this->loadMultiple($rids) as $role) {
       /** @var \Drupal\user\RoleInterface $role */
-      if ($role->isAdmin() || $role->hasPermission($permission)) {
-        $has_permission = TRUE;
-        break;
+      if ($role->hasPermission($permission)) {
+        return TRUE;
       }
     }
 
-    return $has_permission;
+    return FALSE;
   }
 
 }
