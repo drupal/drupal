0ca45bf57b31087f3a5c2280f871552382b5ba1a
catch <catch@35733.no-reply.drupal.org>
2022-07-26 12:36:10 +0900

    Issue #3299853 by andypost, kim.pepper: Apply #[\AllowDynamicProperties] attribute to base classes to make PHP 8.2 log size sane
    (cherry picked from commit db2e2d2544cc0e10fa9f0f17bc26f57db255d1f6)

--- core/modules/views/src/Plugin/views/PluginBase.php
+++ core/modules/views/src/Plugin/views/PluginBase.php
@@ -37,6 +37,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  * @ingroup views_plugins
  */
+#[\AllowDynamicProperties]
 abstract class PluginBase extends ComponentPluginBase implements ContainerFactoryPluginInterface, ViewsPluginInterface, DependentPluginInterface, TrustedCallbackInterface {
 
   /**
