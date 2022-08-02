98deec06652822e2cf3be460de786c99359dc108
Alex Pott <alex.a.pott@googlemail.com>
2022-08-02 23:40:57 +0100

    Issue #3116405 by Spokje, Mile23, Hardik_Patel_12, jofitz, Sahana _N, andypost, alexpott, fgm: Warnings generated when using an optimized autoload file with Composer 1.10 / Composer 2

--- core/tests/Drupal/Tests/ComposerIntegrationTest.php
+++ core/tests/Drupal/Tests/ComposerIntegrationTest.php
@@ -6,6 +6,7 @@ use Drupal\Composer\Plugin\VendorHardening\Config;
 use Drupal\Core\Composer\Composer;
 use Drupal\Tests\Composer\ComposerIntegrationTrait;
 use Drupal\TestTools\PhpUnitCompatibility\RunnerVersion;
+use Symfony\Component\Finder\Finder;
 
 /**
  * Tests Composer integration.
@@ -84,6 +85,49 @@ class ComposerIntegrationTest extends UnitTestCase {
       $data[$composer_json->getPathname()] = [$composer_json->getPathname()];
     }
     return $data;
+  }
+
+  /**
+   * Tests core's composer.json replace section.
+   *
+   * Verify that all core components are also listed in the 'replace' section of
+   * core's composer.json.
+   */
+  public function testAllCoreComponentsReplaced(): void {
+    // Assemble a path to core components.
+    $components_path = $this->root . '/core/lib/Drupal/Component';
+
+    // Grab the 'replace' section of the core composer.json file.
+    $json = json_decode(file_get_contents($this->root . '/core/composer.json'), FALSE);
+    $composer_replace_packages = (array) $json->replace;
+
+    // Get a list of all the composer.json files in the components path.
+    $components_composer_json_files = [];
+
+    $composer_json_finder = new Finder();
+    $composer_json_finder->name('composer.json')
+      ->in($components_path)
+      ->ignoreUnreadableDirs();
+
+    foreach ($composer_json_finder->getIterator() as $composer_json) {
+      $components_composer_json_files[$composer_json->getPathname()] = [$composer_json->getPathname()];
+    }
+
+    $this->assertNotEmpty($components_composer_json_files);
+    $this->assertCount(count($composer_replace_packages), $components_composer_json_files);
+
+    // Assert that each core components has a corresponding 'replace' in
+    // composer.json.
+    foreach ($components_composer_json_files as $components_composer_json_file) {
+      $json = json_decode(file_get_contents(reset($components_composer_json_file)), FALSE);
+      $component_name = $json->name;
+
+      $this->assertArrayHasKey(
+        $component_name,
+        $composer_replace_packages,
+        'Unable to find ' . $component_name . ' in replace list of composer.json'
+      );
+    }
   }
 
   /**
