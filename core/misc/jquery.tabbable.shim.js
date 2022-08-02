ebfd3598d105ecb089883c7ae97521e5df9fcbe7
catch <catch@35733.no-reply.drupal.org>
2022-08-02 14:59:22 +0900

    Issue #3296096 by Gábor Hojtsy, smustgrave, catch, Spokje: Update tabbable deprecation messages to 11.x
    (cherry picked from commit 69d084fce2071e30c30c21b38eabea88018b92f9)

--- core/misc/jquery.tabbable.shim.js
+++ core/misc/jquery.tabbable.shim.js
@@ -12,7 +12,7 @@
   $.extend($.expr[':'], {
     tabbable(element) {
       Drupal.deprecationError({
-        message: 'The :tabbable selector is deprecated in Drupal 9.2.0 and will be removed in Drupal 10.0.0. Use the core/tabbable library instead. See https://www.drupal.org/node/3183730'
+        message: 'The :tabbable selector is deprecated in Drupal 9.2.0 and will be removed in Drupal 11.0.0. Use the core/tabbable library instead. See https://www.drupal.org/node/3183730'
       });
 
       if (element.tagName === 'SUMMARY' || element.tagName === 'DETAILS') {
