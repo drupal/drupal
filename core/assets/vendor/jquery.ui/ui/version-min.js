084ff937c5a2f4bf891064a9266e1a5ef8cec676
Lauri Eskola <lauri.eskola@acquia.com>
2022-08-04 13:09:08 +0300

    Issue #3300040 by xjm, benjifisher, jlariza, cilefen, quietone: Update jQuery UI to the latest versions

--- core/assets/vendor/jquery.ui/ui/version-min.js
+++ core/assets/vendor/jquery.ui/ui/version-min.js
@@ -1,2 +1,2 @@
-!function(e){"use strict";"function"==typeof define&&define.amd?define(["jquery"],e):e(jQuery)}((function(e){"use strict";return e.ui=e.ui||{},e.ui.version="1.13.1"}));
+!function(e){"use strict";"function"==typeof define&&define.amd?define(["jquery"],e):e(jQuery)}((function(e){"use strict";return e.ui=e.ui||{},e.ui.version="1.13.2"}));
 //# sourceMappingURL=version-min.js.map
