7362bc50b21bacda13d5dc20f58147d869fe5c31
Alex Pott <alex.a.pott@googlemail.com>
2022-08-21 17:33:05 +0100

    Issue #3053881 by Luke.Leber, bkosborne, tyler-paavola, Sam152, Rishi Kulshreshtha, Odai Atieh, chrisolof, acbramley, ethomas08, Oscaner, kyberman, aarti zikre, joshua1234511, larowlan, catch: Reverting entity revisions that contain custom blocks erroneously triggers EntityChangedConstraint

---
+++ core/modules/block_content/src/Plugin/Validation/Constraint/BlockContentEntityChangedConstraintValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\block_content\Plugin\Validation\Constraint;
+
+use Drupal\block_content\BlockContentInterface;
+use Drupal\Core\Entity\Plugin\Validation\Constraint\EntityChangedConstraintValidator;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Validates the BlockContentEntityChanged constraint.
+ */
+class BlockContentEntityChangedConstraintValidator extends EntityChangedConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($entity, Constraint $constraint) {
+    // This prevents saving an update to the block via a host entity's form if
+    // the host entity has had other changes made via the API instead of the
+    // entity form, such as a revision revert. This is safe, for example, in the
+    // Layout Builder the inline blocks are not saved until the whole layout is
+    // saved, in which case Layout Builder forces a new revision for the block.
+    // @see \Drupal\layout_builder\InlineBlockEntityOperations::handlePreSave.
+    if ($entity instanceof BlockContentInterface && !$entity->isReusable()) {
+      return;
+    }
+    parent::validate($entity, $constraint);
+  }
+
+}
