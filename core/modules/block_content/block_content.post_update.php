8672c435449b8bb74e26639c494e067df9ca7fb4
Alex Pott <alex.a.pott@googlemail.com>
2022-08-21 17:33:05 +0100

    Issue #3053881 by Luke.Leber, bkosborne, tyler-paavola, Sam152, Rishi Kulshreshtha, Odai Atieh, chrisolof, acbramley, ethomas08, Oscaner, kyberman, aarti zikre, joshua1234511, larowlan, catch: Reverting entity revisions that contain custom blocks erroneously triggers EntityChangedConstraint
    (cherry picked from commit 7362bc50b21bacda13d5dc20f58147d869fe5c31)

--- core/modules/block_content/block_content.post_update.php
+++ core/modules/block_content/block_content.post_update.php
@@ -13,3 +13,10 @@ function block_content_removed_post_updates() {
     'block_content_post_update_add_views_reusable_filter' => '9.0.0',
   ];
 }
+
+/**
+ * Clear the entity type cache.
+ */
+function block_content_post_update_entity_changed_constraint() {
+  // Empty post_update hook.
+}
