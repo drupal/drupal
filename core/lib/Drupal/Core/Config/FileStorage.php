3959be8d83cf339c8a64a6283e1d1f1811009ce2
Alex Pott <alex.a.pott@googlemail.com>
2022-08-22 10:36:31 -0700

    Issue #2232051 by andypost, andyceo, tayzlor, smustgrave, moshe weitzman: Unnessessary chmod during config write
    (cherry picked from commit 581f751582d91d63f476b40bf6af4d19d33f404f)

--- core/lib/Drupal/Core/Config/FileStorage.php
+++ core/lib/Drupal/Core/Config/FileStorage.php
@@ -154,10 +154,7 @@ class FileStorage implements StorageInterface {
       $status = @file_put_contents($target, $encoded_data);
     }
     if ($status === FALSE) {
-      throw new StorageException('Failed to write configuration file: ' . $this->getFilePath($name));
-    }
-    else {
-      $this->getFileSystem()->chmod($target);
+      throw new StorageException('Failed to write configuration file: ' . $target);
     }
 
     $this->fileCache->set($target, $data);
