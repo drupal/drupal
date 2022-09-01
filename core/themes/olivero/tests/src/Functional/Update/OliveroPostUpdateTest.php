4413ad763eacb7940681e835eed90cf3e35a3182
Lauri Eskola <lauri.eskola@acquia.com>
2022-09-01 09:56:21 +0300

    Issue #3306938 by Spokje, catch: Restore olivero_post_update_add_olivero_primary_color()

---
+++ core/themes/olivero/tests/src/Functional/Update/OliveroPostUpdateTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\Tests\olivero\Functional\Update;
+
+use Drupal\FunctionalTests\Update\UpdatePathTestBase;
+
+/**
+ * Tests the update path for Olivero.
+ *
+ * @group Update
+ */
+class OliveroPostUpdateTest extends UpdatePathTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setDatabaseDumpFiles() {
+    $this->databaseDumpFiles = [
+      __DIR__ . '/../../../../../../modules/system/tests/fixtures/update/drupal-9.4.0.filled.standard.php.gz',
+    ];
+  }
+
+  /**
+   * Tests update hook setting base primary color.
+   */
+  public function testOliveroPrimaryColorUpdate() {
+    $config = $this->config('olivero.settings');
+    $this->assertEmpty($config->get('base_primary_color'));
+
+    // Run updates.
+    $this->runUpdates();
+
+    $config = $this->config('olivero.settings');
+    $this->assertSame('#1b9ae4', $config->get('base_primary_color'));
+  }
+
+}
