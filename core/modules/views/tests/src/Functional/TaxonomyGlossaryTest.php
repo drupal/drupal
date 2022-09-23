50e24a281348b0bbbcee45d10cc68516b6a8ad00
Alex Pott <alex.a.pott@googlemail.com>
2022-09-23 15:13:01 +0200

    Issue #3291520 by sardara, Ratan Priya, alexpott, smustgrave: Incorrect label in taxonomy glossary for terms that start with zero
    (cherry picked from commit 5496d64a4a906b2e057a49d6c819c92f5cb73e15)

--- core/modules/views/tests/src/Functional/TaxonomyGlossaryTest.php
+++ core/modules/views/tests/src/Functional/TaxonomyGlossaryTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\views\Functional;
 
+use Drupal\Core\Url;
 use Drupal\Tests\taxonomy\Traits\TaxonomyTestTrait;
 
 /**
@@ -39,6 +40,9 @@ class TaxonomyGlossaryTest extends ViewTestBase {
    */
   protected $taxonomyTerms;
 
+  /**
+   * {@inheritdoc}
+   */
   protected function setUp($import_test_views = TRUE, $modules = ['views_test_config']): void {
     parent::setUp($import_test_views, $modules);
 
@@ -49,15 +53,44 @@ class TaxonomyGlossaryTest extends ViewTestBase {
     for ($i = 0; $i < 10; $i++) {
       $this->taxonomyTerms[] = $this->createTerm($vocabulary);
     }
+    $this->taxonomyTerms[] = $this->createTerm($vocabulary, ['name' => '0' . $this->randomMachineName()]);
   }
 
   /**
    * Tests a taxonomy glossary view.
    */
   public function testTaxonomyGlossaryView() {
+    $initials = [];
+    foreach ($this->taxonomyTerms as $term) {
+      $char = mb_strtolower(substr($term->label(), 0, 1));
+      $initials += [$char => 0];
+      $initials[$char]++;
+    }
+
+    $this->drupalGet('test_taxonomy_glossary');
+    $assert_session = $this->assertSession();
+
+    foreach ($initials as $char => $count) {
+      $href = Url::fromUserInput('/test_taxonomy_glossary/' . $char)->toString();
+
+      $xpath = $assert_session->buildXPathQuery('//a[@href=:href and normalize-space(text())=:label]', [
+        ':href' => $href,
+        ':label' => $char,
+      ]);
+      $link = $assert_session->elementExists('xpath', $xpath);
+
+      // Assert that the expected number of results is indicated in the link.
+      preg_match("/{$char} \(([0-9]+)\)/", $link->getParent()->getText(), $matches);
+      $this->assertEquals($count, $matches[1]);
+    }
+
+    // Check that no other glossary links but the expected ones have been
+    // rendered.
+    $assert_session->elementsCount('xpath', '/ancestor::ul//a', count($initials), $link);
+
     // Go the taxonomy glossary page for the first term.
     $this->drupalGet('test_taxonomy_glossary/' . substr($this->taxonomyTerms[0]->getName(), 0, 1));
-    $this->assertSession()->pageTextContains($this->taxonomyTerms[0]->getName());
+    $assert_session->pageTextContains($this->taxonomyTerms[0]->getName());
   }
 
 }
