fa7196afd446d26f1cb71b94592719472a9a5ccc
Alex Pott <alex.a.pott@googlemail.com>
2022-10-05 13:04:31 +0100

    Issue #3168624 by DerekCresswell, ravi.shankar, BramDriesen, jonathanshaw, catch: Add 'user/edit' route
    (cherry picked from commit b61cd96e9dcc2c40538df3e636971eeee2eb3e17)

---
+++ core/modules/user/tests/src/Kernel/Controller/UserControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\Tests\user\Kernel\Controller;
+
+use Drupal\Core\Url;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\Tests\user\Traits\UserCreationTrait;
+use Drupal\user\Controller\UserController;
+
+/**
+ * Tests for the User controller.
+ *
+ * @group user
+ *
+ * @coversDefaultClass \Drupal\user\Controller\UserController
+ */
+class UserControllerTest extends KernelTestBase {
+
+  use UserCreationTrait;
+
+  /**
+   * The user controller.
+   *
+   * @var \Drupal\user\Controller\UserController
+   */
+  protected $userController;
+
+  /**
+   * The logged in user.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $user;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'user',
+  ];
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function setUp(): void {
+
+    parent::setUp();
+
+    $this->userController = UserController::create(\Drupal::getContainer());
+
+    // Create and log in a user.
+    $this->user = $this->setUpCurrentUser();
+
+  }
+
+  /**
+   * Tests the redirection to a user edit page.
+   *
+   * @covers ::userEditPage
+   */
+  public function testUserEditPage() {
+
+    $response = $this->userController->userEditPage();
+
+    // Ensure the response is directed to the correct user edit page.
+    $edit_url = Url::fromRoute('entity.user.edit_form', [
+      'user' => $this->user->id(),
+    ])->setAbsolute()
+      ->toString();
+    $this->assertEquals($edit_url, $response->getTargetUrl());
+
+    $this->assertEquals(301, $response->getStatusCode());
+
+  }
+
+}
