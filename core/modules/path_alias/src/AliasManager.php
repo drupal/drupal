946d271597fa865ae26a5122e9edba55cb5182bd
Alex Pott <alex.a.pott@googlemail.com>
2022-10-05 10:57:36 +0100

    Issue #3313448 by rakesh.drupal: Missing AliasRepositoryInterface parameter type hint in the AliasManager constructor

--- core/modules/path_alias/src/AliasManager.php
+++ core/modules/path_alias/src/AliasManager.php
@@ -103,7 +103,7 @@ class AliasManager implements AliasManagerInterface {
    * @param \Drupal\Core\Cache\CacheBackendInterface $cache
    *   Cache backend.
    */
-  public function __construct($alias_repository, AliasWhitelistInterface $whitelist, LanguageManagerInterface $language_manager, CacheBackendInterface $cache) {
+  public function __construct(AliasRepositoryInterface $alias_repository, AliasWhitelistInterface $whitelist, LanguageManagerInterface $language_manager, CacheBackendInterface $cache) {
     $this->pathAliasRepository = $alias_repository;
     $this->languageManager = $language_manager;
     $this->whitelist = $whitelist;
