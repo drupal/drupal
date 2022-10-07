ec8452798eb413d529a257c689feb0362059cada
Alex Pott <alex.a.pott@googlemail.com>
2022-10-07 15:22:21 +0100

    Issue #3251647 by dsdeiz, phenaproxima, joachim: Field mapping options in media type form are not sorted correctly
    (cherry picked from commit b3d036aacd431941c7146e876eb8c28242291547)
    (cherry picked from commit 93a3045f5baa6852da6e4f4765b4565eea6feb86)

--- core/modules/media/tests/src/FunctionalJavascript/MediaTypeCreationTest.php
+++ core/modules/media/tests/src/FunctionalJavascript/MediaTypeCreationTest.php
@@ -102,6 +102,13 @@ class MediaTypeCreationTest extends MediaJavascriptTestBase {
     $assert_session->fieldDisabled('Media source');
     $assert_session->pageTextContains('The media source cannot be changed after the media type is created.');
 
+    // Check that the field map options are sorted alphabetically.
+    $options = $this->xpath('//select[@name="field_map[attribute_1]"]/option');
+    $this->assertGreaterThanOrEqual(3, count($options));
+    $this->assertSame('- Skip field -', $options[0]->getText());
+    $this->assertSame('Name', $options[1]->getText());
+    $this->assertSame('Test source', $options[2]->getText());
+
     // Open up the media add form and verify that the source field is right
     // after the name, and before the vertical tabs.
     $this->drupalGet("/media/add/$mediaTypeMachineName");
