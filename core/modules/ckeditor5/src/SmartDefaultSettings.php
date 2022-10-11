151b5e459c3bf4aa265013f5e21c333e37cca4c6
Alex Pott <alex.a.pott@googlemail.com>
2022-10-11 21:13:40 +0100

    Issue #3314511 by Wim Leers: CKEditor 4 → 5 upgrade path may trigger warnings in some edge cases, making upgrade path tests impossible
    (cherry picked from commit 1de949a521823d0406d5d9df0483feed4706325e)

--- core/modules/ckeditor5/src/SmartDefaultSettings.php
+++ core/modules/ckeditor5/src/SmartDefaultSettings.php
@@ -281,6 +281,7 @@ final class SmartDefaultSettings {
       }
 
       $help_enabled = $this->moduleHandler->moduleExists('help');
+      $can_access_dblog = ($this->currentUser->hasPermission('access site reports') && $this->moduleHandler->moduleExists('dblog'));
 
       if (!empty($plugins_enabled) || !$source_editing_additions->allowsNothing()) {
         $beginning = $help_enabled ?
@@ -305,7 +306,6 @@ final class SmartDefaultSettings {
             $this->t("Added these tags/attributes to the Source Editing Plugin's Manually editable HTML tags setting: @tag_list", ['@tag_list' => $source_editing_additions->toFilterHtmlAllowedTagsString()]);
         }
 
-        $can_access_dblog = ($this->currentUser->hasPermission('access site reports') && $this->moduleHandler->moduleExists('dblog'));
         $end = $can_access_dblog ?
           $this->t('Additional details are available <a target="_blank" href=":dblog_url">in your logs</a>.',
             [
