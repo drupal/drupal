e16dac1de047ba8097fc915be6e4fa0347302ac4
catch <catch56@gmail.com>
2022-10-19 21:53:08 +0100

    Issue #3316136 by julien.sibi, flocondetoile: LanguageNegotiationInterface.php : function's name error in docblock
    (cherry picked from commit e7cfffefafcb3db4b5f43024e9dc56a74b731dc7)

--- core/modules/language/src/LanguageNegotiatorInterface.php
+++ core/modules/language/src/LanguageNegotiatorInterface.php
@@ -76,11 +76,11 @@ use Drupal\Core\Session\AccountInterface;
  * }
  *
  * class MyLanguageNegotiationUrl extends LanguageNegotiationUrl {
- *   public function getCurrentLanguage(Request $request = NULL) {
+ *   public function getLangcode(Request $request = NULL) {
  *     if ($request) {
  *       // Use the original URL language negotiation method to get a valid
  *       // language code.
- *       $langcode = parent::getCurrentLanguage($request);
+ *       $langcode = parent::getLangcode($request);
  *
  *       // If we are on an administrative path, override with the default
  *       language.
