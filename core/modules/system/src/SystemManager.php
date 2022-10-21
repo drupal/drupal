5e67517d01085ae5e1d66b7b62f162f20d59ee90
catch <catch56@gmail.com>
2022-10-21 11:29:07 +0100

    Issue #309040 by claudiu.cristea, David_Rothstein, Chi, BryanGullan, larowlan, dww, joachim: Add hook_requirements_alter()

--- core/modules/system/src/SystemManager.php
+++ core/modules/system/src/SystemManager.php
@@ -107,6 +107,7 @@ class SystemManager {
 
     // Check run-time requirements and status information.
     $requirements = $this->moduleHandler->invokeAll('requirements', ['runtime']);
+    $this->moduleHandler->alter('requirements', $requirements);
     uasort($requirements, function ($a, $b) {
       if (!isset($a['weight'])) {
         if (!isset($b['weight'])) {
