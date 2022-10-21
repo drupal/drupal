d31e7caec3a18b0078c94f21536e4fa3091bb8b5
catch <catch56@gmail.com>
2022-10-21 13:31:14 +0100

    Issue #3313946 by nod_, longwave, Wim Leers: Update CKEditor 5 to 35.2.1

---
+++ core/assets/vendor/ckeditor5/block-quote/translations/es-co.js
@@ -0,0 +1,1 @@
+!function(o){const i=o["es-co"]=o["es-co"]||{};i.dictionary=Object.assign(i.dictionary||{},{"Block quote":"Cita de bloque"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
