41dce09cc0aaad556251831eedcdfcf4c8355d68
catch <catch56@gmail.com>
2022-10-21 11:29:07 +0100

    Issue #309040 by claudiu.cristea, David_Rothstein, Chi, BryanGullan, larowlan, dww, joachim: Add hook_requirements_alter()
    (cherry picked from commit 5e67517d01085ae5e1d66b7b62f162f20d59ee90)

---
+++ core/modules/system/tests/src/Kernel/Module/RequirementsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\Tests\system\Kernel\Module;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * @covers \hook_requirements
+ * @covers \hook_requirements_alter
+ * @group Module
+ */
+class RequirementsTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'requirements1_test',
+    'system',
+  ];
+
+  /**
+   * Tests requirements data altering.
+   */
+  public function testRequirementsAlter(): void {
+    $requirements = $this->container->get('system.manager')->listRequirements();
+    // @see requirements1_test_requirements_alter()
+    $this->assertEquals('Requirements 1 Test - Changed', $requirements['requirements1_test_alterable']['title']);
+    $this->assertEquals(REQUIREMENT_WARNING, $requirements['requirements1_test_alterable']['severity']);
+    $this->assertArrayNotHasKey('requirements1_test_deletable', $requirements);
+  }
+
+}
