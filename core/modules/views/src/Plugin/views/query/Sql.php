e9733fd6d5f7ecc8caeaa4ebcc56abb470d81e87
Alex Pott <alex.a.pott@googlemail.com>
2022-11-01 16:47:38 +0000

    Issue #3247121 by ramil g, joelpittet: Aggregation of nid when using COUNT produces syntax error
    (cherry picked from commit bc8ed7fec14b66c1d56e7b7e7ede30e807eee6d4)
    (cherry picked from commit dde89e2238985c0e484076fd985ab9a427b34238)

--- core/modules/views/src/Plugin/views/query/Sql.php
+++ core/modules/views/src/Plugin/views/query/Sql.php
@@ -1351,7 +1351,7 @@ class Sql extends QueryPluginBase {
     $entity_information = $this->getEntityTableInfo();
     if ($entity_information) {
       $params = [];
-      if ($groupby) {
+      if ($this->hasAggregate) {
         // Handle grouping, by retrieving the minimum entity_id.
         $params = [
           'function' => 'min',
