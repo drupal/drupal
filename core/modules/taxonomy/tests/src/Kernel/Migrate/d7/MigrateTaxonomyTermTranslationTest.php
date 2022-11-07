6f855794e07b95a2bd74ba754e6e9ca6d6841cf4
catch <catch56@gmail.com>
2022-11-07 13:02:48 +0000

    Issue #3317758 by DieterHolvoet: The return type of TermInterface::getWeight is wrong
    (cherry picked from commit 8fb7236b1ddb34e685b1f5b05a3044c9b78cb2a3)

--- core/modules/taxonomy/tests/src/Kernel/Migrate/d7/MigrateTaxonomyTermTranslationTest.php
+++ core/modules/taxonomy/tests/src/Kernel/Migrate/d7/MigrateTaxonomyTermTranslationTest.php
@@ -95,7 +95,7 @@ class MigrateTaxonomyTermTranslationTest extends MigrateDrupal7TestBase {
     $this->assertSame($expected_vid, $entity->bundle());
     $this->assertSame($expected_description, $entity->getDescription());
     $this->assertSame($expected_format, $entity->getFormat());
-    $this->assertSame($expected_weight, (int) $entity->getWeight());
+    $this->assertSame($expected_weight, $entity->getWeight());
     $this->assertHierarchy($expected_vid, $id, $expected_parents);
   }
 
