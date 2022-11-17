af6cdbf0626a2c80ecaf99075893f9df7820da74
catch <catch@35733.no-reply.drupal.org>
2022-11-17 14:13:32 +0000

    Issue #3260401 by idebr, Spokje, Akram Khan, mcdruid, longwave, alexpott: Google is abandoning FLoC - so remove the header

--- core/lib/Drupal/Core/Site/Settings.php
+++ core/lib/Drupal/Core/Site/Settings.php
@@ -37,7 +37,12 @@ final class Settings {
    *
    * @see self::handleDeprecations()
    */
-  private static $deprecatedSettings = [];
+  private static $deprecatedSettings = [
+    'block_interest_cohort' => [
+      'replacement' => '',
+      'message' => 'The "block_interest_cohort" setting is deprecated in drupal:9.5.0. This setting should be removed from the settings file, since its usage has been removed. See https://www.drupal.org/node/3320787.',
+    ],
+  ];
 
   /**
    * Constructor.
