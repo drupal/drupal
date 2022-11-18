d7f67e22ca57e52b39d803bf7b460073a5ef1e4b
catch <catch@35733.no-reply.drupal.org>
2022-11-18 12:12:22 +0000

    Issue #3032078 by jrglasgow, dagomar, Berdir, nikitagupta, catch, anmolgoyal74, technoveltyco, idebr, ndf, jonas139, nod_: Multiple webheads can cause infinite growth of Twig cache
    (cherry picked from commit a59f4db3619e738fe5baf8560aff7fa96b7d1666)

--- core/lib/Drupal/Core/DependencyInjection/Compiler/TwigExtensionPass.php
+++ core/lib/Drupal/Core/DependencyInjection/Compiler/TwigExtensionPass.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 /**
  * Adds the twig_extension_hash parameter to the container.
  *
- * Parameter twig_extension_hash is a hash of all extension mtimes for Twig
+ * Parameter twig_extension_hash is a crc32 hash of all extensions for Twig
  * template invalidation.
  */
 class TwigExtensionPass implements CompilerPassInterface {
@@ -23,8 +23,8 @@ class TwigExtensionPass implements CompilerPassInterface {
       $class_name = $container->getDefinition($service_id)->getClass();
       $reflection = new \ReflectionClass($class_name);
       // We use the class names as hash in order to invalidate on new extensions
-      // and mtime for every time we change an existing file.
-      $twig_extension_hash .= $class_name . filemtime($reflection->getFileName());
+      // and crc32 for every time we change an existing file.
+      $twig_extension_hash .= $class_name . hash_file('crc32', $reflection->getFileName());
     }
 
     $container->setParameter('twig_extension_hash', Crypt::hashBase64($twig_extension_hash));
