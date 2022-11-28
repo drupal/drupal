68c51b73f5afb4ec6fee55e1b76934bfe7722fd0
catch <catch@35733.no-reply.drupal.org>
2022-11-28 10:28:02 +0000

    Issue #3301288 by balintpekker, mondrake, mglaman: Deprecated getStatus() in DrupalTestBrowser

--- core/tests/Drupal/Tests/DrupalTestBrowser.php
+++ core/tests/Drupal/Tests/DrupalTestBrowser.php
@@ -239,28 +239,4 @@ class DrupalTestBrowser extends AbstractBrowser {
     return new Response((string) $response->getBody(), $response->getStatusCode(), $response->getHeaders());
   }
 
-  /**
-   * Reads response meta tags to guess content-type charset.
-   *
-   * @param \Symfony\Component\BrowserKit\Response $response
-   *   The origin response to filter.
-   *
-   * @return \Symfony\Component\BrowserKit\Response
-   *   A BrowserKit Response instance.
-   */
-  protected function filterResponse($response) {
-    $content_type = $response->getHeader('Content-Type');
-
-    if (!$content_type || strpos($content_type, 'charset=') === FALSE) {
-      if (preg_match('/\<meta[^\>]+charset *= *["\']?([a-zA-Z\-0-9]+)/i', $response->getContent(), $matches)) {
-        $headers = $response->getHeaders();
-        $headers['Content-Type'] = $content_type . ';charset=' . $matches[1];
-
-        $response = new Response($response->getContent(), $response->getStatus(), $headers);
-      }
-    }
-
-    return parent::filterResponse($response);
-  }
-
 }
