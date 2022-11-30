bfd51109bc9c5438e79cfb00416f63c610685c51
Dave Long <dave@longwaveconsulting.com>
2022-11-30 15:46:09 +0000

    Issue #3303329 by DieterHolvoet: Referenced method in NoCorrespondingEntityClassException docblock does not exist
    (cherry picked from commit bd4448dbdb99391972b3463aad7e5f17df00262f)

--- core/lib/Drupal/Core/Entity/Exception/NoCorrespondingEntityClassException.php
+++ core/lib/Drupal/Core/Entity/Exception/NoCorrespondingEntityClassException.php
@@ -7,7 +7,7 @@ namespace Drupal\Core\Entity\Exception;
  *
  * This might occur by calling a static method on an abstract class.
  *
- * @see \Drupal\Core\Entity\Entity::getEntityTypeFromStaticClass()
+ * @see \Drupal\Core\Entity\EntityTypeRepositoryInterface::getEntityTypeFromClass()
  */
 class NoCorrespondingEntityClassException extends \Exception {
 
