d09bd3817cac6ebd6ea7a63cf2c2148d96a38859
Lee Rowlands <lee.rowlands@previousnext.com.au>
2022-12-06 07:10:59 +1000

    Issue #2939442 by Lendude, joachim, Ankit.Gupta, Akhil Babu, Kristen Pol, jidrone: views 'Entity Operations' field crashes if on a non-required relationship

--- core/modules/views/src/Plugin/views/field/EntityOperations.php
+++ core/modules/views/src/Plugin/views/field/EntityOperations.php
@@ -127,7 +127,14 @@ class EntityOperations extends FieldPluginBase {
    * {@inheritdoc}
    */
   public function render(ResultRow $values) {
-    $entity = $this->getEntityTranslationByRelationship($this->getEntity($values), $values);
+    $entity = $this->getEntity($values);
+    // Allow for the case where there is no entity, if we are on a non-required
+    // relationship.
+    if (empty($entity)) {
+      return '';
+    }
+
+    $entity = $this->getEntityTranslationByRelationship($entity, $values);
     $operations = $this->entityTypeManager->getListBuilder($entity->getEntityTypeId())->getOperations($entity);
     if ($this->options['destination']) {
       foreach ($operations as &$operation) {
