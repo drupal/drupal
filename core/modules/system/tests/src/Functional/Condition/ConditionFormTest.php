c344680c1dc8420440ec545dbf5486d6baf9f409
Lee Rowlands <lee.rowlands@previousnext.com.au>
2022-12-06 07:25:01 +1000

    Issue #2787529 by Chi, rpayanm, tobiasb, Ankit.Gupta, smustgrave, larowlan, alexpott, borisson_, ameymudras: Missing configuration schema for current_theme condition plugin

--- core/modules/system/tests/src/Functional/Condition/ConditionFormTest.php
+++ core/modules/system/tests/src/Functional/Condition/ConditionFormTest.php
@@ -37,13 +37,21 @@ class ConditionFormTest extends BrowserTestBase {
     $article->save();
 
     $this->drupalGet('condition_test');
-    $this->assertSession()->fieldExists('bundles[article]');
-    $this->assertSession()->fieldExists('bundles[page]');
-    $this->submitForm(['bundles[page]' => 'page', 'bundles[article]' => 'article'], 'Submit');
+    $this->assertSession()->fieldExists('entity_bundle[bundles][article]');
+    $this->assertSession()->fieldExists('entity_bundle[bundles][page]');
+    $this->submitForm(['entity_bundle[bundles][page]' => 'page', 'entity_bundle[bundles][article]' => 'article'], 'Submit');
     // @see \Drupal\condition_test\FormController::submitForm()
     $this->assertSession()->pageTextContains('Bundle: page');
     $this->assertSession()->pageTextContains('Bundle: article');
     $this->assertSession()->pageTextContains('Executed successfully.');
+
+    $this->assertSession()->pageTextContains('The current theme is stark');
+    /** @var \Drupal\Core\Extension\ThemeInstallerInterface $theme_installer */
+    $theme_installer = $this->container->get('theme_installer');
+    $theme_installer->install(['olivero']);
+    $this->drupalGet('condition_test');
+    $this->submitForm(['current_theme[theme]' => 'olivero', 'current_theme[negate]' => TRUE], 'Submit');
+    $this->assertSession()->pageTextContains('The current theme is not olivero');
   }
 
 }
