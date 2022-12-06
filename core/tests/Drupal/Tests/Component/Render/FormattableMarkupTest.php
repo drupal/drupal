acad6dac104e9b3b9662ff69a58ee6dc862fb110
catch <catch@35733.no-reply.drupal.org>
2022-12-06 13:41:56 +0000

    Issue #3255637 by Liam Morland, 3li, joegraduate, rivimey, alexpott, mondrake, ravi.shankar, hswong3i, ameymudras, longwave, Anchal_gupta, neclimdul, Martijn de Wit, sgourebi, pradhumanjainOSL, gambry, kim.pepper: Deprecate NULL values in Html::escape(), ::decodeEntities(), and FormattableMarkup::placeholderFormat() to make it easier to upgrade to PHP 8
    (cherry picked from commit 7430cc3cc7128f1bd88972b2714b3d975b4e948f)

--- core/tests/Drupal/Tests/Component/Render/FormattableMarkupTest.php
+++ core/tests/Drupal/Tests/Component/Render/FormattableMarkupTest.php
@@ -53,6 +53,29 @@ class FormattableMarkupTest extends TestCase {
   }
 
   /**
+   * @covers ::__toString
+   * @dataProvider providerTestNullPlaceholder
+   * @group legacy
+   */
+  public function testNullPlaceholder(string $expected, string $string, array $arguments, string $expected_deprecation): void {
+    $this->expectDeprecation($expected_deprecation);
+    $this->assertEquals($expected, (string) new FormattableMarkup($string, $arguments));
+  }
+
+  /**
+   * Data provider for FormattableMarkupTest::testNullPlaceholder().
+   *
+   * @return array
+   */
+  public function providerTestNullPlaceholder() {
+    return [
+      ['', '@empty', ['@empty' => NULL], 'Deprecated NULL placeholder value for key (@empty) in: "@empty". This will throw a PHP error in drupal:11.0.0. See https://www.drupal.org/node/3318826'],
+      ['', ':empty', [':empty' => NULL], 'Deprecated NULL placeholder value for key (:empty) in: ":empty". This will throw a PHP error in drupal:11.0.0. See https://www.drupal.org/node/3318826'],
+      ['<em class="placeholder"></em>', '%empty', ['%empty' => NULL], 'Deprecated NULL placeholder value for key (%%empty) in: "%%empty". This will throw a PHP error in drupal:11.0.0. See https://www.drupal.org/node/3318826'],
+    ];
+  }
+
+  /**
    * Custom error handler that saves the last error.
    *
    * We need this custom error handler because we cannot rely on the error to
