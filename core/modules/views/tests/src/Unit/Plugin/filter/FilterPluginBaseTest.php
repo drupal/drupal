ee3fedcd2488f886492c656235bf3c479419bcb6
Alex Pott <alex.a.pott@googlemail.com>
2022-12-12 09:27:33 +0000

    Issue #3259090 by Lendude, mr.york, Pandepoulus: Exposed filter equality check works differently in PHP 8.0
    (cherry picked from commit 812ec978dbac0d665377c70a10438d98693225b4)

---
+++ core/modules/views/tests/src/Unit/Plugin/filter/FilterPluginBaseTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\Tests\views\Unit\Plugin\filter;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+
+/**
+ * @coversDefaultClass \Drupal\views\Plugin\views\filter\FilterPluginBase
+ * @group views
+ */
+class FilterPluginBaseTest extends UnitTestCase {
+
+  /**
+   * @covers ::acceptExposedInput
+   *
+   * @dataProvider acceptExposedInputProvider
+   */
+  public function testAcceptExposedInput(bool $expected_result, array $options, array $input) {
+    $definition = [
+      'title' => 'Accept exposed input Test',
+      'group' => 'Test',
+    ];
+    $filter = new FilterPluginBaseStub([], 'stub', $definition);
+    $filter->options = $options;
+    $this->assertSame($expected_result, $filter->acceptExposedInput($input));
+  }
+
+  /**
+   * The data provider for testAcceptExposedInput.
+   *
+   * @return array
+   *   The data set.
+   */
+  public function acceptExposedInputProvider() {
+    return [
+      'not-exposed' => [TRUE, ['exposed' => FALSE], []],
+      'exposed-no-input' => [TRUE, ['exposed' => TRUE], []],
+      'exposed-zero-input' => [FALSE, [
+        'exposed' => TRUE,
+        'is_grouped' => FALSE,
+        'expose' => [
+          'use_operator' => TRUE,
+          'operator_id' => '=',
+          'identifier' => 'identifier',
+        ],
+      ], ['identifier' => 0],
+      ],
+      'exposed-empty-array-input' => [FALSE, [
+        'exposed' => TRUE,
+        'is_grouped' => FALSE,
+        'expose' => [
+          'use_operator' => TRUE,
+          'operator_id' => '=',
+          'identifier' => 'identifier',
+        ],
+      ], ['identifier' => []],
+      ],
+    ];
+  }
+
+}
+
+class FilterPluginBaseStub extends FilterPluginBase {}
