d1e535114ce249054c6d27184ca4ac212974048e
xjm <xjm@65776.no-reply.drupal.org>
2022-12-23 03:59:46 -0600

    Issue #2939442 by Lendude, jidrone, joachim, Akhil Babu, Kristen Pol, larowlan: Views 'Entity Operations' field crashes if on a non-required relationship

--- core/modules/views/src/Plugin/views/field/EntityOperations.php
+++ core/modules/views/src/Plugin/views/field/EntityOperations.php
@@ -127,7 +127,13 @@ class EntityOperations extends FieldPluginBase {
    * {@inheritdoc}
    */
   public function render(ResultRow $values) {
-    $entity = $this->getEntityTranslation($this->getEntity($values), $values);
+    $entity = $this->getEntity($values);
+    // Allow for the case where there is no entity, if we are on a non-required
+    // relationship.
+    if (empty($entity)) {
+      return '';
+    }
+    $entity = $this->getEntityTranslation($entity, $values);
     $operations = $this->entityTypeManager->getListBuilder($entity->getEntityTypeId())->getOperations($entity);
     if ($this->options['destination']) {
       foreach ($operations as &$operation) {
--- core/modules/views/tests/src/Unit/Plugin/views/field/EntityOperationsUnitTest.php
+++ core/modules/views/tests/src/Unit/Plugin/views/field/EntityOperationsUnitTest.php
@@ -174,4 +174,18 @@ class EntityOperationsUnitTest extends UnitTestCase {
     $this->assertSame($expected_build, $build);
   }
 
+  /**
+   * @covers ::render
+   */
+  public function testRenderWithoutEntity() {
+    $entity = NULL;
+
+    $result = new ResultRow();
+    $result->_entity = $entity;
+
+    $expected_build = '';
+    $build = $this->plugin->render($result);
+    $this->assertSame($expected_build, $build);
+  }
+
 }
