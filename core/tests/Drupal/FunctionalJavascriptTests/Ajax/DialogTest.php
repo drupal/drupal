4b43d82ac1ca56a2a3b6cd3b9ca7a5aebd3233e3
catch <catch@35733.no-reply.drupal.org>
2023-01-05 16:18:04 +0000

    Issue #3304378 by bnjmnm: Core FunctionalJavascript tests should not depend on Classy or Starterkit
    (cherry picked from commit 0cbb0a30ec6a84027dead81305c0a5dcb336edf7)

--- core/tests/Drupal/FunctionalJavascriptTests/Ajax/DialogTest.php
+++ core/tests/Drupal/FunctionalJavascriptTests/Ajax/DialogTest.php
@@ -21,7 +21,7 @@ class DialogTest extends WebDriverTestBase {
   /**
    * {@inheritdoc}
    */
-  protected $defaultTheme = 'starterkit_theme';
+  protected $defaultTheme = 'stark';
 
   /**
    * Tests sending non-JS and AJAX requests to open and manipulate modals.
