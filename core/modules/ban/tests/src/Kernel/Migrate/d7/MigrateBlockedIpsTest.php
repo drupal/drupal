8d6284210fc249203c699b3255f58da6e88e3fbb
Alex Pott <alex.a.pott@googlemail.com>
2023-01-13 15:39:46 +0000

    Issue #3260391 by huzooka, arunkumark, Spokje, quietone: BlockedIp::import violates MigrateDestinationInterface::import
    (cherry picked from commit 4936c9bd0724e08de05ca5d6596e3451564410c7)

--- core/modules/ban/tests/src/Kernel/Migrate/d7/MigrateBlockedIpsTest.php
+++ core/modules/ban/tests/src/Kernel/Migrate/d7/MigrateBlockedIpsTest.php
@@ -27,13 +27,15 @@ class MigrateBlockedIpsTest extends MigrateDrupal7TestBase {
   protected function setUp(): void {
     parent::setUp();
     $this->installSchema('ban', ['ban_ip']);
-    $this->executeMigration('d7_blocked_ips');
   }
 
   /**
    * Tests migration of blocked IPs.
    */
   public function testBlockedIps() {
+    $this->startCollectingMessages();
+    $this->executeMigration('d7_blocked_ips');
+    $this->assertEmpty($this->migrateMessages);
     $this->assertTrue(\Drupal::service('ban.ip_manager')->isBanned('111.111.111.111'));
   }
 
