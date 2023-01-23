9dc5c66ea2b36fcdcb9c2c5f955535c4a0998c9f
catch <catch@35733.no-reply.drupal.org>
2023-01-23 17:28:40 +0000

    Issue #3112866 by alexpott, andypost, smustgrave: Remove special case of User module install

--- core/lib/Drupal/Core/Config/ConfigImporter.php
+++ core/lib/Drupal/Core/Config/ConfigImporter.php
@@ -415,11 +415,30 @@ class ConfigImporter {
 
     // Determine which modules to install.
     $install = array_keys(array_diff_key($new_extensions['module'], $current_extensions['module']));
+    // Always install required modules first. Respect the dependencies between
+    // the modules.
+    $install_required = [];
+    $install_non_required = [];
+    foreach ($install as $module) {
+      if (!isset($module_data[$module])) {
+        // The module doesn't exist. This is handled in
+        // \Drupal\Core\EventSubscriber\ConfigImportSubscriber::validateModules().
+        continue;
+      }
+      if (!empty($module_data[$module]->info['required'])) {
+        $install_required[$module] = $module_data[$module]->sort;
+      }
+      else {
+        $install_non_required[$module] = $module_data[$module]->sort;
+      }
+    }
     // Ensure that installed modules are sorted in exactly the reverse order
     // (with dependencies installed first, and modules of the same weight sorted
     // in alphabetical order).
-    $module_list = array_reverse($module_list);
-    $this->extensionChangelist['module']['install'] = array_intersect(array_keys($module_list), $install);
+    arsort($install_required);
+    arsort($install_non_required);
+
+    $this->extensionChangelist['module']['install'] = array_keys($install_required + $install_non_required);
 
     // If we're installing the install profile ensure it comes last. This will
     // occur when installing a site from configuration.
