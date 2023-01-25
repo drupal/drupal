da048c0814616a67a7dc8a8a2a6dcf88a440f192
Alex Pott <alex.a.pott@googlemail.com>
2023-01-25 09:13:49 +0000

    Issue #3294619 by mohit_aghera, Kasey_MK, smustgrave, alexpott: Missing schema for comment_link_approve views field configuration
    (cherry picked from commit 7347556c05d2b2bd53e69780ad21dfe44bb0d8c8)

---
+++ core/modules/comment/tests/src/Functional/Views/CommentApproveLinkTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\Tests\comment\Functional\Views;
+
+/**
+ * Test the "approve_comment" views field for approving comments.
+ *
+ * @group comment
+ */
+class CommentApproveLinkTest extends CommentTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'comment',
+    'comment_test_views',
+    'system',
+    'views',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Views used by this test.
+   *
+   * @var array
+   */
+  public static $testViews = ['test_comment_schema'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp($import_test_views = TRUE, $modules = ['comment_test_views']): void {
+    parent::setUp($import_test_views, $modules);
+  }
+
+  /**
+   * Tests that "approve comment" link exists and works as expected.
+   */
+  public function testCommentApproveLink() {
+    $this->drupalLogin($this->drupalCreateUser(['administer comments']));
+    // Set the comment status to unpublished.
+    $this->comment->setUnpublished();
+    $this->comment->save();
+    $this->drupalGet('/admin/moderate-comments');
+    $this->assertSession()->pageTextContains($this->comment->getSubject());
+    $this->assertSession()->linkExists('Approve');
+    $this->clickLink('Approve');
+    $this->drupalGet('/admin/moderate-comments');
+    $this->assertSession()->linkNotExists('Approve');
+    // Ensure that "published" column in table is marked as yes.
+    $this->assertSession()->elementTextContains('xpath', "//table/tbody/tr/td[3]", 'Yes');
+  }
+
+}
