33366362efbe4f50f654e29d0925024cfc45a76a
catch <catch@35733.no-reply.drupal.org>
2023-01-27 13:39:31 +0000

    Issue #3033904 by murilohp, stefan.korn, smustgrave: Hide empty description for an administrative block

--- core/modules/system/tests/src/Functional/System/AdminTest.php
+++ core/modules/system/tests/src/Functional/System/AdminTest.php
@@ -31,7 +31,7 @@ class AdminTest extends BrowserTestBase {
    *
    * @var array
    */
-  protected static $modules = ['locale'];
+  protected static $modules = ['locale', 'menu_test'];
 
   /**
    * {@inheritdoc}
@@ -180,6 +180,19 @@ class AdminTest extends BrowserTestBase {
     $this->assertNull($session->getCookie('Drupal.visitor.admin_compact_mode'), 'Compact mode remains off after a repeat call.');
     $this->drupalGet('');
     $this->assertNull($session->getCookie('Drupal.visitor.admin_compact_mode'), 'Compact mode persists off new requests.');
+  }
+
+  /**
+   * Tests admin config page blocks without descriptions.
+   */
+  public function testConfigBlocksDescription(): void {
+    // Go to Config administration page.
+    $this->drupalGet('admin/config');
+    $this->assertSession()->statusCodeEquals(200);
+    // Validates the custom block without description.
+    $this->assertSession()->pageTextContains('Test custom admin block without description');
+    // Validates an empty description block.
+    $this->assertSession()->elementNotExists('xpath', '//dd[@class="list-group__description"][not(text())]');
   }
 
 }
