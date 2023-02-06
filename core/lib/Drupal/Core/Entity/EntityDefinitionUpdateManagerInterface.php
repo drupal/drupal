c4383a76d227a25c28275b59bcf5e2f1dc14d52a
catch <catch@35733.no-reply.drupal.org>
2023-02-06 13:22:47 +0000

    Issue #3099085 by krystalcode: EntityDefinitionUpdateManagerInterface docblock improvement
    (cherry picked from commit 8fd8edd8f8ba53096f3b2ff1c4911f1826881a1a)

--- core/lib/Drupal/Core/Entity/EntityDefinitionUpdateManagerInterface.php
+++ core/lib/Drupal/Core/Entity/EntityDefinitionUpdateManagerInterface.php
@@ -184,8 +184,9 @@ interface EntityDefinitionUpdateManagerInterface {
    * @param string $entity_type_id
    *   The entity type identifier.
    *
-   * @return \Drupal\Core\Field\FieldStorageDefinitionInterface
-   *   The field storage definition.
+   * @return \Drupal\Core\Field\FieldStorageDefinitionInterface|null
+   *   The field storage definition or NULL if there is none for the given field
+   *   name and entity type.
    *
    * @todo Make this return a mutable storage definition interface when we have
    *   one. See https://www.drupal.org/node/2346329.
