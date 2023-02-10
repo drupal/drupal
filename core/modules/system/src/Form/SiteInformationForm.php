c90678046d831b3f36884996881b7dfdb1837703
catch <catch@35733.no-reply.drupal.org>
2023-02-10 14:50:45 +0000

    Issue #2671174 by pameeela, amol.palhade17, leymannx, longwave, priyanka.sahni, yashingole, quietone, rbaprado, jackalope: Make default front page setting required and remove special case of /user/login

--- core/modules/system/src/Form/SiteInformationForm.php
+++ core/modules/system/src/Form/SiteInformationForm.php
@@ -123,13 +123,13 @@ class SiteInformationForm extends ConfigFormBase {
       '#title' => $this->t('Front page'),
       '#open' => TRUE,
     ];
-    $front_page = $site_config->get('page.front') != '/user/login' ? $this->aliasManager->getAliasByPath($site_config->get('page.front')) : '';
     $form['front_page']['site_frontpage'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Default front page'),
-      '#default_value' => $front_page,
+      '#default_value' => $this->aliasManager->getAliasByPath($site_config->get('page.front')),
+      '#required' => TRUE,
       '#size' => 40,
-      '#description' => $this->t('Optionally, specify a relative URL to display as the front page. Leave blank to display the default front page.'),
+      '#description' => $this->t('Specify a relative URL to display as the front page.'),
       '#field_prefix' => $this->requestContext->getCompleteBaseUrl(),
     ];
     $form['error_page'] = [
@@ -159,15 +159,8 @@ class SiteInformationForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    // Check for empty front page path.
-    if ($form_state->isValueEmpty('site_frontpage')) {
-      // Set to default "user/login".
-      $form_state->setValueForElement($form['front_page']['site_frontpage'], '/user/login');
-    }
-    else {
-      // Get the normal path of the front page.
-      $form_state->setValueForElement($form['front_page']['site_frontpage'], $this->aliasManager->getPathByAlias($form_state->getValue('site_frontpage')));
-    }
+    // Get the normal path of the front page.
+    $form_state->setValueForElement($form['front_page']['site_frontpage'], $this->aliasManager->getPathByAlias($form_state->getValue('site_frontpage')));
     // Validate front page path.
     if (($value = $form_state->getValue('site_frontpage')) && $value[0] !== '/') {
       $form_state->setErrorByName('site_frontpage', $this->t("The path '%path' has to start with a slash.", ['%path' => $form_state->getValue('site_frontpage')]));
