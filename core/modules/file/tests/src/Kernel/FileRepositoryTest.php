8caea2cf9bde9cf204ec6cf1e76bd39aba7de538
Dave Long <dave@longwaveconsulting.com>
2023-02-19 12:23:03 +0000

    Issue #3328694 by murilohp, rpayanm, jonathan1055, xjm, cilefen: Incorrect sprintf parameter usage
    (cherry picked from commit 465482a5718495b253d138aba2ba2b05d3402dbf)

--- core/modules/file/tests/src/Kernel/FileRepositoryTest.php
+++ core/modules/file/tests/src/Kernel/FileRepositoryTest.php
@@ -170,6 +170,7 @@ class FileRepositoryTest extends FileManagedUnitTestBase {
    */
   public function testInvalidStreamWrapper() {
     $this->expectException(InvalidStreamWrapperException::class);
+    $this->expectExceptionMessage('Invalid stream wrapper: foo://');
     $this->fileRepository->writeData('asdf', 'foo://');
   }
 
