6b5efdc1169faf7cc85fbb085c5b80cf7ffb1a95
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-02-20 10:52:48 +1000

    Issue #3039499 by alexpott, acbramley: Role permissions not sorted in config export

--- core/modules/user/user.post_update.php
+++ core/modules/user/user.post_update.php
@@ -5,6 +5,9 @@
  * Post update functions for User module.
  */
 
+use Drupal\Core\Config\Entity\ConfigEntityUpdater;
+use Drupal\user\Entity\Role;
+
 /**
  * Implements hook_removed_post_updates().
  */
@@ -13,4 +16,15 @@ function user_removed_post_updates() {
     'user_post_update_enforce_order_of_permissions' => '9.0.0',
     'user_post_update_update_roles' => '10.0.0',
   ];
+}
+
+/**
+ * Ensure permissions stored in role configuration are sorted using the schema.
+ */
+function user_post_update_sort_permissions(&$sandbox = NULL) {
+  \Drupal::classResolver(ConfigEntityUpdater::class)->update($sandbox, 'user_role', function (Role $role) {
+    $permissions = $role->getPermissions();
+    sort($permissions);
+    return $permissions !== $role->getPermissions();
+  });
 }
