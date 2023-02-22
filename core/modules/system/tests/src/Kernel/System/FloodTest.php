563b977e39e828f4657f9688b73e3101c4190cef
catch <catch@35733.no-reply.drupal.org>
2023-02-22 16:07:58 +0000

    Issue #2106939 by smustgrave, dawehner, catch, jungle, longwave, xjm: Improve test coverage of the flood memory backend test and convert it to a unit test

--- core/modules/system/tests/src/Kernel/System/FloodTest.php
+++ core/modules/system/tests/src/Kernel/System/FloodTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests\system\Kernel\System;
 
 use Drupal\Core\Flood\DatabaseBackend;
-use Drupal\Core\Flood\MemoryBackend;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
@@ -44,46 +43,6 @@ class FloodTest extends KernelTestBase {
     // Run cron and verify event is still not allowed.
     $cron->run();
     $this->assertFalse($flood->isAllowed($name, $threshold));
-  }
-
-  /**
-   * Tests flood control memory backend.
-   */
-  public function testMemoryBackend() {
-    $threshold = 1;
-    $window_expired = -1;
-    $name = 'flood_test_cleanup';
-
-    $request_stack = \Drupal::service('request_stack');
-    $flood = new MemoryBackend($request_stack);
-    $this->assertTrue($flood->isAllowed($name, $threshold));
-    // Register expired event.
-    $flood->register($name, $window_expired);
-    // Verify event is not allowed.
-    $this->assertFalse($flood->isAllowed($name, $threshold));
-    // Run cron and verify event is now allowed.
-    $flood->garbageCollection();
-    $this->assertTrue($flood->isAllowed($name, $threshold));
-
-    // Register unexpired event.
-    $flood->register($name);
-    // Verify event is not allowed.
-    $this->assertFalse($flood->isAllowed($name, $threshold));
-    // Run cron and verify event is still not allowed.
-    $flood->garbageCollection();
-    $this->assertFalse($flood->isAllowed($name, $threshold));
-  }
-
-  /**
-   * Tests memory backend records events to the nearest microsecond.
-   */
-  public function testMemoryBackendThreshold() {
-    $request_stack = \Drupal::service('request_stack');
-    $flood = new MemoryBackend($request_stack);
-    $flood->register('new event');
-    $this->assertTrue($flood->isAllowed('new event', '2'));
-    $flood->register('new event');
-    $this->assertFalse($flood->isAllowed('new event', '2'));
   }
 
   /**
