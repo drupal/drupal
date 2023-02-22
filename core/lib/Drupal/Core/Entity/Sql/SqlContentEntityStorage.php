85c9ee6ab8717858eb1de9b27d9f16619ede3b1a
catch <catch@35733.no-reply.drupal.org>
2023-02-22 10:06:47 +0000

    Issue #2859042 by Sam152, plach, maximpodorov, Ankit.Gupta, Berdir, acbramley, hchonov, timmillwood, mheip, amateescu, matsbla: Impossible to update an entity revision if the field value you are updating matches the default revision

--- core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorage.php
+++ core/lib/Drupal/Core/Entity/Sql/SqlContentEntityStorage.php
@@ -1302,6 +1302,11 @@ class SqlContentEntityStorage extends ContentEntityStorageBase implements SqlEnt
 
     $original = !empty($entity->original) ? $entity->original : NULL;
 
+    // Use the loaded revision instead of default one to check for data change.
+    if ($original && !$entity->isNewRevision() && !$entity->isDefaultRevision()) {
+      $original = $this->loadRevision($entity->getLoadedRevisionId());
+    }
+
     // Determine which fields should be actually stored.
     $definitions = $this->entityFieldManager->getFieldDefinitions($entity_type, $bundle);
     if ($names) {
--- core/tests/Drupal/KernelTests/Core/Entity/EntityFieldTest.php
+++ core/tests/Drupal/KernelTests/Core/Entity/EntityFieldTest.php
@@ -21,6 +21,7 @@ use Drupal\Core\TypedData\Type\StringInterface;
 use Drupal\Core\TypedData\TypedDataInterface;
 use Drupal\entity_test\Entity\EntityTest;
 use Drupal\entity_test\Entity\EntityTestComputedField;
+use Drupal\entity_test\Entity\EntityTestRev;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
 
@@ -96,6 +97,38 @@ class EntityFieldTest extends EntityKernelTestBase {
     $entity->field_test_text->value = $this->entityFieldText;
 
     return $entity;
+  }
+
+  /**
+   * Test setting field values on revisionable entities.
+   */
+  public function testFieldEntityRevisionWrite() {
+    $storage = \Drupal::entityTypeManager()->getStorage('entity_test_rev');
+
+    // Create a new entity, with a field value 'foo'.
+    $entity = EntityTestRev::create();
+    $entity->field_test_text->value = 'foo';
+    $entity->save();
+
+    // Create a new non-default revision and set the field value to 'bar'.
+    $entity->setNewRevision(TRUE);
+    $entity->isDefaultRevision(FALSE);
+    $entity->field_test_text->value = 'bar';
+    $entity->save();
+
+    $forward_revision_id = $entity->getRevisionId();
+
+    // Load the forward revision and set the field value to equal the value of
+    // the default revision.
+    $forward_revision = $storage->loadRevision($forward_revision_id);
+    $forward_revision->field_test_text->value = 'foo';
+    $forward_revision->save();
+
+    $storage->resetCache();
+
+    // The updated field value should have correctly saved as 'foo'.
+    $forward_revision = $storage->loadRevision($forward_revision_id);
+    $this->assertEquals('foo', $forward_revision->field_test_text->value);
   }
 
   /**
