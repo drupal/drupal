70a2c35c4aedcea95920b6ac5050433248332b87
xjm <xjm@65776.no-reply.drupal.org>
2023-02-22 08:25:54 -0600

    Revert "Issue #2106939 by smustgrave, dawehner, jungle: Improve test coverage of the flood memory backend test and convert it to a unit test"
    This reverts commit 295a87f07f4cba3e3eedc1ea25ba030bc03e350c.

--- core/modules/system/tests/src/Kernel/System/FloodTest.php
+++ core/modules/system/tests/src/Kernel/System/FloodTest.php
@@ -47,6 +47,46 @@ class FloodTest extends KernelTestBase {
   }
 
   /**
+   * Tests flood control memory backend.
+   */
+  public function testMemoryBackend() {
+    $threshold = 1;
+    $window_expired = -1;
+    $name = 'flood_test_cleanup';
+
+    $request_stack = \Drupal::service('request_stack');
+    $flood = new MemoryBackend($request_stack);
+    $this->assertTrue($flood->isAllowed($name, $threshold));
+    // Register expired event.
+    $flood->register($name, $window_expired);
+    // Verify event is not allowed.
+    $this->assertFalse($flood->isAllowed($name, $threshold));
+    // Run cron and verify event is now allowed.
+    $flood->garbageCollection();
+    $this->assertTrue($flood->isAllowed($name, $threshold));
+
+    // Register unexpired event.
+    $flood->register($name);
+    // Verify event is not allowed.
+    $this->assertFalse($flood->isAllowed($name, $threshold));
+    // Run cron and verify event is still not allowed.
+    $flood->garbageCollection();
+    $this->assertFalse($flood->isAllowed($name, $threshold));
+  }
+
+  /**
+   * Tests memory backend records events to the nearest microsecond.
+   */
+  public function testMemoryBackendThreshold() {
+    $request_stack = \Drupal::service('request_stack');
+    $flood = new MemoryBackend($request_stack);
+    $flood->register('new event');
+    $this->assertTrue($flood->isAllowed('new event', '2'));
+    $flood->register('new event');
+    $this->assertFalse($flood->isAllowed('new event', '2'));
+  }
+
+  /**
    * Tests flood control database backend.
    */
   public function testDatabaseBackend() {
