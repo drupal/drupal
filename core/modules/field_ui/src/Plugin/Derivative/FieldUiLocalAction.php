e03201efa8cf4ba200d77d1523ba1e327717cea5
bnjmnm <benm@umich.edu>
2023-02-22 14:57:26 -0500

    explain the noop

--- core/modules/field_ui/src/Plugin/Derivative/FieldUiLocalAction.php
+++ core/modules/field_ui/src/Plugin/Derivative/FieldUiLocalAction.php
@@ -62,6 +62,8 @@ class FieldUiLocalAction extends DeriverBase implements ContainerDeriverInterfac
         $this->derivatives["field_storage_config_add_$entity_type_id"] = [
           'route_name' => "field_ui.field_storage_config_add_$entity_type_id",
           'title' => $this->t('Add field'),
+          // Hacky way to get rid of this appearing. This button is replaced by
+          // field type specific ones.
           'appears_on' => ["entity.$entity_type_id.noop.field_ui_fields"],
         ];
       }
