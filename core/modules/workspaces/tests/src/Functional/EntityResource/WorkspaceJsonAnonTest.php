7cff4a621ee064e75721f4a25c5734f4cd5911c0
Alex Pott <alex.a.pott@googlemail.com>
2023-02-23 01:07:24 +0000

    Revert "Issue #3091478 by Tim Bozeman, lauriii, malcomio, EclipseGc, larowlan, tim.plunkett, amateescu, dpi: Improve StringItem::generateSampleValue()"
    This reverts commit 829cd7ff156a3cefd5c7c41f5672b1bb069e0bbe.

--- core/modules/workspaces/tests/src/Functional/EntityResource/WorkspaceJsonAnonTest.php
+++ core/modules/workspaces/tests/src/Functional/EntityResource/WorkspaceJsonAnonTest.php
@@ -28,23 +28,4 @@ class WorkspaceJsonAnonTest extends WorkspaceResourceTestBase {
    */
   protected $defaultTheme = 'stark';
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function getModifiedEntityForPostTesting(): array {
-    $values = parent::getModifiedEntityForPostTesting();
-    // The ID field for a workspace uses a string field, with a validation
-    // constraint that applies a regex pattern that prevents whitespace.
-    // \Drupal\Core\Field\Plugin\Field\FieldType\StringItem::generateSampleValue
-    // used in the parent implementation of ::getModifiedEntityForPostTesting
-    // can generate whitespace, so we use the same regex pattern here to ensure
-    // the generated value is valid for the sake of the test.
-    //
-    // @see \Drupal\Core\Field\Plugin\Field\FieldType\StringItem::generateSampleValue
-    // @see \Drupal\Tests\rest\Functional\EntityResource\EntityResourceTestBase::getModifiedEntityForPostTesting
-    // @see \Drupal\workspaces\Entity\Workspace::baseFieldDefinitions
-    $values['id'] = preg_replace('/[^a-z0-9_]/', '', $values['id']);
-    return $values;
-  }
-
 }
