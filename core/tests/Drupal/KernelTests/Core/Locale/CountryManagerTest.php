17aa9182ff6870611e32e3096c33b5cbe36b4827
Dave Long <dave@longwaveconsulting.com>
2023-03-12 18:56:09 +0000

    Issue #3344424 by Niklan, Chi: Add tests for hook_countries_alter()

---
+++ core/tests/Drupal/KernelTests/Core/Locale/CountryManagerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\KernelTests\Core\Locale;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Tests Country Manager functionality.
+ *
+ * @group CountryManager
+ * @covers Drupal\Core\Locale\CountryManager
+ */
+class CountryManagerTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'language',
+    'locale',
+    'locale_test',
+  ];
+
+  /**
+   * Tests that hook_countries_alters() works as expected.
+   */
+  public function testHookCountriesAlter(): void {
+    $countries = $this->container->get('country_manager')->getList();
+    self::assertArrayHasKey('EB', $countries);
+    self::assertSame('Elbonia', $countries['EB']);
+  }
+
+}
