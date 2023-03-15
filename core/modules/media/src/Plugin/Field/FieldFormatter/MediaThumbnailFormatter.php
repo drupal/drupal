447772e33a079716ed9d6b4a094a905df2dada81
Dave Long <dave@longwaveconsulting.com>
2023-03-15 14:30:17 +0000

    SA-CORE-2023-002 by larowlan, james.williams, xjm, longwave, danflanagan8, jenlampton, pandaski, benjifisher

--- core/modules/media/src/Plugin/Field/FieldFormatter/MediaThumbnailFormatter.php
+++ core/modules/media/src/Plugin/Field/FieldFormatter/MediaThumbnailFormatter.php
@@ -184,6 +184,14 @@ class MediaThumbnailFormatter extends ImageFormatter {
   }
 
   /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity) {
+    return $entity->access('view', NULL, TRUE)
+      ->andIf(parent::checkAccess($entity));
+  }
+
+  /**
    * Get the URL for the media thumbnail.
    *
    * @param \Drupal\media\MediaInterface $media
