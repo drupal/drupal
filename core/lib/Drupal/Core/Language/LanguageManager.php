0da94b8e92d2d5a34a932cb38f6b96aa5624854b
Dave Long <dave@longwaveconsulting.com>
2023-03-17 09:28:45 +0000

    Issue #3348569 by lauriii: Update UN official language list source
    (cherry picked from commit b97fb7838a09ea16a972d5b2f66d5b718f22ad84)

--- core/lib/Drupal/Core/Language/LanguageManager.php
+++ core/lib/Drupal/Core/Language/LanguageManager.php
@@ -338,9 +338,8 @@ class LanguageManager implements LanguageManagerInterface {
   /**
    * The 6 official languages used at the United Nations.
    *
-   * This list is based on
-   * http://www.un.org/en/sections/about-un/official-languages/index.html and it
-   * uses the same format as getStandardLanguageList().
+   * This list is based on https://www.un.org/en/our-work/official-languages
+   * and it uses the same format as getStandardLanguageList().
    *
    * @return array
    *   An array with language codes as keys, and English and native language
