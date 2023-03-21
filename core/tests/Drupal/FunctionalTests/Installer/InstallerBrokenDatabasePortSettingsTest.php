442c0e41e816a47a81d9c94af87dfe6d9e27ff8f
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-03-21 15:15:03 +1000

    Issue #2610858 by cilefen, ravi.shankar, Cameron Tod, shubham.prakash, joachim, mohit_aghera, Madhura BK, kostajh, asvira, vacho, Grevil, daffie, dawehner, mradcliffe: Add informative error message for 'Connection refused' errors in MySQL

---
+++ core/tests/Drupal/FunctionalTests/Installer/InstallerBrokenDatabasePortSettingsTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\FunctionalTests\Installer;
+
+use Drupal\Core\Database\Database;
+
+/**
+ * Tests the installer with incorrect connection info in settings.php.
+ *
+ * @group Installer
+ */
+class InstallerBrokenDatabasePortSettingsTest extends InstallerTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function prepareEnvironment() {
+    parent::prepareEnvironment();
+    // Pre-configure database credentials in settings.php.
+    $connection_info = Database::getConnectionInfo();
+
+    if ($connection_info['default']['driver'] !== 'mysql') {
+      $this->markTestSkipped('This test relies on overriding the mysql driver');
+    }
+
+    // Override the port number to random port.
+    $connection_info['default']['port'] = 1234;
+
+    $this->settings['databases']['default'] = (object) [
+      'value' => $connection_info,
+      'required' => TRUE,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUpSettings() {
+    // This form will never be reached.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUpSite() {
+    // This form will never be reached.
+  }
+
+  /**
+   * Tests the expected requirements problem.
+   */
+  public function testRequirementsProblem() {
+    $this->assertSession()->titleEquals('Requirements problem | Drupal');
+    $this->assertSession()->pageTextContains('Database settings');
+    $this->assertSession()->pageTextContains('Resolve all issues below to continue the installation. For help configuring your database server,');
+    $this->assertSession()->pageTextContains("[Tip: This message normally means that there is no MySQL server running on the system or that you are using an incorrect host name or port number when trying to connect to the server. You should also check that the TCP/IP port you are using has not been blocked by a firewall or port blocking service.]");
+  }
+
+}
