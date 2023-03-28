68c54bf37fd04984c16bfa027ec95b635faecb64
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-03-29 09:51:46 +1000

    Issue #3281557 by smustgrave, ipo4ka704, acbramley, ravi.shankar, benjifisher, Sardis, ameymudras, adeshsharma, Rishabh Vishwakarma, rollins, TanujJain-TJ, larowlan, catch, igorbiki: DateTime::__construct(): Passing null to parameter #1 ($datetime) of type string is deprecated
    (cherry picked from commit 2ce27f62409c17877fae4c1cc53c7031e6c2d7b4)

--- core/tests/Drupal/Tests/Core/Datetime/DateHelperTest.php
+++ core/tests/Drupal/Tests/Core/Datetime/DateHelperTest.php
@@ -4,6 +4,7 @@ namespace Drupal\Tests\Core\Datetime;
 
 use Drupal\Core\Datetime\DateHelper;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\Language\Language;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -11,6 +12,36 @@ use Drupal\Tests\UnitTestCase;
  * @group Datetime
  */
 class DateHelperTest extends UnitTestCase {
+
+  /**
+   * The language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface|\PHPUnit\Framework\MockObject\MockObject
+   */
+  protected $languageManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $container = new ContainerBuilder();
+    $config = ['system.date' => ['first_day' => 'Sunday']];
+    $container->set('config.factory', $this->getConfigFactoryStub($config));
+
+    $this->languageManager = $this->createMock('\Drupal\Core\Language\LanguageManagerInterface');
+    $language = new Language(['langcode' => 'en']);
+    $this->languageManager->expects($this->any())
+      ->method('getDefaultLanguage')
+      ->will($this->returnValue($language));
+    $this->languageManager->expects($this->any())
+      ->method('getCurrentLanguage')
+      ->will($this->returnValue($language));
+    $container->set('language_manager', $this->languageManager);
+
+    \Drupal::setContainer($container);
+  }
 
   /**
    * @covers ::weekDaysOrdered
@@ -126,6 +157,105 @@ class DateHelperTest extends UnitTestCase {
       ],
     ];
     return $data;
+  }
+
+  /**
+   * @covers ::daysInMonth
+   */
+  public function testDaysInMonth() {
+    // @todo Consider deprecating passing NULL in
+    //   https://www.drupal.org/project/drupal/issues/3299788
+    // Passing NULL, FALSE, or an empty string should default to now. Just
+    // check these are NOT null to avoid copying the implementation here.
+    $this->assertNotNull(DateHelper::daysInMonth());
+    $this->assertNotNull(DateHelper::daysInMonth(FALSE));
+    $this->assertNotNull(DateHelper::daysInMonth(''));
+
+    // Pass nothing and expect to get NULL.
+    $this->assertNull(DateHelper::daysInMonth(0));
+    $this->assertNull(DateHelper::daysInMonth('0'));
+
+    $value = '2022-12-31 00:00:00';
+    $dateString = DateHelper::daysInMonth($value);
+    $this->assertEquals('31', $dateString);
+
+    $value = '2020-11-30 00:00:00';
+    $dateString = DateHelper::daysInMonth($value);
+    $this->assertEquals('30', $dateString);
+  }
+
+  /**
+   * @covers ::daysInYear
+   */
+  public function testDaysInYear() {
+    // Passing NULL, FALSE, or an empty string should default to now. Just
+    // check these are NOT null to avoid copying the implementation here.
+    $this->assertNotNull(DateHelper::daysInYear());
+    $this->assertNotNull(DateHelper::daysInYear(FALSE));
+    $this->assertNotNull(DateHelper::daysInYear(''));
+
+    // Pass nothing and expect to get NULL.
+    $this->assertNull(DateHelper::daysInYear(0));
+    $this->assertNull(DateHelper::daysInYear('0'));
+
+    $value = '2022-12-31 00:00:00';
+    $dateString = DateHelper::daysInYear($value);
+    $this->assertEquals('365', $dateString);
+
+    // 2020 is a leap year.
+    $value = '2020-11-30 00:00:00';
+    $dateString = DateHelper::daysInYear($value);
+    $this->assertEquals('366', $dateString);
+  }
+
+  /**
+   * @covers ::dayOfWeek
+   */
+  public function testDayOfWeek() {
+    // Passing NULL, FALSE, or an empty string should default to now. Just
+    // check these are NOT null to avoid copying the implementation here.
+    $this->assertNotNull(DateHelper::dayOfWeek());
+    $this->assertNotNull(DateHelper::dayOfWeek(FALSE));
+    $this->assertNotNull(DateHelper::dayOfWeek(''));
+
+    // Pass nothing and expect to get NULL.
+    $this->assertNull(DateHelper::dayOfWeek(0));
+    $this->assertNull(DateHelper::dayOfWeek('0'));
+
+    // December 31st 2022 is a Saturday.
+    $value = '2022-12-31 00:00:00';
+    $dateString = DateHelper::dayOfWeek($value);
+    $this->assertEquals('6', $dateString);
+
+    // November 30th 2020 is a Monday.
+    $value = '2020-11-30 00:00:00';
+    $dateString = DateHelper::dayOfWeek($value);
+    $this->assertEquals('1', $dateString);
+  }
+
+  /**
+   * @covers ::dayOfWeekName
+   */
+  public function testDayOfWeekName() {
+    // Passing NULL, FALSE, or an empty string should default to now. Just
+    // check these are NOT null to avoid copying the implementation here.
+    $this->assertNotNull(DateHelper::dayOfWeekName());
+    $this->assertNotNull(DateHelper::dayOfWeekName(FALSE));
+    $this->assertNotNull(DateHelper::dayOfWeekName(''));
+
+    // Pass nothing and expect to get NULL.
+    $this->assertNull(DateHelper::dayOfWeekName(0));
+    $this->assertNull(DateHelper::dayOfWeekName('0'));
+
+    // December 31st 2022 is a Saturday.
+    $value = '2022-12-31 00:00:00';
+    $dateString = DateHelper::dayOfWeekName($value);
+    $this->assertEquals('Sat', $dateString);
+
+    // November 30th 2020 is a Monday.
+    $value = '2020-11-30 00:00:00';
+    $dateString = DateHelper::dayOfWeekName($value);
+    $this->assertEquals('Mon', $dateString);
   }
 
 }
