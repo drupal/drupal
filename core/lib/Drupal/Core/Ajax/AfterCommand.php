bfa2a1bfb84d20c417d43aff38c5c40d5d3eaf47
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-03-29 16:41:46 +1000

    Issue #2758915 by mdupont, darvanen, -enzo-, Amber Himes Matz, smustgrave: AJAX commands documentation is misleading when working with render arrays

--- core/lib/Drupal/Core/Ajax/AfterCommand.php
+++ core/lib/Drupal/Core/Ajax/AfterCommand.php
@@ -6,8 +6,8 @@ namespace Drupal\Core\Ajax;
  * An AJAX command for calling the jQuery after() method.
  *
  * The 'insert/after' command instructs the client to use jQuery's after()
- * method to insert the given HTML content after each element matched by the
- * given selector.
+ * method to insert the given render array or HTML content after each element
+ * matched by the given selector.
  *
  * This command is implemented by Drupal.AjaxCommands.prototype.insert()
  * defined in misc/ajax.js.
