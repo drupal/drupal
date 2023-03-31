3e82115d8b32e70ac7819e79449c49ca50539551
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-03-31 18:36:59 +1000

    Issue #3331397 by gcb, DanielVeza, Tatsiana, arisen: ListItemBase::generateSampleValue() white screen when options list is empty
    (cherry picked from commit 52fbc9adcb5f4c9fbec202916b2b4fe529927af2)

--- core/modules/options/src/Plugin/Field/FieldType/ListItemBase.php
+++ core/modules/options/src/Plugin/Field/FieldType/ListItemBase.php
@@ -65,6 +65,10 @@ abstract class ListItemBase extends FieldItemBase implements OptionsProviderInte
    */
   public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
     $allowed_options = options_allowed_values($field_definition->getFieldStorageDefinition());
+    if (empty($allowed_options)) {
+      $values['value'] = NULL;
+      return $values;
+    }
     $values['value'] = array_rand($allowed_options);
     return $values;
   }
