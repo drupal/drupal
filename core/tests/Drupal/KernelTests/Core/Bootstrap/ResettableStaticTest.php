d070ec1d3fe6b53008f52979f0c4cde29755eef1
catch <catch@35733.no-reply.drupal.org>
2023-04-04 09:02:50 +0100

    Issue #3136041 by jweowu: drupal_static() edge case bug and inconsistent comments

--- core/tests/Drupal/KernelTests/Core/Bootstrap/ResettableStaticTest.php
+++ core/tests/Drupal/KernelTests/Core/Bootstrap/ResettableStaticTest.php
@@ -36,6 +36,25 @@ class ResettableStaticTest extends KernelTestBase {
     $var = 'bar';
     drupal_static_reset();
     $this->assertEquals('foo', $var, 'Variable was reset after second invocation of global reset.');
+
+    // Test calling drupal_static() with no arguments (empty string).
+    $name1 = __CLASS__ . '_' . __METHOD__ . '1';
+    $name2 = '';
+    $var1 = &drupal_static($name1, 'initial1');
+    $var2 = &drupal_static($name2, 'initial2');
+    $this->assertEquals('initial1', $var1, 'Variable 1 returned by drupal_static() was set to its default.');
+    $this->assertEquals('initial2', $var2, 'Variable 2 returned by drupal_static() was set to its default.');
+    $var1 = 'modified1';
+    $var2 = 'modified2';
+    drupal_static_reset($name1);
+    drupal_static_reset($name2);
+    $this->assertEquals('initial1', $var1, 'Variable 1 was reset after invocation of name-specific reset.');
+    $this->assertEquals('initial2', $var2, 'Variable 2 was reset after invocation of name-specific reset.');
+    $var1 = 'modified1';
+    $var2 = 'modified2';
+    drupal_static_reset();
+    $this->assertEquals('initial1', $var1, 'Variable 1 was reset after invocation of global reset.');
+    $this->assertEquals('initial2', $var2, 'Variable 2 was reset after invocation of global reset.');
   }
 
 }
