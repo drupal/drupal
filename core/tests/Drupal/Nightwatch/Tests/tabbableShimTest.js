3d947d4c5304b08ff672baf1cd4818fd75c77ba7
nod_ <nod_@598310.no-reply.drupal.org>
2023-04-11 23:52:02 +0200

    Issue #3038336 by bnjmnm, andrewmacpherson: When jQuery UI constrains tabbing it does not consider summary elements

--- core/tests/Drupal/Nightwatch/Tests/tabbableShimTest.js
+++ core/tests/Drupal/Nightwatch/Tests/tabbableShimTest.js
@@ -58,13 +58,13 @@ const tabbableTestScenarios = [
   },
   {
     element:
-      '<details><summary>I am not :tabbable because IE does not support it</summary>This is unfortunate</details>',
-    tabbable: 0,
+      '<details><summary>Summary is now tabbable because IE is not supported anymore</summary>Hooray</details>',
+    tabbable: 1,
   },
   {
     element:
-      '<details>A details without a summary should also not be :tabbable</details>',
-    tabbable: 0,
+      '<details>A details without a summary should be :tabbable</details>',
+    tabbable: 1,
   },
   {
     element: '<ul><li>List item</li></ul>',
