67fc8fbf874b7b5f4a749a89f8626cf2fba20bc9
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-04-12 18:48:31 +1000

    Issue #3134349 by sleitner, yookoala, sharma.amitt16, Akram Khan, penyaskito, sahil.goyal, swatichouhan012, s_leu, larowlan, smustgrave, joachim: \Drupal\language\LanguageNegotiator does not handle PluginNotFoundException and break the site completely

---
+++ core/modules/language/tests/src/Kernel/LanguageNegotiatorPluginTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\Tests\language\Kernel;
+
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Logger\LoggerChannelFactory;
+use Drupal\KernelTests\KernelTestBase;
+use Symfony\Component\ErrorHandler\BufferingLogger;
+
+/**
+ * Tests PluginNotFoundException.
+ *
+ * @group language
+ */
+class LanguageNegotiatorPluginTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['language', 'user'];
+
+  /**
+   * Tests for PluginNotFoundException.
+   */
+  public function testLanguageNegotiatorNoPlugin() {
+    $logger = new BufferingLogger();
+    $logger_factory = $this->createMock(LoggerChannelFactory::class);
+    $logger_factory->expects($this->once())
+      ->method('get')
+      ->with('language')
+      ->willReturn($logger);
+    $this->container->set('logger.factory', $logger_factory);
+    $this->installEntitySchema('user');
+
+    // Test unavailable plugin.
+    $config = $this->config('language.types');
+    $config->set('configurable', [LanguageInterface::TYPE_URL]);
+    $config->set('negotiation.language_url.enabled', [
+      self::CLASS => -3,
+    ]);
+    $config->save();
+    $languageNegotiator = $this->container->get('language_negotiator');
+    $languageNegotiator->setCurrentUser($this->prophesize('Drupal\Core\Session\AccountInterface')->reveal());
+    try {
+      $languageNegotiator->initializeType(LanguageInterface::TYPE_URL);
+    }
+    catch (PluginNotFoundException $exception) {
+      $this->fail('Plugin not found exception unhandled.');
+    }
+    $log_message = $logger->cleanLogs()[0];
+    $this->assertEquals('error', $log_message[0]);
+    $this->assertStringContainsString('The "Drupal\Tests\language\Kernel\LanguageNegotiatorPluginTest" plugin does not exist.', $log_message[1]);
+  }
+
+}
