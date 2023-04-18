40a02eb794798624d30dfa9c38ed1430f0be7439
Lauri Eskola <lauri.eskola@acquia.com>
2023-04-18 19:18:13 +0300

    Issue #3167437 by Akram Khan, alextars: Autocomplete field throbber visibility doesn't correspond to the ajax request

--- core/themes/claro/js/autocomplete.js
+++ core/themes/claro/js/autocomplete.js
@@ -9,8 +9,6 @@
       once('claroAutoComplete', 'input.form-autocomplete', context).forEach(
         (value) => {
           const $input = $(value);
-          const timeout = 400;
-          let classRemoveTimeout;
           const classRemove = ($autoCompleteElem) => {
             $autoCompleteElem.removeClass('is-autocompleting');
             $autoCompleteElem
@@ -18,23 +16,18 @@
               .addClass('hidden');
           };
 
-          $input.on(
-            'input autocompletesearch autocompleteresponses',
-            (event) => {
-              if (event && event.type && event.type === 'autocompletesearch') {
-                $(event.target).addClass('is-autocompleting');
-                $(event.target)
-                  .siblings('[data-drupal-selector="autocomplete-message"]')
-                  .removeClass('hidden');
-              }
-              clearTimeout(classRemoveTimeout);
-              classRemoveTimeout = setTimeout(
-                classRemove,
-                timeout,
-                $(event.target),
-              );
+          $input.on('autocompletesearch', (event) => {
+            $(event.target).addClass('is-autocompleting');
+            $(event.target)
+              .siblings('[data-drupal-selector="autocomplete-message"]')
+              .removeClass('hidden');
+          });
+
+          $input.autocomplete({
+            response(event) {
+              classRemove($(event.target));
             },
-          );
+          });
         },
       );
     },
