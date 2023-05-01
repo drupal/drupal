95ab19416e233a858c73553ecf30a18b6cafd6d0
catch <catch@35733.no-reply.drupal.org>
2023-05-01 09:12:47 +0100

    Issue #3269177 by DieterHolvoet, quietone, smustgrave: Should some TranslatableInterface methods return ContentEntityInterface or TranslatableRevisionableInterface

--- core/lib/Drupal/Core/TypedData/TranslatableInterface.php
+++ core/lib/Drupal/Core/TypedData/TranslatableInterface.php
@@ -69,7 +69,7 @@ interface TranslatableInterface {
    *   LanguageInterface::LANGCODE_DEFAULT
    *   to get the data in default language.
    *
-   * @return \Drupal\Core\Entity\ContentEntityInterface
+   * @return static
    *   A typed data object for the translated data.
    *
    * @throws \InvalidArgumentException
@@ -80,7 +80,7 @@ interface TranslatableInterface {
   /**
    * Returns the translatable object in the language it was created.
    *
-   * @return \Drupal\Core\Entity\ContentEntityInterface
+   * @return static
    *   The translation object referring to the original language.
    */
   public function getUntranslated();
@@ -110,7 +110,7 @@ interface TranslatableInterface {
    *   (optional) An array of initial values to be assigned to the translatable
    *   fields. Defaults to none.
    *
-   * @return \Drupal\Core\Entity\ContentEntityInterface
+   * @return static
    *   A new entity translation object.
    *
    * @throws \InvalidArgumentException
