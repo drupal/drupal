b05f3fa77589fafe0da5a86e4f0be831246d72db
Dave Long <dave@longwaveconsulting.com>
2023-05-02 14:36:37 +0100

    Issue #2985882 by tim.plunkett, Mirakolous, ravi.shankar, ayush9598, Rajab Natshah, andypost, Anybody, kattekrab, devarch, Freddy Rodriguez, adubovskoy, corneliusd, mradcliffe, longwave, lukasss, Pancho, Graber, dpi, joshf, lordzik, dorianwinterfeld, jacov, kdmdrupal, jlancaster, jborgesr, joarferme, tonytheferg, chrisrockwell, agilman, kducharm, Sam152, scotwith1t, labboy0276, Spokje, jazzfiction, Shrutidkadam, ab2211, Dave Kopecek, segx, alexmoreno, themaurice, the_glitch, NWOM, kwfinken, berliner, design.er, ouissla, zterry95, jamsilver, fonant, Rajendar Reddy, Christopher Riley, BramDriesen, webdrips: Workaround for "Call to a member function getLabel() after enabling layout_builder"
    (cherry picked from commit 4b2afe955d4d163fff6276d3ea08cba7cb969806)

--- core/modules/layout_builder/src/Plugin/Derivative/FieldBlockDeriver.php
+++ core/modules/layout_builder/src/Plugin/Derivative/FieldBlockDeriver.php
@@ -9,6 +9,7 @@ use Drupal\Core\Entity\EntityTypeRepositoryInterface;
 use Drupal\Core\Field\FieldConfigInterface;
 use Drupal\Core\Field\FieldTypePluginManagerInterface;
 use Drupal\Core\Field\FormatterPluginManager;
+use Drupal\Core\Logger\LoggerChannelTrait;
 use Drupal\Core\Plugin\Context\ContextDefinition;
 use Drupal\Core\Plugin\Context\EntityContextDefinition;
 use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
@@ -24,6 +25,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class FieldBlockDeriver extends DeriverBase implements ContainerDeriverInterface {
 
   use StringTranslationTrait;
+  use LoggerChannelTrait;
 
   /**
    * The entity type repository.
@@ -99,7 +101,12 @@ class FieldBlockDeriver extends DeriverBase implements ContainerDeriverInterface
 
         foreach ($field_info['bundles'] as $bundle) {
           $derivative = $base_plugin_definition;
-          $field_definition = $this->entityFieldManager->getFieldDefinitions($entity_type_id, $bundle)[$field_name];
+          $field_definitions = $this->entityFieldManager->getFieldDefinitions($entity_type_id, $bundle);
+          if (empty($field_definitions[$field_name])) {
+            $this->getLogger('field')->error('Field %field_name exists but is missing a corresponding field definition and may be misconfigured.', ['%field_name' => "$entity_type_id.$bundle.$field_name"]);
+            continue;
+          }
+          $field_definition = $field_definitions[$field_name];
 
           // Store the default formatter on the definition.
           $derivative['default_formatter'] = '';
