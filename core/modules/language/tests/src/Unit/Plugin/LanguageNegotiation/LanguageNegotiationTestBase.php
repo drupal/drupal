d2eb0235ccea45707c168feab2dba3d9ec7f941f
catch <catch@35733.no-reply.drupal.org>
2023-05-10 15:23:22 +0100

    Issue #3130107 by stefanos.petrakis, rpayanm, smustgrave, jungle: Extend unit test coverage for LanguageNegotiationContentEntity

---
+++ core/modules/language/tests/src/Unit/Plugin/LanguageNegotiation/LanguageNegotiationTestBase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\Tests\language\Unit\Plugin\LanguageNegotiation;
+
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Base class used for testing the various LanguageNegotiation plugins.
+ *
+ * @group language
+ */
+abstract class LanguageNegotiationTestBase extends UnitTestCase {
+
+  /**
+   * Returns the plugin class to use for creating the language negotiation plugin.
+   *
+   * @return string
+   *   The plugin class name.
+   */
+  abstract protected function getPluginClass(): string;
+
+  /**
+   * Creates a @LanguageNegotiation plugin using the factory ::create method.
+   *
+   * @return \Drupal\language\LanguageNegotiationMethodInterface
+   */
+  protected function createLanguageNegotiationPlugin(array $configuration = [], $plugin_definition = NULL) {
+    $class = $this->getPluginClass();
+    $this->assertTrue(in_array(ContainerFactoryPluginInterface::class, class_implements($class)));
+    return $class::create(\Drupal::getContainer(), $configuration, $class::METHOD_ID, $plugin_definition);
+  }
+
+}
