eb08f0528da171367c688b4bec4b0e0989186282
Dave Long <dave@longwaveconsulting.com>
2023-05-22 22:34:10 +0100

    Issue #3361800 by Wim Leers, neclimdul, smustgrave: Update CKEditor 5 to 38.0.0
    (cherry picked from commit 47c4939fec5e516f12ed7700b3a729ba83ffdf64)

---
+++ core/assets/vendor/ckeditor5/horizontal-line/translations/uz.js
@@ -0,0 +1,1 @@
+!function(i){const n=i.uz=i.uz||{};n.dictionary=Object.assign(n.dictionary||{},{"Horizontal line":"Gorizontal chiziq"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
--- core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/drupalmediageneralhtmlsupport.js
+++ core/modules/ckeditor5/js/ckeditor5_plugins/drupalMedia/src/drupalmediageneralhtmlsupport.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// cSpell:words conversionutils datafilter eventinfo downcastdispatcher generalhtmlsupport
+// cSpell:words datafilter eventinfo downcastdispatcher generalhtmlsupport
 import { Plugin } from 'ckeditor5/src/core';
-import { setViewAttributes } from '@ckeditor/ckeditor5-html-support/src/conversionutils';
+import { setViewAttributes } from '@ckeditor/ckeditor5-html-support/src/utils';
 
 /**
  * View-to-model conversion helper for Drupal Media.
