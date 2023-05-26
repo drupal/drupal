c6e65847f6af200160914a67ce32c432331932d4
catch <catch@35733.no-reply.drupal.org>
2023-05-26 10:36:13 +0100

    Issue #3060626 by mohit_aghera, ranjith_kumar_k_u, Suresh Prabhu Parkala, aalin, Prem Suthar, cindytwilliams, larowlan, smustgrave, Kristen Pol: [needs backport] OptionsWidgetBase doesn't respect #required_error

--- core/modules/options/tests/src/Functional/OptionsWidgetsTest.php
+++ core/modules/options/tests/src/Functional/OptionsWidgetsTest.php
@@ -375,6 +375,44 @@ class OptionsWidgetsTest extends FieldTestBase {
   }
 
   /**
+   * Tests the '#required_error' attribute for the select list.
+   */
+  public function testSelectListRequiredErrorAttribute() {
+    // Enable form alter hook.
+    \Drupal::state()->set('options_test.form_alter_enable', TRUE);
+    // Create an instance of the 'single value' field.
+    $field = FieldConfig::create([
+      'field_storage' => $this->card1,
+      'bundle' => 'entity_test',
+      'required' => TRUE,
+    ]);
+    $field->save();
+    \Drupal::service('entity_display.repository')
+      ->getFormDisplay('entity_test', 'entity_test')
+      ->setComponent($this->card1->getName(), [
+        'type' => 'options_select',
+      ])
+      ->save();
+
+    // Create an entity.
+    $entity = EntityTest::create([
+      'user_id' => 1,
+      'name' => $this->randomMachineName(),
+    ]);
+    $entity->save();
+
+    $this->drupalGet('entity_test/manage/' . $entity->id() . '/edit');
+    // A required field without any value has a "none" option.
+    $option = $this->assertSession()->optionExists('edit-card-1', '_none');
+    $this->assertSame('- Select a value -', $option->getText());
+
+    // Submit form: select invalid 'none' option.
+    $edit = ['card_1' => '_none'];
+    $this->submitForm($edit, 'Save');
+    $this->assertSession()->responseContains(t('This is custom message for required field.'));
+  }
+
+  /**
    * Tests the 'options_select' widget (multiple select).
    */
   public function testSelectListMultiple() {
