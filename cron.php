b2805561103563d68355b5ffc0caf498c86652a1
mcdruid <drew@mcdruid.co.uk>
2023-06-06 19:32:35 +0100

    Issue #3007538 by poker10, torotil, DamienMcKenna, Jorrit, Fabianx: Cron.php does not check for maintenance mode correctly

--- cron.php
+++ cron.php
@@ -13,12 +13,12 @@ define('DRUPAL_ROOT', getcwd());
 include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-if (!isset($_GET['cron_key']) || variable_get('cron_key', 'drupal') != $_GET['cron_key']) {
-  watchdog('cron', 'Cron could not run because an invalid key was used.', array(), WATCHDOG_NOTICE);
-  drupal_access_denied();
-}
-elseif (variable_get('maintenance_mode', 0)) {
+if (variable_get('maintenance_mode', 0)) {
   watchdog('cron', 'Cron could not run because the site is in maintenance mode.', array(), WATCHDOG_NOTICE);
+  drupal_site_offline();
+}
+elseif (!isset($_GET['cron_key']) || variable_get('cron_key', 'drupal') != $_GET['cron_key']) {
+  watchdog('cron', 'Cron could not run because an invalid key was used.', array(), WATCHDOG_NOTICE);
   drupal_access_denied();
 }
 else {
