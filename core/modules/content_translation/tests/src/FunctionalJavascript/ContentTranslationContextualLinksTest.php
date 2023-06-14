519297c6d3627b7b11fef7718577bf2f651fe9be
catch <catch@35733.no-reply.drupal.org>
2023-06-14 15:32:22 +0100

    Issue #3355122 by joachim, dww, D34dMan: ContentTranslationContextualLinksTest should use API calls to set up translation
    (cherry picked from commit b1dd9f7e2fd3440793afa051fedb82f1c01a6c2c)

--- core/modules/content_translation/tests/src/FunctionalJavascript/ContentTranslationContextualLinksTest.php
+++ core/modules/content_translation/tests/src/FunctionalJavascript/ContentTranslationContextualLinksTest.php
@@ -42,14 +42,9 @@ class ContentTranslationContextualLinksTest extends WebDriverTestBase {
     $this->drupalCreateContentType(['type' => 'page']);
 
     // Enable content translation.
-    $this->drupalLogin($this->rootUser);
-    $this->drupalGet('admin/config/regional/content-language');
-    $edit = [
-      'entity_types[node]' => TRUE,
-      'settings[node][page][translatable]' => TRUE,
-    ];
-    $this->submitForm($edit, 'Save configuration');
-    $this->drupalLogout();
+    $content_translation_manager = $this->container->get('content_translation.manager');
+    $content_translation_manager->setEnabled('node', 'page', TRUE);
+    $this->rebuildContainer();
 
     // Create a translator user.
     $permissions = [
