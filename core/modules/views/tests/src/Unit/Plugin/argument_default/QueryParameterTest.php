0648571986ba455b9d266ef8115fbad5d84fca67
catch <catch@35733.no-reply.drupal.org>
2023-06-14 11:49:49 +0100

    Issue #3027705 by claudiu.cristea, Pancho, smustgrave: Allow ?edit[field_xyz] as query parameter in contextual filter
    (cherry picked from commit 471c0afd6e0997c9c228bee5620775c0f863c0d2)

--- core/modules/views/tests/src/Unit/Plugin/argument_default/QueryParameterTest.php
+++ core/modules/views/tests/src/Unit/Plugin/argument_default/QueryParameterTest.php
@@ -69,6 +69,24 @@ class QueryParameterTest extends UnitTestCase {
       'blub',
     ];
 
+    $data[] = [
+      ['query_param' => 'test[tier1][tier2][tier3]'],
+      new Request(['test' => ['tier1' => ['tier2' => ['tier3' => 'foo']]]]),
+      'foo',
+    ];
+
+    $data[] = [
+      ['query_param' => 'test[tier1][tier2]'],
+      new Request(['test' => ['tier1' => ['tier2' => ['foo', 'bar']]]]),
+      'foo,bar',
+    ];
+
+    $data[] = [
+      ['query_param' => 'test[tier1][tier2]'],
+      new Request(['test' => 'foo']),
+      NULL,
+    ];
+
     return $data;
   }
 
