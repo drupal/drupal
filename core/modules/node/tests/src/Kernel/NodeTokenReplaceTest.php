b23fad55a8c8aee4757984b9ac75f0a0b8cfe629
Lauri Eskola <lauri.eskola@acquia.com>
2023-07-04 11:56:33 +0300

    Issue #3073554 by mpp, ravi.shankar, smustgrave, mark_fullmer, alexpott, acbramley, lukus, Gábor Hojtsy: Add a token for publication status

--- core/modules/node/tests/src/Kernel/NodeTokenReplaceTest.php
+++ core/modules/node/tests/src/Kernel/NodeTokenReplaceTest.php
@@ -3,6 +3,7 @@
 namespace Drupal\Tests\node\Kernel;
 
 use Drupal\Component\Utility\Html;
+use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\node\Entity\Node;
 use Drupal\node\Entity\NodeType;
@@ -65,6 +66,7 @@ class NodeTokenReplaceTest extends TokenReplaceKernelTestBase {
     $tests['[node:body]'] = $node->body->processed;
     $tests['[node:summary]'] = $node->body->summary_processed;
     $tests['[node:langcode]'] = $node->language()->getId();
+    $tests['[node:published_status]'] = 'Published';
     $tests['[node:url]'] = $node->toUrl('canonical', $url_options)->toString();
     $tests['[node:edit-url]'] = $node->toUrl('edit-form', $url_options)->toString();
     $tests['[node:author]'] = $account->getAccountName();
@@ -86,6 +88,7 @@ class NodeTokenReplaceTest extends TokenReplaceKernelTestBase {
     $metadata_tests['[node:body]'] = $base_bubbleable_metadata;
     $metadata_tests['[node:summary]'] = $base_bubbleable_metadata;
     $metadata_tests['[node:langcode]'] = $base_bubbleable_metadata;
+    $metadata_tests['[node:published_status]'] = $base_bubbleable_metadata;
     $metadata_tests['[node:url]'] = $base_bubbleable_metadata;
     $metadata_tests['[node:edit-url]'] = $base_bubbleable_metadata;
     $bubbleable_metadata = clone $base_bubbleable_metadata;
@@ -104,6 +107,27 @@ class NodeTokenReplaceTest extends TokenReplaceKernelTestBase {
       $output = $this->tokenService->replace($input, ['node' => $node], ['langcode' => $this->interfaceLanguage->getId()], $bubbleable_metadata);
       $this->assertSame((string) $expected, (string) $output, "Failed test case: {$input}");
       $this->assertEquals($metadata_tests[$input], $bubbleable_metadata);
+    }
+
+    // Repeat for an unpublished node.
+    $node = Node::create([
+      'type' => 'article',
+      'uid' => $account->id(),
+      'title' => '<blink>Blinking Text</blink>',
+    ]);
+    $node->setUnpublished();
+    $node->save();
+
+    // Generate and test tokens.
+    $tests = [];
+    $tests['[node:published_status]'] = 'Unpublished';
+
+    // Test to make sure that we generated something for each token.
+    $this->assertFalse(in_array(0, array_map('strlen', $tests)), 'No empty tokens generated for unpublished node.');
+
+    foreach ($tests as $input => $expected) {
+      $output = $this->tokenService->replace($input, ['node' => $node], ['language' => $this->interfaceLanguage]);
+      $this->assertEquals($output, $expected, new FormattableMarkup('Node token %token replaced for unpublished node.', ['%token' => $input]));
     }
 
     // Repeat for a node without a summary.
