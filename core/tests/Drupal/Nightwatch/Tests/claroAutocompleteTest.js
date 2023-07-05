c1fdf4e7c437cdb3eaf979671fa0663fe56e12fa
bnjmnm <benm@umich.edu>
2023-07-05 14:48:02 -0400

    Issue #3371571 by lauriii, smustgrave: Autocomplete throbber is visible when entering a blacklist character

--- core/tests/Drupal/Nightwatch/Tests/claroAutocompleteTest.js
+++ core/tests/Drupal/Nightwatch/Tests/claroAutocompleteTest.js
@@ -36,6 +36,12 @@ module.exports = {
       .drupalLogin({ name: 'user', password: '123' })
       .drupalRelativeURL('/form-test/autocomplete')
       .waitForElementVisible('body', 1000);
+
+    browser
+      .setValue('[name="autocomplete_4"]', '/')
+      .pause(1000)
+      .waitForElementNotPresent('.is-autocompleting');
+
     // eslint-disable-next-line no-unused-expressions
     browser.expect.element(
       '.js-form-item-autocomplete-3 [data-drupal-selector="autocomplete-message"]',
