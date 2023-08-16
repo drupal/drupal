5cc339ea60470a32de8bb770d3a9dc8cd11e7966
Dave Long <dave@longwaveconsulting.com>
2023-08-16 16:11:02 +0100

    Issue #3380637 by lauriii, Wim Leers: Update CKEditor 5 to 39.0.1

---
+++ core/assets/vendor/ckeditor5/code-block/translations/sq.js
@@ -0,0 +1,1 @@
+!function(t){const i=t.sq=t.sq||{};i.dictionary=Object.assign(i.dictionary||{},{"Insert code block":"Shto bllokun e kodit","Plain text":"Teksti i thjeshtë"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
---
+++ core/assets/vendor/ckeditor5/html-support/translations/ug.js
@@ -0,0 +1,1 @@
+!function(i){const n=i.ug=i.ug||{};n.dictionary=Object.assign(n.dictionary||{},{"HTML object":"HTML جىسىم"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
