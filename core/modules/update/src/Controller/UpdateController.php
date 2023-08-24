f474406947bd2ac599ce10455c86559908fc357b
Lauri Eskola <lauri.eskola@acquia.com>
2023-08-24 09:27:30 +0300

    Issue #3375850 by omkar.podey, hooroomoo, smustgrave, narendraR, tedbow: Broken Breadcrumb on Appearance tab

--- core/modules/update/src/Controller/UpdateController.php
+++ core/modules/update/src/Controller/UpdateController.php
@@ -5,9 +5,13 @@ namespace Drupal\update\Controller;
 use Drupal\Core\Batch\BatchBuilder;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Routing\PathChangedHelper;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\update\UpdateFetcherInterface;
 use Drupal\update\UpdateManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Controller routines for update routes.
@@ -95,6 +99,36 @@ class UpdateController extends ControllerBase {
       ->setFinishCallback('update_fetch_data_finished');
     batch_set($batch_builder->toArray());
     return batch_process('admin/reports/updates');
+  }
+
+  /**
+   * Provides a redirect to update page.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   A route match object, used for the route name and the parameters.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request object.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   *   Returns redirect.
+   *
+   * @deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use
+   *   /admin/appearance/update directly instead of /admin/theme/update.
+   *
+   * @see https://www.drupal.org/node/3375850
+   */
+  public function updateRedirect(RouteMatchInterface $route_match, Request $request): RedirectResponse {
+    @trigger_error('The path /admin/theme/update is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use /admin/appearance/update. See https://www.drupal.org/node/3382805', E_USER_DEPRECATED);
+    $helper = new PathChangedHelper($route_match, $request);
+    $params = [
+      '%old_path' => $helper->oldPath(),
+      '%new_path' => $helper->newPath(),
+      '%change_record' => 'https://www.drupal.org/node/3382805',
+    ];
+    $warning_message = $this->t('You have been redirected from %old_path. Update links, shortcuts, and bookmarks to use %new_path.', $params);
+    $this->messenger()->addWarning($warning_message);
+    $this->getLogger('update')->warning('A user was redirected from %old_path to %new_path. This redirect will be removed in a future version of Drupal. Update links, shortcuts, and bookmarks to use %new_path. See %change_record for more information.', $params);
+    return $helper->redirect();
   }
 
 }
