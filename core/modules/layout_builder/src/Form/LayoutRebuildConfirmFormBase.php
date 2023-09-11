928467a904ca9fb3de074dd0d9b8762a7daf77fd
Lee Rowlands <lee.rowlands@previousnext.com.au>
2023-09-11 16:00:45 +1000

    Issue #3152561 by lexbritvin, tim.plunkett, EricRondo, TwiiK: Layout Builder jumps to top when removing section/block
    (cherry picked from commit 57366cba03b6673c2bb52130c96a414b79291d5e)

--- core/modules/layout_builder/src/Form/LayoutRebuildConfirmFormBase.php
+++ core/modules/layout_builder/src/Form/LayoutRebuildConfirmFormBase.php
@@ -84,6 +84,10 @@ abstract class LayoutRebuildConfirmFormBase extends ConfirmFormBase {
       $form['actions']['cancel']['#attributes']['class'][] = 'dialog-cancel';
       $target_highlight_id = !empty($this->uuid) ? $this->blockUpdateHighlightId($this->uuid) : $this->sectionUpdateHighlightId($delta);
       $form['#attributes']['data-layout-builder-target-highlight-id'] = $target_highlight_id;
+      // The AJAX system automatically moves focus to the first tabbable
+      // element after closing a dialog, sometimes scrolling to a page top.
+      // Disable refocus on the button.
+      $form['actions']['submit']['#ajax']['disable-refocus'] = TRUE;
     }
 
     // Mark this as an administrative page for JavaScript ("Back to site" link).
