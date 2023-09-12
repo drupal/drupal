8e42cef117d82c0cd67034df77a30e817f62d91f
Dave Long <dave@longwaveconsulting.com>
2023-09-12 22:42:44 +0100

    Issue #3050578 by elber, quietone, nitin_lama, fengtan, smustgrave, catch, karolinam: Remove unused function getSchemaUpdates()

--- core/lib/Drupal/Core/Updater/Module.php
+++ core/lib/Drupal/Core/Updater/Module.php
@@ -80,8 +80,14 @@ class Module extends Updater implements UpdaterInterface {
    * Returns available database schema updates once a new version is installed.
    *
    * @return array
+   *
+   * @deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use
+   * \Drupal\Core\Update\UpdateHookRegistry::getAvailableUpdates() instead.
+   *
+   * @see https://www.drupal.org/node/3359445
    */
   public function getSchemaUpdates() {
+    @trigger_error(__METHOD__ . "() is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use \Drupal\Core\Update\UpdateHookRegistry::getAvailableUpdates() instead. https://www.drupal.org/node/3359445", E_USER_DEPRECATED);
     require_once DRUPAL_ROOT . '/core/includes/install.inc';
     require_once DRUPAL_ROOT . '/core/includes/update.inc';
 
