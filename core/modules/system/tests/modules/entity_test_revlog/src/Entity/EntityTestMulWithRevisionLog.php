664eb630b1922b05cbc5e97c25b8348649fe08c9
Lauri Eskola <lauri.eskola@acquia.com>
2023-09-28 09:28:34 +0300

    Issue #3356684 by sumit_saini, acbramley, smustgrave: Generic Revision UI's Revision overview page generates wrong operations/view links for a translation
    (cherry picked from commit b0ac814d0a4b16fec5bfc423fcc940cffaded33c)

--- core/modules/system/tests/modules/entity_test_revlog/src/Entity/EntityTestMulWithRevisionLog.php
+++ core/modules/system/tests/modules/entity_test_revlog/src/Entity/EntityTestMulWithRevisionLog.php
@@ -8,6 +8,18 @@ namespace Drupal\entity_test_revlog\Entity;
  * @ContentEntityType(
  *   id = "entity_test_mul_revlog",
  *   label = @Translation("Test entity - data table, revisions log"),
+ *   handlers = {
+ *     "access" = \Drupal\entity_test_revlog\EntityTestRevlogAccessControlHandler::class,
+ *     "form" = {
+ *       "default" = \Drupal\Core\Entity\ContentEntityForm::class,
+ *       "revision-delete" = \Drupal\Core\Entity\Form\RevisionDeleteForm::class,
+ *       "revision-revert" = \Drupal\Core\Entity\Form\RevisionRevertForm::class,
+ *     },
+ *     "route_provider" = {
+ *       "html" = \Drupal\Core\Entity\Routing\DefaultHtmlRouteProvider::class,
+ *       "revision" = \Drupal\Core\Entity\Routing\RevisionHtmlRouteProvider::class,
+ *     },
+ *   },
  *   base_table = "entity_test_mul_revlog",
  *   data_table = "entity_test_mul_revlog_field_data",
  *   revision_table = "entity_test_mul_revlog_revision",
@@ -26,6 +38,16 @@ namespace Drupal\entity_test_revlog\Entity;
  *     "revision_created" = "revision_created",
  *     "revision_log_message" = "revision_log_message"
  *   },
+ *   links = {
+ *     "add-form" = "/entity_test_mul_revlog/add",
+ *     "canonical" = "/entity_test_mul_revlog/manage/{entity_test_mul_revlog}",
+ *     "delete-form" = "/entity_test/delete/entity_test_mul_revlog/{entity_test_mul_revlog}",
+ *     "edit-form" = "/entity_test_mul_revlog/manage/{entity_test_mul_revlog}/edit",
+ *     "revision" = "/entity_test_mul_revlog/{entity_test_mul_revlog}/revision/{entity_test_mul_revlog_revision}/view",
+ *     "revision-delete-form" = "/entity_test_mul_revlog/{entity_test_mul_revlog}/revision/{entity_test_mul_revlog_revision}/delete",
+ *     "revision-revert-form" = "/entity_test_mul_revlog/{entity_test_mul_revlog}/revision/{entity_test_mul_revlog_revision}/revert",
+ *     "version-history" = "/entity_test_mul_revlog/{entity_test_mul_revlog}/revisions",
+ *   }
  * )
  */
 class EntityTestMulWithRevisionLog extends EntityTestWithRevisionLog {
