8b0a91f6027b7d74cd78541bcb0b7e7b8cd3c1ec
Lauri Eskola <lauri.eskola@acquia.com>
2023-10-11 08:46:33 +0300

    Issue #3351600 by andrew.farquharson, Ranjit1032002, Anybody, sanduhrs, gmateos, smustgrave, Wim Leers, ant1, catch: ckeditor5.dialog.fix.js throws "Uncaught TypeError: event.target.classList is undefined" in Firefox in Drupal 10 with the editor in a modal

--- core/modules/ckeditor5/js/ckeditor5.dialog.fix.js
+++ core/modules/ckeditor5/js/ckeditor5.dialog.fix.js
@@ -12,6 +12,12 @@
     // CKEditor 5 in modals can work as expected.
     // @see https://api.jqueryui.com/dialog/#method-_allowInteraction
     _allowInteraction(event) {
+      // Fixes "Uncaught TypeError: event.target.classList is undefined"
+      // in Firefox (only).
+      // @see https://www.drupal.org/project/drupal/issues/3351600
+      if (event.target.classList === undefined) {
+        return this._super(event);
+      }
       return event.target.classList.contains('ck') || this._super(event);
     },
   });
