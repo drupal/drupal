5e8d5e8a4849b48cb18a6ea48890ceee323a3e42
Dave Long <dave@longwaveconsulting.com>
2023-10-13 14:42:49 +0100

    Issue #3387988 by quietone, atul4drupal: Fix @return type, simple fixes

--- core/modules/jsonapi/src/Normalizer/Value/TemporaryArrayObjectThrowingExceptions.php
+++ core/modules/jsonapi/src/Normalizer/Value/TemporaryArrayObjectThrowingExceptions.php
@@ -30,9 +30,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
    * @param int $flags
    *   The flags to sort the ArrayObject by.
    *
-   * @return bool
-   *   This method always returns TRUE.
-   *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
    *   it is extending \ArrayObject.
@@ -44,9 +41,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
 
   /**
    * Count the ArrayObject.
-   *
-   * @return int
-   *   The number of public properties in the \ArrayObject.
    *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
@@ -62,9 +56,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
    * @param array|object $array
    *   The array to replace for the current array.
    *
-   * @return array
-   *   The new array or object to exchange with the current array.
-   *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
    *   it is extending \ArrayObject.
@@ -75,9 +66,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
 
   /**
    * Exports the \ArrayObject to an array.
-   *
-   * @return array
-   *   Returns a copy of the array.
    *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
@@ -90,9 +78,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
   /**
    * Gets the behavior flags of the \ArrayObject.
    *
-   * @return int
-   *   Returns the behavior flags of the \ArrayObject.
-   *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
    *   it is extending \ArrayObject.
@@ -104,9 +89,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
   /**
    * Create a new iterator from an ArrayObject instance.
    *
-   * @return \Iterator
-   *   An iterator from the ArrayObject.
-   *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
    *   it is extending \ArrayObject.
@@ -117,9 +99,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
 
   /**
    * Gets the class name of the array iterator that is used by \ArrayObject::getIterator().
-   *
-   * @return string
-   *   Returns the iterator class name that is used to iterate over this object.
    *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
@@ -135,9 +114,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
    * @param int $flags
    *   The flags to sort the ArrayObject by.
    *
-   * @return bool
-   *   This method always returns TRUE.
-   *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
    *   it is extending \ArrayObject.
@@ -150,9 +126,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
   /**
    * Sort an array using a case insensitive "natural order" algorithm.
    *
-   * @return bool
-   *   This method always returns TRUE.
-   *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
    *   it is extending \ArrayObject.
@@ -164,9 +137,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
 
   /**
    * Sort entries using a "natural order" algorithm.
-   *
-   * @return bool
-   *   This method always returns TRUE.
    *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
@@ -182,9 +152,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
    *
    * @param mixed $key
    *   The index being checked.
-   *
-   * @return bool
-   *   Return TRUE if the requested index exists, otherwise FALSE.
    *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
@@ -279,9 +246,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
    *   greater than zero if the first argument is considered to be respectively
    *   less than, equal to, or greater than the second.
    *
-   * @return bool
-   *   This method always returns TRUE.
-   *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
    *   it is extending \ArrayObject.
@@ -298,9 +262,6 @@ class TemporaryArrayObjectThrowingExceptions extends \ArrayObject {
    *   The comparison function must return an integer less than, equal to, or
    *   greater than zero if the first argument is considered to be respectively
    *   less than, equal to, or greater than the second.
-   *
-   * @return bool
-   *   This method always returns TRUE.
    *
    * @throws \Exception
    *   This class does not support this action but it must implement it, because
--- core/modules/locale/src/PluralFormula.php
+++ core/modules/locale/src/PluralFormula.php
@@ -93,8 +93,6 @@ class PluralFormula implements PluralFormulaInterface {
 
   /**
    * Loads the formulae and stores them on the PluralFormula object if not set.
-   *
-   * @return array
    */
   protected function loadFormulae() {
     if (!isset($this->formulae)) {
