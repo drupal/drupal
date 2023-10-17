5c8213b5b3247b9219f447ada9b922dbabfbea41
nod_ <nod_@598310.no-reply.drupal.org>
2023-10-17 16:57:09 +0200

    Issue #3182458 by alison, dmundra, lauriii, xjm, Gauravvvv, smustgrave, quietone, mgifford, bnjmnm, alexpott: Accessibility: skipped heading level on for search "no results" text

--- core/modules/search/src/Controller/SearchController.php
+++ core/modules/search/src/Controller/SearchController.php
@@ -118,7 +118,9 @@ class SearchController extends ControllerBase {
       '#theme' => ['item_list__search_results__' . $plugin->getPluginId(), 'item_list__search_results'],
       '#items' => $results,
       '#empty' => [
-        '#markup' => '<h3>' . $this->t('Your search yielded no results.') . '</h3>',
+        '#type' => 'html_tag',
+        '#tag' => 'em',
+        '#value' => $this->t('Your search yielded no results.'),
       ],
       '#list_type' => 'ol',
       '#context' => [
