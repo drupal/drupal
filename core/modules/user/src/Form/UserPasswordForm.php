4e03083d85ff747bd2281ee79c1066560bd2272b
nod_ <nod_@598310.no-reply.drupal.org>
2023-10-20 15:54:39 +0200

    Issue #3181647 by ultimike, guiu.rocafort.ferrer, tobiasb, ilgnerfagundes, prudloff, jurgenhaas, guzmanb, maxime.rffd, hexaki, samuel_orhan, rauch, guiu.rocafort.ferrer, mullzk, binnythomas: Add autocomplete attributes on login form and password reset form
    (cherry picked from commit 981921d8234e20a5a4115ad6030a13294f9bf540)

--- core/modules/user/src/Form/UserPasswordForm.php
+++ core/modules/user/src/Form/UserPasswordForm.php
@@ -120,6 +120,7 @@ class UserPasswordForm extends FormBase {
         'autocapitalize' => 'off',
         'spellcheck' => 'false',
         'autofocus' => 'autofocus',
+        'autocomplete' => 'username',
       ],
     ];
     // Allow logged in users to request this also.
