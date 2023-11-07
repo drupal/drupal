c31491b7c3ce37dcc6c80888a56e4fe509d3eeda
Dave Long <dave@longwaveconsulting.com>
2023-11-07 21:10:53 +0000

    Issue #3397488 by nbeaucage, longwave, smustgrave, catch: Deprecate unused CacheDecoratorInterface
    (cherry picked from commit 4cff7cd61dda762dc91e988b67a527734e2ff7b2)

--- core/lib/Drupal/Core/CacheDecorator/CacheDecoratorInterface.php
+++ core/lib/Drupal/Core/CacheDecorator/CacheDecoratorInterface.php
@@ -2,8 +2,15 @@
 
 namespace Drupal\Core\CacheDecorator;
 
+@trigger_error('The ' . __NAMESPACE__ . '\CacheDecoratorInterface is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. There is no replacement. See https://www.drupal.org/node/3398182', E_USER_DEPRECATED);
+
 /**
  * Defines an interface for cache decorator implementations.
+ *
+ * @deprecated in drupal:10.2.0 and is removed from drupal:11.0.0.
+ *  There is no replacement.
+ *
+ * @see https://www.drupal.org/node/3398182
  */
 interface CacheDecoratorInterface {
 
