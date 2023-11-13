3191145832cb618f8548a73a38e4a8b464ef1408
xjm <xjm@65776.no-reply.drupal.org>
2023-11-13 14:29:14 -0600

    Issue #3015369 by quietone, mikelutz, xjm: Fix MigrateTestBase::executeMigrations() to execute migrations in dependency order

--- core/modules/migrate/tests/src/Kernel/MigrateTestBase.php
+++ core/modules/migrate/tests/src/Kernel/MigrateTestBase.php
@@ -186,12 +186,8 @@ abstract class MigrateTestBase extends KernelTestBase implements MigrateMessageI
    */
   protected function executeMigrations(array $ids) {
     $manager = $this->container->get('plugin.manager.migration');
-    array_walk($ids, function ($id) use ($manager) {
-      // This is possibly a base plugin ID and we want to run all derivatives.
-      $instances = $manager->createInstances($id);
-      $this->assertNotEmpty($instances, sprintf("No migrations created for id '%s'.", $id));
-      array_walk($instances, [$this, 'executeMigration']);
-    });
+    $instances = $manager->createInstances($ids);
+    array_walk($instances, [$this, 'executeMigration']);
   }
 
   /**
