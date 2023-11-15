b6052c19ec6b795157550b945f3ef19c5055d7f6
xjm <xjm@65776.no-reply.drupal.org>
2023-11-15 14:58:42 -0600

    Issue #3399992 by mstrelan, smustgrave, xjm: Fix strict type errors in test traits
    (cherry picked from commit c304b09272b6e760db8cba83e718df454e20df7a)

--- core/tests/Drupal/Tests/UiHelperTrait.php
+++ core/tests/Drupal/Tests/UiHelperTrait.php
@@ -3,7 +3,6 @@
 namespace Drupal\Tests;
 
 use Behat\Mink\Driver\BrowserKitDriver;
-use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Component\Utility\Html;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Session\AccountInterface;
@@ -161,7 +160,7 @@ trait UiHelperTrait {
 
     // @see ::drupalUserIsLoggedIn()
     $account->sessionId = $this->getSession()->getCookie(\Drupal::service('session_configuration')->getOptions(\Drupal::request())['name']);
-    $this->assertTrue($this->drupalUserIsLoggedIn($account), new FormattableMarkup('User %name successfully logged in.', ['%name' => $account->getAccountName()]));
+    $this->assertTrue($this->drupalUserIsLoggedIn($account), "User {$account->getAccountName()} successfully logged in.");
 
     $this->loggedInUser = $account;
     $this->container->get('current_user')->setAccount($account);
