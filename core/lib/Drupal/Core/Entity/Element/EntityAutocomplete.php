b78c8746628bf070f2717345c40be3de0a9cabc5
xjm <xjm@65776.no-reply.drupal.org>
2023-11-16 09:09:42 -0600

    Revert "Issue #3383131 by WalkingDexter, xjm, allisonherodevs, ashley_herodev, pradhumanjain2311, smustgrave, marcoliver, lauriii: Entity autocomplete form element ignores entities with label "0""
    This reverts commit 18732679ed6bd133e0758c7af1db85dac349e957.

--- core/lib/Drupal/Core/Entity/Element/EntityAutocomplete.php
+++ core/lib/Drupal/Core/Entity/Element/EntityAutocomplete.php
@@ -203,8 +203,7 @@ class EntityAutocomplete extends Textfield {
   public static function validateEntityAutocomplete(array &$element, FormStateInterface $form_state, array &$complete_form) {
     $value = NULL;
 
-    // Check the value for emptiness, but allow the use of (string) "0".
-    if ((!empty($element['#value']) || (is_string($element['#value']) && strlen($element['#value'])))) {
+    if (!empty($element['#value'])) {
       $options = $element['#selection_settings'] + [
         'target_type' => $element['#target_type'],
         'handler' => $element['#selection_handler'],
