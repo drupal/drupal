7ddc1b8e0e312c46b62b8fecf49cf5795323052d
xjm <xjm@65776.no-reply.drupal.org>
2023-11-16 09:11:01 -0600

    Revert "Issue #3383131 by WalkingDexter, xjm, allisonherodevs, ashley_herodev, pradhumanjain2311, smustgrave, marcoliver, lauriii: Entity autocomplete form element ignores entities with label "0""
    This reverts commit 68dfec3ca8777d4e976c7f7a8d76e66cef0d73e6.

--- core/modules/system/src/Controller/EntityAutocompleteController.php
+++ core/modules/system/src/Controller/EntityAutocompleteController.php
@@ -77,12 +77,8 @@ class EntityAutocompleteController extends ControllerBase {
    */
   public function handleAutocomplete(Request $request, $target_type, $selection_handler, $selection_settings_key) {
     $matches = [];
-
     // Get the typed string from the URL, if it exists.
-    $input = $request->query->get('q');
-
-    // Check this string for emptiness, but allow any non-empty string.
-    if (is_string($input) && strlen($input)) {
+    if ($input = $request->query->get('q')) {
       $tag_list = Tags::explode($input);
       $typed_string = !empty($tag_list) ? mb_strtolower(array_pop($tag_list)) : '';
 
