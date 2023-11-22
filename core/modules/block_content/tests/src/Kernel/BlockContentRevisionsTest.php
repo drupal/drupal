4203cff7ac02f3f5b916286fc885a383f1718e3a
Dave Long <dave@longwaveconsulting.com>
2023-11-22 22:28:23 +0000

    Issue #3403265 by acbramley: Remove RevisionLogEntityTrait overrides in BlockContent

---
+++ core/modules/block_content/tests/src/Kernel/BlockContentRevisionsTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\Tests\block_content\Kernel;
+
+use Drupal\block_content\Entity\BlockContent;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\block_content\Entity\BlockContentType;
+
+/**
+ * Tests revision based functions for Block Content.
+ *
+ * @group block_content
+ */
+class BlockContentRevisionsTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'block',
+    'block_content',
+    'system',
+    'user',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    parent::setUp();
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('block_content');
+  }
+
+  /**
+   * Tests block content revision user id doesn't throw error with null field.
+   */
+  public function testNullRevisionUser(): void {
+    BlockContentType::create([
+      'id' => 'basic',
+      'label' => 'A basic block type',
+    ])->save();
+
+    $block = BlockContent::create([
+      'info' => 'Test',
+      'type' => 'basic',
+      'revision_user' => NULL,
+    ]);
+    $block->save();
+    $this->assertNull($block->getRevisionUserId());
+  }
+
+}
