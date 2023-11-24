6959691f4219c2003cb4dd9d6fc7219b9f3f44e6
Lauri Eskola <lauri.eskola@acquia.com>
2023-11-24 18:04:33 +0200

    Issue #3028191 by srishtiiee, tedbow, smustgrave, narendraR, yash.rode: When using Layout Builder, remove contextual links for blocks outside of the current layout

--- core/modules/layout_builder/tests/src/FunctionalJavascript/ContextualLinksTest.php
+++ core/modules/layout_builder/tests/src/FunctionalJavascript/ContextualLinksTest.php
@@ -46,6 +46,7 @@ class ContextualLinksTest extends WebDriverTestBase {
       'administer nodes',
       'bypass node access',
       'administer views',
+      'administer blocks',
     ]);
     $user->save();
     $this->drupalLogin($user);
@@ -69,7 +70,6 @@ class ContextualLinksTest extends WebDriverTestBase {
    * Tests that the contextual links inside Layout Builder are removed.
    */
   public function testContextualLinks() {
-    // Skipped due to frequent random test failures.
     $page = $this->getSession()->getPage();
 
     $this->drupalGet('node/1/layout');
@@ -92,6 +92,33 @@ class ContextualLinksTest extends WebDriverTestBase {
 
     $this->drupalGet('node/1');
     $this->assertCorrectContextualLinksInNode();
+  }
+
+  /**
+   * Tests that contextual links outside the layout are removed.
+   */
+  public function testContextualLinksOutsideLayout() {
+    $assert_session = $this->assertSession();
+    $this->drupalPlaceBlock('system_powered_by_block', ['id' => 'global_block']);
+
+    $this->drupalGet('node/1');
+    // Ensure global blocks contextual link is present when not on
+    // Layout Builder.
+    $assert_session->elementsCount('css', '[data-contextual-id*=\'block:block=global_block:\']', 1);
+
+    $this->drupalGet('node/1/layout');
+    $this->addBlock('Test Block View: Teaser block');
+    // Ensure that only the layout specific contextual links are present.
+    $this->assertCorrectContextualLinks();
+
+    $page = $this->getSession()->getPage();
+    $page->hasButton('Save layout');
+    $page->pressButton('Save layout');
+    $this->drupalGet('node/1/layout');
+
+    // Ensure the contextual links are correct when the Layout Builder is loaded
+    // after being saved.
+    $this->assertCorrectContextualLinks();
   }
 
   /**
@@ -151,6 +178,22 @@ class ContextualLinksTest extends WebDriverTestBase {
     // Ensure that the contextual links that are hidden in Layout Builder UI
     // are visible on node view.
     $this->assertNotEmpty($page->findAll('css', '.layout-content [data-contextual-id]'));
+  }
+
+  /**
+   * Assert the contextual links are correct.
+   *
+   * @internal
+   */
+  protected function assertCorrectContextualLinks() {
+    $assert_session = $this->assertSession();
+    $page = $this->getSession()->getPage();
+    $this->assertNotEmpty($assert_session->waitForElementVisible('css', '.block-views-blocktest-block-view-block-2'));
+    $assert_session->assertNoElementAfterWait('css', '[data-contextual-id*=\'node:\']');
+    // Ensure that the Layout Builder's own contextual links are not removed.
+    $this->assertCount(3, $page->findAll('css', '[data-contextual-id*=\'layout_builder_block:\']'));
+    // Ensure that the global block's contextual links are removed.
+    $assert_session->elementNotExists('css', '[data-contextual-id*=\'block:block=global_block:\']');
   }
 
 }
