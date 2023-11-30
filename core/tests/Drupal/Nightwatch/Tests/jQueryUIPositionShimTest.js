0ce88f3f849bd10381a61a2a5f3e0063f7a635a4
Lauri Eskola <lauri.eskola@acquia.com>
2023-11-30 11:35:00 +0200

    Issue #3404847 by Spokje: Update JavaScript dependencies for 10.2.0-RC

--- core/tests/Drupal/Nightwatch/Tests/jQueryUIPositionShimTest.js
+++ core/tests/Drupal/Nightwatch/Tests/jQueryUIPositionShimTest.js
@@ -909,9 +909,8 @@ module.exports = {
                   y -= refRect.y;
                 }
                 if (!withinRange(x, options.x) || !withinRange(y, options.y)) {
-                  toReturn[
-                    idKey
-                  ] = `${idKey} EXPECTED x:${options.x} y:${options.y} ACTUAL x:${x} y:${y}`;
+                  toReturn[idKey] =
+                    `${idKey} EXPECTED x:${options.x} y:${options.y} ACTUAL x:${x} y:${y}`;
                 } else {
                   toReturn[idKey] = true;
                 }
