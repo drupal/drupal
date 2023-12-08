fca86410e6b8741fc16a08d8eeb62f4bb83d4570
Lauri Eskola <lauri.eskola@acquia.com>
2023-12-08 11:30:24 +0200

    Issue #3325167 by smustgrave, acbramley, murilohp, rkoller, larowlan, AaronMcHale, dww, BramDriesen: Revisit the redirect to 'add block' form in the 'add block content' form

--- core/modules/block_content/src/BlockContentForm.php
+++ core/modules/block_content/src/BlockContentForm.php
@@ -41,6 +41,51 @@ class BlockContentForm extends ContentEntityForm {
   /**
    * {@inheritdoc}
    */
+  public function actions(array $form, FormStateInterface $form_state): array {
+    $element = parent::actions($form, $form_state);
+
+    if ($this->getRequest()->query->has('theme')) {
+      $element['submit']['#value'] = $this->t('Save and configure');
+    }
+
+    if ($this->currentUser()->hasPermission('administer blocks') && !$this->getRequest()->query->has('theme') && $this->entity->isNew()) {
+      $element['configure_block'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Save and configure'),
+        '#weight' => 20,
+        '#submit' => array_merge($element['submit']['#submit'], ['::configureBlock']),
+      ];
+    }
+
+    return $element;
+  }
+
+  /**
+   * Form submission handler for the 'configureBlock' action.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function configureBlock(array $form, FormStateInterface $form_state): void {
+    $block = $this->entity;
+    if (!$theme = $block->getTheme()) {
+      $theme = $this->config('system.theme')->get('default');
+    }
+    $form_state->setRedirect(
+      'block.admin_add',
+      [
+        'plugin_id' => 'block_content:' . $block->uuid(),
+        'theme' => $theme,
+      ]
+    );
+    $form_state->setIgnoreDestination();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function save(array $form, FormStateInterface $form_state) {
     $block = $this->entity;
 
@@ -64,19 +109,19 @@ class BlockContentForm extends ContentEntityForm {
       $form_state->setValue('id', $block->id());
       $form_state->set('id', $block->id());
       if ($insert) {
-        if (!$theme = $block->getTheme()) {
-          $theme = $this->config('system.theme')->get('default');
+        $theme = $block->getTheme();
+        if ($theme) {
+          $form_state->setRedirect(
+            'block.admin_add',
+            [
+              'plugin_id' => 'block_content:' . $block->uuid(),
+              'theme' => $theme,
+            ]
+          );
         }
-        $form_state->setRedirect(
-          'block.admin_add',
-          [
-            'plugin_id' => 'block_content:' . $block->uuid(),
-            'theme' => $theme,
-          ]
-        );
-      }
-      else {
-        $form_state->setRedirectUrl($block->toUrl('collection'));
+        else {
+          $form_state->setRedirectUrl($block->toUrl('collection'));
+        }
       }
     }
     else {
