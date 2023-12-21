4dee9b3a579860160bd1238a5dcc57b01ab0c23a
catch <catch@35733.no-reply.drupal.org>
2023-12-21 10:26:09 +0000

    Issue #3395626 by amateescu: Fix workspace-support check in entity queries
    (cherry picked from commit 2e06d213c02e51204ad73cade9ab8dd178376f40)

--- core/modules/workspaces/src/EntityQuery/QueryTrait.php
+++ core/modules/workspaces/src/EntityQuery/QueryTrait.php
@@ -54,7 +54,7 @@ trait QueryTrait {
 
     // Only alter the query if the active workspace is not the default one and
     // the entity type is supported.
-    if ($this->workspaceManager->hasActiveWorkspace() && $this->workspaceManager->isEntityTypeSupported($this->entityType)) {
+    if ($this->workspaceManager->isEntityTypeSupported($this->entityType) && $this->workspaceManager->hasActiveWorkspace()) {
       $active_workspace = $this->workspaceManager->getActiveWorkspace();
       $this->sqlQuery->addMetaData('active_workspace_id', $active_workspace->id());
       $this->sqlQuery->addMetaData('simple_query', FALSE);
