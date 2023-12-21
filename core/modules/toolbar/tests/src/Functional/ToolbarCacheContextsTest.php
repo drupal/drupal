e578a5526bd95b787c9d9091a0e271d3363d4cda
catch <catch@35733.no-reply.drupal.org>
2023-12-21 08:02:03 +0000

    Issue #3405662 by smustgrave, quietone, larowlan: Remove use of Tour module in ToolbarCacheContextsTest.php

--- core/modules/toolbar/tests/src/Functional/ToolbarCacheContextsTest.php
+++ core/modules/toolbar/tests/src/Functional/ToolbarCacheContextsTest.php
@@ -95,11 +95,11 @@ class ToolbarCacheContextsTest extends BrowserTestBase {
     $this->assertToolbarCacheContexts(['user.permissions'], 'Expected cache contexts found with contextual module enabled.');
     \Drupal::service('module_installer')->uninstall(['contextual']);
 
-    // Test with the tour module enabled.
-    $this->installExtraModules(['tour']);
-    $this->adminUser2 = $this->drupalCreateUser(array_merge($this->perms, ['access tour']));
-    $this->assertToolbarCacheContexts(['user.permissions'], 'Expected cache contexts found with tour module enabled.');
-    \Drupal::service('module_installer')->uninstall(['tour']);
+    // Test with the comment module enabled.
+    $this->installExtraModules(['comment']);
+    $this->adminUser2 = $this->drupalCreateUser(array_merge($this->perms, ['access comments']));
+    $this->assertToolbarCacheContexts(['user.permissions'], 'Expected cache contexts found with comment module enabled.');
+    \Drupal::service('module_installer')->uninstall(['comment']);
   }
 
   /**
