bb42fa92acefe90174c43b101379724e68c1cd05
Lee Rowlands <lee.rowlands@previousnext.com.au>
2024-01-02 06:51:05 +1000

    Issue #3410126 by kim.pepper, catch, larowlan, DamienMcKenna, alexpott: File validation logic from #3221793 broke backwards compatibility
    (cherry picked from commit bd37e393c3292bfb4e2203bf74eba16050915322)

--- core/modules/file/tests/src/Kernel/Validation/FileValidatorTest.php
+++ core/modules/file/tests/src/Kernel/Validation/FileValidatorTest.php
@@ -31,9 +31,11 @@ class FileValidatorTest extends FileValidatorTestBase {
     // Use a mix of legacy functions and plugin IDs to test both work.
     // Each Constraint has its own tests under
     // core/modules/file/tests/src/Kernel/Plugin/Validation/Constraint.
+    // Also check that arbitrary strings can be used.
     $validators = [
       'file_validate_name_length' => [],
       'FileNameLength' => [],
+      'foo' => [],
     ];
     file_test_reset();
 
@@ -42,6 +44,7 @@ class FileValidatorTest extends FileValidatorTestBase {
     $this->assertCount(0, $violations);
     $this->assertCount(1, file_test_get_calls('validate'));
 
+    $this->expectDeprecation('Passing invalid constraint plugin ID "foo" in the list of $validators to Drupal\file\Validation\FileValidator::validate() is deprecated in drupal:10.2.0 and will throw an exception in drupal:11.0.0. See https://www.drupal.org/node/3363700');
     file_test_reset();
     $this->file->set('filename', '');
     $violations = $this->validator->validate($this->file, $validators);
