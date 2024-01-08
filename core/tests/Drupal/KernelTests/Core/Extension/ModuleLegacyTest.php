e4d7436a763726a9b064f2f4d3de86a9695eaeb6
quietone <quietone@2572884.no-reply.drupal.org>
2024-01-08 13:25:09 +1300

    Issue #3405412 by sijumpk, quietone, dww, bbrala: Fix deprecation message to use link to CR not the issue

--- core/tests/Drupal/KernelTests/Core/Extension/ModuleLegacyTest.php
+++ core/tests/Drupal/KernelTests/Core/Extension/ModuleLegacyTest.php
@@ -16,7 +16,7 @@ class ModuleLegacyTest extends KernelTestBase {
    */
   public function testModuleLoadInclude() {
     $this->assertFalse($this->container->get('module_handler')->moduleExists('module_test'), 'Ensure module is uninstalled so we test the ability to include uninstalled code.');
-    $this->expectDeprecation('module_load_include() is deprecated in drupal:9.4.0 and is removed from drupal:11.0.0. Instead, you should use \Drupal::moduleHandler()->loadInclude(). Note that including code from uninstalled extensions is no longer supported. See https://www.drupal.org/project/drupal/issues/697946');
+    $this->expectDeprecation('module_load_include() is deprecated in drupal:9.4.0 and is removed from drupal:11.0.0. Instead, you should use \Drupal::moduleHandler()->loadInclude(). Note that including code from uninstalled extensions is no longer supported. See https://www.drupal.org/node/2948698');
     $filename = module_load_include('inc', 'module_test', 'module_test.file');
     $this->assertStringEndsWith("module_test.file.inc", $filename);
 
