e7ab9440f24a18d7cd4415f80c3540a8309dd760
quietone <quietone@2572884.no-reply.drupal.org>
2024-01-10 16:32:11 +1300

    Issue #2957279 by alexpott, pguillard, chr.fritsch, Wim Leers: Fix typo in locale_config_batch_refresh_name causing "[warning] No configuration objects have been updated."

--- core/tests/Drupal/FunctionalTests/Installer/InstallerExistingConfigMultilingualTest.php
+++ core/tests/Drupal/FunctionalTests/Installer/InstallerExistingConfigMultilingualTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\FunctionalTests\Installer;
 
+use Drupal\Core\Logger\RfcLogLevel;
+
 /**
  * Verifies that installing from existing configuration works.
  *
@@ -19,6 +21,22 @@ class InstallerExistingConfigMultilingualTest extends InstallerExistingConfigTes
    */
   protected function getConfigTarball() {
     return __DIR__ . '/../../../fixtures/config_install/multilingual.tar.gz';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function testConfigSync() {
+    parent::testConfigSync();
+
+    // Ensure no warning, error, critical, alert or emergency messages have been
+    // logged.
+    $count = (int) \Drupal::database()->select('watchdog', 'w')->fields('w')->condition('severity', RfcLogLevel::WARNING, '<=')->countQuery()->execute()->fetchField();
+    $this->assertSame(0, $count);
+
+    // Ensure the correct message is logged from locale_config_batch_finished().
+    $count = (int) \Drupal::database()->select('watchdog', 'w')->fields('w')->condition('message', 'The configuration was successfully updated. %number configuration objects updated.')->countQuery()->execute()->fetchField();
+    $this->assertSame(1, $count);
   }
 
 }
