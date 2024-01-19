93e75890aff52f5f7fcc1b0b575528f609942dbe
catch <catch@35733.no-reply.drupal.org>
2024-01-19 13:11:47 +0000

    Issue #3414957 by Spokje: \Drupal\Tests\mysql\Functional\RequirementsTest is always skipped
    (cherry picked from commit ec64ab515981d50fb3d08213ef940d3d36d3bc2b)

--- core/modules/mysql/tests/src/Functional/RequirementsTest.php
+++ core/modules/mysql/tests/src/Functional/RequirementsTest.php
@@ -29,9 +29,9 @@ class RequirementsTest extends BrowserTestBase {
     parent::setUp();
 
     // The isolation_level option is only available for MySQL.
-    $connectionInfo = Database::getConnectionInfo();
-    if ($connectionInfo['default']['driver'] !== 'mysql') {
-      $this->markTestSkipped("This test does not support the {$connectionInfo['default']['driver']} database driver.");
+    $connection = Database::getConnection();
+    if ($connection->driver() !== 'mysql') {
+      $this->markTestSkipped("This test does not support the {$connection->driver()} database driver.");
     }
   }
 
