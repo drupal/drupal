7025180d1121b4d5ab2dbdc627a5228b740648c6
Dave Long <dave@longwaveconsulting.com>
2024-01-24 22:44:03 +0000

    Issue #3414800 by plopesc, smustgrave: Access check in AnnounceBlock does not take into account $return_as_object parameter

--- core/modules/announcements_feed/src/Plugin/Block/AnnounceBlock.php
+++ core/modules/announcements_feed/src/Plugin/Block/AnnounceBlock.php
@@ -6,6 +6,7 @@ namespace Drupal\announcements_feed\Plugin\Block;
 
 use Drupal\announcements_feed\AnnounceRenderer;
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultInterface;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
@@ -34,37 +35,34 @@ class AnnounceBlock extends BlockBase implements ContainerFactoryPluginInterface
    *   The plugin implementation definition.
    * @param \Drupal\announcements_feed\AnnounceRenderer $announceRenderer
    *   The AnnounceRenderer service.
-   * @param \Drupal\Core\Session\AccountInterface $currentUser
-   *   The current user.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, protected AnnounceRenderer $announceRenderer, protected AccountInterface $currentUser) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, protected AnnounceRenderer $announceRenderer) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): static {
     return new static(
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('announcements_feed.renderer'),
-      $container->get('current_user')
+      $container->get('announcements_feed.renderer')
     );
   }
 
   /**
    * {@inheritdoc}
    */
-  public function access(AccountInterface $account, $return_as_object = FALSE) {
-    return AccessResult::allowedIfHasPermission($this->currentUser, 'access announcements');
+  public function blockAccess(AccountInterface $account): AccessResultInterface {
+    return AccessResult::allowedIfHasPermission($account, 'access announcements');
   }
 
   /**
    * {@inheritdoc}
    */
-  public function build() {
+  public function build(): array {
     return $this->announceRenderer->render();
   }
 
