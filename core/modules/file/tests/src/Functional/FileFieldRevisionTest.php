1d3255c78173ffbe03e0040141906ac5534aff4d
catch <catch@35733.no-reply.drupal.org>
2024-01-30 09:57:42 +0000

    Issue #3417935 by mstrelan, acbramley: Remove calls to clearstatcache in \Drupal\Tests\file\Functional\FileFieldRevisionTest::testRevisions
    (cherry picked from commit 9d0eacebe1a8154df38fcd632d84d1a297246918)

--- core/modules/file/tests/src/Functional/FileFieldRevisionTest.php
+++ core/modules/file/tests/src/Functional/FileFieldRevisionTest.php
@@ -117,13 +117,6 @@ class FileFieldRevisionTest extends FileFieldTestBase {
 
     // Delete the user and check that the file is also deleted.
     $user->delete();
-    // TODO: This seems like a bug in File API. Clearing the stat cache should
-    // not be necessary here. The file really is deleted, but stream wrappers
-    // doesn't seem to think so unless we clear the PHP file stat() cache.
-    clearstatcache($node_file_r1->getFileUri());
-    clearstatcache($node_file_r2->getFileUri());
-    clearstatcache($node_file_r3->getFileUri());
-    clearstatcache($node_file_r4->getFileUri());
 
     // Call file_cron() to clean up the file. Make sure the changed timestamp
     // of the file is older than the system.file.temporary_maximum_age
