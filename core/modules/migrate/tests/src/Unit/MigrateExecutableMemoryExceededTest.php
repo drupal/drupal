c5db4bb2b677b9d5d4ca5c0f2a04a7c0f2fa8dea
catch <catch@35733.no-reply.drupal.org>
2024-02-12 15:50:13 +0000

    Issue #3419349 by longwave: Remove withConsecutive() in MigrateExecutableMemoryExceededTest

--- core/modules/migrate/tests/src/Unit/MigrateExecutableMemoryExceededTest.php
+++ core/modules/migrate/tests/src/Unit/MigrateExecutableMemoryExceededTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\migrate\Unit;
 
+use Prophecy\Argument;
+
 /**
  * Tests the \Drupal\migrate\MigrateExecutable::memoryExceeded() method.
  *
@@ -21,7 +23,7 @@ class MigrateExecutableMemoryExceededTest extends MigrateTestCase {
   /**
    * The mocked migrate message.
    *
-   * @var \Drupal\migrate\MigrateMessageInterface|\PHPUnit\Framework\MockObject\MockObject
+   * @var \Drupal\migrate\MigrateMessageInterface|\Prophecy\Prophecy\ObjectProphecy
    */
   protected $message;
 
@@ -54,9 +56,9 @@ class MigrateExecutableMemoryExceededTest extends MigrateTestCase {
   protected function setUp(): void {
     parent::setUp();
     $this->migration = $this->getMigration();
-    $this->message = $this->createMock('Drupal\migrate\MigrateMessageInterface');
+    $this->message = $this->prophesize('Drupal\migrate\MigrateMessageInterface');
 
-    $this->executable = new TestMigrateExecutable($this->migration, $this->message);
+    $this->executable = new TestMigrateExecutable($this->migration, $this->message->reveal());
     $this->executable->setStringTranslation($this->getStringTranslationStub());
   }
 
@@ -80,24 +82,14 @@ class MigrateExecutableMemoryExceededTest extends MigrateTestCase {
     $this->executable->setMemoryUsage($memory_usage_first ?: $this->memoryLimit, $memory_usage_second ?: $this->memoryLimit);
     $this->executable->setMemoryThreshold(0.85);
     if ($message) {
-      $this->executable->message->expects($this->exactly(2))
-        ->method('display')
-        ->withConsecutive(
-          [
-            $this->callback(function ($subject) {
-              return mb_stripos((string) $subject, 'reclaiming memory') !== FALSE;
-            }),
-          ],
-          [
-            $this->callback(function ($subject) use ($message) {
-              return mb_stripos((string) $subject, $message) !== FALSE;
-            }),
-          ],
-        );
+      $this->message->display(Argument::that(fn(string $subject) => str_contains($subject, 'reclaiming memory')), 'warning')
+        ->shouldBeCalledOnce();
+      $this->message->display(Argument::that(fn(string $subject) => str_contains($subject, $message)), 'warning')
+        ->shouldBeCalledOnce();
     }
     else {
-      $this->executable->message->expects($this->never())
-        ->method($this->anything());
+      $this->message->display(Argument::cetera())
+        ->shouldNotBeCalled();
     }
     $result = $this->executable->memoryExceeded();
     $this->assertEquals($memory_exceeded, $result);
