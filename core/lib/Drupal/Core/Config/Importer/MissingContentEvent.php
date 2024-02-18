476e65a1e15ab24ef644b08b6acfb56d1c5d6ab7
Dave Long <dave@longwaveconsulting.com>
2024-02-18 13:46:18 +0000

    Issue #3414993 by alexpott: Add ConfigImporter to \Drupal\Core\Config\Importer\MissingContentEvent
    (cherry picked from commit eafa856443000bf8022165f588753a57639daa1a)

--- core/lib/Drupal/Core/Config/Importer/MissingContentEvent.php
+++ core/lib/Drupal/Core/Config/Importer/MissingContentEvent.php
@@ -2,14 +2,15 @@
 
 namespace Drupal\Core\Config\Importer;
 
-use Drupal\Component\EventDispatcher\Event;
+use Drupal\Core\Config\ConfigImporter;
+use Drupal\Core\Config\ConfigImporterEvent;
 
 /**
  * Wraps a configuration event for event listeners.
  *
  * @see \Drupal\Core\Config\ConfigEvents::IMPORT_MISSING_CONTENT
  */
-class MissingContentEvent extends Event {
+class MissingContentEvent extends ConfigImporterEvent {
 
   /**
    * A list of missing content dependencies.
@@ -23,8 +24,11 @@ class MissingContentEvent extends Event {
    *
    * @param array $missing_content
    *   Missing content information.
+   * @param \Drupal\Core\Config\ConfigImporter $config_importer
+   *   The config importer that triggered this event.
    */
-  public function __construct(array $missing_content) {
+  public function __construct(array $missing_content, ConfigImporter $config_importer) {
+    parent::__construct($config_importer);
     $this->missingContent = $missing_content;
   }
 
