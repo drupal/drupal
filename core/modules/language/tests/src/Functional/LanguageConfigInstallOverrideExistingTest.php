0069d07794b3478d0736fc3f0a624ee5622c1700
catch <catch@35733.no-reply.drupal.org>
2024-02-26 10:55:14 +0000

    Issue #2613222 by webflo, alexpott, ameymudras, pooja saraah, _utsavsharma, penyaskito, Janvi Dasani: Language module shouldn't alter config in sync mode
    (cherry picked from commit 7e2db381c38de9d292603e52c231fb1f618abe7d)

---
+++ core/modules/language/tests/src/Functional/LanguageConfigInstallOverrideExistingTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\Tests\language\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Ensures the language config is installed but not altered on install.
+ *
+ * @group language
+ */
+class LanguageConfigInstallOverrideExistingTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $profile = 'test_language_negotiation';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests when language config is installed existing config is not overridden.
+   */
+  public function testLanguageConfigInstallOverrideExisting() {
+    /** @var \Drupal\Core\Config\StorageInterface $storage */
+    $storage = $this->container->get('config.storage');
+    $config = $this->config('language.types');
+
+    // The negotiation methods that have been removed should be disabled after
+    // purging if not avoided in language_modules_installed().
+    $language_types_data = $storage->read('language.types');
+    $this->assertTrue(isset($language_types_data['negotiation']['language_content']['enabled']['test_language_negotiation_method']));
+    $this->assertTrue(isset($language_types_data['negotiation']['language_content']['enabled']['language-selected']));
+
+    $this->assertEquals(-10, $config->get('negotiation.language_content.enabled.test_language_negotiation_method'));
+    $this->assertEquals(12, $config->get('negotiation.language_content.enabled.language-selected'));
+  }
+
+}
