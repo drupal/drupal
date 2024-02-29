f4d6cdf6ac9f542fedf6949995ee810ebfcaa347
nod_ <nod_@598310.no-reply.drupal.org>
2024-02-29 21:08:40 +0100

    Issue #3416304 by sleitner, RoloDMonkey, smustgrave: Javascript warning from content language and translation page
    (cherry picked from commit 6923b1a62992f8cfd55d07c248b45e49dc7aff9d)
    (cherry picked from commit 5cf244cb809d20e58518eac1ff193cb4fc2be757)

---
+++ core/modules/content_translation/tests/src/FunctionalJavascript/ContentTranslationConfigUITest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\content_translation\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+
+/**
+ * Tests that the content translation configuration javascript does't fail.
+ *
+ * @group content_translation
+ */
+class ContentTranslationConfigUITest extends WebDriverTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['content_translation', 'node'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Use the minimal profile.
+   *
+   * @var string
+   */
+  protected $profile = 'standard';
+
+  /**
+   * Tests that the content translation configuration javascript does't fail.
+   */
+  public function testContentTranslationConfigUI() {
+    $content_translation_manager = $this->container->get('content_translation.manager');
+    $content_translation_manager->setEnabled('node', 'article', TRUE);
+    $this->rebuildContainer();
+
+    $admin = $this->drupalCreateUser([], NULL, TRUE);
+    $this->drupalLogin($admin);
+    $this->drupalGet('/admin/config/regional/content-language');
+    $this->failOnJavaScriptErrors();
+  }
+
+}
