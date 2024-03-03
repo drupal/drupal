e5c3c870e3a2376a8e6fde9ee00122f881bf2cef
catch <catch@35733.no-reply.drupal.org>
2024-03-03 15:11:34 +0000

    Issue #3419914 by Spokje, longwave, andypost, smustgrave: Remove UpdateHookRegistryFactory and UpdateRegistryFactory services

--- core/lib/Drupal/Core/Update/UpdateRegistry.php
+++ core/lib/Drupal/Core/Update/UpdateRegistry.php
@@ -6,7 +6,8 @@ use Drupal\Core\Config\ConfigCrudEvent;
 use Drupal\Core\Config\ConfigEvents;
 use Drupal\Core\Extension\Extension;
 use Drupal\Core\Extension\ExtensionDiscovery;
-use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
+use Drupal\Core\Extension\ThemeHandlerInterface;
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 // cspell:ignore updatetype
@@ -56,13 +57,6 @@ class UpdateRegistry implements EventSubscriberInterface {
   protected $keyValue;
 
   /**
-   * Should we respect update functions in tests.
-   *
-   * @var bool|null
-   */
-  protected $includeTests = NULL;
-
-  /**
    * The site path.
    *
    * @var string
@@ -86,19 +80,26 @@ class UpdateRegistry implements EventSubscriberInterface {
    *   The app root.
    * @param string $site_path
    *   The site path.
-   * @param string[] $enabled_extensions
-   *   A list of enabled extensions.
-   * @param \Drupal\Core\KeyValueStore\KeyValueStoreInterface $key_value
-   *   The key value store.
-   * @param bool|null $include_tests
-   *   (optional) A flag whether to include tests in the scanning of extensions.
+   * @param array $module_list
+   *   An associative array whose keys are the names of installed modules.
+   * @param \Drupal\Core\KeyValueStore\KeyValueFactoryInterface $key_value_factory
+   *   The key value factory.
+   * @param \Drupal\Core\Extension\ThemeHandlerInterface|bool|null $theme_handler
+   *   The theme handler.
    */
-  public function __construct($root, $site_path, array $enabled_extensions, KeyValueStoreInterface $key_value, $include_tests = NULL) {
+  public function __construct($root, $site_path, $module_list, KeyValueFactoryInterface $key_value_factory, ThemeHandlerInterface|bool $theme_handler = NULL) {
     $this->root = $root;
     $this->sitePath = $site_path;
-    $this->enabledExtensions = $enabled_extensions;
-    $this->keyValue = $key_value;
-    $this->includeTests = $include_tests;
+    if ($module_list !== [] && array_is_list($module_list)) {
+      @trigger_error('Calling ' . __METHOD__ . '() with the $enabled_extensions argument is deprecated in drupal:10.3.0 and is removed from drupal:11.0.0. Use an associative array whose keys are the names of installed modules instead. See https://www.drupal.org/node/3423659', E_USER_DEPRECATED);
+      $module_list = \Drupal::service('module_handler')->getModuleList();
+    }
+    if ($theme_handler === NULL || is_bool($theme_handler)) {
+      @trigger_error('Calling ' . __METHOD__ . '() with the $include_tests argument is deprecated in drupal:10.3.0 and is removed from drupal:11.0.0. See https://www.drupal.org/node/3423659', E_USER_DEPRECATED);
+      $theme_handler = \Drupal::service('theme_handler');
+    }
+    $this->enabledExtensions = array_merge(array_keys($module_list), array_keys($theme_handler->listInfo()));
+    $this->keyValue = $key_value_factory->get('post_update');
   }
 
   /**
