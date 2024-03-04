3f01ed41d712b8c797056dca6c32de5a2a980313
nod_ <nod_@598310.no-reply.drupal.org>
2024-03-04 11:13:51 +0100

    Issue #3423384 by govind_giri_goswami, Gauravvvv, dineshkumarbollu, adwivedi008, Pravesh_Poonia, smustgrave, Tanuj., shreyal999, Ruturaj Chaubey, mherchel, andy-blum: Extra Close icon in Messages

--- core/themes/olivero/js/messages.js
+++ core/themes/olivero/js/messages.js
@@ -14,25 +14,26 @@
     const messageContainer = message.querySelector(
       '[data-drupal-selector="messages-container"]',
     );
+    if (!messageContainer.querySelector('.messages__button')) {
+      const closeBtnWrapper = document.createElement('div');
+      closeBtnWrapper.setAttribute('class', 'messages__button');
 
-    const closeBtnWrapper = document.createElement('div');
-    closeBtnWrapper.setAttribute('class', 'messages__button');
+      const closeBtn = document.createElement('button');
+      closeBtn.setAttribute('type', 'button');
+      closeBtn.setAttribute('class', 'messages__close');
 
-    const closeBtn = document.createElement('button');
-    closeBtn.setAttribute('type', 'button');
-    closeBtn.setAttribute('class', 'messages__close');
+      const closeBtnText = document.createElement('span');
+      closeBtnText.setAttribute('class', 'visually-hidden');
+      closeBtnText.innerText = Drupal.t('Close message');
 
-    const closeBtnText = document.createElement('span');
-    closeBtnText.setAttribute('class', 'visually-hidden');
-    closeBtnText.innerText = Drupal.t('Close message');
+      messageContainer.appendChild(closeBtnWrapper);
+      closeBtnWrapper.appendChild(closeBtn);
+      closeBtn.appendChild(closeBtnText);
 
-    messageContainer.appendChild(closeBtnWrapper);
-    closeBtnWrapper.appendChild(closeBtn);
-    closeBtn.appendChild(closeBtnText);
-
-    closeBtn.addEventListener('click', () => {
-      message.classList.add('hidden');
-    });
+      closeBtn.addEventListener('click', () => {
+        message.classList.add('hidden');
+      });
+    }
   };
 
   /**
