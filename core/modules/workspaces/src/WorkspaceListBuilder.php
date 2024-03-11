0aaa9b8d49a256801ef5bc99071a3881d44f55d4
Dave Long <dave@longwaveconsulting.com>
2024-03-11 17:13:33 +0000

    Issue #3167126 by ranjith_kumar_k_u, rpayanm, amateescu, mheip, CedricL, smustgrave: Call to a member function getDisplayname() on null in WorkspaceListBuilder.php

--- core/modules/workspaces/src/WorkspaceListBuilder.php
+++ core/modules/workspaces/src/WorkspaceListBuilder.php
@@ -10,6 +10,7 @@ use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Url;
+use Drupal\user\UserInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -121,7 +122,9 @@ class WorkspaceListBuilder extends EntityListBuilder {
           '#url' => $entity->toUrl(),
         ],
       ],
-      'owner' => $entity->getOwner()->getDisplayName(),
+      'owner' => (($owner = $entity->getOwner()) && $owner instanceof UserInterface)
+        ? $owner->getDisplayName()
+        : $this->t('N/A'),
     ];
     $row['data'] = $row['data'] + parent::buildRow($entity);
 
