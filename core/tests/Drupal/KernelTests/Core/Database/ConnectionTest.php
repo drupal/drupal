6b7883f9c6853f745d7cf90dd8467840877406e1
Dave Long <dave@longwaveconsulting.com>
2024-03-11 16:56:39 +0000

    Issue #3427044 by mondrake: Replace calls to ::expectError*() in ConnectionTest
    (cherry picked from commit dc85a3c65bd726f22ca04a5d408e79a27618b697)

--- core/tests/Drupal/KernelTests/Core/Database/ConnectionTest.php
+++ core/tests/Drupal/KernelTests/Core/Database/ConnectionTest.php
@@ -125,7 +125,7 @@ class ConnectionTest extends DatabaseTestBase {
       'test_table' => $connection_info['default']['prefix'] . '_bar',
     ];
     Database::addConnectionInfo('default', 'foo', $new_connection_info);
-    $this->expectError();
+    $this->expectException(\AssertionError::class);
     $foo_connection = Database::getConnection('foo', 'default');
   }
 
@@ -139,7 +139,7 @@ class ConnectionTest extends DatabaseTestBase {
       'default' => $connection_info['default']['prefix'],
     ];
     Database::addConnectionInfo('default', 'foo', $new_connection_info);
-    $this->expectError();
+    $this->expectException(\AssertionError::class);
     $foo_connection = Database::getConnection('foo', 'default');
   }
 
