fb49143fab43dc0e5fb76084aa73016ae3ff150a
Alex Pott <alex.a.pott@googlemail.com>
2024-03-12 10:20:08 +0000

    Issue #3421009 by sorlov, mohit_aghera, pradhumanjain2311, larowlan, smustgrave, alexpott: Convert ViewsCache plugin discovery to attributes

--- core/modules/views/src/Plugin/views/cache/Tag.php
+++ core/modules/views/src/Plugin/views/cache/Tag.php
@@ -3,18 +3,19 @@
 namespace Drupal\views\Plugin\views\cache;
 
 use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\views\Attribute\ViewsCache;
 
 /**
  * Simple caching of query results for Views displays.
  *
  * @ingroup views_cache_plugins
- *
- * @ViewsCache(
- *   id = "tag",
- *   title = @Translation("Tag based"),
- *   help = @Translation("Tag based caching of data. Caches will persist until any related cache tags are invalidated.")
- * )
  */
+#[ViewsCache(
+  id: 'tag',
+  title: new TranslatableMarkup('Tag based'),
+  help: new TranslatableMarkup('Tag based caching of data. Caches will persist until any related cache tags are invalidated.'),
+)]
 class Tag extends CachePluginBase {
 
   /**
