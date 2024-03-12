9a6e38c000c21f23b6d3be0844f07dddb5b36fe7
catch <catch@35733.no-reply.drupal.org>
2024-03-12 10:51:45 +0000

    Issue #3057399 by quietone, Akhil Babu, kristiaanvandeneynde, alexpott, pradhumanjain2311, smustgrave, catch, poker10, longwave, jimafisk, Newb_Druper, robpowell, dpi, tomdewild: Add user_requirements to check for missing anonymous user

---
+++ core/modules/user/tests/src/Functional/UserRequirementsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\user\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests the requirements checks of the User module.
+ *
+ * @group user
+ */
+class UserRequirementsTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests that the requirements check can detect a missing anonymous user.
+   */
+  public function testAnonymousUser(): void {
+    // Remove the anonymous user.
+    \Drupal::database()
+      ->delete('users')
+      ->condition('uid', 0)
+      ->execute();
+
+    $this->drupalLogin($this->rootUser);
+    $this->drupalGet('/admin/reports/status');
+    $this->assertSession()->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains("The anonymous user does not exist.");
+  }
+
+}
