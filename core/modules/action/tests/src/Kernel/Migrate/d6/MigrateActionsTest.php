081b69b53eb60e84a5be32c85f4c931f4ee3df2d
catch <catch@35733.no-reply.drupal.org>
2024-03-12 10:57:10 +0000

    Issue #3413949 by andypost, quietone, Spokje, taraskorpach, smustgrave, Wim Leers, DanielVeza: Move some Action plugins to Action module
    (cherry picked from commit 63a2ba06aa535a46286ebc10f3c87a77b85d7afd)

--- core/modules/action/tests/src/Kernel/Migrate/d6/MigrateActionsTest.php
+++ core/modules/action/tests/src/Kernel/Migrate/d6/MigrateActionsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\Tests\system\Kernel\Migrate\d6;
+namespace Drupal\Tests\action\Kernel\Migrate\d6;
 
 use Drupal\system\Entity\Action;
 use Drupal\Tests\migrate_drupal\Kernel\d6\MigrateDrupal6TestBase;
@@ -12,7 +12,7 @@ use Drupal\Tests\migrate_drupal\Kernel\d6\MigrateDrupal6TestBase;
  */
 class MigrateActionsTest extends MigrateDrupal6TestBase {
 
-  protected static $modules = ['comment', 'node'];
+  protected static $modules = ['action', 'comment', 'node'];
 
   /**
    * {@inheritdoc}
@@ -26,21 +26,9 @@ class MigrateActionsTest extends MigrateDrupal6TestBase {
    * Tests Drupal 6 action migration to Drupal 8.
    */
   public function testActions() {
-    // Test default actions.
-    $this->assertEntity('node_publish_action', 'Publish post', 'node', []);
-    $this->assertEntity('node_make_sticky_action', 'Make post sticky', 'node', []);
-    $this->assertEntity('user_block_user_action', 'Block current user', 'user', []);
-    $this->assertEntity('comment_publish_action', 'Publish comment', 'comment', []);
-
     // Test advanced actions.
-    $this->assertEntity('display_a_message_to_the_user', 'Display a message to the user', 'system', ["message" => "Drupal migration test"]);
-    $this->assertEntity('send_e_mail', 'Send e-mail', 'system', [
-      "recipient" => "test@example.com",
-      "subject" => "Drupal migration test",
-      "message" => "Drupal migration test",
-    ]);
-    $this->assertEntity('redirect_to_url', 'Redirect to URL', 'system', ["url" => "https://www.drupal.org"]);
-
+    $this->assertEntity('unpublish_comment_containing_keyword_s_', 'Unpublish comment containing keyword(s)', 'comment', ["keywords" => [0 => "drupal"]]);
+    $this->assertEntity('unpublish_post_containing_keyword_s_', 'Unpublish post containing keyword(s)', 'node', ["keywords" => [0 => "drupal"]]);
   }
 
   /**
