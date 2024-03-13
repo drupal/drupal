4586e9fc62d21e5f18a696775037d6c6fc840d37
Alex Pott <alex.a.pott@googlemail.com>
2024-03-13 11:36:11 +0000

    Issue #3421008 by el7cosmos, sorlov, smustgrave, larowlan, alexpott, quietone: Convert ViewsExposedForm plugin discovery to attributes

--- core/modules/views/src/Plugin/views/exposed_form/Basic.php
+++ core/modules/views/src/Plugin/views/exposed_form/Basic.php
@@ -2,17 +2,19 @@
 
 namespace Drupal\views\Plugin\views\exposed_form;
 
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\views\Attribute\ViewsExposedForm;
+
 /**
  * Exposed form plugin that provides a basic exposed form.
  *
  * @ingroup views_exposed_form_plugins
- *
- * @ViewsExposedForm(
- *   id = "basic",
- *   title = @Translation("Basic"),
- *   help = @Translation("Basic exposed form")
- * )
  */
+#[ViewsExposedForm(
+  id: 'basic',
+  title: new TranslatableMarkup('Basic'),
+  help: new TranslatableMarkup('Basic exposed form')
+)]
 class Basic extends ExposedFormPluginBase {
 
 }
