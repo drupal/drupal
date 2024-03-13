cbbdff700877f9b5c07a79129a4821a150ee9038
Alex Pott <alex.a.pott@googlemail.com>
2024-03-13 11:18:35 +0000

    Issue #3427388 by godotislate, Berdir: Update Drupal\Component\Annotation\Doctrine\StaticReflectionParser::hasClassAttribute() to allow attribute subclasses
    (cherry picked from commit 0356ea590c5a90d9082d90b43b84f40e2612c691)

--- core/tests/Drupal/Tests/Component/Annotation/Doctrine/StaticReflectionParserTest.php
+++ core/tests/Drupal/Tests/Component/Annotation/Doctrine/StaticReflectionParserTest.php
@@ -15,8 +15,10 @@ class StaticReflectionParserTest extends TestCase {
 
   /**
    * @testWith ["AttributeClass", "\\Attribute", true]
+   *           ["AttributeClass", "attribute", true]
    *           ["AttributeClass", "Attribute", true]
    *           ["AttributeClass", "\\DoesNotExist", false]
+   *           ["Nonexistent", "NonexistentAttribute", false]
    *           ["MultipleAttributes", "Attribute", true]
    *           ["MultipleAttributes", "Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\Attribute\\AttributeClass", true]
    *           ["MultipleAttributes", "DoesNotExist", false]
@@ -26,15 +28,16 @@ class StaticReflectionParserTest extends TestCase {
    *           ["UsedAsQualified", "Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\ExtraAttributes\\ExampleAttribute", true]
    *           ["Qualified", "Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\ExtraAttributes\\ExampleAttribute", true]
    *           ["Relative", "Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\Attribute\\SubDir\\SubDirAttribute", true]
+   *           ["FullyQualified", "Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\ExtraAttributes\\ExampleParentAttribute", true]
+   *           ["Used", "Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\ExtraAttributes\\ExampleParentAttribute", true]
+   *           ["UsedAs", "Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\ExtraAttributes\\ExampleParentAttribute", true]
+   *           ["UsedAsQualified", "Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\ExtraAttributes\\ExampleParentAttribute", true]
+   *           ["Qualified", "Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\ExtraAttributes\\ExampleParentAttribute", true]
    */
   public function testAttribute(string $class, string $attribute_class, bool $expected) {
     $finder = MockFileFinder::create(__DIR__ . '/Fixtures/Attribute/' . $class . '.php');
     $parser = new StaticReflectionParser('\\Drupal\\Tests\\Component\\Annotation\\Doctrine\\Fixtures\\Attribute\\' . $class, $finder);
-    $this->assertSame($expected, $parser->hasClassAttribute($attribute_class), "'$class' has '$attribute_class'");
-    // Attribute names and namespaces are case-insensitive in PHP. Practically
-    // Composer autoloading makes this untrue but builtins like \Attribute are
-    // case-insensitive so we should support that.
-    $this->assertSame($expected, $parser->hasClassAttribute(strtoupper($attribute_class)), "'$class' has '$attribute_class'");
+    $this->assertSame($expected, $parser->hasClassAttribute($attribute_class), "'$class' has attribute that is a '$attribute_class'");
   }
 
 }
