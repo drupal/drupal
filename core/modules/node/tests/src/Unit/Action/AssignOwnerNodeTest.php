1c279bde183ca8b15efd0668442483f13e3d3472
catch <catch@35733.no-reply.drupal.org>
2024-03-13 10:25:35 +0000

    Issue #3413949 by andypost, quietone, Spokje, taraskorpach, smustgrave, catch, Wim Leers, DanielVeza: Move some Action plugins to Action module
    (cherry picked from commit 7e81b860288f4dedef086de034d9bc0f66cfb457)

---
+++ core/modules/node/tests/src/Unit/Action/AssignOwnerNodeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\node\Unit\Action;
+
+use Drupal\Core\Database\Connection;
+use Drupal\node\Plugin\Action\AssignOwnerNode;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @group node
+ * @group legacy
+ */
+class AssignOwnerNodeTest extends UnitTestCase {
+
+  /**
+   * Tests deprecation of \Drupal\node\Plugin\Action\AssignOwnerNodeTest.
+   */
+  public function testAssignOwnerNode() {
+    $this->expectDeprecation('Drupal\node\Plugin\Action\AssignOwnerNode is deprecated in drupal:10.3.0 and is removed from drupal:11.0.0. Use \Drupal\action\Plugin\Action\AssignOwnerNode instead. See https://www.drupal.org/node/3424506');
+    $this->assertIsObject(new AssignOwnerNode([], 'foo', [], $this->prophesize(Connection::class)->reveal()));
+  }
+
+}
