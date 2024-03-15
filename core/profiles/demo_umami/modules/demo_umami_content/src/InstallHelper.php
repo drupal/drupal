0e6acad498579bf240603a90b0597245f725eaa4
Alex Pott <alex.a.pott@googlemail.com>
2024-03-15 14:41:32 +0000

    Issue #3061489 by penyaskito, shaal, betoscopio, smustgrave: Umami changes the admin interface language based on the current page

--- core/profiles/demo_umami/modules/demo_umami_content/src/InstallHelper.php
+++ core/profiles/demo_umami/modules/demo_umami_content/src/InstallHelper.php
@@ -11,6 +11,8 @@ use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\State\StateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+// cSpell:ignore María García Gregorio Sánchez
+
 /**
  * Defines a helper class for importing default content.
  *
@@ -137,7 +139,7 @@ class InstallHelper implements ContainerInjectionInterface {
    */
   public function importContent() {
     $this->getModulePath()
-      ->importEditors()
+      ->importUsers()
       ->importContentFromFile('taxonomy_term', 'tags')
       ->importContentFromFile('taxonomy_term', 'recipe_category')
       ->importContentFromFile('media', 'image')
@@ -297,25 +299,42 @@ class InstallHelper implements ContainerInjectionInterface {
   }
 
   /**
-   * Imports editors.
+   * Imports users.
    *
-   * Other users are created as their content is imported. However, editors
-   * don't have their own content so are created here instead.
+   * Users are created as their content is imported. However, some users might
+   * have non-default values (as preferred language), or editors don't have
+   * their own content so are created here instead.
    *
    * @return $this
    */
-  protected function importEditors() {
+  protected function importUsers() {
     $user_storage = $this->entityTypeManager->getStorage('user');
-    $editors = [
-      'Margaret Hopper',
-      'Grace Hamilton',
+    $users = [
+      'Gregorio Sánchez' => [
+        'preferred_language' => 'es',
+        'roles' => ['author'],
+      ],
+      'Margaret Hopper' => [
+        'preferred_language' => 'en',
+        'roles' => ['editor'],
+      ],
+      'Grace Hamilton' => [
+        'preferred_language' => 'en',
+        'roles' => ['editor'],
+      ],
+      'María García' => [
+        'preferred_language' => 'es',
+        'roles' => ['editor'],
+      ],
     ];
-    foreach ($editors as $name) {
+    foreach ($users as $name => $user_data) {
       $user = $user_storage->create([
         'name' => $name,
         'status' => 1,
-        'roles' => ['editor'],
-        'mail' => mb_strtolower(str_replace(' ', '.', $name)) . '@example.com',
+        'roles' => $user_data['roles'],
+        'preferred_langcode' => $user_data['preferred_language'],
+        'preferred_admin_langcode' => $user_data['preferred_language'],
+        'mail' => \Drupal::transliteration()->transliterate(mb_strtolower(str_replace(' ', '.', $name))) . '@example.com',
       ]);
       $user->enforceIsNew();
       $user->save();
