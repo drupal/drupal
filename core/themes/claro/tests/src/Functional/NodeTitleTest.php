e5081e919f05793e723bace586d80fec81af6687
nod_ <nod_@598310.no-reply.drupal.org>
2024-03-15 23:40:26 +0100

    Issue #3376566 by kunal.sachdev, sakthi_dev, divya.sejekan, Nitin shrivastava, djsagar, smustgrave, nod_, longwave, quietone, catch: Display the page title, even if "0" in olivero
    (cherry picked from commit 00432f54687116f1798cb6c79d3696b0b23cae45)

---
+++ core/themes/claro/tests/src/Functional/NodeTitleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\claro\Functional;
+
+use Drupal\Tests\system\Functional\Theme\NodeTitleTestBase;
+
+/**
+ * Tests node title for claro.
+ *
+ * @group node
+ */
+class NodeTitleTest extends NodeTitleTestBase {
+
+}
