f46190554cf98e32d31f16bc7356efca5ec5d1b3
catch <catch@35733.no-reply.drupal.org>
2024-03-18 15:43:11 +0000

    Issue #3416357 by longwave, arunkumark, taraskorpach, Spokje, andypost: Convert QueueFactory to use a service locator
    (cherry picked from commit dd446b636eda9a8262891f4769f2573d75cc22e2)

--- core/lib/Drupal/Core/Queue/QueueFactory.php
+++ core/lib/Drupal/Core/Queue/QueueFactory.php
@@ -3,15 +3,13 @@
 namespace Drupal\Core\Queue;
 
 use Drupal\Core\Site\Settings;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\Attribute\AutowireLocator;
 
 /**
  * Defines the queue factory.
  */
-class QueueFactory implements ContainerAwareInterface {
-
-  use ContainerAwareTrait;
+class QueueFactory {
 
   /**
    * Instantiated queues, keyed by name.
@@ -28,9 +26,18 @@ class QueueFactory implements ContainerAwareInterface {
   protected $settings;
 
   /**
-   * Constructs a queue factory.
+   * Constructs QueueFactory object.
+   *
+   * @param \Drupal\Core\Site\Settings $settings
+   *   The site settings.
+   * @param \Psr\Container\ContainerInterface $container
+   *   A service locator that contains the queue services.
    */
-  public function __construct(Settings $settings) {
+  public function __construct(
+    Settings $settings,
+    #[AutowireLocator('queue_factory')]
+    protected ContainerInterface $container,
+  ) {
     $this->settings = $settings;
   }
 
@@ -59,9 +66,6 @@ class QueueFactory implements ContainerAwareInterface {
         $service_name = $this->settings->get('queue_service_' . $name, $this->settings->get('queue_default', 'queue.database'));
       }
       $factory = $this->container->get($service_name);
-      if (!$factory instanceof QueueFactoryInterface) {
-        @trigger_error(sprintf('Not implementing %s in %s is deprecated in drupal:10.3.0 and the factory will not be discovered in drupal:11.0.0. Implement the interface in your factory class. See https://www.drupal.org/node/3417034', QueueFactoryInterface::class, $factory::class), E_USER_DEPRECATED);
-      }
       $this->queues[$name] = $factory->get($name);
     }
     return $this->queues[$name];
