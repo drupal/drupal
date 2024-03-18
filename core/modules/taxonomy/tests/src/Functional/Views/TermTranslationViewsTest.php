46564ee73302dcf9f95a71d7fcfe028bc8833492
Alex Pott <alex.a.pott@googlemail.com>
2024-03-18 13:26:17 +0000

    Issue #2765297 by mohit_aghera, joseph.olstad, sylus, alexpott, ducktape, anmolgoyal74, heikki, kishor_kolekar, kriboogh, googletorp, dnotes, mpp, catch, Lendude, Berdir, borisson_, xjm: Return translated term name on views "Content: Has taxonomy term ID (with depth)"
    (cherry picked from commit 1bcc7f34e6b9479e8a868c7a5067cca8c04c757c)

---
+++ core/modules/taxonomy/tests/src/Functional/Views/TermTranslationViewsTest.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\taxonomy\Functional\Views;
+
+use Drupal\Core\Url;
+use Drupal\Tests\taxonomy\Functional\TaxonomyTranslationTestTrait;
+
+/**
+ * Tests for views translation.
+ *
+ * @group taxonomy
+ */
+class TermTranslationViewsTest extends TaxonomyTestBase {
+
+  use TaxonomyTranslationTestTrait;
+
+  /**
+   * Term to translated term mapping.
+   *
+   * @var array
+   */
+  protected $termTranslationMap = [
+    'one' => 'translatedOne',
+    'two' => 'translatedTwo',
+    'three' => 'translatedThree',
+  ];
+
+  /**
+   * Created terms.
+   *
+   * @var \Drupal\taxonomy\Entity\Term[]
+   */
+  protected $terms = [];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['taxonomy', 'language', 'content_translation', 'views'];
+
+  /**
+   * Views used by this test.
+   *
+   * @var array
+   */
+  public static $testViews = ['taxonomy_translated_term_name_test'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Language object.
+   *
+   * @var \Drupal\Core\Language\LanguageInterface|null
+   */
+  protected $translationLanguage;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp($import_test_views = TRUE, $modules = []): void {
+    parent::setUp($import_test_views, $modules);
+    $this->setupLanguages();
+    $this->enableTranslation();
+    $this->setUpTerms();
+    $this->translationLanguage = \Drupal::languageManager()->getLanguage($this->translateToLangcode);
+  }
+
+  /**
+   * Ensure that proper translation is returned when contextual filter.
+   *
+   * Taxonomy term: Term ID & Content: Has taxonomy term ID (with depth)
+   * contextual filters are enabled for two separate view modes.
+   */
+  public function testTermsTranslationWithContextualFilter() {
+    $this->drupalLogin($this->rootUser);
+
+    foreach ($this->terms as $term) {
+      // Test with "Content: Has taxonomy term ID (with depth)" contextual filter.
+      // Generate base language url and send request.
+      $url = Url::fromRoute('view.taxonomy_translated_term_name_test.page_1', ['arg_0' => $term->id()])->toString();
+      $this->drupalGet($url);
+      $this->assertSession()->pageTextContains($term->label());
+
+      // Generate translation URL and send request.
+      $url = Url::fromRoute('view.taxonomy_translated_term_name_test.page_1', ['arg_0' => $term->id()], ['language' => $this->translationLanguage])->toString();
+      $this->drupalGet($url);
+      $this->assertSession()->pageTextContains($this->termTranslationMap[$term->label()]);
+
+      // Test with "Taxonomy term: Term ID" contextual filter.
+      // Generate base language url and send request.
+      $url = Url::fromRoute('view.taxonomy_translated_term_name_test.page_2', ['arg_0' => $term->id()])->toString();
+      $this->drupalGet($url);
+      $this->assertSession()->pageTextContains($term->label());
+
+      // Generate translation URL and send request.
+      $url = Url::fromRoute('view.taxonomy_translated_term_name_test.page_2', ['arg_0' => $term->id()], ['language' => $this->translationLanguage])->toString();
+      $this->drupalGet($url);
+      $this->assertSession()->pageTextContains($this->termTranslationMap[$term->label()]);
+    }
+  }
+
+  /**
+   * Setup translated terms in a hierarchy.
+   */
+  protected function setUpTerms() {
+    $parent_vid = 0;
+    foreach ($this->termTranslationMap as $name => $translation) {
+
+      $term = $this->createTerm([
+        'name' => $name,
+        'langcode' => $this->baseLangcode,
+        'parent' => $parent_vid,
+        'vid' => $this->vocabulary->id(),
+      ]);
+
+      $term->addTranslation($this->translateToLangcode, [
+        'name' => $translation,
+      ]);
+      $term->save();
+
+      // Each term is nested under the last.
+      $parent_vid = $term->id();
+
+      $this->terms[] = $term;
+    }
+  }
+
+}
