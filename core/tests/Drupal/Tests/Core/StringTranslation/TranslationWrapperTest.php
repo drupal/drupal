67604e0923e32e9d72ee7058f0b7faf197c2124e
Alex Pott <alex.a.pott@googlemail.com>
2024-03-25 08:42:46 +0000

    Issue #3269141 by ameymudras, longwave, ravi.shankar, sharayurajput, catch, alexpott: Remove deprecated Drupal\Core\StringTranslation\TranslationWrapper to TranslatableMarkup in Drupal 11

--- core/tests/Drupal/Tests/Core/StringTranslation/TranslationWrapperTest.php
+++ core/tests/Drupal/Tests/Core/StringTranslation/TranslationWrapperTest.php
@@ -9,7 +9,7 @@ use Drupal\Core\StringTranslation\TranslationWrapper;
 use Drupal\Tests\UnitTestCase;
 
 /**
- * Tests the TranslationWrapper class.
+ * Tests the TranslationWrapper backward compatibility layer.
  *
  * @coversDefaultClass \Drupal\Core\StringTranslation\TranslationWrapper
  * @group StringTranslation
@@ -18,11 +18,9 @@ class TranslationWrapperTest extends UnitTestCase {
 
   /**
    * @covers ::__construct
-   * @group legacy
    */
   public function testTranslationWrapper() {
-    $this->expectDeprecation('Drupal\Core\StringTranslation\TranslationWrapper is deprecated in drupal:8.0.0 and is removed from drupal:11.0.0. Use the \Drupal\Core\StringTranslation\TranslatableMarkup class instead. See https://www.drupal.org/node/2571255');
-    $object = new TranslationWrapper('Deprecated');
+    $object = new TranslationWrapper('Backward compatibility');
     $this->assertInstanceOf(TranslatableMarkup::class, $object);
   }
 
