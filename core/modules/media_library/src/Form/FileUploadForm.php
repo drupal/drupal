f4bf71d660a269b35881c131ef06136976446872
catch <catch@35733.no-reply.drupal.org>
2024-03-26 10:09:02 +0000

    Issue #3418781 by sukr_s, Charles Belov, smustgrave: UI refers to "files" when you can only upload one file

--- core/modules/media_library/src/Form/FileUploadForm.php
+++ core/modules/media_library/src/Form/FileUploadForm.php
@@ -165,7 +165,9 @@ class FileUploadForm extends AddFormBase {
       // @todo Move validation in https://www.drupal.org/node/2988215
       '#process' => array_merge(['::validateUploadElement'], $process, ['::processUploadElement']),
       '#upload_validators' => $item->getUploadValidators(),
-      '#multiple' => TRUE,
+      // Set multiple to true only if available slots is not exactly one
+      // to ensure correct language (singular or plural) in UI
+      '#multiple' => $slots != 1 ? TRUE : FALSE,
       // Do not limit the number uploaded. There is validation based on the
       // number selected in the media library that prevents overages.
       // @see Drupal\media_library\Form\AddFormBase::updateLibrary()
