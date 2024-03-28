6c2ed729900d36a845bb653a109f949b0a5157a9
Alex Pott <alex.a.pott@googlemail.com>
2024-03-28 09:29:18 +0000

    Issue #3364885 by andy-blum, mglaman, alexpott, mherchel, lauriii, smustgrave, isholgueras, Dave Reid, kostyashupenko, matthieuscarset, camoa, ctrlADel, fjgarlin, jwilson3: Allow themes to use a starterkit.yml file so it is easier to use the theme generator

--- core/themes/starterkit_theme/src/StarterKit.php
+++ core/themes/starterkit_theme/src/StarterKit.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\starterkit_theme;
 
-use Drupal\Component\Serialization\Yaml;
 use Drupal\Core\Theme\StarterKitInterface;
 
 final class StarterKit implements StarterKitInterface {
@@ -11,10 +10,12 @@ final class StarterKit implements StarterKitInterface {
    * {@inheritdoc}
    */
   public static function postProcess(string $working_dir, string $machine_name, string $theme_name): void {
-    $info_file = "$working_dir/$machine_name.info.yml";
-    $info = Yaml::decode(file_get_contents($info_file));
-    unset($info['hidden']);
-    file_put_contents($info_file, Yaml::encode($info));
+    $readme_file = "$working_dir/README.md";
+    try {
+      file_put_contents($readme_file, "$theme_name theme, generated from starterkit_theme. Additional information on generating themes can be found in the [Starterkit documentation](https://www.drupal.org/docs/core-modules-and-themes/core-themes/starterkit-theme).");
+    }
+    catch (\Throwable $th) {
+    }
   }
 
 }
