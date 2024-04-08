7e28ed514aeca58baba0be32b7b370ee64d66b5c
nod_ <nod_@598310.no-reply.drupal.org>
2024-04-08 10:50:38 +0200

    Issue #3419621 by dburiak, sijumpk, smustgrave, pameeela, alexpott: tablePositionSticky should not be called on a non-array variable

--- core/tests/Drupal/KernelTests/Core/Theme/ClaroTableTest.php
+++ core/tests/Drupal/KernelTests/Core/Theme/ClaroTableTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\KernelTests\Core\Theme;
 
+use Drupal\claro\ClaroPreRender;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
@@ -36,6 +37,37 @@ class ClaroTableTest extends KernelTestBase {
     // Confirm that position-sticky is used instead of sticky-enabled.
     $this->assertNoRaw('sticky-enabled');
     $this->assertRaw('position-sticky');
+  }
+
+  /**
+   * Confirm Claro prerender callback is not executed for non-array class.
+   */
+  public function testThemeTablePositionStickyPreRender(): void {
+    // Enable the Claro theme.
+    \Drupal::service('theme_installer')->install(['claro']);
+    $this->config('system.theme')->set('default', 'claro')->save();
+    $header = ['one', 'two', 'three'];
+    $rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    $table = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+      '#sticky' => TRUE,
+      '#attributes' => [
+        'class' => 'class',
+      ],
+      '#pre_render' => [
+        [
+          ClaroPreRender::class,
+          'tablePositionSticky',
+        ],
+      ],
+    ];
+
+    $renderedTable = \Drupal::service('renderer')->renderRoot($table);
+
+    // Confirm that table is rendered.
+    $this->assertStringContainsString('class="class"', $renderedTable);
   }
 
 }
