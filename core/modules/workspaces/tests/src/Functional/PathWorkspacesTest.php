6c1fc7e4d831aa6d71cf5757e50ddb63588e9bee
Alex Pott <alex.a.pott@googlemail.com>
2024-04-10 15:20:16 +0100

    Issue #3439922 by amateescu, vensires: Fix Workspaces tests that rely on UID1's super user behavior

--- core/modules/workspaces/tests/src/Functional/PathWorkspacesTest.php
+++ core/modules/workspaces/tests/src/Functional/PathWorkspacesTest.php
@@ -34,14 +34,6 @@ class PathWorkspacesTest extends BrowserTestBase {
 
   /**
    * {@inheritdoc}
-   *
-   * @todo Remove and fix test to not rely on super user.
-   * @see https://www.drupal.org/project/drupal/issues/3437620
-   */
-  protected bool $usesSuperUserAccessPolicy = TRUE;
-
-  /**
-   * {@inheritdoc}
    */
   protected $defaultTheme = 'stark';
 
@@ -60,7 +52,17 @@ class PathWorkspacesTest extends BrowserTestBase {
       'type' => 'article',
     ]);
 
-    $this->drupalLogin($this->rootUser);
+    $permissions = [
+      'administer languages',
+      'administer nodes',
+      'administer url aliases',
+      'administer workspaces',
+      'create article content',
+      'create content translations',
+      'edit any article content',
+      'translate any entity',
+    ];
+    $this->drupalLogin($this->drupalCreateUser($permissions));
 
     // Enable URL language detection and selection.
     $edit = ['language_interface[enabled][language-url]' => 1];
