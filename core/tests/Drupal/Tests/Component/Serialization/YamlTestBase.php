2e02b4c2cf0762ede8cf0dd4cfde9f8098a83961
Alex Pott <alex.a.pott@googlemail.com>
2024-04-21 19:57:02 +0100

    Issue #3442295 by mondrake: Change @dataprovider to static in YamlTestBase
    (cherry picked from commit 4cdc4313020b75c0355412aab62cbe447aed89ce)

--- core/tests/Drupal/Tests/Component/Serialization/YamlTestBase.php
+++ core/tests/Drupal/Tests/Component/Serialization/YamlTestBase.php
@@ -14,7 +14,7 @@ abstract class YamlTestBase extends TestCase {
   /**
    * Some data that should be able to be serialized.
    */
-  public function providerEncodeDecodeTests() {
+  public static function providerEncodeDecodeTests() {
     return [
       [
         'foo' => 'bar',
@@ -46,7 +46,7 @@ abstract class YamlTestBase extends TestCase {
   /**
    * Some data that should be able to be deserialized.
    */
-  public function providerDecodeTests() {
+  public static function providerDecodeTests() {
     $data = [
       // NULL files.
       ['', NULL],
@@ -74,10 +74,10 @@ jquery.ui.accordion:
     ];
 
     // 1.2 Bool values.
-    foreach ($this->providerBoolTest() as $test) {
+    foreach (static::providerBoolTest() as $test) {
       $data[] = ['bool: ' . $test[0], ['bool' => $test[1]]];
     }
-    $data = array_merge($data, $this->providerBoolTest());
+    $data = array_merge($data, static::providerBoolTest());
 
     return $data;
   }
@@ -85,7 +85,7 @@ jquery.ui.accordion:
   /**
    * Tests different boolean serialization and deserialization.
    */
-  public function providerBoolTest() {
+  public static function providerBoolTest() {
     return [
       ['true', TRUE],
       ['TRUE', TRUE],
