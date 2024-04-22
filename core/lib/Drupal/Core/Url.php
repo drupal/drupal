eceeb8f66945e59d430324aaeb0f95d5f4696d53
catch <catch@35733.no-reply.drupal.org>
2024-04-22 21:21:39 +0100

    Issue #3343153 by samit.310@gmail.com, andypost, smustgrave, alexpott: Remove deprecated code from \Drupal\Core\Url

--- core/lib/Drupal/Core/Url.php
+++ core/lib/Drupal/Core/Url.php
@@ -766,29 +766,6 @@ class Url implements TrustedCallbackInterface {
   }
 
   /**
-   * Returns the route information for a render array.
-   *
-   * @return array
-   *   An associative array suitable for a render array.
-   *
-   * @deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. There is no
-   *   replacement.
-   *
-   * @see https://www.drupal.org/node/3342977
-   */
-  public function toRenderArray() {
-    @trigger_error(__METHOD__ . '() is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. There is no replacement. See https://www.drupal.org/node/3342977', E_USER_DEPRECATED);
-    $render_array = [
-      '#url' => $this,
-      '#options' => $this->getOptions(),
-    ];
-    if (!$this->unrouted) {
-      $render_array['#access_callback'] = [self::class, 'renderAccess'];
-    }
-    return $render_array;
-  }
-
-  /**
    * Returns the internal path (system path) for this route.
    *
    * This path will not include any prefixes, fragments, or query strings.
@@ -832,25 +809,6 @@ class Url implements TrustedCallbackInterface {
       return $this->accessManager()->checkNamedRoute($this->getRouteName(), $this->getRouteParameters(), $account, $return_as_object);
     }
     return $return_as_object ? AccessResult::allowed() : TRUE;
-  }
-
-  /**
-   * Checks a URL render element against applicable access check services.
-   *
-   * @param array $element
-   *   A render element as returned from \Drupal\Core\Url::toRenderArray().
-   *
-   * @return bool
-   *   Returns TRUE if the current user has access to the URL, otherwise FALSE.
-   *
-   * @deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. There is no
-   *   replacement.
-   *
-   * @see https://www.drupal.org/node/3342977
-   */
-  public static function renderAccess(array $element) {
-    @trigger_error(__METHOD__ . '() is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. There is no replacement. See https://www.drupal.org/node/3342977', E_USER_DEPRECATED);
-    return $element['#url']->access();
   }
 
   /**
@@ -920,8 +878,7 @@ class Url implements TrustedCallbackInterface {
    * {@inheritdoc}
    */
   public static function trustedCallbacks() {
-    // @todo Clean-up in https://www.drupal.org/i/3343153
-    return ['renderAccess'];
+    return [];
   }
 
 }
--- core/tests/Drupal/Tests/Core/UrlTest.php
+++ core/tests/Drupal/Tests/Core/UrlTest.php
@@ -498,38 +498,6 @@ class UrlTest extends UnitTestCase {
   }
 
   /**
-   * Tests the renderAccess() method.
-   *
-   * @param bool $access
-   *   The access value.
-   *
-   * @covers ::renderAccess
-   * @dataProvider accessProvider
-   * @group legacy
-   */
-  public function testRenderAccess($access) {
-    $element = [
-      '#url' => Url::fromRoute('entity.node.canonical', ['node' => 3]),
-    ];
-    $this->container->set('current_user', $this->createMock('Drupal\Core\Session\AccountInterface'));
-    $this->container->set('access_manager', $this->getMockAccessManager($access));
-    $this->expectDeprecation('Drupal\Core\Url::renderAccess() is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. There is no replacement. See https://www.drupal.org/node/3342977');
-    $this->assertEquals($access, TestUrl::renderAccess($element));
-  }
-
-  /**
-   * Tests deprecation of toRenderArray() method.
-   *
-   * @covers ::toRenderArray
-   * @group legacy
-   */
-  public function testToRenderArray() {
-    $this->expectDeprecation('Drupal\Core\Url::toRenderArray() is deprecated in drupal:10.1.0 and is removed from drupal:11.0.0. There is no replacement. See https://www.drupal.org/node/3342977');
-    $url = Url::fromRoute('entity.node.canonical', ['node' => 3]);
-    $this->assertIsArray($url->toRenderArray());
-  }
-
-  /**
    * Tests the fromRouteMatch() method.
    */
   public function testFromRouteMatch() {
