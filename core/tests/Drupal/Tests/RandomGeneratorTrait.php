3eb086bb694dc7900afac3341ce4a56a2b8c29f3
Dave Long <dave@longwaveconsulting.com>
2024-04-23 22:03:01 +0100

    Issue #3442763 by smustgrave: Remove deprecated code in RandomGeneratorTrait.php

--- core/tests/Drupal/Tests/RandomGeneratorTrait.php
+++ core/tests/Drupal/Tests/RandomGeneratorTrait.php
@@ -34,28 +34,6 @@ trait RandomGeneratorTrait {
   }
 
   /**
-   * Callback for random string validation.
-   *
-   * @see \Drupal\Component\Utility\Random::string()
-   *
-   * @param string $string
-   *   The random string to validate.
-   *
-   * @return bool
-   *   TRUE if the random string is valid, FALSE if not.
-   *
-   * @deprecated in drupal:10.2.0 and is removed from drupal:11.0.0.
-   *   Use \Drupal\TestTools\Random::stringValidate() instead.
-   *
-   * @see https://www.drupal.org/node/3358389
-   */
-  public function randomStringValidate($string) {
-    @trigger_error(__METHOD__ . "() is deprecated in drupal:10.2.0 and is removed from drupal:11.0.0. Use \Drupal\TestTools\Random::stringValidate() instead. See https://www.drupal.org/node/3358389", E_USER_DEPRECATED);
-
-    return Random::stringValidate($string);
-  }
-
-  /**
    * Generates a unique random string containing letters and numbers.
    *
    * Do not use this method when testing non validated user input. Instead, use
