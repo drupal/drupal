fecffbffe878c231c2688a7d29bcb144c5a9c254
quietone <quietone@2572884.no-reply.drupal.org>
2024-04-25 14:04:35 +1200

    Issue #3442162 by catch, smustgrave: Remove redundant hook_ENTITY_TYPE_presave() and ViewsConfigUpdater methods

--- core/modules/responsive_image/src/Entity/ResponsiveImageStyle.php
+++ core/modules/responsive_image/src/Entity/ResponsiveImageStyle.php
@@ -3,9 +3,7 @@
 namespace Drupal\responsive_image\Entity;
 
 use Drupal\Core\Config\Entity\ConfigEntityBase;
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\image\Entity\ImageStyle;
-use Drupal\responsive_image\ResponsiveImageConfigUpdater;
 use Drupal\responsive_image\ResponsiveImageStyleInterface;
 
 /**
@@ -110,15 +108,6 @@ class ResponsiveImageStyle extends ConfigEntityBase implements ResponsiveImageSt
    */
   public function __construct(array $values, $entity_type_id = 'responsive_image_style') {
     parent::__construct($values, $entity_type_id);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function preSave(EntityStorageInterface $storage) {
-    parent::preSave($storage);
-    $config_updater = \Drupal::classResolver(ResponsiveImageConfigUpdater::class);
-    $config_updater->orderMultipliersNumerically($this);
   }
 
   /**
--- core/modules/views/tests/src/Kernel/ViewsConfigUpdaterTest.php
+++ core/modules/views/tests/src/Kernel/ViewsConfigUpdaterTest.php
@@ -5,11 +5,6 @@ declare(strict_types=1);
 namespace Drupal\Tests\views\Kernel;
 
 use Drupal\Core\Config\FileStorage;
-use Drupal\field\Entity\FieldConfig;
-use Drupal\field\Entity\FieldStorageConfig;
-use Drupal\responsive_image\Entity\ResponsiveImageStyle;
-use Drupal\Tests\responsive_image\Functional\ViewsIntegrationTest;
-use Drupal\views\ViewsConfigUpdater;
 
 /**
  * @coversDefaultClass \Drupal\views\ViewsConfigUpdater
@@ -25,88 +20,16 @@ class ViewsConfigUpdaterTest extends ViewsKernelTestBase {
   protected static $modules = [
     'views_config_entity_test',
     'entity_test',
-    'breakpoint',
     'field',
-    'file',
-    'image',
-    'responsive_image',
-    'responsive_image_test_module',
   ];
 
   /**
-   * @covers ::needsResponsiveImageLazyLoadFieldUpdate
+   * Tests ViewsConfigUpdater.
    */
-  public function testNeedsResponsiveImageLazyLoadFieldUpdate(): void {
-    $config_updater = $this->container
-      ->get('class_resolver')
-      ->getInstanceFromDefinition(ViewsConfigUpdater::class);
-    assert($config_updater instanceof ViewsConfigUpdater);
-
-    FieldStorageConfig::create([
-      'field_name' => 'user_picture',
-      'entity_type' => 'user',
-      'type' => 'image',
-    ])->save();
-    FieldConfig::create([
-      'entity_type' => 'user',
-      'field_name' => 'user_picture',
-      'file_directory' => 'pictures/[date:custom:Y]-[date:custom:m]',
-      'bundle' => 'user',
-    ])->save();
-
-    // Create a responsive image style.
-    ResponsiveImageStyle::create([
-      'id' => ViewsIntegrationTest::RESPONSIVE_IMAGE_STYLE_ID,
-      'label' => 'Foo',
-      'breakpoint_group' => 'responsive_image_test_module',
-    ]);
-    // Create an image field to be used with a responsive image formatter.
-    FieldStorageConfig::create([
-      'type' => 'image',
-      'entity_type' => 'entity_test',
-      'field_name' => 'bar',
-    ])->save();
-    FieldConfig::create([
-      'entity_type' => 'entity_test',
-      'bundle' => 'entity_test',
-      'field_name' => 'bar',
-    ])->save();
-
-    $test_view = $this->loadTestView('views.view.test_responsive_images');
-    $needs_update = $config_updater->needsResponsiveImageLazyLoadFieldUpdate($test_view);
-    $test_view->save();
-    $this->assertTrue($needs_update);
-
-    $default_display = $test_view->getDisplay('default');
-    self::assertEquals('eager', $default_display['display_options']['fields']['bar']['settings']['image_loading']['attribute']);
-  }
-
-  /**
-   * @covers ::needsRenderedEntityFieldUpdate
-   */
-  public function testNeedsRenderedEntityFieldUpdate(): void {
-    $config_updater = $this->container
-      ->get('class_resolver')
-      ->getInstanceFromDefinition(ViewsConfigUpdater::class);
-    assert($config_updater instanceof ViewsConfigUpdater);
-    $test_view = $this->loadTestView('views.view.test_entity_field_renderered_entity');
-    $needs_update = $config_updater->needsRenderedEntityFieldUpdate($test_view);
-    $test_view->save();
-    $this->assertTrue($needs_update);
-
-    $displays = [
-      'default',
-      'page_1',
-      'page_2',
-      'page_3',
-      'page_4',
-      'page_5',
-      'page_6',
-    ];
-    foreach ($displays as $display) {
-      $display = $test_view->getDisplay($display);
-      self::assertEmpty($display['cache_metadata']['tags']);
-    }
+  public function testViewsConfigUpdater(): void {
+    // ViewsConfigUpdater currently contains no actual configuration update
+    // logic. Replace this method with a real test when it does.
+    $this->markTestSkipped();
   }
 
   /**
