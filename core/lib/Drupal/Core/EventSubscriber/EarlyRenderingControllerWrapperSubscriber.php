bd2add4c456275f7dc4201212e3a6b231c282279
catch <catch@35733.no-reply.drupal.org>
2024-04-29 10:02:50 +0100

    Issue #2638686 by solideogloria, garphy, loziju, smustgrave, roderik, Berdir, dww, catch, Wim Leers: Correctly handle cache data instead of throwing an Exception in EarlyRenderingControllerWrapperSubscriber()
    (cherry picked from commit c0d8b4bb9f382280a07b966fd0363c2613844815)

--- core/lib/Drupal/Core/EventSubscriber/EarlyRenderingControllerWrapperSubscriber.php
+++ core/lib/Drupal/Core/EventSubscriber/EarlyRenderingControllerWrapperSubscriber.php
@@ -146,11 +146,14 @@ class EarlyRenderingControllerWrapperSubscriber implements EventSubscriberInterf
           $response->addCacheableDependency($early_rendering_bubbleable_metadata);
         }
       }
+      elseif ($response instanceof CacheableResponseInterface) {
+        $response->addCacheableDependency($early_rendering_bubbleable_metadata);
+      }
       // If a non-Ajax Response or domain object is returned and it cares about
       // attachments or cacheability, then throw an exception: early rendering
       // is not permitted in that case. It is the developer's responsibility
       // to not use early rendering.
-      elseif ($response instanceof AttachmentsInterface || $response instanceof CacheableResponseInterface || $response instanceof CacheableDependencyInterface) {
+      elseif ($response instanceof AttachmentsInterface || $response instanceof CacheableDependencyInterface) {
         throw new \LogicException(sprintf('The controller result claims to be providing relevant cache metadata, but leaked metadata was detected. Ensure you are not rendering content too early. Returned object class: %s.', get_class($response)));
       }
       else {
