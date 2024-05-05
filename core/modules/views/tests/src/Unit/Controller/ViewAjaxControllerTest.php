3e486f783a9e95ebe4264bfd26f172ddfe8e4fcb
Alex Pott <alex.a.pott@googlemail.com>
2024-05-05 21:50:14 +0100

    Issue #3441920 by scott_euser, catch, smustgrave, quietone: Support #attached settings in ViewAjaxController

--- core/modules/views/tests/src/Unit/Controller/ViewAjaxControllerTest.php
+++ core/modules/views/tests/src/Unit/Controller/ViewAjaxControllerTest.php
@@ -209,6 +209,14 @@ class ViewAjaxControllerTest extends UnitTestCase {
     $this->assertSame($response->getView(), $executable);
 
     $this->assertViewResultCommand($response);
+
+    // Test that the ajax controller for Views contains the
+    // Drupal Settings.
+    $this->assertEquals([
+      'drupalSettings' => [
+        'testSetting' => ['Setting'],
+      ],
+    ], $response->getAttachments());
   }
 
   /**
@@ -389,7 +397,14 @@ class ViewAjaxControllerTest extends UnitTestCase {
       ->willReturn(TRUE);
     $executable->expects($this->atMost(1))
       ->method('preview')
-      ->willReturn(['#markup' => 'View result']);
+      ->willReturn([
+        '#markup' => 'View result',
+        '#attached' => [
+          'drupalSettings' => [
+            'testSetting' => ['Setting'],
+          ],
+        ],
+      ]);
 
     $this->executableFactory->expects($this->once())
       ->method('get')
