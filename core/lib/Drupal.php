dcabf3377db66fd95f1e8dd795562975bc38e398
quietone <quietone@2572884.no-reply.drupal.org>
2024-05-14 18:23:15 +1200

    Issue #3446936 by xjm, longwave: Recommend PHP 8.3 in Drupal 10.3
    (cherry picked from commit 28035aa52a88000373d5b74562fad2e766137e08)

--- core/lib/Drupal.php
+++ core/lib/Drupal.php
@@ -131,7 +131,7 @@ class Drupal {
    * message, but Drupal can still be installed. Used for (e.g.) PHP versions
    * that have reached their EOL or will in the near future.
    */
-  const RECOMMENDED_PHP = '8.2.0';
+  const RECOMMENDED_PHP = '8.3.0';
 
   /**
    * The currently active container object, or NULL if not initialized yet.
