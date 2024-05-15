2c3c957208e442e59e6d232ae62697cdb467a707
Dave Long <dave@longwaveconsulting.com>
2024-05-15 16:45:49 +0100

    Issue #3447210 by alexpott, jnicola: Move RecipeDiscovery into RecipeConfigurator
    (cherry picked from commit 2e3e874662237ad38bd2006b3c8126a458d7cb20)

--- core/tests/Drupal/KernelTests/Core/Recipe/RecipeConfiguratorTest.php
+++ core/tests/Drupal/KernelTests/Core/Recipe/RecipeConfiguratorTest.php
@@ -6,20 +6,19 @@ namespace Drupal\KernelTests\Core\Recipe;
 
 use Drupal\Core\Recipe\Recipe;
 use Drupal\Core\Recipe\RecipeConfigurator;
-use Drupal\Core\Recipe\RecipeDiscovery;
+use Drupal\Core\Recipe\UnknownRecipeException;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
- * @covers \Drupal\Core\Recipe\RecipeConfigurator
+ * @coversDefaultClass \Drupal\Core\Recipe\RecipeConfigurator
  * @group Recipe
  */
 class RecipeConfiguratorTest extends KernelTestBase {
 
   public function testRecipeConfigurator(): void {
-    $discovery = new RecipeDiscovery('core/tests/fixtures/recipes');
     $recipe_configurator = new RecipeConfigurator(
       ['install_two_modules', 'install_node_with_config', 'recipe_include'],
-      $discovery
+      'core/tests/fixtures/recipes'
     );
     // Private method "listAllRecipes".
     $reflection = new \ReflectionMethod('\Drupal\Core\Recipe\RecipeConfigurator', 'listAllRecipes');
@@ -48,6 +47,39 @@ class RecipeConfiguratorTest extends KernelTestBase {
     $this->assertEquals($expected_recipe_extensions, $recipe_extensions);
     $this->assertEquals(1, array_count_values($recipes_names)['Install node with config']);
     $this->assertEquals(1, array_count_values($recipe_extensions)['field']);
+  }
+
+  /**
+   * Tests that RecipeConfigurator can load recipes.
+   *
+   * @testWith ["install_two_modules", "Install two modules"]
+   *           ["recipe_include", "Recipe include"]
+   *
+   * @covers ::getIncludedRecipe
+   */
+  public function testIncludedRecipeLoader(string $recipe, string $name): void {
+    $recipe = RecipeConfigurator::getIncludedRecipe('core/tests/fixtures/recipes', $recipe);
+    $this->assertSame($name, $recipe->name);
+  }
+
+  /**
+   * Tests exception thrown when RecipeConfigurator cannot find a recipe.
+   *
+   * @testWith ["no_recipe"]
+   *           ["does_not_exist"]
+   *
+   * @covers ::getIncludedRecipe
+   */
+  public function testIncludedRecipeLoaderException(string $recipe): void {
+    try {
+      RecipeConfigurator::getIncludedRecipe('core/tests/fixtures/recipes', $recipe);
+      $this->fail('Expected exception not thrown');
+    }
+    catch (UnknownRecipeException $e) {
+      $this->assertSame($recipe, $e->recipe);
+      $this->assertSame('core/tests/fixtures/recipes', $e->searchPath);
+      $this->assertSame('Can not find the ' . $recipe . ' recipe, search path: ' . $e->searchPath, $e->getMessage());
+    }
   }
 
 }
