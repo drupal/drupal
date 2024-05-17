0a4f122fcbf096ea32556e268aa8de21b0d015c1
Dave Long <dave@longwaveconsulting.com>
2024-05-17 10:28:17 +0100

    Issue #3439591 by quietone, mstrelan, pradhumanjain2311, andypost, catch, mfb, longwave, coaston, penyaskito: Add composer dependency on ext-zlib

--- core/lib/Drupal/Component/Utility/UrlHelper.php
+++ core/lib/Drupal/Component/Utility/UrlHelper.php
@@ -80,9 +80,6 @@ class UrlHelper {
    *   The data compressed into a URL-safe string.
    */
   public static function compressQueryParameter(string $data): string {
-    if (!\extension_loaded('zlib')) {
-      return $data;
-    }
     // Use 'base64url' encoding. Note that the '=' sign is only used for padding
     // on the right of the string, and is otherwise not part of the data.
     // @see https://datatracker.ietf.org/doc/html/rfc4648#section-5
@@ -104,9 +101,6 @@ class UrlHelper {
    *   uncompressed.
    */
   public static function uncompressQueryParameter(string $compressed): string {
-    if (!\extension_loaded('zlib')) {
-      return $compressed;
-    }
     // Because this comes from user data, suppress the PHP warning that
     // gzcompress() throws if the base64-encoded string is invalid.
     $return = @gzuncompress(base64_decode(str_replace(['-', '_'], ['+', '/'], $compressed)));
--- core/lib/Drupal/Core/Asset/AssetDumper.php
+++ core/lib/Drupal/Core/Asset/AssetDumper.php
@@ -60,15 +60,14 @@ class AssetDumper implements AssetDumperUriInterface {
     catch (FileException $e) {
       return FALSE;
     }
-    // If CSS/JS gzip compression is enabled and the zlib extension is available
-    // then create a gzipped version of this file. This file is served
-    // conditionally to browsers that accept gzip using .htaccess rules.
-    // It's possible that the rewrite rules in .htaccess aren't working on this
-    // server, but there's no harm (other than the time spent generating the
-    // file) in generating the file anyway. Sites on servers where rewrite rules
-    // aren't working can set css.gzip to FALSE in order to skip
-    // generating a file that won't be used.
-    if (extension_loaded('zlib') && \Drupal::config('system.performance')->get($file_extension . '.gzip')) {
+    // If CSS/JS gzip compression is enabled then create a gzipped version of
+    // this file. This file is served conditionally to browsers that accept gzip
+    // using .htaccess rules. It's possible that the rewrite rules in .htaccess
+    // aren't working on this server, but there's no harm (other than the time
+    // spent generating the file) in generating the file anyway. Sites on
+    // servers where rewrite rules aren't working can set css.gzip to FALSE in
+    // order to skip generating a file that won't be used.
+    if (\Drupal::config('system.performance')->get($file_extension . '.gzip')) {
       try {
         if (!file_exists($uri . '.gz') && !$this->fileSystem->saveData(gzencode($data, 9, FORCE_GZIP), $uri . '.gz', FileExists::Replace)) {
           return FALSE;
