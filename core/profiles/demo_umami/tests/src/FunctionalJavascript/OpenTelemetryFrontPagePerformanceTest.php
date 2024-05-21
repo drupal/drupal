b6026c121501c100049e2da499a3b2756d26a128
nod_ <nod_@598310.no-reply.drupal.org>
2024-05-21 16:14:24 +0200

    Issue #3425104 by finnsky, catch, smustgrave: Umami views should use responsive grid
    (cherry picked from commit 587350c208b66027011f3fdaea8ebaf13714a7de)

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryFrontPagePerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryFrontPagePerformanceTest.php
@@ -67,7 +67,7 @@ class OpenTelemetryFrontPagePerformanceTest extends PerformanceTestBase {
     $this->assertSame(1, $performance_data->getScriptCount());
     $this->assertLessThan(7500, $performance_data->getScriptBytes());
     $this->assertSame(2, $performance_data->getStylesheetCount());
-    $this->assertLessThan(42000, $performance_data->getStylesheetBytes());
+    $this->assertLessThan(40400, $performance_data->getStylesheetBytes());
   }
 
   /**
