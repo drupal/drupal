142c8a675eaee26aafbdbb9ac823a70a9b7b7b0e
Alex Pott <alex.a.pott@googlemail.com>
2024-05-21 11:25:23 +0100

    Issue #3448487 by catch: CoreRecipesTest is slow
    (cherry picked from commit 0ad1db0b2f0a5549d02d3a55501952613dc4b45d)

--- core/tests/Drupal/FunctionalTests/Core/Recipe/CoreRecipesTest.php
+++ core/tests/Drupal/FunctionalTests/Core/Recipe/CoreRecipesTest.php
@@ -11,6 +11,7 @@ use Symfony\Component\Finder\Finder;
  * Tests applying all core-provided recipes on top of the Empty profile.
  *
  * @group Recipe
+ * @group #slow
  */
 class CoreRecipesTest extends BrowserTestBase {
 
