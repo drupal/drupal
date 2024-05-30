8b19e42873d65a07adf8bb84ff1c482d946f952f
catch <catch@35733.no-reply.drupal.org>
2024-05-30 10:41:23 +0100

    Issue #3085360 by bradjones1, josephdpurcell, Giuseppe87, ravi.shankar, rajandro, ridhimaabrol24, bbrala, andregp, jhedstrom: RouteProvider::getRouteCollectionForRequest() can poison query string of next request

--- core/modules/system/tests/modules/router_test_directory/src/RouterTestServiceProvider.php
+++ core/modules/system/tests/modules/router_test_directory/src/RouterTestServiceProvider.php
@@ -14,9 +14,12 @@ class RouterTestServiceProvider implements ServiceProviderInterface {
    * {@inheritdoc}
    */
   public function register(ContainerBuilder $container) {
-    $container->register('router_test.subscriber', 'Drupal\router_test\RouteTestSubscriber')->addTag('event_subscriber');
+    $container->register('router_test.subscriber', 'Drupal\router_test\RouteTestSubscriber')
+      ->addTag('event_subscriber');
     $container->register('access_check.router_test', 'Drupal\router_test\Access\TestAccessCheck')
       ->addTag('access_check', ['applies_to' => '_access_router_test']);
+    $container->register('router_test.early_exception.subscriber', 'Drupal\router_test\RouterTestEarlyExceptionSubscriber')
+      ->addTag('event_subscriber');
   }
 
 }
