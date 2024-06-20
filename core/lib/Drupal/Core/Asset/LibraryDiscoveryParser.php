4074145ef78d6d60d82184149958b86585cbd87a
nod_ <nod_@598310.no-reply.drupal.org>
2024-06-20 23:55:31 +0200

    Issue #3455513 by xjm, e0ipso: "Single-Directory Components" needs to be hyphenated because of English grammar and content style rules
    (cherry picked from commit 1a487f3b7cb69d2355afacd30220e6c3190797d4)

--- core/lib/Drupal/Core/Asset/LibraryDiscoveryParser.php
+++ core/lib/Drupal/Core/Asset/LibraryDiscoveryParser.php
@@ -399,7 +399,7 @@ class LibraryDiscoveryParser {
       }
     }
     // Core also provides additional libraries that don't come from the YAML,
-    // file nor the hook_library_info_build. They come from single directory
+    // file nor the hook_library_info_build. They come from single-directory
     // component definitions.
     $additional_libraries = $extension === 'core'
       ? $this->librariesForComponents()
@@ -474,10 +474,10 @@ class LibraryDiscoveryParser {
   }
 
   /**
-   * Builds the dynamic library definitions for single directory components.
+   * Builds the dynamic library definitions for single-directory components.
    *
    * @return array
-   *   The core library definitions for Single Directory Components.
+   *   The core library definitions for Single-Directory Components.
    */
   protected function librariesForComponents(): array {
     // Iterate over all the components to get the CSS and JS files.
