f271302c0bc1de656721dd594851b5b787f74dbc
catch <catch@35733.no-reply.drupal.org>
2024-06-27 10:55:33 +0100

    Issue #3155411 by quietone, smustgrave, jungle, longwave: Add "writeable" to the "flagWords" in core/.cspell.json
    (cherry picked from commit c3e68680adbf0c6ce585bf32c948c80d5f90975a)

--- core/tests/Drupal/Tests/StreamCapturer.php
+++ core/tests/Drupal/Tests/StreamCapturer.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Drupal\Tests;
 
+// cspell:ignore datalen PSFS_FEED_ME writeable
+
 /**
  * Captures output to a stream and stores it for retrieval.
  */
@@ -14,11 +16,9 @@ class StreamCapturer extends \php_user_filter {
   public function filter($in, $out, &$consumed, $closing): int {
     while ($bucket = stream_bucket_make_writeable($in)) {
       self::$cache .= $bucket->data;
-      // cSpell:disable-next-line
       $consumed += $bucket->datalen;
       stream_bucket_append($out, $bucket);
     }
-    // cSpell:disable-next-line
     return PSFS_FEED_ME;
   }
 
