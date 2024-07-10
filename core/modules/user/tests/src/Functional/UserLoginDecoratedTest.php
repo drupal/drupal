8f08ecc0e1ed68170a12e2422782209383e4731c
Alex Pott <alex.a.pott@googlemail.com>
2024-07-10 08:41:14 +0100

    Issue #3456738 by cmlara, Anybody, andrewbelcher, Berdir, catch: BC break in login auth changes from #3444978
    (cherry picked from commit e015f80827e2a92da1a1cd7f13bd7e47b453b956)

---
+++ core/modules/user/tests/src/Functional/UserLoginDecoratedTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\user\Functional;
+
+use Drupal\user_auth_decorator_test\UserAuthDecorator;
+
+/**
+ * Ensure that login works as expected with a decorator.
+ *
+ * The decorator does not implement UserAuthenticationInterface.
+ *
+ * @group user
+ */
+class UserLoginDecoratedTest extends UserLoginTest {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  protected static $modules = ['user_auth_decorator_test'];
+
+  /**
+   * Test that the UserAuthDecorator is providing user.auth.
+   */
+  public function testServiceDecorated(): void {
+    $service = \Drupal::service('user.auth');
+    $this->assertInstanceOf(UserAuthDecorator::class, $service);
+  }
+
+}
