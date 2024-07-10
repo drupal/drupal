1602eb31805280cf2404a051fd42ebf55ca51807
catch <6915-catch@users.noreply.drupalcode.org>
2024-07-10 13:57:20 +0100

    Issue #2408549 by alexpott, narendraR, yash.rode, kunal.sachdev, lauriii, Liam Morland, Wim Leers, Hardik_Patel_12, jofitz, DamienMcKenna, eiriksm, andypost, jenlampton, Gábor Hojtsy, swentel, borisson_, jhedstrom, snehi, Elijah Lynn, narendra.rajwar27, Shubham Chandra, smustgrave, sime, AaronMcHale, Chi, karolus, rkoller, joshua.boltz, anavarre, colan, frob, Berdir, bircher, minnur, effulgentsia, quietone, catch, xjm, hanoii, benjifisher, worldlinemine, larowlan, longwave, simohell, shaal, worldlinemine: Display status message on configuration forms when there are overridden values

--- core/modules/config/tests/src/Functional/ConfigFormOverrideTest.php
+++ core/modules/config/tests/src/Functional/ConfigFormOverrideTest.php
@@ -15,6 +15,18 @@ use Drupal\Tests\BrowserTestBase;
 class ConfigFormOverrideTest extends BrowserTestBase {
 
   /**
+   * Message text that appears when forms have values for overridden config.
+   */
+  private const OVERRIDE_TEXT = 'These values are overridden. Changes on this form will be saved, but overrides will take precedence. See configuration overrides documentation for more information.';
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = ['update', 'config_override_message_test'];
+
+  /**
    * {@inheritdoc}
    */
   protected $defaultTheme = 'stark';
@@ -26,31 +38,61 @@ class ConfigFormOverrideTest extends BrowserTestBase {
     $this->drupalLogin($this->drupalCreateUser([
       'access administration pages',
       'administer site configuration',
+      'link to any page',
     ]));
 
-    $overridden_name = 'Site name global conf override';
-
-    // Set up an override.
-    $settings['config']['system.site']['name'] = (object) [
-      'value' => $overridden_name,
+    // Set up an overrides for configuration that is present in the form.
+    $settings['config']['system.site']['weight_select_max'] = (object) [
+      'value' => 200,
       'required' => TRUE,
     ];
     $this->writeSettings($settings);
 
-    // Test that everything on the form is the same, but that the override
-    // worked for the actual site name.
+    // Test that although system.site has an overridden key no override
+    // information is displayed because there is no corresponding form field.
     $this->drupalGet('admin/config/system/site-information');
-    $this->assertSession()->titleEquals('Basic site settings | ' . $overridden_name);
     $this->assertSession()->fieldValueEquals("site_name", 'Drupal');
+    $this->assertSession()->pageTextNotContains(self::OVERRIDE_TEXT);
 
-    // Submit the form and ensure the site name is not changed.
-    $edit = [
-      'site_name' => 'Custom site name',
+    // Set up an overrides for configuration that is present in the form.
+    $overridden_name = 'Site name global conf override';
+    $settings['config']['system.site']['name'] = (object) [
+      'value' => $overridden_name,
+      'required' => TRUE,
     ];
+    $settings['config']['update.settings']['notification']['emails'] = (object) [
+      'value' => [
+        0 => 'a@abc.com',
+        1 => 'admin@example.com',
+      ],
+      'required' => TRUE,
+    ];
+    $this->writeSettings($settings);
     $this->drupalGet('admin/config/system/site-information');
-    $this->submitForm($edit, 'Save configuration');
     $this->assertSession()->titleEquals('Basic site settings | ' . $overridden_name);
-    $this->assertSession()->fieldValueEquals("site_name", $edit['site_name']);
+    $this->assertSession()->elementTextContains('css', 'div[data-drupal-messages]', self::OVERRIDE_TEXT);
+    // Ensure the configuration overrides message is at the top of the form.
+    $this->assertSession()->elementExists('css', 'div[data-drupal-messages] + details#edit-site-information');
+    $this->assertSession()->elementContains('css', 'div[data-drupal-messages]', '<a href="#edit-site-name" title="\'Site name\' form element">Site name</a>');
+    $this->assertSession()->fieldValueEquals("site_name", 'Drupal');
+    $this->submitForm([
+      'site_name' => 'Custom site name',
+    ], 'Save configuration');
+    $this->assertSession()->titleEquals('Basic site settings | ' . $overridden_name);
+    $this->assertSession()->fieldValueEquals("site_name", 'Custom site name');
+
+    // Ensure it works for sequence.
+    $this->drupalGet('admin/reports/updates/settings');
+    $this->submitForm([], 'Save configuration');
+    $this->assertSession()->pageTextContainsOnce(self::OVERRIDE_TEXT);
+    // There are two status messages on the page due to the save.
+    $messages = $this->getSession()->getPage()->findAll('css', 'div[data-drupal-messages]');
+    $this->assertCount(2, $messages);
+    $this->assertStringContainsString('The configuration options have been saved.', $messages[0]->getText());
+    $this->assertTrue(
+      $messages[1]->hasLink('Email addresses to notify when updates are available'),
+      "Link to 'Email addresses to notify when updates are available' exists"
+    );
   }
 
 }
