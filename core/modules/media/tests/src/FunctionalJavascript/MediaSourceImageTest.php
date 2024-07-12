bcdb2a7c7d8e14cb9162dac8c5b3b33e7973cd1c
catch <6915-catch@users.noreply.drupalcode.org>
2024-07-13 00:30:17 +0100

    Issue #3376942 by fromme, Kingdutch, smustgrave: Replace usage of generateString with generate in Media module
    (cherry picked from commit 01b55ef51357df4f2ea26f091668758d0d3bd091)

--- core/modules/media/tests/src/FunctionalJavascript/MediaSourceImageTest.php
+++ core/modules/media/tests/src/FunctionalJavascript/MediaSourceImageTest.php
@@ -79,7 +79,7 @@ class MediaSourceImageTest extends MediaSourceTestBase {
     $image_element = $field->find('css', 'img');
     /** @var \Drupal\Core\File\FileUrlGeneratorInterface $file_url_generator */
     $file_url_generator = \Drupal::service('file_url_generator');
-    $expected_image_src = $file_url_generator->generateString(\Drupal::token()->replace('public://styles/large/public/[date:custom:Y]-[date:custom:m]/example_1.jpeg'));
+    $expected_image_src = $file_url_generator->generate(\Drupal::token()->replace('public://styles/large/public/[date:custom:Y]-[date:custom:m]/example_1.jpeg'))->toString();
     $this->assertStringContainsString($expected_image_src, $image_element->getAttribute('src'));
     $assert_session->elementNotExists('css', 'a', $field);
 
