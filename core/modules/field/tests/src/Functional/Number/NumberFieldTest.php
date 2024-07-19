9320092df7ee01153588a3acebbd77c27da53fb6
Dave Long <dave@longwaveconsulting.com>
2024-07-19 20:48:04 +0100

    Issue #3462556 by catch: Consolidate two test methods in NumberFieldTest
    (cherry picked from commit 63bc78c2d4052312df8ed486cc1689a8c3311d33)

--- core/modules/field/tests/src/Functional/Number/NumberFieldTest.php
+++ core/modules/field/tests/src/Functional/Number/NumberFieldTest.php
@@ -13,7 +13,6 @@ use Drupal\Tests\BrowserTestBase;
  * Tests the creation of numeric fields.
  *
  * @group field
- * @group #slow
  */
 class NumberFieldTest extends BrowserTestBase {
 
@@ -393,9 +392,9 @@ class NumberFieldTest extends BrowserTestBase {
   }
 
   /**
-   * Tests setting the minimum value of a float field through the interface.
+   * Tests setting minimum values through the interface.
    */
-  public function testCreateNumberFloatField(): void {
+  public function testMinimumValues(): void {
     // Create a float field.
     $field_name = $this->randomMachineName();
     FieldStorageConfig::create([
@@ -415,12 +414,7 @@ class NumberFieldTest extends BrowserTestBase {
     $this->assertSetMinimumValue($field, 0.0001);
     // Set the minimum value to an integer value.
     $this->assertSetMinimumValue($field, 1);
-  }
 
-  /**
-   * Tests setting the minimum value of a decimal field through the interface.
-   */
-  public function testCreateNumberDecimalField(): void {
     // Create a decimal field.
     $field_name = $this->randomMachineName();
     FieldStorageConfig::create([
