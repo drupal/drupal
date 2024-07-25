d890962445bff6751a74577e062a0d74fd0f0bbf
nod_ <nod_@598310.no-reply.drupal.org>
2024-07-25 21:28:35 +0200

    Issue #3308432 by devkinetic, BramDriesen, Shifali Baghel, cilefen, smustgrave, quietone, bnjmnm: The link on the Image tag is redirecting to an undefined page from the node preview screen
    (cherry picked from commit bd6abf7c4d6e5f4e92f0d8b63284844b7c8e9765)

--- core/modules/node/tests/src/FunctionalJavascript/NodePreviewLinkTest.php
+++ core/modules/node/tests/src/FunctionalJavascript/NodePreviewLinkTest.php
@@ -54,12 +54,26 @@ class NodePreviewLinkTest extends WebDriverTestBase {
     $assertSession = $this->assertSession();
     $this->drupalGet('node/add/test');
     $this->submitForm([
-      'title[0][value]' => 'Test node',
-      'body[0][value]' => '<a href="#foo">Anchor link</a><a href="/foo">Normal link</a>',
+      'title[0][value]' => 'Test anchor link',
+      'body[0][value]' => '<a href="#foo">Anchor link</a>',
     ], 'Preview');
     $this->clickLink('Anchor link');
     $assertSession->pageTextNotContains('Leave preview?');
+    $this->drupalGet('node/add/test');
+    $this->submitForm([
+      'title[0][value]' => 'Test normal link',
+      'body[0][value]' => '<a href="/foo">Normal link</a>',
+    ], 'Preview');
     $this->clickLink('Normal link');
+    $assertSession->pageTextContains('Leave preview?');
+    $this->click('button:contains("Leave preview")');
+    $this->assertStringEndsWith('/foo', $this->getUrl());
+    $this->drupalGet('node/add/test');
+    $this->submitForm([
+      'title[0][value]' => 'Test child element link',
+      'body[0][value]' => '<a href="/foo" class="preview-child-element"><span>Child element link</span></a>',
+    ], 'Preview');
+    $this->getSession()->getPage()->find('css', '.preview-child-element span')->click();
     $assertSession->pageTextContains('Leave preview?');
     $this->click('button:contains("Leave preview")');
     $this->assertStringEndsWith('/foo', $this->getUrl());
