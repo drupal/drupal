1bf1a4c9cdb8c8435be60223e1c97bebca882d28
catch <6915-catch@users.noreply.drupalcode.org>
2024-08-10 09:53:29 +0900

    Issue #3465507 by mstrelan, nicxvan: Deprecate and / or remove unused Controllers and routes from WebAssert tests

--- core/modules/system/tests/modules/test_page_test/src/Controller/Test.php
+++ core/modules/system/tests/modules/test_page_test/src/Controller/Test.php
@@ -5,7 +5,6 @@ namespace Drupal\test_page_test\Controller;
 use Drupal\Core\Render\Markup;
 use Drupal\Core\Url;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
@@ -96,42 +95,6 @@ class Test {
     ];
   }
 
-  /**
-   * Sets an HTTP header.
-   *
-   * @param string $name
-   *   The header name.
-   * @param string $value
-   *   (optional) The header value ot set.
-   */
-  public function setHeader($name, $value = NULL) {
-    $response = new Response();
-    $response->headers->set($name, $value);
-    return $response;
-  }
-
-  /**
-   * Renders a page with encoded markup.
-   *
-   * @return array
-   *   A render array as expected by
-   *   \Drupal\Core\Render\RendererInterface::render().
-   */
-  public function renderEncodedMarkup() {
-    return ['#plain_text' => 'Bad html <script>alert(123);</script>'];
-  }
-
-  /**
-   * Renders a page with pipe character in link test.
-   *
-   * @return array
-   *   A render array as expected by
-   *   \Drupal\Core\Render\RendererInterface::render().
-   */
-  public function renderPipeInLink() {
-    return ['#markup' => '<a href="http://example.com">foo|bar|baz</a>'];
-  }
-
   public function escapedCharacters() {
     return [
       '#prefix' => '<div class="escaped">',
@@ -168,56 +131,6 @@ class Test {
    */
   public function metaRefresh() {
     return new RedirectResponse(Url::fromRoute('test_page_test.test_page', [], ['absolute' => TRUE])->toString(), 302);
-  }
-
-  /**
-   * Returns a page render array with 2 elements with the same HTML IDs.
-   *
-   * @return array
-   *   A render array as expected by
-   *   \Drupal\Core\Render\RendererInterface::render().
-   */
-  public function renderPageWithDuplicateIds() {
-    return [
-      '#type' => 'container',
-      'title' => [
-        '#type' => 'html_tag',
-        '#tag' => 'h1',
-        '#value' => 'Hello',
-        '#attributes' => ['id' => 'page-element'],
-      ],
-      'description' => [
-        '#type' => 'html_tag',
-        '#tag' => 'h2',
-        '#value' => 'World',
-        '#attributes' => ['id' => 'page-element'],
-      ],
-    ];
-  }
-
-  /**
-   * Returns a page render array with 2 elements with the unique HTML IDs.
-   *
-   * @return array
-   *   A render array as expected by
-   *   \Drupal\Core\Render\RendererInterface::render().
-   */
-  public function renderPageWithoutDuplicateIds() {
-    return [
-      '#type' => 'container',
-      'title' => [
-        '#type' => 'html_tag',
-        '#tag' => 'h1',
-        '#value' => 'Hello',
-        '#attributes' => ['id' => 'page-element-title'],
-      ],
-      'description' => [
-        '#type' => 'html_tag',
-        '#tag' => 'h2',
-        '#value' => 'World',
-        '#attributes' => ['id' => 'page-element-description'],
-      ],
-    ];
   }
 
   /**
