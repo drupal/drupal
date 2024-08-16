73a1fb9d0fc827703bc3cfa68dc84b021cd10e49
quietone <quietone@2572884.no-reply.drupal.org>
2024-08-16 16:32:43 +1200

    Issue #3464294 by mondrake, smustgrave: Some tests fail with 'Cannot use positional argument after named argument during unpacking'
    (cherry picked from commit 4e3be454b1c998663e1bea03ebc69eb3c323eaff)

--- core/tests/Drupal/Tests/Core/Batch/PercentagesTest.php
+++ core/tests/Drupal/Tests/Core/Batch/PercentagesTest.php
@@ -75,11 +75,11 @@ class PercentagesTest extends UnitTestCase {
       // but for the last pass through, when 500 out of 501 items have been
       // processed, we do not want to round up to 100%, since that would
       // erroneously indicate that the processing is complete.
-      ['total' => 1, 'current' => 100 / 501, '20'],
-      ['total' => 1, 'current' => 200 / 501, '40'],
-      ['total' => 1, 'current' => 300 / 501, '60'],
-      ['total' => 1, 'current' => 400 / 501, '80'],
-      ['total' => 1, 'current' => 500 / 501, '99.8'],
+      ['total' => 1, 'current' => 100 / 501, 'expected_result' => '20'],
+      ['total' => 1, 'current' => 200 / 501, 'expected_result' => '40'],
+      ['total' => 1, 'current' => 300 / 501, 'expected_result' => '60'],
+      ['total' => 1, 'current' => 400 / 501, 'expected_result' => '80'],
+      ['total' => 1, 'current' => 500 / 501, 'expected_result' => '99.8'],
     ];
   }
 
