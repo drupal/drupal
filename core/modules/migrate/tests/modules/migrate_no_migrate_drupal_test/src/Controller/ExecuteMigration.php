835708cc6325f9dd7400d3e68a7ec4b74aeb87e8
quietone <quietone@2572884.no-reply.drupal.org>
2024-09-02 17:03:56 +1200

    Issue #3466289 by catch, benjifisher, smustgrave: MigrateNoMigrateDrupalTest fails with missing classes in certain situations
    (cherry picked from commit 984772a9df39e9c9af0f582cf887a3d689b15b80)

--- core/modules/migrate/tests/modules/migrate_no_migrate_drupal_test/src/Controller/ExecuteMigration.php
+++ core/modules/migrate/tests/modules/migrate_no_migrate_drupal_test/src/Controller/ExecuteMigration.php
@@ -30,7 +30,7 @@ class ExecuteMigration extends ControllerBase {
     if ($definitions['node_migration_no_migrate_drupal']['label'] !== 'Node Migration No Migrate Drupal') {
       throw new InvalidPluginDefinitionException('node_migration_no_migrate_drupal');
     }
-    $migrations = $migration_plugin_manager->createInstances('');
+    $migrations = $migration_plugin_manager->createInstances('node_migration_no_migrate_drupal');
     $result = (new MigrateExecutable($migrations['node_migration_no_migrate_drupal']))->import();
     if ($result !== MigrationInterface::RESULT_COMPLETED) {
       throw new \RuntimeException('Migration failed');
