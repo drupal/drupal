572b08c076de102254547e6d53284656e6f44265
nod_ <nod_@598310.no-reply.drupal.org>
2024-09-07 00:03:45 +0200

    Issue #3454507 by hablat, catch: Aggregated asset generation causes uncacheable assets
    (cherry picked from commit 4f37d8d64c2ffb6e0de12a79f03a4fc99afe9373)

--- core/tests/Drupal/FunctionalTests/Asset/AssetOptimizationTest.php
+++ core/tests/Drupal/FunctionalTests/Asset/AssetOptimizationTest.php
@@ -187,16 +187,12 @@ class AssetOptimizationTest extends BrowserTestBase {
     $session->visit($this->setInvalidLibrary($url));
     $this->assertSession()->statusCodeEquals(200);
 
+    // When an invalid asset hash name is given.
     $session->visit($this->replaceGroupHash($url));
     $this->assertSession()->statusCodeEquals(200);
-    $headers = $session->getResponseHeaders();
-    $this->assertEquals(['no-store, private'], $headers['Cache-Control']);
-
-    // And again to confirm it's not cached on disk.
-    $session->visit($this->replaceGroupHash($url));
-    $this->assertSession()->statusCodeEquals(200);
-    $headers = $session->getResponseHeaders();
-    $this->assertEquals(['no-store, private'], $headers['Cache-Control']);
+    $current_url = $session->getCurrentUrl();
+    // Redirect to the correct one.
+    $this->assertEquals($url, $current_url);
   }
 
   /**
