9258113bc8f45ec8b6f6ac4035c58ab21f05011c
nod_ <nod_@598310.no-reply.drupal.org>
2024-09-09 16:48:13 +0200

    Issue #3469309 by mstrelan, smustgrave, moshe weitzman: Use one-time login link instead of user login form in BrowserTestBase tests

--- core/modules/locale/tests/src/Functional/LocaleTranslationUiTest.php
+++ core/modules/locale/tests/src/Functional/LocaleTranslationUiTest.php
@@ -29,6 +29,11 @@ class LocaleTranslationUiTest extends BrowserTestBase {
   protected $defaultTheme = 'stark';
 
   /**
+   * {@inheritdoc}
+   */
+  protected bool $useOneTimeLoginLinks = FALSE;
+
+  /**
    * Enable interface translation to English.
    */
   public function testEnglishTranslation(): void {
