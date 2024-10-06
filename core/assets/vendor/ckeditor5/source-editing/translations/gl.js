067962713b10de2628cd1983eda2739e80628c8f
nod_ <nod_@598310.no-reply.drupal.org>
2024-10-06 23:44:09 +0200

    Issue #3477799 by spokje, longwave, smustgrave: Update CKEditor 5 to 43.1.1

--- core/assets/vendor/ckeditor5/source-editing/translations/gl.js
+++ core/assets/vendor/ckeditor5/source-editing/translations/gl.js
@@ -1,1 +1,1 @@
-!function(i){const o=i.gl=i.gl||{};o.dictionary=Object.assign(o.dictionary||{},{"Show source":"",Source:"Orixe"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
+!function(o){const i=o.gl=o.gl||{};i.dictionary=Object.assign(i.dictionary||{},{"Show source":"Amosar a orixe",Source:"Orixe"})}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
