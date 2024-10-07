858610568b5e216fe87b77296c6d51c978adb37d
catch <6915-catch@users.noreply.drupalcode.org>
2024-10-07 18:50:31 +0100

    Issue #3479160 by longwave, catch, fjgarlin, nod_: 11.0.x yarn dependencies have mushroomed

---
+++ core/modules/ckeditor5/tests/src/Unit/VersionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\ckeditor5\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @group ckeditor5
+ * @internal
+ */
+class VersionTest extends UnitTestCase {
+
+  /**
+   * Ensure that CKEditor5 versions are aligned.
+   */
+  public function testVersionAlignment(): void {
+    $package_json = json_decode(file_get_contents(__DIR__ . '/../../../../../package.json'), TRUE);
+    $ckeditor_dependencies = array_filter($package_json['devDependencies'], fn ($key) => str_starts_with($key, '@ckeditor/ckeditor5-'), ARRAY_FILTER_USE_KEY);
+    $this->assertCount(1, array_unique($ckeditor_dependencies));
+  }
+
+}
