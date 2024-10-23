25ad53d3df09d486ea85bcf19edabea34f914fb9
catch <6915-catch@users.noreply.drupalcode.org>
2024-10-23 12:54:56 +0100

    Issue #3038040 by claudiu.cristea, bradjones1, swentel, bvoynick, longwave, grayle, catch, timwood, alexpott: Support priority for needs_destruction service tag

--- core/lib/Drupal/Core/DependencyInjection/Compiler/RegisterServicesForDestructionPass.php
+++ core/lib/Drupal/Core/DependencyInjection/Compiler/RegisterServicesForDestructionPass.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Core\DependencyInjection\Compiler;
 
+use Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 
@@ -14,12 +15,14 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
  */
 class RegisterServicesForDestructionPass implements CompilerPassInterface {
 
+  use PriorityTaggedServiceTrait;
+
   /**
    * {@inheritdoc}
    */
   public function process(ContainerBuilder $container) {
-    $services = $container->findTaggedServiceIds('needs_destruction');
-    $container->setParameter('kernel.destructable_services', array_keys($services));
+    $service_ids = array_values(array_map('strval', $this->findAndSortTaggedServices('needs_destruction', $container)));
+    $container->setParameter('kernel.destructable_services', $service_ids);
   }
 
 }
