cba026ef5b32e9cb65a6b1faf8cad108c61123b7
Lee Rowlands <lee.rowlands@previousnext.com.au>
2024-11-04 08:11:17 +1000

    Issue #3479411 by phenaproxima, thejimbirch: The addItemToToolbar config action should not, by default, add items that are already in the toolbar
    (cherry picked from commit c1927736a85e629aec99d6abc12ede795355f522)

--- core/modules/ckeditor5/src/Plugin/ConfigAction/AddItemToToolbar.php
+++ core/modules/ckeditor5/src/Plugin/ConfigAction/AddItemToToolbar.php
@@ -47,30 +47,40 @@ final class AddItemToToolbar implements ConfigActionPluginInterface, ContainerFa
       throw new ConfigActionException(sprintf('The %s config action only works with editors that use CKEditor 5.', $this->pluginId));
     }
 
-    $editor_settings = $editor->getSettings();
     if (is_string($value)) {
-      $editor_settings['toolbar']['items'][] = $item_name = $value;
+      $value = ['item_name' => $value];
+    }
+    assert(is_array($value));
+
+    $item_name = $value['item_name'];
+    assert(is_string($item_name));
+
+    $replace = $value['replace'] ?? FALSE;
+    assert(is_bool($replace));
+
+    $position = $value['position'] ?? NULL;
+
+    $allow_duplicate = $value['allow_duplicate'] ?? FALSE;
+    assert(is_bool($allow_duplicate));
+
+    $editor_settings = $editor->getSettings();
+
+    // If the item is already in the toolbar and we're not allowing duplicate
+    // items, we're done.
+    if (in_array($item_name, $editor_settings['toolbar']['items'], TRUE) && $allow_duplicate === FALSE && $item_name !== '|') {
+      return;
+    }
+
+    if (is_int($position)) {
+      // If we want to replace the item at this position, then `replace`
+      // should be true. This would be useful if, for example, we wanted to
+      // replace the Image button with the Media Library.
+      array_splice($editor_settings['toolbar']['items'], $position, $replace ? 1 : 0, $item_name);
     }
     else {
-      assert(is_array($value));
-
-      $item_name = $value['item_name'];
-      assert(is_string($item_name));
-
-      $replace = $value['replace'] ?? FALSE;
-      assert(is_bool($replace));
-
-      $position = $value['position'] ?? NULL;
-      if (is_int($position)) {
-        // If we want to replace the item at this position, then `replace`
-        // should be true. This would be useful if, for example, we wanted to
-        // replace the Image button with the Media Library.
-        array_splice($editor_settings['toolbar']['items'], $position, $replace ? 1 : 0, $item_name);
-      }
-      else {
-        $editor_settings['toolbar']['items'][] = $item_name;
-      }
+      $editor_settings['toolbar']['items'][] = $item_name;
     }
+
     // If we're just adding a vertical separator, there's nothing else we need
     // to do at this point.
     if ($item_name === '|') {
