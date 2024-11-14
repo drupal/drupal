68013115a83ef66805039a752b1f6dcecfda6221
Alex Pott <alex.a.pott@googlemail.com>
2024-11-14 11:22:11 +0000

    Issue #3113989 by acbramley, mohit_aghera, maskedjellybean, scott_euser, yogeshmpawar, lendude, ranjith_kumar_k_u, ameymudras, kristen pol, rahul b, larowlan, quietone, dww, alexpott, daffie: Media author views filter should use the user_name plugin

--- core/modules/media/src/MediaViewsData.php
+++ core/modules/media/src/MediaViewsData.php
@@ -18,6 +18,12 @@ class MediaViewsData extends EntityViewsData {
     $data['media_field_data']['table']['wizard_id'] = 'media';
     $data['media_field_revision']['table']['wizard_id'] = 'media_revision';
 
+    $data['media_field_data']['user_name']['filter'] = $data['media_field_data']['uid']['filter'];
+    $data['media_field_data']['user_name']['filter']['title'] = $this->t('Authored by');
+    $data['media_field_data']['user_name']['filter']['help'] = $this->t('The username of the content author.');
+    $data['media_field_data']['user_name']['filter']['id'] = 'user_name';
+    $data['media_field_data']['user_name']['filter']['real field'] = 'uid';
+
     $data['media_field_data']['status_extra'] = [
       'title' => $this->t('Published status or admin user'),
       'help' => $this->t('Filters out unpublished media if the current user cannot view it.'),
