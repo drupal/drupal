6d5a320db6c7916b6e519b8233abe1b11956402d
Alex Pott <alex.a.pott@googlemail.com>
2024-11-14 22:49:06 +0000

    Issue #3486981 by phenaproxima, thejimbirch: Allow recipes to enable Layout Builder via config actions
    (cherry picked from commit 524cf737ec1284471552fb632bb833b2d25b5fd1)

---
+++ core/modules/layout_builder/tests/src/Kernel/ConfigActionsTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\layout_builder\Kernel;
+
+use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
+use Drupal\entity_test\Entity\EntityTestBundle;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\layout_builder\Entity\LayoutBuilderEntityViewDisplay;
+
+/**
+ * @group Recipe
+ */
+class ConfigActionsTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'entity_test',
+    'field',
+    'layout_builder',
+    'layout_discovery',
+  ];
+
+  /**
+   * Tests config actions exposed by Layout Builder.
+   */
+  public function testLayoutBuilderActions(): void {
+    /** @var \Drupal\Core\Config\Action\ConfigActionManager $manager */
+    $manager = $this->container->get('plugin.manager.config_action');
+
+    EntityTestBundle::create(['id' => 'test'])->save();
+
+    /** @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface $display_repository */
+    $display_repository = $this->container->get(EntityDisplayRepositoryInterface::class);
+
+    /** @var \Drupal\layout_builder\Entity\LayoutBuilderEntityViewDisplay $display */
+    $display = $display_repository->getViewDisplay('entity_test_with_bundle', 'test');
+    $this->assertInstanceOf(LayoutBuilderEntityViewDisplay::class, $display);
+    $display->save();
+
+    $this->assertFalse($display->isLayoutBuilderEnabled());
+    $manager->applyAction('enableLayoutBuilder', $display->getConfigDependencyName(), []);
+    $this->assertTrue($display_repository->getViewDisplay('entity_test_with_bundle', 'test')->isLayoutBuilderEnabled());
+
+    $this->assertFalse($display->isOverridable());
+    $manager->applyAction('allowLayoutOverrides', $display->getConfigDependencyName(), TRUE);
+    $this->assertTrue($display_repository->getViewDisplay('entity_test_with_bundle', 'test')->isOverridable());
+    $manager->applyAction('allowLayoutOverrides', $display->getConfigDependencyName(), FALSE);
+    $this->assertFalse($display_repository->getViewDisplay('entity_test_with_bundle', 'test')->isOverridable());
+
+    $manager->applyAction('disableLayoutBuilder', $display->getConfigDependencyName(), []);
+    $this->assertFalse($display_repository->getViewDisplay('entity_test_with_bundle', 'test')->isLayoutBuilderEnabled());
+  }
+
+}
