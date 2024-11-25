46e33eda0aea3c2897ebb4628f6c8e6d016b03d3
quietone <quietone@2572884.no-reply.drupal.org>
2024-11-25 16:48:51 +1300

    Issue #2969406 by bhanu951, robincs, quietone, ranjit1032002, sahil.goyal, urvashi_vora, opdavies, elber, aaronmchale, avpaderno, smustgrave, alberto56, cilefen, benjifisher, lauriii, larowlan, Mahima_Mathur23, nkoporec, borisson_, poker10: Fix incorrect message after resetting password
    (cherry picked from commit 5874bcd35c81240c26234f828309e406517feeee)

--- core/modules/user/src/Controller/UserController.php
+++ core/modules/user/src/Controller/UserController.php
@@ -264,7 +264,7 @@ class UserController extends ControllerBase {
 
     user_login_finalize($user);
     $this->logger->info('User %name used one-time login link at time %timestamp.', ['%name' => $user->getDisplayName(), '%timestamp' => $timestamp]);
-    $this->messenger()->addStatus($this->t('You have just used your one-time login link. It is no longer necessary to use this link to log in. It is recommended that you set your password.'));
+    $this->messenger()->addStatus($this->t('You have used a one-time login link. You can set your new password now.'));
     // Let the user's password be changed without the current password
     // check.
     $token = Crypt::randomBytesBase64(55);
