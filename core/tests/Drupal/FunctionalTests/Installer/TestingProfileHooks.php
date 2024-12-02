6cc34e199d147c36fc5ccd1958c6a2ebc6f57746
catch <6915-catch@users.noreply.drupalcode.org>
2024-12-02 18:38:43 +0000

    Issue #3490298 by nicxvan: Profiles can be missed in OOP hooks

---
+++ core/tests/Drupal/FunctionalTests/Installer/TestingProfileHooks.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\FunctionalTests\Installer;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests installing the Testing profile with update notifications on.
+ *
+ * @group Installer
+ */
+class TestingProfileHooks extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $profile = 'testing_hooks';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Test hooks are picked up.
+   */
+  public function testHookPickup(): void {
+    $this->assertFalse(isset($GLOBALS['profile_procedural']));
+    $this->assertFalse(isset($GLOBALS['profile_oop']));
+    drupal_flush_all_caches();
+    $this->assertTrue(isset($GLOBALS['profile_procedural']));
+    $this->assertTrue(isset($GLOBALS['profile_oop']));
+  }
+
+}
