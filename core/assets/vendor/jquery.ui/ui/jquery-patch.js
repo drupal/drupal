0c91bf7e7d9ae3b502306ccbc89403ca6f45b771
catch <6915-catch@users.noreply.drupalcode.org>
2024-12-13 19:17:30 +0000

    Issue #3493146 by spokje, smustgrave: Update all JavaScript dependencies which cause no changes

--- core/assets/vendor/jquery.ui/ui/jquery-patch.js
+++ core/assets/vendor/jquery.ui/ui/jquery-patch.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery UI Legacy jQuery Core patches 1.14.0
+ * jQuery UI Legacy jQuery Core patches 1.14.1
  * https://jqueryui.com
  *
  * Copyright OpenJS Foundation and other contributors
