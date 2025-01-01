3df2a1668edbd61797c520ee6b4d20c39f3596b8
catch <6915-catch@users.noreply.drupalcode.org>
2025-01-01 07:53:27 +0000

    Issue #3477586 by spokje, catch, godotislate, berdir, dww, nicxvan: [random test failure] LayoutBuilderBlocksTest::testBlockPlaceholder failing

--- core/modules/block/tests/modules/block_test/src/Plugin/Block/TestHtmlBlock.php
+++ core/modules/block/tests/modules/block_test/src/Plugin/Block/TestHtmlBlock.php
@@ -22,8 +22,8 @@ class TestHtmlBlock extends BlockBase {
    */
   public function build() {
     return [
-      '#attributes' => \Drupal::state()->get('block_test.attributes'),
-      '#children' => \Drupal::state()->get('block_test.content'),
+      '#attributes' => \Drupal::keyvalue('block_test')->get('attributes'),
+      '#children' => \Drupal::keyValue('block_test')->get('content'),
     ];
   }
 
