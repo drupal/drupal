34fe29fb92e071ff0fa6ba39d16dc3cbfd6128c0
Lee Rowlands <lee.rowlands@previousnext.com.au>
2025-01-03 09:55:16 +1000

    Issue #1986330 by bhanu951, subhojit777, marcelodornelas, ravi.shankar, immaculatexavier, wheatpenny, nikunjkotecha, shashikant_chauhan, vacho, smagdits, kasperg, aron.beal, andriansyah, suresh prabhu parkala, rajeevk, benjifisher, mangy.fox, cebasqueira, stefank, richardcanoe, quietone, yesct, AkshayKalose, drdam, DevElCuy, john cook, moymilo, alexpott, xjm, dawehner, tstoeckler, webchick, akashkumar07: When Batch ID doesn't exist, Drupal should emit a 404
    (cherry picked from commit 787cc8ab26cd80acc4142448702127a5333835fe)

---
+++ core/modules/system/tests/src/Functional/Batch/BatchNotFoundTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\system\Functional\Batch;
+
+use Drupal\Core\Batch\BatchStorageInterface;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests if a page not found error is returned when a batch ID does not exist.
+ *
+ * @group Batch
+ */
+class BatchNotFoundTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['batch_test'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests for page not found error if batch ID does not exist.
+   */
+  public function testBatchNotFound(): void {
+
+    $edit = ['batch' => 'batch_0'];
+    $this->drupalGet('batch-test');
+    $this->submitForm($edit, 'Submit');
+    $this->assertSession()->statusCodeEquals(200);
+
+    $batch_id = \Drupal::service(BatchStorageInterface::class)->getId();
+
+    $this->drupalGet('batch', [
+      'query' => [
+        'op' => 'start',
+        'id' => $batch_id,
+      ],
+    ]);
+
+    $this->assertSession()->statusCodeEquals(404);
+  }
+
+}
