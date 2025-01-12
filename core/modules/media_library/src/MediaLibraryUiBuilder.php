c98883170e3ed1a12d0b0f8d835032b1887cea63
Alex Pott <alex.a.pott@googlemail.com>
2025-01-12 11:36:03 +0000

    Issue #3488583 by grimreaper: Media library links: remove #markup

--- core/modules/media_library/src/MediaLibraryUiBuilder.php
+++ core/modules/media_library/src/MediaLibraryUiBuilder.php
@@ -248,13 +248,9 @@ class MediaLibraryUiBuilder {
       $link_state->set('media_library_content', 1);
 
       $title = $allowed_type->label();
-      $display_title = [
-        '#markup' => $this->t('<span class="visually-hidden">Show </span>@title<span class="visually-hidden"> media</span>', ['@title' => $title]),
-      ];
+      $display_title = $this->t('<span class="visually-hidden">Show </span>@title<span class="visually-hidden"> media</span>', ['@title' => $title]);
       if ($allowed_type_id === $selected_type_id) {
-        $display_title = [
-          '#markup' => $this->t('<span class="visually-hidden">Show </span>@title<span class="visually-hidden"> media</span><span class="active-tab visually-hidden"> (selected)</span>', ['@title' => $title]),
-        ];
+        $display_title = $this->t('<span class="visually-hidden">Show </span>@title<span class="visually-hidden"> media</span><span class="active-tab visually-hidden"> (selected)</span>', ['@title' => $title]);
       }
 
       $menu['#links']['media-library-menu-' . $allowed_type_id] = [
