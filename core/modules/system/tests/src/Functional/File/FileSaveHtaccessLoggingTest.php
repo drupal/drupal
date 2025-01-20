798ad9e459020a5142a3b705fd95017e50179716
quietone <quietone@2572884.no-reply.drupal.org>
2025-01-20 13:48:40 +1300

    Issue #3495778 by bramdriesen, smustgrave, cmlara, dawehner, drumm, effulgentsia, fabianx, catch, berdir, alexpott, amateescu, BarisW, benjifisher, gábor hojtsy, guypaddock, tim.plunkett, webchick, wim leers, quicksketch, xjm, tedbow, poker10, kim.pepper, jenlampton, larowlan, longwave, plach, mondrake: Update all references to file_save_htaccess()

--- core/modules/system/tests/src/Functional/File/FileSaveHtaccessLoggingTest.php
+++ core/modules/system/tests/src/Functional/File/FileSaveHtaccessLoggingTest.php
@@ -8,7 +8,7 @@ use Drupal\Component\FileSecurity\FileSecurity;
 use Drupal\Tests\BrowserTestBase;
 
 /**
- * Tests the log message added by file_save_htaccess().
+ * Tests the log message added by the HtaccessWriter service.
  *
  * @group File
  */
@@ -25,15 +25,15 @@ class FileSaveHtaccessLoggingTest extends BrowserTestBase {
   protected $defaultTheme = 'stark';
 
   /**
-   * Tests file_save_htaccess().
+   * Tests the HtaccessWriter service write functionality.
    */
   public function testHtaccessSave(): void {
     // Prepare test directories.
     $private = $this->publicFilesDirectory . '/test/private';
 
-    // Verify that file_save_htaccess() returns FALSE if .htaccess cannot be
-    // written and writes a correctly formatted message to the error log. Set
-    // $private to TRUE so all possible .htaccess lines are written.
+    // Verify that HtaccessWriter service returns FALSE if .htaccess cannot be
+    // written and writes a correctly formatted message to the error log.
+    // Set $private to TRUE so all possible .htaccess lines are written.
     /** @var \Drupal\Core\File\HtaccessWriterInterface $htaccess */
     $htaccess = \Drupal::service('file.htaccess_writer');
     $this->assertFalse($htaccess->write($private, TRUE));
