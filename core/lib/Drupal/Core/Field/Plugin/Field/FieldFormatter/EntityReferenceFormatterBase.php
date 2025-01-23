80f2ff71dc27cf89d8d1741a24f4c9621c01f99d
catch <6915-catch@users.noreply.drupalcode.org>
2025-01-23 17:12:16 +0000

    Issue #3489179 by godotislate, ghost of drupal past: Referring the same entity multiple times breaks _referringItem
    (cherry picked from commit 40c1ab5260fa15ae6b0ab607d175025436a241ec)

--- core/lib/Drupal/Core/Field/Plugin/Field/FieldFormatter/EntityReferenceFormatterBase.php
+++ core/lib/Drupal/Core/Field/Plugin/Field/FieldFormatter/EntityReferenceFormatterBase.php
@@ -57,7 +57,14 @@ abstract class EntityReferenceFormatterBase extends FormatterBase {
         $item->_accessCacheability = CacheableMetadata::createFromObject($access);
         if ($access->isAllowed()) {
           // Add the referring item, in case the formatter needs it.
-          $entity->_referringItem = $items[$delta];
+          if (isset($entity->_referringItem) && ($entity->_referringItem !== $item)) {
+            // If the entity is already being referenced by another field item,
+            // clone the entity so that _referringItem is set to the correct
+            // item in each instance.
+            $entity = clone $entity;
+            $item->entity = $entity;
+          }
+          $entity->_referringItem = $item;
           $entities[$delta] = $entity;
         }
       }
