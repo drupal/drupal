b92e644d65a419c2c239a605452f2b67f6924060
catch <6915-catch@users.noreply.drupalcode.org>
2025-03-04 22:17:30 +0000

    Revert "Issue #3464388 by vidorado, mogtofu33, smustgrave, wim leers: SDC *.component.yml metadata is cached aggressively, gets in the way of component development"
    This reverts commit f3b0c8db916677f64e57d046a809634540e1c51a.

--- core/tests/Drupal/KernelTests/Components/ComponentPluginManagerTest.php
+++ core/tests/Drupal/KernelTests/Components/ComponentPluginManagerTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Drupal\KernelTests\Components;
 
 use Drupal\Core\Render\Component\Exception\ComponentNotFoundException;
-use Drupal\Core\Cache\CacheBackendInterface;
 
 /**
  * Tests the component plugin manager.
@@ -48,49 +47,6 @@ class ComponentPluginManagerTest extends ComponentKernelTestBase {
   public function testMismatchingFolderName(): void {
     $this->expectException(ComponentNotFoundException::class);
     $this->manager->find('sdc_theme_test:mismatching-folder-name');
-  }
-
-  /**
-   * Test component definitions caching depending on twig debug/cache settings.
-   *
-   * @param bool $twigDebug
-   *   Whether twig debug is enabled.
-   * @param bool $cacheEnabled
-   *   Whether cache is enabled.
-   * @param bool $expectCacheGet
-   *   Whether we expect the cache to be called.
-   *
-   * @dataProvider providerTestComponentCachingDependingOnDevelopmentSettings
-   */
-  public function testComponentCachingDependingOnDevelopmentSettings(bool $twigDebug, bool $cacheEnabled, bool $expectCacheGet): void {
-    // Set the development settings.
-    $developmentSettings = $this->keyValue->get('development_settings');
-    $developmentSettings->set('twig_debug', $twigDebug);
-    $developmentSettings->set('twig_cache_disable', !$cacheEnabled);
-
-    // Set the cache backend as a spy mock.
-    $cacheBackend = $this->createMock(CacheBackendInterface::class);
-    $cacheBackend->expects($expectCacheGet ? $this->once() : $this->never())
-      ->method('get')
-      ->with('cache_key');
-    $this->manager->setCacheBackend($cacheBackend, 'cache_key');
-
-    // Make two calls to getDefinitions() to ensure the
-    // cache is/isn't called if it should/shouldn't be.
-    $this->manager->getDefinitions();
-    $this->manager->getDefinitions();
-  }
-
-  /**
-   * Data provider for testComponentCachingDependingOnDevelopmentSettings().
-   */
-  public static function providerTestComponentCachingDependingOnDevelopmentSettings(): array {
-    return [
-      'Debug enabled, cache enabled' => [TRUE, TRUE, FALSE],
-      'Debug enabled, cache disabled' => [TRUE, FALSE, FALSE],
-      'Debug disabled, cache enabled' => [FALSE, TRUE, TRUE],
-      'Debug disabled, cache disabled' => [FALSE, FALSE, FALSE],
-    ];
   }
 
 }
