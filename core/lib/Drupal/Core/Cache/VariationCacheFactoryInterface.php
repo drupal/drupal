7c8f30704a824826df5c931cbb5575f1d5ebb682
nod_ <nod_@598310.no-reply.drupal.org>
2025-03-05 09:43:49 +0100

    Issue #3508828 by kristiaanvandeneynde, borisson_: VariationCacheFactoryInterface documentation isn't fully correct

--- core/lib/Drupal/Core/Cache/VariationCacheFactoryInterface.php
+++ core/lib/Drupal/Core/Cache/VariationCacheFactoryInterface.php
@@ -8,13 +8,13 @@ namespace Drupal\Core\Cache;
 interface VariationCacheFactoryInterface {
 
   /**
-   * Gets a variation cache backend for a given cache bin.
+   * Gets a variation cache for a given cache bin.
    *
    * @param string $bin
-   *   The cache bin for which a variation cache backend should be returned.
+   *   The cache bin for which a variation cache should be returned.
    *
    * @return \Drupal\Core\Cache\VariationCacheInterface
-   *   The variation cache backend associated with the specified bin.
+   *   The variation cache associated with the specified bin.
    */
   public function get($bin);
 
