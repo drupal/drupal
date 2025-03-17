d0743bcb531a51476b797f6582abd18ae973f0f3
catch <6915-catch@users.noreply.drupalcode.org>
2025-03-17 12:17:45 +0000

    Issue #3498154 by alexpott, catch, kristiaanvandeneynde: Use LRU Cache for static entity cache

--- core/modules/migrate/src/MigrateExecutable.php
+++ core/modules/migrate/src/MigrateExecutable.php
@@ -612,9 +612,6 @@ class MigrateExecutable implements MigrateExecutableInterface {
     // plenty of memory to continue.
     drupal_static_reset();
 
-    // Entity storage can blow up with caches, so clear it out.
-    \Drupal::service('entity.memory_cache')->deleteAll();
-
     // @todo Explore resetting the container.
 
     // Run garbage collector to further reduce memory.
