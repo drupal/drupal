14d9ea360dfc57b14b1081909751b1ab2e506897
nod_ <nod_@598310.no-reply.drupal.org>
2025-04-02 22:44:11 +0200

    Issue #3504204 by murz: Nightwatch command drupalInstall doesn't set the user_agent environment variable with the simpletest site instance

--- core/tests/Drupal/Nightwatch/Commands/drupalInstall.js
+++ core/tests/Drupal/Nightwatch/Commands/drupalInstall.js
@@ -50,6 +50,9 @@ exports.command = function drupalInstall(
       value: encodeURIComponent(installData.user_agent),
       path: url.pathname,
     });
+    // Set the HTTP_USER_AGENT environment variable to detect the test
+    // environment in the command line.
+    process.env.HTTP_USER_AGENT = installData.user_agent;
   } catch (error) {
     this.assert.fail(error);
   }
