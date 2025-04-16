718a97e6db565fce0baafcd8d7e410b992392c6b
Dave Long <dave@longwaveconsulting.com>
2025-04-16 10:37:29 +0200

    Issue #3499245 by mondrake, bbrala: [CI] Report PHPStan baseline statistics in job

---
+++ .gitlab-ci/scripts/phpstan-baseline-statistics.php
@@ -0,0 +1,38 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * @file
+ * PHPStan baseline statistics.
+ */
+
+if (PHP_SAPI !== 'cli') {
+  return;
+}
+
+$ignoreErrors = [];
+require __DIR__ . '/../../core/.phpstan-baseline.php';
+
+$stats = ['__total' => 0];
+foreach ($ignoreErrors as $ignore) {
+  $identifier = $ignore['identifier'] ?? '* not specified *';
+  $count = $ignore['count'] ?? 1;
+  $stats['__total'] += $count;
+  $stats[$identifier] = isset($stats[$identifier]) ? $stats[$identifier] + $count : $count;
+}
+
+echo "----------------------------------------\n";
+echo "PHPStan baseline statistics\n";
+echo "----------------------------------------\n";
+echo sprintf("%6d * Total baselined errors\n", $stats['__total']);
+echo "----------------------------------------\n";
+echo "Breakdown by error identifier:\n";
+file_put_contents(__DIR__ . '/../../phpstan-metrics.txt', 'phpstan-baseline ' . $stats['__total'] . PHP_EOL, FILE_APPEND);
+
+unset($stats['__total']);
+arsort($stats);
+
+foreach ($stats as $identifier => $stat) {
+  echo sprintf("%6d %s\n", $stat, $identifier);
+  file_put_contents(__DIR__ . '/../../phpstan-metrics.txt', 'phpstan-baseline.' . $identifier . ' ' . $stat . PHP_EOL, FILE_APPEND);
+}
