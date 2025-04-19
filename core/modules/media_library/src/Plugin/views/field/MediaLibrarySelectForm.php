8a10f82ffc93f3fcf5a6d95a65cebe40f9c10011
catch <6915-catch@users.noreply.drupalcode.org>
2025-04-19 07:58:12 +0100

    Issue #3147148 by DarKFlameS, amateescu, mandclu, smustgrave: Media library form can only be submitted in the default workspace

--- core/modules/media_library/src/Plugin/views/field/MediaLibrarySelectForm.php
+++ core/modules/media_library/src/Plugin/views/field/MediaLibrarySelectForm.php
@@ -7,6 +7,7 @@ use Drupal\Core\Ajax\CloseDialogCommand;
 use Drupal\Core\Ajax\MessageCommand;
 use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\WorkspaceSafeFormInterface;
 use Drupal\Core\Url;
 use Drupal\media_library\MediaLibraryState;
 use Drupal\views\Attribute\ViewsField;
@@ -22,7 +23,7 @@ use Symfony\Component\HttpFoundation\Request;
  *   Plugin classes are internal.
  */
 #[ViewsField("media_library_select_form")]
-class MediaLibrarySelectForm extends FieldPluginBase {
+class MediaLibrarySelectForm extends FieldPluginBase implements WorkspaceSafeFormInterface {
 
   /**
    * {@inheritdoc}
