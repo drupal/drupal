17be2e1bf72b037cbd0fa44b312bae4562f4d788
catch <6915-catch@users.noreply.drupalcode.org>
2025-04-26 08:41:34 +0100

    Issue #3511566 by prudloff, smustgrave: Remove srcdoc attributes in Xss::filter()
    (cherry picked from commit 26caa6b122b8c214644272ac61a3418251a8559f)

--- core/lib/Drupal/Component/Utility/Xss.php
+++ core/lib/Drupal/Component/Utility/Xss.php
@@ -216,7 +216,7 @@ class Xss {
           if (preg_match('/^([-a-zA-Z][-a-zA-Z0-9]*)/', $attributes, $match)) {
             $attribute_name = strtolower($match[1]);
             $skip = (
-              $attribute_name == 'style' ||
+              in_array($attribute_name, ['style', 'srcdoc']) ||
               str_starts_with($attribute_name, 'on') ||
               str_starts_with($attribute_name, '-') ||
               // Ignore long attributes to avoid unnecessary processing
