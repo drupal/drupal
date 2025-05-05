7800942b9ab5a8fd3e7f29fad4731027c7d014d4
Lee Rowlands <lee.rowlands@previousnext.com.au>
2025-05-06 09:30:33 +1000

    Revert "Issue #3512835 by nicxvan: [11.1.x] Add BC stubs for Hook ordering"
    This reverts commit 81f5ac29ce912f1d6f6ea5968b5fa1efba4a917a.

--- core/lib/Drupal/Core/Hook/Attribute/Hook.php
+++ core/lib/Drupal/Core/Hook/Attribute/Hook.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Core\Hook\Attribute;
 
-use Drupal\Core\Hook\Order\OrderInterface;
-
 /**
  * This class will not have an effect until Drupal 11.1.0.
  *
@@ -14,29 +12,13 @@ use Drupal\Core\Hook\Order\OrderInterface;
  * #LegacyHook attributes.
  */
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
-class Hook implements HookAttributeInterface {
-  /**
-   * The hook prefix such as `form`.
-   *
-   * @var string
-   */
-  public const string PREFIX = '';
-
-  /**
-   * The hook suffix such as `alter`.
-   *
-   * @var string
-   */
-  public const string SUFFIX = '';
+class Hook {
 
   /**
    * Constructs a Hook attribute object.
    *
    * @param string $hook
    *   The short hook name, without the 'hook_' prefix.
-   *   $hook is only optional when Hook is extended and a PREFIX or SUFFIX is
-   *   defined. When using the [#Hook] attribute directly $hook is required.
-   *   See Drupal\Core\Hook\Attribute\Preprocess.
    * @param string $method
    *   (optional) The method name. If this attribute is on a method, this
    *   parameter is not required. If this attribute is on a class and this
@@ -48,26 +30,16 @@ class Hook implements HookAttributeInterface {
    *   are executed first. If omitted, the module order is used to order the
    *   hook implementations.
    * @param string|null $module
-   *   (optional) The module this implementation is for. This allows one module
-   *   to implement a hook on behalf of another module. Defaults to the module
-   *   the implementation is in.
-   * @param \Drupal\Core\Hook\Order\OrderInterface|null $order
-   *   (optional) Set the order of the implementation. This parameter is
-   *   supported in Drupal 11.2 and greater. It will have no affect in Drupal
-   *   11.1.
+   *   (optional) The module this implementation is for. This allows one module to
+   *   implement a hook on behalf of another module. Defaults to the module the
+   *   implementation is in.
    */
   public function __construct(
-    public string $hook = '',
+    public string $hook,
     public string $method = '',
     public ?int $priority = NULL,
     public ?string $module = NULL,
-    public OrderInterface|null $order = NULL,
-  ) {
-    $this->hook = implode('_', array_filter([static::PREFIX, $hook, static::SUFFIX]));
-    if ($this->hook === '') {
-      throw new \LogicException('The Hook attribute or an attribute extending the Hook attribute must provide the $hook parameter, a PREFIX or a SUFFIX.');
-    }
-  }
+  ) {}
 
   /**
    * Set the method the hook should apply to.
