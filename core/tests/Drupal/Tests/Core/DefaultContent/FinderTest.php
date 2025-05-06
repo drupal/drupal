a2270154b776a1adb84b81b20f2fe095707c7cec
catch <6915-catch@users.noreply.drupalcode.org>
2025-05-06 11:43:30 +0100

    Issue #3522795 by alexpott, bbrala: \Drupal\Tests\Core\DefaultContent\FinderTest::testExceptionIfNoUuid() can cause random fails
    (cherry picked from commit d0d9ddba3def7a90652c7085af7d41a940c7e97c)

--- core/tests/Drupal/Tests/Core/DefaultContent/FinderTest.php
+++ core/tests/Drupal/Tests/Core/DefaultContent/FinderTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\Core\DefaultContent;
 
-use Drupal\Component\FileSystem\FileSystem;
 use Drupal\Core\DefaultContent\Finder;
 use Drupal\Core\DefaultContent\ImportException;
 use Drupal\Tests\UnitTestCase;
@@ -38,14 +37,9 @@ class FinderTest extends UnitTestCase {
    * Tests that files without UUIDs will raise an exception.
    */
   public function testExceptionIfNoUuid(): void {
-    $dir = FileSystem::getOsTemporaryDirectory();
-    $this->assertIsString($dir);
-    /** @var string $dir */
-    file_put_contents($dir . '/no-uuid.yml', '_meta: {}');
-
     $this->expectException(ImportException::class);
-    $this->expectExceptionMessage("$dir/no-uuid.yml does not have a UUID.");
-    new Finder($dir);
+    $this->expectExceptionMessageMatches("#/no-uuid\.yml does not have a UUID\.$#");
+    new Finder(__DIR__ . '/../../../../fixtures/default_content_broken');
   }
 
 }
