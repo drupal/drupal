905a8ddd202bc0ab939384cb88c6bbb06c6a0e36
Alex Pott <alex.a.pott@googlemail.com>
2025-05-13 11:45:42 +0100

    Issue #3500683 by berdir, nod_, kristiaanvandeneynde, catch: Backport interface to allow access policies to opt out of caching

---
+++ core/lib/Drupal/Core/Cache/CacheOptionalInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\Core\Cache;
+
+/**
+ * Indicates that caching is optional.
+ *
+ * This interface is backported to Drupal 10.5 to make it easier to support it,
+ * There are no usages in Drupal core before 11.2
+ */
+interface CacheOptionalInterface {
+
+}
