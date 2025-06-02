46820e24e09fb008bb8a2465f2b0d8ee4dcd8ba8
catch <6915-catch@users.noreply.drupalcode.org>
2025-06-02 09:10:35 +0100

    Issue #3514197 by donquixote, acbramley, nicxvan, godotislate, longwave: ModuleHandler::resetImplementations should reset all properties with hook implementations

--- core/tests/Drupal/KernelTests/Core/Extension/ModuleHandlerTest.php
+++ core/tests/Drupal/KernelTests/Core/Extension/ModuleHandlerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\KernelTests\Core\Extension;
 
 use Drupal\Core\Extension\Exception\UnknownExtensionException;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
@@ -34,6 +35,80 @@ class ModuleHandlerTest extends KernelTestBase {
   public function testGetNameDeprecation(): void {
     $this->expectDeprecation('Drupal\Core\Extension\ModuleHandler::getName() is deprecated in drupal:10.3.0 and is removed from drupal:12.0.0. Use \Drupal\Core\Extension\ModuleExtensionList::getName($module) instead. See https://www.drupal.org/node/3310017');
     $this->assertNotNull(\Drupal::service('module_handler')->getName('module_test'));
+  }
+
+  /**
+   * Tests that resetImplementations() clears the hook memory cache.
+   *
+   * @covers ::resetImplementations
+   */
+  public function testResetImplementationsClearsHooks(): void {
+    $oldModuleHandler = \Drupal::moduleHandler();
+    $this->assertHasResetHookImplementations(FALSE, $oldModuleHandler);
+
+    // Installing a module does not trigger ->resetImplementations().
+    /** @var \Drupal\Core\Extension\ModuleInstallerInterface $moduleInstaller */
+    $moduleInstaller = \Drupal::service('module_installer');
+    $moduleInstaller->install(['module_test']);
+    $this->assertHasResetHookImplementations(FALSE, $oldModuleHandler);
+    // Only the new ModuleHandler instance has the updated implementations.
+    $moduleHandler = \Drupal::moduleHandler();
+    $this->assertHasResetHookImplementations(TRUE, $moduleHandler);
+    $backupModuleList = $moduleHandler->getModuleList();
+    $moduleListWithout = array_diff_key($backupModuleList, ['module_test' => TRUE]);
+    $this->assertArrayHasKey('module_test', $backupModuleList);
+
+    // Silently setting the property does not clear the hooks cache.
+    $moduleListProperty = (new \ReflectionProperty($moduleHandler, 'moduleList'));
+    $this->assertSame($backupModuleList, $moduleListProperty->getValue($moduleHandler));
+    $moduleListProperty->setValue($moduleHandler, $moduleListWithout);
+    $this->assertHasResetHookImplementations(TRUE, $moduleHandler);
+
+    // Directly calling ->resetImplementations() clears the hook caches.
+    $moduleHandler->resetImplementations();
+    $this->assertHasResetHookImplementations(FALSE, $moduleHandler);
+    $moduleListProperty->setValue($moduleHandler, $backupModuleList);
+    $this->assertHasResetHookImplementations(FALSE, $moduleHandler);
+    $moduleHandler->resetImplementations();
+    $this->assertHasResetHookImplementations(TRUE, $moduleHandler);
+
+    // Calling ->setModuleList() triggers ->resetImplementations().
+    $moduleHandler->setModuleList(['system']);
+    $this->assertHasResetHookImplementations(FALSE, $moduleHandler);
+    $moduleHandler->setModuleList($backupModuleList);
+    $this->assertHasResetHookImplementations(TRUE, $moduleHandler);
+
+    // Uninstalling a module triggers ->resetImplementations().
+    /** @var \Drupal\Core\Extension\ModuleInstallerInterface $moduleInstaller */
+    $moduleInstaller = \Drupal::service('module_installer');
+    $moduleInstaller->uninstall(['module_test']);
+    $this->assertSame($moduleListWithout, $moduleHandler->getModuleList());
+    $this->assertHasResetHookImplementations(FALSE, $moduleHandler);
+  }
+
+  /**
+   * Asserts whether certain hook implementations exist.
+   *
+   * This is used to verify that all internal hook cache properties have been
+   * reset and updated.
+   *
+   * @param bool $exists
+   *   TRUE if the implementations are expected to exist, FALSE if not.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
+   *   The module handler.
+   *
+   * @see \module_test_test_reset_implementations_hook()
+   * @see \module_test_test_reset_implementations_alter()
+   */
+  protected function assertHasResetHookImplementations(bool $exists, ModuleHandlerInterface $moduleHandler): void {
+    $this->assertSame($exists, $moduleHandler->hasImplementations('test_reset_implementations_hook'));
+    $this->assertSame($exists, $moduleHandler->hasImplementations('test_reset_implementations_alter'));
+    $expected_list = $exists ? ['module_test_test_reset_implementations_hook'] : [];
+    $this->assertSame($expected_list, $moduleHandler->invokeAll('test_reset_implementations_hook'));
+    $expected_alter_list = $exists ? ['module_test_test_reset_implementations_alter'] : [];
+    $alter_list = [];
+    $moduleHandler->alter('test_reset_implementations', $alter_list);
+    $this->assertSame($expected_alter_list, $alter_list);
   }
 
 }
