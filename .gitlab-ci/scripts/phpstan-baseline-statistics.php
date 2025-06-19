1de2d93b5140d0ebb786b4a00fb5de8d132a0e88
catch <6915-catch@users.noreply.drupalcode.org>
2025-06-19 10:00:55 +0100

    Issue #3525031 by mondrake, andypost: [CI] Run PHPStan job on PHP 8.4
    (cherry picked from commit 661709200386761e7967b3bab119d3c82db60b65)

--- .gitlab-ci/scripts/phpstan-baseline-statistics.php
+++ .gitlab-ci/scripts/phpstan-baseline-statistics.php
@@ -13,6 +13,12 @@ if (PHP_SAPI !== 'cli') {
 $ignoreErrors = [];
 require __DIR__ . '/../../core/.phpstan-baseline.php';
 
+$outputFilePath = __DIR__ . "/../../";
+if ($artifactsDirectory = getenv('_ARTIFACTS_DIR')) {
+  $outputFilePath .= $artifactsDirectory . \DIRECTORY_SEPARATOR;
+}
+$outputFilePath .= "phpstan-metrics.txt";
+
 $stats = ['__total' => 0];
 foreach ($ignoreErrors as $ignore) {
   $identifier = $ignore['identifier'] ?? '* not specified *';
@@ -27,12 +33,20 @@ echo "----------------------------------------\n";
 echo sprintf("%6d * Total baselined errors\n", $stats['__total']);
 echo "----------------------------------------\n";
 echo "Breakdown by error identifier:\n";
-file_put_contents(__DIR__ . '/../../phpstan-metrics.txt', 'phpstan-baseline ' . $stats['__total'] . PHP_EOL, FILE_APPEND);
+file_put_contents(
+  $outputFilePath,
+  'phpstan-baseline ' . $stats['__total'] . \PHP_EOL,
+  \FILE_APPEND,
+);
 
 unset($stats['__total']);
 arsort($stats);
 
 foreach ($stats as $identifier => $stat) {
   echo sprintf("%6d %s\n", $stat, $identifier);
-  file_put_contents(__DIR__ . '/../../phpstan-metrics.txt', 'phpstan-baseline.' . $identifier . ' ' . $stat . PHP_EOL, FILE_APPEND);
+  file_put_contents(
+    $outputFilePath,
+    'phpstan-baseline.' . $identifier . ' ' . $stat . \PHP_EOL,
+    \FILE_APPEND
+  );
 }
