86faa69f084736a8d7f805848c4c3e27ac79075d
Dave Long <dave@longwaveconsulting.com>
2025-07-08 11:05:11 +0100

    Issue #3278759 by mxr576, kristiaanvandeneynde, ericgsmith, acbramley, larowlan, danflanagan8, bbrala, rosk0, catch: Access cacheability is not correct when "view own unpublished content" is in use

--- core/modules/node/tests/node_access_test_auto_bubbling/src/Controller/NodeAccessTestAutoBubblingController.php
+++ core/modules/node/tests/node_access_test_auto_bubbling/src/Controller/NodeAccessTestAutoBubblingController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\node_access_test_auto_bubbling\Controller;
 
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\node\NodeInterface;
@@ -25,6 +26,30 @@ class NodeAccessTestAutoBubblingController extends ControllerBase implements Con
       ->range(0, 3)
       ->execute();
     return ['#markup' => $this->t('The three latest nodes are: @nids.', ['@nids' => implode(', ', $nids)])];
+  }
+
+  /**
+   * Exposes the node label when a user has access to view a node.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   A node entity.
+   *
+   * @return array
+   *   A render array.
+   */
+  public function nodeAccessCacheability(NodeInterface $node) {
+    $build = [];
+    $cacheability = new CacheableMetadata();
+    $access = $node->access('view', return_as_object: TRUE);
+    $cacheability->addCacheableDependency($access);
+    if ($access->isAllowed()) {
+      $build[] = [
+        '#markup' => $node->label(),
+      ];
+      $cacheability->addCacheableDependency($node);
+    }
+    $cacheability->applyTo($build);
+    return $build;
   }
 
 }
