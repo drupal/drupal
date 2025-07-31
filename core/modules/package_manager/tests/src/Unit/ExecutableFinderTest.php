da488722ca0bd64d2ca164473eb0038622390afd
catch <6915-catch@users.noreply.drupalcode.org>
2025-07-31 12:17:04 +0100

    Issue #3537668 by phenaproxima: Dynamically figure out the actual path to Composer's binary, and make it read-only
    (cherry picked from commit e8e948120acfb579189b52466860ad57d068583d)

--- core/modules/package_manager/tests/src/Unit/ExecutableFinderTest.php
+++ core/modules/package_manager/tests/src/Unit/ExecutableFinderTest.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\package_manager\Unit;
 
+use ColinODell\PsrTestLogger\TestLogger;
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\package_manager\ExecutableFinder;
 use Drupal\Tests\UnitTestCase;
 use org\bovigo\vfs\vfsStream;
 use PhpTuf\ComposerStager\API\Finder\Service\ExecutableFinderInterface;
+use PHPUnit\Framework\Attributes\TestWith;
 
 /**
  * @covers \Drupal\package_manager\ExecutableFinder
@@ -32,26 +36,45 @@ class ExecutableFinderTest extends UnitTestCase {
     $decorated->find('composer')->shouldNotBeCalled();
     $decorated->find('rsync')->shouldBeCalled();
 
-    $finder = new ExecutableFinder($decorated->reveal(), $config_factory);
+    $finder = new ExecutableFinder(
+      $decorated->reveal(),
+      $config_factory,
+      $this->prophesize(FileSystemInterface::class)->reveal(),
+    );
     $this->assertSame('/path/to/composer', $finder->find('composer'));
     $finder->find('rsync');
   }
 
   /**
    * Tests that the executable finder tries to use a local copy of Composer.
+   *
+   * @param bool $chmod_result
+   *   Whether the Composer binary will be successfully made read-only.
    */
-  public function testComposerInstalledInProject(): void {
+  #[TestWith([TRUE])]
+  #[TestWith([FALSE])]
+  public function testComposerInstalledInProject(bool $chmod_result): void {
     vfsStream::setup('root', NULL, [
       'composer-path' => [
-        'bin' => [],
+        'bin' => [
+          'composer' => 'A fake Composer executable',
+        ],
+        'composer.json' => Json::encode([
+          'bin' => ['bin/composer'],
+        ]),
       ],
     ]);
-    $composer_path = 'vfs://root/composer-path/bin/composer';
-    touch($composer_path);
-    $this->assertFileExists($composer_path);
+    $composer_bin = 'vfs://root/composer-path/bin/composer';
+    $this->assertTrue(chmod($composer_bin, 0755));
 
     $decorated = $this->prophesize(ExecutableFinderInterface::class);
     $decorated->find('composer')->willReturn('the real Composer');
+
+    // The Composer binary is executable and should be made read-only.
+    $file_system = $this->prophesize(FileSystemInterface::class);
+    $file_system->chmod($composer_bin, 0644)
+      ->willReturn($chmod_result)
+      ->shouldBeCalled();
 
     $finder = new ExecutableFinder(
       $decorated->reveal(),
@@ -60,14 +83,28 @@ class ExecutableFinderTest extends UnitTestCase {
           'executables' => [],
         ],
       ]),
+      $file_system->reveal(),
     );
-    $reflector = new \ReflectionProperty($finder, 'composerPath');
-    $reflector->setValue($finder, $composer_path);
-    $this->assertSame($composer_path, $finder->find('composer'));
+
+    $logger = new TestLogger();
+    $finder->setLogger($logger);
+
+    $reflector = new \ReflectionProperty($finder, 'composerPackagePath');
+    $reflector->setValue($finder, dirname($composer_bin, 2));
+    $this->assertSame($composer_bin, $finder->find('composer'));
+
+    // If the permissions change will fail, a warning should be logged.
+    $predicate = function (array $record) use ($composer_bin): bool {
+      return (
+        $record['message'] === 'Composer was found at %path, but could not be made read-only.' &&
+        $record['context']['%path'] === $composer_bin
+      );
+    };
+    $this->assertSame(!$chmod_result, $logger->hasRecordThatPasses($predicate));
 
     // If the executable disappears, or Composer isn't locally installed, the
     // decorated executable finder should be called.
-    unlink($composer_path);
+    unlink($composer_bin);
     $this->assertSame('the real Composer', $finder->find('composer'));
 
     $reflector->setValue($finder, FALSE);
