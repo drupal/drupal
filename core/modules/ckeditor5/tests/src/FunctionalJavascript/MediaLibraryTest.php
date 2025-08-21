9e0dd59428d706f75c5b5e1e7e4bd13f13f4e060
catch <6915-catch@users.noreply.drupalcode.org>
2025-08-21 19:17:47 +0100

    Issue #3351597 by acbramley, smustgrave, catch: [random test failure] Drupal\Tests\ckeditor5\FunctionalJavascript\MediaLibraryTest

--- core/modules/ckeditor5/tests/src/FunctionalJavascript/MediaLibraryTest.php
+++ core/modules/ckeditor5/tests/src/FunctionalJavascript/MediaLibraryTest.php
@@ -174,9 +174,7 @@ class MediaLibraryTest extends WebDriverTestBase {
       $this->assertSame($expected_tab_order[$key], $tab->getText());
     }
 
-    $assert_session->pageTextContains('0 of 1 item selected');
     $assert_session->elementExists('css', '.js-media-library-item')->click();
-    $assert_session->pageTextContains('1 of 1 item selected');
     $assert_session->elementExists('css', '.ui-dialog-buttonpane')->pressButton('Insert selected');
     $this->assertNotEmpty($assert_session->waitForElementVisible('css', $media_preview_selector, 1000));
     $xpath = new \DOMXPath($this->getEditorDataAsDom());
