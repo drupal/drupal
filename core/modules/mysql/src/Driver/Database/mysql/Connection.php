4b2163cd0cc01f3bb5cb2966556541c577fbf260
catch <6915-catch@users.noreply.drupalcode.org>
2025-08-29 21:47:56 +0100

    Issue #3543710 by andypost, mondrake: Address PHP 8.5 deprecations of driver specific PDO constants and methods

--- core/modules/mysql/src/Driver/Database/mysql/Connection.php
+++ core/modules/mysql/src/Driver/Database/mysql/Connection.php
@@ -10,6 +10,7 @@ use Drupal\Core\Database\DatabaseNotFoundException;
 use Drupal\Core\Database\StatementWrapperIterator;
 use Drupal\Core\Database\SupportsTemporaryTablesInterface;
 use Drupal\Core\Database\Transaction\TransactionManagerInterface;
+use PDO\Mysql;
 
 /**
  * @addtogroup database
@@ -121,16 +122,19 @@ class Connection extends DatabaseConnection implements SupportsTemporaryTablesIn
       \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
       // So we don't have to mess around with cursors and unbuffered queries by
       // default.
-      \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => TRUE,
+      // @phpstan-ignore class.notFound
+      (\PHP_VERSION_ID < 80400 ? \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY : Mysql::ATTR_USE_BUFFERED_QUERY) => TRUE,
       // Make sure MySQL returns all matched rows on update queries including
       // rows that actually didn't have to be updated because the values didn't
       // change. This matches common behavior among other database systems.
-      \PDO::MYSQL_ATTR_FOUND_ROWS => TRUE,
+      // @phpstan-ignore class.notFound
+      (\PHP_VERSION_ID < 80400 ? \PDO::MYSQL_ATTR_FOUND_ROWS : Mysql::ATTR_FOUND_ROWS) => TRUE,
       // Because MySQL's prepared statements skip the query cache, because it's
       // dumb.
       \PDO::ATTR_EMULATE_PREPARES => TRUE,
       // Limit SQL to a single statement like mysqli.
-      \PDO::MYSQL_ATTR_MULTI_STATEMENTS => FALSE,
+      // @phpstan-ignore class.notFound
+      (\PHP_VERSION_ID < 80400 ? \PDO::MYSQL_ATTR_MULTI_STATEMENTS : Mysql::ATTR_MULTI_STATEMENTS) => FALSE,
       // Convert numeric values to strings when fetching. In PHP 8.1,
       // \PDO::ATTR_EMULATE_PREPARES now behaves the same way as non emulated
       // prepares and returns integers. See https://externals.io/message/113294
