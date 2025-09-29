a45b634dfd97b585a82f7dd7a2cac214218e13c3
quietone <quietone@2572884.no-reply.drupal.org>
2025-09-29 21:57:00 +1300

    Issue #3532771 by jepster_, quietone, smustgrave, xjm, dcam, poker10: Misleading method parameter comment for

--- core/lib/Drupal/Core/Mail/MailManager.php
+++ core/lib/Drupal/Core/Mail/MailManager.php
@@ -208,7 +208,8 @@ class MailManager extends DefaultPluginManager implements MailManagerInterface {
    *   to provide translatable markup to display as a message if an error
    *   occurs, or set this to false to disable error display.
    * @param string|null $reply
-   *   Optional email address to be used to answer.
+   *   (optional) The email address or addresses to set as the reply-to header.
+   *   The format must be the same as the $to parameter.
    * @param bool $send
    *   If TRUE, call an implementation of
    *   \Drupal\Core\Mail\MailInterface->mail() to deliver the message, and
