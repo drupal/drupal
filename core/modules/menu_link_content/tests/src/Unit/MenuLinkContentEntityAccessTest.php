13d06af09f2f5c7a52ffb63df959d112d07a1e94
Alex Pott <alex.a.pott@googlemail.com>
2025-11-03 17:31:47 +0000

    Issue #3555522 by tobiasb: Fix account mock in \Drupal\Tests\menu_link_content\Unit\MenuLinkContentEntityAccessTest::testUnrecognizedOperation for php 8.5
    (cherry picked from commit ca32fdf4d2eeb6ed15bebfd742b09b7e8d64169e)

--- core/modules/menu_link_content/tests/src/Unit/MenuLinkContentEntityAccessTest.php
+++ core/modules/menu_link_content/tests/src/Unit/MenuLinkContentEntityAccessTest.php
@@ -46,7 +46,9 @@ class MenuLinkContentEntityAccessTest extends UnitTestCase {
       ->willReturn($language);
 
     $account = $this->createMock(AccountInterface::class);
-
+    $account->expects($this->atLeastOnce())
+      ->method('id')
+      ->willReturn(42);
     $accessControl = new MenuLinkContentAccessControlHandler($entityType, $accessManager);
     $accessControl->setModuleHandler($moduleHandler);
     $access = $accessControl->access($entity, 'not-an-op', $account, TRUE);
