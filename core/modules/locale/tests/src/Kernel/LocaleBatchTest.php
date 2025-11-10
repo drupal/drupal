76737efbd55cc5a79fc028bf6e896e1b6009e03d
catch <6915-catch@users.noreply.drupalcode.org>
2025-11-10 10:45:57 +0000

    Issue #3218674 by promes, anybody, tobiasb, ressa, jordan.jamous, grevil, grimreaper: "Translation file not found" for English when "translate_english" is false
    (cherry picked from commit 84b3f93f8532ae6103e596066cae7739c74acef4)

--- core/modules/locale/tests/src/Kernel/LocaleBatchTest.php
+++ core/modules/locale/tests/src/Kernel/LocaleBatchTest.php
@@ -47,4 +47,66 @@ class LocaleBatchTest extends KernelTestBase {
     $this->assertEquals('Ignoring already imported translation for drupal.', $context['message']);
   }
 
+  /**
+   * Tests English translations skip default drupal.org pattern.
+   */
+  public function testEnglishTranslationSkipsDefaultPattern(): void {
+    $this->installConfig(['locale', 'language']);
+    $this->installSchema('locale', ['locales_source', 'locales_target', 'locale_file']);
+    $this->container->get('module_handler')->loadInclude('locale', 'batch.inc');
+
+    // Create source matching default drupal.org pattern.
+    $source = (object) [
+      'name' => 'test_module',
+      'langcode' => 'en',
+      'project' => 'test_module',
+      'version' => '1.0.0',
+      'core' => 'all',
+      'files' => [
+        LOCALE_TRANSLATION_REMOTE => (object) [
+          'uri' => 'https://ftp.drupal.org/files/translations/all/test_module/test_module-1.0.0.en.po',
+        ],
+      ],
+    ];
+
+    \Drupal::keyValue('locale.translation_status')->setMultiple(['test_module' => ['en' => $source]]);
+
+    $context = ['results' => []];
+    locale_translation_batch_status_check('test_module', 'en', ['use_remote' => TRUE, 'finish_feedback' => TRUE], $context);
+
+    // Should be marked as failed (skipped) for English default pattern.
+    $this->assertContains('test_module', $context['results']['failed_files']);
+    $this->assertCount(0, $context['results']['files'] ?? []);
+  }
+
+  /**
+   * Tests non-English languages do not skip default drupal.org pattern.
+   */
+  public function testNonEnglishLanguagesUnaffected(): void {
+    $this->installConfig(['locale', 'language']);
+    $this->installSchema('locale', ['locales_source', 'locales_target', 'locale_file']);
+    $this->container->get('module_handler')->loadInclude('locale', 'batch.inc');
+
+    $source = (object) [
+      'name' => 'test_module',
+      'langcode' => 'de',
+      'project' => 'test_module',
+      'version' => '1.0.0',
+      'core' => 'all',
+      'files' => [
+        LOCALE_TRANSLATION_REMOTE => (object) [
+          'uri' => 'https://ftp.drupal.org/files/translations/all/test_module/test_module-1.0.0.de.po',
+        ],
+      ],
+    ];
+
+    \Drupal::keyValue('locale.translation_status')->setMultiple(['test_module' => ['de' => $source]]);
+
+    $context = ['results' => []];
+    locale_translation_batch_status_check('test_module', 'de', ['use_remote' => TRUE, 'finish_feedback' => TRUE], $context);
+
+    $this->assertContains('test_module', $context['results']['files']);
+    $this->assertCount(0, $context['results']['failed_files'] ?? []);
+  }
+
 }
