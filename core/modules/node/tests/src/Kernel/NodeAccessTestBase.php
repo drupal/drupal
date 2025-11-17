c1328dfa59556523b548844cffd09d2807549f3f
catch <6915-catch@users.noreply.drupalcode.org>
2025-11-17 09:26:58 +0000

    fix: #3557391 Fix deprecations in \Drupal\Core\Entity\EntityAccessControlHandler caused by kernel tests
    By: @alexpott
    (cherry picked from commit 55174bf909e6a9f8eb27d7043a57d20c924fef25)

--- core/modules/node/tests/src/Kernel/NodeAccessTestBase.php
+++ core/modules/node/tests/src/Kernel/NodeAccessTestBase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\node\Kernel;
 
+use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\node\NodeInterface;
@@ -109,13 +110,13 @@ abstract class NodeAccessTestBase extends KernelTestBase {
    *   Whether access should be granted or not.
    * @param \Drupal\Core\Session\AccountInterface $account
    *   The user account for which to check access.
-   * @param string|null $langcode
+   * @param string $langcode
    *   (optional) The language code indicating which translation of the node
-   *   to check. If NULL, the untranslated (fallback) access is checked.
+   *   to check. If omitted, the untranslated (fallback) access is checked.
    *
    * @internal
    */
-  public function assertNodeCreateAccess(string $bundle, bool $result, AccountInterface $account, ?string $langcode = NULL) {
+  public function assertNodeCreateAccess(string $bundle, bool $result, AccountInterface $account, string $langcode = LanguageInterface::LANGCODE_DEFAULT) {
     $this->assertEquals($result, $this->accessHandler->createAccess($bundle, $account, [
       'langcode' => $langcode,
     ]), $this->nodeAccessAssertMessage('create', $result, $langcode));
