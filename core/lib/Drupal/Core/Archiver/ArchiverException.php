f619069c4a357c5c8f6e7abcb4714cf9a314236f
catch <6915-catch@users.noreply.drupalcode.org>
2025-11-17 16:25:06 +0000

    fix: #3556706 Deprecate ArchiverManager and ArchiverInterface
    By: @longwave

--- core/lib/Drupal/Core/Archiver/ArchiverException.php
+++ core/lib/Drupal/Core/Archiver/ArchiverException.php
@@ -2,7 +2,14 @@
 
 namespace Drupal\Core\Archiver;
 
+@trigger_error('\Drupal\Core\Archiver\ArchiverException is deprecated in drupal:11.3.0 and is removed from drupal:12.0.0. There is no replacement. See https://www.drupal.org/node/3556927', E_USER_DEPRECATED);
+
 /**
  * Defines an exception class for Drupal\Core\Archiver\ArchiverInterface.
+ *
+ * @deprecated in drupal:11.3.0 and is removed from drupal:12.0.0. There is no
+ *   replacement.
+ *
+ * @see https://www.drupal.org/node/3556927
  */
 class ArchiverException extends \Exception {}
--- core/lib/Drupal/Core/Archiver/Attribute/Archiver.php
+++ core/lib/Drupal/Core/Archiver/Attribute/Archiver.php
@@ -12,6 +12,10 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
  *
  * For a working example, see \Drupal\system\Plugin\Archiver\Zip
  *
+ * @deprecated in drupal:11.3.0 and is removed from drupal:12.0.0. There is no
+ *   replacement.
+ *
+ * @see https://www.drupal.org/node/3556927
  * @see \Drupal\Core\Archiver\ArchiverManager
  * @see \Drupal\Core\Archiver\ArchiverInterface
  * @see plugin_api
