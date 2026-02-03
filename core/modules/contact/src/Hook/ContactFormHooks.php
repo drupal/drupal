47ae02bb65847c3ca566a539f8798adaca3051a7
catch <6915-catch@users.noreply.drupalcode.org>
2026-02-03 07:56:36 +0000

    task: #3566888 Move contact module form callbacks from procedural to hook class
    By: claudiu.cristea
    By: nicxvan

--- core/modules/contact/src/Hook/ContactFormHooks.php
+++ core/modules/contact/src/Hook/ContactFormHooks.php
@@ -19,7 +19,7 @@ class ContactFormHooks {
   public function __construct(
     protected readonly AccountInterface $currentUser,
     protected readonly UserDataInterface $userData,
-    protected readonly configFactoryInterface $configFactory,
+    protected readonly ConfigFactoryInterface $configFactory,
   ) {}
 
   /**
@@ -47,7 +47,22 @@ class ContactFormHooks {
       '#default_value' => $account_data ?? $this->configFactory->getEditable('contact.settings')->get('user_default_enabled'),
       '#description' => $this->t('Allow other users to contact you via a personal contact form which keeps your email address hidden. Note that some privileged users such as site administrators are still able to contact you even if you choose to disable this feature.'),
     ];
-    $form['actions']['submit']['#submit'][] = 'contact_user_profile_form_submit';
+    $form['actions']['submit']['#submit'][] = self::class . ':profileFormSubmit';
+  }
+
+  /**
+   * Provides an additional submit handler for \Drupal\user\ProfileForm form.
+   *
+   * @param array $form
+   *   The form render array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state instance.
+   */
+  public function profileFormSubmit(array $form, FormStateInterface $form_state): void {
+    $account = $form_state->getFormObject()->getEntity();
+    if ($account->id() && $form_state->hasValue('contact')) {
+      $this->userData->set('contact', $account->id(), 'enabled', (int) $form_state->getValue('contact'));
+    }
   }
 
   /**
@@ -69,8 +84,31 @@ class ContactFormHooks {
       '#description' => $this->t('Changing this setting will not affect existing users.'),
       '#default_value' => $this->configFactory->getEditable('contact.settings')->get('user_default_enabled'),
     ];
-    // Add submit handler to save contact configuration.
-    $form['#submit'][] = 'contact_form_user_admin_settings_submit';
+    // Add submit handler to save contact configuration. Note that, in this
+    // case, it's not possible to simply use the #config_target key. The reason
+    // is that, with #config_target, the entire contact.settings object gets
+    // validated. But values, other than `user_default_enabled` might not pass
+    // the validation, and there's nothing the user can do, as such values are
+    // not exposed by this form alter. A typical case can be observed when
+    // installing the Contact module. The default configuration shipped with the
+    // module sets contact.settings:default_form value to 'feedback' but the
+    // module doesn't ship any contact form config entity with this ID, hence
+    // the config validation will fail.
+    $form['#submit'][] = self::class . ':userAdminSettingsSubmit';
+  }
+
+  /**
+   * Provides an additional submit handler for 'user_admin_settings' form.
+   *
+   * @param array $form
+   *   The form render array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state instance.
+   */
+  public function userAdminSettingsSubmit(array $form, FormStateInterface $form_state): void {
+    $this->configFactory->getEditable('contact.settings')
+      ->set('user_default_enabled', $form_state->getValue('contact_default_status'))
+      ->save();
   }
 
 }
