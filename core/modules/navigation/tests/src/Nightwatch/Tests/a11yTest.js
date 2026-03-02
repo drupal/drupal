87c6eea50b5b1faca1aa8937976486b173513182
Lauri Timmanee <lauri.timmanee@acquia.com>
2026-03-02 17:36:34 +0000

    fix: #3551528 Mobile expanded sidebar modal overlay has invalid ARIA attribute: aria-expanded
    By: kentr
    By: mherchel
    By: smustgrave
    By: bernardm28

--- core/modules/navigation/tests/src/Nightwatch/Tests/a11yTest.js
+++ core/modules/navigation/tests/src/Nightwatch/Tests/a11yTest.js
@@ -6,8 +6,7 @@ const navigationTest = {
       .drupalInstall({
         installProfile: 'nightwatch_a11y_testing',
       })
-      .drupalInstallModule('navigation', true)
-      .setWindowSize(1220, 800);
+      .drupalInstallModule('navigation', true);
   },
   after(browser) {
     browser.drupalUninstall();
@@ -20,9 +19,27 @@ testCases.forEach((testCase) => {
     browser,
   ) => {
     browser.drupalLoginAsAdmin(() => {
+      browser.drupalRelativeURL(testCase.path).axeInject();
+
+      // Wide viewport ("desktop") version.
+      browser.setWindowSize(1220, 800).axeRun('body', testCase.options || {});
+
+      // Narrow viewport ("mobile") version.
       browser
-        .drupalRelativeURL(testCase.path)
-        .axeInject()
+        .setWindowSize(1000, 800)
+        .element.find({
+          selector:
+            '//*[@class="admin-toolbar-control-bar"]//button//*[text()="Expand sidebar"]',
+          locateStrategy: 'xpath',
+        })
+        .waitUntil('visible')
+        .click();
+
+      browser
+        .waitForElementVisible({
+          selector: '//*[@id="admin-toolbar"]//button//*[text()="Create"]',
+          locateStrategy: 'xpath',
+        })
         .axeRun('body', testCase.options || {});
     });
   };
