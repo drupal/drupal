1bcdac075aa48e010a74daaf5a4d2a59c5c93030
Alex Pott <alex.a.pott@googlemail.com>
2026-03-09 22:21:39 +0000

    fix: #3576648 Skip automated cron on all CLI commands
    By: mstrelan
    By: godotislate

--- core/modules/automated_cron/tests/src/Kernel/AutomatedCronTest.php
+++ core/modules/automated_cron/tests/src/Kernel/AutomatedCronTest.php
@@ -23,11 +23,11 @@ class AutomatedCronTest extends KernelTestBase {
   protected static $modules = ['automated_cron'];
 
   /**
-   * Tests that automated cron runs cron on an HTTP request.
+   * Tests that automated cron does not run cron on a CLI request.
    *
    * @legacy-covers \Drupal\automated_cron\EventSubscriber\AutomatedCron::onTerminate
    */
-  public function testRunsCronOnHttpRequest(): void {
+  public function testCronDoesNotRunOnCliRequest(): void {
     // Set automated_cron interval and times.
     // Any interval > 0 should work.
     $this->config('automated_cron.settings')->set('interval', 10800)->save();
@@ -36,13 +36,12 @@ class AutomatedCronTest extends KernelTestBase {
     // Cron uses `$_SERVER['REQUEST_TIME']` to set `system.cron_last`
     // because there is no request stack, so we set the request time
     // to the same.
-    $expected = $_SERVER['REQUEST_TIME'];
-    $request->server->set('REQUEST_TIME', $expected);
+    $request->server->set('REQUEST_TIME', $_SERVER['REQUEST_TIME']);
 
     // Invoke `AutomatedCron::onTerminate` and check result.
     $this->assertNull($this->container->get('state')->get('system.cron_last'));
     $this->container->get('kernel')->terminate($request, new Response());
-    $this->assertEquals($expected, $this->container->get('state')->get('system.cron_last'));
+    $this->assertNull($this->container->get('state')->get('system.cron_last'));
   }
 
 }
