6af1da00000eced413fd4fc93e288a44ca36b8ad
godotislate <godotislate@3483619.no-reply.drupal.org>
2026-04-08 13:32:10 -0700

    fix: #3526541 Cannot set default values on multi-valued formatted Text fields
    By: smustgrave
    By: ewout goosmann
    By: julio_retkwa
    By: oily
    By: dcam
    By: godotislate

---
+++ core/modules/text/tests/src/Unit/Plugin/Field/FieldType/TextFieldItemListTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\text\Unit\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Tests\UnitTestCase;
+use Drupal\text\Plugin\Field\FieldType\TextFieldItemList;
+
+/**
+ * Tests TextFieldItemList.
+ *
+ * @coversClass \Drupal\text\Plugin\Field\FieldType\TextFieldItemList
+ * @group text
+ */
+class TextFieldItemListTest extends UnitTestCase {
+
+  /**
+   * Tests ::defaultValuesFormValidate() if the field allows multiple values.
+   *
+   * Ensures that the add_more button is filtered out of the submitted values
+   * before validation, preventing PHP warnings due to trying to access array
+   * keys on a TranslatableMarkup object.
+   */
+  public function testDefaultValuesFormValidateWithMultipleCardinality(): void {
+    $field_name = 'field_test_text';
+    $definition = $this->createMock(FieldDefinitionInterface::class);
+    $definition->expects($this->once())
+      ->method('getName')
+      ->willReturn($field_name);
+    $definition->expects($this->once())
+      ->method('getSetting')
+      ->with('allowed_formats')
+      ->willReturn(['restricted_html']);
+    $form_state = $this->createMock(FormStateInterface::class);
+    $form_state->expects($this->once())
+      ->method('getValue')
+      ->with(['default_value_input', $field_name])
+      ->willReturn([
+        'add_more' => new TranslatableMarkup('Add another item'),
+      ]);
+    $form_state->expects($this->once())
+      ->method('has')
+      ->with('default_value_widget')
+      ->willReturn(TRUE);
+    $form_state->expects($this->once())
+      ->method('get')
+      ->with('default_value_widget')
+      ->willReturn([]);
+
+    $item_list = new TextFieldItemList($definition);
+    $form = [];
+    $item_list->defaultValuesFormValidate([], $form, $form_state);
+  }
+
+}
