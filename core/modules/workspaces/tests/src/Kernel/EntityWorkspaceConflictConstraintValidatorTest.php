05f04511ce002f717ca81689fb73ff4e5e6d8617
Andrei Mateescu <amateescu@729614.no-reply.drupal.org>
2026-04-22 18:29:10 +0300

    task: #3582184 Remove obsolete reloadEntity() from EntityWorkspaceConflictConstraintValidatorTest
    By: stborchert
    By: joachim
    By: amateescu
    (cherry picked from commit 6a83ca350d6217d743f7a70c89f8fea8aa8cbc88)

--- core/modules/workspaces/tests/src/Kernel/EntityWorkspaceConflictConstraintValidatorTest.php
+++ core/modules/workspaces/tests/src/Kernel/EntityWorkspaceConflictConstraintValidatorTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\workspaces\Kernel;
 
-use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\entity_test\Entity\EntityTestMulRevPub;
 use Drupal\KernelTests\KernelTestBase;
@@ -74,20 +73,17 @@ class EntityWorkspaceConflictConstraintValidatorTest extends KernelTestBase {
     $entity = EntityTestMulRevPub::create();
     $this->assertCount(0, $entity->validate());
     $entity->save();
-    $entity = $this->reloadEntity($entity);
     $this->assertCount(0, $entity->validate());
 
     // Edit the entity in Stage.
     $this->switchToWorkspace('stage');
     $entity->save();
-    $entity = $this->reloadEntity($entity);
     $this->assertCount(0, $entity->validate());
 
     $expected_message = 'The content is being edited in the Stage workspace. As a result, your changes cannot be saved.';
 
     // Check that the entity can no longer be edited in Live.
     $this->switchToLive();
-    $entity = $this->reloadEntity($entity);
     $violations = $entity->validate();
     $this->assertCount(1, $violations);
     $this->assertSame($expected_message, (string) $violations->get(0)->getMessage());
@@ -95,34 +91,29 @@ class EntityWorkspaceConflictConstraintValidatorTest extends KernelTestBase {
     // Check that the entity can no longer be edited in another top-level
     // workspace.
     $this->switchToWorkspace('other');
-    $entity = $this->reloadEntity($entity);
     $violations = $entity->validate();
     $this->assertCount(1, $violations);
     $this->assertSame($expected_message, (string) $violations->get(0)->getMessage());
 
     // Check that the entity can still be edited in a sub-workspace of Stage.
     $this->switchToWorkspace('dev');
-    $entity = $this->reloadEntity($entity);
     $this->assertCount(0, $entity->validate());
 
     // Edit the entity in Dev.
     $this->switchToWorkspace('dev');
     $entity->save();
-    $entity = $this->reloadEntity($entity);
     $this->assertCount(0, $entity->validate());
 
     $expected_message = 'The content is being edited in the Dev workspace. As a result, your changes cannot be saved.';
 
     // Check that the entity can no longer be edited in Live.
     $this->switchToLive();
-    $entity = $this->reloadEntity($entity);
     $violations = $entity->validate();
     $this->assertCount(1, $violations);
     $this->assertSame($expected_message, (string) $violations->get(0)->getMessage());
 
     // Check that the entity can no longer be edited in the parent workspace.
     $this->switchToWorkspace('stage');
-    $entity = $this->reloadEntity($entity);
     $violations = $entity->validate();
     $this->assertCount(1, $violations);
     $this->assertSame($expected_message, (string) $violations->get(0)->getMessage());
@@ -130,25 +121,9 @@ class EntityWorkspaceConflictConstraintValidatorTest extends KernelTestBase {
     // Check that the entity can no longer be edited in another top-level
     // workspace.
     $this->switchToWorkspace('other');
-    $entity = $this->reloadEntity($entity);
     $violations = $entity->validate();
     $this->assertCount(1, $violations);
     $this->assertSame($expected_message, (string) $violations->get(0)->getMessage());
-  }
-
-  /**
-   * Reloads the given entity from the storage and returns it.
-   *
-   * @param \Drupal\Core\Entity\EntityInterface $entity
-   *   The entity to be reloaded.
-   *
-   * @return \Drupal\Core\Entity\EntityInterface
-   *   The reloaded entity.
-   */
-  protected function reloadEntity(EntityInterface $entity): EntityInterface {
-    $storage = $this->entityTypeManager->getStorage($entity->getEntityTypeId());
-    $storage->resetCache([$entity->id()]);
-    return $storage->load($entity->id());
   }
 
 }
