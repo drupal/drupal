3826a181fb53028edc9b8bfed57dfc502a187ae8
catch <6915-catch@users.noreply.drupalcode.org>
2026-05-09 06:54:07 +0100

    fix: #3589156 [random test failure] AutomatedCronTest randomly fails
    By: catch
    By: longwave
    By: mstrelan
    (cherry picked from commit b078dad5f8f6ec61cdf762adb3b97c4bc7a01113)

--- core/modules/automated_cron/tests/src/Functional/AutomatedCronTest.php
+++ core/modules/automated_cron/tests/src/Functional/AutomatedCronTest.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\automated_cron\Functional;
 
+use Drupal\automated_cron\EventSubscriber\AutomatedCron;
 use Drupal\Tests\BrowserTestBase;
 use Drupal\Tests\WaitTerminateTestTrait;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
 
 /**
  * Tests for automated_cron.
  */
+#[CoversClass(AutomatedCron::class)]
 #[Group('automated_cron')]
 #[RunTestsInSeparateProcesses]
 class AutomatedCronTest extends BrowserTestBase {
@@ -21,23 +24,19 @@ class AutomatedCronTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected static $modules = ['automated_cron'];
-
-  /**
-   * {@inheritdoc}
-   */
   protected $defaultTheme = 'stark';
 
   /**
    * Tests that automated cron runs cron on an HTTP request.
-   *
-   * @legacy-covers \Drupal\automated_cron\EventSubscriber\AutomatedCron::onTerminate
    */
   public function testRunsCronOnHttpRequest(): void {
+    // Install cron manually to avoid test setup making an HTTP request which
+    // would trigger cron to run before the test starts.
+    \Drupal::service('module_installer')->install(['automated_cron']);
+
     // Set automated_cron interval and times.
     // Any interval > 0 should work.
     $this->config('automated_cron.settings')->set('interval', 10800)->save();
-    \Drupal::state()->delete('system.cron_last');
 
     $this->assertNull(\Drupal::state()->get('system.cron_last'));
     $this->assertNotNull($_SERVER['REQUEST_TIME']);
