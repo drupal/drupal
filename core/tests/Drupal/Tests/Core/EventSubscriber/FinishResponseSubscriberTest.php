c4c50b2dcf6461e01f79831d75246ffe205381b0
Alex Pott <alex.a.pott@googlemail.com>
2026-05-12 10:24:40 +0100

    Revert "fix: #2844620 Automatically split cache debug headers into multiple lines when they exceed 8k"
    This reverts commit f0cd52cd6326b16b5538f9b0d56fb0bebe52a484.

--- core/tests/Drupal/Tests/Core/EventSubscriber/FinishResponseSubscriberTest.php
+++ core/tests/Drupal/Tests/Core/EventSubscriber/FinishResponseSubscriberTest.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Drupal\Tests\Core\EventSubscriber;
 
 use Drupal\Component\Datetime\TimeInterface;
-use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Cache\CacheableResponse;
 use Drupal\Core\Cache\Context\CacheContextsManager;
 use Drupal\Core\EventSubscriber\FinishResponseSubscriber;
 use Drupal\Core\Language\Language;
@@ -71,9 +69,6 @@ class FinishResponseSubscriberTest extends UnitTestCase {
    */
   protected $time;
 
-  /**
-   * {@inheritdoc}
-   */
   protected function setUp(): void {
     parent::setUp();
 
@@ -149,101 +144,6 @@ class FinishResponseSubscriberTest extends UnitTestCase {
     // 'X-Content-Type-Options' will be unconditionally set by core.
     $this->assertEquals(['nosniff'], $response->headers->all('X-Content-Type-Options'));
     $this->assertEquals(['DENY'], $response->headers->all('X-Frame-Options'));
-  }
-
-  /**
-   * Finish subscriber outputs tags, context, max-age if debug is on.
-   */
-  public function testDebugHeaders(): void {
-    $finishSubscriber = new FinishResponseSubscriber(
-      $this->languageManager,
-      $this->getConfigFactoryStub(),
-      $this->requestPolicy,
-      $this->responsePolicy,
-      $this->cacheContextsManager,
-      $this->time,
-      TRUE
-    );
-
-    $this->languageManager->method('getCurrentLanguage')
-      ->willReturn(new Language(['id' => 'en']));
-
-    $this->cacheContextsManager->method('optimizeTokens')
-      ->willReturn(['context1', 'context2']);
-
-    $request = $this->createStub(Request::class);
-    $response = $this->createStub(CacheableResponse::class);
-    $response->headers = new ResponseHeaderBag();
-
-    // Set cache tags, context, max-age.
-    $cacheData = new CacheableMetadata()
-      ->setCacheTags(['tag1', 'tag2'])
-      ->setCacheContexts(['context1', 'context2'])
-      ->setCacheMaxAge(123);
-    $response->method('getCacheableMetadata')
-      ->willReturn($cacheData);
-
-    $event = new ResponseEvent($this->kernel, $request, HttpKernelInterface::MAIN_REQUEST, $response);
-    $finishSubscriber->onRespond($event);
-
-    // Check that X-Drupal-Cache-Tags is in the response header.
-    $this->assertSame(['tag1 tag2'], $response->headers->all('X-Drupal-Cache-Tags'));
-    $this->assertSame(['context1 context2'], $response->headers->all('X-Drupal-Cache-Contexts'));
-    $this->assertSame(['123'], $response->headers->all('X-Drupal-Cache-Max-Age'));
-  }
-
-  /**
-   * Tests that long tags and contexts headers are split into multiple lines.
-   */
-  public function testDebugCacheTagAndCacheContextsHeadersLength(): void {
-    $finishSubscriber = new FinishResponseSubscriber(
-      $this->languageManager,
-      $this->getConfigFactoryStub(),
-      $this->requestPolicy,
-      $this->responsePolicy,
-      $this->cacheContextsManager,
-      $this->time,
-      TRUE
-    );
-
-    $this->languageManager->method('getCurrentLanguage')
-      ->willReturn(new Language(['id' => 'en']));
-
-    $request = $this->createStub(Request::class);
-    $response = $this->createStub(CacheableResponse::class);
-    $response->headers = new ResponseHeaderBag();
-
-    // Create multiple cache tags that add up to more than 8k bytes. Each tag is
-    // 15 bytes. The tags imploded together will have a space between
-    // each value, so the total length is 8015.
-    for ($i = 0; $i < 501; $i++) {
-      $tags[] = 'cache-tag:' . str_pad("$i", 5, '0', STR_PAD_LEFT);
-    }
-
-    // For contexts, create multiple values that add up to more than 16k. Each
-    // context is 19 bytes. The contexts imploded together will have a space
-    // between each value, so the total length is 16019.
-    for ($i = 0; $i < 801; $i++) {
-      $contexts[] = 'cache-context:' . str_pad("$i", 5, '0', STR_PAD_LEFT);
-    }
-
-    $this->cacheContextsManager->method('optimizeTokens')
-      ->willReturn($contexts);
-
-    $cacheData = new CacheableMetadata()
-      ->setCacheTags($tags);
-
-    $response->method('getCacheableMetadata')
-      ->willReturn($cacheData);
-
-    $event = new ResponseEvent($this->kernel, $request, HttpKernelInterface::MAIN_REQUEST, $response);
-    $finishSubscriber->onRespond($event);
-
-    // Check that X-Drupal-Cache-Tags has been split into two lines.
-    $headers = (string) $response->headers;
-    $this->assertEquals(2, substr_count($headers, 'X-Drupal-Cache-Tags: '));
-    // Check that X-Drupal-Cache-Contexts has been split into three lines.
-    $this->assertEquals(3, substr_count($headers, 'X-Drupal-Cache-Contexts: '));
   }
 
 }
