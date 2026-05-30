f964c116991d3ae5d9c96706d49c13d80adc27af
catch <6915-catch@users.noreply.drupalcode.org>
2026-05-30 07:30:22 +0100

    task: #3582690 Remove or replace usages of Shortcut from themes
    By: smustgrave
    By: quietone
    By: dcam
    By: longwave
    By: nicxvan

--- core/themes/olivero/src/Hook/OliveroHooks.php
+++ core/themes/olivero/src/Hook/OliveroHooks.php
@@ -19,34 +19,6 @@ class OliveroHooks {
   use StringTranslationTrait;
 
   /**
-   * Implements hook_preprocess_HOOK() for page-title.
-   */
-  #[Hook('preprocess_page_title')]
-  public function preprocessPageTitle(array &$variables): void {
-    // Since the title and the shortcut link are both block level elements,
-    // positioning them next to each other is much simpler with a wrapper div.
-    if (!empty($variables['title_suffix']['add_or_remove_shortcut']) && $variables['title']) {
-      // Add a wrapper div using the title_prefix and title_suffix render
-      // elements.
-      $variables['title_prefix']['shortcut_wrapper'] = [
-        '#markup' => '<div class="shortcut-wrapper">',
-        '#weight' => 100,
-      ];
-      $variables['title_suffix']['shortcut_wrapper'] = [
-        '#markup' => '</div>',
-        '#weight' => -99,
-      ];
-      // Make sure the shortcut link is the first item in title_suffix.
-      $variables['title_suffix']['add_or_remove_shortcut']['#weight'] = -100;
-    }
-    // Unset shortcut link on front page.
-    $variables['is_front'] = \Drupal::service('path.matcher')->isFrontPage();
-    if ($variables['is_front'] === TRUE) {
-      unset($variables['title_suffix']['add_or_remove_shortcut']);
-    }
-  }
-
-  /**
    * Implements hook_preprocess_HOOK() for maintenance-page.
    */
   #[Hook('preprocess_maintenance_page')]
