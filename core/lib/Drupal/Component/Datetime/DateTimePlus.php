7b0fc80ebfadc8c6aefcad913d64e0263a624297
Dave Long <dave@longwaveconsulting.com>
2026-06-17 16:59:19 +0100

    SA-CORE-2026-006 by michaelmaturi, larowlan, mohit_aghera, akalata, benjifisher, cilefen, greggles, longwave, mcdruid, poker10, xjm

--- core/lib/Drupal/Component/Datetime/DateTimePlus.php
+++ core/lib/Drupal/Component/Datetime/DateTimePlus.php
@@ -64,10 +64,8 @@ class DateTimePlus implements \Stringable {
 
   /**
    * The DateTime object.
-   *
-   * @var \DateTime
    */
-  protected $dateTimeObject = NULL;
+  protected ?\DateTimeInterface $dateTimeObject = NULL;
 
   /**
    * Creates a date object from an input date object.
