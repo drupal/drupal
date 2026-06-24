56e090549959c0c28b0c4cecae22f7ccecf16dc3
catch <6915-catch@users.noreply.drupalcode.org>
2026-06-24 16:14:37 +0100

    task: #3594207 PHPUnit 13.2 - The data provider key must not be an empty string
    By: mondrake
    By: dcam

--- core/tests/Drupal/Tests/Core/Extension/ExtensionVersionTest.php
+++ core/tests/Drupal/Tests/Core/Extension/ExtensionVersionTest.php
@@ -445,12 +445,17 @@ class ExtensionVersionTest extends UnitTestCase {
    *   as a single item array
    */
   protected static function createKeyedTestCases(array $test_arguments): array {
-    return array_combine(
+    $return = array_combine(
       $test_arguments,
       array_map(function (string $test_argument): array {
         return [$test_argument];
       }, $test_arguments)
     );
+    if (isset($return[''])) {
+      $return['empty_string'] = $return[''];
+      unset($return['']);
+    }
+    return $return;
   }
 
 }
