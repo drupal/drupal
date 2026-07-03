c6b410288686450ccbf4fd8e56ef682e977bb335
godotislate <godotislate@3483619.no-reply.drupal.org>
2026-07-03 06:14:16 -0700

    fix: #3607938 Chunking of Op in loadFromDedicatedTables causes Cartesian multiplication
    By: jviitamaki
    By: thefancywizard
    By: catch
    By: kulturmensch
    By: longwave

--- core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
+++ core/profiles/demo_umami/tests/src/FunctionalJavascript/OpenTelemetryAuthenticatedPerformanceTest.php
@@ -130,7 +130,7 @@ class OpenTelemetryAuthenticatedPerformanceTest extends PerformanceTestBase {
     }, 'administratorNodePage');
 
     $expected = [
-      'QueryCount' => 270,
+      'QueryCount' => 279,
       'CacheGetCount' => 268,
       'CacheGetCountByBin' => [
         'config' => 60,
