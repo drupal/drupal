d75b4dddeb5d67d8703056f0e1506bc3d52f5c88
catch <6915-catch@users.noreply.drupalcode.org>
2026-07-09 17:49:14 +0100

    fix: #3609363 Regression: install_import_translations() no longer lets contrib/custom translations override core on install-from-config
    By: chr.fritsch
    By: nicxvan
    By: alexpott
    (cherry picked from commit 7056932df399b5e24f61f85012c6006b03a3cd59)

---
+++ core/modules/locale/tests/modules/locale_test_additional/src/Hook/LocaleTestAdditionalHooks.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\locale_test_additional\Hook;
+
+use Drupal\Core\Extension\Extension;
+use Drupal\Core\Hook\Attribute\Hook;
+
+/**
+ * Hook implementations for locale_test_extra.
+ */
+class LocaleTestAdditionalHooks {
+
+  /**
+   * Implements hook_system_info_alter().
+   *
+   * By default this modules is hidden but once enabled it behaves like a normal
+   * (not hidden) module. This hook implementation changes the .info.yml data by
+   * setting the hidden status to FALSE.
+   */
+  #[Hook('system_info_alter')]
+  public function systemInfoAlter(&$info, Extension $file, $type): void {
+    if ($file->getName() == 'locale_test_additional') {
+      // Don't hide the module.
+      $info['hidden'] = FALSE;
+    }
+  }
+
+}
