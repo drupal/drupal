642dbc744a75e4c2399f0d416272c6bba0a3acfe
godotislate <godotislate@3483619.no-reply.drupal.org>
2026-07-30 12:00:21 -0700

    task: #3613243 Convert block.module routes to attributes
    By: longwave
    (cherry picked from commit 8d4627eaf4c72a91f68e14c0378497f1a1cba316)

--- core/modules/block/src/Controller/BlockListController.php
+++ core/modules/block/src/Controller/BlockListController.php
@@ -4,8 +4,10 @@ namespace Drupal\block\Controller;
 
 use Drupal\Core\Entity\Controller\EntityListController;
 use Drupal\Core\Extension\ThemeHandlerInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Attribute\Route;
 
 /**
  * Defines a controller to list blocks.
@@ -41,6 +43,21 @@ class BlockListController extends EntityListController {
    *   A render array as expected by
    *   \Drupal\Core\Render\RendererInterface::render().
    */
+  #[Route(
+    path: '/admin/structure/block',
+    name: 'block.admin_display',
+    requirements: ['_permission' => 'administer blocks'],
+    defaults: ['_title' => new TranslatableMarkup('Block layout')],
+  )]
+  #[Route(
+    path: '/admin/structure/block/list/{theme}',
+    name: 'block.admin_display_theme',
+    requirements: [
+      '_access_theme' => 'TRUE',
+      '_permission' => 'administer blocks',
+    ],
+    defaults: ['_title' => new TranslatableMarkup('Block layout')],
+  )]
   public function listing($theme = NULL, ?Request $request = NULL) {
     $theme = $theme ?: $this->config('system.theme')->get('default');
     if (!$this->themeHandler->hasUi($theme)) {
