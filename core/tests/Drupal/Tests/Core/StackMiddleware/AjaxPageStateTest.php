f3333be0f19034af3720460ad2c2e8ff49d5de07
catch <6915-catch@users.noreply.drupalcode.org>
2026-08-06 09:10:42 +0900

    fix: #3603762 HTML injection in ajax_page_state GET parameter
    By: prudloff
    By: longwave
    By: smustgrave
    (cherry picked from commit 2fdf57a0ce666f140de1f95875660c277ab1215d)

--- core/tests/Drupal/Tests/Core/StackMiddleware/AjaxPageStateTest.php
+++ core/tests/Drupal/Tests/Core/StackMiddleware/AjaxPageStateTest.php
@@ -61,32 +61,52 @@ class AjaxPageStateTest extends UnitTestCase {
    * Provides data for testHandle().
    */
   public static function providerHandle(): array {
-    $foo_bar = UrlHelper::compressQueryParameter('foo,bar');
-    $foo_baz = UrlHelper::compressQueryParameter('foo,baz');
+    $foo_bar = UrlHelper::compressQueryParameter('core/foo,core/bar');
+    $foo_baz = UrlHelper::compressQueryParameter('core/foo,core/baz');
     $data = [];
     $data['only query'] = [
       $foo_bar,
       NULL,
-      'foo,bar',
+      'core/foo,core/bar',
       NULL,
     ];
     $data['only request'] = [
       NULL,
       $foo_bar,
       NULL,
-      'foo,bar',
+      'core/foo,core/bar',
     ];
     $data['matching'] = [
       $foo_bar,
       $foo_bar,
-      'foo,bar',
-      'foo,bar',
+      'core/foo,core/bar',
+      'core/foo,core/bar',
     ];
     $data['different'] = [
       $foo_baz,
       $foo_bar,
-      'foo,bar,baz',
-      'foo,bar,baz',
+      'core/foo,core/bar,core/baz',
+      'core/foo,core/bar,core/baz',
+    ];
+    $data['invalid libraries discarded'] = [
+      UrlHelper::compressQueryParameter('<b>foo'),
+      NULL,
+      NULL,
+      NULL,
+    ];
+    $data['mixed valid and invalid'] = [
+      UrlHelper::compressQueryParameter('core/drupal,<b>foo'),
+      NULL,
+      'core/drupal',
+      NULL,
+    ];
+    // A name containing a slash is structurally valid; the middleware does not
+    // need to filter it because the asset system ignores unknown libraries.
+    $data['slash-containing name preserved'] = [
+      UrlHelper::compressQueryParameter('core/<b>foo'),
+      NULL,
+      'core/<b>foo',
+      NULL,
     ];
     return $data;
   }
