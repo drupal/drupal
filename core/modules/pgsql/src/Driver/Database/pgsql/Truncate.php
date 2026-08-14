7bc7faf1836fc3a588a0d725b3fcbe655f24b511
catch <6915-catch@users.noreply.drupalcode.org>
2026-08-14 20:08:43 +0900

    task: #3615225 Truncate queries on PostgreSQL do not need to fallback to delete queries
    By: daffie
    (cherry picked from commit d54d3fe0eee09c909037c9fe37c18e44ec25d1d5)

--- core/modules/pgsql/src/Driver/Database/pgsql/Truncate.php
+++ core/modules/pgsql/src/Driver/Database/pgsql/Truncate.php
@@ -4,6 +4,8 @@ namespace Drupal\pgsql\Driver\Database\pgsql;
 
 use Drupal\Core\Database\Query\Truncate as QueryTruncate;
 
+// cspell:ignore MVCC
+
 /**
  * PostgreSQL implementation of \Drupal\Core\Database\Query\Truncate.
  */
@@ -28,6 +30,28 @@ class Truncate extends QueryTruncate {
     if (isset($savepoint)) {
       $savepoint->commitOrRelease();
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * On PostgreSQL, TRUNCATE is fully transactional: it can be rolled back
+   * and does not cause an implicit commit, so there is no need to fall back
+   * to the slower DELETE query when inside a transaction. TRUNCATE is much
+   * faster on large tables and, unlike DELETE, does not leave dead rows
+   * behind for VACUUM to clean up.
+   *
+   * Note that TRUNCATE takes an ACCESS EXCLUSIVE lock on the table that is
+   * held until the transaction ends, and it is not MVCC-safe with respect to
+   * concurrent transactions using a snapshot taken before the truncation.
+   *
+   * @see https://www.postgresql.org/docs/current/sql-truncate.html
+   */
+  public function __toString() {
+    // Create a sanitized comment string to prepend to the query.
+    $comments = $this->connection->makeComment($this->comments);
+
+    return $comments . 'TRUNCATE {' . $this->connection->escapeTable($this->table) . '} ';
   }
 
 }
