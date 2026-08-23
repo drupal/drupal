56283a6681fa6f8064146560444fc3d4b3a99b6c
catch <6915-catch@users.noreply.drupalcode.org>
2026-08-23 10:25:02 +0100

    task: #3615122 Move test modules to 'Testing' package
    By: quietone
    By: kieran.cott
    By: longwave

--- core/tests/Drupal/Tests/Core/Extension/ModuleInfoTest.php
+++ core/tests/Drupal/Tests/Core/Extension/ModuleInfoTest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\Core\Extension;
 
+// cspell:ignore nyan
+
 use Drupal\Component\Serialization\Yaml;
+use Drupal\Core\Extension\ExtensionDiscovery;
 use Drupal\KernelTests\FileSystemModuleDiscoveryDataProviderTrait;
 use Drupal\Tests\UnitTestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
@@ -27,6 +30,43 @@ class ModuleInfoTest extends UnitTestCase {
     $info = Yaml::decode(file_get_contents($module_directory . '/' . $module . '.info.yml'));
     $this->assertArrayHasKey('version', $info);
     $this->assertEquals('VERSION', $info['version']);
+  }
+
+  /**
+   * Tests that non-hidden test modules use their expected package.
+   */
+  #[DataProvider('providerTestTestModulePackages')]
+  public function testTestModulePackages(string $filename, string $expected_package): void {
+    $info = Yaml::decode(file_get_contents($filename));
+    $this->assertSame($expected_package, $info['package'] ?? NULL);
+  }
+
+  /**
+   * Provides test module info files and their expected packages.
+   */
+  public static function providerTestTestModulePackages(): \Generator {
+    $root = dirname(__DIR__, 6);
+    // These modules deliberately use other packages to test module metadata.
+    $expected_packages = [
+      'config_mapping_test' => 'Core',
+      'experimental_module_requirements_test' => 'Core (Experimental)',
+      'experimental_module_test' => 'Core (Experimental)',
+      'nyan_cat' => 'Core',
+    ];
+
+    $modules = (new ExtensionDiscovery($root))->scan('module', TRUE);
+    foreach ($modules as $module) {
+      if (!str_contains($module->getPath(), '/tests/modules/')) {
+        continue;
+      }
+      $filename = $root . '/' . $module->getPathname();
+      $info = Yaml::decode(file_get_contents($filename));
+      if (!empty($info['hidden'])) {
+        continue;
+      }
+      $package = $expected_packages[$module->getName()] ?? 'Testing';
+      yield $module->getPathname() => [$filename, $package];
+    }
   }
 
 }
