f6d327d17ed662de87f0e1b4fa14d1b901e9a385
godotislate <godotislate@3483619.no-reply.drupal.org>
2026-08-24 10:33:00 -0700

    task: #3593789 Extend RouteSubscriberBase to define as well as alter routes
    By: longwave
    By: godotislate
    By: dcam

--- core/lib/Drupal/Core/Routing/RouteSubscriberBase.php
+++ core/lib/Drupal/Core/Routing/RouteSubscriberBase.php
@@ -6,9 +6,22 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
- * Provides a base implementation for RouteSubscriber.
+ * Provides a base implementation for routing event subscribers.
  */
 abstract class RouteSubscriberBase implements EventSubscriberInterface {
+
+  /**
+   * Provides new routes during the RoutingEvents::STATIC phase.
+   *
+   * This is the object-oriented equivalent of a 'route_callbacks' entry in a
+   * *.routing.yml file.
+   *
+   * @return \Symfony\Component\Routing\RouteCollection
+   *   A collection of new routes to add.
+   */
+  protected function buildRoutes(): RouteCollection {
+    return new RouteCollection();
+  }
 
   /**
    * Alters existing routes for a specific collection.
@@ -16,14 +29,25 @@ abstract class RouteSubscriberBase implements EventSubscriberInterface {
    * @param \Symfony\Component\Routing\RouteCollection $collection
    *   The route collection for adding routes.
    */
-  abstract protected function alterRoutes(RouteCollection $collection);
+  protected function alterRoutes(RouteCollection $collection) {}
 
   /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents(): array {
+    $events[RoutingEvents::STATIC] = 'onBuildRoutes';
     $events[RoutingEvents::ALTER] = 'onAlterRoutes';
     return $events;
+  }
+
+  /**
+   * Delegates the route building to self::buildRoutes().
+   *
+   * @param \Drupal\Core\Routing\RouteBuildEvent $event
+   *   The route build event.
+   */
+  public function onBuildRoutes(RouteBuildEvent $event): void {
+    $event->getRouteCollection()->addCollection($this->buildRoutes());
   }
 
   /**
