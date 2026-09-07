cdefa18d84925de89b1419b1c47ad270a77ef882
Dave Long <dave@longwaveconsulting.com>
2026-09-07 17:26:57 +0100

    test: #3608939 Move/split remaining parts of StandardPerformanceTest and StandardJavascriptTest
    By: gábor hojtsy
    By: catch
    By: berdir

--- core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
+++ core/profiles/standard/tests/src/FunctionalJavascript/StandardPerformanceTest.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\standard\FunctionalJavascript;
 
-use Drupal\Core\Cache\Cache;
 use Drupal\FunctionalJavascriptTests\PerformanceTestBase;
-use Drupal\Tests\PerformanceData;
-use Drupal\user\Entity\Role;
-use Drupal\user\RoleInterface;
 use Drupal\user\UserInterface;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\RequiresPhpExtension;
@@ -42,94 +38,26 @@ class StandardPerformanceTest extends PerformanceTestBase {
   protected ?UserInterface $user = NULL;
 
   /**
-   * {@inheritdoc}
-   */
-  protected function setUp(): void {
-    parent::setUp();
-    // Create a content type and node to test performance for.
-    $this->drupalCreateContentType(['type' => 'test_content', 'name' => 'Test Content']);
-    $this->drupalCreateNode(['type' => 'test_content']);
-    // Grant the anonymous user the permission to look at user profiles.
-    Role::loadOverrideFree(RoleInterface::ANONYMOUS_ID)->grantPermissions([
-      'access user profiles',
-    ])->save();
-  }
-
-  /**
-   * Tests performance of the standard profile.
-   */
-  public function testStandardPerformance(): void {
-    $this->testAnonymous();
-    $this->testAdmin();
-  }
-
-  /**
-   * Tests performance for anonymous users.
-   */
-  protected function testAnonymous(): void {
-    // Request the front page, then immediately clear all object caches, so that
-    // aggregates and image styles are created on disk but otherwise caches are
-    // empty.
-    $this->drupalGet('');
-    // Give time for big pipe placeholders, asset aggregate requests, and post
-    // response tasks to finish processing and write to any caches before
-    // clearing caches again.
-    sleep(2);
-    foreach (Cache::getBins() as $bin) {
-      $bin->deleteAll();
-    }
-    // Now visit a different page to warm some caches.
-    $this->drupalGet('user/password');
-    // Ensure everything finishes before we collect performance data.
-    sleep(2);
-
-    // Test frontpage.
-    $performance_data = $this->collectPerformanceData(function () {
-      $this->drupalGet('');
-    }, 'standardFrontPage');
-    $this->assertNoJavaScript($performance_data);
-    $this->assertQueriesByName('standardFrontPage', $performance_data->getQueries());
-    $this->assertMetricsByName('standardFrontPage', $performance_data);
-
-    // Test node page.
-    $performance_data = $this->collectPerformanceData(function () {
-      $this->drupalGet('node/1');
-    }, 'standardNodePage');
-
-    $this->assertQueriesByName('standardNodePage', $performance_data->getQueries());
-    $this->assertMetricsByName('standardNodePage', $performance_data);
-
-    // Test user profile page.
-    $this->user = $this->drupalCreateUser();
-    $performance_data = $this->collectPerformanceData(function () {
-      $this->drupalGet('user/' . $this->user->id());
-    }, 'standardUserPage');
-    $this->assertNoJavaScript($performance_data);
-    $this->assertQueriesByName('standardUserPage', $performance_data->getQueries());
-    $this->assertMetricsByName('standardUserPage', $performance_data);
-  }
-
-  /**
    * Tests performance of a logged-in admin user with the navigation toolbar.
    */
-  protected function testAdmin(): void {
+  public function testAdmin(): void {
     $admin_user = $this->drupalCreateUser();
     $admin_user->addRole('administrator');
     $admin_user->save();
 
     $this->drupalLogin($admin_user);
-    // Request the node/1 page twice to ensure all cache collectors are fully
+    // Request the front page twice to ensure all cache collectors are fully
     // warmed. The exact contents of cache collectors depends on the order in
     // which requests complete so this ensures that the second request completes
     // after asset aggregates are served.
-    $this->drupalGet('node/1');
+    $this->drupalGet('');
     sleep(1);
-    $this->drupalGet('node/1');
+    $this->drupalGet('');
     // Flush the dynamic page cache to simulate visiting a page that is not
     // already fully cached.
     \Drupal::cache('dynamic_page_cache')->deleteAll();
     $performance_data = $this->collectPerformanceData(function () {
-      $this->drupalGet('node/1');
+      $this->drupalGet('');
     }, 'testAdmin');
 
     $this->assertQueriesByName('testAdmin', $performance_data->getQueries());
@@ -139,26 +67,5 @@ class StandardPerformanceTest extends PerformanceTestBase {
     // not per-user, to ensure it scales for authenticated admins.
     $this->assertIsObject(\Drupal::cache('render')->get('navigation:navigation:[languages:language_interface]=en:[theme]=stark:[user.permissions]=is-admin'));
   }
-
-  /**
-   * Passes if no JavaScript is found on the page.
-   *
-   * @param \Drupal\Tests\PerformanceData $performance_data
-   *   A PerformanceData value object.
-   *
-   * @internal
-   */
-  protected function assertNoJavaScript(PerformanceData $performance_data): void {
-    // Ensure drupalSettings is not set.
-    $settings = $this->getDrupalSettings();
-    $this->assertEmpty($settings, 'drupalSettings is not set.');
-    $this->assertSession()->responseNotMatches('/\.js/');
-    $this->assertSame(0, $performance_data->getScriptCount());
-  }
-
-  /**
-   * Provides an empty implementation to prevent the resetting of caches.
-   */
-  protected function refreshVariables(): void {}
 
 }
