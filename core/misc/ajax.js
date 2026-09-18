6b3b36937414e03c0434929a73851f9dba88163f
quietone <quietone@2572884.no-reply.drupal.org>
2026-09-18 15:44:35 +1200

    task: #3623168 Update deprecation notices in ajax.js
    By: shahul hameed
    By: abenbow

--- core/misc/ajax.js
+++ core/misc/ajax.js
@@ -1237,7 +1237,7 @@
    * @param {object} response
    *   The response from the Ajax request.
    *
-   * @deprecated in drupal:8.6.0 and is removed from drupal:12.0.0.
+   * @deprecated in drupal:8.6.0 and is removed from drupal:13.0.0.
    *   Use data with desired wrapper.
    *
    * @see https://www.drupal.org/node/2940704
@@ -1266,7 +1266,7 @@
    * @param {jQuery} $elements
    *   Response elements after parsing.
    *
-   * @deprecated in drupal:8.6.0 and is removed from drupal:12.0.0.
+   * @deprecated in drupal:8.6.0 and is removed from drupal:13.0.0.
    *   Use data with desired wrapper.
    *
    * @see https://www.drupal.org/node/2940704
