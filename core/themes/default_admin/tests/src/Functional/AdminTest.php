bea4f5e45b541be0bffbc6a86f94a5dc541e112a
Andrei Mateescu <amateescu@729614.no-reply.drupal.org>
2026-09-21 17:59:54 +0300

    fix: #3618586 default_admin loses the footer group on forms that set #tree, silently unpublishing entities on save
    By: f0ns
    By: dcam
    By: smustgrave
    (cherry picked from commit eaba66f418831210acc6bc5b49c61d171853455b)

--- core/themes/default_admin/tests/src/Functional/AdminTest.php
+++ core/themes/default_admin/tests/src/Functional/AdminTest.php
@@ -27,7 +27,7 @@ class AdminTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected $defaultTheme = 'stark';
+  protected $defaultTheme = 'default_admin';
 
   /**
    * {@inheritdoc}
@@ -35,7 +35,6 @@ class AdminTest extends BrowserTestBase {
   protected function setUp(): void {
     parent::setUp();
 
-    $this->assertTrue(\Drupal::service('theme_installer')->install(['default_admin']));
     $this->container->get('config.factory')
       ->getEditable('system.theme')
       ->set('default', 'default_admin')
